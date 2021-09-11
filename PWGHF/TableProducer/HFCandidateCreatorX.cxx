8e49fcacdb62667427e048870ab0f6be92ffbabd
lucamicheletti93 <38209984+lucamicheletti93@users.noreply.github.com>
2021-09-11 09:13:16 +0200

    PWGHF:  setting minimum pion pT in X candidate creator (#55)
    * Adding a threshold on the pion pt in the X candidate creator
    
    Co-authored-by: Luca Micheletti <lmichele@alicecerno2.cern.ch>

--- PWGHF/TableProducer/HFCandidateCreatorX.cxx
+++ PWGHF/TableProducer/HFCandidateCreatorX.cxx
@@ -49,6 +49,7 @@ struct HFCandidateCreatorX {
   Configurable<double> d_maxdzini{"d_maxdzini", 4., "reject (if>0) PCA candidate if tracks DZ exceeds threshold"};
   Configurable<double> d_minparamchange{"d_minparamchange", 1.e-3, "stop iterations if largest change of any X is smaller than this"};
   Configurable<double> d_minrelchi2change{"d_minrelchi2change", 0.9, "stop iterations is chi2/chi2old > this"};
+  Configurable<double> ptPionMin{"ptPionMin", 0.15, "minimum pion pT threshold (GeV/c)"};
   Configurable<bool> b_dovalplots{"b_dovalplots", true, "do validation plots"};
 
   OutputObj<TH1F> hMassJpsi{TH1F("hMassJpsi", "J/#psi candidates;inv. mass (#e+ e-) (GeV/#it{c}^{2});entries", 500, 0., 5.)};
@@ -134,6 +135,9 @@ struct HFCandidateCreatorX {
         if (trackPos.globalIndex() == index0Jpsi) {
           continue;
         }
+        if (trackPos.pt() < ptPionMin) {
+          continue;
+        }
 
         // loop over pi- candidates
         for (auto& trackNeg : tracks) {
@@ -141,6 +145,9 @@ struct HFCandidateCreatorX {
             continue;
           }
           if (trackNeg.globalIndex() == index1Jpsi) {
+            continue;
+          }
+          if (trackNeg.pt() < ptPionMin) {
             continue;
           }
 
