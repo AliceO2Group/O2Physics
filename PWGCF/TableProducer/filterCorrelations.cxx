2870c110c551b0a085276c876884e63719256900
Jan Fiete <jgrosseo@cern.ch>
2021-09-23 17:45:52 +0200

    Unifying correlation and correlationfiltered task (#113)
    Renaming filter

--- PWGCF/TableProducer/filterCorrelations.cxx
+++ PWGCF/TableProducer/filterCorrelations.cxx
@@ -34,8 +34,11 @@ struct FilterCF {
   O2_DEFINE_CONFIGURABLE(cfgCutEta, float, 0.8f, "Eta range for tracks")
 
   // Filters and input definitions
-  Filter collisionFilter = nabs(aod::collision::posZ) < cfgCutVertex && aod::cent::centV0M <= 80.0f;
-  Filter trackFilter = (nabs(aod::track::eta) < cfgCutEta) && (aod::track::pt > cfgCutPt) && ((aod::track::isGlobalTrack == (uint8_t) true) || (aod::track::isGlobalTrackSDD == (uint8_t) true));
+  Filter collisionZVtxFilter = nabs(aod::collision::posZ) < cfgCutVertex;
+  Filter centralityFilter = aod::cent::centV0M >= 0.0f && aod::cent::centV0M <= 100.0f;
+  Filter trackFilter = (nabs(aod::track::eta) < cfgCutEta) && (aod::track::pt > cfgCutPt);
+  Filter collisionVertexTypeFilter = (aod::collision::flags & (uint16_t)aod::collision::CollisionFlagsRun2::Run2VertexerTracks) == (uint16_t)aod::collision::CollisionFlagsRun2::Run2VertexerTracks;
+  Filter trackSelection = (aod::track::isGlobalTrack == (uint8_t) true) || (aod::track::isGlobalTrackSDD == (uint8_t) true);
 
   OutputObj<TH3F> yields{TH3F("yields", "centrality vs pT vs eta", 100, 0, 100, 40, 0, 20, 100, -2, 2)};
   OutputObj<TH3F> etaphi{TH3F("etaphi", "centrality vs eta vs phi", 100, 0, 100, 100, -2, 2, 200, 0, 2 * M_PI)};
@@ -43,21 +46,11 @@ struct FilterCF {
   Produces<aod::CFCollisions> outputCollisions;
   Produces<aod::CFTracks> outputTracks;
 
-  void init(o2::framework::InitContext&)
-  {
-  }
-
   void process(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels, aod::Cents>>::iterator const& collision, aod::BCsWithTimestamps const&, soa::Filtered<soa::Join<aod::Tracks, aod::TrackSelection>> const& tracks)
   {
     LOGF(info, "Tracks for collision: %d | Vertex: %.1f (%d) | INT7: %d | V0M: %.1f", tracks.size(), collision.posZ(), collision.flags(), collision.sel7(), collision.centV0M());
 
     if (!collision.alias()[kINT7] || !collision.sel7()) {
-      return;
-    }
-
-    // vertex range already checked as filter, but bitwise operations not yet supported
-    // TODO (collision.flags() != 0) can be removed with next conversion (AliPhysics >= 20210305)
-    if ((collision.flags() != 0) && ((collision.flags() & aod::collision::CollisionFlagsRun2::Run2VertexerTracks) != aod::collision::CollisionFlagsRun2::Run2VertexerTracks)) {
       return;
     }
 
