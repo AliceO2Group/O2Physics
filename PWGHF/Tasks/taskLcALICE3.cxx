ef052dce22db9f07041a61fd5a4dbf1b56ae0fff
skundu692 <86804743+skundu692@users.noreply.github.com>
2021-10-07 14:52:35 +0200

    PWGHF: Improvement in temprary Lc PID task (#158)
    * PWGHF: Improvement in temprary Lc PID task
    
    Add both LctoPKPi and LctoPiKP
    
    * PWGHF: Fixes braces, remove sqrt and improvement according to coding guidelines
    
    * PWGHF:hard-coded PDG codes replaced with name values

--- PWGHF/Tasks/taskLcALICE3.cxx
+++ PWGHF/Tasks/taskLcALICE3.cxx
@@ -46,10 +46,6 @@ struct TaskLcALICE3MC {
      {"hMassSigBkgLcTOFPID", "3-prong candidates (not checked);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0, 4.0}}}},
      {"hMassSigLcTOFPID", "3-prong candidates (matched);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0., 4.}}}},
      {"hMassBkgLcTOFPID", "3-prong candidates (checked);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0., 4.}}}},
-     {"hMassSigBkgLcRICHPID", "3-prong candidates (not checked);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0, 4.0}}}},
-     {"hMassSigLcRICHPID", "3-prong candidates (matched);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0., 4.}}}},
-     {"hMassBkgLcRICHPID", "3-prong candidates (checked);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0., 4.}}}},
-     {"hMassReflBkgLcRICHPID", "3-prong candidates (checked);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0., 4.}}}},
      {"hMassSigBkgLcTOFplusRICHPID", "3-prong candidates (not checked);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0, 4.0}}}},
      {"hMassSigLcTOFplusRICHPID", "3-prong candidates (matched);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0., 4.}}}},
      {"hMassBkgLcTOFplusRICHPID", "3-prong candidates (checked);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0., 4.}}}},
@@ -57,8 +53,7 @@ struct TaskLcALICE3MC {
      {"hMassSigLcPerfectPID", "3-prong candidates (matched);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0., 4.}}}},
      {"hMassBkgLcPerfectPID", "3-prong candidates (checked);#it{m}_{inv} (GeV/#it{c}^{2}); #it{p}_{T}; #it{y}", {HistType::kTH3F, {{500, 1.6, 3.1}, {150, 0., 30.}, {8, 0., 4.}}}}}};
 
-  Filter filterSelectCandidates = (aod::hf_selcandidate_lc_ALICE3::isSelHFFlag >= 1);
-
+  Filter filterSelectCandidates = (aod::hf_selcandidate_lc_ALICE3::isSelLcPKPiNoPID == 1 || aod::hf_selcandidate_lc_ALICE3::isSelLcPiKPNoPID == 1);
   void process(soa::Filtered<soa::Join<aod::HfCandProng3, aod::HFSelLcCandidateALICE3, aod::HfCandProng3MCRec>> const& candidates,
                soa::Join<aod::McParticles, aod::HfCandProng3MCGen> const& particlesMC, aod::BigTracksMC const& tracks)
   {
@@ -71,53 +66,75 @@ struct TaskLcALICE3MC {
       }
 
       auto massLc = InvMassLcpKpi(candidate);
-      auto massLcbar = InvMassLcpiKp(candidate);
+      auto massLcSwap = InvMassLcpiKp(candidate);
       auto ptCandidate = candidate.pt();
       auto rapidityCandidate = std::abs(YLc(candidate));
 
-      if (candidate.isSelLcNoPID() >= 1) {
+      if (candidate.isSelLcPKPiNoPID() == 1) {
         registry.fill(HIST("hMassSigBkgLcNoPID"), massLc, ptCandidate, rapidityCandidate);
-        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi)) {
+        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi) && candidate.isSelLcPKPiPerfectPID() == 1) {
           registry.fill(HIST("hMassSigLcNoPID"), massLc, ptCandidate, rapidityCandidate);
         } else {
           registry.fill(HIST("hMassBkgLcNoPID"), massLc, ptCandidate, rapidityCandidate);
         }
       }
+      if (candidate.isSelLcPiKPNoPID() == 1) {
+        registry.fill(HIST("hMassSigBkgLcNoPID"), massLcSwap, ptCandidate, rapidityCandidate);
+        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi) && candidate.isSelLcPiKPPerfectPID() == 1) {
+          registry.fill(HIST("hMassSigLcNoPID"), massLcSwap, ptCandidate, rapidityCandidate);
+        } else {
+          registry.fill(HIST("hMassBkgLcNoPID"), massLcSwap, ptCandidate, rapidityCandidate);
+        }
+      }
 
-      if (candidate.isSelLcTOFPID() >= 1) {
+      if (candidate.isSelLcPKPiTOFPID() == 1) {
         registry.fill(HIST("hMassSigBkgLcTOFPID"), massLc, ptCandidate, rapidityCandidate);
-        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi)) {
+        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi) && candidate.isSelLcPKPiPerfectPID() == 1) {
           registry.fill(HIST("hMassSigLcTOFPID"), massLc, ptCandidate, rapidityCandidate);
         } else {
           registry.fill(HIST("hMassBkgLcTOFPID"), massLc, ptCandidate, rapidityCandidate);
         }
       }
-
-      if (candidate.isSelLcRICHPID() >= 1) {
-        registry.fill(HIST("hMassSigBkgLcRICHPID"), massLc, ptCandidate, rapidityCandidate);
-        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi)) {
-          registry.fill(HIST("hMassSigLcRICHPID"), massLc, ptCandidate, rapidityCandidate);
+      if (candidate.isSelLcPiKPTOFPID() == 1) {
+        registry.fill(HIST("hMassSigBkgLcTOFPID"), massLcSwap, ptCandidate, rapidityCandidate);
+        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi) && candidate.isSelLcPiKPPerfectPID() == 1) {
+          registry.fill(HIST("hMassSigLcTOFPID"), massLcSwap, ptCandidate, rapidityCandidate);
         } else {
-          if (candidate.flagMCMatchRec() == -(1 << DecayType::LcToPKPi)) {
-            registry.fill(HIST("hMassReflBkgLcRICHPID"), massLc, ptCandidate, rapidityCandidate);
-          }
-          registry.fill(HIST("hMassBkgLcRICHPID"), massLc, ptCandidate, rapidityCandidate);
+          registry.fill(HIST("hMassBkgLcTOFPID"), massLcSwap, ptCandidate, rapidityCandidate);
         }
       }
-      if (candidate.isSelLcTOFplusRICHPID() >= 1) {
+
+      if (candidate.isSelLcPKPiTOFplusRICHPID() == 1) {
         registry.fill(HIST("hMassSigBkgLcTOFplusRICHPID"), massLc, ptCandidate, rapidityCandidate);
-        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi)) {
+        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi) && candidate.isSelLcPKPiPerfectPID() == 1) {
           registry.fill(HIST("hMassSigLcTOFplusRICHPID"), massLc, ptCandidate, rapidityCandidate);
         } else {
           registry.fill(HIST("hMassBkgLcTOFplusRICHPID"), massLc, ptCandidate, rapidityCandidate);
         }
       }
-      if (candidate.isSelLcPerfectPID() >= 1) {
+      if (candidate.isSelLcPiKPTOFplusRICHPID() == 1) {
+        registry.fill(HIST("hMassSigBkgLcTOFplusRICHPID"), massLcSwap, ptCandidate, rapidityCandidate);
+        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi) && candidate.isSelLcPiKPPerfectPID() == 1) {
+          registry.fill(HIST("hMassSigLcTOFplusRICHPID"), massLcSwap, ptCandidate, rapidityCandidate);
+        } else {
+          registry.fill(HIST("hMassBkgLcTOFplusRICHPID"), massLcSwap, ptCandidate, rapidityCandidate);
+        }
+      }
+
+      if (candidate.isSelLcPKPiPerfectPID() == 1) {
         registry.fill(HIST("hMassSigBkgLcPerfectPID"), massLc, ptCandidate, rapidityCandidate);
         if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi)) {
           registry.fill(HIST("hMassSigLcPerfectPID"), massLc, ptCandidate, rapidityCandidate);
         } else {
           registry.fill(HIST("hMassBkgLcPerfectPID"), massLc, ptCandidate, rapidityCandidate);
+        }
+      }
+      if (candidate.isSelLcPiKPPerfectPID() == 1) {
+        registry.fill(HIST("hMassSigBkgLcPerfectPID"), massLcSwap, ptCandidate, rapidityCandidate);
+        if (candidate.flagMCMatchRec() == (1 << DecayType::LcToPKPi)) {
+          registry.fill(HIST("hMassSigLcPerfectPID"), massLcSwap, ptCandidate, rapidityCandidate);
+        } else {
+          registry.fill(HIST("hMassBkgLcPerfectPID"), massLcSwap, ptCandidate, rapidityCandidate);
         }
       }
     }
