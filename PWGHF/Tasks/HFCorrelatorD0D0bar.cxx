694f61aebda936258f34434dde9ed28bf6f178b1
fcolamar <fabio.colamaria@ba.infn.it>
2021-10-07 11:13:28 +0200

    PWGHF: Adding centrality selection to DDbar correlation framework (#155)
    * PWGHF: Adding centrality selection to DDbar correlation framework
    
    * Update to previous commit on DDbar centrality selection
    
    * Further update to a histogram axis

--- PWGHF/Tasks/HFCorrelatorD0D0bar.cxx
+++ PWGHF/Tasks/HFCorrelatorD0D0bar.cxx
@@ -18,6 +18,7 @@
 #include "Framework/HistogramRegistry.h"
 #include "PWGHF/DataModel/HFSecondaryVertex.h"
 #include "PWGHF/DataModel/HFCandidateSelectionTables.h"
+#include "Common/DataModel/Centrality.h"
 
 using namespace o2;
 using namespace o2::framework;
@@ -77,6 +78,8 @@ struct HfCorrelatorD0D0bar {
      {"hEta", "D0,D0bar candidates;candidate #it{#eta};entries", {HistType::kTH1F, {{100, -5., 5.}}}},
      {"hPhi", "D0,D0bar candidates;candidate #it{#varphi};entries", {HistType::kTH1F, {{32, 0., 2. * o2::constants::math::PI}}}},
      {"hY", "D0,D0bar candidates;candidate #it{#y};entries", {HistType::kTH1F, {{100, -5., 5.}}}},
+     {"hCentralityPreSelection", "centrality prior to selection;centrality percentile;entries", {HistType::kTH1F, {{101, -1., 100.}}}},
+     {"hCentrality", "centrality;centrality percentile;entries", {HistType::kTH1F, {{101, -1., 100.}}}},
      {"hDDbarVsEtaCut", "D0,D0bar pairs vs #eta cut;#eta_{max};entries", {HistType::kTH2F, {{(int)(maxEtaCut / incrementEtaCut), 0., maxEtaCut}, {(int)(ptThresholdForMaxEtaCut / incrementPtThreshold), 0., ptThresholdForMaxEtaCut}}}}}};
 
   Configurable<int> selectionFlagD0{"selectionFlagD0", 1, "Selection Flag for D0"};
@@ -86,6 +89,8 @@ struct HfCorrelatorD0D0bar {
   Configurable<std::vector<double>> bins{"ptBinsForMassAndEfficiency", std::vector<double>{o2::analysis::hf_cuts_d0_topik::pTBins_v}, "pT bin limits for candidate mass plots and efficiency"};
   Configurable<std::vector<double>> efficiencyDmeson{"efficiencyDmeson", std::vector<double>{efficiencyDmeson_v}, "Efficiency values for D0 meson"};
   Configurable<int> flagApplyEfficiency{"efficiencyFlagD", 1, "Flag for applying D-meson efficiency weights"};
+  Configurable<double> centMin{"centMin", -1., "minimum centrality accepted"};
+  Configurable<double> centMax{"centMax", 100., "maximum centrality accepted"};
 
   void init(o2::framework::InitContext&)
   {
@@ -96,8 +101,15 @@ struct HfCorrelatorD0D0bar {
 
   Filter filterSelectCandidates = (aod::hf_selcandidate_d0::isSelD0 >= selectionFlagD0 || aod::hf_selcandidate_d0::isSelD0bar >= selectionFlagD0bar);
 
-  void process(aod::Collision const& collision, soa::Filtered<soa::Join<aod::HfCandProng2, aod::HFSelD0Candidate>> const& candidates)
+  void process(soa::Join<aod::Collisions, aod::Cents>::iterator const& collision, soa::Filtered<soa::Join<aod::HfCandProng2, aod::HFSelD0Candidate>> const& candidates)
   {
+    float centrality = collision.centV0M();
+    registry.fill(HIST("hCentralityPreSelection"), centrality);
+    if (centrality < centMin || centrality > centMax) {
+      return;
+    }
+    registry.fill(HIST("hCentrality"), centrality);
+
     for (auto& candidate1 : candidates) {
       if (cutYCandMax >= 0. && std::abs(YD0(candidate1)) > cutYCandMax) {
         continue;
@@ -199,6 +211,8 @@ struct HfCorrelatorD0D0barMcRec {
      {"hEtaMCRec", "D0,D0bar candidates - MC reco;candidate #it{#eta};entries", {HistType::kTH1F, {{100, -5., 5.}}}},
      {"hPhiMCRec", "D0,D0bar candidates - MC reco;candidate #it{#varphi};entries", {HistType::kTH1F, {{32, 0., 2. * o2::constants::math::PI}}}},
      {"hYMCRec", "D0,D0bar candidates - MC reco;candidate #it{#y};entries", {HistType::kTH1F, {{100, -5., 5.}}}},
+     {"hCentralityPreSelectionMCRec", "centrality prior to selection;centrality percentile;entries", {HistType::kTH1F, {{101, -1., 100.}}}},
+     {"hCentralityMCRec", "centrality;centrality percentile;entries", {HistType::kTH1F, {{101, -1., 100.}}}},
      {"hDDbarVsEtaCut", "D0,D0bar pairs vs #eta cut;#eta_{max};entries", {HistType::kTH2F, {{(int)(maxEtaCut / incrementEtaCut), 0., maxEtaCut}, {(int)(ptThresholdForMaxEtaCut / incrementPtThreshold), 0., ptThresholdForMaxEtaCut}}}}}};
 
   Configurable<int> selectionFlagD0{"selectionFlagD0", 1, "Selection Flag for D0"};
@@ -208,6 +222,8 @@ struct HfCorrelatorD0D0barMcRec {
   Configurable<std::vector<double>> bins{"ptBinsForMassAndEfficiency", std::vector<double>{o2::analysis::hf_cuts_d0_topik::pTBins_v}, "pT bin limits for candidate mass plots and efficiency"};
   Configurable<std::vector<double>> efficiencyDmeson{"efficiencyDmeson", std::vector<double>{efficiencyDmeson_v}, "Efficiency values for D0 meson"};
   Configurable<int> flagApplyEfficiency{"efficiencyFlagD", 1, "Flag for applying D-meson efficiency weights"};
+  Configurable<double> centMin{"centMin", -1., "minimum centrality accepted"};
+  Configurable<double> centMax{"centMax", 100., "maximum centrality accepted"};
 
   void init(o2::framework::InitContext&)
   {
@@ -221,8 +237,15 @@ struct HfCorrelatorD0D0barMcRec {
 
   Filter filterSelectCandidates = (aod::hf_selcandidate_d0::isSelD0 >= selectionFlagD0 || aod::hf_selcandidate_d0::isSelD0bar >= selectionFlagD0bar);
 
-  void process(aod::Collision const& collision, soa::Filtered<soa::Join<aod::HfCandProng2, aod::HFSelD0Candidate, aod::HfCandProng2MCRec>> const& candidates)
+  void process(soa::Join<aod::Collisions, aod::Cents>::iterator const& collision, soa::Filtered<soa::Join<aod::HfCandProng2, aod::HFSelD0Candidate, aod::HfCandProng2MCRec>> const& candidates)
   {
+    float centrality = collision.centV0M();
+    registry.fill(HIST("hCentralityPreSelectionMCRec"), centrality);
+    if (centrality < centMin || centrality > centMax) {
+      return;
+    }
+    registry.fill(HIST("hCentralityMCRec"), centrality);
+
     //MC reco level
     bool flagD0Signal = false;
     bool flagD0Reflection = false;
