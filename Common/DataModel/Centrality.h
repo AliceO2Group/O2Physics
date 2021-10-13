4054562025431c4fd049c45ec479cbca7470d761
Victor Gonzalez <victor.gonzalez@cern.ch>
2021-10-13 15:45:30 +0200

    Formating issues fixed

--- Common/DataModel/Centrality.h
+++ Common/DataModel/Centrality.h
@@ -17,12 +17,12 @@ namespace o2::aod
 {
 namespace cent
 {
-DECLARE_SOA_COLUMN(CentEstV0M, centV0M, float);                   //! Centrality percentile estimated from V0C+V0A multiplicities
+DECLARE_SOA_COLUMN(CentEstV0M, centV0M, float);                           //! Centrality percentile estimated from V0C+V0A multiplicities
 DECLARE_SOA_COLUMN(CentEstRun2SPDTracklets, centRun2SPDTracklets, float); //! Run2 centrality percentile estimated from SPD tracklets multiplicity
 DECLARE_SOA_COLUMN(CentEstRun2CL0, centRun2CL0, float);                   //! Run2 centrality percentile estimated from CL0 multiplicity
 DECLARE_SOA_COLUMN(CentEstRun2CL1, centRun2CL1, float);                   //! Run2 centrality percentile estimated from CL1 multiplicity
 } // namespace cent
-DECLARE_SOA_TABLE(CentV0Ms, "AOD", "CENTV0M", cent::CentEstV0M);          //! V0M estimated centrality table
+DECLARE_SOA_TABLE(CentV0Ms, "AOD", "CENTV0M", cent::CentEstV0M);                      //! V0M estimated centrality table
 DECLARE_SOA_TABLE(CentRun2SPDs, "AOD", "CENTRUN2SPD", cent::CentEstRun2SPDTracklets); //! Run2 SPD tracklets estimated centrality table
 DECLARE_SOA_TABLE(CentRun2CL0s, "AOD", "CENTRUN2CL0", cent::CentEstRun2CL0);          //! Run2 CL0 estimated centrality table
 DECLARE_SOA_TABLE(CentRun2CL1s, "AOD", "CENTRUN2CL1", cent::CentEstRun2CL1);          //! Run2 CL1 estimated centrality table
