2d31b8873e706a5ffbeff172185d1223baf9920b
Christian Sonnabend <sonnabendch@gmail.com>
2021-11-08 14:43:20 +0100

    - Changed TPCPIDResponse.h: No template-parameters, particle-id is passed to functions directly - Modified pidTPCFullTest.cxx accordingly - Adjusted for clang-format and checked that it builds

--- Common/Core/PID/TPCPIDResponse.h
+++ Common/Core/PID/TPCPIDResponse.h
@@ -16,25 +16,26 @@
 #ifndef O2_PID_TPC_RESPONSE_H_
 #define O2_PID_TPC_RESPONSE_H_
 
-#include <array>
+//  #include <array>
 #include <vector>
-#include "Framework/Logger.h"
+//  #include "Framework/Logger.h"
 
-// ROOT includes
-#include "Rtypes.h"
-#include "TMath.h"
-#include "TNamed.h"
+// // ROOT includes
+//  #include "Rtypes.h"
+//  #include "TMath.h"
+// // #include "TNamed.h"
 
-// O2 includes
-#include "ReconstructionDataFormats/PID.h"
-#include "Framework/ASoA.h"
-#include "Framework/AnalysisDataModel.h"
+// // O2 includes
+// #include "ReconstructionDataFormats/PID.h"
+// #include "Framework/ASoA.h"
+// #include "Framework/AnalysisDataModel.h"
+#include "TPCSimulation/Detector.h"
 
 namespace o2::pid::tpc
 {
 
 /// \brief Class to handle the TPC PID response
-template <typename TrackType, o2::track::PID::ID id>
+
 class TPCPIDResponse
 {
 
@@ -47,38 +48,32 @@ class TPCPIDResponse
   void SetResolutionParams(const std::array<float, 2>& resolutionParams) { mResolutionParams = resolutionParams; };
   void SetMIP(const float mip) { mMIP = mip; };
   void SetChargeFactor(const float chargeFactor) { mChargeFactor = chargeFactor; };
-  /// A: I would either rename or remove this function. From the name it is not clear what it does. The ones before already set everything
-  void Set(const std::array<float, 5>& betheBlochParameters,
-           const std::array<float, 2>& resolutionParameters,
-           const float chargeFactor = 2.3f,
-           const float mip = 50.f)
-  {
-    mBetheBlochParams = betheBlochParameters;
-    mResolutionParams = resolutionParameters;
-    mMIP = mip;
-    mChargeFactor = chargeFactor;
-  };
+  // // /// A: I would either rename or remove this function. From the name it is not clear what it does. The ones before already set everything
+  // // void Set(const std::array<float, 5>& betheBlochParameters,
+  // //          const std::array<float, 2>& resolutionParameters,
+  // //          const float chargeFactor = 2.3f,
+  // //          const float mip = 50.f)
+  // // {
+  // //   mBetheBlochParams = betheBlochParameters;
+  // //   mResolutionParams = resolutionParameters;
+  // //   mMIP = mip;
+  // //   mChargeFactor = chargeFactor;
+  // // };
 
   const std::array<float, 5> GetBetheBlochParams() const { return mBetheBlochParams; };
   const std::array<float, 2> GetResolutionParams() const { return mResolutionParams; };
   const float GetMIP() const { return mMIP; };
   const float GetChargeFactor() const { return mChargeFactor; };
 
-  /////////// Actual calculation on the tracks //////////////
-
-  /// Empirical ALEPH parameterization of the Bethe-Bloch formula, normalized to 1 at the minimum.
-  template <typename T>
-  static T BetheBlochAleph(T bg, T kp1, T kp2, T kp3, T kp4, T kp5);
-
   /// Gets the expected signal of the track
-  float GetExpectedSignal(const TrackType& track) const;
+  template <typename TrackType>
+  float GetExpectedSignal(const TrackType& track, const o2::track::PID::ID id) const;
   /// Gets the expected resolution of the track
-  float GetExpectedSigma(const TrackType& trk) const;
+  template <typename TrackType>
+  float GetExpectedSigma(const TrackType& trk, const o2::track::PID::ID id) const;
   /// Gets the number of sigmas with respect the expected value
-  float GetSeparation(const TrackType& trk) const;
-
-  /// Save to ccdb
-  /// Get object from ccdb
+  template <typename TrackType>
+  float GetSeparation(const TrackType& trk, const o2::track::PID::ID id) const;
 
  private:
   std::array<float, 5> mBetheBlochParams = {0.0320981, 19.9768, 2.52666e-16, 2.72123, 6.08092};
@@ -89,6 +84,29 @@ class TPCPIDResponse
   ClassDef(TPCPIDResponse, 1);
 
 }; // class TPCPIDResponse
+
+/// Get expected Signal of the measurement
+template <typename TrackType>
+inline float TPCPIDResponse::GetExpectedSignal(const TrackType& track, const o2::track::PID::ID id) const
+{
+  const float bethe = mMIP * o2::tpc::Detector::BetheBlochAleph(track.tpcInnerParam() / o2::track::pid_constants::sMasses[id], mBetheBlochParams[0], mBetheBlochParams[1], mBetheBlochParams[2], mBetheBlochParams[3], mBetheBlochParams[4]) * pow((float)o2::track::pid_constants::sCharges[id], mChargeFactor);
+  return bethe >= 0.f ? bethe : 0.f;
+}
+
+/// Gets the expected resolution of the measurement
+template <typename TrackType>
+inline float TPCPIDResponse::GetExpectedSigma(const TrackType& track, const o2::track::PID::ID id) const
+{
+  const float reso = track.tpcSignal() * mResolutionParams[0] * ((float)track.tpcNClsFound() > 0 ? sqrt(1. + mResolutionParams[1] / (float)track.tpcNClsFound()) : 1.f);
+  return reso >= 0.f ? reso : 0.f;
+}
+
+/// Gets the separation between the actual signal and the expected signal
+template <typename TrackType>
+inline float TPCPIDResponse::GetSeparation(const TrackType& trk, const o2::track::PID::ID id) const
+{
+  return ((trk.tpcSignal() - GetExpectedSignal(trk, id)) / GetExpectedSigma(trk, id));
+}
 
 } // namespace o2::pid::tpc
 
