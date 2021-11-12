5816bb05b1a52407bda4eaff5250873671c6a499
Jan Fiete <jgrosseo@cern.ch>
2021-11-12 15:16:26 +0100

    fix

--- Tutorials/src/mcHistograms.cxx
+++ Tutorials/src/mcHistograms.cxx
@@ -46,7 +46,7 @@ struct AccessMcData {
     LOGF(info, "First: %d | Length: %d", mcParticles.begin().index(), mcParticles.size());
     int count = 0;
     for (auto& mcParticle : mcParticles) {
-      if (MC::isPhysicalPrimary(mcParticle)) {
+      if (particle.isPhysicalPrimary()) {
         phiH->Fill(mcParticle.phi());
         etaH->Fill(mcParticle.eta());
         count++;
