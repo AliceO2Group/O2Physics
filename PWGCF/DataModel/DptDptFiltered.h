6239fab4deb6f7a4a0eedb1d3a5703ed54de2bc0
Victor Gonzalez <victor.gonzalez@cern.ch>
2021-11-15 11:01:22 +0100

    Filter task separated in its own task dptdpt-filter (#265)
    * Filter task separated in its own task dptdpt-filter
    
    It feeds
    - match-reco-gen independent separated from the filter-qa task
    - dptdptcorrelations independent
    - dptdpt-filter-qa independent separated from the match-reco-gen task
    
    * Using bool columns and proper columns and table names
    
    Also
    - getting rid of long qualifiers for int
    - using float pi constants

---
+++ PWGCF/DataModel/DptDptFiltered.h
@@ -0,0 +1,68 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+#ifndef O2_ANALYSIS_DPTDPTFILTERED_H
+#define O2_ANALYSIS_DPTDPTFILTERED_H
+
+#include "Framework/ASoA.h"
+#include "Framework/AnalysisDataModel.h"
+
+namespace o2
+{
+namespace aod
+{
+/* we have to change from int to bool when bool columns work properly */
+namespace dptdptfilter
+{
+DECLARE_SOA_COLUMN(DptDptCFCollisionAccepted, collisionaccepted, bool); //! If the collision/event has been accepted or not
+DECLARE_SOA_COLUMN(DptDptCFCollisionCentMult, centmult, float);         //! The centrality/multiplicity pecentile
+} // namespace dptdptfilter
+DECLARE_SOA_TABLE(DptDptCFAcceptedCollisions, "AOD", "DPTDPTCFACCCOLL", //! Accepted reconstructed collisions/events filtered table
+                  o2::soa::Index<>,
+                  collision::BCId,
+                  collision::PosZ,
+                  dptdptfilter::DptDptCFCollisionAccepted,
+                  dptdptfilter::DptDptCFCollisionCentMult);
+using DptDptCFAcceptedCollision = DptDptCFAcceptedCollisions::iterator;
+DECLARE_SOA_TABLE(DptDptCFAcceptedTrueCollisions, "AOD", "DPTCFACCGENCOLL", //! Accepted generated collisions/events filtered table
+                  o2::soa::Index<>,
+                  collision::BCId,
+                  mccollision::PosZ,
+                  dptdptfilter::DptDptCFCollisionAccepted,
+                  dptdptfilter::DptDptCFCollisionCentMult);
+using DptDptCFAcceptedTrueCollision = DptDptCFAcceptedTrueCollisions::iterator;
+namespace dptdptfilter
+{
+DECLARE_SOA_INDEX_COLUMN(DptDptCFAcceptedCollision, event);       //! Reconstructed collision/event
+DECLARE_SOA_INDEX_COLUMN(DptDptCFAcceptedTrueCollision, mcevent); //! Generated collision/event
+DECLARE_SOA_COLUMN(TrackacceptedAsOne, trackacceptedasone, bool); //! Track accepted as type one
+DECLARE_SOA_COLUMN(TrackacceptedAsTwo, trackacceptedastwo, bool); //! Track accepted as type two
+DECLARE_SOA_COLUMN(Pt, pt, float);                                //! The track transverse momentum
+DECLARE_SOA_COLUMN(Eta, eta, float);                              //! The track pseudorapidity
+DECLARE_SOA_COLUMN(Phi, phi, float);                              //! The track azimuthal angle
+} // namespace dptdptfilter
+DECLARE_SOA_TABLE(ScannedTracks, "AOD", "SCANNEDTRACKS", //! The reconstructed tracks filtered table
+                  dptdptfilter::DptDptCFAcceptedCollisionId,
+                  dptdptfilter::TrackacceptedAsOne,
+                  dptdptfilter::TrackacceptedAsTwo,
+                  dptdptfilter::Pt,
+                  dptdptfilter::Eta,
+                  dptdptfilter::Phi);
+DECLARE_SOA_TABLE(ScannedTrueTracks, "AOD", "SCANTRUETRACKS", //! The generated particles filtered table
+                  dptdptfilter::DptDptCFAcceptedTrueCollisionId,
+                  dptdptfilter::TrackacceptedAsOne,
+                  dptdptfilter::TrackacceptedAsTwo,
+                  dptdptfilter::Pt,
+                  dptdptfilter::Eta,
+                  dptdptfilter::Phi);
+} // namespace aod
+} // namespace o2
+
+#endif // O2_ANALYSIS_DPTDPTFILTERED_H
