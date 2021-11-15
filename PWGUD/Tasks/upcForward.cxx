629d0e1e8fac859cc95f8408daa83a40666d749f
amrit gautam <agautam@localhost.localdomain>
2021-11-15 00:58:40 -0500

    updated upc Forward code for O2Physics

--- PWGUD/Tasks/upcForward.cxx
+++ PWGUD/Tasks/upcForward.cxx
@@ -17,12 +17,9 @@ alien:///alice/data/2015/LHC15o/000246392/pass5_lowIR/PWGZZ/Run3_Conversion/148_
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
-//<<<<<<< HEAD:Tasks/PWGUD/upcForward.cxx
-#include "AnalysisDataModel/EventSelection.h"
-#include "iostream"
-//=======
 #include "Common/DataModel/EventSelection.h"
-//>>>>>>> f2ae263c88efdf00592158d82e65aee3629195e7:PWGUD/Tasks/upcForward.cxx
+#include "iostream"
+#include "Common/DataModel/EventSelection.h"
 #include <TH1D.h>
 #include <TH2D.h>
 #include <TString.h>
@@ -32,221 +29,139 @@ using namespace std;
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
-#define mmuon 0.1057 //mass of muon
+#define mmuon 0.1057 // mass of muon
 struct UPCForward {
-  //defining histograms using histogram registry
-  HistogramRegistry registry{
-    "registry",
-    {{"hMass", ";#it{m_{#mu#mu}}, GeV/c^{2};", {HistType::kTH1D, {{500, 0, 10}}}},
-     {"hPt", ";#it{p_{t}}, GeV/c;", {HistType::kTH1D, {{500, 0., 5.}}}},
-     {"hPtsingle_muons", ";#it{p_{t}}, GeV/c;", {HistType::kTH1D, {{500, 0., 5.}}}},
-     {"hPx", ";#it{p_{x}}, GeV/c;", {HistType::kTH1D, {{500, -5., 5.}}}},
-     {"hPy", ";#it{p_{y}}, GeV/c;", {HistType::kTH1D, {{500, -5., 5.}}}},
-     {"hPz", ";#it{p_{z}}, GeV/c;", {HistType::kTH1D, {{500, -5., 5.}}}},
-     {"hRap", ";#it{y},..;", {HistType::kTH1D, {{500, -10., 10.}}}},
-     {"hEta", ";#it{y},..;", {HistType::kTH1D, {{500, -10., 10.}}}},
-     {"hCharge", ";#it{charge},..;", {HistType::kTH1D, {{500, -10., 10.}}}},
-     {"hSelectionCounter", ";#it{Selection},..;", {HistType::kTH1I, {{30, 0., 30.}}}},
-     {"hPhi", ";#it{#Phi},;", {HistType::kTH1D, {{500, -6., 6.}}}}}};
+  // defining histograms using histogram registry
+  HistogramRegistry registry{"registry", {}, OutputObjHandlingPolicy::AnalysisObject};
 
   void init(o2::framework::InitContext&)
   {
-    //TString SelectionCuts[8] = {"NoSelection", "CMup11and10Trigger","V0Selection","FDSelection", "twotracks", "oppositecharge", "-2.5<Eta<-4", "Pt<1"};
-    /* // commenting out this part now as histogram registry do not seem to suppor the binlabel using AxisSpec() or any other method
+
+    auto hSelectionCounter = registry.add<TH1>("hSelectionCounter", "hSelectionCounter;;NEvents", HistType::kTH1I, {{30, 0., 30.}});
+
+    // THIS IS THE SELECTION CUTS FOR NOW. We will add further cuts
+    TString SelectionCuts[8] = {"NoSelection", "CMup11and10Trigger", "V0Selection", "FDSelection", "twotracks", "oppositecharge", "-2.5<Eta<-4", "Pt<1"};
+    // now we can set BinLabel in histogram Registry
     for (int i = 0; i < 6; i++) {
-      registry.GetXaxis().(HIST("hSelectionCounter",SetBinLabel(i + 1, SelectionCuts[i].Data())));
-    }*/
-  }
-//  Configurable<float> etalow{"etalow", -4.0f, ""}; //
-//  Configurable<float> etahigh{"etahigh", -2.5f, ""}; //
-  Configurable<float> etalow{"etalow", -10.0f, ""};
-  Configurable<float> etahigh{"etahigh", 10.0f, ""};
-  Filter etaFilter_a = aod::fwdtrack::eta >etalow;
-  Filter etaFilter_b = aod::fwdtrack::eta < etahigh;
-  //new
-  //void process(soa::Join<aod::BCs, aod::Run2BCInfos ,aod::BcSels>::iterator const& bc, aod::FwdTracks const& tracksMuon)
-  void process(soa::Join<aod::BCs, aod::Run2BCInfos, aod::BcSels>::iterator const& bc ,soa::Filtered<aod::FwdTracks> const& tracksMuon)
+      hSelectionCounter->GetXaxis()->SetBinLabel(i + 1, SelectionCuts[i].Data());
+    }
+
+    registry.add("hMass", "Mass of Mother;#it{m_{#mu#mu}}, GeV/c^{2};", kTH1D, {{500, 0., 10.}});
+    registry.add("hPt", "Pt of Mother;#it{p_{t}}, GeV/c;", kTH1D, {{500, 0., 5.}});
+    registry.add("hPtsingle_muons", "Pt of Daughters;#it{p_{t}}, GeV/c;", kTH1D, {{500, 0., 5.}});
+    registry.add("hPx", "Px;#it{P_{x}}, GeV/c;", kTH1D, {{500, -5., 5.}});
+    registry.add("hPy", "Py;#it{P_{y}}, GeV/c;", kTH1D, {{500, -5., 5.}});
+    registry.add("hPz", "Pz;#it{P_{z}}, GeV/c;", kTH1D, {{500, -5., 5.}});
+    registry.add("hRap", "Rapidity of Mother;#it{y};", kTH1D, {{500, -10., 10.}});
+    registry.add("hEta", "Eta;#it{#eta};", kTH1D, {{500, -10., 10.}});
+    registry.add("hCharge", "Charge;#it{charge};", kTH1D, {{500, -10., 10.}});
+    registry.add("hPhi", "Phi;#it{#Phi};", kTH1D, {{500, -6., 6.}});
+
+    Configurable<float> etalow{"etalow", -4.0f, ""};   //
+    Configurable<float> etahigh{"etahigh", -2.5f, ""}; //
+    Filter etaFilter_a = aod::fwdtrack::eta > etalow;
+    Filter etaFilter_b = aod::fwdtrack::eta < etahigh;
+    // new
+    void process(soa::Join<aod::BCs, aod::Run2BCInfos, aod::BcSels>::iterator const& bc, soa::Filtered<aod::FwdTracks> const& tracksMuon)
+    {
+
+      registry.fill(HIST("hSelectionCounter"), 0);
+
+      int iMuonTracknumber = 0;
+      TLorentzVector p1, p2, p;
+      bool ispositive = kFALSE;
+      bool isnegative = kFALSE;
+
+      // V0 and FD information
+      bool isBeamBeamV0A = bc.bbV0A();
+      bool isBeamGasV0A = bc.bgV0A();
+      bool isBeamBeamV0C = bc.bbV0C();
+      bool isBeamGasV0C = bc.bgV0C();
+
+      bool isBeamBeamFDA = bc.bbFDA();
+      bool isBeamGasFDA = bc.bgFDA();
+      bool isBeamBeamFDC = bc.bbFDC();
+      bool isBeamGasFDC = bc.bgFDC();
+
+      // offline V0 and FD selection
+      bool isV0Selection = isBeamBeamV0A || isBeamGasV0A || isBeamGasV0C;
+      bool isFDSelection = isBeamBeamFDA || isBeamGasFDA || isBeamBeamFDC || isBeamGasFDC;
+
+      // CCUP10 and CCUP11 information
+      bool iskMUP11fired = bc.alias()[kMUP11];
+      bool iskMUP10fired = bc.alias()[kMUP10];
+      // cout << iskMUP11fired << iskMUP10fired<< endl;
+      //  selecting kMUP10 and 11 triggers
+      if (!iskMUP11fired && !iskMUP10fired) {
+        return;
+      }
+      registry.fill(HIST("hSelectionCounter"), 1);
+
+      if (isV0Selection) {
+        return;
+      }
+      registry.fill(HIST("hSelectionCounter"), 2);
+
+      if (isFDSelection) {
+        return;
+      }
+      registry.fill(HIST("hSelectionCounter"), 3);
+
+      for (auto& muon : tracksMuon) {
+        registry.fill(HIST("hCharge"), muon.sign());
+        iMuonTracknumber++;
+
+        if (muon.sign() > 0) {
+          p1.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
+          ispositive = kTRUE;
+        }
+        if (muon.sign() < 0) {
+          p2.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
+          isnegative = kTRUE;
+        }
+      }
+      if (iMuonTracknumber != 2) {
+        return;
+      }
+
+      registry.fill(HIST("hSelectionCounter"), 4);
+      if (!ispositive || !isnegative) {
+        return;
+      }
+      registry.fill(HIST("hSelectionCounter"), 5);
+
+      if ((p1.Eta() < -4) || (p2.Eta() < -4)) {
+        return;
+      }
+      if ((p1.Eta() > -2.5) || (p2.Eta() > -2.5)) {
+        return;
+      }
+
+      registry.fill(HIST("hSelectionCounter"), 6);
+      p = p1 + p2;
+      cout << "pt of dimuon " << p.Pt() << endl;
+      if (p.Pt() > 1) {
+        return;
+      }
+      registry.fill(HIST("hSelectionCounter"), 7);
+      registry.fill(HIST("hPt"), p.Pt());
+      registry.fill(HIST("hPx"), p.Px());
+      registry.fill(HIST("hPy"), p.Py());
+      registry.fill(HIST("hPz"), p.Pz());
+      registry.fill(HIST("hRap"), p.Rapidity());
+      registry.fill(HIST("hMass"), p.M());
+      registry.fill(HIST("hPhi"), p.Phi());
+      registry.fill(HIST("hEta"), p1.Eta());
+      registry.fill(HIST("hEta"), p2.Eta());
+      registry.fill(HIST("hPtsingle_muons"), p1.Pt());
+      registry.fill(HIST("hPtsingle_muons"), p2.Pt());
+
+    } // end of process
+
+  }; // end of struct
+
+  WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
   {
-    registry.fill(HIST("hSelectionCounter"), 0);
-
-    int iMuonTracknumber = 0;
-    TLorentzVector p1, p2, p;
-    bool ispositive = kFALSE;
-    bool isnegative = kFALSE;
-
-    // V0 and FD information
-    bool isBeamBeamV0A = bc.bbV0A();
-    bool isBeamGasV0A = bc.bgV0A();
-    bool isBeamBeamV0C = bc.bbV0C();
-    bool isBeamGasV0C = bc.bgV0C();
-
-    bool isBeamBeamFDA = bc.bbFDA();
-    bool isBeamGasFDA = bc.bgFDA();
-    bool isBeamBeamFDC = bc.bbFDC();
-    bool isBeamGasFDC = bc.bgFDC();
-
-    //offline V0 and FD selection
-    bool isV0Selection = isBeamBeamV0A || isBeamGasV0A || isBeamGasV0C;
-    bool isFDSelection = isBeamBeamFDA || isBeamGasFDA || isBeamBeamFDC || isBeamGasFDC;
-
-    //CCUP10 and CCUP11 information
-    bool iskMUP11fired = bc.alias()[kMUP11];
-    bool iskMUP10fired = bc.alias()[kMUP10];
-    //cout << iskMUP11fired << iskMUP10fired<< endl;
-    // selecting kMUP10 and 11 triggers
-    if (!iskMUP11fired && !iskMUP10fired) {
-      return;
-    }
-    registry.fill(HIST("hSelectionCounter"), 1);
-
-    if (isV0Selection) {
-      return;
-    }
-    registry.fill(HIST("hSelectionCounter"), 2);
-
-    if (isFDSelection) {
-      return;
-    }
-    registry.fill(HIST("hSelectionCounter"), 3);
-
-    for (auto& muon : tracksMuon) {
-      registry.fill(HIST("hCharge"), muon.sign());
-      iMuonTracknumber++;
-
-      if (muon.sign() > 0) {
-        p1.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
-        ispositive = kTRUE;
-      }
-      if (muon.sign() < 0) {
-        p2.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
-        isnegative = kTRUE;
-      }
-    }
-    if (iMuonTracknumber != 2) {
-      return;
-    }
-
-    registry.fill(HIST("hSelectionCounter"), 4);
-    if (!ispositive || !isnegative) {
-      return;
-    }
-    registry.fill(HIST("hSelectionCounter"), 5);
-
-    if ( (p1.Eta()<-4)  || (p2.Eta() <-4)) {return; }
-    if ( (p1.Eta()>-2.5)  || (p2.Eta() >-2.5)) {return; }
-
-    registry.fill(HIST("hSelectionCounter"), 6);
-    p = p1 + p2;
-    cout<< "pt of dimuon "<<p.Pt()<< endl;
-    if (p.Pt() > 1) {return;}
-    registry.fill(HIST("hSelectionCounter"), 7);
-    registry.fill(HIST("hPt"), p.Pt());
-    registry.fill(HIST("hPx"), p.Px());
-    registry.fill(HIST("hPy"), p.Py());
-    registry.fill(HIST("hPz"), p.Pz());
-    registry.fill(HIST("hRap"), p.Rapidity());
-    registry.fill(HIST("hMass"), p.M());
-    registry.fill(HIST("hPhi"), p.Phi());
-    registry.fill(HIST("hEta"), p1.Eta());
-    registry.fill(HIST("hEta"), p2.Eta());
-    registry.fill(HIST("hPtsingle_muons"), p1.Pt());
-    registry.fill(HIST("hPtsingle_muons"), p2.Pt());
-
-  } //end of process
-
-};
-
-/*struct UPCForward_Filtred{
-
-  HistogramRegistry registry{
-    "registry",
-    {{"hMass_filtered", ";#it{m_{#mu#mu}}, GeV/c^{2};", {HistType::kTH1D, {{500, 0, 10}}}},
-     {"hPt_filtered", ";#it{p_{t}}, GeV/c;", {HistType::kTH1D, {{500, 0., 5.}}}}}};
-
-
-  Configurable<float> etalow{"etalow", -4.0f, ""};
-  Configurable<float> etahigh{"etahigh", -2.5f, ""};
-  Filter etaFilter_a = aod::fwdtrack::eta >etalow;
-  Filter etaFilter_b = aod::fwdtrack::eta < etahigh;
-  //using BCevents = soa::Join<aod::BCs, aod::Run2BCInfos ,aod::BcSels>;
-
-  //Configurable<string> UpcTrigger1{"UpcTrigger1", "kMUP11",""};
-  //Configurable<string> UpcTrigger2{"UpcTrigger2","kMUP10",""};
-  //Filter upctrigger_a = !BCevents::Alias()[UpcTrigger1] && !BCevents::Alias()[UpcTrigger1] ;
-
-
-  void process(soa::Filtered<soa::Join<aod::BCs, aod::Run2BCInfos, aod::BcSels>>::iterator const& bc ,soa::Filtered<aod::FwdTracks> const& filteredmuons){
-    // V0 and FD information
-
-    int iMuonTracknumber = 0;
-    TLorentzVector p1, p2, p;
-    bool ispositive = kFALSE;
-    bool isnegative = kFALSE;
-
-
-    bool isBeamBeamV0A = bc.bbV0A();
-    bool isBeamGasV0A = bc.bgV0A();
-    bool isBeamBeamV0C = bc.bbV0C();
-    bool isBeamGasV0C = bc.bgV0C();
-
-    bool isBeamBeamFDA = bc.bbFDA();
-    bool isBeamGasFDA = bc.bgFDA();
-    bool isBeamBeamFDC = bc.bbFDC();
-    bool isBeamGasFDC = bc.bgFDC();
-
-    //offline V0 and FD selection
-    bool isV0Selection = isBeamBeamV0A || isBeamGasV0A || isBeamGasV0C;
-    bool isFDSelection = isBeamBeamFDA || isBeamGasFDA || isBeamBeamFDC || isBeamGasFDC;
-
-    //CCUP10 and CCUP11 information
-    bool iskMUP11fired = bc.alias()[kMUP11];
-    bool iskMUP10fired = bc.alias()[kMUP10];
-    //cout << iskMUP11fired << iskMUP10fired<< endl;
-    // selecting kMUP10 and 11 triggers
-    if (!iskMUP11fired && !iskMUP10fired) {
-      return;
-    }
-
-    if (isV0Selection) {
-      return;
-    }
-
-    if (isFDSelection) {
-      return;
-    }
-
-    for (auto& muon : filteredmuons) {
-      iMuonTracknumber++;
-      if (muon.sign() > 0) {
-        p1.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
-        ispositive = kTRUE;
-      }
-      if (muon.sign() < 0) {
-        p2.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
-        isnegative = kTRUE;
-      }
-    }
-    if (iMuonTracknumber != 2) {
-      return;
-    }
-    if (!ispositive || !isnegative) {
-      return;
-    }
-
-
-    p = p1+p2;
-    if (p.Pt() > 1) { return;}
-
-    registry.fill(HIST("hMass_filtered"), p.M());
-    registry.fill(HIST("hPt_filtered"), p.Pt());
-
-  } //end process
-
-};//end UPCForward_Filtred */
-
-WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
-{
-  return WorkflowSpec{
-    adaptAnalysisTask<UPCForward>(cfgc)};
-    //adaptAnalysisTask<UPCForward_Filtred>(cfgc)};
-}
+    return WorkflowSpec{
+      adaptAnalysisTask<UPCForward>(cfgc)};
+  }
