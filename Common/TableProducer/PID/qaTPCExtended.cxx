053a976616a09014911dc3827213c2da43f9a8cc
Jeremy Wilkinson <jeremy.wilkinson@cern.ch>
2021-11-19 13:07:39 +0100

    add V0 loop to QA task

--- Common/TableProducer/PID/qaTPCExtended.cxx
+++ Common/TableProducer/PID/qaTPCExtended.cxx
@@ -18,11 +18,14 @@
 #include "Common/Core/trackUtilities.h"
 #include "Common/Core/PID/PIDResponse.h"
 #include "Common/DataModel/TrackSelectionTables.h"
+#include "Common/DataModel/StrangenessTables.h"
 
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 using namespace o2::track;
+using TPCV0Tracks = soa::Join<aod::Tracks, aod::TracksExtra, aod::pidTPCFullEl, aod::pidTPCFullPi,
+                              aod::pidTPCFullKa, aod::pidTPCFullPr, aod::TrackSelection>;
 
 void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
 {
@@ -130,6 +133,112 @@ struct QaTpcPid {
   }//process
 };//struct QaTPCPID
 
+struct QaTpcV0 {
+  static constexpr int NpV0 = 4;
+  enum EV0DaughID {kEle=0, kPi=1, kKa=2, kPr=3};
+  static constexpr const char* partName[NpV0] = {"e", "#pi", "K", "p", };
+  static constexpr std::string_view hdEdxV0[NpV0] = {"dEdxV0/El", "dEdxV0/Pi",
+                                                   "dEdxV0/Ka", "dEdxV0/Pr"};
+  static constexpr std::string_view hdEdxDiffV0[NpV0] = {"dEdxDiffTPCV0/El", "dEdxDiffTPCV0/Pi",
+                                                   "dEdxDiffTPCV0/Ka", "dEdxDiffTPCV0/Pr"};
+  static constexpr std::string_view hnsigmaV0[NpV0] = {"nsigmaTPCV0/El", "nsigmaTPCV0/Pi",
+                                                   "nsigmaTPCV0/Ka", "nsigmaTPCV0/Pr"};
+  HistogramRegistry histos{"TPCPIDQA_V0"};
+  Configurable<int> logAxis{"logAxis",0, "Flag to use log momentum axis in V0 QA"};
+  Configurable<int> nBinsP{"nBinsP", 400, "Number of bins for the momentum"};
+  Configurable<float> minP{"minP", 0, "Minimum momentum in range"};
+  Configurable<float> maxP{"maxP", 20, "Maximum momentum in range"};
+  Configurable<int> nBinsNSigma{"nBinsNSigma",200, "Number of bins for TPC nSigma"};
+  Configurable<float> minNSigma{"minNSigma", -10.f, "Lower limit for TPC nSigma"};
+  Configurable<float> maxNSigma{"maxNSigma",  10.f, "Upper limit for TPC nSigma"};
+  
+  //Definition of V0 preselection cuts for K0S, Lambda, Anti-lambda
+  //K0S
+  static constexpr const float cutQTK0[2] = {0.1075f, 0.215f};
+  static constexpr const float cutAPK0[2] = {0.199f, 0.8f};
+  //Lambda/Antilambda
+  static constexpr const float cutQTL = 0.03f;
+  static constexpr const float cutAlphaL[2] = {0.35f, 0.7f};
+  static constexpr const float cutAlphaAL[2] = {-0.7f, -0.35f};
+  static constexpr const float cutAPL[3] = {0.107f, -0.69f, 0.5f};
+  
+
+  template <uint8_t i>
+  void addV0Histos()
+  {
+    AxisSpec pAxis{nBinsP, minP, maxP, "#it{p} (GeV/#it{c})"};
+    if (logAxis) {
+      pAxis.makeLogaritmic();
+    }
+
+    // corrected dE/dx for clean V0
+    AxisSpec expAxis{1000,0,1000,Form("d#it{E}/d#it{x}_(%s from V^{0}) A.U.", partName[i])};
+    histos.add(hdEdxV0[i].data(), "", kTH2F, {pAxis, expAxis});
+
+    AxisSpec deltaAxis{1000, -500, 500, Form("d#it{E}/d#it{x} - d#it{E}/d#it{x}(expected(%s from V^{0})",partName[i])};
+    histos.add(hdEdxDiffV0[i].data(), "", kTH2F, {pAxis, deltaAxis});
+
+    AxisSpec nSigmaAxis{nBinsNSigma, minNSigma, maxNSigma, Form("n_{#sigma}^{TPC}(%s from V^{0})",partName[i])};
+    histos.add(hnsigmaV0[i].data(), "", kTH2F, {pAxis, nSigmaAxis});
+
+  }//addV0Histos
+
+  template <uint8_t i, typename T>
+  void fillV0Histos(const T& t, const float mom, const float exp_diff, const float nsigma)
+  {
+    histos.fill(HIST(hdEdxV0[i]), mom, t.tpcSignal() - exp_diff);
+    histos.fill(HIST(hdEdxDiffV0[i]), mom, exp_diff);
+    histos.fill(HIST(hnsigmaV0[i]), t.p(), nsigma);
+  }
+
+
+  void init(o2::framework::InitContext&)
+  {
+    addV0Histos<0>();
+    addV0Histos<1>();
+    addV0Histos<2>();
+    addV0Histos<3>();
+
+  }//init
+
+  void process(aod::Collision const& collision, aod::V0Datas const& v0s, TPCV0Tracks const& tracks)
+  {
+    for (auto v0 : v0s) {//for loop on built v0 candidates
+      // initialise dynamic variables
+      float alpha = v0.alpha();
+      float qt = v0.qtarm();
+      auto posTrack = v0.posTrack_as<TPCV0Tracks>();
+      auto negTrack = v0.negTrack_as<TPCV0Tracks>();
+      // Check for K0
+      float q = cutAPK0[0] * sqrt(abs(1 - alpha * alpha / (cutAPK0[1] * cutAPK0[1])));
+      if ((qt > cutQTK0[0]) && (qt < cutQTK0[1]) && (qt > q) ) {
+        // Treat as K0 (both tracks pions)
+        fillV0Histos<kPi>(posTrack,posTrack.tpcInnerParam(), posTrack.tpcExpSignalDiffPi(), posTrack.tpcNSigmaPi());
+        fillV0Histos<kPi>(negTrack,negTrack.tpcInnerParam(), negTrack.tpcExpSignalDiffPi(), negTrack.tpcNSigmaPi());
+
+      }
+
+      // Check for Lambda
+      q = cutAPL[0] * sqrt(abs(1 - ( (alpha + cutAPL[1]) * (alpha + cutAPL[1]) ) / (cutAPL[2] * cutAPL[2])));
+      if ( (alpha > cutAlphaL[0]) && (alpha < cutAlphaL[1]) && (qt > cutQTL) && (qt < q)) {
+        //Treat as Lambda (pos proton, neg pion)
+        fillV0Histos<kPr>(posTrack,posTrack.tpcInnerParam(), posTrack.tpcExpSignalDiffPr(), posTrack.tpcNSigmaPr());
+        fillV0Histos<kPi>(negTrack,negTrack.tpcInnerParam(), negTrack.tpcExpSignalDiffPi(), negTrack.tpcNSigmaPi());
+
+      }
+
+      // Check for antilambda
+      q = cutAPL[0] * sqrt(abs(1 - ( (alpha - cutAPL[1]) * (alpha - cutAPL[1]) ) / (cutAPL[2] * cutAPL[2]) ));
+      if ( (alpha > cutAlphaAL[0]) && (alpha < cutAlphaAL[1]) && (qt <  q) ) {
+        //Treat as antilambda (pos pion, neg proton)
+        fillV0Histos<kPi>(posTrack,posTrack.tpcInnerParam(), posTrack.tpcExpSignalDiffPi(), posTrack.tpcNSigmaPi());
+        fillV0Histos<kPr>(negTrack,negTrack.tpcInnerParam(), negTrack.tpcExpSignalDiffPr(), negTrack.tpcNSigmaPr());
+      }
+      
+    } //for
+  }//process
+    
+};//struct QaTpcV0
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
