69bab9bbb49b86f390c2fb61cb79ea851602450d
Timo Wilken <timo.wilken@cern.ch>
2021-11-23 09:41:34 +0000

    Add check for copyright headers (#325)
    * Make copyright headers consistent
    
    This is to prepare the code for introducing copyright header checking.
    
    One file, PWGDQ/Tasks/filterPPTiny.cxx, had CR-LF line endings, which would
    confuse the checker. It was converted to LF line endings to match the rest of
    O2Physics.
    
    * Add check for copyright headers
    
    It runs in parallel with the clang-format check, and makes sure the copyright
    notice of any changed .cxx or .h files is OK.

--- EventFiltering/PWGMM/multFilter.cxx
+++ EventFiltering/PWGMM/multFilter.cxx
@@ -1,4 +1,3 @@
-
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
@@ -9,11 +8,13 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-// O2 includes
+//
 // \file multFilter.cxx
 // \brief task for selection of high multiplicity events
 //
 // \author Antonio Ortiz <antonio.ortiz.velasquez@cern.ch>, ICN-UNAM
+//
+// O2 includes
 
 #include "ReconstructionDataFormats/Track.h"
 #include "Framework/runDataProcessing.h"
