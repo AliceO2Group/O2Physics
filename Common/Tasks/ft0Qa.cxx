4594d7b7cb31a4ae16351510af1eae72c7d64dad
Timo Wilken <timo.wilken@cern.ch>
2021-11-23 10:42:39 +0100

    DO NOT MERGE: test copyright checker

--- Common/Tasks/ft0Qa.cxx
+++ Common/Tasks/ft0Qa.cxx
@@ -1,3 +1,5 @@
+// wrong line to create an error in copyright check
+//
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
