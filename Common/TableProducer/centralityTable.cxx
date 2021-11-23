f6afc333fcb48f083136307f9bbc793545608aac
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-11-23 10:53:54 +0100

    Update centralityTable.cxx

--- Common/TableProducer/centralityTable.cxx
+++ Common/TableProducer/centralityTable.cxx
@@ -1,13 +1,4 @@
-// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
-//
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
+// Copyright 2019-2020 by me.
 
 /// \file centrality.cxx
 /// \brief Task to produce the centrality tables associated to each of the required centrality estimators
