7cd8e2aa9812ff0caa4dead962b69413d9773434
Alla Maevskaya <Alla.Maevskaya@cern.ch>
2021-11-25 14:08:34 +0300

    fix warning

--- Common/Tasks/ft0Qa.cxx
+++ Common/Tasks/ft0Qa.cxx
@@ -35,8 +35,8 @@ struct FT0Qa {
   OutputObj<TH1F> hT0res{TH1F("hT0res", "T0 resolution; ns", 200, -0.5, 0.5)};
   OutputObj<TH1F> hT0Vertex{TH1F("hT0vertex", "T0 vertex; cm", 200, -30, 30.)};
   OutputObj<TH2F> hVertex_T0_PV{TH2F(" hVertex_T0_PV", "T0 vertex vs Primary Vertex; T0 vertex, cm; PV, cm", 200, -30, 30., 200, -30, 30.)};
-  OutputObj<TH2F> hResMult{TH2F("hResMult", "T0 resolution vs event multiplicity", 600, 0., 2000, 100, -500, 500.)};
-  OutputObj<TH2F> hT0V0mult{TH2F("hT0V0mult", "T0A vs V0 multiplicity;V0Mult;T0Mmult", 200, 0., 50000., 200, 0., 2000.)};
+  OutputObj<TH2F> hResMult{TH2F("hResMult", "T0 resolution vs event multiplicity", 200, 0., 2000, 100, -0.5, 0.5)};
+  OutputObj<TH2F> hT0V0mult{TH2F("hT0V0mult", "T0A vs V0 multiplicity;V0Mult;T0Mmult", 500, 0., 15000., 200, 0., 2000.)};
   OutputObj<TH2F> hT0V0time{TH2F("hT0V0time", "T0A vs V0 time ;V0time;T0A", 200, -2, 2., 200, -2, 2)};
 
   //Configurable<bool> isMC{"isMC", 0, "0 - data, 1 - MC"};
@@ -44,6 +44,8 @@ struct FT0Qa {
   void process(soa::Join<aod::Collisions, aod::EvSels, aod::Mults, aod::FT0sCorrected>::iterator const& col, aod::FT0s const& ft0s, aod::FV0As const& fv0s)
   {
     int64_t foundFT0 = col.foundFT0();
+    float sumAmpFT0 = 0;
+    float sumAmpFV0 = 0;
     if (foundFT0 != -1) {
       auto ft0 = ft0s.iteratorAt(foundFT0);
       LOGF(debug, "multV0A=%5.0f;  multT0A=%5.0f; PV=%f; T0vertex=%f; T0A=%f; T0C=%f; T0AC=%f ", col.multV0A(), col.multT0A(), col.posZ(), col.t0ACorrected(), col.t0CCorrected(), col.t0AC(), ft0.posZ());
@@ -52,7 +54,6 @@ struct FT0Qa {
         auto fv0 = fv0s.iteratorAt(foundFT0);
         float fv0Time = fv0.time();
         hT0V0time->Fill(fv0Time, ft0.timeA());
-        float sumAmpFT0 = 0, sumAmpFV0;
         for (int ich = 0; ich < 96; ich++) {
           sumAmpFT0 += ft0.amplitudeA()[ich];
         }
@@ -69,6 +70,7 @@ struct FT0Qa {
         hT0Vertex->Fill(ft0.posZ());
         hVertex_T0_PV->Fill(ft0.posZ(), col.posZ());
         hT0res->Fill((col.t0ACorrected() - col.t0CCorrected()) / 2.);
+        hResMult->Fill(sumAmpFT0, (col.t0ACorrected() - col.t0CCorrected()) / 2.);
       }
     }
   }
