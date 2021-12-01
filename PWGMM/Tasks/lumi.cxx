6bafad0979c928637d92f982e4183e15461f2027
JUNLEE KIM <jikim1290@gmail.com>
2021-12-01 22:36:08 +0900

    updates

--- PWGMM/Tasks/lumi.cxx
+++ PWGMM/Tasks/lumi.cxx
@@ -20,28 +20,27 @@ using namespace o2::framework;
 using namespace o2::framework::expressions;
 
 struct lumiTask {
-  HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
+  HistogramRegistry histos{"histos",{
+      {"vertexx", "", {HistType::kTH1F, {{1000, -1, 1, "x"}}}}, //
+      {"vertexy", "", {HistType::kTH1F, {{1000, -1, 1, "y"}}}}, //
+      {"timestamp", "", {HistType::kTH1F, {{1000, 0, 5e7, "t"}}}}, //
+      {"vertexx_timestamp", "", {HistType::kTH2F, {{1000, 0, 5e7, "t"}, {1000, -1, 1, "x"}}}}, //
+      {"vertexy_timestamp", "", {HistType::kTH2F, {{1000, 0, 5e7, "t"}, {1000, -1, 1, "y"}}}} //
+  } };
   int first_time = 1530314294062; // to be updated
-  void init(o2::framework::InitContext&)
-  {
-    histos.add("vertexx", "", HistType::kTH1F, {{1000, -1, 1, "x"}});
-    histos.add("vertexy", "", HistType::kTH1F, {{1000, -1, 1, "y"}});
-    histos.add("timestamp", "", HistType::kTH1F, {{1000, 0, 5e7, "t"}});
-    histos.add("vertexx_timestamp", "", HistType::kTH2F, {{1000, 0, 5e7, "t"}, {1000, -1, 1, "x"}});
-    histos.add("vertexy_timestamp", "", HistType::kTH2F, {{1000, 0, 5e7, "t"}, {1000, -1, 1, "y"}});
-  } // init
 
   void process(aod::Collision const& collision, aod::BCsWithTimestamps const&)
   {
     auto bc = collision.bc_as<aod::BCsWithTimestamps>();
     histos.fill(HIST("vertexx"), collision.posX());
     histos.fill(HIST("vertexy"), collision.posY());
-    histos.fill(HIST("timestamp"), bc.timestamp() - 1530314294062);
-    //		LOGF(info, "Got timestamp %llu", bc.timestamp());
-    histos.fill(HIST("vertexx_timestamp"), bc.timestamp() - 1530314294062, collision.posX());
-    histos.fill(HIST("vertexy_timestamp"), bc.timestamp() - 1530314294062, collision.posY());
+    histos.fill(HIST("timestamp"), bc.timestamp() - first_time);
+    //          LOGF(info, "Got timestamp {}", bc.timestamp());
+    histos.fill(HIST("vertexx_timestamp"), bc.timestamp() - first_time, collision.posX());
+    histos.fill(HIST("vertexy_timestamp"), bc.timestamp() - first_time, collision.posY());
   } // need selections
 };
+
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{adaptAnalysisTask<lumiTask>(cfgc)};
