f36f2f54d768d2d2fb394d4d620c246d7eb49f15
Annalena Kalteyer <akalteye@lxir128.gsi.de>
2021-12-15 13:27:21 +0100

    Remove old TPC PID response from tasks

--- Common/TableProducer/PID/qaTPCMC.cxx
+++ Common/TableProducer/PID/qaTPCMC.cxx
@@ -23,7 +23,8 @@
 #include "ReconstructionDataFormats/Track.h"
 #include <CCDB/BasicCCDBManager.h>
 #include "Common/Core/PID/PIDResponse.h"
-#include "Common/Core/PID/PIDTPC.h"
+#include "Common/Core/PID/ParamBase.h"
+#include "Common/Core/MC.h"
 
 using namespace o2;
 using namespace o2::framework;
