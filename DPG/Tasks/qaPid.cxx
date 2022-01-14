c461ce6dc2d09248cddf4a3bc2d7dfcf0abd1c6f
Maja Kabus <mkabus@cern.ch>
2022-01-14 13:43:12 +0100

    Separate directory for PID ML (#440)

--- DPG/Tasks/qaPid.cxx
+++ DPG/Tasks/qaPid.cxx
@@ -355,7 +355,7 @@ struct pid {
     const float p = track.p();
 
     // list of Nsigmas for particles
-    float particleNSigma[arrLen] = {};
+    float particleNSigma[arrLen];
 
     bool isPidFalse = false;
 
@@ -416,7 +416,7 @@ struct pid {
     bool isPidFalse = false;
 
     // list of Nsigmas for particles
-    float particleNSigma[arrLen] = {};
+    float particleNSigma[arrLen];
 
     // calculate Nsigmas for every particle
     for (int j = 0; j < arrLen; ++j) {
