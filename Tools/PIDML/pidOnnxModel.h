ca9007c4a4bb096ec786f1b61098b98cc99067dd
saganatt <8majak8@gmail.com>
2022-01-17 18:10:24 +0100

    Working locally after a small refactor of filenames. CCDB needs to be checked on Hyperloop.

--- Tools/PIDML/pidOnnxModel.h
+++ Tools/PIDML/pidOnnxModel.h
@@ -8,29 +8,19 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file pidONNXInferer.h
-/// \brief A class that wraps PID ML ONNX model inference. See https://github.com/saganatt/PID_ML_in_O2 for more detailed instructions.
+/// \file pidONNXModel.h
+/// \brief A class that wraps PID ML ONNX model. See https://github.com/saganatt/PID_ML_in_O2 for more detailed instructions.
 ///
 /// \author Maja Kabus <mkabus@cern.ch>
 
-#ifndef O2_ANALYSIS_PIDONNXINFERER_H_
-#define O2_ANALYSIS_PIDONNXINFERER_H_
-
-#include "CCDB/BasicCCDBManager.h"
+#ifndef O2_ANALYSIS_PIDONNXMODEL_H_
+#define O2_ANALYSIS_PIDONNXMODEL_H_
 
 #include <onnxruntime/core/session/experimental_onnxruntime_cxx_api.h>
 #include <rapidjson/document.h>
 #include <rapidjson/filereadstream.h>
 #include <string>
-#include <cctype>
-#include <chrono>
 
-// Temporary solution for Hyperloop tests - model files placed on CCDB. In the future, they will be taken probably from cvmfs (to be discussed).
-#ifndef USE_CCDB
-#define USE_CCDB 0
-#endif
-
-#if USE_CCDB == 0
 // TODO: Copied from cefpTask, shall we put it in some common utils code?
 namespace
 {
@@ -50,40 +40,17 @@ bool readJsonFile(const std::string& config, rapidjson::Document& d)
   return true;
 }
 } // namespace
-#endif
 
-struct PidONNXInferer {
+struct PidONNXModel {
  public:
-  PidONNXInferer(std::string& modelFile, const std::string& trainColumnsFile, const std::string& scalingParamsFile, const std::string& url, long nolaterthan, bool useGPU) : mUseGPU(useGPU)
+  PidONNXModel(const std::string& modelDir, const std::string& scalingParamsFile, int pid = 211, bool useTOF = false)
   {
-    TString* onnxModel = nullptr;
-    loadInputFiles(modelFile, trainColumnsFile, scalingParamsFile, url, nolaterthan, onnxModel);
-#if USE_CCDB == 1
-    //if (onnxModel) {
-    //  LOG(info) << "Loaded ONNX model: " << onnxModel->Data();
-    //} else {
-    //  LOG(error) << "ONNX model is null!";
-    //}
-#endif
+    std::string modelFile;
+    loadInputFiles(modelDir, scalingParamsFile, useTOF, pid, modelFile);
 
     Ort::SessionOptions sessionOptions;
-    if (useGPU) {
-      LOG(fatal) << "GPU not supported yet in PID ONNX Inferer" << std::endl;
-      // FIXME: This can be used only if ONNXRuntime is build with CUDA support
-      // Do we need GPU for inference?
-      //Ort::ThrowOnError(OrtSessionOptionsAppendExecutionProvider_CUDA(sessionOptions, 0));
-    } else {
-      LOG(info) << "Using CPU";
-      // Memory info is set each time in Ort::Experimental::Value::CreateTensor()
-    }
-    LOG(info) << "Creating ONNX env";
     mEnv = std::make_shared<Ort::Env>(ORT_LOGGING_LEVEL_WARNING, "pid-onnx-inferer");
-#if USE_CCDB == 1
-    char* onnxData = (char*)onnxModel->Data(); // to get rid of const in TString::Data
-    mSession = std::make_shared<Ort::Experimental::Session>(*mEnv, onnxData, onnxModel->Length() * sizeof(char), sessionOptions);
-#else
     mSession.reset(new Ort::Experimental::Session{*mEnv, modelFile, sessionOptions});
-#endif
 
     mInputNames = mSession->GetInputNames();
     mInputShapes = mSession->GetInputShapes();
@@ -103,9 +70,9 @@ struct PidONNXInferer {
     // Assume model has 1 input node and 1 output node.
     assert(mInputNames.size() == 1 && mOutputNames.size() == 1);
   }
-  PidONNXInferer() = default;
-  PidONNXInferer(PidONNXInferer& other) = default;
-  ~PidONNXInferer() = default;
+  PidONNXModel() = default;
+  PidONNXModel(PidONNXModel& other) = default;
+  ~PidONNXModel() = default;
 
   template <typename T>
   float applyModel(const T& track)
@@ -137,50 +104,20 @@ struct PidONNXInferer {
   }
 
  private:
-  void loadInputFiles(std::string& modelFile, const std::string& trainColumnsFile, const std::string& scalingParamsFile, const std::string& url, long nolaterthan, TString* onnxModel)
+  void loadInputFiles(const std::string& modelDir, const std::string& scalingParamsFile, bool useTOF, int pid, const std::string& url, long nolaterthan, std::string& modelFile)
   {
     rapidjson::Document trainColumnsDoc;
     rapidjson::Document scalingParamsDoc;
 
-#if USE_CCDB == 1
-    // TODO: Temporary quick solution with CCDB and TStrings in ROOT files
-    o2::framework::Service<o2::ccdb::BasicCCDBManager> ccdb;
-    // Set CCDB url
-    ccdb->setURL(url);
-    // Enabling object caching, otherwise each call goes to the CCDB server
-    ccdb->setCaching(true);
-    ccdb->setLocalObjectValidityChecking();
-    // Not later than now, will be replaced by the value of the train creation
-    // This avoids that users can replace objects **while** a train is running
-    ccdb->setCreatedNotAfter(nolaterthan);
+    std::string modelSubdir = useTOF ? "All" : "TPC";
+    std::ostringstream tmp;
+    tmp << modelDir << "/" << modelSubdir << "/simple_model_" << pid << ".onnx";
+    modelFile = tmp.str();
+    tmp.str("");
+    tmp.clear();
+    tmp << modelDir << "/" << modelSubdir << "/columns_for_training.json";
+    std::string trainColumnsFile = tmp.str();
 
-    LOGF(info, "Getting object %s", modelFile.data());
-    onnxModel = ccdb->getForTimeStamp<TString>(modelFile, nolaterthan);
-    if (!onnxModel) {
-      LOGF(fatal, "ONNX model %s not found!", modelFile);
-    }
-    LOGF(info, "Getting object %s", trainColumnsFile.data());
-    TString* trainColumnsJson = ccdb->getForTimeStamp<TString>(trainColumnsFile, nolaterthan);
-    if (!trainColumnsJson) {
-      LOGF(fatal, "Train columns file %s not found!", trainColumnsFile);
-    }
-    LOGF(info, "Getting object %s", scalingParamsFile.data());
-    TString* scalingParamsJson = ccdb->getForTimeStamp<TString>(scalingParamsFile, nolaterthan);
-    if (!scalingParamsJson) {
-      LOGF(fatal, "Scaling parameters file %s not found!", scalingParamsFile);
-    }
-
-    trainColumnsDoc.Parse(trainColumnsJson->Data());
-    for (auto& param : trainColumnsDoc["columns_for_training"].GetArray()) {
-      mTrainColumns.emplace_back(param.GetString());
-    }
-    scalingParamsDoc.Parse(scalingParamsJson->Data());
-    for (auto& param : scalingParamsDoc["data"].GetArray()) {
-      mScalingParams[param[0].GetString()] = std::make_pair(param[1].GetFloat(), param[2].GetFloat());
-    }
-
-#else
-    // TODO: This will be used when we will have direct access to JSON files
     if (readJsonFile(trainColumnsFile, trainColumnsDoc)) {
       for (auto& param : trainColumnsDoc["columns_for_training"].GetArray()) {
         mTrainColumns.emplace_back(param.GetString());
@@ -191,18 +128,13 @@ struct PidONNXInferer {
         mScalingParams[param[0].GetString()] = std::make_pair(param[1].GetFloat(), param[2].GetFloat());
       }
     }
-#endif
   }
-
-#define GET_VALUE_FOR_COLUMN(_TableIt_, _ColumnGetter_) \
-  static_assert(false, _ColumnGetter_);                 \
-  float INPUT = static_cast<o2::aod::track::#_ColumnGetter_>(_TableIt_).getIterator().mCurrentPos;
 
   template <typename T>
   std::vector<float> createInputsSingle(const T& track)
   {
-    //TODO: Hardcoded for now, needs to be modifiable
-    //sign is short, trackType and tpcNClsShared uint8_t
+    // TODO: Hardcoded for now. Planning to implement RowView extension to get runtime access to selected columns
+    // sign is short, trackType and tpcNClsShared uint8_t
     float scaledTPCSignal = (track.tpcSignal() - mScalingParams.at("fTPCSignal").first) / mScalingParams.at("fTPCSignal").second;
     float scaledTOFSignal = (track.tofSignal() - mScalingParams.at("fTOFSignal").first) / mScalingParams.at("fTOFSignal").second;
     float scaledX = (track.x() - mScalingParams.at("fX").first) / mScalingParams.at("fX").second;
@@ -215,17 +147,6 @@ struct PidONNXInferer {
 
     std::vector<float> inputValues{scaledTPCSignal, scaledTOFSignal, track.beta(), track.px(), track.py(), track.pz(), (float)track.sign(), scaledX, scaledY, scaledZ, scaledAlpha, (float)track.trackType(), scaledTPCNClsShared, scaledDcaXY, scaledDcaZ, track.p()};
 
-    // TODO: Any more elegant way to select columns? This doesn't compile, a macro cannot interpret a string.
-    //for (auto& columnLabel : mTrainColumns) {
-    //  std::string columnGetter = columnLabel.substr(1);
-    //  columnGetter[0] = std::tolower((unsigned char)columnGetter[0]);
-    //  GET_VALUE_FOR_COLUMN(track, columnGetter);
-    //  auto scaleIt = std::find(mScalingParams.begin(), mScalingParams.end(), columnLabel);
-    //  if (scaleIt != mScalingParams.end()) {
-    //    INPUT = (INPUT - scaleIt->second.first) / scaleIt->second.second;
-    //  }
-    //  inputValues.push_back(INPUT);
-    //}
     return inputValues;
   }
 
@@ -239,10 +160,10 @@ struct PidONNXInferer {
     return ss.str();
   }
 
+  std::string mModelDir;
   std::vector<std::string> mTrainColumns;
   std::map<std::string, std::pair<float, float>> mScalingParams;
 
-  bool mUseGPU;
   std::shared_ptr<Ort::Env> mEnv = nullptr;
   // No empty constructors for Session, we need a pointer
   std::shared_ptr<Ort::Experimental::Session> mSession = nullptr;
@@ -253,4 +174,4 @@ struct PidONNXInferer {
   std::vector<std::vector<int64_t>> mOutputShapes;
 };
 
-#endif // O2_ANALYSIS_PIDONNXINFERER_H_
+#endif // O2_ANALYSIS_PIDONNXMODEL_H_
