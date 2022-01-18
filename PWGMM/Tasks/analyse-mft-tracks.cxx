04510644ceab948d68f2ca9a27edd5af43eb0493
sarahherrmann <83958698+sarahherrmann@users.noreply.github.com>
2022-01-18 15:23:47 +0100

    Clang format fix to analyse-mft-tracks with the added histograms

--- PWGMM/Tasks/analyse-mft-tracks.cxx
+++ PWGMM/Tasks/analyse-mft-tracks.cxx
@@ -28,7 +28,7 @@ struct analyseMFTTracks {
     "registry",
     {
       {"TracksPhiEta_in_coll", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
-      {"TracksPhiEta", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
+      {"TracksPhiEta", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}},         //
 
       {"TracksPhiEta_5", "#eta_{kin} versus #varphi_{kin} of MFT tracks with ncls #\geq 5; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
       {"TracksPhiEta_6", "#eta_{kin} versus #varphi_{kin} of MFT tracks with ncls #\geq 6; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
@@ -36,25 +36,23 @@ struct analyseMFTTracks {
       {"TracksPhiEta_8", "#eta_{kin} versus #varphi_{kin} of MFT tracks with ncls #\geq 8; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
       {"TracksPhiEta_9", "#eta_{kin} versus #varphi_{kin} of MFT tracks with ncls #\geq 9; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
 
-      {"TracksEtaZvtx", "; #eta; Z_{vtx}; ", {HistType::kTH2F, {{35, -4.5, -1.}, {201, -20.1, 20.1}}}},        //
-      {"NtrkZvtx", "; N_{trk}; Z_{vtx}; ", {HistType::kTH2F, {{301, -0.5, 300.5}, {201, -20.1, 20.1}}}},       //
-      {"NtrkEta", "#eta; N_{trk}; ", {HistType::kTH1F, {{35, -4.5, -1.}}}},                                    //
-      {"TracksXLastClsYLastCls", "; x_{LastCls}; y_{LastCls}; ", {HistType::kTH2F, {{400, -20, 20.}, {400, -20, 20.}}}},        //
-      {"TracksXLastClsYLastCls_firstFace", "; x_{LastCls}; y_{LastCls}; ", {HistType::kTH2F, {{400, -20, 20.}, {400, -20, 20.}}}},        //
+      {"TracksEtaZvtx", "; #eta; Z_{vtx}; ", {HistType::kTH2F, {{35, -4.5, -1.}, {201, -20.1, 20.1}}}},                            //
+      {"NtrkZvtx", "; N_{trk}; Z_{vtx}; ", {HistType::kTH2F, {{301, -0.5, 300.5}, {201, -20.1, 20.1}}}},                           //
+      {"NtrkEta", "#eta; N_{trk}; ", {HistType::kTH1F, {{35, -4.5, -1.}}}},                                                        //
+      {"TracksXLastClsYLastCls", "; x_{LastCls}; y_{LastCls}; ", {HistType::kTH2F, {{400, -20, 20.}, {400, -20, 20.}}}},           //
+      {"TracksXLastClsYLastCls_firstFace", "; x_{LastCls}; y_{LastCls}; ", {HistType::kTH2F, {{400, -20, 20.}, {400, -20, 20.}}}}, //
       {"TracksXLastClsYLastCls_secondFace", "; x_{LastCls}; y_{LastCls}; ", {HistType::kTH2F, {{400, -20, 20.}, {400, -20, 20.}}}},
-      {"TracksPhiEta_LastCls", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
-      {"TracksPhiEta_LastClsLastDisk", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
-      {"TracksPhiEta_LastClsLastDisk_firstFace", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
+      {"TracksPhiEta_LastCls", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}},                    //
+      {"TracksPhiEta_LastClsLastDisk", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}},            //
+      {"TracksPhiEta_LastClsLastDisk_firstFace", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}},  //
       {"TracksPhiEta_LastClsLastDisk_secondFace", "; #varphi; #eta; ", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
 
-
-
-      {"TracksPhiEtaGen", "; #varphi; #eta; ", {HistType::kTH2F, {{600, 0, 2 * M_PI}, {35, -4.5, -1.}}}},      //
-      {"TracksEtaZvtxGen", "; #eta; Z_{vtx}; ", {HistType::kTH2F, {{35, -4.5, -1.}, {201, -20.1, 20.1}}}},     //
+      {"TracksPhiEtaGen", "; #varphi; #eta; ", {HistType::kTH2F, {{600, 0, 2 * M_PI}, {35, -4.5, -1.}}}},  //
+      {"TracksEtaZvtxGen", "; #eta; Z_{vtx}; ", {HistType::kTH2F, {{35, -4.5, -1.}, {201, -20.1, 20.1}}}}, //
       {"NtrkZLastCls", "z_{LastCls}; count; ", {HistType::kTH1F, {{26, -79, -66}}}},
       //{"NtrkZvtxGen", "; N_{trk}; Z_{vtx}; events", {HistType::kTH2F, {{301, -0.5, 300.5}, {201, -20.1, 20.1}}}},            //
       {"NtrkEtaGen", "#eta; N_{trk}; ", {HistType::kTH1F, {{35, -4.5, -1.}}}}, //
-    }                                                                                //
+    }                                                                          //
   };
 
   void processRec(o2::aod::Collision const& collision, o2::aod::MFTTracks const& tracks)
@@ -66,25 +64,24 @@ struct analyseMFTTracks {
 
     for (auto& track : tracks) {
       double zLastCls = track.z();
-      v.SetXYZ(track.x(),track.y(),zLastCls);
+      v.SetXYZ(track.x(), track.y(), zLastCls);
       double etaLastCls = v.Eta();
       double phiLastCls = v.Phi();
       registry.fill(HIST("TracksPhiEta_in_coll"), track.phi(), track.eta());
       registry.fill(HIST("TracksEtaZvtx"), track.eta(), z);
       registry.fill(HIST("NtrkEta"), track.eta());
-      if (zLastCls < -70)//disk 4
+      if (zLastCls < -70) //disk 4
       {
         registry.fill(HIST("TracksXLastClsYLastCls"), track.x(), track.y());
         registry.fill(HIST("TracksPhiEta_LastClsLastDisk"), phiLastCls, etaLastCls);
 
-        if (zLastCls > -77)//layer 8
+        if (zLastCls > -77) //layer 8
         {
           registry.fill(HIST("TracksXLastClsYLastCls_firstFace"), track.x(), track.y());
           registry.fill(HIST("TracksPhiEta_LastClsLastDisk_firstFace"), phiLastCls, etaLastCls);
         }
-
       }
-      if (zLastCls < -77)//layer 9
+      if (zLastCls < -77) //layer 9
       {
         registry.fill(HIST("TracksXLastClsYLastCls_secondFace"), track.x(), track.y());
         registry.fill(HIST("TracksPhiEta_LastClsLastDisk_secondFace"), phiLastCls, etaLastCls);
@@ -97,43 +94,31 @@ struct analyseMFTTracks {
   //end of processRec
   PROCESS_SWITCH(analyseMFTTracks, processRec, "Process rec level", true);
 
-
-
   void processTracks(o2::aod::MFTTracks const& tracks)
   {
 
-    for (auto& track : tracks)
-    {
+    for (auto& track : tracks) {
       registry.fill(HIST("TracksPhiEta"), track.phi(), track.eta());
 
-      if (track.nClusters()>=5)
-      {
+      if (track.nClusters() >= 5) {
         registry.fill(HIST("TracksPhiEta_5"), track.phi(), track.eta());
       }
-      if (track.nClusters()>=6)
-      {
+      if (track.nClusters() >= 6) {
         registry.fill(HIST("TracksPhiEta_6"), track.phi(), track.eta());
       }
-      if (track.nClusters()>=7)
-      {
+      if (track.nClusters() >= 7) {
         registry.fill(HIST("TracksPhiEta_7"), track.phi(), track.eta());
       }
-      if (track.nClusters()>=8)
-      {
+      if (track.nClusters() >= 8) {
         registry.fill(HIST("TracksPhiEta_8"), track.phi(), track.eta());
       }
-      if (track.nClusters()>=9)
-      {
+      if (track.nClusters() >= 9) {
         registry.fill(HIST("TracksPhiEta_9"), track.phi(), track.eta());
       }
-    }//end of tracks loop
+    } //end of tracks loop
   }
   //end of processTracks
   PROCESS_SWITCH(analyseMFTTracks, processTracks, "Process rec level for all tracks", true);
-
-
-
-
 
   void processGen(aod::McCollisions::iterator const& mcCollision, Particles const& particles)
   {
