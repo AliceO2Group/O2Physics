6d47a518836e3ae2c8287cc989deb8e817547a42
David Dobrigkeit Chinellato <david.dobrigkeit.chinellato@cern.ch>
2022-03-01 08:59:42 +0100

    Fix bin search for ALICE 3 cent

--- ALICE3/TableProducer/alice3-centrality.cxx
+++ ALICE3/TableProducer/alice3-centrality.cxx
@@ -69,7 +69,7 @@ struct ALICE3Centrality {
     LOG(info) << nevs++ << ") Event " << collision.globalIndex() << " has " << nTracks << " tracks";
     histos.fill(HIST("centrality/numberOfTracks"), nTracks);
 
-    float centALICE3 = hCumMultALICE3->GetBinContent(nTracks);
+    float centALICE3 = hCumMultALICE3->GetBinContent(hCumMultALICE3->FindBin(nTracks));
     cent(centALICE3);
   }
 };
