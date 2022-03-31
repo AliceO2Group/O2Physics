30b09092d404b7f0116868460cf36cf29b7b6e69
sarahherrmann <83958698+sarahherrmann@users.noreply.github.com>
2022-03-31 15:25:42 +0200

    clang format change

--- PWGMM/Tasks/assessment-mft.cxx
+++ PWGMM/Tasks/assessment-mft.cxx
@@ -18,25 +18,20 @@
 using namespace o2;
 using namespace o2::framework;
 
-
 struct AssessmentMFT {
 
   HistogramRegistry registry{
     "registry",
     {
 
-      {"TracksPhiEta", "; #varphi; #eta; tracks", {HistType::kTH2F, {{600, 0, 2 * M_PI}, {35, -4.5, -1.}}}},         //
-      {"TracksTime", "; time; #count", {HistType::kTH1D, {{6000000, 0, 60000}}}},         //
-    }                                                                                                                //
+      {"TracksPhiEta", "; #varphi; #eta; tracks", {HistType::kTH2F, {{600, 0, 2 * M_PI}, {35, -4.5, -1.}}}}, //
+      {"TracksTime", "; time; #count", {HistType::kTH1D, {{6000000, 0, 60000}}}},                            //
+    }                                                                                                        //
   };
-
-
-
 
   void process(aod::Collisions::iterator const& collision, aod::MFTTracks const& tracks, aod::BCs const& bcs)
   {
-    for (auto& track : tracks)
-    {
+    for (auto& track : tracks) {
       float phi = track.phi();
       o2::math_utils::bringTo02Pi(phi);
       registry.fill(HIST("TracksPhiEta"), phi, track.eta());
@@ -44,16 +39,13 @@ struct AssessmentMFT {
       auto collisionBCID = collision.bcId();
       auto collisionBC = (bcs.iteratorAt(collisionBCID)).globalBC();
       //printf("collisionBC %d\n", collisionBC);
-      double seconds = (collisionBC* o2::constants::lhc::LHCBunchSpacingNS + track.trackTime() )/1e9;
+      double seconds = (collisionBC * o2::constants::lhc::LHCBunchSpacingNS + track.trackTime()) / 1e9;
       //this is in s
       //printf("seconds  %f\n", seconds);
       registry.fill(HIST("TracksTime"), seconds);
-
     }
     //printf("minTrackTime = %f, maxTrackTime = %f\n", minTrackTime, maxTrackTime);
   }
-
-
 };
 
 WorkflowSpec
