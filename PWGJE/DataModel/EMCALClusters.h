6d7991be41f706a97b640229c035074f9bae97ae
Florian Jonas <florian.jonas@cern.ch>
2022-04-01 11:22:42 +0200

    [EMCAL-734] fixed merge conflicts

--- PWGJE/DataModel/EMCALClusters.h
+++ PWGJE/DataModel/EMCALClusters.h
@@ -22,17 +22,27 @@ namespace o2::aod
 {
 namespace emcalcluster
 {
-DECLARE_SOA_INDEX_COLUMN(Collision, collision); //!
-DECLARE_SOA_COLUMN(Energy, energy, float);      //!
-DECLARE_SOA_COLUMN(Eta, eta, float);            //!
-DECLARE_SOA_COLUMN(Phi, phi, float);            //!
-DECLARE_SOA_COLUMN(M02, m02, float);            //!
+DECLARE_SOA_INDEX_COLUMN(Collision, collision);                        //!
+DECLARE_SOA_COLUMN(ID, id, int);                                       //!
+DECLARE_SOA_COLUMN(Energy, energy, float);                             //!
+DECLARE_SOA_COLUMN(CoreEnergy, coreEnergy, float);                     //!
+DECLARE_SOA_COLUMN(Eta, eta, float);                                   //!
+DECLARE_SOA_COLUMN(Phi, phi, float);                                   //!
+DECLARE_SOA_COLUMN(M02, m02, float);                                   //!
+DECLARE_SOA_COLUMN(M20, m20, float);                                   //!
+DECLARE_SOA_COLUMN(NCells, nCells, int);                               //!
+DECLARE_SOA_COLUMN(Time, time, float);                                 //!
+DECLARE_SOA_COLUMN(IsExotic, isExotic, bool);                          //!
+DECLARE_SOA_COLUMN(DistanceToBadChannel, distanceToBadChannel, float); //!
+DECLARE_SOA_COLUMN(NLM, nlm, int);                                     //!
+
 } // namespace emcalcluster
 
 DECLARE_SOA_TABLE(EMCALClusters, "AOD", "EMCALCLUSTERS", //!
-                  o2::soa::Index<>, emcalcluster::CollisionId,
-                  emcalcluster::Energy, emcalcluster::Eta, emcalcluster::Phi,
-                  emcalcluster::M02);
+                  o2::soa::Index<>, emcalcluster::CollisionId, emcalcluster::ID, emcalcluster::Energy,
+                  emcalcluster::CoreEnergy, emcalcluster::Eta, emcalcluster::Phi, emcalcluster::M02,
+                  emcalcluster::M20, emcalcluster::NCells, emcalcluster::Time,
+                  emcalcluster::IsExotic, emcalcluster::DistanceToBadChannel, emcalcluster::NLM);
 
 using EMCALCluster = EMCALClusters::iterator;
 
