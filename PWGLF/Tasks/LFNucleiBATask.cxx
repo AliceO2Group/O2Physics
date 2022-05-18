954d69ebca0a90779ec63047397d48d9c073ad45
Giovanni Malfattore <giovanni.malfattore@cern.ch>
2022-05-18 17:16:06 +0200

    Fix histogram names

--- PWGLF/Tasks/LFNucleiBATask.cxx
+++ PWGLF/Tasks/LFNucleiBATask.cxx
@@ -32,33 +32,33 @@
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
-// class o2::aod::LfTreeCreatorNuclei;
+
 struct LFNucleiDeuteronTask {
   OutputObj<TH1F> h1VtxZ{TH1F("h1VtxZ", "V_{z};V_{z} (in cm); counts", 3000, -15, 15)};
   OutputObj<TH1F> h1CentV0M{TH1F("h1CentV0M", "V0M; Multiplicity; counts", 27000, 0, 27000)};
-  OutputObj<TH2F> h2TPCsignVsTPCmomentum{TH2F(" h2TPCsignVsTPCmomentum", " -dE/dX vs p; p (GeV/c); -dE/dx (a.u.)", 500, 0.0, 5.0, 1000, 0.0, 1E3)};
-  OutputObj<TH2F> h2TOFbetaVsP{TH2F("h2TOFbetaVsP", " #beta (TOF) vs p; p (GeV/c); #beta", 500, 0.0, 5, 1200, 0.0, 1.2)};
-  OutputObj<TH2F> h2PionVspNSigmaTPC{TH2F(" h2PionVspNSigmaTPC", "NSigmaTPC(pi) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
-  OutputObj<TH2F> h2KaonVspNSigmaTPC{TH2F(" h2KaonVspNSigmaTPC", "NSigmaTPC(Ka) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
-  OutputObj<TH2F> h2ProtonVspNSigmaTPC{TH2F(" h2ProtonVspNSigmaTPC", "NSigmaTPC(proton) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
-  OutputObj<TH2F> h2DeuteronVspNSigmaTPC{TH2F(" h2DeuteronVspNSigmaTPC", "NSigmaTPC(D) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
-  OutputObj<TH2F> h2HeliumVspNSigmaTPC{TH2F(" h2HeliumVspNSigmaTPC", "NSigmaTPC(He) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
-  OutputObj<TH2F> h2PionVspNSigmaTOF{TH2F(" h2PionVspNSigmaTOF", "NSigmaTOF(pi) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
-  OutputObj<TH2F> h2KaonVspNSigmaTOF{TH2F(" h2KaonVspNSigmaTOF", "NSigmaTOF(Ka) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
-  OutputObj<TH2F> h2ProtonVspNSigmaTOF{TH2F(" h2ProtonVspNSigmaTOF", "NSigmaTOF(proton) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
-  OutputObj<TH2F> h2DeuteronVspNSigmaTOF{TH2F(" h2DeuteronVspNSigmaTOF", "NSigmaTOF(D) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
-  OutputObj<TH2F> h2HeliumVspNSigmaTOF{TH2F(" h2HeliumVspNSigmaTOF", "NSigmaTOF(He) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2TPCsignVsTPCmomentum{TH2F("h2TPCsignVsTPCmomentum", "-dE/dX vs p; p (GeV/c); -dE/dx (a.u.)", 500, 0.0, 5.0, 1000, 0.0, 1E3)};
+  OutputObj<TH2F> h2TOFbetaVsP{TH2F("h2TOFbetaVsP", "#beta (TOF) vs p; p (GeV/c); #beta", 500, 0.0, 5, 1200, 0.0, 1.2)};
+  OutputObj<TH2F> h2PionVspNSigmaTPC{TH2F("h2PionVspNSigmaTPC", "NSigmaTPC(pi) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2KaonVspNSigmaTPC{TH2F("h2KaonVspNSigmaTPC", "NSigmaTPC(Ka) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2ProtonVspNSigmaTPC{TH2F("h2ProtonVspNSigmaTPC", "NSigmaTPC(proton) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2DeuteronVspNSigmaTPC{TH2F("h2DeuteronVspNSigmaTPC", "NSigmaTPC(D) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2HeliumVspNSigmaTPC{TH2F("h2HeliumVspNSigmaTPC", "NSigmaTPC(He) vs p; Momentum(p) in GeV; NSigmaTPC", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2PionVspNSigmaTOF{TH2F("h2PionVspNSigmaTOF", "NSigmaTOF(pi) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2KaonVspNSigmaTOF{TH2F("h2KaonVspNSigmaTOF", "NSigmaTOF(Ka) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2ProtonVspNSigmaTOF{TH2F("h2ProtonVspNSigmaTOF", "NSigmaTOF(proton) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2DeuteronVspNSigmaTOF{TH2F("h2DeuteronVspNSigmaTOF", "NSigmaTOF(D) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
+  OutputObj<TH2F> h2HeliumVspNSigmaTOF{TH2F("h2HeliumVspNSigmaTOF", "NSigmaTOF(He) vs p; Momentum(p) in GeV; NSigmaTOF", 100, 0, 10., 200, -10, 10.)};
   OutputObj<TH1F> h1ProtonSpectra{TH1F("h1ProtonSpectra", "#it{p}_{T} (p); #it{p}_{T} (GeV/#it{c}); counts", 800, 0, 8)};
   OutputObj<TH1F> h1DeuteronSpectra{TH1F("h1DeuteronSpectra", "#it{p}_{T} (d); #it{p}_{T} (GeV/#it{c}); counts", 800, 0, 8)};
   OutputObj<TH1F> h1HeliumSpectra{TH1F("h1HeliumSpectra", "#it{p}_{T} (He); #it{p}_{T} (GeV/#it{c}); counts", 800, 0, 8)};
 
   // trackQA
-  // OutputObj<TH1F> h1Y{TH1F("h1Y", " rapidity; Y; counts", 200, -10.0, 10)};
-  OutputObj<TH1F> h1Eta{TH1F("h1Eta", " pseudoRapidity; #eta; counts", 200, -1.0, 1.0)};
-  OutputObj<TH1F> h1VarPhi{TH1F("h1VarPhi", " #phi; #phi; counts", 80, -0.5, 7.5)};
-  OutputObj<TH2F> h2EtaVsPhi{TH2F("h2EtaVsPhi", " #eta (TOF) vs #phi; #eta; #phi", 200, -1.0, 1.0, 80, -0.5, 7.5)};
-  OutputObj<TH1F> h1pT{TH1F("h1pT", " Track #it{p}_{T}; #it{p}_{T} (GeV/#it{c}); counts", 1000, 0., 10)};
-  OutputObj<TH1F> h1p{TH1F("h1p", " Track momentum; p (GeV/#it{c}); counts", 1000, 0., 10.)};
+  // OutputObj<TH1F> h1Y{TH1F("h1Y", "rapidity; Y; counts", 200, -10.0, 10)};
+  OutputObj<TH1F> h1Eta{TH1F("h1Eta", "pseudoRapidity; #eta; counts", 200, -1.0, 1.0)};
+  OutputObj<TH1F> h1VarPhi{TH1F("h1VarPhi", "#phi; #phi; counts", 80, -0.5, 7.5)};
+  OutputObj<TH2F> h2EtaVsPhi{TH2F("h2EtaVsPhi", "#eta (TOF) vs #phi; #eta; #phi", 200, -1.0, 1.0, 80, -0.5, 7.5)};
+  OutputObj<TH1F> h1pT{TH1F("h1pT", "Track #it{p}_{T}; #it{p}_{T} (GeV/#it{c}); counts", 1000, 0., 10)};
+  OutputObj<TH1F> h1p{TH1F("h1p", "Track momentum; p (GeV/#it{c}); counts", 1000, 0., 10.)};
 
   // TPCQA
   OutputObj<TH1F> h1TPCncr{TH1F("h1TPCncr", "number of crossed rows in TPC; TPCncr; counts", 150, 60, 170)};
@@ -67,12 +67,12 @@ struct LFNucleiDeuteronTask {
   OutputObj<TH1F> h1chi2TPC{TH1F("h1chi2TPC", "#chi^{2}_{ITS}/n_{TPC}; #chi^{2}_{ITS}/n_{TPC}; counts", 11, -0.5, 10.5)};
 
   // TOF mass histograms
-  OutputObj<TH2F> h2TOFmassVsPt{TH2F("h2TOFmassVsPt", " h2TOFmassVsPt; TOFmass; #it{p}_{T} (GeV)", 600, 0., 3., 500, 0., 5.)};
+  OutputObj<TH2F> h2TOFmassVsPt{TH2F("h2TOFmassVsPt", "h2TOFmassVsPt; TOFmass; #it{p}_{T} (GeV)", 600, 0., 3., 500, 0., 5.)};
 
   // TOF mass squared histograms
-  OutputObj<TH2F> h2TOFmass2ProtonVsPt{TH2F("h2TOFmass2ProtonVsPt", " #Delta M^{2} (p) vs #it{p}_{T}; #Delta M^{2} (p); #it{p}_{T} (GeV/#it{c})", 600, -3., 3., 800, 0., 8.)};
-  OutputObj<TH2F> h2TOFmass2DeuteronVsPt{TH2F("h2TOFmass2DeuteronVsPt", " #Delta M^{2} (d) vs #it{p}_{T}; #Delta M^{2} (d); #it{p}_{T} (GeV/#it{c})", 1000, -5., 5., 800, 0., 8.)};
-  OutputObj<TH2F> h2TOFmass2HeliumVsPt{TH2F("h2TOFmass2HeliumVsPt", " #Delta M^{2} (He) vs #it{p}_{T}; #Delta M^{2} (He); #it{p}_{T} (GeV/#it{c})", 1800, -9., 9., 800, 0., 8.)};
+  OutputObj<TH2F> h2TOFmass2ProtonVsPt{TH2F("h2TOFmass2ProtonVsPt", "#Delta M^{2} (p) vs #it{p}_{T}; #Delta M^{2} (p); #it{p}_{T} (GeV/#it{c})", 600, -3., 3., 800, 0., 8.)};
+  OutputObj<TH2F> h2TOFmass2DeuteronVsPt{TH2F("h2TOFmass2DeuteronVsPt", "#Delta M^{2} (d) vs #it{p}_{T}; #Delta M^{2} (d); #it{p}_{T} (GeV/#it{c})", 1000, -5., 5., 800, 0., 8.)};
+  OutputObj<TH2F> h2TOFmass2HeliumVsPt{TH2F("h2TOFmass2HeliumVsPt", "#Delta M^{2} (He) vs #it{p}_{T}; #Delta M^{2} (He); #it{p}_{T} (GeV/#it{c})", 1800, -9., 9., 800, 0., 8.)};
 
   void init(o2::framework::InitContext&)
   {
