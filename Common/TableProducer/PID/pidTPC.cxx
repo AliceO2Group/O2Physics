670fe04ad8fa5c1f0f02519050c65c95de157bcf
Jeremy Wilkinson <jeremy.wilkinson@cern.ch>
2022-06-09 16:12:27 +0200

    fix caching behaviour for TPC CCDB objects

--- Common/TableProducer/PID/pidTPC.cxx
+++ Common/TableProducer/PID/pidTPC.cxx
@@ -136,12 +136,12 @@ struct tpcPid {
       const std::string path = ccdbPath.value;
       const auto time = ccdbTimestamp.value;
       ccdb->setURL(url.value);
-      ccdb->setTimestamp(time);
+    //  ccdb->setTimestamp(time);
       ccdb->setCaching(true);
       ccdb->setLocalObjectValidityChecking();
       ccdb->setCreatedNotAfter(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
-      response.SetParameters(ccdb->getForTimeStamp<o2::pid::tpc::Response>(path, time));
-      LOGP(info, "Loading TPC response from CCDB, using path: {} for ccdbTimestamp {}", path, time);
+    //  response.SetParameters(ccdb->getForTimeStamp<o2::pid::tpc::Response>(path, time));
+      LOGP(info, "Initialising default TPC PID response:");
       response.PrintAll();
     }
 
