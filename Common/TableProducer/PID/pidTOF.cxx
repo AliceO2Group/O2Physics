9f4529f6e31417ba07f549b97fd449aaea35b12c
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2022-06-14 12:36:15 +0200

    Add common helper function

--- Common/TableProducer/PID/pidTOF.cxx
+++ Common/TableProducer/PID/pidTOF.cxx
@@ -79,18 +79,7 @@ struct tofPid {
   {
     // Checking the tables are requested in the workflow and enabling them
     auto enableFlag = [&](const std::string particle, Configurable<int>& flag) {
-      const std::string table = "pidTOF" + particle;
-      if (isTableRequiredInWorkflow(initContext, table)) {
-        if (flag < 0) {
-          flag.value = 1;
-          LOG(info) << "Auto-enabling table: " + table;
-        } else if (flag > 0) {
-          flag.value = 1;
-          LOG(info) << "Table enabled: " + table;
-        } else {
-          LOG(info) << "Table disabled: " + table;
-        }
-      }
+      enableFlagIfTableRequired(initContext, "pidTOF" + particle, flag);
     };
 
     enableFlag("El", pidEl);
