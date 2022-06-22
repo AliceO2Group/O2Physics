3b251744f7e6f69213689b3e614f9f55a5d9af12
sarahherrmann <83958698+sarahherrmann@users.noreply.github.com>
2022-06-22 17:48:40 +0200

    Made a change to enable the new correction on events

--- PWGMM/Mult/Tasks/dndeta-mft.cxx
+++ PWGMM/Mult/Tasks/dndeta-mft.cxx
@@ -68,7 +68,7 @@ struct PseudorapidityDensityMFT {
       registry.add({"TracksEtaZvtxGen", "; #eta; Z_{vtx}; tracks", {HistType::kTH2F, {{18, -4.6, -1.}, {201, -20.1, 20.1}}}});
       registry.add({"TracksEtaZvtxGen_t", "; #eta; Z_{vtx}; tracks", {HistType::kTH2F, {{18, -4.6, -1.}, {201, -20.1, 20.1}}}});
       registry.add({"TracksPhiEtaGen", "; #varphi; #eta; tracks", {HistType::kTH2F, {{600, 0, 2 * M_PI}, {18, -4.6, -1.}}}});
-      registry.add({"TracksToPartPtEta", " ; p_{T} (GeV/c); #eta", {HistType::kTH2F, {PtAxis, {18, -4.6, -1.}}}}); //
+      registry.add({"TracksToPartPtEta", " ; p_{T} (GeV/c); #eta", {HistType::kTH2F, {PtAxis, {18, -4.6, -1.}}}});                       //
       registry.add({"TracksPtEtaGen", " ; p_{T} (GeV/c); #eta", {HistType::kTH2F, {PtAxis, {18, -4.6, -1.}}}});
       registry.add({"TracksPtEtaGen_t", " ; p_{T} (GeV/c); #eta", {HistType::kTH2F, {PtAxis, {18, -4.6, -1.}}}});
       registry.add({"EventEfficiency", "; status; events", {HistType::kTH1F, {{5, 0.5, 5.5}}}});
@@ -114,6 +114,8 @@ struct PseudorapidityDensityMFT {
 
   PROCESS_SWITCH(PseudorapidityDensityMFT, processTagging, "Collect event sample stats", true);
 
+  Partition<aod::MFTTracks> sample = (aod::fwdtrack::eta < -2.8f) && (aod::fwdtrack::eta > -3.2f);
+
   void process(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, aod::MFTTracks const& tracks)
   {
     registry.fill(HIST("EventSelection"), 1.);
@@ -121,9 +123,9 @@ struct PseudorapidityDensityMFT {
       registry.fill(HIST("EventSelection"), 2.);
       auto z = collision.posZ();
 
-      auto groupedTracks = tracks.sliceBy(o2::aod::fwdtrack::collisionId, collision.globalIndex());
+      auto perCollisionSample = sample->sliceByCached(o2::aod::fwdtrack::collisionId, collision.globalIndex());
 
-      registry.fill(HIST("EventsNtrkZvtx"), groupedTracks.size(), z);
+      registry.fill(HIST("EventsNtrkZvtx"), perCollisionSample.size(), z);
       for (auto& track : tracks) {
         registry.fill(HIST("TracksEtaZvtx"), track.eta(), z);
         float phi = track.phi();
@@ -136,12 +138,29 @@ struct PseudorapidityDensityMFT {
     }
   }
 
-  using Particles = aod::McParticles;
+  using Particles = soa::Filtered<aod::McParticles>;
   expressions::Filter primaries = (aod::mcparticle::flags & (uint8_t)o2::aod::mcparticle::enums::PhysicalPrimary) == (uint8_t)o2::aod::mcparticle::enums::PhysicalPrimary;
+  Partition<Particles> mcSample = (aod::mcparticle::eta < -2.8f) && (aod::mcparticle::eta > -3.2f);
 
-  void processGen(aod::McCollisions::iterator const& mcCollision, o2::soa::SmallGroups<soa::Join<aod::Collisions, aod::EvSels, aod::McCollisionLabels>> const& collisions, soa::Filtered<Particles> const& particles, aod::MFTTracks const& tracks)
+  void processGen(aod::McCollisions::iterator const& mcCollision, o2::soa::SmallGroups<soa::Join<aod::Collisions, aod::EvSels, aod::McCollisionLabels>> const& collisions, Particles const& particles, aod::MFTTracks const& tracks)
   {
     registry.fill(HIST("EventEfficiency"), 1.);
+
+    auto perCollisionMCSample = mcSample->sliceByCached(aod::mcparticle::mcCollisionId, mcCollision.globalIndex());
+    auto nCharged = 0;
+    for (auto& particle : perCollisionMCSample) {
+      auto charge = 0.;
+      auto p = pdg->GetParticle(particle.pdgCode());
+      if (p != nullptr) {
+        charge = p->Charge();
+      }
+      if (std::abs(charge) < 3.) {
+        continue;
+      }
+      nCharged++;
+    }
+    registry.fill(HIST("EventsNtrkZvtxGen_t"), nCharged, mcCollision.posZ());
+
 
     bool atLeastOne = false;
 
@@ -150,17 +169,17 @@ struct PseudorapidityDensityMFT {
       registry.fill(HIST("EventEfficiency"), 3.);
       if (!useEvSel || (useEvSel && collision.sel8())) {
         atLeastOne = true;
-        auto groupedTracks = tracks.sliceBy(o2::aod::fwdtrack::collisionId, collision.globalIndex());
+        auto perCollisionSample = sample->sliceByCached(o2::aod::fwdtrack::collisionId, collision.globalIndex());
 
         registry.fill(HIST("EventEfficiency"), 4.);
 
-        registry.fill(HIST("EventsNtrkZvtxGen"), groupedTracks.size(), collision.posZ());
+        registry.fill(HIST("EventsNtrkZvtxGen"), perCollisionSample.size(), collision.posZ());
       }
     }
     if (collisions.size() == 0) {
       registry.fill(HIST("NotFoundEventZvtx"), mcCollision.posZ());
     }
-    auto nCharged = 0;
+
     for (auto& particle : particles) {
       auto p = pdg->GetParticle(particle.pdgCode());
       auto charge = 0;
@@ -179,22 +198,29 @@ struct PseudorapidityDensityMFT {
       registry.fill(HIST("TracksPhiEtaGen"), particle.phi(), particle.eta());
       registry.fill(HIST("TracksPtEtaGen_t"), particle.pt(), particle.eta());
 
-      nCharged++;
     }
-    registry.fill(HIST("EventsNtrkZvtxGen_t"), nCharged, mcCollision.posZ());
+
   }
 
   PROCESS_SWITCH(PseudorapidityDensityMFT, processGen, "Process generator-level info", false);
 
+
   void processGenPt(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, MFTTracksLabeled const& tracks, aod::McParticles const&)
   {
     //In the MFT the measurement of pT is not precise, so we access it by using the particle's pT instead
-    if (!useEvSel || (useEvSel && collision.sel8())) {
-      for (auto& track : tracks) {
-        if (!track.has_mcParticle()) {
+    if (!useEvSel || (useEvSel && collision.sel8()))
+    {
+      for (auto& track : tracks)
+      {
+        if (!track.has_mcParticle())
+        {
           continue;
         }
         auto particle = track.mcParticle();
+        if (!particle.isPhysicalPrimary())
+        {
+          continue;
+        }
         registry.fill(HIST("TracksToPartPtEta"), particle.pt(), particle.eta());
       }
     }
