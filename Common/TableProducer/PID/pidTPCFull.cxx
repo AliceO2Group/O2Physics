7df0f1510e309407421482230c22f1d5981cbdae
Christian Sonnabend <sonnabendch@gmail.com>
2022-07-05 20:36:40 +0200

    Bug fix: Fixing build error on MacOS

--- Common/TableProducer/PID/pidTPCFull.cxx
+++ Common/TableProducer/PID/pidTPCFull.cxx
@@ -168,7 +168,7 @@ struct tpcPidFull {
 
     const unsigned long tracks_size = tracks.size();
 
-    auto reserveTable = [&tracks, &tracks_size](const Configurable<int>& flag, auto& table) {
+    auto reserveTable = [&tracks_size](const Configurable<int>& flag, auto& table) {
       if (flag.value != 1) {
         return;
       }
