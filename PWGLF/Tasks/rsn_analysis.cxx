81cf6e1654905beaa817b70af51b39e70519860e
Nicola Rubini <nrubini@Nicolas-MacBook-Pro.local>
2022-07-07 23:47:42 +0200

    header

--- PWGLF/Tasks/rsn_analysis.cxx
+++ PWGLF/Tasks/rsn_analysis.cxx
@@ -1,12 +1,21 @@
-// Copyright CERN and copyright holders of ALICE O2. This software is
-// distributed under the terms of the GNU General Public License v3 (GPL
-// Version 3), copied verbatim in the file "COPYING".
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
-// See http://alice-o2.web.cern.ch/license for full licensing information.
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
+
+///
+/// \file rsn_analysis.cxx
+///
+/// \brief  Analysis task for the measurement of resonances
+///
+/// \author Nicola Rubini <nrubini@cern.ch>
+///
 
 // O2 includes
 #include "Common/DataModel/EventSelection.h"
