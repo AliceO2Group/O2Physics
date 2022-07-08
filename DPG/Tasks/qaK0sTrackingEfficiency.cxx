dc746c5d6def05afdb8a7c1a7088ca6eb6166310
Alessandro Balbino <45421980+alessandrobalbino@users.noreply.github.com>
2022-07-08 14:08:12 +0200

    [DPG] Add information on ITS and TPC tracks + minor fixes (#961)
    * Collect selections in a single function
    
    * Require TPC-tracks only
    
    * Add ITS matching information
    
    * Some cleanup
    
    * Store complete ITS hit-map information
    
    Co-authored-by: Alessandro Balbino <alessandro.balbino@cern.ch>

--- DPG/Tasks/qaK0sTrackingEfficiency.cxx
+++ DPG/Tasks/qaK0sTrackingEfficiency.cxx
@@ -26,6 +26,7 @@ using namespace o2;
 using namespace o2::framework;
 
 using PIDTracks = soa::Join<aod::Tracks, aod::TracksExtra, aod::pidTPCPi>;
+using SelectedCollisions = soa::Join<aod::Collisions, aod::EvSels>;
 
 struct qaK0sTrackingEfficiency {
 
@@ -37,21 +38,21 @@ struct qaK0sTrackingEfficiency {
     const AxisSpec pTAxis{200, 0.f, 10.f, "#it{p}_{T} (GeV/#it{c})"};
     const AxisSpec mAxis{200, 0.4f, 0.6f, "#it{m} (GeV/#it{c}^{2})"};
     const AxisSpec statusAxis{2, -0.5f, 1.5f, ""};
-    const AxisSpec nhitsAxis{4, -0.5f, 3.5f, ""};
+    const AxisSpec hitMapAxis{128, -0.5f, 127.5f, ""};
 
     registry.add("h_EventCounter", "", kTH1D, {{2, -0.5f, 1.5f, ""}});
     registry.get<TH1>(HIST("h_EventCounter"))->GetXaxis()->SetBinLabel(1, "Total");
     registry.get<TH1>(HIST("h_EventCounter"))->GetXaxis()->SetBinLabel(2, "Selected");
 
-    registry.add("h5_RpTmassStatus", "h5_RpTmassStatus", {HistType::kTHnSparseD, {RAxis, pTAxis, mAxis, statusAxis, statusAxis}});
+    registry.add("h5_RpTmassITSHitMap", "h5_RpTmassITSHitMap", {HistType::kTHnSparseD, {RAxis, pTAxis, mAxis, hitMapAxis, hitMapAxis}});
 
     registry.add("Test/h_R", "h_R", {HistType::kTH1D, {RAxis}});
     registry.add("Test/h_pT", "h_pT", {HistType::kTH1D, {pTAxis}});
     registry.add("Test/h_mass", "h_mass", {HistType::kTH1D, {mAxis}});
-    registry.add("Test/h_negStatus", "h_negStatus", {HistType::kTH1D, {statusAxis}});
-    registry.add("Test/h_posStatus", "h_posStatus", {HistType::kTH1D, {statusAxis}});
-    registry.add("Test/h_negIBhits", "h_negIBhits", {HistType::kTH1D, {nhitsAxis}});
-    registry.add("Test/h_posIBhits", "h_posIBhits", {HistType::kTH1D, {nhitsAxis}});
+    registry.add("Test/h_negITSStatus", "h_negITSStatus", {HistType::kTH1I, {statusAxis}});
+    registry.add("Test/h_posITSStatus", "h_posITSStatus", {HistType::kTH1I, {statusAxis}});
+    registry.add("Test/h_negITSHitMap", "h_negITSHitMap", {HistType::kTH1I, {hitMapAxis}});
+    registry.add("Test/h_posITSHitMap", "h_posITSHitMap", {HistType::kTH1I, {hitMapAxis}});
   }
 
   // Selection criteria
@@ -60,7 +61,24 @@ struct qaK0sTrackingEfficiency {
   Configurable<float> nSigTPC{"nSigTPC", 10., "nSigTPC"};
   Configurable<bool> eventSelection{"eventSelection", true, "event selection"};
 
-  void process(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, aod::V0Datas const& fullV0s, PIDTracks const& tracks)
+  template <typename T1, typename T2, typename C>
+  bool acceptV0(T1 v0, T2 ntrack, T2 ptrack, C collision)
+  {
+    // Apply selections on V0
+    if (v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) < v0cospa)
+      return kFALSE;
+    if (TMath::Abs(v0.yK0Short()) > rapidity)
+      return kFALSE;
+
+    // Apply selections on V0 daughters
+    if (!ntrack.hasTPC() || !ptrack.hasTPC())
+      return kFALSE;
+    if (ntrack.tpcNSigmaPi() > nSigTPC || ptrack.tpcNSigmaPi() > nSigTPC)
+      return kFALSE;
+    return kTRUE;
+  }
+
+  void process(SelectedCollisions::iterator const& collision, aod::V0Datas const& fullV0s, PIDTracks const& tracks)
   // TODO: add centrality
   {
     registry.fill(HIST("h_EventCounter"), 0.);
@@ -70,36 +88,23 @@ struct qaK0sTrackingEfficiency {
     registry.fill(HIST("h_EventCounter"), 1.);
 
     for (auto& v0 : fullV0s) {
-      if (v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) > v0cospa) {
 
-        auto reconegtrack = v0.negTrack_as<PIDTracks>();
-        auto recopostrack = v0.posTrack_as<PIDTracks>();
+      auto reconegtrack = v0.negTrack_as<PIDTracks>();
+      auto recopostrack = v0.posTrack_as<PIDTracks>();
 
-        if (TMath::Abs(v0.yK0Short()) < rapidity) {
-          if (reconegtrack.tpcNSigmaPi() < nSigTPC && recopostrack.tpcNSigmaPi() < nSigTPC) {
-            registry.fill(HIST("Test/h_R"), v0.v0radius());
-            registry.fill(HIST("Test/h_pT"), v0.pt());
-            registry.fill(HIST("Test/h_mass"), v0.mK0Short());
+      if (acceptV0(v0, reconegtrack, recopostrack, collision)) {
+        registry.fill(HIST("Test/h_R"), v0.v0radius());
+        registry.fill(HIST("Test/h_pT"), v0.pt());
+        registry.fill(HIST("Test/h_mass"), v0.mK0Short());
 
-            int negIBNhits = 0, posIBNhits = 0;
-            for (unsigned int i = 0; i < 3; i++) {
-              if (reconegtrack.itsClusterMap() & (1 << i)) {
-                negIBNhits++;
-              }
-              if (recopostrack.itsClusterMap() & (1 << i)) {
-                posIBNhits++;
-              }
-            }
-            bool negHasIB = (bool)negIBNhits;
-            bool posHasIB = (bool)posIBNhits;
-            registry.fill(HIST("Test/h_negStatus"), negHasIB);
-            registry.fill(HIST("Test/h_posStatus"), posHasIB);
-            registry.fill(HIST("Test/h_negIBhits"), negIBNhits);
-            registry.fill(HIST("Test/h_posIBhits"), posIBNhits);
+        registry.fill(HIST("Test/h_negITSStatus"), reconegtrack.hasITS());
+        registry.fill(HIST("Test/h_posITSStatus"), recopostrack.hasITS());
 
-            registry.fill(HIST("h5_RpTmassStatus"), v0.v0radius(), v0.pt(), v0.mK0Short(), negHasIB, posHasIB);
-          }
-        }
+        uint8_t negITSHitMap = reconegtrack.itsClusterMap();
+        uint8_t posITSHitMap = recopostrack.itsClusterMap();
+        registry.fill(HIST("Test/h_negITSHitMap"), negITSHitMap);
+        registry.fill(HIST("Test/h_posITSHitMap"), posITSHitMap);
+        registry.fill(HIST("h5_RpTmassITSHitMap"), v0.v0radius(), v0.pt(), v0.mK0Short(), negITSHitMap, posITSHitMap);
       }
     }
   }
