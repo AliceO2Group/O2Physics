42b26fc4d15d14aa4a2eb5ae08af865ceaa6dbd2
AnnalenaKa <annalena.kalteyer@gmail.com>
2022-07-28 16:28:33 +0200

    Fix formatting errors

--- Common/TableProducer/PID/pidTPCML.h
+++ Common/TableProducer/PID/pidTPCML.h
@@ -46,8 +46,8 @@ class Network
   template <typename C, typename T>
   std::array<float, 6> createInputFromTrack(const C&, const T&, const uint8_t) const; // create a std::vector<float> with all the inputs for the network
   std::vector<Ort::Value> createTensor(std::array<float, 6>) const;                   // create a std::vector<Ort::Value> (= ONNX tensor) for model input
-  float* evalNetwork(std::vector<Ort::Value>);                                      // evaluate the network on a std::vector<Ort::Value> (= ONNX tensor)
-  float* evalNetwork(std::vector<float>);                                           // evaluate the network on a std::vector<float>
+  float* evalNetwork(std::vector<Ort::Value>);                                        // evaluate the network on a std::vector<Ort::Value> (= ONNX tensor)
+  float* evalNetwork(std::vector<float>);                                             // evaluate the network on a std::vector<float>
 
   // Getters & Setters
   int getInputDimensions() const { return mInputShapes[0][1]; };
@@ -66,7 +66,7 @@ class Network
   std::vector<std::vector<int64_t>> mInputShapes;
   std::vector<std::string> mOutputNames;
   std::vector<std::vector<int64_t>> mOutputShapes;
-  
+
   float nClNorm = 152.f;
 
   // Internal function for printing the shape of tensors: See https://github.com/saganatt/PID_ML_in_O2 or O2Physics/Tools/PIDML/simpleApplyPidOnnxModel.cxx
