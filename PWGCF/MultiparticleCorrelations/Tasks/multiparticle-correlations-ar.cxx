6d440933cf07d562d9e9315dfb69dd1a1bc0918b
Anton Riedel <anton.riedel@cern.ch>
2022-08-05 14:31:41 +0200

    Fix: filter events inside the process function instead of using filters

--- PWGCF/MultiparticleCorrelations/Tasks/multiparticle-correlations-ar.cxx
+++ PWGCF/MultiparticleCorrelations/Tasks/multiparticle-correlations-ar.cxx
@@ -17,6 +17,7 @@
 #include <cstdint>
 #include <string_view>
 #include <string>
+#include <vector>
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/Expressions.h"
@@ -28,8 +29,8 @@ using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
-// Define useful constant and enums in a seperate name space
-namespace MultiParticleCorrelationsARTaskGlobalConfig
+// Define useful constants and enums in a separate name space
+namespace MultiParticleCorrelationsARTaskGlobalVariables
 {
 // for before and after applying cuts
 enum BA {
@@ -46,6 +47,8 @@ enum RS {
   kLAST_RS
 };
 static constexpr std::string_view RecoSim[kLAST_RS] = {"reco/", "sim/"};
+
+// for configuring control histograms
 enum HistConfig {
   kBIN,   // number of bins
   kLEDGE, // lower edge of the histogram
@@ -56,7 +59,7 @@ enum HistConfig {
   kLAST_HistConfig
 };
 
-// setup for event variables
+// event variables
 enum EventVariable {
   kVX,
   kVY,
@@ -78,7 +81,7 @@ static constexpr std::string_view EventVariableNames[kLAST_EventVariable] = {"Ve
                                                                              "MultiplicityWeights",
                                                                              "MultiplicityNumContrib",
                                                                              "MultiplicityTPC"};
-// setup for track variables
+// track variables
 enum TrackVariable {
   kPT,
   kPHI,
@@ -103,49 +106,52 @@ static constexpr std::string_view TrackVariableNames[kLAST_TrackVariable] = {"Pt
                                                                              "TPCChi2",
                                                                              "ITSClusters"};
 
-// info string for configurables
+// common info string for all configurables
 std::string info = std::string(": Hist bins, Hist lower edge, Hist upper edge, lower cut, upper cut, cut ON(1)/OFF(-1)");
 
+// function for computing the absolute distance of the primary vertex form the origin
 inline float abs(float vx, float vy, float vz)
 {
   return std::sqrt(vx * vx + vy * vy * vz * vz);
 }
-}; // namespace MultiParticleCorrelationsARTaskGlobalConfig
 
-// use an alias for the namespace
-namespace AR = MultiParticleCorrelationsARTaskGlobalConfig;
+// generice function for checking if the value of a variable passes a cut
+inline bool SurviveCut(std::vector<float> ConfigValue, float Value)
+{
+  bool flag = true;
+  // check if the cut is configured to be use in the first place
+  if (ConfigValue.at(kOCUT) > 0.) {
+    // check if the value of the variable is not lower than the lower bound and
+    // not not larger than the upper bound
+    if (!(Value > ConfigValue.at(kLCUT) && Value < ConfigValue.at(kUCUT))) {
+      flag = false;
+    }
+  }
+  return flag;
+}
+}; // namespace MultiParticleCorrelationsARTaskGlobalVariables
+
+// use an alias for our namespace
+namespace AR = MultiParticleCorrelationsARTaskGlobalVariables;
 
 struct MultiParticleCorrelationsARTask {
 
-  // configurables and cuts (where possible)
-  // for event variables
+  // event configurables and cuts
   Configurable<std::vector<float>> cfgVX = {std::string(AR::EventVariableNames[AR::kVX]),
                                             {400., -2., 2., -1., 1., 1.},
                                             std::string("Vertex X") + AR::info};
-  Filter filterVX = aod::collision::posX > cfgVX.value.at(AR::kLCUT) &&
-                    aod::collision::posX<cfgVX.value.at(AR::kUCUT) &&
-                                         cfgVX.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgVY = {std::string(AR::EventVariableNames[AR::kVY]),
                                             {400., -2., 2., -1., 1., 1.},
                                             std::string("Vertex Y") + AR::info};
-  Filter filterVY = aod::collision::posY > cfgVY.value.at(AR::kLCUT) &&
-                    aod::collision::posY<cfgVY.value.at(AR::kUCUT) &&
-                                         cfgVY.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgVZ = {std::string(AR::EventVariableNames[AR::kVZ]),
                                             {2400., -12., 12., -10., 10., 1.},
                                             std::string("Vertex Z") + AR::info};
-  Filter filterVZ = aod::collision::posZ > cfgVZ.value.at(AR::kLCUT) &&
-                    aod::collision::posZ<cfgVZ.value.at(AR::kUCUT) &&
-                                         cfgVZ.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgVABS = {std::string(AR::EventVariableNames[AR::kVABS]),
                                               {150., 0., 15, 1.e-6, 15., 1.},
                                               std::string("Vertex distance from origin") + AR::info};
   Configurable<std::vector<float>> cfgCEN = {std::string(AR::EventVariableNames[AR::kCEN]),
                                              {120., 0., 120., 0., 80., 1.},
                                              std::string("Centrality") + AR::info};
-  Filter filterCEN = aod::cent::centRun2V0M > cfgCEN.value.at(AR::kLCUT) &&
-                     aod::cent::centRun2V0M<cfgCEN.value.at(AR::kUCUT) &&
-                                            cfgCEN.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgMULQ = {std::string(AR::EventVariableNames[AR::kMULQ]),
                                               {3000., 0., 3000., 10., 3000., 1.},
                                               std::string("Multiplicity (QVector)") + AR::info};
@@ -155,16 +161,9 @@ struct MultiParticleCorrelationsARTask {
   Configurable<std::vector<float>> cfgMULNC = {std::string(AR::EventVariableNames[AR::kMULNC]),
                                                {3000., 0., 3000., 10., 3000., 1.},
                                                std::string("Multiplicity (NumContrib)") + AR::info};
-  Filter filterMULNC = aod::collision::numContrib > static_cast<uint16_t>(cfgMULNC.value.at(AR::kLCUT)) &&
-                       aod::collision::numContrib<static_cast<uint16_t>(cfgMULNC.value.at(AR::kUCUT)) &&
-                                                  cfgMULNC.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgMULTPC = {std::string(AR::EventVariableNames[AR::kMULTPC]),
                                                 {3000., 0., 3000., 12., 3000., 1.},
                                                 std::string("Multiplicity (TPC)") + AR::info};
-  Filter filterMULTPC = aod::collision::numContrib > static_cast<uint16_t>(cfgMULTPC.value.at(AR::kLCUT)) &&
-                        aod::collision::numContrib<static_cast<uint16_t>(cfgMULTPC.value.at(AR::kUCUT)) &&
-                                                   cfgMULTPC.value.at(AR::kOCUT)> 0.;
-
   // write all event configurables into a vector
   std::vector<Configurable<std::vector<float>>> cfgEvent = {cfgVX,
                                                             cfgVY,
@@ -176,40 +175,25 @@ struct MultiParticleCorrelationsARTask {
                                                             cfgMULNC,
                                                             cfgMULTPC};
 
-  // for track variables and cuts (where possible)
+  // track configurables and cuts
   Configurable<std::vector<float>> cfgPT = {std::string(AR::TrackVariableNames[AR::kPT]),
                                             {600., 0., 6., 0.2, 5., 1.},
                                             std::string("pt") + AR::info};
-  Filter filterPT = aod::track::pt > cfgPT.value.at(AR::kLCUT) &&
-                    aod::track::pt<cfgPT.value.at(AR::kUCUT) &&
-                                   cfgPT.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgPHI = {std::string(AR::TrackVariableNames[AR::kPHI]),
                                              {360., 0., 2. * M_PI, 0., 2. * M_PI, 1.},
                                              std::string("phi") + AR::info};
-  Filter filterPHI = aod::track::phi > cfgPHI.value.at(AR::kLCUT) &&
-                     aod::track::phi<cfgPHI.value.at(AR::kUCUT) &&
-                                     cfgPHI.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgETA = {std::string(AR::TrackVariableNames[AR::kETA]),
                                              {1000., -1., 1., -0.8, 0.8, 1.},
                                              std::string("eta") + AR::info};
-  Filter filterETA = aod::track::eta > cfgETA.value.at(AR::kLCUT) &&
-                     aod::track::eta<cfgETA.value.at(AR::kUCUT) &&
-                                     cfgETA.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgCHARGE = {std::string(AR::TrackVariableNames[AR::kCHARGE]),
                                                 {5., -2.5, 2.5, -1.5, 1.5, 1.},
                                                 std::string("charge") + AR::info};
   Configurable<std::vector<float>> cfgDCAZ = {std::string(AR::TrackVariableNames[AR::kDCAZ]),
                                               {100., -4., 4., -3.2, 3.2, 1.},
                                               std::string("DCA in Z") + AR::info};
-  Filter filterDCAZ = aod::track::dcaZ > cfgDCAZ.value.at(AR::kLCUT) &&
-                      aod::track::dcaZ<cfgDCAZ.value.at(AR::kUCUT) &&
-                                       cfgDCAZ.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgDCAXY = {std::string(AR::TrackVariableNames[AR::kDCAXY]),
                                                {100., -3., 3., -2.4, 2.4, 1.},
                                                std::string("DCA in XY") + AR::info};
-  Filter filterDCAXY = aod::track::dcaXY > cfgDCAXY.value.at(AR::kLCUT) &&
-                       aod::track::dcaXY<cfgDCAXY.value.at(AR::kUCUT) &&
-                                         cfgDCAXY.value.at(AR::kOCUT)> 0.;
   Configurable<std::vector<float>> cfgTPCCLUSTERS = {std::string(AR::TrackVariableNames[AR::kTPCCLUSTERS]),
                                                      {160., 0., 160., 80., 161., 1.},
                                                      std::string("TPC clusters") + AR::info};
@@ -278,7 +262,9 @@ struct MultiParticleCorrelationsARTask {
     }
   }
 
-  // function for filling control histograms of event variables
+  // function for filling event control histograms
+  // exclude MultiplicityQ, i.e. number of tracks in the QVector, and
+  // MultiplicityW, i.e. the weighted number of tracks in the QVector, and fill them separably
   template <AR::RS rs, AR::BA ba, typename CollisionObject>
   void FillEventControlHist(CollisionObject const& collision, HistogramRegistry& registry)
   {
@@ -299,6 +285,9 @@ struct MultiParticleCorrelationsARTask {
     registry.fill(HIST(AR::RecoSim[rs]) + HIST("EventControl/") + HIST(AR::BeforeAfter[ba]) + HIST(AR::EventVariableNames[AR::kMULTPC]),
                   collision.multTPC());
   }
+
+  // function for filling event control histograms for Multiplicity{Q,W},
+  // since they have to be computed separately
   template <AR::RS rs, AR::BA ba>
   void FillEventControlHistMul(HistogramRegistry& registry, double mulQvector, double mulWeights)
   {
@@ -308,7 +297,7 @@ struct MultiParticleCorrelationsARTask {
                   mulWeights);
   }
 
-  // function for fill control histograms of track variables
+  // function for filling track control histograms
   template <AR::RS rs, AR::BA ba, typename TrackObject>
   void FillTrackControlHist(TrackObject const& track, HistogramRegistry& registry)
   {
@@ -334,20 +323,69 @@ struct MultiParticleCorrelationsARTask {
                   track.itsNCls());
   }
 
-  // templated function for processing data
-  template <AR::RS rs, AR::BA ba, typename CollisionObject, typename TrackObject>
-  void ProcessReconstructed(CollisionObject const& collision, TrackObject const& tracks)
+  // function for checking if collision survives event cuts
+  template <typename CollisionObject, typename TrackObject>
+  bool SurviveEventCuts(CollisionObject collision, TrackObject tracks, float* reducedMultiplicityQ, float* reducedMultiplicityW)
   {
-    // fill event control histograms before cutting on the collision
-    FillEventControlHist<rs, ba, CollisionObject>(collision, registry);
-    // fill multiplicity for qvector and weights separately
-    FillEventControlHistMul<rs, ba>(registry, collision.size(), collision.size());
+    bool flag = true;
 
-    // loop over all tracks in the event
-    for (auto const& track : tracks) {
-      // fill track control histograms before track cut
-      FillTrackControlHist<rs, ba, typename TrackObject::iterator>(track, registry);
+    // Check if event survives event cuts, where we can get the values for the variables immediately
+    if (!AR::SurviveCut(cfgVX.value, collision.posX()) ||
+        !AR::SurviveCut(cfgVY.value, collision.posY()) ||
+        !AR::SurviveCut(cfgVZ.value, collision.posZ()) ||
+        !AR::SurviveCut(cfgVABS.value, AR::abs(collision.posX(), collision.posY(), collision.posZ())) ||
+        !AR::SurviveCut(cfgCEN.value, collision.centRun2V0M()) ||
+        !AR::SurviveCut(cfgMULNC.value, collision.numContrib()) ||
+        !AR::SurviveCut(cfgMULTPC.value, collision.multTPC())) {
+      flag = false;
     }
+
+    int MultiplicityQ = 0.;
+    float MultiplicityW = 0.;
+
+    // only compute Multiplicity{Q,W} if the other checks pass, i.e. our flag is still set to true
+    if (flag) {
+      for (auto& track : tracks) {
+        if (SurviveTrackCuts(track) == true) {
+          MultiplicityQ += 1.;
+          MultiplicityW += 1.;
+        }
+      }
+    }
+
+    // update the values for Multiplicity{Q,W}, whose references are passed to this function
+    *reducedMultiplicityQ = MultiplicityQ;
+    *reducedMultiplicityW = MultiplicityW;
+
+    // at last, check if event also passes this cut
+    if (!AR::SurviveCut(cfgMULQ.value, MultiplicityQ) ||
+        !AR::SurviveCut(cfgMULW.value, MultiplicityW)) {
+      flag = false;
+    }
+
+    return flag;
+  }
+
+  // function for checking if track survices trach cuts
+  template <typename TrackObject>
+  bool SurviveTrackCuts(TrackObject track)
+  {
+    bool flag = true;
+
+    if (!AR::SurviveCut(cfgPT.value, track.pt()) ||
+        !AR::SurviveCut(cfgPHI.value, track.phi()) ||
+        !AR::SurviveCut(cfgETA.value, track.eta()) ||
+        !AR::SurviveCut(cfgCHARGE.value, track.sign()) ||
+        !AR::SurviveCut(cfgDCAZ.value, track.dcaZ()) ||
+        !AR::SurviveCut(cfgDCAXY.value, track.dcaXY()) ||
+        !AR::SurviveCut(cfgTPCCLUSTERS.value, track.tpcNClsFound()) ||
+        !AR::SurviveCut(cfgTPCCROSSEDROWS.value, track.tpcNClsCrossedRows()) ||
+        !AR::SurviveCut(cfgTPCCHI2.value, track.tpcChi2NCl()) ||
+        !AR::SurviveCut(cfgITSCLUSTERS.value, track.itsNCls())) {
+      flag = false;
+    }
+
+    return flag;
   }
 
   using CollisionsInstance = soa::Join<aod::Collisions, aod::CentRun2V0Ms, aod::Mults>;
@@ -356,27 +394,35 @@ struct MultiParticleCorrelationsARTask {
   using CollisionsInstanceIterator = CollisionsInstance::iterator;
   // using TracksInstanceIterator = TracksInstance::iterator;
 
-  // process reconstructed data before applying cuts
-  void ProcessReconstructedBeforeCut(CollisionsInstanceIterator const& collision, TracksInstance const& tracks)
+  void process(CollisionsInstanceIterator const& collision, TracksInstance const& tracks)
   {
-    LOGF(info, "Process reconstructed data before applying cuts");
-    ProcessReconstructed<AR::kRECO, AR::kBEFORE, CollisionsInstanceIterator, TracksInstance>(collision, tracks);
-  };
-  PROCESS_SWITCH(MultiParticleCorrelationsARTask, ProcessReconstructedBeforeCut, "Process Reco before Cuts", true);
+    LOGF(info, "Process reconstructed event: %d", collision.index());
 
-  using FilteredCollisionsInstance = soa::Filtered<CollisionsInstance>;
-  using FilteredTracksInstance = soa::Filtered<TracksInstance>;
+    FillEventControlHist<AR::kRECO, AR::kBEFORE, CollisionsInstanceIterator>(collision, registry);
+    FillEventControlHistMul<AR::kRECO, AR::kBEFORE>(registry, collision.size(), collision.size());
 
-  using FilteredCollisionsInstanceIterator = FilteredCollisionsInstance::iterator;
-  // using FilteredTracksInstanceIterator = FilteredTracksInstance::iterator;
+    float reducedMultiplicityQ = 0;
+    float reducedMultiplicityW = 0;
 
-  // process reconstructed data after applying cuts
-  void ProcessReconstructedAfterCut(FilteredCollisionsInstanceIterator const& collision, FilteredTracksInstance const& tracks)
-  {
-    LOGF(info, "Process reconstructed data after applying cuts");
-    ProcessReconstructed<AR::kRECO, AR::kAFTER, FilteredCollisionsInstanceIterator, FilteredTracksInstance>(collision, tracks);
-  };
-  PROCESS_SWITCH(MultiParticleCorrelationsARTask, ProcessReconstructedAfterCut, "Process Reco after Cuts", true);
+    if (!SurviveEventCuts(collision, tracks, &reducedMultiplicityQ, &reducedMultiplicityW)) {
+      LOGF(info, "Event was CUT");
+      return;
+    }
+
+    FillEventControlHist<AR::kRECO, AR::kAFTER, CollisionsInstanceIterator>(collision, registry);
+    FillEventControlHistMul<AR::kRECO, AR::kAFTER>(registry, reducedMultiplicityQ, reducedMultiplicityW);
+
+    // loop over all tracks in the event
+    for (auto const& track : tracks) {
+      // fill track control histograms before track cut
+      FillTrackControlHist<AR::kRECO, AR::kBEFORE, TracksInstance::iterator>(track, registry);
+      if (!SurviveTrackCuts(track)) {
+        continue;
+      }
+      // fill track control histograms after cut
+      FillTrackControlHist<AR::kRECO, AR::kAFTER, TracksInstance::iterator>(track, registry);
+    }
+  }
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
