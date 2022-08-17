40b59ac82cf26b3f545603db9765f0dc33405c67
ALICE Builder <alibuild@users.noreply.github.com>
2022-08-17 09:27:09 +0200

    MegaLinter fixes (#2)

--- PWGDQ/Macros/DQFitter.py
+++ PWGDQ/Macros/DQFitter.py
@@ -2,30 +2,42 @@
 \author Luca Micheletti <luca.micheletti@cern.ch>, CERN
 """
 from os.path import exists
+
 import ROOT
-from ROOT import TCanvas, TFile, TH1F, RooRealVar, RooDataSet, RooWorkspace, RooDataHist, RooArgSet
-from ROOT import gPad, gROOT
+from ROOT import (
+    TH1F,
+    RooArgSet,
+    RooDataHist,
+    RooDataSet,
+    RooRealVar,
+    RooWorkspace,
+    TCanvas,
+    TFile,
+    gPad,
+    gROOT,
+)
+
 
 class DQFitter:
     def __init__(self, fInName, fInputName):
         if not exists(fInName):
             print("The input file does not exist, exit...")
             exit()
-        self.fPdfDict          = {}
-        self.fFileOut          = TFile("FitResults.root", "RECREATE")
-        self.fFileIn           = TFile.Open(fInName)
-        self.fInput            = self.fFileIn.Get(fInputName)
-        self.fRooWorkspace     = RooWorkspace('w','workspace')
-        self.fParNames         = []
-        self.fFitRangeMin      = []
-        self.fFitRangeMax      = []
-        self.fTrialName        = ""
-        self.fRooMass          = RooRealVar("m", "#it{M} (GeV/#it{c}^{2})", 2, 5)
+        self.fPdfDict = {}
+        self.fFileOut = TFile("FitResults.root", "RECREATE")
+        self.fFileIn = TFile.Open(fInName)
+        self.fInput = self.fFileIn.Get(fInputName)
+        self.fRooWorkspace = RooWorkspace("w", "workspace")
+        self.fParNames = []
+        self.fFitRangeMin = []
+        self.fFitRangeMax = []
+        self.fTrialName = ""
+        self.fRooMass = RooRealVar("m", "#it{M} (GeV/#it{c}^{2})", 2, 5)
 
     def SetFitConfig(self, pdfDict):
-        '''
+        """
         Method to set the fit PDFs
-        '''
+        """
         self.fPdfDict = pdfDict
         self.fFitRangeMin = pdfDict["fitRangeMin"]
         self.fFitRangeMax = pdfDict["fitRangeMax"]
@@ -41,13 +53,17 @@ class DQFitter:
             parName = self.fPdfDict["parName"][i]
 
             if not self.fPdfDict["pdf"][i] == "SUM":
-                gROOT.ProcessLineSync(".x fit_library/{}Pdf.cxx+".format(self.fPdfDict["pdf"][i]))
+                gROOT.ProcessLineSync(
+                    ".x fit_library/{}Pdf.cxx+".format(self.fPdfDict["pdf"][i])
+                )
                 nameFunc = self.fPdfDict["pdf"][i]
                 nameFunc += "Pdf::{}Pdf(m[2,5]".format(self.fPdfDict["pdf"][i])
                 pdfList.append(self.fPdfDict["pdf"][i])
 
                 for j in range(0, len(parVal)):
-                    nameFunc += ",{}[{},{},{}]".format(parName[j], parVal[j], parLimMin[j], parLimMax[j])
+                    nameFunc += ",{}[{},{},{}]".format(
+                        parName[j], parVal[j], parLimMin[j], parLimMax[j]
+                    )
                     self.fParNames.append(parName[j])
                 nameFunc += ")"
                 print(nameFunc)
@@ -57,7 +73,9 @@ class DQFitter:
                 nameFunc += "::sum("
 
                 for j in range(0, len(pdfList)):
-                    nameFunc += "{}[{},{},{}]*{}Pdf".format(parName[j], parVal[j], parLimMin[j], parLimMax[j], pdfList[j])
+                    nameFunc += "{}[{},{},{}]*{}Pdf".format(
+                        parName[j], parVal[j], parLimMin[j], parLimMax[j], pdfList[j]
+                    )
                     self.fParNames.append(parName[j])
                     if not j == len(pdfList) - 1:
                         nameFunc += ","
@@ -66,9 +84,9 @@ class DQFitter:
                 self.fRooWorkspace.factory(nameFunc)
 
     def FitInvMassSpectrum(self, fitRangeMin, fitRangeMax):
-        '''
+        """
         Method to perform binned / unbinned fit to a ROOT histogram / tree
-        '''
+        """
         trialName = self.fTrialName + "_" + str(fitRangeMin) + "_" + str(fitRangeMax)
         self.fRooWorkspace.Print()
         pdf = self.fRooWorkspace.pdf("sum")
@@ -76,14 +94,30 @@ class DQFitter:
         fRooPlot = self.fRooMass.frame(ROOT.RooFit.Title(trialName))
         if "TTree" in self.fInput.ClassName():
             print("Perform unbinned fit")
-            rooDs = RooDataSet("data", "data", RooArgSet(self.fRooMass), ROOT.RooFit.Import(self.fInput))
+            rooDs = RooDataSet(
+                "data",
+                "data",
+                RooArgSet(self.fRooMass),
+                ROOT.RooFit.Import(self.fInput),
+            )
         else:
             print("Perform binned fit")
-            rooDs = RooDataHist("data", "data", RooArgSet(self.fRooMass), ROOT.RooFit.Import(self.fInput))
+            rooDs = RooDataHist(
+                "data",
+                "data",
+                RooArgSet(self.fRooMass),
+                ROOT.RooFit.Import(self.fInput),
+            )
         pdf.fitTo(rooDs)
 
         index = 1
-        histResults = TH1F("fit_results_{}".format(trialName), "fit_results_{}".format(trialName), len(self.fParNames), 0., len(self.fParNames))
+        histResults = TH1F(
+            "fit_results_{}".format(trialName),
+            "fit_results_{}".format(trialName),
+            len(self.fParNames),
+            0.0,
+            len(self.fParNames),
+        )
         for parName in self.fParNames:
             histResults.GetXaxis().SetBinLabel(index, parName)
             histResults.SetBinContent(index, self.fRooWorkspace.var(parName).getVal())
@@ -95,7 +129,9 @@ class DQFitter:
         pdf.paramOn(fRooPlot, ROOT.RooFit.Layout(0.55))
 
         # Fit plot
-        canvasFit = TCanvas("fit_plot_{}".format(trialName), "fit_plot_{}".format(trialName), 600, 600)
+        canvasFit = TCanvas(
+            "fit_plot_{}".format(trialName), "fit_plot_{}".format(trialName), 600, 600
+        )
         canvasFit.SetLeftMargin(0.15)
         gPad.SetLeftMargin(0.15)
         fRooPlot.GetYaxis().SetTitleOffset(1.4)
@@ -104,8 +140,13 @@ class DQFitter:
         # Ratio plot
         rooHistRatio = fRooPlot.residHist()
         rooPlotRatio = self.fRooMass.frame(ROOT.RooFit.Title("Residual Distribution"))
-        rooPlotRatio.addPlotable(rooHistRatio,"P")
-        canvasRatio = TCanvas("ratio_plot_{}".format(trialName), "ratio_plot_{}".format(trialName), 600, 600)
+        rooPlotRatio.addPlotable(rooHistRatio, "P")
+        canvasRatio = TCanvas(
+            "ratio_plot_{}".format(trialName),
+            "ratio_plot_{}".format(trialName),
+            600,
+            600,
+        )
         canvasRatio.SetLeftMargin(0.15)
         rooPlotRatio.GetYaxis().SetTitleOffset(1.4)
         rooPlotRatio.Draw()
@@ -113,8 +154,10 @@ class DQFitter:
         # Pull plot
         rooHistPull = fRooPlot.pullHist()
         rooPlotPull = self.fRooMass.frame(ROOT.RooFit.Title("Pull Distribution"))
-        rooPlotPull.addPlotable(rooHistPull,"P")
-        canvasPull = TCanvas("pull_plot_{}".format(trialName), "pull_plot_{}".format(trialName), 600, 600)
+        rooPlotPull.addPlotable(rooHistPull, "P")
+        canvasPull = TCanvas(
+            "pull_plot_{}".format(trialName), "pull_plot_{}".format(trialName), 600, 600
+        )
         canvasPull.SetLeftMargin(0.15)
         rooPlotPull.GetYaxis().SetTitleOffset(1.4)
         rooPlotPull.Draw()
@@ -126,11 +169,12 @@ class DQFitter:
         canvasPull.Write()
         histResults.Write()
 
-
     def MultiTrial(self):
-        '''
+        """
         Method to run multiple fits of the same invariant mass distribution
-        '''
+        """
         for iRange in range(0, len(self.fFitRangeMin)):
-            self.FitInvMassSpectrum(self.fFitRangeMin[iRange], self.fFitRangeMax[iRange])
+            self.FitInvMassSpectrum(
+                self.fFitRangeMin[iRange], self.fFitRangeMax[iRange]
+            )
         self.fFileOut.Close()
--- PWGDQ/Macros/tutorial.py
+++ PWGDQ/Macros/tutorial.py
@@ -1,36 +1,37 @@
 """
 \author Luca Micheletti <luca.micheletti@cern.ch>, CERN
 """
-import json
 import argparse
+import json
 from array import array
-from ROOT import TFile, TF1, TH1F, TTree
-from ROOT import gRandom
+
 from DQFitter import DQFitter
+from ROOT import TF1, TH1F, TFile, TTree, gRandom
+
 
 def GenerateTutorialSample():
-    '''
+    """
     This method create the sample for the tutorial
-    '''
+    """
     nEvents = 100000
     SigOverBkg = 0.1
     fOut = TFile("tutorial.root", "RECREATE")
 
-    funcMassBkg = TF1("funcMassBkg", "expo", 0., 5.)
+    funcMassBkg = TF1("funcMassBkg", "expo", 0.0, 5.0)
     funcMassBkg.SetParameter(0, 0.00)
     funcMassBkg.SetParameter(1, -0.5)
 
-    funcMassSig = TF1("funcMassSig", "gaus", 0., 5.)
+    funcMassSig = TF1("funcMassSig", "gaus", 0.0, 5.0)
     funcMassSig.SetParameter(0, 1.0)
     funcMassSig.SetParameter(1, 3.1)
     funcMassSig.SetParameter(2, 0.07)
 
-    histMass = TH1F("histMass", "histMass", 100, 0., 5.)
+    histMass = TH1F("histMass", "histMass", 100, 0.0, 5.0)
     histMass.FillRandom("funcMassBkg", int(nEvents - (nEvents * SigOverBkg)))
     histMass.FillRandom("funcMassSig", int(nEvents * SigOverBkg))
     histMass.Write()
 
-    m = array('f', [0.])
+    m = array("f", [0.0])
     tree = TTree("data", "data")
     tree.Branch("m", m, "m/F")
 
@@ -45,23 +46,32 @@ def GenerateTutorialSample():
 
     fOut.Close()
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Arguments to pass')
-    parser.add_argument('cfgFileName', metavar='text', default='config.yml', help='config file name')
-    parser.add_argument("--gen_tutorial", help="generate tutorial sample", action="store_true")
+    parser = argparse.ArgumentParser(description="Arguments to pass")
+    parser.add_argument(
+        "cfgFileName", metavar="text", default="config.yml", help="config file name"
+    )
+    parser.add_argument(
+        "--gen_tutorial", help="generate tutorial sample", action="store_true"
+    )
     parser.add_argument("--run_fit", help="run the multi trial", action="store_true")
     args = parser.parse_args()
 
-    print('Loading task configuration: ...', end='\r')
-    with open(args.cfgFileName, 'r') as jsonCfgFile:
+    print("Loading task configuration: ...", end="\r")
+    with open(args.cfgFileName, "r") as jsonCfgFile:
         inputCfg = json.load(jsonCfgFile)
-    print('Loading task configuration: Done!')
+    print("Loading task configuration: Done!")
 
     if args.gen_tutorial:
         GenerateTutorialSample()
 
     if args.run_fit:
-        dqFitter = DQFitter(inputCfg["input"]["input_file_name"], inputCfg["input"]["input_name"])
+        dqFitter = DQFitter(
+            inputCfg["input"]["input_file_name"], inputCfg["input"]["input_name"]
+        )
         dqFitter.SetFitConfig(inputCfg["input"]["pdf_dictionary"])
         dqFitter.MultiTrial()
+
+
 main()
