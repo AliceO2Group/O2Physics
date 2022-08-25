957d55c9c470c0c03d1a93754cf2303b62639919
Christian Sonnabend <sonnabendch@gmail.com>
2022-08-25 10:56:26 +0200

    Adding purely timestamp-based CCDB-fetching of NN's (#1065)
    * Adding purely timestamp-based CCDB-fetching of NN's
    
    * Fixing formatting
    
    * Removing nClNorm from NN class
    
    * Fixing formatting
    
    * Removing createInputFromTrack function since it is not needed
    
    * Fixing formatting
    
    * Bug-fix: template instance removed

--- Common/TableProducer/PID/pidTPCML.h
+++ Common/TableProducer/PID/pidTPCML.h
@@ -37,25 +37,30 @@ class Network
   // Constructor, destructor and copy-constructor
   Network() = default;
   Network(std::string, bool);
+  Network(std::string, unsigned long, unsigned long, bool); // initialization with timestamps
   ~Network() = default;
 
   // Operators
   Network& operator=(Network&); // copy a network into an existing class instance
 
   // Functions
-  template <typename C, typename T>
-  std::array<float, 6> createInputFromTrack(const C&, const T&, const uint8_t) const; // create a std::vector<float> with all the inputs for the network
-  std::vector<Ort::Value> createTensor(std::array<float, 6>) const;                   // create a std::vector<Ort::Value> (= ONNX tensor) for model input
-  float* evalNetwork(std::vector<Ort::Value>);                                        // evaluate the network on a std::vector<Ort::Value> (= ONNX tensor)
-  float* evalNetwork(std::vector<float>);                                             // evaluate the network on a std::vector<float>
+  std::vector<Ort::Value> createTensor(std::array<float, 6>) const; // create a std::vector<Ort::Value> (= ONNX tensor) for model input
+  float* evalNetwork(std::vector<Ort::Value>);                      // evaluate the network on a std::vector<Ort::Value> (= ONNX tensor)
+  float* evalNetwork(std::vector<float>);                           // evaluate the network on a std::vector<float>
 
   // Getters & Setters
-  int getInputDimensions() const { return mInputShapes[0][1]; };
-  int getOutputDimensions() const { return mOutputShapes[0][1]; };
-  void SetNClNormalization(const float nclnorm) { nClNorm = nclnorm; }
-  const float GetNClNormalization() const { return nClNorm; }
+  int getInputDimensions() const { return mInputShapes[0][1]; }
+  int getOutputDimensions() const { return mOutputShapes[0][1]; }
+  unsigned long getValidityFrom() const { return valid_from; }
+  unsigned long getValidityUntil() const { return valid_until; }
+  void setValidityFrom(unsigned long t) { valid_from = t; }
+  void setValidityUntil(unsigned long t) { valid_until = t; }
 
  private:
+  // Range of validity in timestamps
+  unsigned long valid_from = 0;
+  unsigned long valid_until = 0;
+
   // Environment variables for the ONNX runtime
   std::shared_ptr<Ort::Env> mEnv = nullptr;
   std::shared_ptr<Ort::Experimental::Session> mSession = nullptr;
@@ -67,13 +72,11 @@ class Network
   std::vector<std::string> mOutputNames;
   std::vector<std::vector<int64_t>> mOutputShapes;
 
-  float nClNorm = 152.f;
-
   // Internal function for printing the shape of tensors: See https://github.com/saganatt/PID_ML_in_O2 or O2Physics/Tools/PIDML/simpleApplyPidOnnxModel.cxx
   std::string printShape(const std::vector<int64_t>& v);
 
   // Class version
-  ClassDefNV(Network, 2);
+  ClassDefNV(Network, 3);
 
 }; // class Network
 
