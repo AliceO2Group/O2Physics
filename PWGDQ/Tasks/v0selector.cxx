1caa937eab2b2239ea0068fa144e22e524a72066
dsekihat <daiki.sekihata@cern.ch>
2022-09-13 23:05:44 +0200

    PWGDQ/Tasks: fix pid for v0selector (#1241)

--- PWGDQ/Tasks/v0selector.cxx
+++ PWGDQ/Tasks/v0selector.cxx
@@ -48,7 +48,7 @@ using namespace o2::framework;
 using namespace o2::framework::expressions;
 using std::array;
 
-using FullTracksExt = soa::Join<aod::FullTracks, aod::TracksCov, aod::TracksExtra, aod::TracksDCA,
+using FullTracksExt = soa::Join<aod::Tracks, aod::TracksCov, aod::TracksExtra, aod::TracksDCA,
                                 aod::pidTPCFullEl, aod::pidTPCFullPi,
                                 aod::pidTPCFullKa, aod::pidTPCFullPr,
                                 aod::pidTOFFullEl, aod::pidTOFFullPi,
