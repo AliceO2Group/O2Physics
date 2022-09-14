d51882ba8ad6bdda35d07e594ede039fcddbd05d
DongJo Kim <dong.jo.kim@cern.ch>
2022-09-14 12:06:51 +0200

    new class

--- PWGCF/Tasks/AliJO2Analysis.cxx
+++ PWGCF/Tasks/AliJO2Analysis.cxx
@@ -1,3 +1,15 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+/// \author Dong Jo Kim (djkim@jyu.fi)
+/// \since Sep 2022
 
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
@@ -5,7 +17,6 @@
 #include <CCDB/BasicCCDBManager.h>
 #include "Framework/RunningWorkflowInfo.h"
 #include "Framework/HistogramRegistry.h"
-
 
 #include "Common/DataModel/EventSelection.h"
 #include "Common/Core/TrackSelection.h"
@@ -22,65 +33,67 @@ using namespace o2::framework::expressions;
 
 #define O2_DEFINE_CONFIGURABLE(NAME, TYPE, DEFAULT, HELP) Configurable<TYPE> NAME{#NAME, DEFAULT, HELP};
 
-class JFlucAnalysis{
-public:
-	~JFlucAnalysis(){
-		delete pcf;
-	}
+class JFlucAnalysis
+{
+ public:
+  ~JFlucAnalysis()
+  {
+    delete pcf;
+  }
 
-	O2_DEFINE_CONFIGURABLE(etamin,double,0.4,"Minimal eta for tracks");
-	O2_DEFINE_CONFIGURABLE(etamax,double,0.8,"Maximal eta for tracks");
+  O2_DEFINE_CONFIGURABLE(etamin, double, 0.4, "Minimal eta for tracks");
+  O2_DEFINE_CONFIGURABLE(etamax, double, 0.8, "Maximal eta for tracks");
 
-	//OutputObj<TDirectory> output{TDirectory("jflucO2","jflucO2","",0)};
-	OutputObj<TDirectory> output{"jflucO2"};
+  // OutputObj<TDirectory> output{TDirectory("jflucO2","jflucO2","",0)};
+  OutputObj<TDirectory> output{"jflucO2"};
 
-	void init(InitContext const &ic){
-		//
-		fInputList.reserve(2500);
+  void init(InitContext const& ic)
+  {
+    //
+    fInputList.reserve(2500);
 
-		pcf = new AliJFFlucAnalysis("jflucAnalysis");
-		pcf->SetNumBins(sizeof(jflucCentBins)/sizeof(jflucCentBins[0]));
-		pcf->AddFlags(AliJFFlucAnalysis::FLUC_EBE_WEIGHTING);
+    pcf = new AliJFFlucAnalysis("jflucAnalysis");
+    pcf->SetNumBins(sizeof(jflucCentBins) / sizeof(jflucCentBins[0]));
+    pcf->AddFlags(AliJFFlucAnalysis::FLUC_EBE_WEIGHTING);
 
-		output->cd();
-		pcf->UserCreateOutputObjects();
-	}
+    output->cd();
+    pcf->UserCreateOutputObjects();
+  }
 
-	//void process(aod::Collision const& collision, aod::ParticleTrack const& tracks){
-	//void process(soa::Join<aod::Collisions, aod::CentV0Ms>::iterator const& collision, aod::ParticleTrack const& tracks){
-	void process(soa::Join<aod::Collisions, aod::CollisionData>::iterator const& collision, aod::ParticleTrack const& tracks){
-		if(tracks.size() == 0)
-			return; //rejected event
-		fInputList.clear();
+  // void process(aod::Collision const& collision, aod::ParticleTrack const& tracks){
+  // void process(soa::Join<aod::Collisions, aod::CentV0Ms>::iterator const& collision, aod::ParticleTrack const& tracks){
+  void process(soa::Join<aod::Collisions, aod::CollisionData>::iterator const& collision, aod::ParticleTrack const& tracks)
+  {
+    if (tracks.size() == 0)
+      return; // rejected event
+    fInputList.clear();
 
-		for(auto &track : tracks){
-			fInputList.emplace_back();
-			PtEtaPhiEVector &t = fInputList.back();
-			
-			//ptrack->SetLabel(ntrack);
-			//ptrack->SetParticleType(0);
-			t.SetPt(track.pt());
-			t.SetEta(track.eta());
-			t.SetPhi(track.phi());
-		}
+    for (auto& track : tracks) {
+      fInputList.emplace_back();
+      PtEtaPhiEVector& t = fInputList.back();
 
-		const double fVertex[3] = {collision.posX(),collision.posY(),collision.posZ()};
+      // ptrack->SetLabel(ntrack);
+      // ptrack->SetParticleType(0);
+      t.SetPt(track.pt());
+      t.SetEta(track.eta());
+      t.SetPhi(track.phi());
+    }
 
-		pcf->Init();
-		pcf->SetInputList(&fInputList);
-		pcf->SetEventCentralityAndBin(collision.cent(),collision.cbin());
-		pcf->SetEventVertex(fVertex);
-		pcf->SetEtaRange(etamin,etamax);
-		pcf->UserExec("");
+    const double fVertex[3] = {collision.posX(), collision.posY(), collision.posZ()};
 
-	}
-	std::vector<PtEtaPhiEVector> fInputList;
-	AliJFFlucAnalysis *pcf;
+    pcf->Init();
+    pcf->SetInputList(&fInputList);
+    pcf->SetEventCentralityAndBin(collision.cent(), collision.cbin());
+    pcf->SetEventVertex(fVertex);
+    pcf->SetEtaRange(etamin, etamax);
+    pcf->UserExec("");
+  }
+  std::vector<PtEtaPhiEVector> fInputList;
+  AliJFFlucAnalysis* pcf;
 };
 
-WorkflowSpec defineDataProcessing(ConfigContext const& cfgc){
-	return WorkflowSpec{
-		adaptAnalysisTask<JFlucAnalysis>(cfgc)
-	};
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<JFlucAnalysis>(cfgc)};
 }
-
