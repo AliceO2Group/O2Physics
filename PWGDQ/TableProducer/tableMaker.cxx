ff7e81633141e1802f4bec8abc8274d6fd3ac27b
Lucamicheletti93 <luca.mike93@gmail.com>
2022-10-13 17:05:01 +0200

    Fixing the issue with CovMatrix tracks

--- PWGDQ/TableProducer/tableMaker.cxx
+++ PWGDQ/TableProducer/tableMaker.cxx
@@ -282,7 +282,8 @@ struct TableMaker {
           isAmbiguous = 0;
           for (auto& ambiTrackMid : ambiTracksMid) {
             auto ambiTrack = ambiTrackMid.template track_as<MyBarrelTracks>();
-            if (ambiTrack.globalIndex() == track.globalIndex()) {
+            auto ambiTrackWithCov = ambiTrackMid.template track_as<MyBarrelTracksWithCov>();
+            if (ambiTrack.globalIndex() == track.globalIndex() || ambiTrackWithCov.globalIndex() == track.globalIndex()) {
               isAmbiguous = 1;
               break;
             }
@@ -399,7 +400,8 @@ struct TableMaker {
           isAmbiguous = 0;
           for (auto& ambiTrackFwd : ambiTracksFwd) {
             auto ambiMuon = ambiTrackFwd.template fwdtrack_as<MyMuons>();
-            if (ambiMuon.globalIndex() == muon.globalIndex()) {
+            auto ambiMuonWithCov = ambiTrackFwd.template fwdtrack_as<MyMuonsWithCov>();
+            if (ambiMuon.globalIndex() == muon.globalIndex() || ambiMuonWithCov.globalIndex() == muon.globalIndex()) {
               isAmbiguous = 1;
               break;
             }
