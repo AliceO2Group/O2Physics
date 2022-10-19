2dd4a068627295b7fa8acf359aa650af187e00a2
feisenhu <53603353+feisenhu@users.noreply.github.com>
2022-10-19 16:59:52 +0200

    PWGDQ: TableMaker, Add BeforeCut Histograms for each MC signal defined (if detailedQA true) + adding few histograms and cuts (#1371)
    * PWGDQ: TableMaker, add BeforeCut histograms for each MC signal
    
    * PWGDQ: add a few histograms to HitogramsLibrary.h and add new cut to CutsLibrary.h
    
    * PWGDQ: adjust tpc_electron acceptance cut

--- PWGDQ/TableProducer/tableMakerMC.cxx
+++ PWGDQ/TableProducer/tableMakerMC.cxx
@@ -255,6 +255,7 @@ struct TableMakerMC {
         }
         if (fDoDetailedQA) {
           if (enableBarrelHistos) {
+            histClasses += Form("TrackBarrel_BeforeCuts_%s;", objArray->At(isig)->GetName());
             for (auto& cut : fTrackCuts) {
               histClasses += Form("TrackBarrel_%s_%s;", cut.GetName(), objArray->At(isig)->GetName());
             }
@@ -264,6 +265,7 @@ struct TableMakerMC {
               }
             }          }
           if (enableMuonHistos) {
+            histClasses += Form("Muons_BeforeCuts_%s;", objArray->At(isig)->GetName());
             for (auto& cut : fMuonCuts) {
               histClasses += Form("Muons_%s_%s;", cut.GetName(), objArray->At(isig)->GetName());
             }
@@ -498,6 +500,7 @@ struct TableMakerMC {
               mcflags |= (uint16_t(1) << i);
               if (fDoDetailedQA) {
                 j = 0;
+                fHistMan->FillHistClass(Form("TrackBarrel_BeforeCuts_%s", sig.GetName()), VarManager::fgValues); // fill the reconstructed truth BeforeCuts
                 for (auto& cut : fTrackCuts) {
                   if (trackTempFilterMap & (uint8_t(1) << j)) {
 		    // if (trackFilteringTag & (uint8_t(1) << j+15)) { // selected by dalitz (cuts must correspond)
@@ -644,6 +647,7 @@ struct TableMakerMC {
             if (sig.CheckSignal(true, mcTracks, mctrack)) {
               mcflags |= (uint16_t(1) << i);
               if (fDoDetailedQA) {
+                fHistMan->FillHistClass(Form("Muons_BeforeCuts_%s", sig.GetName()), VarManager::fgValues); // fill the reconstructed truth BeforeCuts
                 for (auto& cut : fMuonCuts) {
                   if (trackTempFilterMap & (uint8_t(1) << j)) {
                     fHistMan->FillHistClass(Form("Muons_%s_%s", cut.GetName(), sig.GetName()), VarManager::fgValues); // fill the reconstructed truth
