cdb4a95946363661b836395b70985c8a54d6c035
Vít Kučera <vit.kucera@cern.ch>
2022-11-02 13:23:12 +0100

    Add a script to find a table producer

---
+++ Scripts/find-o2-table-producer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Find the workflow that produces a given table.
+
+# Check that we are inside the O2 or the O2Physics directory.
+[[ "$PWD/" != *"/O2"*"/"* ]] && { echo "You must be inside the O2 or the O2Physics directory."; exit 1; }
+[ ! "$1" ] && { echo "Provide a table name."; exit 1; }
+# Find files that produce the table.
+table="$1"
+echo "Table $table is produced in:"
+files=$(grep -r -i --include="*.cxx" "<aod::$table>" | grep -E 'Produces|Spawns' | cut -d: -f1 | sort -u)
+for f in $files; do
+  # Extract the workflow name from the CMakeLists.txt in the same directory.
+  wf=$(grep -B 1 "$(basename "$f")" "$(dirname "$f")/CMakeLists.txt" | head -n 1 | cut -d\( -f2)
+  echo "$wf in $f"
+done
