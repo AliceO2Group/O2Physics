983abf9330f1bd6bb9ac0fbf7642d902a5f1b575
Vít Kučera <vit.kucera@cern.ch>
2022-11-02 17:10:39 +0100

    produced by

--- Scripts/find-o2-table-producer.sh
+++ Scripts/find-o2-table-producer.sh
@@ -7,7 +7,7 @@
 [ ! "$1" ] && { echo "Provide a table name."; exit 1; }
 # Find files that produce the table.
 table="$1"
-echo "Table $table is produced in:"
+echo "Table $table is produced by:"
 files=$(grep -r -i --include="*.cxx" -E "<$table>|<aod::$table>|<o2::aod::$table>" | grep -E 'Produces<|Spawns<|Builds<' | cut -d: -f1 | sort -u)
 for f in $files; do
   # Extract the workflow name from the CMakeLists.txt in the same directory.
