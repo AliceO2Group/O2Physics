c06cd589e4296b600e31d061f52ed185307c5998
smonira <shirajum.monira@cern.ch>
2022-11-25 01:11:08 +0100

    fixed comments

--- Tools/PIDML/KaonPidTask.cxx
+++ Tools/PIDML/KaonPidTask.cxx
@@ -16,15 +16,16 @@
 #include "Framework/runDataProcessing.h"
 #include "Common/DataModel/EventSelection.h"
 #include "Common/DataModel/Multiplicity.h"
-#include "Common/DataModel/PIDResponse.h"
-#include "CommonConstants/PhysicsConstants.h"
-#include "TMath.h"
-#include <math.h>
 #include "TLorentzVector.h"
 #include "TDatabasePDG.h"
 #include "Framework/AnalysisDataModel.h"
 #include "Tools/PIDML/pidOnnxModel.h"
 #include "Common/DataModel/TrackSelectionTables.h"
+#include "Common/DataModel/PIDResponse.h"
+#include "CommonConstants/PhysicsConstants.h"
+#include "TMath.h"
+#include <math.h>
+#include <memory>
 
 using namespace o2;
 using namespace o2::framework;
@@ -41,36 +42,36 @@ using MyCollision = MyCollisions::iterator;
 using MyTrack = MyTracks::iterator;
 } // namespace o2::aod
 
-struct kaon_pid {
-  PidONNXModel* pidModel; // creates a pointer to a new instance 'pidmodel'.
+struct KaonPidTask {
+  std::shared_ptr<PidONNXModel>pidModel; // creates a shared pointer to a new instance 'pidmodel'.
   HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
-  Configurable<float> ConfZvtxCut{"ConfZvtxCut", 10, "Z vtx cut"};
-  Configurable<float> ConfEtaCut{"ConfEtaCut", 0.8, "Pseudorapidity cut"};
-  Configurable<float> ConfMaxPtCut{"ConfMaxPtCut", 3.0, "Max Pt cut"};
-  Configurable<float> ConfMinPtCut{"ConfMinPtCut", 0.5, "Min Pt cut"};
-  Configurable<float> ConfMinNSigmaTPCCut{"ConfMinNSigmaTPCCut", 3., "N-sigma TPC cut"};
-  Configurable<float> ConfChargeCut{"ConfChargeCut", 0., "N-sigma TPC cut"};
+  Configurable<float> cfgZvtxCut{"cfgZvtxCut", 10, "Z vtx cut"};
+  Configurable<float> cfgEtaCut{"cfgEtaCut", 0.8, "Pseudorapidity cut"};
+  Configurable<float> cfgMaxPtCut{"cfgMaxPtCut", 3.0, "Max Pt cut"};
+  Configurable<float> cfgMinPtCut{"cfgMinPtCut", 0.5, "Min Pt cut"};
+  Configurable<float> cfgMinNSigmaTPCCut{"cfgMinNSigmaTPCCut", 3., "N-sigma TPC cut"};
+  Configurable<float> cfgChargeCut{"cfgChargeCut", 0., "N-sigma TPC cut"};
   Configurable<std::string> cfgPathLocal{"local-path", "/home/shira/kaon-mlpid/", "base path to the local directory with ONNX models"};
   Configurable<std::string> cfgPathCCDB{"ccdb-path", "Users/m/mkabus/PIDML", "base path to the CCDB directory with ONNX models"};
   Configurable<std::string> cfgCCDBURL{"ccdb-url", "http://alice-ccdb.cern.ch", "URL of the CCDB repository"};
-  Configurable<int> CfgPid{"pid", 321, "PID to predict"};
-  Configurable<double> CfgCertainty{"certainty", 0.5, "Minimum certainty above which the model accepts a particular type of particle"};
+  Configurable<int> cfgPid{"pid", 321, "PID to predict"};
+  Configurable<double> cfgCertainty{"certainty", 0.5, "Minimum certainty above which the model accepts a particular type of particle"};
   Configurable<uint32_t> cfgDetector{"detector", kTPCTOFTRD, "What detectors to use: 0: TPC only, 1: TPC + TOF, 2: TPC + TOF + TRD"};
-  Configurable<uint32_t> cfgTimestamp{"timestamp", -8.0, "Fixed timestamp"};
+  Configurable<uint32_t> cfgTimestamp{"timestamp", -1, "Fixed timestamp"};
   Configurable<bool> cfgUseCCDB{"useCCDB", false, "Whether to autofetch ML model from CCDB. If false, local file will be used."};
 
   o2::ccdb::CcdbApi ccdbApi;
 
-  Filter collisionFilter = (nabs(aod::collision::posZ) < ConfZvtxCut);
-  Filter trackFilter = (nabs(aod::track::eta) < ConfEtaCut) && (aod::track::pt > ConfMinPtCut) && (aod::track::pt < ConfMaxPtCut);
+  Filter collisionFilter = (nabs(aod::collision::posZ) < cfgZvtxCut);
+  Filter trackFilter = (nabs(aod::track::eta) < cfgEtaCut) && (aod::track::pt > cfgMinPtCut) && (aod::track::pt < cfgMaxPtCut);
 
   // Applying filters
   using MyFilteredCollisions = soa::Filtered<o2::aod::MyCollisions>;
   using MyFilteredCollision = MyFilteredCollisions::iterator;
 
-  Partition<o2::aod::MyTracks> positive = (nabs(aod::track::eta) < ConfEtaCut) && (aod::track::pt > ConfMinPtCut) && (aod::track::pt < ConfMaxPtCut) && (aod::track::signed1Pt > ConfChargeCut);
-  Partition<o2::aod::MyTracks> negative = (nabs(aod::track::eta) < ConfEtaCut) && (aod::track::pt > ConfMinPtCut) && (aod::track::pt < ConfMaxPtCut) && (aod::track::signed1Pt < ConfChargeCut);
+  Partition<o2::aod::MyTracks> positive = (nabs(aod::track::eta) < cfgEtaCut) && (aod::track::pt > cfgMinPtCut) && (aod::track::pt < cfgMaxPtCut) && (aod::track::signed1Pt > cfgChargeCut);
+  Partition<o2::aod::MyTracks> negative = (nabs(aod::track::eta) < cfgEtaCut) && (aod::track::pt > cfgMinPtCut) && (aod::track::pt < cfgMaxPtCut) && (aod::track::signed1Pt < cfgChargeCut);
 
   void init(o2::framework::InitContext&)
   {
@@ -81,12 +82,12 @@ struct kaon_pid {
     if (cfgUseCCDB) {
       ccdbApi.init(cfgCCDBURL); // Initializes ccdbApi when cfgUseCCDB is set to 'true'
     }
-    pidModel = new PidONNXModel(cfgPathLocal.value, cfgPathCCDB.value, cfgUseCCDB.value, ccdbApi, cfgTimestamp.value, cfgPid.value, static_cast<PidMLDetector>(cfgDetector.value), cfgCertainty.value);
+    pidModel = std::make_shared<PidONNXModel>(cfgPathLocal.value, cfgPathCCDB.value, cfgUseCCDB.value, ccdbApi, cfgTimestamp.value, cfgPid.value, static_cast<PidMLDetector>(cfgDetector.value), cfgCertainty.value);
 
     histos.add("hChargePos", ";z;", kTH1F, {{3, -1.5, 1.5}});
     histos.add("hChargeNeg", ";z;", kTH1F, {{3, -1.5, 1.5}});
     histos.add("hInvariantMass", ";M_{k^{+}k^{-}} (GeV/#it{c}^{2});", kTH1F, {{100, 0., 2.}});
-    histos.add("hPvsdEdxKaon", ";P_{K^{+}K^{-}}; dE/dx in TPC (keV/cm)", kTH2F, {{100, 0., 4.}, {200, 20., 400.}});
+    histos.add("hdEdXvsMomentum", ";P_{K^{+}K^{-}}; dE/dx in TPC (keV/cm)", kTH2F, {{100, 0., 4.}, {200, 20., 400.}});
   }
 
   void process(MyFilteredCollision const& coll, o2::aod::MyTracks const& tracks)
@@ -97,22 +98,22 @@ struct kaon_pid {
 
     for (auto track : groupPositive) {
       histos.fill(HIST("hChargePos"), track.sign());
-      if (pidModel->applyModelBoolean(track)) {
-        histos.fill(HIST("hPvsdEdxKaon"), track.p(), track.tpcSignal());
+      if (pidModel.get()->applyModelBoolean(track)) {
+        histos.fill(HIST("hdEdXvsMomentum"), track.p(), track.tpcSignal());
       }
     }
 
     for (auto track : groupNegative) {
       histos.fill(HIST("hChargeNeg"), track.sign());
-      if (pidModel->applyModelBoolean(track)) {
-        histos.fill(HIST("hPvsdEdxKaon"), track.p(), track.tpcSignal());
+      if (pidModel.get()->applyModelBoolean(track)) {
+        histos.fill(HIST("hdEdXvsMomentum"), track.p(), track.tpcSignal());
       }
     }
 
     for (auto& [pos, neg] : combinations(soa::CombinationsFullIndexPolicy(groupPositive, groupNegative))) {
-      if (!(pidModel->applyModelBoolean(pos))) {
+      if (!(pidModel.get()->applyModelBoolean(pos))) {
         continue;
-      } else if (!(pidModel->applyModelBoolean(neg))) {
+      } else if (!(pidModel.get()->applyModelBoolean(neg))) {
         continue;
       }
       TLorentzVector part1Vec;
@@ -133,6 +134,6 @@ struct kaon_pid {
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
-  WorkflowSpec workflow{adaptAnalysisTask<kaon_pid>(cfgc)};
+  WorkflowSpec workflow{adaptAnalysisTask<KaonPidTask>(cfgc)};
   return workflow;
 }
