720ac3d18f03da843188092ae46756303a586f73
amaringarcia <a.marin@gsi.de>
2022-11-30 14:32:59 +0100

    change to int64_t

--- PWGEM/PhotonMeson/TableProducer/skimmerGammaConversions.cxx
+++ PWGEM/PhotonMeson/TableProducer/skimmerGammaConversions.cxx
@@ -70,7 +70,7 @@ struct skimmerGammaConversions {
   Configurable<std::string> path{"ccdb-path", "GLO/GRP/GRP", "path to the ccdb object"};
   Configurable<std::string> grpmagPath{"grpmagPath", "GLO/Config/GRPMagField", "CCDB path of the GRPMagField object"};
   Configurable<std::string> url{"ccdb-url", "http://alice-ccdb.cern.ch", "url of the ccdb repository"};
-  Configurable<uint64_t> nolaterthan{"ccdb-no-later-than", std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(), "latest acceptable timestamp of creation for the object"};
+  Configurable<int64_t> nolaterthan{"ccdb-no-later-than", std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(), "latest acceptable timestamp of creation for the object"};
 
   HistogramRegistry fRegistry{
     "fRegistry",
