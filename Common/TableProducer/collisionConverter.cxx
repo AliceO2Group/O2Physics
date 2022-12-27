24fd9ca0681dcc76d620ac2d3efb2018167b0529
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2022-12-27 15:21:33 +0100

    cross-check if all is fine, otherwise fatal (#1726)
    * cross-check if all is fine, otherwise fatal
    
    * Please consider the following formatting changes
    
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

--- Common/TableProducer/collisionConverter.cxx
+++ Common/TableProducer/collisionConverter.cxx
@@ -19,17 +19,45 @@ using namespace o2::framework;
 struct collisionConverter {
   Produces<aod::Collisions_001> Collisions_001;
 
+  Configurable<bool> doNotSwap{"doNotSwap", false, "simple pass-through"};
+
   void process(aod::Collisions_000 const& collisionTable)
   {
     for (auto& collision : collisionTable) {
-      // Simple swap of XZ and YY with respect to expectations
+      float lYY = collision.covXZ();
+      float lXZ = collision.covYY();
+      if (doNotSwap) {
+        lYY = collision.covYY();
+        lXZ = collision.covXZ();
+      };
+      if (lYY < 1e-8) {
+        // This happened by accident!
+        if (!doNotSwap) {
+          LOGF(info, "Collision converter task found negative YY element!");
+          LOGF(info, "This is an indication that you're looping over data");
+          LOGF(info, "produced with an O2 version of late December 2022.");
+          LOGF(info, "Unfortunately, O2 versions of late December 2022");
+          LOGF(info, "have a mistake in them for which a special mode");
+          LOGF(info, "of this task exists. ");
+          LOGF(info, "For this data, please operate the collision converter");
+          LOGF(info, "with the configurable 'doNotSwap' set to true.");
+          LOGF(info, "This program will now crash. Please adjust your settings!");
+          LOGF(fatal, "FATAL: please set doNotSwap to true!");
+        };
+        LOGF(info, "Collision converter task found negative YY element!");
+        LOGF(info, "You're running with 'doNotSwap' enabled, but the ");
+        LOGF(info, "data your're analysing requires it to be disabled. ");
+        LOGF(info, "This program will now crash. Please adjust your settings!");
+        LOGF(fatal, "FATAL: please set doNotSwap to false!");
+      }
+      // Repopulate new table
       Collisions_001(
         collision.bcId(),
         collision.posX(), collision.posY(), collision.posZ(),
         collision.covXX(),
         collision.covXY(),
-        collision.covXZ(), // deliberate: Collisions_001 expects YY
-        collision.covYY(), // deliberate: Collisions_001 expects XZ
+        lYY, // <- this is the fixed part
+        lXZ, // <- this is the fixed part
         collision.covYZ(),
         collision.covZZ(),
         collision.flags(), collision.chi2(), collision.numContrib(),
