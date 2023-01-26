64c72bc50ee9e341cdffa1badc5c6aa6d2895242
ALICE Builder <alibuild@users.noreply.github.com>
2023-01-26 07:22:09 +0100

    MegaLinter fixes (#8)
    Co-authored-by: Vít Kučera <vit.kucera@cern.ch>

--- Scripts/find_dependencies.py
+++ Scripts/find_dependencies.py
@@ -104,7 +104,9 @@ def get_outputs(specs_wf: dict, device=""):
     for dev in specs_wf:
         if device and dev["name"] != device:
             continue
-        list_outputs += [i["description"] for i in dev["outputs"] if i["origin"] == "AOD"]
+        list_outputs += [
+            i["description"] for i in dev["outputs"] if i["origin"] == "AOD"
+        ]
     return list(dict.fromkeys(list_outputs))  # Remove duplicities
 
 
