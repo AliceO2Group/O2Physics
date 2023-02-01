d7cee93559e716ecff6dd58d838d4e22cad283a0
feisenhu <eisenhut@ikf.uni-frankfurt.de>
2023-02-01 16:58:42 +0100

    MegaLinter furhter fix

--- PWGDQ/TableProducer/tableMaker.cxx
+++ PWGDQ/TableProducer/tableMaker.cxx
@@ -17,6 +17,7 @@
 // The skimming can optionally produce just the barrel, muon, or both barrel and muon tracks
 // The event filtering (filterPP), centrality, and V0Bits (from v0-selector) can be switched on/off by selecting one
 //  of the process functions
+#include <iostream>
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
 #include "Framework/ASoAHelpers.h"
@@ -37,7 +38,6 @@
 #include "PWGDQ/Core/HistogramsLibrary.h"
 #include "PWGDQ/Core/CutsLibrary.h"
 
-#include <iostream>
 using std::cout;
 using std::endl;
 
@@ -515,19 +515,19 @@ struct TableMaker {
         trackFilteringTag |= uint64_t(trackTempFilterMap); // BIT0-7:  user selection cuts
 
         // update the matching MCH/MFT index
-        if (int(muon.trackType()) == 0 || int(muon.trackType()) == 2) { // MCH-MFT(2) or GLB(0) track
+        if (static_cast<int>(muon.trackType()) == 0 || static_cast<int>(muon.trackType()) == 2) { // MCH-MFT(2) or GLB(0) track
           int matchIdx = muon.matchMCHTrackId() - muon.offsets();
           if (newEntryNb.count(matchIdx) > 0) {                                                  // if the key exists i.e the match will not get deleted
             newMatchIndex[muon.index()] = newEntryNb[matchIdx];                                  // update the match for this muon to the updated entry of the match
             newMatchIndex[muon.index()] += muonBasic.lastIndex() + 1 - newEntryNb[muon.index()]; // adding the offset of muons, muonBasic.lastIndex() start at -1
-            if (int(muon.trackType()) == 0) {                                                    // for now only do this to global tracks
+            if (static_cast<int>(muon.trackType()) == 0) {                                       // for now only do this to global tracks
               newMatchIndex[matchIdx] = newEntryNb[muon.index()];                                // add the  updated index of this muon as a match to mch track
               newMatchIndex[matchIdx] += muonBasic.lastIndex() + 1 - newEntryNb[muon.index()];   // adding the offset, muonBasic.lastIndex() start at -1
             }
           } else {
             newMatchIndex[muon.index()] = -1;
           }
-        } else if (int(muon.trackType() == 4)) { // an MCH track
+        } else if (static_cast<int>(muon.trackType() == 4)) { // an MCH track
           // in this case the matches should be filled from the other types but we need to check
           if (newMatchIndex.count(muon.index()) == 0) { // if an entry for this mch was not added it simply mean that non of the global tracks were matched to it
             newMatchIndex[muon.index()] = -1;
