7934c3f84639cb9453406c71eed7a84dedd28cd7
Paul <paul.buehler@oeaw.ac.at>
2023-02-10 22:37:56 +0100

    clang-format

--- PWGUD/Core/UDFSParser.h
+++ PWGUD/Core/UDFSParser.h
@@ -23,9 +23,9 @@ class UDFSParser
 {
  public:
   // constructor/destructor
-  UDFSParser(){}
+  UDFSParser() {}
   explicit UDFSParser(const char* filename);
-  ~UDFSParser(){}
+  ~UDFSParser() {}
 
   // read filleingScheme from file
   bool readFS(const char* filename);
