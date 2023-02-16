3b9d9152c4964c15850723ae213a4a56fcc83e8d
Mike Sas <mike.sas@cern.ch>
2023-02-16 22:08:52 +0100

    PWGEM axis fix for SigmadEdx

--- PWGEM/PhotonMeson/Utils/gammaConvDefinitions.h
+++ PWGEM/PhotonMeson/Utils/gammaConvDefinitions.h
@@ -28,7 +28,7 @@ AxisSpec const gAxis_r_extended{800, 0.f, 500.f};
 AxisSpec const gAxis_dr{200, -100.f, 100.f};
 AxisSpec const gAxis_r2d{400, 0.f, 250.f};
 AxisSpec const gAxis_z2d{400, -250.f, 250.f};
-AxisSpec const gAxis_TPCdEdxSig{800, -10.f, 10.f};
+AxisSpec const gAxis_TPCdEdxSig{401, -10.025f, 10.025f};
 AxisSpec const gAxis_radRes{800, -PI, PI};
 AxisSpec const gAxis_xyz{2400, -300.f, 300.f};
 AxisSpec gAxis_pT_log{800, 0.01f, 25.f};
