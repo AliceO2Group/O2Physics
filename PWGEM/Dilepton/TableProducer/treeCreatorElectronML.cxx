cb62212e5f5efd8148af97541acd4e9551deca21
dsekihat <daiki.sekihata@cern.ch>
2023-02-20 23:24:07 +0900

    PWGEM/Dilepton/TableProducer: update treeCreatorElectronML.cxx (#2025)

--- PWGEM/Dilepton/TableProducer/treeCreatorElectronML.cxx
+++ PWGEM/Dilepton/TableProducer/treeCreatorElectronML.cxx
@@ -11,12 +11,9 @@
 //
 // ========================
 //
-// This code will create data table for inputs to machine learning to classify V0s.
+// This code will create data table for inputs to machine learning for electrons.
 //    Please write to: daiki.sekihata@cern.ch
 
-#include <array>
-#include <map>
-#include "Math/Vector4D.h"
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
@@ -29,23 +26,15 @@
 #include "Common/DataModel/Multiplicity.h"
 #include "Common/DataModel/Centrality.h"
 #include "Common/DataModel/PIDResponse.h"
-#include "Common/Core/RecoDecay.h"
-#include "DetectorsVertexing/DCAFitterN.h"
-#include "DetectorsBase/Propagator.h"
-#include "DetectorsBase/GeometryManager.h"
-#include "DataFormatsParameters/GRPObject.h"
-#include "DataFormatsParameters/GRPMagField.h"
-#include "CCDB/BasicCCDBManager.h"
 
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 using namespace o2::soa;
-using std::array;
 
 using FullTracksExt = soa::Join<aod::Tracks, aod::TracksCov, aod::TracksExtra, aod::TracksDCA,
-                                aod::pidTPCFullEl, aod::pidTPCFullPi, aod::pidTPCFullKa, aod::pidTPCFullPr,
-                                aod::pidTOFFullEl, aod::pidTOFFullPi, aod::pidTOFFullKa, aod::pidTOFFullPr, aod::pidTOFbeta>;
+                                aod::pidTPCFullEl, aod::pidTPCFullMu, aod::pidTPCFullPi, aod::pidTPCFullKa, aod::pidTPCFullPr,
+                                aod::pidTOFFullEl, aod::pidTOFFullMu, aod::pidTOFFullPi, aod::pidTOFFullKa, aod::pidTOFFullPr, aod::pidTOFbeta>;
 
 using FullTrackExt = FullTracksExt::iterator;
 
@@ -57,38 +46,45 @@ namespace o2::aod
 
 namespace mycollision // reconstructed collision information
 {
-}
+DECLARE_SOA_COLUMN(MCPosX, mcposX, float); //!
+DECLARE_SOA_COLUMN(MCPosY, mcposY, float); //!
+DECLARE_SOA_COLUMN(MCPosZ, mcposZ, float); //!
+} // namespace mycollision
 DECLARE_SOA_TABLE(MyCollisions, "AOD", "MYCOLLISION", //! vertex information of collision
                   o2::soa::Index<>, bc::RunNumber, collision::PosX, collision::PosY, collision::PosZ, collision::NumContrib, evsel::Sel8,
+                  mccollision::GeneratorsID, mycollision::MCPosX, mycollision::MCPosY, mycollision::MCPosZ,
                   mult::MultTPC, mult::MultFV0A, mult::MultFV0C, mult::MultFT0A, mult::MultFT0C,
                   mult::MultFDDA, mult::MultFDDC, mult::MultZNA, mult::MultZNC, mult::MultTracklets, mult::MultNTracksPV);
 using MyCollision = MyCollisions::iterator;
 
-namespace mymccollision // generated collision information
-{
-DECLARE_SOA_INDEX_COLUMN(MyCollision, Mycollision); //!
-}
-DECLARE_SOA_TABLE(MyMCCollisions, "AOD", "MYMCCOLLISION", //! vertex information of collision
-                  o2::soa::Index<>, mymccollision::MyCollisionId,
-                  mccollision::GeneratorsID, mccollision::PosX, mccollision::PosY, mccollision::PosZ);
-using MyCollision = MyCollisions::iterator;
-
 namespace mytrack
 {
-DECLARE_SOA_INDEX_COLUMN(Collision, collision); //!
-DECLARE_SOA_COLUMN(Sign, sign, int);            //!
+DECLARE_SOA_INDEX_COLUMN(MyCollision, mycollision);              //!
+DECLARE_SOA_COLUMN(Sign, sign, int);                             //!
+DECLARE_SOA_COLUMN(MCPt, mcpt, float);                           //!
+DECLARE_SOA_COLUMN(MCEta, mceta, float);                         //!
+DECLARE_SOA_COLUMN(MCPhi, mcphi, float);                         //!
+DECLARE_SOA_COLUMN(MCVx, mcvx, float);                           //!
+DECLARE_SOA_COLUMN(MCVy, mcvy, float);                           //!
+DECLARE_SOA_COLUMN(MCVz, mcvz, float);                           //!
+DECLARE_SOA_COLUMN(IsPhysicalPrimary, isPhysicalPrimary, bool);  //!
+DECLARE_SOA_COLUMN(MotherPdgCode, motherpdgCode, int);           //!
+DECLARE_SOA_COLUMN(GrandMotherPdgCode, grandmotherpdgCode, int); //!
 } // namespace mytrack
 
 // reconstructed track information
 DECLARE_SOA_TABLE(MyTracks, "AOD", "MYTRACK", //!
-                  o2::soa::Index<>, mytrack::CollisionId, mytrack::Sign,
+                  o2::soa::Index<>, mytrack::MyCollisionId, mytrack::Sign,
                   track::Pt, track::Eta, track::Phi,
                   track::DcaXY, track::DcaZ,
                   track::TPCNClsFindable, track::TPCNClsFindableMinusFound, track::TPCNClsFindableMinusCrossedRows,
                   track::TPCChi2NCl, track::TPCInnerParam,
-                  track::TPCSignal, pidtpc::TPCNSigmaEl, pidtpc::TPCNSigmaPi, pidtpc::TPCNSigmaKa, pidtpc::TPCNSigmaPr,
-                  pidtofbeta::Beta, pidtof::TOFNSigmaEl, pidtof::TOFNSigmaPi, pidtof::TOFNSigmaKa, pidtof::TOFNSigmaPr,
+                  track::TPCSignal, pidtpc::TPCNSigmaEl, pidtpc::TPCNSigmaMu, pidtpc::TPCNSigmaPi, pidtpc::TPCNSigmaKa, pidtpc::TPCNSigmaPr,
+                  pidtofbeta::Beta, pidtof::TOFNSigmaEl, pidtof::TOFNSigmaMu, pidtof::TOFNSigmaPi, pidtof::TOFNSigmaKa, pidtof::TOFNSigmaPr,
                   track::ITSClusterMap, track::ITSChi2NCl, track::DetectorMap,
+                  mytrack::MCPt, mytrack::MCEta, mytrack::MCPhi,
+                  mytrack::MCVx, mytrack::MCVy, mytrack::MCVz,
+                  mcparticle::PdgCode, mytrack::IsPhysicalPrimary, mytrack::MotherPdgCode, mytrack::GrandMotherPdgCode,
                   // dynamic column
                   track::TPCNClsFound<track::TPCNClsFindable, track::TPCNClsFindableMinusFound>,
                   track::TPCNClsCrossedRows<track::TPCNClsFindable, track::TPCNClsFindableMinusCrossedRows>,
@@ -101,28 +97,11 @@ DECLARE_SOA_TABLE(MyTracks, "AOD", "MYTRACK", //!
 // iterators
 using MyTrack = MyTracks::iterator;
 
-namespace mymctrack
-{
-DECLARE_SOA_INDEX_COLUMN(MyTrack, mytrack);                     //!
-DECLARE_SOA_COLUMN(MotherPdgCode, motherpdgCode, int);          //!
-DECLARE_SOA_COLUMN(IsPhysicalPrimary, isPhysicalPrimary, bool); //!
-} // namespace mymctrack
-
-// generated track information
-DECLARE_SOA_TABLE(MyMCTracks, "AOD", "MYMCTRACK", //!
-                  o2::soa::Index<>, mymctrack::MyTrackId,
-                  mcparticle::Pt, mcparticle::Eta, mcparticle::Phi,
-                  mcparticle::Vx, mcparticle::Vy, mcparticle::Vz,
-                  mcparticle::PdgCode, mymctrack::IsPhysicalPrimary, mymctrack::MotherPdgCode);
-using MyMCTrack = MyMCTracks::iterator;
-
 } // namespace o2::aod
 
 struct TreeCreatorElectronML {
   Produces<o2::aod::MyCollisions> mycollision;
-  Produces<o2::aod::MyMCCollisions> mymccollision;
   Produces<o2::aod::MyTracks> mytrack;
-  Produces<o2::aod::MyMCTracks> mymctrack;
 
   // Basic checks
   HistogramRegistry registry{
@@ -154,12 +133,11 @@ struct TreeCreatorElectronML {
       registry.fill(HIST("hEventCounter"), 1.0); // all
 
       auto bc = collision.bc_as<aod::BCsWithTimestamps>();
+      auto mccollision = collision.mcCollision();
       mycollision(bc.runNumber(), collision.posX(), collision.posY(), collision.posZ(), collision.numContrib(), collision.sel8(),
+                  mccollision.generatorsID(), mccollision.posX(), mccollision.posY(), mccollision.posZ(),
                   collision.multTPC(), collision.multFV0A(), collision.multFV0C(), collision.multFT0A(), collision.multFT0C(),
                   collision.multFDDA(), collision.multFDDC(), collision.multZNA(), collision.multZNC(), collision.multTracklets(), collision.multNTracksPV());
-
-      auto mccollision = collision.mcCollision();
-      mymccollision(mycollision.lastIndex(), mccollision.generatorsID(), mccollision.posX(), mccollision.posY(), mccollision.posZ());
 
       auto tracks_coll = tracks.sliceBy(perCollision, collision.globalIndex());
       for (auto& track : tracks_coll) {
@@ -167,29 +145,29 @@ struct TreeCreatorElectronML {
         if (track.tpcNClsCrossedRows() < mincrossedrows) {
           continue;
         }
+        int mother_pdg = 0;
+        int grand_mother_pdg = 0;
+        auto mcparticle = track.mcParticle_as<aod::McParticles>();
+
+        if (mcparticle.has_mothers()) {
+          auto mother = mcparticle.mothers_as<aod::McParticles>().front(); // first mother
+          mother_pdg = mother.pdgCode();
+          if (mother.has_mothers()) {
+            auto grand_mother = mother.mothers_as<aod::McParticles>().front(); // first mother of mother
+            grand_mother_pdg = grand_mother.pdgCode();
+          }
+        }
 
         mytrack(mycollision.lastIndex(),
                 track.sign(), track.pt(), track.eta(), track.phi(), track.dcaXY(), track.dcaZ(),
                 track.tpcNClsFindable(), track.tpcNClsFindableMinusFound(), track.tpcNClsFindableMinusCrossedRows(),
                 track.tpcChi2NCl(), track.tpcInnerParam(),
-                track.tpcSignal(), track.tpcNSigmaEl(), track.tpcNSigmaPi(), track.tpcNSigmaKa(), track.tpcNSigmaPr(),
-                track.beta(), track.tofNSigmaEl(), track.tofNSigmaPi(), track.tofNSigmaKa(), track.tofNSigmaPr(),
-                track.itsClusterMap(), track.itsChi2NCl(), track.detectorMap());
-
-        auto mcparticle = track.mcParticle_as<aod::McParticles>();
-
-        if (mcparticle.has_mothers()) {
-          auto fm = mcparticle.mothers_as<aod::McParticles>().front(); // first mother
-          mymctrack(mytrack.lastIndex(),
-                    mcparticle.pt(), mcparticle.eta(), mcparticle.phi(),
-                    mcparticle.vx(), mcparticle.vy(), mcparticle.vz(),
-                    mcparticle.pdgCode(), mcparticle.isPhysicalPrimary(), fm.pdgCode());
-        } else {
-          mymctrack(mytrack.lastIndex(),
-                    mcparticle.pt(), mcparticle.eta(), mcparticle.phi(),
-                    mcparticle.vx(), mcparticle.vy(), mcparticle.vz(),
-                    mcparticle.pdgCode(), mcparticle.isPhysicalPrimary(), 0);
-        }
+                track.tpcSignal(), track.tpcNSigmaEl(), track.tpcNSigmaMu(), track.tpcNSigmaPi(), track.tpcNSigmaKa(), track.tpcNSigmaPr(),
+                track.beta(), track.tofNSigmaEl(), track.tofNSigmaMu(), track.tofNSigmaPi(), track.tofNSigmaKa(), track.tofNSigmaPr(),
+                track.itsClusterMap(), track.itsChi2NCl(), track.detectorMap(),
+                mcparticle.pt(), mcparticle.eta(), mcparticle.phi(),
+                mcparticle.vx(), mcparticle.vy(), mcparticle.vz(),
+                mcparticle.pdgCode(), mcparticle.isPhysicalPrimary(), mother_pdg, grand_mother_pdg);
 
       } // end of track loop
     }   // end of collision loop
