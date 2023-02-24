33bb1978b471aa7da47cada52ff1477a22d9184b
Fabio Catalano <fabio.catalano@cern.ch>
2023-02-24 11:46:33 +0100

    Add configurables for vertexer minimisation options

--- PWGHF/TableProducer/candidateCreatorChic.cxx
+++ PWGHF/TableProducer/candidateCreatorChic.cxx
@@ -48,6 +48,8 @@ struct HfCandidateCreatorChic {
   // vertexing
   Configurable<double> bz{"bz", 20., "magnetic field"};
   Configurable<bool> propagateToPCA{"propagateToPCA", true, "create tracks version propagated to PCA"};
+  Configurable<bool> useAbsDCA{"useAbsDCA", false, "Minimise abs. distance rather than chi2"};
+  Configurable<bool> useWeightedFinalPCA{"useWeightedFinalPCA", false, "Recalculate vertex position using track covariances, effective only if useAbsDCA is true"};
   Configurable<double> maxR{"maxR", 200., "reject PCA's above this radius"};
   Configurable<double> maxDZIni{"maxDZIni", 4., "reject (if>0) PCA candidate if tracks DZ exceeds threshold"};
   Configurable<double> minParamChange{"minParamChange", 1.e-3, "stop iterations if largest change of any X is smaller than this"};
@@ -88,6 +90,8 @@ struct HfCandidateCreatorChic {
     df2.setMaxDZIni(maxDZIni);
     df2.setMinParamChange(minParamChange);
     df2.setMinRelChi2Change(minRelChi2Change);
+    df2.setUseAbsDCA(useAbsDCA);
+    df2.setWeightedFinalPCA(useWeightedFinalPCA);
 
     // loop over Jpsi candidates
     for (auto& jpsiCand : jpsiCands) {
