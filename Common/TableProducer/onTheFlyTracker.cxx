2bd24e61b861a2ba47c6cd7df7f4e6c5c13b4584
David Dobrigkeit Chinellato <david.dobrigkeit.chinellato.cern.ch>
2023-03-08 04:41:00 -0300

    on-the-fly TOF PID, fully configurable

--- Common/TableProducer/onTheFlyTracker.cxx
+++ Common/TableProducer/onTheFlyTracker.cxx
@@ -30,6 +30,7 @@
 #include "Framework/runDataProcessing.h"
 #include "DataFormatsCalibration/MeanVertexObject.h"
 #include "CommonConstants/GeomConstants.h"
+#include "Framework/O2DatabasePDGPlugin.h"
 
 // This task allows for the calculation of aod::collisions and aod::Tracks in a synthetic mannter, 
 // smearing MC particles with very configurable settings
@@ -49,6 +50,9 @@ struct onTheFlyTracker {
 
   bool fillTracksDCA = false;
 
+  // necessary for particle charges 
+  Service<O2DatabasePDG> pdg;
+
   o2::base::Propagator::MatCorrType matCorr = o2::base::Propagator::MatCorrType::USEMatCorrNONE;
   
   void init(o2::framework::InitContext& initContext)
@@ -64,6 +68,35 @@ struct onTheFlyTracker {
     }
   }
 
+  template <typename mcParticleType>
+  void convertMCParticleToO2Track(mcParticleType& particle, o2::track::TrackParCov& o2track)
+  {
+    std::array<float, 3> xyz = {static_cast<float>(particle.vx()), static_cast<float>(particle.vy()), static_cast<float>(particle.vz())};
+    std::array<float, 3> ptetaphi = {static_cast<float>(particle.pt()), static_cast<float>(particle.eta()), static_cast<float>(particle.phi())};
+    auto pdgInfo = pdg->GetParticle(particle.pdgCode());
+    int charge = 0; 
+    if (pdgInfo != nullptr)
+        charge = pdgInfo->Charge();
+    std::array<float, 5> params;
+    std::array<float, 15> covm = {
+          0.,
+          0., 0.,
+          0., 0., 0.,
+          0., 0., 0., 0.,
+          0., 0., 0., 0., 0.};
+    float s, c, x;
+    o2::math_utils::sincos(ptetaphi[2], s, c);
+    o2::math_utils::rotateZInv(xyz[0], xyz[1], x, params[0], s, c);
+    params[1] = xyz[2];
+    params[2] = 0.; // since alpha = phi
+    auto theta = 2. * atan(exp(-ptetaphi[1]));  
+    params[3] = 1. / tan(theta);
+    params[4] = charge / ptetaphi[0];
+    
+    // Initialize TrackParCov in-place
+    new (&o2track) (o2::track::TrackParCov)(x, ptetaphi[2], params, covm);
+  }
+
   template <typename CollType, typename TTrackPar>
   void FillTracksPar(CollType& coll, aod::track::TrackTypeEnum trackType, TTrackPar& trackPar)
   {
@@ -77,26 +110,25 @@ struct onTheFlyTracker {
     o2::dataformats::VertexBase vtx;
 
     for (auto& mcParticle : mcParticles) {
-      // Ideally: o2::delphes::TrackSmearer
-      // For now: demonstration
-
       auto pdg = std::abs(mcParticle.pdgCode());
       if (pdg != 11 && pdg != 13 && pdg != 211 && pdg != 321 && pdg != 2212) continue;
 
-      std::array<float, 21> covV = {0.};
-      constexpr int MomInd[6] = {9, 13, 14, 18, 19, 20}; // cov matrix elements for momentum component
-      for (int i = 0; i < 6; i++) {
-        covV[MomInd[i]] = 1e-3;
-        covV[i] = 1e-3;
-      }
-      o2::track::TrackParCov trackParCov = o2::track::TrackParCov(
-        {mcParticle.vx(), mcParticle.vy(), mcParticle.vz()},
-        {mcParticle.px(), mcParticle.py(), mcParticle.pz()},
-        covV, 0, true);
+      o2::track::TrackParCov trackParCov; 
+      convertMCParticleToO2Track( mcParticle, trackParCov );
 
-      aod::track::TrackTypeEnum trackType = aod::track::Track;
+      // *+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*
+      // do the smearing in a one-liner with TrackSmearer
+      // FIXME this has to be made available! 
+      // if (!smearer.smearTrack(o2track, pdg, nch)) continue;
+      // *+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*
+
+      // *+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*
+      // Calculate primary vertex 
+      // To be added once smeared tracks are in place
+      // *+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*+~+*
 
       //Fixme: collision index could be changeable
+      aod::track::TrackTypeEnum trackType = aod::track::Track;
       FillTracksPar(mcCollision, trackType, trackParCov);
       if (fillTracksDCA) {
         tracksDCA(1e-3, 1e-3);
@@ -110,7 +142,7 @@ struct onTheFlyTracker {
                             trackParCov.getSigma1Pt2());
       tracksLabels( mcParticle.globalIndex(), 0 );
     }
-    collisions(-1,
+    collisions(-1, // BC is irrelevant in synthetic MC tests for now, could be adjusted in future
               mcCollision.posX(), mcCollision.posY(), mcCollision.posZ(), 
               1e-3, 0.0, 1e-3, 0.0, 0.0, 1e-3,
               0, 1e-3, mcParticles.size(),
