5e9e7b427bee0766ebcf589958f307d21fa1ea99
Victor Feuillard <victor.jose.gaston.feuillard@cern.ch>
2023-03-10 14:52:58 +0100

    PWGDQ : Adding muon cuts to the cuts library

--- PWGDQ/Core/CutsLibrary.cxx
+++ PWGDQ/Core/CutsLibrary.cxx
@@ -935,6 +935,18 @@ AnalysisCompositeCut* o2::aod::dqcuts::GetCompositeCut(const char* cutName)
     cut->AddCut(GetAnalysisCut("muonQualityCuts"));
     return cut;
   }
+    
+    if (!nameStr.compare("muonLowPt5")) {
+      cut->AddCut(GetAnalysisCut("muonLowPt5"));
+      cut->AddCut(GetAnalysisCut("muonQualityCuts"));
+      return cut;
+    }
+    
+    if (!nameStr.compare("muonLowPt6")) {
+      cut->AddCut(GetAnalysisCut("muonLowPt6"));
+      cut->AddCut(GetAnalysisCut("muonQualityCuts"));
+      return cut;
+    }
 
   if (!nameStr.compare("muonLowPtMatchingOnly")) {
     cut->AddCut(GetAnalysisCut("muonLowPt"));
@@ -959,6 +971,18 @@ AnalysisCompositeCut* o2::aod::dqcuts::GetCompositeCut(const char* cutName)
     cut->AddCut(GetAnalysisCut("muonQualityCutsMatchingOnly"));
     return cut;
   }
+    
+    if (!nameStr.compare("muonLowPtMatchingOnly5")) {
+      cut->AddCut(GetAnalysisCut("muonLowPt5"));
+      cut->AddCut(GetAnalysisCut("muonQualityCutsMatchingOnly"));
+      return cut;
+    }
+    
+    if (!nameStr.compare("muonLowPtMatchingOnly6")) {
+      cut->AddCut(GetAnalysisCut("muonLowPt6"));
+      cut->AddCut(GetAnalysisCut("muonQualityCutsMatchingOnly"));
+      return cut;
+    }
 
   if (!nameStr.compare("muonHighPt")) {
     cut->AddCut(GetAnalysisCut("muonHighPt"));
@@ -1056,6 +1080,16 @@ AnalysisCompositeCut* o2::aod::dqcuts::GetCompositeCut(const char* cutName)
     cut->AddCut(GetAnalysisCut("pairMassLow7"));
     return cut;
   }
+    
+    if (!nameStr.compare("pairMassLow8")) {
+      cut->AddCut(GetAnalysisCut("pairMassLow8"));
+      return cut;
+    }
+    
+    if (!nameStr.compare("pairMassLow9")) {
+      cut->AddCut(GetAnalysisCut("pairMassLow9"));
+      return cut;
+    }
 
   if (!nameStr.compare("pairDalitz1")) {
     cut->AddCut(GetAnalysisCut("pairDalitz1"));
@@ -1912,13 +1946,23 @@ AnalysisCut* o2::aod::dqcuts::GetAnalysisCut(const char* cutName)
     cut->AddCut(VarManager::kPt, 0.7, 1000.0);
     return cut;
   }
+    
+    if (!nameStr.compare("muonLowPt3")) {
+      cut->AddCut(VarManager::kPt, 0.8, 1000.0);
+      return cut;
+    }
+    
+    if (!nameStr.compare("muonLowPt4")) {
+      cut->AddCut(VarManager::kPt, 0.9, 1000.0);
+      return cut;
+    }
 
-  if (!nameStr.compare("muonLowPt3")) {
+  if (!nameStr.compare("muonLowPt5")) {
     cut->AddCut(VarManager::kPt, 1.0, 1000.0);
     return cut;
   }
 
-  if (!nameStr.compare("muonLowPt4")) {
+  if (!nameStr.compare("muonLowPt6")) {
     cut->AddCut(VarManager::kPt, 2.0, 1000.0);
     return cut;
   }
@@ -2035,23 +2079,28 @@ AnalysisCut* o2::aod::dqcuts::GetAnalysisCut(const char* cutName)
     cut->AddCut(VarManager::kMass, 1.8, 1000.0);
     return cut;
   }
+    
+    if (!nameStr.compare("pairMassLow5")) {
+      cut->AddCut(VarManager::kMass, 1.85, 1000.0);
+      return cut;
+    }
 
-  if (!nameStr.compare("pairMassLow4")) {
+  if (!nameStr.compare("pairMassLow6")) {
     cut->AddCut(VarManager::kMass, 1.9, 1000.0);
     return cut;
   }
 
-  if (!nameStr.compare("pairMassLow5")) {
+  if (!nameStr.compare("pairMassLow7")) {
     cut->AddCut(VarManager::kMass, 2.0, 1000.0);
     return cut;
   }
 
-  if (!nameStr.compare("pairMassLow6")) {
+  if (!nameStr.compare("pairMassLow8")) {
     cut->AddCut(VarManager::kMass, 2.2, 1000.0);
     return cut;
   }
 
-  if (!nameStr.compare("pairMassLow7")) {
+  if (!nameStr.compare("pairMassLow9")) {
     cut->AddCut(VarManager::kMass, 2.5, 1000.0);
     return cut;
   }
