b18c279d1d6ae1f86e6d0997f555ce73de3edbdb
Zuzanna Chochulska <zuzanna.chochulska.stud@pw.edu.pl>
2023-03-17 12:37:53 +0100

    Fixing MegaLinter (3)

--- PWGCF/FemtoWorld/Core/FemtoWorldContainer.h
+++ PWGCF/FemtoWorld/Core/FemtoWorldContainer.h
@@ -30,6 +30,8 @@
 #include "TLorentzVector.h"
 #include "CommonConstants/MathConstants.h"
 #include "TRandom.h"
+#include <vector>
+#include <string>
 
 using namespace o2::framework;
 using namespace o2::constants::math;
