2eeaec74a00d9e776d4dab980fa02beaa171eb7a
Lucamicheletti93 <luca.mike93@gmail.com>
2023-03-28 12:12:37 +0200

    clang format

--- PWGDQ/Macros/tutorial.py
+++ PWGDQ/Macros/tutorial.py
@@ -8,7 +8,7 @@ from os import path
 from array import array
 
 from DQFitter import DQFitter
-from ROOT import TF1, TH1F, TFile, TTree, gRandom, gROOT
+from ROOT import TF1, TH1F, TFile, TTree, gRandom
 
 
 def GenerateTutorialSample():
@@ -18,7 +18,7 @@ def GenerateTutorialSample():
     nEvents = 500000
     SigOverBkg1 = 0.03
     SigOverBkg2 = SigOverBkg1 / 10.
-    
+
     fOut = TFile("tutorial.root", "RECREATE")
 
     funcMassBkg = TF1("funcMassBkg", "expo", 2., 5.)
