dcec481df4219d4d22277f6b93a302f6d4aacd81
Chiara De Martin <chiara.de.martin@cern.ch>
2023-03-31 20:40:05 +0200

    adapt ctau column name + add multiplciity histos + add selection on NsigmaTOF for cascade daughters

--- PWGLF/Tasks/cascpostprocessing.cxx
+++ PWGLF/Tasks/cascpostprocessing.cxx
@@ -22,8 +22,8 @@
 #include "Common/DataModel/PIDResponse.h"
 
 // constants
-const float ctauxi = 4.91;     // from PDG
-const float ctauomega = 2.461; // from PDG
+const float ctauxiPDG = 4.91;     // from PDG
+const float ctauomegaPDG = 2.461; // from PDG
 
 using namespace o2;
 using namespace o2::framework;
@@ -38,10 +38,14 @@ namespace mycascades
 {
 
 DECLARE_SOA_INDEX_COLUMN(Collision, collision);
+DECLARE_SOA_COLUMN(CollisionZ, zcoll, float);
+DECLARE_SOA_COLUMN(MultFT0M, multFT0M, float);
+DECLARE_SOA_COLUMN(MultFV0A, multFV0A, float);
 DECLARE_SOA_COLUMN(Sign, sign, float);
 DECLARE_SOA_COLUMN(Pt, pt, float);
 DECLARE_SOA_COLUMN(RapXi, rapxi, float);
 DECLARE_SOA_COLUMN(RapOmega, rapomega, float);
+DECLARE_SOA_COLUMN(Eta, eta, float);
 DECLARE_SOA_COLUMN(MassXi, massxi, float);
 DECLARE_SOA_COLUMN(MassOmega, massomega, float);
 DECLARE_SOA_COLUMN(MassLambdaDau, masslambdadau, float);
@@ -61,10 +65,8 @@ DECLARE_SOA_COLUMN(BachEta, bacheta, float);
 DECLARE_SOA_COLUMN(PosITSHits, positshits, float);
 DECLARE_SOA_COLUMN(NegITSHits, negitshits, float);
 DECLARE_SOA_COLUMN(BachITSHits, bachitshits, float);
-DECLARE_SOA_COLUMN(CtauXiMinus, ctauximinus, float);
-DECLARE_SOA_COLUMN(CtauXiPlus, ctauxiplus, float);
-DECLARE_SOA_COLUMN(CtauOmegaMinus, ctauomegaminus, float);
-DECLARE_SOA_COLUMN(CtauOmegaPlus, ctauomegaplus, float);
+DECLARE_SOA_COLUMN(CtauXi, ctauxi, float);
+DECLARE_SOA_COLUMN(CtauOmega, ctauomega, float);
 DECLARE_SOA_COLUMN(NTPCSigmaNegPr, ntpcsigmanegpr, float);
 DECLARE_SOA_COLUMN(NTPCSigmaPosPr, ntpcsigmapospr, float);
 DECLARE_SOA_COLUMN(NTPCSigmaNegPi, ntpcsigmanegpi, float);
@@ -83,27 +85,33 @@ DECLARE_SOA_COLUMN(BachNTPCClusters, bachntpcscls, float);
 DECLARE_SOA_COLUMN(PosHasTOF, poshastof, float);
 DECLARE_SOA_COLUMN(NegHasTOF, neghastof, float);
 DECLARE_SOA_COLUMN(BachHasTOF, bachhastof, float);
+DECLARE_SOA_COLUMN(PosPt, pospt, float);
+DECLARE_SOA_COLUMN(NegPt, negpt, float);
+DECLARE_SOA_COLUMN(BachPt, bachpt, float);
 
 } // namespace mycascades
 
 DECLARE_SOA_TABLE(MyCascades, "AOD", "MYCASCADES", o2::soa::Index<>,
-                  mycascades::CollisionId, mycascades::Sign, mycascades::Pt, mycascades::RapXi, mycascades::RapOmega,
+                  mycascades::CollisionId, mycascades::CollisionZ, mycascades::MultFT0M, mycascades::MultFV0A, mycascades::Sign, mycascades::Pt, mycascades::RapXi, mycascades::RapOmega, mycascades::Eta,
                   mycascades::MassXi, mycascades::MassOmega, mycascades::MassLambdaDau, mycascades::CascRadius, mycascades::V0Radius,
                   mycascades::CascCosPA, mycascades::V0CosPA, mycascades::DCAPosToPV, mycascades::DCANegToPV,
                   mycascades::DCABachToPV, mycascades::DCACascDaughters, mycascades::DCAV0Daughters, mycascades::DCAV0ToPV, mycascades::PosEta, mycascades::NegEta,
                   mycascades::BachEta, mycascades::PosITSHits, mycascades::NegITSHits, mycascades::BachITSHits,
-                  mycascades::CtauXiMinus, mycascades::CtauXiPlus, mycascades::CtauOmegaMinus, mycascades::CtauOmegaPlus,
+                  mycascades::CtauXi, mycascades::CtauOmega,
                   mycascades::NTPCSigmaNegPr, mycascades::NTPCSigmaPosPr, mycascades::NTPCSigmaNegPi, mycascades::NTPCSigmaPosPi,
                   mycascades::NTPCSigmaBachPi, mycascades::NTPCSigmaBachKa, mycascades::NTOFSigmaNegPr, mycascades::NTOFSigmaPosPr, mycascades::NTOFSigmaNegPi,
                   mycascades::NTOFSigmaPosPi, mycascades::NTOFSigmaBachPi, mycascades::NTOFSigmaBachKa,
                   mycascades::PosNTPCClusters, mycascades::NegNTPCClusters, mycascades::BachNTPCClusters,
-                  mycascades::PosHasTOF, mycascades::NegHasTOF, mycascades::BachHasTOF);
+                  mycascades::PosHasTOF, mycascades::NegHasTOF, mycascades::BachHasTOF,
+		  mycascades::PosPt, mycascades::NegPt, mycascades::BachPt);
 
 } // namespace o2::aod
 
 struct cascpostprocessing {
   // Xi or Omega
   Configurable<bool> isXi{"isXi", 1, "Apply cuts for Xi identification"};
+  Configurable<bool> hastof{"hastof", 0, "Apply nsigmaTOF to daughter tracks of cascade"};
+  Configurable<float> ptthrtof{"ptthrtof", 2, "NsigmaTOF selection is only applied if the track has pt larger than ptthr"};
 
   // Event selection criteria
   Configurable<float> cutzvertex{"cutzvertex", 15.0f, "Accepted z-vertex range (cm)"};
@@ -134,7 +142,6 @@ struct cascpostprocessing {
   Configurable<float> nsigmatofPi{"nsigmatofPi", 6, "N sigma TOF Pion"};
   Configurable<float> nsigmatofPr{"nsigmatofPr", 6, "N sigma TOF Proton"};
   Configurable<float> nsigmatofKa{"nsigmatofKa", 6, "N sigma TOF Kaon"};
-  Configurable<float> hasTOF{"hasTOF", 0, "Has TOF"};
 
   HistogramRegistry registry{"registryts"};
 
@@ -149,12 +156,18 @@ struct cascpostprocessing {
     AxisSpec ptAxis = {200, 0.0f, 10.0f, "#it{p}_{T} (GeV/#it{c})"};
     AxisSpec pTPCAxis = {100, 0.0f, 10.0f, "#it{p} TPC (GeV/#it{c})"};
     AxisSpec etaAxis = {200, -2.0f, 2.0f, "#eta"};
+    AxisSpec centFT0MAxis = {100, 0.0f, 100.0f, "FT0M (%)"};
+    AxisSpec centFV0AAxis = {100, 0.0f, 100.0f, "FV0A (%)"};
     AxisSpec rapidityAxis = {200, -2.0f, 2.0f, "y"};
     AxisSpec phiAxis = {100, -TMath::Pi() / 2, 3. * TMath::Pi() / 2, "#varphi"};
 
     registry.add("hPt", "hPt", {HistType::kTH1F, {ptAxis}});
     registry.add("hCascMinusInvMassvsPt", "hCascMinusInvMassvsPt", HistType::kTH2F, {ptAxis, massAxis});
     registry.add("hCascPlusInvMassvsPt", "hCascPlusInvMassvsPt", HistType::kTH2F, {ptAxis, massAxis});
+    registry.add("hCascMinusInvMassvsPt_FT0M", "hCascMinusInvMassvsPt_FT0M", HistType::kTH3F, {centFT0MAxis, ptAxis, massAxis});
+    registry.add("hCascPlusInvMassvsPt_FT0M", "hCascPlusInvMassvsPt_FT0M", HistType::kTH3F, {centFT0MAxis, ptAxis, massAxis});
+    registry.add("hCascMinusInvMassvsPt_FV0A", "hCascMinusInvMassvsPt_FV0A", HistType::kTH3F, {centFV0AAxis, ptAxis, massAxis});
+    registry.add("hCascPlusInvMassvsPt_FV0A", "hCascPlusInvMassvsPt_FV0A", HistType::kTH3F, {centFV0AAxis, ptAxis, massAxis});
     registry.add("hXiMinusInvMassvsPt_BefSels", "hXiMinusInvMassvsPt_BefSels", HistType::kTH2F, {ptAxis, ximassAxis});
     registry.add("hOmegaMinusInvMassvsPt_BefSels", "hOmegaMinusInvMassvsPt_BefSels", {HistType::kTH2F, {ptAxis, omegamassAxis}});
     registry.add("hXiPlusInvMassvsPt_BefSels", "hXiPlusInvMassvsPt_BefSels", {HistType::kTH2F, {ptAxis, ximassAxis}});
@@ -255,45 +268,47 @@ struct cascpostprocessing {
         if (TMath::Abs(candidate.ntpcsigmapospi()) > nsigmatpcPi)
           continue;
       }
-      // TOF PID only for daughters with pT > threshold (to be implemented)
-      /*
-  if (candidate.sign() < 0){
-  if (TMath::Abs(candidate.ntofsigmapospr()) > nsigmatofPr) continue;
-  if (TMath::Abs(candidate.ntofsigmanegpi()) > nsigmatofPi) continue;
-  }
-  else if (candidate.sign() > 0){
-  if (TMath::Abs(candidate.ntofsigmanegpr()) > nsigmatofPr) continue;
-  if (TMath::Abs(candidate.ntofsigmapospi()) > nsigmatofPi) continue;
-  }
-      */
+      // TOF required only if hastof is set to one. In this case, daughter tracks with pt > threshold should have a hit in the tof
+      if (hastof){
+	if (candidate.sign() < 0){
+	  if (TMath::Abs(candidate.ntofsigmapospr()) > nsigmatofPr && candidate.pospt() > ptthrtof && candidate.poshastof()) continue;
+	  if (TMath::Abs(candidate.ntofsigmanegpi()) > nsigmatofPi && candidate.negpt() > ptthrtof && candidate.neghastof()) continue;
+	}
+	else if (candidate.sign() > 0){
+	  if (TMath::Abs(candidate.ntofsigmanegpr()) > nsigmatofPr && candidate.negpt() > ptthrtof && candidate.neghastof()) continue;
+	  if (TMath::Abs(candidate.ntofsigmapospi()) > nsigmatofPi && candidate.pospt() > ptthrtof && candidate.poshastof()) continue;
+	}
+      }
 
       if (isXi) {
         if (TMath::Abs(candidate.ntpcsigmabachpi()) > nsigmatpcPi)
           continue;
+	if (hastof && TMath::Abs(candidate.ntofsigmabachpi()) > nsigmatofPi && candidate.bachpt() > ptthrtof && candidate.bachhastof()) continue;
         if (TMath::Abs(candidate.rapxi()) > rap)
           continue;
-        if (candidate.ctauximinus() > proplifetime * ctauxi)
+        if (candidate.ctauxi() > proplifetime * ctauxiPDG)
           continue;
         if (TMath::Abs(candidate.massxi() - RecoDecay::getMassPDG(3312)) > masswin)
           continue;
         if (TMath::Abs(candidate.massomega() - RecoDecay::getMassPDG(3334)) < rejcomp)
           continue;
         rapidity = candidate.rapxi();
-        ctau = candidate.ctauximinus();
+        ctau = candidate.ctauxi();
         invmass = candidate.massxi();
       } else {
         if (TMath::Abs(candidate.ntpcsigmabachka()) > nsigmatpcKa)
           continue;
+	if (hastof && TMath::Abs(candidate.ntofsigmabachka()) > nsigmatofKa && candidate.bachpt() > ptthrtof && candidate.bachhastof()) continue;
         if (TMath::Abs(candidate.rapxi()) > rap)
           continue;
-        if (candidate.ctauomegaminus() > proplifetime * ctauomega)
+        if (candidate.ctauomega() > proplifetime * ctauomegaPDG)
           continue;
         if (TMath::Abs(candidate.massomega() - RecoDecay::getMassPDG(3334)) > masswin)
           continue;
         if (TMath::Abs(candidate.massxi() - RecoDecay::getMassPDG(3312)) < rejcomp)
           continue;
         rapidity = candidate.rapomega();
-        ctau = candidate.ctauomegaminus();
+        ctau = candidate.ctauomega();
         invmass = candidate.massomega();
       }
 
@@ -312,13 +327,13 @@ struct cascpostprocessing {
 
       if (candidate.sign() > 0) {
         registry.fill(HIST("hCtauPlus"), ctau);
-        // registry.fill(HIST("hEtaPlus"), candidate.pt(), candidate.eta());
+	registry.fill(HIST("hEtaPlus"), candidate.pt(), candidate.eta());
         registry.fill(HIST("hRapPlus"), candidate.pt(), rapidity);
         registry.fill(HIST("hRapPlus1D"), rapidity);
         // registry.fill(HIST("hPhiPlus"), candidate.pt(), candidate.phi());
       } else {
         registry.fill(HIST("hCtauMinus"), ctau);
-        // registry.fill(HIST("hEtaMinus"), candidate.pt(), candidate.eta());
+        registry.fill(HIST("hEtaMinus"), candidate.pt(), candidate.eta());
         registry.fill(HIST("hRapMinus"), candidate.pt(), rapidity);
         registry.fill(HIST("hRapMinus1D"), rapidity);
         // registry.fill(HIST("hPhiMinus"), candidate.pt(), candidate.phi());
@@ -360,9 +375,13 @@ struct cascpostprocessing {
 
       if (candidate.sign() < 0) {
         registry.fill(HIST("hCascMinusInvMassvsPt"), candidate.pt(), invmass);
+        registry.fill(HIST("hCascMinusInvMassvsPt_FT0M"), candidate.multFT0M(), candidate.pt(), invmass);
+        registry.fill(HIST("hCascMinusInvMassvsPt_FV0A"), candidate.multFV0A(), candidate.pt(), invmass);
       }
       if (candidate.sign() > 0) {
         registry.fill(HIST("hCascPlusInvMassvsPt"), candidate.pt(), invmass);
+        registry.fill(HIST("hCascPlusInvMassvsPt_FT0M"), candidate.multFT0M(), candidate.pt(), invmass);
+        registry.fill(HIST("hCascPlusInvMassvsPt_FV0A"), candidate.multFV0A(), candidate.pt(), invmass);
       }
     }
   }
