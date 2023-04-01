796d0d55f05173c9865854f2feec90425c13a357
Christian Sonnabend <sonnabendch@gmail.com>
2023-04-01 16:05:43 +0200

    Adapting applyOnnxModel for sample-input

--- Tutorials/ML/applyOnnxModel.cxx
+++ Tutorials/ML/applyOnnxModel.cxx
@@ -17,10 +17,9 @@
 /// \brief  Showcase application of an ONNX model in O2Physics
 ///
 
+#include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
-#include "Framework/AnalysisDataModel.h"
 #include "Framework/Logger.h"
-#include "CCDB/CcdbApi.h"
 #include "Tools/ML/model.h"
 
 using namespace o2;
@@ -30,20 +29,22 @@ using namespace o2::ml;
 
 struct applyModel {
 
-  Configurable<std::string> modelPathLocally{"modelPath", "test_net.onnx", "(std::string) Path to the local .onnx file"};
+  Configurable<std::string> modelPath{"modelPath", "test_net.onnx", "(std::string) Path to the local .onnx file"};
   Configurable<bool> enableOptimizations{"enableOptimizations", true, "Enables the ONNX extended model-optimization: sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_EXTENDED)"};
 
   OnnxModel network;
 
+  // This is our input data from the tutorial
+  std::vector<float> modelInput;
+
   void init(InitContext const&)
   {
-    network.initModel(modelPathLocally.value, enableOptimizations.value);
+    network.initModel(modelPath.value, enableOptimizations.value);
+    modelInput = std::vector<float>{.5, 3.1415926536, 0., 0.001, -3.};
   }
 
-  void process()
+  void run(ProcessingContext& pc)
   {
-    // This is our input data from the tutorial
-    std::vector<float> modelInput = std::vector<float>{0.5, 3.1415926536, 0., 0.001, -3.};
 
     // Here we evaluate the model
     float* modelOutput = network.evalModel(modelInput);
@@ -52,6 +53,8 @@ struct applyModel {
     for (int i = 0; i < 5; i++) {
       LOG(info) << "Input: " << modelInput[i] << ", Output: " << modelOutput[i];
     }
+    pc.services().get<ControlService>().endOfStream();
+    pc.services().get<ControlService>().readyToQuit(QuitRequest::Me);
   }
 };
 
