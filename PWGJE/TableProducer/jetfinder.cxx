5eb32e8cca219684047a1fdaf4ac50d366d65dd3
feisenhu <eisenhut@ikf.uni-frankfurt.de>
2023-04-03 15:35:54 +0200

    PWGJE: Fix PR2048 adding template in missing lines

--- PWGJE/TableProducer/jetfinder.cxx
+++ PWGJE/TableProducer/jetfinder.cxx
@@ -204,8 +204,8 @@ struct JetFinderTask {
           if (constituent.template user_info<FastJetUtilities::fastjet_user_info>().getStatus() == static_cast<int>(JetConstituentStatus::track)) {
             trackconst.push_back(constituent.template user_info<FastJetUtilities::fastjet_user_info>().getIndex());
           }
-          if (constituent.user_info<FastJetUtilities::fastjet_user_info>().getStatus() == static_cast<int>(JetConstituentStatus::cluster)) {
-            clusterconst.push_back(constituent.user_info<FastJetUtilities::fastjet_user_info>().getIndex());
+          if (constituent.template user_info<FastJetUtilities::fastjet_user_info>().getStatus() == static_cast<int>(JetConstituentStatus::cluster)) {
+            clusterconst.push_back(constituent.template user_info<FastJetUtilities::fastjet_user_info>().getIndex());
           }
         }
         constituentsTable(jetsTable.lastIndex(), trackconst, clusterconst, std::vector<int>());
