b3ab6d872d65ede6bd36376fecc168c85a00d30a
Chiara De Martin <chiara.de.martin@cern.ch>
2023-04-11 11:55:23 +0200

    clang fix

--- Tutorials/PWGLF/strangeness_step0.cxx
+++ Tutorials/PWGLF/strangeness_step0.cxx
@@ -22,29 +22,26 @@ using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
-//STEP 0
-//Starting point
-struct strangeness_step0 {
+// STEP 0
+// Starting point: loop over all V0s and fill invariant mass histogram
+struct strangeness_tutorial {
 
-  //Configurable for number of bins
+  // Configurable for number of bins
   Configurable<int> nBins{"nBins", 100, "N bins in all histos"};
 
   // histogram defined with HistogramRegistry
   HistogramRegistry registry{
     "registry",
-      {
-	{"hVertexZ", "hVertexZ", {HistType::kTH1F, {{nBins, -15., 15.}}}},
-	{"hMassK0Short", "hMassK0Short", {HistType::kTH1F, {{200, 0.45f, 0.55f}}}}
-      }
-  };
+    {{"hVertexZ", "hVertexZ", {HistType::kTH1F, {{nBins, -15., 15.}}}},
+     {"hMassK0Short", "hMassK0Short", {HistType::kTH1F, {{200, 0.45f, 0.55f}}}}}};
 
-  void process(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, aod::V0Datas const& V0s) 
+  void process(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, aod::V0Datas const& V0s)
   {
-    //basic event selection
+    // basic event selection
     if (!collision.sel8()) {
       return;
     }
-    //Fill the event counter
+    // Fill the event counter
     registry.fill(HIST("hVertexZ"), collision.posZ());
 
     for (auto& v0 : V0s) {
@@ -56,5 +53,5 @@ struct strangeness_step0 {
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<strangeness_step0>(cfgc, TaskName{"strangeness-step0"})};
+    adaptAnalysisTask<strangeness_tutorial>(cfgc, TaskName{"strangeness-step0"})};
 }
