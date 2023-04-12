4f0bf353a1ba8209a3a58e678c67a955c68dde1a
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2023-04-12 14:56:35 +0200

    U

--- ALICE3/Core/DelphesO2Smearer.h
+++ ALICE3/Core/DelphesO2Smearer.h
@@ -1,4 +1,21 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// @author: Roberto Preghenella
+/// @email: preghenella@bo.infn.it
+///
+
 #include "TRandom.h"
+#include <map>
 
 using namespace o2;
 using namespace o2::framework;
