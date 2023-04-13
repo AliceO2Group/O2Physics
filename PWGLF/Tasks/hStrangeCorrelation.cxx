b047e12461cb814a4a33f47cd1fdb5da9186b823
David Dobrigkeit Chinellato <david.dobrigkeit.chinellato.cern.ch>
2023-04-13 10:49:36 -0300

    Change computeDeltaPhi

--- PWGLF/Tasks/hStrangeCorrelation.cxx
+++ PWGLF/Tasks/hStrangeCorrelation.cxx
@@ -65,28 +65,14 @@ struct correlateStrangeness {
   /// \param phi2 second phi value
   Double_t ComputeDeltaPhi(Double_t phi1, Double_t phi2)
   {
-    // To be completely sure, use inner products
-    Double_t x1, y1, x2, y2;
-    x1 = TMath::Cos(phi1);
-    y1 = TMath::Sin(phi1);
-    x2 = TMath::Cos(phi2);
-    y2 = TMath::Sin(phi2);
-    Double_t lInnerProd = x1 * x2 + y1 * y2;
-    Double_t lVectorProd = x1 * y2 - x2 * y1;
-
-    Double_t lReturnVal = 0;
-    if (lVectorProd > 1e-8) {
-      lReturnVal = TMath::ACos(lInnerProd);
+    Double_t deltaPhi = phi1-phi2;
+    if (deltaPhi < -TMath::Pi() / 2.) {
+      deltaPhi += 2. * TMath::Pi();
     }
-    if (lVectorProd < -1e-8) {
-      lReturnVal = -TMath::ACos(lInnerProd);
+    if(deltaPhi> 3*TMath::Pi() / 2.){
+      deltaPhi -= 2. * TMath::Pi();
     }
-
-    if (lReturnVal < -TMath::Pi() / 2.) {
-      lReturnVal += 2. * TMath::Pi();
-    }
-
-    return lReturnVal;
+    return deltaPhi;
   }
 
   void fillCorrelationsV0(aod::TriggerTracks const& triggers, aod::AssocV0s const& assocs, bool mixing)
