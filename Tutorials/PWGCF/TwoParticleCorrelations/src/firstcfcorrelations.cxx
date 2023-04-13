a762544e193f2c9f961ee1a9fc93646d2a7d6462
Victor <victor@cern.ch>
2023-04-13 17:56:03 +0200

    Remove for the time being unused variable

--- Tutorials/PWGCF/TwoParticleCorrelations/src/firstcfcorrelations.cxx
+++ Tutorials/PWGCF/TwoParticleCorrelations/src/firstcfcorrelations.cxx
@@ -30,8 +30,6 @@ using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
-static constexpr float cfgPairCutDefaults[1][5] = {{-1, -1, -1, -1, -1}};
-
 struct firstcorrelations {
   void init(InitContext&)
   {
