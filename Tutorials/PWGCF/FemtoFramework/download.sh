4592d059d53432360ad14f2f9328016cbb94e90d
Anton Riedel <anton.riedel@tum.de>
2023-04-15 18:01:18 +0200

    Fix: fix copyright notice in scirpts

--- Tutorials/PWGCF/FemtoFramework/download.sh
+++ Tutorials/PWGCF/FemtoFramework/download.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-## See https://alice-o2.web.cern.ch/copyright for details of the copyright
-## holders. All rights not expressly granted are reserved.
-##
-## This software is distributed under the terms of the GNU General Public
-## License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-##
-## In applying this license CERN does not waive the privileges and immunities
-## granted to it by virtue of its status as an Intergovernmental Organization
-## or submit itself to any jurisdiction.
-
-### \author Anton Riedel
 
 # available periods and runs can be found here: https://alimonitor.cern.ch/hyperloop/datasets
 # For development, try to find files with reasonable size (<1GB) for faster execution
--- Tutorials/PWGCF/FemtoFramework/prod.sh
+++ Tutorials/PWGCF/FemtoFramework/prod.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-## See https://alice-o2.web.cern.ch/copyright for details of the copyright
-## holders. All rights not expressly granted are reserved.
-##
-## This software is distributed under the terms of the GNU General Public
-## License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-##
-## In applying this license CERN does not waive the privileges and immunities
-## granted to it by virtue of its status as an Intergovernmental Organization
-## or submit itself to any jurisdiction.
-
-### \author Anton Riedel
 
 # in case the central server is not reachable, set SE yourself
 # https://alimonitor.cern.ch/stats?page=SE/table
