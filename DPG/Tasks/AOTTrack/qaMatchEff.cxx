c853f2aaad2feaf186752d33d06a3c236ee01056
Rosario Turrisi <rosario.turrisi@pd.infn.it>
2023-04-27 09:18:22 +0200

    holders in new line in copyright

--- DPG/Tasks/AOTTrack/qaMatchEff.cxx
+++ DPG/Tasks/AOTTrack/qaMatchEff.cxx
@@ -1,6 +1,6 @@
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright
+// holders. All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
@@ -13,7 +13,7 @@
 /// \brief ITS-TPC track matching and prim/sec separation checks
 ///
 /// \author Rosario Turrisi  <rosario.turrisi@pd.infn.it>, INFN-PD
-/// \author Mattia Faggin <mattia.faggin@ts.infn.it>, UniPd & INFN-TS
+/// \author Mattia Faggin <mattia.faggin@pd.infn.it>, UniPd & INFN-PD
 
 #include "Common/Core/TrackSelection.h"
 #include "Common/DataModel/EventSelection.h"
