050a1e3315cbb5f51653e0185c60070cb268b666
Jochen Klein <jochen.klein@cern.ch>
2023-04-29 12:43:51 +0200

    Implement comments from review

--- PWGHF/D2H/Tasks/taskOmegacSt.cxx
+++ PWGHF/D2H/Tasks/taskOmegacSt.cxx
@@ -9,61 +9,61 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
+/// \file taskOmegacSt.cxx
+/// \brief Task to reconstruct Ωc from strangeness-tracked Ω and pion
+///
+/// \author Jochen Klein
+
 #include "TDatabasePDG.h"
-#include "Framework/O2DatabasePDGPlugin.h"
-#include "Framework/AnalysisTask.h"
-#include "Framework/AnalysisDataModel.h"
-#include "Framework/ASoA.h"
-#include "Framework/runDataProcessing.h"
-#include "Common/DataModel/TrackSelectionTables.h"
-#include "Common/DataModel/StrangenessTracking.h"
+#include "CCDB/BasicCCDBManager.h"
 #include "Common/Core/RecoDecay.h"
 #include "Common/Core/trackUtilities.h"
-#include "ReconstructionDataFormats/DCA.h"
-#include "DetectorsBase/Propagator.h"
-#include "CCDB/BasicCCDBManager.h"
+#include "Common/DataModel/TrackSelectionTables.h"
+#include "Common/DataModel/StrangenessTracking.h"
 #include "DataFormatsParameters/GRPObject.h"
 #include "DataFormatsParameters/GRPMagField.h"
+#include "DetectorsBase/Propagator.h"
+#include "Framework/AnalysisDataModel.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/ASoA.h"
+#include "Framework/O2DatabasePDGPlugin.h"
+#include "Framework/runDataProcessing.h"
+#include "ReconstructionDataFormats/DCA.h"
 
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
-struct HfTaskOmegacST {
+struct HfTaskOmegacSt {
+  Configurable<double> bz{"bz", 50., "magnetic field"};
+  Configurable<int> cfgMaterialCorrection{"cfgMaterialCorrection", static_cast<int>(o2::base::Propagator::MatCorrType::USEMatCorrNONE), "Type of material correction"};
+  Configurable<std::string> ccdburl{"ccdb-url", "http://alice-ccdb.cern.ch", "url of the ccdb repository"};
+  Configurable<std::string> grpmagPath{"grpmagPath", "GLO/Config/GRPMagField", "CCDB path of the GRPMagField object"};
+  Configurable<std::string> grpPath{"grpPath", "GLO/GRP/GRP", "Path of the grp file"};
+
+  Service<o2::ccdb::BasicCCDBManager> ccdb;
+  int mRunNumber;
+
   using TrackedCascades = soa::Join<aod::TrackedCascades, aod::TrackedCascadeColls>;
   using TrackedV0s = soa::Join<aod::TrackedV0s, aod::TrackedV0Colls>;
   using Tracked3Bodys = soa::Join<aod::Tracked3Bodys, aod::Tracked3BodyColls>;
   using TracksExt = soa::Join<aod::TracksIU, aod::TracksCovIU, aod::TracksExtra, aod::McTrackLabels>;
 
-  Configurable<double> bz{"bz", 50., "magnetic field"};
-  Configurable<int> cfgMaterialCorrection{"cfgMaterialCorrection", static_cast<int>(o2::base::Propagator::MatCorrType::USEMatCorrNONE), "Type of material correction"};
-  Configurable<std::string> ccdburl{"ccdb-url", "http://alice-ccdb.cern.ch", "url of the ccdb repository"};
-  Configurable<std::string> grpPath{"grpPath", "GLO/GRP/GRP", "Path of the grp file"};
-  Configurable<std::string> grpmagPath{"grpmagPath", "GLO/Config/GRPMagField", "CCDB path of the GRPMagField object"};
-
-  Service<o2::ccdb::BasicCCDBManager> ccdb;
-  int mRunNumber;
-
-  OutputObj<TH1F> hDCA{"h_DCA"};
-  OutputObj<TH1F> hMassOmegac{"h_MassOmegac"};
-  OutputObj<TH1F> hMassOmegacMc{"h_MassOmegac_mc"};
-  OutputObj<TH1F> hDCAxy{"h_DCAxy"};
-  OutputObj<TH1F> hDCAz{"h_DCAz"};
-  OutputObj<TH2F> hDCAVsPt{"h_DCAVsPt"};
-  OutputObj<TH2F> hDCAVsR{"h_DCAVsR"};
-  OutputObj<TH2F> hMassVsPt{"h_MassVsPt"};
+  HistogramRegistry registry{
+    "registry",
+    {
+      {"hDca", "DCA;DCA (cm)", {HistType::kTH1D, {{200, 0., .5}}}},
+      {"hDcaXY", "DCA;DCA_{xy} (cm)", {HistType::kTH1D, {{200, -.5, .5}}}},
+      {"hDcaZ", "DCA;DCA_{z} (cm)", {HistType::kTH1D, {{200, -.5, .5}}}},
+      {"hDcaVsPt", "DCA;DCA (cm);p_{T} (GeV/#it{c})", {HistType::kTH2D, {{200, 0., .5}, {200, 0., 10.}}}},
+      {"hDcaVsR", "DCA;DCA (cm);R (cm)", {HistType::kTH2D, {{200, 0., .5}, {200, 0., 10.}}}},
+      {"hMassOmegac", "inv. mass #Omega + #pi;inv. mass (GeV/#it{c}^{2})", {HistType::kTH1D, {{400, 1.5, 3.}}}},
+      {"hMassOmegacGen", "inv. mass #Omega + #pi (from MC);inv. mass (GeV/#it{c}^{2})", {HistType::kTH1D, {{400, 1.5, 3.}}}},
+      {"hMassVsPt", "DCA;Mass (GeV/#it{c}^2);p_{T} (GeV/#it{c})", {HistType::kTH2D, {{200, 0., 10.}, {200, 0., 10.}}}},
+    }};
 
   void init(InitContext const&)
   {
-    hDCA.setObject(new TH1F("h_dca", "DCA;DCA (cm)", 200, 0., .5));
-    hMassOmegac.setObject(new TH1F("h_mass_omegac", "inv. mass #Omega + #pi;inv. mass (GeV/#it{c}^{2})", 400, 1.5, 3.));
-    hMassOmegacMc.setObject(new TH1F("h_mass_omegac_mc", "inv. mass #Omega + #pi (from MC);inv. mass (GeV/#it{c}^{2})", 400, 1.5, 3.));
-    // hMassOmegac.setObject(new TH1F("h_mass_omegac", "Omega_{c} mass;inv. mass (GeV/#it{c}^{2})", 200, 2.685, 2.715));
-    hDCAxy.setObject(new TH1F("h_dcaxy", "DCA xy;DCA_{xy} (cm)", 200, -.5, .5));
-    hDCAz.setObject(new TH1F("h_dcaz", "DCA z;DCA_{z} (cm)", 200, -.5, .5));
-    hDCAVsPt.setObject(new TH2F("h_dcavspt", "DCA vs p_{T};DCA (cm);p_{T} (GeV/#it{c})", 200, -.5, .5, 200, 0., 10.));
-    hDCAVsR.setObject(new TH2F("h_dcavspt", "DCA vs R;DCA (cm);R (cm)", 200, -.5, .5, 200, 0., 10.));
-    hMassVsPt.setObject(new TH2F("h_massvspt", "Mass vs p_{T};Mass (GeV/#it{c}^2);p_{T} (GeV/#it{c})", 200, 0., 10., 200, 0., 10.));
   }
 
   void process(aod::Collision const& collision,
@@ -101,12 +101,12 @@ struct HfTaskOmegacST {
       const auto& ptrack = v0.posTrack_as<TracksExt>();
       const auto& ntrack = v0.negTrack_as<TracksExt>();
 
-      hDCA->Fill(TMath::Sqrt(impactParameterTrk.getR2()));
-      hDCAxy->Fill(impactParameterTrk.getY());
-      hDCAz->Fill(impactParameterTrk.getZ());
-      hDCAVsPt->Fill(impactParameterTrk.getY(), trackCasc.pt());
-      hDCAVsR->Fill(impactParameterTrk.getY(), RecoDecay::sqrtSumOfSquares(trackCasc.x(), trackCasc.y()));
-      hMassVsPt->Fill(trackedCascade.omegaMass(), trackCasc.pt());
+      registry.fill(HIST("hDca"), std::sqrt(impactParameterTrk.getR2()));
+      registry.fill(HIST("hDcaXY"), impactParameterTrk.getY());
+      registry.fill(HIST("hDcaZ"), impactParameterTrk.getZ());
+      registry.fill(HIST("hDcaVsPt"), impactParameterTrk.getY(), trackCasc.pt());
+      registry.fill(HIST("hDcaVsR"), impactParameterTrk.getY(), RecoDecay::sqrtSumOfSquares(trackCasc.x(), trackCasc.y()));
+      registry.fill(HIST("hMassVsPt"), trackedCascade.omegaMass(), trackCasc.pt());
 
       if (!ptrack.has_mcParticle() || !ntrack.has_mcParticle() || !bachelor.has_mcParticle())
         continue;
@@ -128,7 +128,7 @@ struct HfTaskOmegacST {
           LOG(debug) << "cascade with PDG code: " << pdgCode;
           if (std::abs(pdgCode) == 3334) {
             LOG(debug) << "found Omega, looking for pions";
-            std::array<double, 2> masses{RecoDecay::getMassPDG(3334), RecoDecay::getMassPDG(211)};
+            std::array<double, 2> masses{RecoDecay::getMassPDG(3334), RecoDecay::getMassPDG(kPiPlus)};
             std::array<std::array<float, 3>, 2> momenta;
             trackParCovTrk.getPxPyPzGlo(momenta[0]);
             // momenta[0] = {trackCasc.px(), trackCasc.py(), trackCasc.pz()};
@@ -144,12 +144,12 @@ struct HfTaskOmegacST {
                 o2::base::Propagator::Instance()->propagateToDCABxByBz(primaryVertex, trackParCovPion, 2.f, static_cast<o2::base::Propagator::MatCorrType>(cfgMaterialCorrection.value), &impactParameterPion);
                 trackParCovPion.getPxPyPzGlo(momenta[1]);
                 // momenta[1] = {track.px(), track.py(), track.pz()};
-                hMassOmegac->Fill(RecoDecay::m(momenta, masses));
+                registry.fill(HIST("hMassOmegac"), RecoDecay::m(momenta, masses));
 
                 // MC-based mass
                 momenta[0] = {mother.px(), mother.py(), mother.pz()};
                 momenta[1] = {mcpart.px(), mcpart.py(), mcpart.pz()};
-                hMassOmegacMc->Fill(RecoDecay::m2(momenta, masses));
+                registry.fill(HIST("hMassOmegacGen"), RecoDecay::m(momenta, masses));
               }
             }
           }
@@ -162,5 +162,5 @@ struct HfTaskOmegacST {
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<HfTaskOmegacST>(cfgc)};
+    adaptAnalysisTask<HfTaskOmegacSt>(cfgc)};
 }
