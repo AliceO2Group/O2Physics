467e7be5c93009ba904574497ad522c473a60ca8
mcoquet642 <maurice.louis.coquet@cern.ch>
2023-05-03 17:41:07 +0200

    Using templated processes

--- Common/TableProducer/fwdtrackToCollisionAssociator.cxx
+++ Common/TableProducer/fwdtrackToCollisionAssociator.cxx
@@ -29,133 +29,71 @@ using namespace o2::aod;
 
 struct FwdTrackToCollisionAssociation {
   Produces<FwdTrackAssoc> fwdassociation;
-  Produces<FwdTrackCompColls> fwdreverseIndices;
+  Produces<FwdTrkCompColls> fwdreverseIndices;
   Produces<MFTTrackAssoc> mftassociation;
-  Produces<MFTTrackCompColls> mftreverseIndices;
+  Produces<MFTTrkCompColls> mftreverseIndices;
 
   Configurable<float> nSigmaForTimeCompat{"nSigmaForTimeCompat", 4.f, "number of sigmas for time compatibility"};
   Configurable<float> timeMargin{"timeMargin", 0.f, "time margin in ns added to uncertainty because of uncalibrated TPC"};
   Configurable<bool> includeUnassigned{"includeUnassigned", false, "consider also tracks which are not assigned to any collision"};
   Configurable<bool> fillTableOfCollIdsPerTrack{"fillTableOfCollIdsPerTrack", false, "fill additional table with vector of collision ids per track"};
 
-  using MyMuons = soa::Join<aod::FwdTracks, aod::FwdTracksDCA>;
+  Preslice<FwdTracks> muonsPerCollisions = aod::fwdtrack::collisionId;
+  Preslice<MFTTracks> mftsPerCollisions = aod::fwdtrack::collisionId;
+
+  constexpr static uint32_t gkMFTFillMap = BIT(0);
+  constexpr static uint32_t gkMuonFillMap = BIT(1);
 
   void init(InitContext const&)
   {
   }
 
-  void processFwdAssocWithTime(Collisions const& collisions,
-                               MyMuons const& muons,
-                               AmbiguousFwdTracks const& ambiTracksFwd,
-                               BCs const& bcs)
-  {
-    // cache globalBC
-    std::vector<uint64_t> globalBC;
-    for (const auto& muon : muons) {
-      if (muon.has_collision()) {
-        globalBC.push_back(muon.collision().bc().globalBC());
-      } else {
-        for (const auto& ambTrack : ambiTracksFwd) {
-          if (ambTrack.fwdtrackId() == muon.globalIndex()) {
-            globalBC.push_back(ambTrack.bc().begin().globalBC());
-            break;
-          }
-        }
-      }
-    }
-
-    // define vector of vectors to store indices of compatible collisions per track
-    std::vector<std::unique_ptr<std::vector<int>>> collsPerTrack(muons.size());
-
-    // loop over collisions to find time-compatible tracks
-    auto muonBegin = muons.begin();
-    constexpr auto bOffsetMax = 241; // 6 mus (ITS)
-    for (const auto& collision : collisions) {
-      const float collTime = collision.collisionTime();
-      const float collTimeRes2 = collision.collisionTimeRes() * collision.collisionTimeRes();
-      uint64_t collBC = collision.bc().globalBC();
-      // bool iteratorMoved = false;
-      for (auto muon = muonBegin; muon != muons.end(); ++muon) {
-        if (!includeUnassigned && !muon.has_collision()) {
-          continue;
-        }
-        const int64_t bcOffset = (int64_t)globalBC[muon.globalIndex()] - (int64_t)collBC;
-        if (std::abs(bcOffset) > bOffsetMax) {
-          continue;
-        }
-
-        float trackTime = muon.trackTime();
-        float trackTimeRes = muon.trackTimeRes();
-        const float deltaTime = trackTime - collTime + bcOffset * constants::lhc::LHCBunchSpacingNS;
-        float sigmaTimeRes2 = collTimeRes2 + trackTimeRes * trackTimeRes;
-        LOGP(debug, "collision time={}, collision time res={}, track time={}, track time res={}, bc collision={}, bc track={}, delta time={}", collTime, collision.collisionTimeRes(), muon.trackTime(), muon.trackTimeRes(), collBC, globalBC[muon.filteredIndex()], deltaTime);
-
-        float thresholdTime = 0.;
-        thresholdTime = nSigmaForTimeCompat * std::sqrt(sigmaTimeRes2) + timeMargin;
-
-        if (std::abs(deltaTime) < thresholdTime) {
-          const auto collIdx = collision.globalIndex();
-          const auto muonIdx = muon.globalIndex();
-          LOGP(debug, "Filling track id {} for coll id {}", muonIdx, collIdx);
-          fwdassociation(collIdx, muonIdx);
-          if (fillTableOfCollIdsPerTrack) {
-            if (collsPerTrack[muonIdx] == nullptr) {
-              collsPerTrack[muonIdx] = std::make_unique<std::vector<int>>();
-            }
-            collsPerTrack[muonIdx].get()->push_back(collIdx);
-          }
-        }
-      }
-    }
-
-    // create reverse index track to collisions if enabled
-    if (fillTableOfCollIdsPerTrack) {
-      std::vector<int> empty{};
-      for (const auto& muon : muons) {
-
-        const auto muonId = muon.globalIndex();
-        if (collsPerTrack[muonId] == nullptr) {
-          fwdreverseIndices(empty);
-        } else {
-          fwdreverseIndices(*collsPerTrack[muonId].get());
-        }
-      }
-    }
-  }
-
-  PROCESS_SWITCH(FwdTrackToCollisionAssociation, processFwdAssocWithTime, "Use fwdtrack-to-collision association based on time", false);
-
-  Preslice<MyMuons> muonsPerCollisions = aod::fwdtrack::collisionId;
-
-  void processStandardAssoc(Collisions const& collisions,
-                            MyMuons const& muons)
+  template <uint32_t TTrackFillMap, typename TTracks>
+  void runStandardAssoc(Collisions const& collisions,
+                            TTracks const& tracks)
   {
     // we do it for all tracks, to be compatible with Run 2 analyses
     for (const auto& collision : collisions) {
-      auto muonsThisCollision = muons.sliceBy(muonsPerCollisions, collision.globalIndex());
-      for (const auto& muon : muonsThisCollision) {
-        fwdassociation(collision.globalIndex(), muon.globalIndex());
-      }
+	  if constexpr (static_cast<bool>(TTrackFillMap & BIT(0))) {
+      		auto mftsThisCollision = tracks.sliceBy(mftsPerCollisions, collision.globalIndex());
+	        for (const auto& track : mftsThisCollision) {
+		        mftassociation(collision.globalIndex(), track.globalIndex());
+      		}
+	  }else if constexpr (static_cast<bool>(TTrackFillMap & BIT(1))) {
+      		auto muonsThisCollision = tracks.sliceBy(muonsPerCollisions, collision.globalIndex());
+	        for (const auto& muon : muonsThisCollision) {
+		        fwdassociation(collision.globalIndex(), muon.globalIndex());
+      		}
+	  }
     }
+
 
     // create reverse index track to collisions if enabled
     std::vector<int> empty{};
     if (fillTableOfCollIdsPerTrack) {
-      for (const auto& muon : muons) {
-        if (muon.has_collision()) {
-          fwdreverseIndices(std::vector<int>{muon.collisionId()});
+      for (const auto& track : tracks) {
+        if (track.has_collision()) {
+	  if constexpr (static_cast<bool>(TTrackFillMap & BIT(0))) {
+          	mftreverseIndices(std::vector<int>{track.collisionId()});
+	  }else if constexpr (static_cast<bool>(TTrackFillMap & BIT(1))) {
+          	fwdreverseIndices(std::vector<int>{track.collisionId()});
+	  }
         } else {
-          fwdreverseIndices(empty);
+	  if constexpr (static_cast<bool>(TTrackFillMap & BIT(0))) {
+          	mftreverseIndices(empty);
+	  }else if constexpr (static_cast<bool>(TTrackFillMap & BIT(1))) {
+          	fwdreverseIndices(empty);
+	  }
         }
       }
     }
   }
 
-  PROCESS_SWITCH(FwdTrackToCollisionAssociation, processStandardAssoc, "Use standard fwdtrack-to-collision association", true);
 
-  void processMFTAssocWithTime(Collisions const& collisions,
-                               MFTTracks const& tracks,
-                               AmbiguousMFTTracks const& ambiguousTracks,
+  template <uint32_t TTrackFillMap, typename TTracks, typename TAmbiTracks>
+  void runAssocWithTime(Collisions const& collisions,
+                               TTracks const& tracks,
+                               TAmbiTracks const& ambiguousTracks,
                                BCs const& bcs)
   {
     // cache globalBC
@@ -165,10 +103,17 @@ struct FwdTrackToCollisionAssociation {
         globalBC.push_back(track.collision().bc().globalBC());
       } else {
         for (const auto& ambTrack : ambiguousTracks) {
-          if (ambTrack.mfttrackId() == track.globalIndex()) {
-            globalBC.push_back(ambTrack.bc().begin().globalBC());
-            break;
-          }
+	  if constexpr (static_cast<bool>(TTrackFillMap & BIT(0))) {
+          	if (ambTrack.mfttrackId() == track.globalIndex()) {
+	            globalBC.push_back(ambTrack.bc().begin().globalBC());
+        	    break;
+	          }
+	  }else if constexpr (static_cast<bool>(TTrackFillMap & BIT(1))) {
+          	if (ambTrack.fwdtrackId() == track.globalIndex()) {
+	            globalBC.push_back(ambTrack.bc().begin().globalBC());
+        	    break;
+	          }
+	  }
         }
       }
     }
@@ -207,7 +152,11 @@ struct FwdTrackToCollisionAssociation {
           const auto collIdx = collision.globalIndex();
           const auto trackIdx = track.globalIndex();
           LOGP(debug, "Filling track id {} for coll id {}", trackIdx, collIdx);
-          mftassociation(collIdx, trackIdx);
+	  if constexpr (static_cast<bool>(TTrackFillMap & BIT(0))) {
+            mftassociation(collIdx, trackIdx);
+	  }else if constexpr (static_cast<bool>(TTrackFillMap & BIT(1))) {
+            fwdassociation(collIdx, trackIdx);
+	  }
           if (fillTableOfCollIdsPerTrack) {
             if (collsPerTrack[trackIdx] == nullptr) {
               collsPerTrack[trackIdx] = std::make_unique<std::vector<int>>();
@@ -224,15 +173,53 @@ struct FwdTrackToCollisionAssociation {
 
         const auto trackId = track.globalIndex();
         if (collsPerTrack[trackId] == nullptr) {
-          mftreverseIndices(empty);
+	  if constexpr (static_cast<bool>(TTrackFillMap & BIT(0))) {
+            mftreverseIndices(empty);
+	  }else if constexpr (static_cast<bool>(TTrackFillMap & BIT(1))) {
+            fwdreverseIndices(empty);
+	  }
         } else {
-          mftreverseIndices(*collsPerTrack[trackId].get());
+	  if constexpr (static_cast<bool>(TTrackFillMap & BIT(0))) {
+            mftreverseIndices(*collsPerTrack[trackId].get());
+	  }else if constexpr (static_cast<bool>(TTrackFillMap & BIT(1))) {
+            fwdreverseIndices(*collsPerTrack[trackId].get());
+	  }
         }
       }
     }
   }
 
+  void processFwdAssocWithTime(Collisions const& collisions,
+                               FwdTracks const& muons,
+                               AmbiguousFwdTracks const& ambiTracksFwd,
+                               BCs const& bcs)
+  {
+	  runAssocWithTime<gkMuonFillMap>(collisions, muons, ambiTracksFwd, bcs);
+  }
+  PROCESS_SWITCH(FwdTrackToCollisionAssociation, processFwdAssocWithTime, "Use fwdtrack-to-collision association based on time", true);
+
+  void processFwdStandardAssoc(Collisions const& collisions,
+                            FwdTracks const& muons)
+  {
+	  runStandardAssoc<gkMuonFillMap>(collisions, muons);
+  }
+  PROCESS_SWITCH(FwdTrackToCollisionAssociation, processFwdStandardAssoc, "Use standard fwdtrack-to-collision association", false);
+
+  void processMFTAssocWithTime(Collisions const& collisions,
+                               MFTTracks const& tracks,
+                               AmbiguousMFTTracks const& ambiguousTracks,
+                               BCs const& bcs)
+  {
+	  runAssocWithTime<gkMFTFillMap>(collisions, tracks, ambiguousTracks, bcs);
+  }
   PROCESS_SWITCH(FwdTrackToCollisionAssociation, processMFTAssocWithTime, "Use MFTtrack-to-collision association based on time", true);
+
+  void processMFTStandardAssoc(Collisions const& collisions,
+                            MFTTracks const& tracks)
+  {
+	  runStandardAssoc<gkMFTFillMap>(collisions, tracks);
+  }
+  PROCESS_SWITCH(FwdTrackToCollisionAssociation, processMFTStandardAssoc, "Use standard mfttrack-to-collision association", false);
 };
 
 //________________________________________________________________________________________________________________________
