6cd53f17ddd68c3ddb86d01e6bbefff134981c4f
Lucamicheletti93 <luca.mike93@gmail.com>
2023-05-05 14:36:12 +0200

    Adding selection on D0 / D0bar when combining with Jpsi

--- PWGDQ/Tasks/taskJpsiHf.cxx
+++ PWGDQ/Tasks/taskJpsiHf.cxx
@@ -214,8 +214,14 @@ struct taskJpsiHf {
 
       // loop over D mesons
       for (auto& dmeson : dmesons) {
-        VarManager::FillDileptonHadron(dilepton, dmeson, fValuesDmeson, invMassD0ToPiK(dmeson));
-        fHistMan->FillHistClass("DileptonHadronInvMass", fValuesDmeson);
+        if (dmeson.isSelD0() >= selectionFlagD0) {
+            VarManager::FillDileptonHadron(dilepton, dmeson, fValuesDmeson, invMassD0ToPiK(dmeson));
+            fHistMan->FillHistClass("DileptonHadronInvMass", fValuesDmeson);
+        }
+        if (dmeson.isSelD0bar() >= selectionFlagD0bar) {
+            VarManager::FillDileptonHadron(dilepton, dmeson, fValuesDmeson, invMassD0barToKPi(dmeson));
+            fHistMan->FillHistClass("DileptonHadronInvMass", fValuesDmeson);
+        }
       }
     }
   }
