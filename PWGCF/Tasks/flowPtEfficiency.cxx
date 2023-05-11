7f3b44d9ec2d6cdba7d0cddedc45345b8a73407b
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2023-05-11 14:33:26 +0200

    Add a efficiency correction task for flow analysis

---
+++ PWGCF/Tasks/flowPtEfficiency.cxx
@@ -0,0 +1,80 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+///
+/// \brief This task is an empty skeleton that fills a simple eta histogram.
+///        it is meant to be a blank page for further developments.
+/// \author everyone
+
+#include <iostream>
+#include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
+#include "Common/DataModel/TrackSelectionTables.h"
+#include "Framework/ASoAHelpers.h"
+#include "Framework/HistogramRegistry.h"
+
+using namespace o2;
+using namespace o2::framework;
+using namespace o2::framework::expressions;
+
+#define O2_DEFINE_CONFIGURABLE(NAME, TYPE, DEFAULT, HELP) Configurable<TYPE> NAME{#NAME, DEFAULT, HELP};
+
+struct flowPtEfficiency {
+
+  O2_DEFINE_CONFIGURABLE(cfgCutVertex, float, 10.0f, "Accepted z-vertex range")
+  O2_DEFINE_CONFIGURABLE(cfgCutPtMin, float, 0.2f, "Minimal pT for tracks")
+  O2_DEFINE_CONFIGURABLE(cfgCutPtMax, float, 3.0f, "Maximal pT for tracks")
+  O2_DEFINE_CONFIGURABLE(cfgCutEta, float, 0.8f, "Eta range for tracks")
+
+  ConfigurableAxis axisPt{"axisPt", {VARIABLE_WIDTH, 0.2, 0.25, 0.30, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00, 1.10, 1.20, 1.30, 1.40, 1.50, 1.60, 1.70, 1.80, 1.90, 2.00, 2.20, 2.40, 2.60, 2.80, 3.00}, "pt axis for histograms"};
+
+  // Filter the tracks
+  Filter trackFilter = (nabs(aod::track::eta) < cfgCutEta) && (aod::track::pt > cfgCutPtMin) && (aod::track::pt < cfgCutPtMax);
+  using myTracks = soa::Filtered<soa::Join<aod::Tracks, aod::TrackSelection> >;
+
+  // Filter for MCParticle
+  Filter particleFilter = (nabs(aod::mcparticle::eta) < cfgCutEta) && (aod::mcparticle::pt > cfgCutPtMin) && (aod::mcparticle::pt < cfgCutPtMax);
+  using myMcParticles = soa::Filtered<aod::McParticles>;
+
+  // Define the output
+  HistogramRegistry registry{"registry"};
+
+  void init(InitContext const&)
+  {
+    // create histograms
+    registry.add("hPtMCGen", "Monte Carlo Truth", {HistType::kTH1D, {axisPt}});
+    registry.add("hPtMCRec", "Monte Carlo Reco", {HistType::kTH1D, {axisPt}});
+  }
+
+  void processReco(myTracks const& tracks)
+  {
+    for (const auto& track : tracks) {
+      registry.fill(HIST("hPtMCRec"), track.pt());
+    }
+  }
+  PROCESS_SWITCH(flowPtEfficiency, processReco, "process reconstructed information", true);
+
+  void processSim(myMcParticles const& mcParticles)
+  {
+    //Loop over particles in this mcCollision (first argument of process: iterator)
+    for (const auto& mcParticle : mcParticles) {
+      if (mcParticle.isPhysicalPrimary()) {
+        registry.fill(HIST("hPtMCGen"), mcParticle.pt());
+      }
+    }
+  }
+  PROCESS_SWITCH(flowPtEfficiency, processSim, "process pure simulation information", true);
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<flowPtEfficiency>(cfgc)};
+}
