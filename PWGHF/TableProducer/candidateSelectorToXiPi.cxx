59d06f093563f13c0a6cc6a305043c3f50e02e06
ZFederica <zanone@physi.uni-heidelberg.de>
2023-05-23 16:08:17 +0200

    Add more fills for a QA histogram

--- PWGHF/TableProducer/candidateSelectorToXiPi.cxx
+++ PWGHF/TableProducer/candidateSelectorToXiPi.cxx
@@ -143,7 +143,7 @@ struct HfCandidateSelectorToXiPi {
   OutputObj<TH1F> hSelNCrossRowsTPCPiFromLam{TH1F("hSelNCrossRowsTPCPiFromLam", "hSelNCrossRowsTPCPiFromLam;status;entries", 5, 0., 5.)};
   OutputObj<TH1F> hSelNCrossRowsTPCPrFromLam{TH1F("hSelNCrossRowsTPCPrFromLam", "hSelNCrossRowsTPCPrFromLam;status;entries", 5, 0., 5.)};
   OutputObj<TH1F> hSelNCrossRowsTPCPiFromCasc{TH1F("hSelNCrossRowsTPCPiFromCasc", "hSelNCrossRowsTPCPiFromCasc;status;entries", 5, 0., 5.)};
-  OutputObj<TH1F> hSelCrossRowsOverFindClsTPCPiFromOme{TH1F("hSelCrossRowsOverFindClsTPCPiFromOme", "hSelCrossRowsOverFindClsTPCPiFromOme;status;entries", 5, 0., 5.)};
+  OutputObj<TH1F> hSelCrossRowsOverFindClsTPCAllTracks{TH1F("hSelCrossRowsOverFindClsTPCAllTracks", "hSelCrossRowsOverFindClsTPCAllTracks;status;entries", 10, 0., 10.)};
   OutputObj<TH1F> hSelNClsITSPiFromOme{TH1F("hSelNClsITSPiFromOme", "hSelNClsITSPiFromOme;status;entries", 5, 0., 5.)};
   OutputObj<TH1F> hSelNClsITSInnerPiFromOme{TH1F("hSelNClsITSInnerPiFromOme", "hSelNClsITSInnerPiFromOme;status;entries", 5, 0., 5.)};
   OutputObj<TH1F> hSelMassLam{TH1F("hSelMassLam", "hSelMassLam;status;entries", 5, 0., 5.)};
@@ -376,9 +376,27 @@ struct HfCandidateSelectorToXiPi {
       // further TPC selectiion
       if (trackPiFromOmeg.tpcCrossedRowsOverFindableCls() < tpcCrossedRowsOverFindableClustersRatioMin) {
         resultSelections = false;
-        hSelCrossRowsOverFindClsTPCPiFromOme->Fill(0);
+        hSelCrossRowsOverFindClsTPCAllTracks->Fill(0);
       } else {
-        hSelCrossRowsOverFindClsTPCPiFromOme->Fill(1);
+        hSelCrossRowsOverFindClsTPCAllTracks->Fill(1);
+      }
+      if (trackPiFromCasc.tpcCrossedRowsOverFindableCls() < tpcCrossedRowsOverFindableClustersRatioMin) {
+        resultSelections = false;
+        hSelCrossRowsOverFindClsTPCAllTracks->Fill(2);
+      } else {
+        hSelCrossRowsOverFindClsTPCAllTracks->Fill(3);
+      }
+      if (trackPiFromLam.tpcCrossedRowsOverFindableCls() < tpcCrossedRowsOverFindableClustersRatioMin) {
+        resultSelections = false;
+        hSelCrossRowsOverFindClsTPCAllTracks->Fill(4);
+      } else {
+        hSelCrossRowsOverFindClsTPCAllTracks->Fill(5);
+      }
+      if (trackPrFromLam.tpcCrossedRowsOverFindableCls() < tpcCrossedRowsOverFindableClustersRatioMin) {
+        resultSelections = false;
+        hSelCrossRowsOverFindClsTPCAllTracks->Fill(6);
+      } else {
+        hSelCrossRowsOverFindClsTPCAllTracks->Fill(7);
       }
 
       //  ITS clusters selection
