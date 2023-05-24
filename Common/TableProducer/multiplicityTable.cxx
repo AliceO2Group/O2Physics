722b154ddc2f77953703a2c7f0931b33d5c79db4
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2023-05-24 12:33:10 +0200

    Revert "Reserve memory in mult/cent (#2666)"
    This reverts commit 09608050079a45114aaa5e5dbde71170ca9a3686.

--- Common/TableProducer/multiplicityTable.cxx
+++ Common/TableProducer/multiplicityTable.cxx
@@ -132,7 +132,7 @@ struct MultiplicityTableTaskIndexed {
   Partition<Run3Tracks> tracksIUWithTPC = (aod::track::tpcNClsFindable > (uint8_t)0);
   Partition<Run3Tracks> pvContribTracksIU = (nabs(aod::track::eta) < 0.8f) && ((aod::track::flags & (uint32_t)o2::aod::track::PVContributor) == (uint32_t)o2::aod::track::PVContributor);
   Partition<Run3Tracks> pvContribTracksIUEta1 = (nabs(aod::track::eta) < 1.0f) && ((aod::track::flags & (uint32_t)o2::aod::track::PVContributor) == (uint32_t)o2::aod::track::PVContributor);
-  void processRun3(soa::Join<aod::Collisions, aod::EvSels> const& collisions,
+  void processRun3(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision,
                    Run3Tracks const& tracksExtra,
                    soa::Join<aod::BCs, aod::Timestamps> const& bcs,
                    aod::Zdcs const& zdcs,
@@ -140,99 +140,94 @@ struct MultiplicityTableTaskIndexed {
                    aod::FT0s const& ft0s,
                    aod::FDDs const& fdds)
   {
-    // reserve memory
-    mult.reserve(collisions.size());
-    multzeq.reserve(collisions.size());
-    for (auto const& collision : collisions) {
-      float multFV0A = 0.f;
-      float multFV0C = 0.f;
-      float multFT0A = 0.f;
-      float multFT0C = 0.f;
-      float multFDDA = 0.f;
-      float multFDDC = 0.f;
-      float multZNA = 0.f;
-      float multZNC = 0.f;
-      int multTracklets = 0;
+    float multFV0A = 0.f;
+    float multFV0C = 0.f;
+    float multFT0A = 0.f;
+    float multFT0C = 0.f;
+    float multFDDA = 0.f;
+    float multFDDC = 0.f;
+    float multZNA = 0.f;
+    float multZNC = 0.f;
+    int multTracklets = 0;
 
-      float multZeqFV0A = 0.f;
-      float multZeqFT0A = 0.f;
-      float multZeqFT0C = 0.f;
-      float multZeqFDDA = 0.f;
-      float multZeqFDDC = 0.f;
-      float multZeqNContribs = 0.f;
+    float multZeqFV0A = 0.f;
+    float multZeqFT0A = 0.f;
+    float multZeqFT0C = 0.f;
+    float multZeqFDDA = 0.f;
+    float multZeqFDDC = 0.f;
+    float multZeqNContribs = 0.f;
 
-      auto tracksGrouped = tracksIUWithTPC->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
-      auto pvContribsGrouped = pvContribTracksIU->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
-      auto pvContribsEta1Grouped = pvContribTracksIUEta1->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
-      int multTPC = tracksGrouped.size();
-      int multNContribs = pvContribsGrouped.size();
-      int multNContribsEta1 = pvContribsEta1Grouped.size();
+    auto tracksGrouped = tracksIUWithTPC->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
+    auto pvContribsGrouped = pvContribTracksIU->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
+    auto pvContribsEta1Grouped = pvContribTracksIUEta1->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
+    int multTPC = tracksGrouped.size();
+    int multNContribs = pvContribsGrouped.size();
+    int multNContribsEta1 = pvContribsEta1Grouped.size();
 
-      /* check the previous run number */
-      auto bc = collision.bc_as<soa::Join<aod::BCs, aod::Timestamps>>();
-      if (doVertexZeq > 0) {
-        if (bc.runNumber() != mRunNumber) {
-          mRunNumber = bc.runNumber(); // mark this run as at least tried
-          lCalibObjects = ccdb->getForTimeStamp<TList>("Centrality/Calibration", bc.timestamp());
-          if (lCalibObjects) {
-            hVtxZFV0A = (TProfile*)lCalibObjects->FindObject("hVtxZFV0A");
-            hVtxZFT0A = (TProfile*)lCalibObjects->FindObject("hVtxZFT0A");
-            hVtxZFT0C = (TProfile*)lCalibObjects->FindObject("hVtxZFT0C");
-            hVtxZFDDA = (TProfile*)lCalibObjects->FindObject("hVtxZFDDA");
-            hVtxZFDDC = (TProfile*)lCalibObjects->FindObject("hVtxZFDDC");
-            hVtxZNTracks = (TProfile*)lCalibObjects->FindObject("hVtxZNTracksPV");
-            lCalibLoaded = true;
-            // Capture error
-            if (!hVtxZFV0A || !hVtxZFT0A || !hVtxZFT0C || !hVtxZFDDA || !hVtxZFDDC || !hVtxZNTracks) {
-              LOGF(error, "Problem loading CCDB objects! Please check");
-              lCalibLoaded = false;
-            }
-          } else {
-            LOGF(error, "Problem loading CCDB object! Please check");
+    /* check the previous run number */
+    auto bc = collision.bc_as<soa::Join<aod::BCs, aod::Timestamps>>();
+    if (doVertexZeq > 0) {
+      if (bc.runNumber() != mRunNumber) {
+        mRunNumber = bc.runNumber(); // mark this run as at least tried
+        lCalibObjects = ccdb->getForTimeStamp<TList>("Centrality/Calibration", bc.timestamp());
+        if (lCalibObjects) {
+          hVtxZFV0A = (TProfile*)lCalibObjects->FindObject("hVtxZFV0A");
+          hVtxZFT0A = (TProfile*)lCalibObjects->FindObject("hVtxZFT0A");
+          hVtxZFT0C = (TProfile*)lCalibObjects->FindObject("hVtxZFT0C");
+          hVtxZFDDA = (TProfile*)lCalibObjects->FindObject("hVtxZFDDA");
+          hVtxZFDDC = (TProfile*)lCalibObjects->FindObject("hVtxZFDDC");
+          hVtxZNTracks = (TProfile*)lCalibObjects->FindObject("hVtxZNTracksPV");
+          lCalibLoaded = true;
+          // Capture error
+          if (!hVtxZFV0A || !hVtxZFT0A || !hVtxZFT0C || !hVtxZFDDA || !hVtxZFDDC || !hVtxZNTracks) {
+            LOGF(error, "Problem loading CCDB objects! Please check");
             lCalibLoaded = false;
           }
+        } else {
+          LOGF(error, "Problem loading CCDB object! Please check");
+          lCalibLoaded = false;
         }
       }
-      // using FT0 row index from event selection task
-      if (collision.has_foundFT0()) {
-        auto ft0 = collision.foundFT0();
-        for (auto amplitude : ft0.amplitudeA()) {
-          multFT0A += amplitude;
-        }
-        for (auto amplitude : ft0.amplitudeC()) {
-          multFT0C += amplitude;
-        }
-      }
-      // using FDD row index from event selection task
-      if (collision.has_foundFDD()) {
-        auto fdd = collision.foundFDD();
-        for (auto amplitude : fdd.chargeA()) {
-          multFDDA += amplitude;
-        }
-        for (auto amplitude : fdd.chargeC()) {
-          multFDDC += amplitude;
-        }
-      }
-      // using FV0 row index from event selection task
-      if (collision.has_foundFV0()) {
-        auto fv0 = collision.foundFV0();
-        for (auto amplitude : fv0.amplitude()) {
-          multFV0A += amplitude;
-        }
-      }
-      if (fabs(collision.posZ()) < 15.0f && lCalibLoaded) {
-        multZeqFV0A = hVtxZFV0A->Interpolate(0.0) * multFV0A / hVtxZFV0A->Interpolate(collision.posZ());
-        multZeqFT0A = hVtxZFT0A->Interpolate(0.0) * multFT0A / hVtxZFT0A->Interpolate(collision.posZ());
-        multZeqFT0C = hVtxZFT0C->Interpolate(0.0) * multFT0C / hVtxZFT0C->Interpolate(collision.posZ());
-        multZeqFDDA = hVtxZFDDA->Interpolate(0.0) * multFDDA / hVtxZFDDA->Interpolate(collision.posZ());
-        multZeqFDDC = hVtxZFDDC->Interpolate(0.0) * multFDDC / hVtxZFDDC->Interpolate(collision.posZ());
-        multZeqNContribs = hVtxZNTracks->Interpolate(0.0) * multNContribs / hVtxZNTracks->Interpolate(collision.posZ());
-      }
-
-      LOGF(debug, "multFV0A=%5.0f multFV0C=%5.0f multFT0A=%5.0f multFT0C=%5.0f multFDDA=%5.0f multFDDC=%5.0f multZNA=%6.0f multZNC=%6.0f multTracklets=%i multTPC=%i", multFV0A, multFV0C, multFT0A, multFT0C, multFDDA, multFDDC, multZNA, multZNC, multTracklets, multTPC);
-      mult(multFV0A, multFV0C, multFT0A, multFT0C, multFDDA, multFDDC, multZNA, multZNC, multTracklets, multTPC, multNContribs, multNContribsEta1);
-      multzeq(multZeqFV0A, multZeqFT0A, multZeqFT0C, multZeqFDDA, multZeqFDDC, multZeqNContribs);
     }
+    // using FT0 row index from event selection task
+    if (collision.has_foundFT0()) {
+      auto ft0 = collision.foundFT0();
+      for (auto amplitude : ft0.amplitudeA()) {
+        multFT0A += amplitude;
+      }
+      for (auto amplitude : ft0.amplitudeC()) {
+        multFT0C += amplitude;
+      }
+    }
+    // using FDD row index from event selection task
+    if (collision.has_foundFDD()) {
+      auto fdd = collision.foundFDD();
+      for (auto amplitude : fdd.chargeA()) {
+        multFDDA += amplitude;
+      }
+      for (auto amplitude : fdd.chargeC()) {
+        multFDDC += amplitude;
+      }
+    }
+    // using FV0 row index from event selection task
+    if (collision.has_foundFV0()) {
+      auto fv0 = collision.foundFV0();
+      for (auto amplitude : fv0.amplitude()) {
+        multFV0A += amplitude;
+      }
+    }
+    if (fabs(collision.posZ()) < 15.0f && lCalibLoaded) {
+      multZeqFV0A = hVtxZFV0A->Interpolate(0.0) * multFV0A / hVtxZFV0A->Interpolate(collision.posZ());
+      multZeqFT0A = hVtxZFT0A->Interpolate(0.0) * multFT0A / hVtxZFT0A->Interpolate(collision.posZ());
+      multZeqFT0C = hVtxZFT0C->Interpolate(0.0) * multFT0C / hVtxZFT0C->Interpolate(collision.posZ());
+      multZeqFDDA = hVtxZFDDA->Interpolate(0.0) * multFDDA / hVtxZFDDA->Interpolate(collision.posZ());
+      multZeqFDDC = hVtxZFDDC->Interpolate(0.0) * multFDDC / hVtxZFDDC->Interpolate(collision.posZ());
+      multZeqNContribs = hVtxZNTracks->Interpolate(0.0) * multNContribs / hVtxZNTracks->Interpolate(collision.posZ());
+    }
+
+    LOGF(debug, "multFV0A=%5.0f multFV0C=%5.0f multFT0A=%5.0f multFT0C=%5.0f multFDDA=%5.0f multFDDC=%5.0f multZNA=%6.0f multZNC=%6.0f multTracklets=%i multTPC=%i", multFV0A, multFV0C, multFT0A, multFT0C, multFDDA, multFDDC, multZNA, multZNC, multTracklets, multTPC);
+    mult(multFV0A, multFV0C, multFT0A, multFT0C, multFDDA, multFDDC, multZNA, multZNC, multTracklets, multTPC, multNContribs, multNContribsEta1);
+    multzeq(multZeqFV0A, multZeqFT0A, multZeqFT0C, multZeqFDDA, multZeqFDDC, multZeqNContribs);
   }
   PROCESS_SWITCH(MultiplicityTableTaskIndexed, processRun3, "Produce Run 3 multiplicity tables", false);
 };
