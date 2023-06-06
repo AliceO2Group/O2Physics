4363e6458587e4532fc405ca57f78ccd248e8e51
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2023-06-06 18:48:23 +0200

    Re-organize the directory structure, and create more reasonable efficiency

--- PWGCF/Flow/Tasks/flowPtEfficiency.cxx
+++ PWGCF/Flow/Tasks/flowPtEfficiency.cxx
@@ -29,15 +29,16 @@ using namespace o2::framework::expressions;
 struct flowPtEfficiency {
 
   O2_DEFINE_CONFIGURABLE(cfgCutVertex, float, 10.0f, "Accepted z-vertex range")
-  O2_DEFINE_CONFIGURABLE(cfgCutPtMin, float, 0.2f, "Minimal pT for tracks")
-  O2_DEFINE_CONFIGURABLE(cfgCutPtMax, float, 3.0f, "Maximal pT for tracks")
-  O2_DEFINE_CONFIGURABLE(cfgCutEta, float, 0.8f, "Eta range for tracks")
+  O2_DEFINE_CONFIGURABLE(cfgCutPtMin,  float, 0.2f,  "Minimal pT for tracks")
+  O2_DEFINE_CONFIGURABLE(cfgCutPtMax,  float, 3.0f,  "Maximal pT for tracks")
+  O2_DEFINE_CONFIGURABLE(cfgCutEta,    float, 0.8f,  "Eta range for tracks")
+  O2_DEFINE_CONFIGURABLE(cfgCutDCAxy,  float, 0.2f,  "DCAxy cut for tracks")
 
   ConfigurableAxis axisPt{"axisPt", {VARIABLE_WIDTH, 0.2, 0.25, 0.30, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00, 1.10, 1.20, 1.30, 1.40, 1.50, 1.60, 1.70, 1.80, 1.90, 2.00, 2.20, 2.40, 2.60, 2.80, 3.00}, "pt axis for histograms"};
 
   // Filter the tracks
-  Filter trackFilter = (nabs(aod::track::eta) < cfgCutEta) && (aod::track::pt > cfgCutPtMin) && (aod::track::pt < cfgCutPtMax) && (nabs(aod::track::dcaXY) < 0.2f);
-  using myTracks = soa::Filtered<soa::Join<aod::Tracks, aod::TrackSelection, aod::TracksDCA>>;
+  Filter trackFilter = (nabs(aod::track::eta) < cfgCutEta) && (aod::track::pt > cfgCutPtMin) && (aod::track::pt < cfgCutPtMax) && (nabs(aod::track::dcaXY) < cfgCutDCAxy);
+  using myTracks = soa::Filtered<soa::Join<aod::Tracks, aod::TracksExtra, aod::TrackSelection, aod::TracksDCA, aod::McTrackLabels>>;
 
   // Filter for MCParticle
   Filter particleFilter = (nabs(aod::mcparticle::eta) < cfgCutEta) && (aod::mcparticle::pt > cfgCutPtMin) && (aod::mcparticle::pt < cfgCutPtMax);
@@ -46,27 +47,49 @@ struct flowPtEfficiency {
   // Define the output
   HistogramRegistry registry{"registry"};
 
-  void init(InitContext const&)
-  {
-    // create histograms
-    registry.add("hPtMCGen", "Monte Carlo Truth", {HistType::kTH1D, {axisPt}});
-    registry.add("hPtMCRec", "Monte Carlo Reco", {HistType::kTH1D, {axisPt}});
+  bool isStable(int pdg) {
+    if (abs(pdg) == 211) return true;
+    if (abs(pdg) == 321) return true;
+    if (abs(pdg) == 2212) return true;
+    if (abs(pdg) == 11) return true;
+    if (abs(pdg) == 13) return true;
+    return false;
   }
 
-  void processReco(myTracks const& tracks)
+  void init(InitContext const&)
   {
+    const AxisSpec axisCounter{1, 0, +1, ""};
+    // create histograms
+    registry.add("eventCounter", "eventCounter", kTH1F, {axisCounter});
+    registry.add("hPtMCRec", "Monte Carlo Reco", {HistType::kTH1D, {axisPt}});
+
+    registry.add("mcEventCounter", "Monte Carlo Truth EventCounter", kTH1F, {axisCounter});
+    registry.add("hPtMCGen", "Monte Carlo Truth", {HistType::kTH1D, {axisPt}});
+  }
+
+  void processReco(o2::aod::Collision const& collision, myTracks const& tracks, aod::McParticles const&)
+  {
+    registry.fill(HIST("eventCounter"), 0.5);
     for (const auto& track : tracks) {
-      registry.fill(HIST("hPtMCRec"), track.pt());
+      if (track.tpcNClsCrossedRows() < 70) continue;
+      if (track.has_mcParticle()) {
+        auto mcParticle = track.mcParticle();
+        if (isStable(mcParticle.pdgCode())) {
+            registry.fill(HIST("hPtMCRec"), track.pt());
+          }
+      }
     }
   }
   PROCESS_SWITCH(flowPtEfficiency, processReco, "process reconstructed information", true);
 
-  void processSim(myMcParticles const& mcParticles)
+  void processSim(aod::McCollision const& mcCollision, soa::SmallGroups<soa::Join<aod::McCollisionLabels, aod::Collisions>> const& collisions, myMcParticles const& mcParticles)
   {
-    // Loop over particles in this mcCollision (first argument of process: iterator)
-    for (const auto& mcParticle : mcParticles) {
-      if (mcParticle.isPhysicalPrimary()) {
-        registry.fill(HIST("hPtMCGen"), mcParticle.pt());
+    if (collisions.size() > -1) {
+      registry.fill(HIST("mcEventCounter"), 0.5);
+      for (const auto& mcParticle : mcParticles) {
+        if (mcParticle.isPhysicalPrimary() && isStable(mcParticle.pdgCode())) {
+          registry.fill(HIST("hPtMCGen"), mcParticle.pt());
+        }
       }
     }
   }
