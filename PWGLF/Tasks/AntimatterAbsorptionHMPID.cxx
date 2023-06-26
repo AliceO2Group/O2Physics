7b1cb845c74fe175c09821fa44944d24622bcc28
Nicolò Jacazio <njacazio@users.noreply.github.com>
2023-06-26 18:41:54 +0200

    Update AntimatterAbsorptionHMPID.cxx

--- PWGLF/Tasks/AntimatterAbsorptionHMPID.cxx
+++ PWGLF/Tasks/AntimatterAbsorptionHMPID.cxx
@@ -1,4 +1,3 @@
-//============================================================================================
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
@@ -9,9 +8,12 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-//
-// Authors: Alberto Caliva
-//============================================================================================
+
+///============================================================================================
+///
+/// Authors: Alberto Caliva
+///
+///============================================================================================
 
 #include <cmath>
 #include <TLorentzVector.h>
