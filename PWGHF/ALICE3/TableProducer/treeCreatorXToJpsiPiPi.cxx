b9d2a574310e83e86110e98ab6636f21669281dd
Vít Kučera <vit.kucera@cern.ch>
2023-06-29 19:28:52 +0200

    PWGHF: Remove unnecessary linked libraries and included headers (#2920)

--- PWGHF/ALICE3/TableProducer/treeCreatorXToJpsiPiPi.cxx
+++ PWGHF/ALICE3/TableProducer/treeCreatorXToJpsiPiPi.cxx
@@ -17,10 +17,8 @@
 ///
 /// \author Luca Micheletti <luca.micheletti@to.infn.it>, INFN
 
-#include "Common/Core/trackUtilities.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/runDataProcessing.h"
-#include "ReconstructionDataFormats/DCA.h"
 
 #include "PWGHF/DataModel/CandidateReconstructionTables.h"
 #include "PWGHF/DataModel/CandidateSelectionTables.h"
