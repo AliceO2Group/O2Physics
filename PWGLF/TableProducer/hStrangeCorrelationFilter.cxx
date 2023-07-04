cc2b50c20c28a0c081b2dbc0c0e76c120621471b
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2023-07-04 14:36:37 +0200

    Fix comparison sign (#2967)

--- PWGLF/TableProducer/hStrangeCorrelationFilter.cxx
+++ PWGLF/TableProducer/hStrangeCorrelationFilter.cxx
@@ -290,12 +290,12 @@ struct hstrangecorrelationfilter {
         compatibleK0Short = true;
       }
       if (TMath::Abs(posdau.tpcNSigmaPr()) < strangedEdxNSigma && TMath::Abs(negdau.tpcNSigmaPi()) < strangedEdxNSigma) {
-        if (v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) < lambdaCospa) {
+        if (v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) > lambdaCospa) {
           compatibleLambda = true;
         }
       }
       if (TMath::Abs(posdau.tpcNSigmaPi()) < strangedEdxNSigma && TMath::Abs(negdau.tpcNSigmaPr()) < strangedEdxNSigma) {
-        if (v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) < lambdaCospa) {
+        if (v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) > lambdaCospa) {
           compatibleAntiLambda = true;
         }
       }
