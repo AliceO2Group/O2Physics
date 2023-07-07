abcd74246059b3b4d905013ad08e6d8d97a4b012
jikim1290 <jikim1290@gmail.com>
2023-07-07 15:52:24 +0900

    add QA (#2991)
    Co-authored-by: junleekim <junleekim@junleekims-MacBook-Pro.local>

--- PWGLF/Tasks/f0980analysis.cxx
+++ PWGLF/Tasks/f0980analysis.cxx
@@ -43,6 +43,7 @@ struct f0980analysis {
   Configurable<float> cfgMaxTOF{"cfgMaxTOF", 3.0, "Maximum TOF PID with TPC"};
   Configurable<float> cfgMinRap{"cfgMinRap", -0.5, "Minimum rapidity for pair"};
   Configurable<float> cfgMaxRap{"cfgMaxRap", 0.5, "Maximum rapidity for pair"};
+  Configurable<uint8_t> cfgMinTPCncr{"cfgMinTPCncr", 70, "minimum TPC cluster"};
 
   void init(o2::framework::InitContext&)
   {
@@ -57,9 +58,16 @@ struct f0980analysis {
     AxisSpec massAxis = {400, 0.2, 2.2};
     AxisSpec epAxis = {20, -constants::math::PI, constants::math::PI};
 
+    AxisSpec PIDqaAxis = {120, -6, 6};
+    AxisSpec pTqaAxis = {100, 0, 20};
+
     histos.add("hInvMass_f0980_US", "unlike invariant mass", {HistType::kTH3F, {massAxis, ptAxis, centAxis}});
     histos.add("hInvMass_f0980_LSpp", "++ invariant mass", {HistType::kTH3F, {massAxis, ptAxis, centAxis}});
     histos.add("hInvMass_f0980_LSmm", "-- invariant mass", {HistType::kTH3F, {massAxis, ptAxis, centAxis}});
+
+    histos.add("QA/Nsigma_TPC", "", {HistType::kTH2F, {pTqaAxis, PIDqaAxis}});
+    histos.add("QA/Nsigma_TOF", "", {HistType::kTH2F, {pTqaAxis, PIDqaAxis}});
+    histos.add("QA/TPC_TOF", "", {HistType::kTH2F, {PIDqaAxis, PIDqaAxis}});
 
     histos.print();
   }
@@ -100,7 +108,15 @@ struct f0980analysis {
   void fillHistograms(const CollisionType& collision, const TracksType& dTracks)
   {
     TLorentzVector Pion1, Pion2, Reco;
-    for (auto& [trk1, trk2] : combinations(CombinationsStrictlyUpperIndexPolicy(dTracks, dTracks))) {
+    for (auto& [trk1, trk2] : combinations(CombinationsUpperIndexPolicy(dTracks, dTracks))) {
+
+      if (trk1.index() == trk2.index()) {
+        histos.fill(HIST("QA/Nsigma_TPC"), trk1.pt(), trk1.tpcNSigmaPi());
+        histos.fill(HIST("QA/Nsigma_TPC"), trk1.pt(), trk1.tofNSigmaPi());
+        histos.fill(HIST("QA/TPC_TOF"), trk1.tpcNSigmaPi(), trk1.tofNSigmaPi());
+        continue;
+      }
+
       if (!SelTrack(trk1) || !SelTrack(trk2))
         continue;
       if (!SelPion(trk1) || !SelPion(trk2))
@@ -128,7 +144,7 @@ struct f0980analysis {
   {
     LOGF(debug, "[DATA] Processing %d collisions", collisions.size());
     for (auto& collision : collisions) {
-      Partition<aod::ResoTracks> selectedTracks = requireTOFPIDPionCutInFilter() && o2::aod::track::pt > static_cast<float_t>(cfgMinPt) && (nabs(o2::aod::track::dcaZ) < static_cast<float_t>(cfgMaxDCAzToPVcut)) && (nabs(o2::aod::track::dcaXY) < static_cast<float_t>(cfgMaxDCArToPVcut));
+      Partition<aod::ResoTracks> selectedTracks = requireTOFPIDPionCutInFilter() && o2::aod::track::pt > static_cast<float_t>(cfgMinPt) && (nabs(o2::aod::track::dcaZ) < static_cast<float_t>(cfgMaxDCAzToPVcut)) && (nabs(o2::aod::track::dcaXY) < static_cast<float_t>(cfgMaxDCArToPVcut)) && (aod::resodaughter::tpcNClsCrossedRows > static_cast<uint8_t>(cfgMinTPCncr));
       selectedTracks.bindTable(resotracks);
       auto colTracks = selectedTracks->sliceByCached(aod::resodaughter::resoCollisionId, collision.globalIndex(), cache);
       fillHistograms<false>(collision, colTracks);
