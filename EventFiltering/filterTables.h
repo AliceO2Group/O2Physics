763d625e3958e22fdb39f8acb6c1040ca43f208f
Jochen Klein <qgp@users.noreply.github.com>
2023-07-10 09:45:43 +0200

    Extend event filter for strangeness tracking (#2937)
    * PWGLF: Extend strangeness tracking filter
    
    - add QA histograms
    - add additional (configurable) selection criteria
    - separate Xi and Omega selection
    
    * PWGLF: Add competing mass rejection for strangeness filter
    
    * Fix MegaLinter issues

--- EventFiltering/filterTables.h
+++ EventFiltering/filterTables.h
@@ -84,7 +84,8 @@ DECLARE_SOA_COLUMN(QuadrupleXi, hasQuadrupleXi, bool);           //! at least 4 
 DECLARE_SOA_COLUMN(SingleXiYN, hasSingleXiYN, bool);             //! at least 1 Xi with high radius (YN interactions)
 DECLARE_SOA_COLUMN(OmegaLargeRadius, hasOmegaLargeRadius, bool); //! at least 1 Omega with high radius
 DECLARE_SOA_COLUMN(TrackedCascade, hasTrackedCascade, bool);     //! at least 1 tracked cascade
-DECLARE_SOA_COLUMN(TrackedV0, hasTrackedV0, bool);               //! at least 1 tracked V0
+DECLARE_SOA_COLUMN(TrackedXi, hasTrackedXi, bool);               //! at least 1 tracked Xi
+DECLARE_SOA_COLUMN(TrackedOmega, hasTrackedOmega, bool);         //! at least 1 tracked Omega
 DECLARE_SOA_COLUMN(Tracked3Body, hasTracked3Body, bool);         //! at least 1 tracked 3Body
 
 // F1-proton
@@ -169,7 +170,7 @@ using FullJetFilter = FullJetFilters::iterator;
 
 // strangeness (lf)
 DECLARE_SOA_TABLE(StrangenessFilters, "AOD", "LFStrgFilters", //!
-                  filtering::Omega, filtering::hadronXi, filtering::DoubleXi, filtering::TripleXi, filtering::QuadrupleXi, filtering::SingleXiYN, filtering::OmegaLargeRadius, filtering::TrackedCascade, filtering::TrackedV0, filtering::Tracked3Body);
+                  filtering::Omega, filtering::hadronXi, filtering::DoubleXi, filtering::TripleXi, filtering::QuadrupleXi, filtering::SingleXiYN, filtering::OmegaLargeRadius, filtering::TrackedCascade, filtering::TrackedXi, filtering::TrackedOmega, filtering::Tracked3Body);
 
 using StrangenessFilter = StrangenessFilters::iterator;
 
