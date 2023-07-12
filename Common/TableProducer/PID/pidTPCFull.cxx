55570ee8081bfb435422c797de2f6cbcf2d08dc1
Nicolò Jacazio <njacazio@users.noreply.github.com>
2023-07-12 12:27:33 +0200

    PIDTPC: Add possibility to skip TPC only tracks (#3018)
    * PIDTOF: Add possibility to skip tracks that don't have ITS
    
    * Remove in TOF implement in TPC

--- Common/TableProducer/PID/pidTPCFull.cxx
+++ Common/TableProducer/PID/pidTPCFull.cxx
@@ -88,6 +88,7 @@ struct tpcPidFull {
   // Parameters for loading network from a file / downloading the file
   Configurable<bool> useNetworkCorrection{"useNetworkCorrection", 0, "(bool) Wether or not to use the network correction for the TPC dE/dx signal"};
   Configurable<bool> autofetchNetworks{"autofetchNetworks", 1, "(bool) Automatically fetches networks from CCDB for the correct run number"};
+  Configurable<bool> skipTPCOnly{"skipTPCOnly", false, "Flag to skip TPC only tracks (faster but affects the analyses that use TPC only tracks)"};
   Configurable<std::string> networkPathLocally{"networkPathLocally", "network.onnx", "(std::string) Path to the local .onnx file. If autofetching is enabled, then this is where the files will be downloaded"};
   Configurable<std::string> networkPathCCDB{"networkPathCCDB", "Analysis/PID/TPC/ML", "Path on CCDB"};
   Configurable<bool> enableNetworkOptimizations{"enableNetworkOptimizations", 1, "(bool) If the neural network correction is used, this enables GraphOptimizationLevel::ORT_ENABLE_EXTENDED in the ONNX session"};
@@ -164,6 +165,10 @@ struct tpcPidFull {
     if (!useNetworkCorrection) {
       return;
     } else {
+      if (skipTPCOnly) {
+        LOG(fatal) << "Cannot skip TPC only tracks when using the neural network correction";
+      }
+
       /// CCDB and auto-fetching
       ccdbApi.init(url);
       if (!autofetchNetworks) {
@@ -329,6 +334,13 @@ struct tpcPidFull {
             LOGF(fatal, "Network output-dimensions incompatible!");
           }
         } else {
+          if (skipTPCOnly) {
+            if (!trk.hasITS() && !trk.hasTRD() && !trk.hasTOF()) {
+              table(-999.f, -999.f);
+              return;
+            }
+          }
+
           table(response.GetExpectedSigma(collisions.iteratorAt(trk.collisionId()), trk, pid),
                 response.GetNumberOfSigma(collisions.iteratorAt(trk.collisionId()), trk, pid));
         }
