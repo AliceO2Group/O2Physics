eea0dbdcf8de55b0ee054a53309c19e0677077dc
Johanna Lömker <jloemker@yahoo.de>
2023-07-13 07:58:44 +0200

    [PWGLF] Configurables for eta range selection and THnSparse in spectarTOF.cxx (#3047)
    * confugurables for eta range and THnSparse
    
    * clang formatting for PR
    
    * include suggestions for eta range and eta track histogram
    
    * Extend binning of nsigma
    
    - Streamline
    
    * fix dervied tables
    
    * adjust max,min binning
    
    ---------
    
    Co-authored-by: Nicolò Jacazio <nicolo.jacazio@cern.ch>

--- PWGLF/Tasks/spectraTOF.cxx
+++ PWGLF/Tasks/spectraTOF.cxx
@@ -44,7 +44,8 @@ using namespace o2::framework::expressions;
 struct tofSpectra {
   Configurable<float> cfgNSigmaCut{"cfgNSigmaCut", 3, "Value of the Nsigma cut"};
   Configurable<float> cfgCutVertex{"cfgCutVertex", 10.0f, "Accepted z-vertex range"};
-  Configurable<float> cfgCutEta{"cfgCutEta", 0.8f, "Eta range for tracks"};
+  Configurable<float> cfgCutEtaMax{"cfgCutEtaMax", 0.8f, "Max eta range for tracks"};
+  Configurable<float> cfgCutEtaMin{"cfgCutEtaMin", -0.8f, "Min eta range for tracks"};
   Configurable<float> cfgCutY{"cfgCutY", 0.5f, "Y range for tracks"};
   Configurable<int> cfgINELCut{"cfgINELCut", 0, "Event selection: 0 no sel, 1 sel8, 2 INEL>0, 3 INEL>1"};
   Configurable<bool> enableDcaGoodEvents{"enableDcaGoodEvents", true, "Enables the MC plots with the correct match between data and MC"};
@@ -56,6 +57,7 @@ struct tofSpectra {
   Configurable<bool> requireNoTrd{"requireNoTrd", false, "Require tracks without TRD"};
   Configurable<int> selectEvTime{"selectEvTime", 0, "Select event time flags; 0: any event time, 1: isEvTimeDefined, 2: IsEvTimeTOF, 3: IsEvTimeT0AC, 4: IsEvTimeTOFT0AV, 5: NOT isEvTimeDefined"};
   ConfigurableAxis binsPt{"binsPt", {VARIABLE_WIDTH, 0.0, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0}, "Binning of the pT axis"};
+  ConfigurableAxis binsEta{"binsEta", {100, -1, 1}, "Binning of the eta axis"};
   ConfigurableAxis binsnsigmaTPC{"binsnsigmaTPC", {200, -10, 10}, "Binning of the nsigmaTPC axis"};
   ConfigurableAxis binsnsigmaTOF{"binsnsigmaTOF", {200, -10, 10}, "Binning of the nsigmaTOF axis"};
   ConfigurableAxis binsdeltaTPC{"binsdeltaTPC", {500, -1000, 1000}, "Binning of the nsigmaTPC axis"};
@@ -189,7 +191,7 @@ struct tofSpectra {
 
     h = histos.add<TH1>("tracksel", "tracksel", HistType::kTH1D, {{10, 0.5, 10.5}});
     h->GetXaxis()->SetBinLabel(1, "Tracks read");
-    h->GetXaxis()->SetBinLabel(2, Form("|#eta| < %.2f", cfgCutEta.value));
+    h->GetXaxis()->SetBinLabel(2, Form(" %.2f < #eta < %.2f ", cfgCutEtaMin.value, cfgCutEtaMax.value));
     h->GetXaxis()->SetBinLabel(3, "Quality passed");
     h->GetXaxis()->SetBinLabel(4, "TOF passed (partial)");
 
@@ -228,6 +230,7 @@ struct tofSpectra {
 
     if (enableTrackCutHistograms) {
       const AxisSpec chargeAxis{2, -2.f, 2.f, "Charge"};
+      histos.add("track/Eta", "Eta", HistType::kTH1D, {{binsEta, "#eta tracks"}});
       // its histograms
       histos.add("track/ITS/itsNCls", "number of found ITS clusters;# clusters ITS", kTH2D, {{8, -0.5, 7.5}, chargeAxis});
       histos.add("track/ITS/itsChi2NCl", "chi2 per ITS cluster;chi2 / cluster ITS", kTH2D, {{100, 0, 40}, chargeAxis});
@@ -368,10 +371,14 @@ struct tofSpectra {
       const AxisSpec nsigmaTOFAxis{binsnsigmaTOF, Form("N_{#sigma}^{TOF}(%s)", pTCharge[i])};
       const AxisSpec deltaTPCAxis{binsdeltaTPC, Form("#Delta^{TPC}(%s)", pTCharge[i])};
       const AxisSpec deltaTOFAxis{binsdeltaTOF, Form("#Delta^{TOF}(%s)", pTCharge[i])};
+      const AxisSpec etaAxis{binsEta, Form("#eta^{TPC}_{TOF}(%s)", pTCharge[i])};
       AxisSpec multAxis{binsMultiplicity, "Undefined multiplicity estimator"};
 
-      if (enableTPCTOFHistograms) {
+      if (enableTPCTOFHistograms && !makeTHnSparseChoice) {
         histos.add(hnsigmatpctof[i].data(), pTCharge[i], kTH3D, {ptAxis, nsigmaTPCAxis, nsigmaTOFAxis});
+      }
+      if (enableTPCTOFHistograms && makeTHnSparseChoice) {
+        histos.add(hnsigmatpctof[i].data(), pTCharge[i], kTHnSparseD, {ptAxis, etaAxis, nsigmaTPCAxis, nsigmaTOFAxis});
       }
 
       switch (multiplicityEstimator) {
@@ -678,7 +685,15 @@ struct tofSpectra {
       }
     }
 
-    if (enableTPCTOFHistograms) {
+    if (enableTPCTOFHistograms && makeTHnSparseChoice) {
+      if (track.sign() > 0) {
+        histos.fill(HIST(hnsigmatpctof[id]), track.pt(), track.eta(), nsigmaTPC, nsigmaTOF);
+      } else {
+        histos.fill(HIST(hnsigmatpctof[id + Np]), track.pt(), track.eta(), nsigmaTPC, nsigmaTOF);
+      }
+    }
+
+    if (enableTPCTOFHistograms && !makeTHnSparseChoice) {
       if (track.sign() > 0) {
         histos.fill(HIST(hnsigmatpctof[id]), track.pt(), nsigmaTPC, nsigmaTOF);
       } else {
@@ -846,7 +861,7 @@ struct tofSpectra {
     if constexpr (fillHistograms) {
       histos.fill(HIST("tracksel"), 1);
     }
-    if (abs(track.eta()) > cfgCutEta) {
+    if (track.eta() < cfgCutEtaMin || track.eta() > cfgCutEtaMax) {
       return false;
     }
     if constexpr (fillHistograms) {
@@ -1011,6 +1026,7 @@ struct tofSpectra {
         if (!isTrackSelected<true>(track)) {
           continue;
         }
+        histos.fill(HIST("track/Eta"), track.eta());
         fillParticleHistos<false, PID::Pion>(track, collision);
         fillParticleHistos<false, PID::Kaon>(track, collision);
         fillParticleHistos<false, PID::Proton>(track, collision);
@@ -1173,9 +1189,10 @@ struct tofSpectra {
     if (mcParticle.pdgCode() != PDGs[i]) {
       return;
     }
-    if (std::abs(mcParticle.eta()) > cfgCutEta) {
+    if (track.eta() < cfgCutEtaMin || track.eta() > cfgCutEtaMax) {
       return;
     }
+
     if (std::abs(mcParticle.y()) > cfgCutY) {
       return;
     }
