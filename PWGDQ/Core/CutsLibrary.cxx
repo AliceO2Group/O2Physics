3aa8de23115b69713dc88381755e4cfe91665c02
Ida Torkjellsdatter Storehaug <idats@Idas-MacBook-Pro.local>
2023-07-13 12:50:19 +0200

    removed duplicated cuts

--- PWGDQ/Core/CutsLibrary.cxx
+++ PWGDQ/Core/CutsLibrary.cxx
@@ -2044,7 +2044,6 @@ AnalysisCut* o2::aod::dqcuts::GetAnalysisCut(const char* cutName)
     cut->AddCut(VarManager::kTPCnSigmaEl, -3.0, 3.0);
     return cut;
   }
-
   if (!nameStr.compare("electronPIDOnly_Corr")) {
     cut->AddCut(VarManager::kTPCnSigmaEl_Corr, -3.0, 3.0);
     return cut;
@@ -2053,15 +2052,6 @@ AnalysisCut* o2::aod::dqcuts::GetAnalysisCut(const char* cutName)
   if (!nameStr.compare("kaonPIDnsigma2")) {
     cut->AddCut(VarManager::kTPCnSigmaKa, -2.0, 2.0);
     return cut;
-  }
-
-  if (!nameStr.compare("kaonPIDnsigma2")) {
-    cut->AddCut(VarManager::kTPCnSigmaKa, -2.0, 2.0);
-    return cut;
-  }
-
-  if (!nameStr.compare("kaonPIDnsigma2")) {
-    cut->AddCut(VarManager::kTPCnSigmaKa, -2.0, 2.0);
   }
 
   if (!nameStr.compare("kaonPID_TPCnTOF")) {
