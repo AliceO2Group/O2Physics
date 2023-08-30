dcb28ab73f8189815cac78840a04d32dcc36815f
Felix Schlepper <f3sch.git@outlook.com>
2023-08-30 09:11:18 +0200

    PWGEM/PhotonMeson: Add MCV0 task (#3390)
    * git: add clangd .cache to .gitignore
    
    clangd stores the index inside the project root when using background
    indexing.
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
    
    * PWGEM: V0: Add MC V0 correlation plots
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
    
    ---------
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

--- PWGEM/PhotonMeson/Tasks/CheckMCV0.cxx
+++ PWGEM/PhotonMeson/Tasks/CheckMCV0.cxx
@@ -9,35 +9,18 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \brief write relevant information for photon conversion analysis to an AO2D.root file.
+/// \brief check the v0 phase-space
 /// dependencies: o2-analysis-lf-lambdakzeromcfinder
-/// \author daiki.sekihata@cern.ch
-
-// #include "PWGEM/PhotonMeson/DataModel/gammaTables.h"
-// #include "PWGEM/PhotonMeson/Utils/gammaConvDefinitions.h"
-// #include "PWGEM/PhotonMeson/Utils/PCMUtilities.h"
+/// \author daiki.sekihata@cern.ch felix.schlepper@cern.ch
 
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
 #include "Framework/HistogramRegistry.h"
-#include "Common/DataModel/EventSelection.h"
 #include "Common/DataModel/TrackSelectionTables.h"
-
-#include "DetectorsBase/Propagator.h"
-#include "DetectorsBase/GeometryManager.h"
-#include "DataFormatsParameters/GRPObject.h"
-#include "DataFormatsParameters/GRPMagField.h"
-#include "CCDB/BasicCCDBManager.h"
-
-#include "DCAFitter/HelixHelper.h"
-#include "ReconstructionDataFormats/TrackFwd.h"
-#include "Common/Core/trackUtilities.h"
-#include "CommonConstants/PhysicsConstants.h"
 
 #include <TMath.h>
 #include <TVector2.h>
-#include "Math/Vector4D.h"
 
 using namespace o2;
 using namespace o2::aod;
@@ -46,41 +29,193 @@ using namespace o2::framework;
 using namespace o2::framework::expressions;
 using namespace o2::constants::physics;
 
-using MyTracks = soa::Join<aod::TracksIU, aod::TracksExtra, aod::TracksDCA, aod::TracksCovIU>;
-// using MyTracksMC = soa::Join<MyTracks, aod::McTrackLabels>;
-
 struct CheckMCV0 {
-  Configurable<float> minpt{"minpt", 0.01, "min pt for track in GeV/c"};
+  // Track selection
+  Configurable<bool> ptLogAxis{"ptLogAxis", false, "Flag to use a log momentum axis"};
+  Configurable<float> minpt{"minpt", 0.001, "min pt for track in GeV/c"};
+  Configurable<float> maxpt{"maxpt", 999.f, "max pt for track in GeV/c"};
+  Configurable<float> maxZ{"maxZ", 200.f, "max z for track"};
   Configurable<float> maxeta{"maxeta", 999.f, "eta acceptance"};
   Configurable<float> dcamin{"dcamin", 0.1, "dcamin"};
   Configurable<float> dcamax{"dcamax", 1e+10, "dcamax"};
+  Configurable<float> minX{"minX", 0.0, "minimum X (starting point of track X)"};
+  Configurable<float> maxX{"maxX", 200.0, "maximum X (starting point of track X)"};
+  Configurable<float> minY{"minY", 0.0, "minimum Y (starting point of track Y)"};
+  Configurable<float> maxY{"maxY", 200.0, "maximum Y (starting point of track Y)"};
+  Configurable<int> minTPCNCls{"minTPCNCls", 10, "minimum number TPC clusters"};
+
+  // Filters
+  using Tracks = soa::Join<aod::TracksIU, aod::TracksExtra, aod::TracksDCA, aod::TracksCovIU>;
+  using FilteredTracks = soa::Filtered<Tracks>;
+  Filter trackPt = aod::track::pt > minpt&& aod::track::pt < maxpt;
+  Filter trackZ = nabs(aod::track::z) < 200.f;
+  Filter trackX = nabs(aod::track::x) > minX&& nabs(aod::track::x) < maxX;
+  Filter trackY = nabs(aod::track::y) > minY&& nabs(aod::track::y) < maxY;
+  Filter trackEta = nabs(aod::track::eta) < maxeta;
+  Filter trackDCA = nabs(aod::track::dcaXY) > dcamin&& nabs(aod::track::dcaXY) < dcamax;
+
+  // Histogram Parameters
+  Configurable<int> tglNBins{"tglNBins", 500, "nBins for tgl"};
+  Configurable<int> zNBins{"zNBins", 1000, "nBins for z"};
+  Configurable<int> xNBins{"xNBins", 1000, "nBins for x"};
+  Configurable<int> yNBins{"yNBins", 1000, "nBins for y"};
+  Configurable<int> ptNBins{"ptNBins", 1000, "nBins for pt"};
 
   HistogramRegistry registry{"output", {}, OutputObjHandlingPolicy::AnalysisObject};
-  void init(InitContext const&)
+
+  static constexpr std::array<std::string_view, 6> v0types = {"ITSTPC_ITSTPC", "TPConly_TPConly", "ITSonly_ITSonly", "ITSTPC_TPConly", "ITSTPC_ITSonly", "TPConly_ITSonly"};
+
+  void init(InitContext const& /*unused*/)
   {
-    addhistograms();
+    const AxisSpec tglPos{tglNBins, -5.f, +5.f, "tan(#lambda) of e^{+}"};
+    const AxisSpec tglEle{tglNBins, -5.f, +5.f, "tan(#lambda) of e^{-}"};
+    const AxisSpec zEle{zNBins, -maxZ, maxZ, "z of e^{-}"};
+    const AxisSpec zPos{zNBins, -maxZ, maxZ, "z of e^{+}"};
+    const AxisSpec zPosEle{zNBins, -maxZ, maxZ, "z of e^{+} - z of e^{-}"};
+    const AxisSpec xEle{xNBins, -maxX, maxX, "x of e^{-}"};
+    const AxisSpec xPos{xNBins, -maxX, maxX, "x of e^{+}"};
+    const AxisSpec yPos{yNBins, -maxY, maxY, "y of e^{+}"};
+    const AxisSpec yPosEle{yNBins, -maxY, maxY, "y of e^{+} - y of e^{-}"};
+    AxisSpec ptPos{ptNBins, minpt, maxpt, "#it{p}_{T} GeV/#it{c} of e^{+}"};
+    AxisSpec ptEle{ptNBins, minpt, maxpt, "#it{p}_{T} GeV/#it{c} of e^{-}"};
+    if (ptLogAxis) {
+      ptPos.makeLogarithmic();
+      ptEle.makeLogarithmic();
+    }
+
+    for (const auto& v0type : v0types) {
+      LOGF(info, "Adding histograms for %s", v0type);
+      registry.add(Form("%s/TglTgl", v0type.data()), "tan(#lambda) vs. tan(#lambda)", HistType::kTH2F, {tglPos, tglEle});
+      registry.add(Form("%s/PtPt", v0type.data()), "p_{T} vs. p_{T}", HistType::kTH2F, {{ptPos}, {ptEle}});
+
+      registry.add(Form("%s/XX", v0type.data()), "x vs. x", HistType::kTH2F, {{xPos, xEle}});
+      registry.add(Form("%s/ZZ", v0type.data()), "z vs. z", HistType::kTH2F, {{zPos, zEle}});
+      registry.add(Form("%s/YY", v0type.data()), "y vs. y", HistType::kTH2F, {{yPos, yPosEle}});
+      registry.add(Form("%s/XY", v0type.data()), "x vs. y", HistType::kTH2F, {{xPos, yPosEle}});
+      registry.add(Form("%s/XZ", v0type.data()), "x vs. Z", HistType::kTH2F, {{xPos, zPosEle}});
+
+      registry.add(Form("%s/ZPosTglPos", v0type.data()), "z vs. tan(#lambda)", HistType::kTH2F, {{zPos}, {tglPos}});
+      registry.add(Form("%s/ZEleTglPos", v0type.data()), "z vs. tan(#lambda)", HistType::kTH2F, {{zEle}, {tglPos}});
+      registry.add(Form("%s/ZPosTglEle", v0type.data()), "z vs. tan(#lambda)", HistType::kTH2F, {{zPos}, {tglEle}});
+      registry.add(Form("%s/ZEleTglEle", v0type.data()), "z vs. tan(#lambda)", HistType::kTH2F, {{zEle}, {tglEle}});
+
+      registry.add(Form("%s/PtPosTglPos", v0type.data()), "p_{T} vs. tan(#lambda)", HistType::kTH2F, {{ptPos}, {tglPos}});
+      registry.add(Form("%s/PtPosTglEle", v0type.data()), "p_{T} vs. tan(#lambda)", HistType::kTH2F, {{ptPos}, {tglEle}});
+      registry.add(Form("%s/PtEleTglPos", v0type.data()), "p_{T} vs. tan(#lambda)", HistType::kTH2F, {{ptEle}, {tglPos}});
+      registry.add(Form("%s/PtEleTglEle", v0type.data()), "p_{T} vs. tan(#lambda)", HistType::kTH2F, {{ptEle}, {tglEle}});
+
+      // AntiCor
+      registry.add(Form("%s/anti/TglTgl", v0type.data()), "tan(#lambda) vs. tan(#lambda)", HistType::kTH2F, {tglPos, tglEle});
+      registry.add(Form("%s/anti/PtPt", v0type.data()), "p_{T} vs. p_{T}", HistType::kTH2F, {{ptPos}, {ptEle}});
+
+      registry.add(Form("%s/anti/XX", v0type.data()), "x vs. x", HistType::kTH2F, {{xPos, xEle}});
+      registry.add(Form("%s/anti/ZZ", v0type.data()), "z vs. z", HistType::kTH2F, {{zPos, zEle}});
+      registry.add(Form("%s/anti/YY", v0type.data()), "y vs. y", HistType::kTH2F, {{yPos, yPosEle}});
+      registry.add(Form("%s/anti/XY", v0type.data()), "x vs. y", HistType::kTH2F, {{xPos, yPosEle}});
+      registry.add(Form("%s/anti/XZ", v0type.data()), "x vs. Z", HistType::kTH2F, {{xPos, zPosEle}});
+
+      registry.add(Form("%s/anti/ZPosTglPos", v0type.data()), "z vs. tan(#lambda)", HistType::kTH2F, {{zPos}, {tglPos}});
+      registry.add(Form("%s/anti/ZEleTglPos", v0type.data()), "z vs. tan(#lambda)", HistType::kTH2F, {{zEle}, {tglPos}});
+      registry.add(Form("%s/anti/ZPosTglEle", v0type.data()), "z vs. tan(#lambda)", HistType::kTH2F, {{zPos}, {tglEle}});
+      registry.add(Form("%s/anti/ZEleTglEle", v0type.data()), "z vs. tan(#lambda)", HistType::kTH2F, {{zEle}, {tglEle}});
+
+      registry.add(Form("%s/anti/PtPosTglPos", v0type.data()), "p_{T} vs. tan(#lambda)", HistType::kTH2F, {{ptPos}, {tglPos}});
+      registry.add(Form("%s/anti/PtPosTglEle", v0type.data()), "p_{T} vs. tan(#lambda)", HistType::kTH2F, {{ptPos}, {tglEle}});
+      registry.add(Form("%s/anti/PtEleTglPos", v0type.data()), "p_{T} vs. tan(#lambda)", HistType::kTH2F, {{ptEle}, {tglPos}});
+      registry.add(Form("%s/anti/PtEleTglEle", v0type.data()), "p_{T} vs. tan(#lambda)", HistType::kTH2F, {{ptEle}, {tglEle}});
+    }
+    registry.add("V0Counter", "V0 counter", HistType::kTH1F, {{6, 0.5, 6.5}});
   }
 
-  static constexpr std::string_view v0types[6] = {"ITSTPC_ITSTPC", "TPConly_TPConly", "ITSonly_ITSonly", "ITSTPC_TPConly", "ITSTPC_ITSonly", "TPConly_ITSonly"};
-  void addhistograms()
+  void processV0(aod::Collisions const& /*collisions*/, aod::V0s const& v0s, FilteredTracks const& /*tracks*/)
   {
-    registry.add("V0Counter", "V0 counter", HistType::kTH1F, {{6, 0.5, 6.5}});
-    for (int i = 0; i < 6; i++) {
-      LOGF(info, "adding histogram for %s", v0types[i].data());
-      registry.add(Form("%s/h2TglTgl", v0types[i].data()), "tgl vs. tgl;tan(#lambda) of e^{+};tan(#lambda) of e^{-}", HistType::kTH2F, {{100, -5, +5}, {100, -5, +5}});
+    for (const auto& v0 : v0s) {
+      auto pos = v0.posTrack_as<Tracks>(); // positive daughter
+      auto ele = v0.negTrack_as<Tracks>(); // negative daughter
+      if (!checkV0leg(pos) || !checkV0leg(ele)) {
+        continue;
+      }
+      registry.fill(HIST("V0Counter"), 1);
+
+#define FILL(name)                                                        \
+  {                                                                       \
+    registry.fill(HIST(name "/TglTgl"), pos.tgl(), ele.tgl());            \
+    registry.fill(HIST(name "/PtPt"), pos.pt(), ele.pt());                \
+    registry.fill(HIST(name "/ZPosTglPos"), pos.z(), pos.tgl());          \
+    registry.fill(HIST(name "/ZEleTglPos"), ele.z(), pos.tgl());          \
+    registry.fill(HIST(name "/ZPosTglEle"), pos.z(), ele.tgl());          \
+    registry.fill(HIST(name "/ZEleTglEle"), ele.z(), ele.tgl());          \
+    registry.fill(HIST(name "/PtPosTglPos"), pos.pt(), pos.tgl());        \
+    registry.fill(HIST(name "/PtPosTglEle"), pos.pt(), ele.tgl());        \
+    registry.fill(HIST(name "/PtEleTglPos"), ele.pt(), pos.tgl());        \
+    registry.fill(HIST(name "/PtEleTglEle"), ele.pt(), ele.tgl());        \
+    registry.fill(HIST(name "/XX"), pos.x(), ele.x());                    \
+    registry.fill(HIST(name "/ZZ"), pos.z(), ele.z());                    \
+    registry.fill(HIST(name "/YY"), pos.y(), ele.y());                    \
+    registry.fill(HIST(name "/XY"), pos.x(), pos.y() - ele.y());          \
+    registry.fill(HIST(name "/XZ"), pos.x(), pos.z() - ele.z());          \
+    if (isAntiCorTgl(pos, ele)) {                                         \
+      registry.fill(HIST(name "/anti/TglTgl"), pos.tgl(), ele.tgl());     \
+      registry.fill(HIST(name "/anti/PtPt"), pos.pt(), ele.pt());         \
+      registry.fill(HIST(name "/anti/ZPosTglPos"), pos.z(), pos.tgl());   \
+      registry.fill(HIST(name "/anti/ZEleTglPos"), ele.z(), pos.tgl());   \
+      registry.fill(HIST(name "/anti/ZPosTglEle"), pos.z(), ele.tgl());   \
+      registry.fill(HIST(name "/anti/ZEleTglEle"), ele.z(), ele.tgl());   \
+      registry.fill(HIST(name "/anti/PtPosTglPos"), pos.pt(), pos.tgl()); \
+      registry.fill(HIST(name "/anti/PtPosTglEle"), pos.pt(), ele.tgl()); \
+      registry.fill(HIST(name "/anti/PtEleTglPos"), ele.pt(), pos.tgl()); \
+      registry.fill(HIST(name "/anti/PtEleTglEle"), ele.pt(), ele.tgl()); \
+      registry.fill(HIST(name "/anti/XX"), pos.x(), ele.x());             \
+      registry.fill(HIST(name "/anti/ZZ"), pos.z(), ele.z());             \
+      registry.fill(HIST(name "/anti/YY"), pos.y(), ele.y());             \
+      registry.fill(HIST(name "/anti/XY"), pos.x(), pos.y() - ele.y());   \
+      registry.fill(HIST(name "/anti/XZ"), pos.x(), pos.z() - ele.z());   \
+    }                                                                     \
+  }
+
+      if (isTPConly_TPConly(ele, pos)) {
+        FILL("TPConly_TPConly");
+      }
+      if (isITSonly_ITSonly(ele, pos)) {
+        FILL("ITSonly_ITSonly");
+      }
+      if (isTPConly_ITSonly(ele, pos)) {
+        FILL("TPConly_ITSonly");
+      }
+      if (isITSTPC_ITSTPC(ele, pos)) {
+        FILL("ITSTPC_ITSTPC");
+      }
+      if (isITSTPC_TPConly(ele, pos)) {
+        FILL("ITSTPC_TPConly");
+      }
+      if (isITSTPC_ITSonly(ele, pos)) {
+        FILL("ITSTPC_ITSonly");
+      }
     }
   }
+  PROCESS_SWITCH(CheckMCV0, processV0, "process reconstructed info", true);
 
+  void processDummy(aod::V0s const& v0s) {}
+  PROCESS_SWITCH(CheckMCV0, processDummy, "process dummy", false);
+
+  // Templates
   template <typename TTrack>
   bool checkV0leg(TTrack const& track)
   {
-    if (track.pt() < minpt || abs(track.eta()) > maxeta) {
+    if (track.pt() < minpt && track.pt() > maxpt) {
       return false;
     }
+    if (abs(track.eta()) > maxeta) {
+      return false;
+    }
+
     if (abs(track.dcaXY()) < dcamin || dcamax < abs(track.dcaXY())) {
       return false;
     }
     if (!track.hasITS() && !track.hasTPC()) {
+      return false;
+    }
+    if (track.tpcNClsCrossedRows() < minTPCNCls) {
       return false;
     }
     return true;
@@ -89,57 +224,62 @@ struct CheckMCV0 {
   template <typename TTrack>
   bool isITSTPCMatchedTrack(TTrack const& track)
   {
-    return track.hasITS() & track.hasTPC();
+    return track.hasITS() && track.hasTPC();
   }
 
   template <typename TTrack>
   bool isTPConlyTrack(TTrack const& track)
   {
-    return !track.hasITS() & track.hasTPC();
+    return !track.hasITS() && track.hasTPC();
   }
 
   template <typename TTrack>
   bool isITSonlyTrack(TTrack const& track)
   {
-    return track.hasITS() & !track.hasTPC();
+    return track.hasITS() && !track.hasTPC();
   }
 
-  // Preslice<aod::V0s> perCollision = aod::v0::collisionId;
-  void processV0(aod::Collisions const& collisions, aod::V0s const& v0s, MyTracks const& tracks)
+  template <typename TTrack>
+  bool isITSTPC_ITSTPC(TTrack const& track0, TTrack const& track1)
   {
-    for (auto& v0 : v0s) {
-      auto pos = v0.template posTrack_as<MyTracks>(); // positive daughter
-      auto ele = v0.template negTrack_as<MyTracks>(); // negative daughter
-      if (!checkV0leg(pos) || !checkV0leg(ele)) {
-        continue;
-      }
-      registry.fill(HIST("V0Counter"), 1);
-
-      if (isITSTPCMatchedTrack(pos) && isITSTPCMatchedTrack(ele)) {
-        registry.fill(HIST("ITSTPC_ITSTPC/h2TglTgl"), pos.tgl(), ele.tgl());
-      }
-      if ((isITSTPCMatchedTrack(pos) && isTPConlyTrack(ele)) || (isITSTPCMatchedTrack(ele) && isTPConlyTrack(pos))) {
-        registry.fill(HIST("ITSTPC_TPConly/h2TglTgl"), pos.tgl(), ele.tgl());
-      }
-      if ((isITSTPCMatchedTrack(pos) && isITSonlyTrack(ele)) || (isITSTPCMatchedTrack(ele) && isITSonlyTrack(pos))) {
-        registry.fill(HIST("ITSTPC_ITSonly/h2TglTgl"), pos.tgl(), ele.tgl());
-      }
-      if (isTPConlyTrack(pos) && isTPConlyTrack(ele)) {
-        registry.fill(HIST("TPConly_TPConly/h2TglTgl"), pos.tgl(), ele.tgl());
-      }
-      if (isITSonlyTrack(pos) && isITSonlyTrack(ele)) {
-        registry.fill(HIST("ITSonly_ITSonly/h2TglTgl"), pos.tgl(), ele.tgl());
-      }
-      if ((isTPConlyTrack(pos) && isITSonlyTrack(ele)) || (isTPConlyTrack(ele) && isITSonlyTrack(pos))) {
-        registry.fill(HIST("TPConly_ITSonly/h2TglTgl"), pos.tgl(), ele.tgl());
-      }
-
-    } // end of v0 loop
+    return isITSTPCMatchedTrack(track0) && isITSTPCMatchedTrack(track1);
   }
-  PROCESS_SWITCH(CheckMCV0, processV0, "process reconstructed info", true);
 
-  void processDummy(aod::V0s const& v0s) {}
-  PROCESS_SWITCH(CheckMCV0, processDummy, "process dummy", false);
+  template <typename TTrack>
+  bool isITSTPC_TPConly(TTrack const& track0, TTrack const& track1)
+  {
+    return (isITSTPCMatchedTrack(track0) && isTPConlyTrack(track1)) || (isITSTPCMatchedTrack(track1) && isTPConlyTrack(track0));
+  }
+
+  template <typename TTrack>
+  bool isITSTPC_ITSonly(TTrack const& track0, TTrack const& track1)
+  {
+    return (isITSTPCMatchedTrack(track0) && isITSonlyTrack(track1)) || (isITSTPCMatchedTrack(track1) && isITSonlyTrack(track0));
+  }
+
+  template <typename TTrack>
+  bool isTPConly_TPConly(TTrack const& track0, TTrack const& track1)
+  {
+    return isTPConlyTrack(track0) && isTPConlyTrack(track1);
+  }
+
+  template <typename TTrack>
+  bool isITSonly_ITSonly(TTrack const& track0, TTrack const& track1)
+  {
+    return isITSonlyTrack(track0) && isITSonlyTrack(track1);
+  }
+
+  template <typename TTrack>
+  bool isTPConly_ITSonly(TTrack const& track0, TTrack const& track1)
+  {
+    return (isTPConlyTrack(track0) && isITSonlyTrack(track1)) || (isTPConlyTrack(track1) && isITSonlyTrack(track0));
+  }
+
+  template <typename TTrack>
+  bool isAntiCorTgl(TTrack const& track0, TTrack const& track1)
+  {
+    return (track0.tgl() < 0.0 && track1.tgl() > 0.0) || (track1.tgl() < 0.0 && track0.tgl() > 0.0);
+  }
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
