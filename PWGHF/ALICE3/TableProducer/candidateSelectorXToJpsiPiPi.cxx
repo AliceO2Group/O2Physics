ab8790c693465b9cffb05284f536b232d6ad3b9d
Vít Kučera <vit.kucera@cern.ch>
2023-09-12 21:51:46 +0200

    Do not use TMath

--- PWGHF/ALICE3/TableProducer/candidateSelectorXToJpsiPiPi.cxx
+++ PWGHF/ALICE3/TableProducer/candidateSelectorXToJpsiPiPi.cxx
@@ -83,7 +83,7 @@ struct HfCandidateSelectorXToJpsiPiPi {
     }
 
     // TODO: replace hardcoded mass with "RecoDecay::getMassPDG(9920443)"
-    if (TMath::Abs(invMassXToJpsiPiPi(hfCandX) - 3.87168) > cuts->get(pTBin, "m")) {
+    if (std::abs(invMassXToJpsiPiPi(hfCandX) - 3.87168) > cuts->get(pTBin, "m")) {
       // Printf("X topol selection failed at mass diff check");
       return false; // check that mass difference is within bounds
     }
@@ -97,9 +97,9 @@ struct HfCandidateSelectorXToJpsiPiPi {
       return false; // CPA check
     }
 
-    if ((TMath::Abs(hfCandX.impactParameter0()) > cuts->get(pTBin, "d0 Jpsi")) ||
-        (TMath::Abs(hfCandX.impactParameter1()) > cuts->get(pTBin, "d0 Pi")) ||
-        (TMath::Abs(hfCandX.impactParameter2()) > cuts->get(pTBin, "d0 Pi"))) {
+    if ((std::abs(hfCandX.impactParameter0()) > cuts->get(pTBin, "d0 Jpsi")) ||
+        (std::abs(hfCandX.impactParameter1()) > cuts->get(pTBin, "d0 Pi")) ||
+        (std::abs(hfCandX.impactParameter2()) > cuts->get(pTBin, "d0 Pi"))) {
       return false; // DCA check on daughters
     }
 
@@ -115,7 +115,7 @@ struct HfCandidateSelectorXToJpsiPiPi {
   template <typename T>
   bool validTPCPID(const T& track)
   {
-    if (TMath::Abs(track.pt()) < ptPidTpcMin || TMath::Abs(track.pt()) >= ptPidTpcMax) {
+    if (std::abs(track.pt()) < ptPidTpcMin || std::abs(track.pt()) >= ptPidTpcMax) {
       return false;
     }
     // if (track.TPCNClsFindable() < TPCNClsFindableMin) return false;
@@ -129,7 +129,7 @@ struct HfCandidateSelectorXToJpsiPiPi {
   template <typename T>
   bool validTofPid(const T& track)
   {
-    if (TMath::Abs(track.pt()) < ptPidTofMin || TMath::Abs(track.pt()) >= ptPidTofMax) {
+    if (std::abs(track.pt()) < ptPidTofMin || std::abs(track.pt()) >= ptPidTofMax) {
       return false;
     }
     return true;
