b2cb426d1975f53bcb7cd1cb34e96f8a62aa1b6c
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2023-09-20 19:26:19 +0200

    Remove GRPO query (#3486)
    Co-authored-by: David Dobrigkeit Chinellato <david.dobrigkeit.chinellato.cern.ch>

--- PWGLF/TableProducer/lambdakzerobuilder.cxx
+++ PWGLF/TableProducer/lambdakzerobuilder.cxx
@@ -129,6 +129,7 @@ struct lambdakzeroBuilder {
   Configurable<std::string> lutPath{"lutPath", "GLO/Param/MatLUT", "Path of the Lut parametrization"};
   Configurable<std::string> geoPath{"geoPath", "GLO/Config/GeometryAligned", "Path of the geometry file"};
   Configurable<std::string> mVtxPath{"mVtxPath", "GLO/Calib/MeanVertex", "Path of the mean vertex file"};
+  Configurable<bool> skipGRPOquery{"skipGRPOquery", true, "skip grpo query"};
 
   // generate and fill extra QA histograms is requested
   Configurable<bool> d_doQA{"d_doQA", false, "Do basic QA"};
@@ -446,8 +447,10 @@ struct lambdakzeroBuilder {
     }
 
     auto run3grp_timestamp = bc.timestamp();
-    o2::parameters::GRPObject* grpo = ccdb->getForTimeStamp<o2::parameters::GRPObject>(grpPath, run3grp_timestamp);
+    o2::parameters::GRPObject* grpo = 0x0;
     o2::parameters::GRPMagField* grpmag = 0x0;
+    if (!skipGRPOquery)
+      grpo = ccdb->getForTimeStamp<o2::parameters::GRPObject>(grpPath, run3grp_timestamp);
     if (grpo) {
       o2::base::Propagator::initFieldFromGRP(grpo);
       // Fetch magnetic field from ccdb for current collision
