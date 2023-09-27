09b741ecb31fbaa2ec771e6614e987f35e764346
aimeric-landou <46970521+aimeric-landou@users.noreply.github.com>
2023-09-27 16:38:40 +0200

    PWGJE: jetmatching - fix split collisions handling (#3516)

--- PWGJE/TableProducer/jetmatching.cxx
+++ PWGJE/TableProducer/jetmatching.cxx
@@ -416,14 +416,8 @@ struct JetMatching {
           fillJetIdArraysTagToBase(jetsBasePerColl, jetsTagPerColl, tagToBaseGeo, tagToBasePt, tagToBaseHF);
         }
         if (i_collision > 1) { // collision is split
-          for (std::size_t colli = 1; colli < collisionsPerMcColl.size(); colli++) {
-            if (jetsBaseIsMC && !jetsTagIsMC) { // (if both are MC, allowing this line below to happen would make duplicates in the matching table)
-              fillJetIdArraysBaseToTag(jetsBasePerColl, jetsTagPerColl, baseToTagGeo, baseToTagPt, baseToTagHF);
-            }
-            if (jetsTagIsMC && !jetsBaseIsMC) { // (if both are MC, allowing this line below to happen would make duplicates in the matching table)
-              fillJetIdArraysTagToBase(jetsBasePerColl, jetsTagPerColl, tagToBaseGeo, tagToBasePt, tagToBaseHF);
-            }
-            if (!jetsBaseIsMC && !jetsTagIsMC) {
+          if (!jetsBaseIsMC || !jetsTagIsMC) { // if both are MC, allowing this FOR loop below to happen would make duplicates in the matching table; this is why thie i_collision>1 case is not treated the same way as the i_collision==1 case
+            for (std::size_t colli = 1; colli < collisionsPerMcColl.size(); colli++) {
               fillJetIdArraysBaseToTag(jetsBasePerColl, jetsTagPerColl, baseToTagGeo, baseToTagPt, baseToTagHF);
               fillJetIdArraysTagToBase(jetsBasePerColl, jetsTagPerColl, tagToBaseGeo, tagToBasePt, tagToBaseHF);
             }
