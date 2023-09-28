b1342fdb7e25a1b1179c31983f6a54524d33409b
Ravindra Singh <ravindra.singh@cern.ch>
2023-09-28 21:15:46 +0530

    PWGHF Hfe_h correlation

--- PWGHF/HFC/Tasks/taskCorrelationHfeHadrons.cxx
+++ PWGHF/HFC/Tasks/taskCorrelationHfeHadrons.cxx
@@ -15,6 +15,7 @@
 
 #include "THnSparse.h"
 
+#include "DataFormatsEMCAL/AnalysisCluster.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/HistogramRegistry.h"
 #include "Framework/runDataProcessing.h"
@@ -22,6 +23,7 @@
 #include "Common/Core/PID/TPCPIDResponse.h"
 #include "Common/Core/RecoDecay.h"
 #include "Common/Core/TrackSelection.h"
+
 #include "Common/DataModel/Centrality.h"
 #include "Common/DataModel/EventSelection.h"
 #include "Common/DataModel/Multiplicity.h"
@@ -29,57 +31,76 @@
 #include "Common/DataModel/TrackSelectionTables.h"
 
 #include "PWGJE/DataModel/EMCALClusters.h"
-#include "DataFormatsEMCAL/AnalysisCluster.h"
+
 
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 using namespace o2::soa;
 
-using aodCollisions = soa::Filtered<soa::Join<aod::Collisions, aod::Mults, aod::EvSels>>::iterator;
-using GTrks = o2::soa::Join<o2::aod::Tracks, o2::aod::TracksExtra, o2::aod::pidTPCFullEl, o2::aod::TracksDCA, o2::aod::TrackSelection, o2::aod::TrackSelectionExtension>;
-using GTrk = GTrks::iterator;
-
 struct HfTaskCorrelationHfeHadrons {
-  Configurable<bool> isRun3{"isRun3", true, "Data is from Run3 or Run2"};
+  // Configurables
+
+  // Cluster information
   Configurable<bool> clusterInfo{"clusterInfo", true, "EMCal cluster info before and after track match"};
+  // Run3 selection
+  Configurable<bool> isRun3{"isRun3", true, "Data is from Run3 or Run2"};
+  // Electron hadron correlation condition
+  Configurable<bool> pTCondition{"pTCondition", true, "Electron pT should be greater than Associate particle pT"};
+
+  // Deltaphi binning
   Configurable<int> delPhiBins{"delPhiBins", 32, "Bins for #Delta#varphi bins"};
-  Configurable<float> etaTrackMin{"etaTrackMin", -0.6f, "Eta range for electron tracks"};
-  Configurable<float> etaTrackDcalRightMin{"etaTrackDcalRightMin", 0.22f, "Eta range for electron tracks"};
-  Configurable<float> etaTrackDcalLeftMin{"etaTrackDcalLeftMin", -0.6f, "Eta range for electron tracks"};
-  Configurable<float> ptTrackMin{"ptTrackMin", 2.0f, "Transverse MOmentum range for electron tracks"};
-  Configurable<float> phiTrackEMcalMax{"phiTrackEMcalMax", 5.708f, "phi range for electron tracks associated Emcal"};
-  Configurable<float> phiTrackEMcalMin{"phiTrackEMcalMin", 4.5355f, "phi range for electron tracks associated Emcal"};
-  Configurable<float> phiTrackDCalMax{"phiTrackDCalMax", 3.3621f, "phi range for electron tracks associated Dcal"};
-  Configurable<float> phiTrackDcalMin{"phiTrackDcalMin", 1.3955f, "phi range for electron tracks associated Dcal"};
-  Configurable<float> etaTrackMax{"etaTrackMax", 0.6f, "Eta range for electron tracks"};
-  Configurable<float> etaTrackDcalRightMax{"etaTrackDcalRightMax", 0.6f, "Eta range for electron Dcal tracks"};
-  Configurable<float> etaTrackDcalLeftMax{"etaTrackDcalLeftMax", -0.22f, "Eta range for electron Dcal tracks"};
-  Configurable<float> eopElectronMin{"eopElectronMin", 0.8f, "Minimum E/p for electron tracks"};
-  Configurable<float> eopElectronMax{"eopElectronMax", 1.2f, "Maximum E/p for electron tracks"};
+
+  // Track selection
   Configurable<float> dcaXYTrackMax{"dcaXYTrackMax", 0.5f, "DCA XY cut"};
   Configurable<float> dcaZTrackMax{"dcaZTrackMax", 1.0f, "DCA Z cut"};
-  Configurable<float> tpcnSigmaElectronMin{"tpcnSigmaElectronMin", -1.0f, "min Electron TPCnsigma"};
-  Configurable<float> tpcnSigmaElectronMax{"tpcnSigmaElectronMax", 3.0f, "max Electron TPCnsigma"};
-  Configurable<float> m02ElectronMin{"m02ElectronMin", 0.02f, "min Electron M02"};
-  Configurable<float> m02ElectronMax{"m02ElectronMax", 0.9f, "max Electron M02"};
-  Configurable<float> m20ElectronMin{"m20ElectronMin", 0.0f, "min Electron M20"};
-  Configurable<float> m20ElectronMax{"m20ElectronMax", 1000.f, "max Electron M20"};
-  Configurable<float> ptAssociatedMin{"ptAssociatedMin", 0.2f, "Minimum pT for associated track"};
-  Configurable<float> etaAssociatedTrack{"etaAssociatedTrack", 0.8f, "Eta range for Associated or partner electron tracks"};
-  Configurable<float> deltaPhiMatchMin{"deltaPhiMatchMin", 0.01f, "Phi distance of EMCAL cluster to its closest track"};
+  Configurable<float> etaTrackMax{"etaTrackMax", 0.6f, "Eta range for electron tracks"};
+  Configurable<float> etaTrackMin{"etaTrackMin", -0.6f, "Eta range for electron tracks"};
+  Configurable<float> ptTrackMin{"ptTrackMin", 2.0f, "Transverse MOmentum range for electron tracks"};
+
+  // EMcal and Dcal selection cut
+  Configurable<float> etaTrackDcalLeftMax{"etaTrackDcalLeftMax", -0.22f, "Eta range for electron Dcal tracks"};
+  Configurable<float> etaTrackDcalLeftMin{"etaTrackDcalLeftMin", -0.6f, "Eta range for electron tracks"};
+  Configurable<float> etaTrackDcalRightMax{"etaTrackDcalRightMax", 0.6f, "Eta range for electron Dcal tracks"};
+  Configurable<float> etaTrackDcalRightMin{"etaTrackDcalRightMin", 0.22f, "Eta range for electron tracks"};
+  Configurable<float> phiTrackDCalMax{"phiTrackDCalMax", 3.3621f, "phi range for electron tracks associated Dcal"};
+  Configurable<float> phiTrackDcalMin{"phiTrackDcalMin", 1.3955f, "phi range for electron tracks associated Dcal"};
+  Configurable<float> phiTrackEMcalMax{"phiTrackEMcalMax", 5.708f, "phi range for electron tracks associated Emcal"};
+  Configurable<float> phiTrackEMcalMin{"phiTrackEMcalMin", 4.5355f, "phi range for electron tracks associated Emcal"};
+
+  // Track and Cluster matching cut
   Configurable<float> deltaEtaMatchMin{"deltaEtaMatchMin", 0.01f, "Eta distance of EMCAL cluster to its closest track"};
   Configurable<float> timeEMCClsCut{"timeEMCClsCut", 100.f, "Cluster time"};
-  Configurable<bool> pTCondition{"pTCondition", true, "Electron pT should be greater than Associate particle pT"};
+
+  // Inclusive electron selection cut
+  Configurable<float> eopElectronMin{"eopElectronMin", 0.8f, "Minimum E/p for electron tracks"};
+  Configurable<float> eopElectronMax{"eopElectronMax", 1.2f, "Maximum E/p for electron tracks"};
+  Configurable<float> m02ElectronMax{"m02ElectronMax", 0.9f, "max Electron M02"};
+  Configurable<float> m02ElectronMin{"m02ElectronMin", 0.02f, "min Electron M02"};
+  Configurable<float> m20ElectronMax{"m20ElectronMax", 1000.f, "max Electron M20"};
+  Configurable<float> m20ElectronMin{"m20ElectronMin", 0.0f, "min Electron M20"};
+  Configurable<float> tpcnSigmaElectronMin{"tpcnSigmaElectronMin", -1.0f, "min Electron TPCnsigma"};
+  Configurable<float> tpcnSigmaElectronMax{"tpcnSigmaElectronMax", 3.0f, "max Electron TPCnsigma"};
+
+  // Hadron selection cut
+  Configurable<float> deltaPhiMatchMin{"deltaPhiMatchMin", 0.01f, "Phi distance of EMCAL cluster to its closest track"};
+  Configurable<float> etaAssociatedTrack{"etaAssociatedTrack", 0.8f, "Eta range for Associated or partner electron tracks"};
+  Configurable<float> ptAssociatedMin{"ptAssociatedMin", 0.2f, "Minimum pT for associated track"};
+
+  using aodCollisions = soa::Filtered<soa::Join<aod::Collisions, aod::Mults, aod::EvSels>>::iterator;
+  using GTrks = o2::soa::Join<o2::aod::Tracks, o2::aod::TracksExtra, o2::aod::pidTPCFullEl, o2::aod::TracksDCA, o2::aod::TrackSelection, o2::aod::TrackSelectionExtension>;
+  using GTrk = GTrks::iterator;
+
+  Filter CollisionFilter = nabs(aod::collision::posZ) < 10.f && aod::collision::numContrib > (uint16_t)1;
 
   PresliceUnsorted<o2::aod::EMCALMatchedTracks> perClusterMatchedTracks = o2::aod::emcalmatchedtrack::trackId;
 
-  HistogramConfigSpec hCorrelSpec{HistType::kTHnSparseD, {{30, 0., 30.}, {20, 0., 20.}, {delPhiBins, -o2::constants::math::PIHalf, 3. * o2::constants::math::PIHalf}, {50, -1.8, 1.8}}};
-  HistogramConfigSpec hTrackInfoSpec{HistType::kTHnSparseD, {{500, 0, 160}, {300, -15, 15}, {500, 0., 50.}, {500, 0., 50.}, {100, -1.5, 1.5}, {100, 0, 7}}};
-  HistogramConfigSpec hTrackAllInfoSpec{HistType::kTHnSparseD, {{500, 0, 160}, {300, -15, 15}, {500, 0., 50.}, {500, 0., 50.}, {100, -1.5, 1.5}, {100, 0, 7}, {2000, -1, 1}, {2000, -1, 1}, {3, 0, 3}}};
   HistogramConfigSpec hClusterInfoSpec{HistType::kTHnSparseD, {{300, 0.0, 30.0}, {100, -0.9, 0.9}, {200, 0, 6.3}, {50, 0, 50}, {1800, -900, 900}}};
-  HistogramConfigSpec hPIDSpec{HistType::kTHnSparseD, {{500, 0.0, 50.0}, {500, 0., 50.}, {300, -15, 15}, {300, 0.0, 30.0}, {400, 0, 2}, {400, 0, 2}}};
+  HistogramConfigSpec hCorrelSpec{HistType::kTHnSparseD, {{30, 0., 30.}, {20, 0., 20.}, {delPhiBins, -o2::constants::math::PIHalf, 3. * o2::constants::math::PIHalf}, {50, -1.8, 1.8}}};
   HistogramConfigSpec hDphiDetaClusterTrackSpec{HistType::kTH3F, {{400, -0.2, 0.2}, {400, -0.2, 0.2}, {280, 0, 70}}};
+  HistogramConfigSpec hPIDSpec{HistType::kTHnSparseD, {{500, 0.0, 50.0}, {500, 0., 50.}, {300, -15, 15}, {300, 0.0, 30.0}, {400, 0, 2}, {400, 0, 2}}};
+  HistogramConfigSpec hTrackAllInfoSpec{HistType::kTHnSparseD, {{500, 0, 160}, {300, -15, 15}, {500, 0., 50.}, {500, 0., 50.}, {100, -1.5, 1.5}, {100, 0, 7}, {2000, -1, 1}, {2000, -1, 1}, {3, 0, 3}}};
+  HistogramConfigSpec hTrackInfoSpec{HistType::kTHnSparseD, {{500, 0, 160}, {300, -15, 15}, {500, 0., 50.}, {500, 0., 50.}, {100, -1.5, 1.5}, {100, 0, 7}}};
 
   HistogramRegistry registry{
     "registry",
@@ -112,11 +133,11 @@ struct HfTaskCorrelationHfeHadrons {
 
   void correlation(GTrk const& eTrack, GTrks const& assotrks, int sparseNo = -1)
   {
-    if (sparseNo == -1) {
+    if (sparseNo < 0 || sparseNo > 1) {
       LOGF(info, "Error: pass sparse value from '0' to 'N'");
       return;
     }
-    std::shared_ptr<THnSparse> hEHCorrArray[4] = {
+    std::shared_ptr<THnSparse> hEHCorrArray[2] = {
       registry.get<THnSparse>(HIST("hHHCorrel")),
       registry.get<THnSparse>(HIST("hInclusiveEHCorrel"))};
 
@@ -164,7 +185,6 @@ struct HfTaskCorrelationHfeHadrons {
     }
   }
 
-  Filter CollisionFilter = nabs(aod::collision::posZ) < 10.f && aod::collision::numContrib > (uint16_t)1;
 
   void process(aodCollisions const& collision, aod::EMCALClusters const& mAnalysisClusters, o2::aod::EMCALMatchedTracks const& MatchedTracks, GTrks const& tracks)
   {
