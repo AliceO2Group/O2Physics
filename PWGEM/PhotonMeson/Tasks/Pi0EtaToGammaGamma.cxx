0649d9b9062bc805de88a5efa2c595545066b486
Daiki Sekihata <daiki.sekihata@cern.ch>
2023-10-09 21:50:54 +0200

    PWGEM/PhotonMeson: remove unnecessary cuts and change default values for cut names (#3574)

--- PWGEM/PhotonMeson/Tasks/Pi0EtaToGammaGamma.cxx
+++ PWGEM/PhotonMeson/Tasks/Pi0EtaToGammaGamma.cxx
@@ -60,7 +60,7 @@ struct Pi0EtaToGammaGamma {
 
   Configurable<float> maxY{"maxY", 0.9, "maximum rapidity for reconstructed particles"};
   Configurable<std::string> fConfigPCMCuts{"cfgPCMCuts", "analysis,qc,nocut", "Comma separated list of V0 photon cuts"};
-  Configurable<std::string> fConfigDalitzEECuts{"cfgDalitzEECuts", "pi0ee_tpchadrejortofreq,etaee_tpchadrejortofreq,nocut", "Comma separated list of Dalitz ee cuts"};
+  Configurable<std::string> fConfigDalitzEECuts{"cfgDalitzEECuts", "mee_0_120_tpchadrejortofreq_lowB,mee_120_500_tpchadrejortofreq_lowB,mee_0_500_tpchadrejortofreq_lowB", "Comma separated list of Dalitz ee cuts"};
   Configurable<std::string> fConfigPHOSCuts{"cfgPHOSCuts", "test02,test03", "Comma separated list of PHOS photon cuts"};
   Configurable<std::string> fConfigPairCuts{"cfgPairCuts", "nocut,asym08", "Comma separated list of pair cuts"};
 
