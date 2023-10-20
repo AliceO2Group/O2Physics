a6eaceb36a82df23610d6db8f16fec0dfd81b193
Arvind Khuntia <arvind.khuntia@cern.ch>
2023-10-20 23:13:55 +0200

    Comments from Anton are implemented

--- PWGMM/Lumi/Tasks/fitLumi.cxx
+++ PWGMM/Lumi/Tasks/fitLumi.cxx
@@ -9,6 +9,8 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
+// author: arvind.khuntia@cern.ch
+
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
@@ -24,9 +26,8 @@
 #include "DataFormatsParameters/GRPLHCIFData.h"
 #include "TH1F.h"
 #include "TH2F.h"
-#include <iostream>
 
-// author: arvind.khuntia@cern.ch
+
 
 using namespace o2;
 using namespace o2::framework;
@@ -41,13 +42,13 @@ struct fitLumi {
   HistogramRegistry registry;
   int nBins, nCollBins;
   int relTS;
-  Configurable<ULong64_t> startTimeInS{"startTime", 1668079200, "unix start time"};
-  Configurable<ULong64_t> endTimeInS{"endTime", 1668098700, "unix end time"};
+  Configurable<uint64_t> startTimeInS{"startTime", 1668079200, "unix start time"};
+  Configurable<uint64_t> endTimeInS{"endTime", 1668098700, "unix end time"};
   Configurable<std::vector<int>> collBCArray{"collBCArray", {1022, 1062, 1102, 1142, 2015, 2055, 2161, 2201, 2241, 2281, 2321, 2361, 2401, 2441, 2481, 2521, 2561, 2601, 2641, 2681}, "Colliding BC for the VdM"};
 
   void init(InitContext&)
   {
-    nBins = ULong64_t(endTimeInS - startTimeInS) / 2.;
+    nBins = uint64_t(endTimeInS - startTimeInS) / 2.;
     nCollBins = collBCArray->size();
     // Hist for FT0
     registry.add("FT0/VtxTrig", "vertex trigger;ts (s); Counts", {HistType::kTH1F, {{nBins, 0., static_cast<double>(endTimeInS - startTimeInS)}}});
@@ -65,16 +66,18 @@ struct fitLumi {
     if (nTF < 1) {
       for (int iBin = 0; iBin < nCollBins; iBin++) {
         registry.get<TH1>(HIST("FT0/CollBCMap"))->SetBinContent(iBin + 1, collBCArray->at(iBin));
-        std::cout << "bin " << iBin << " value " << collBCArray->at(iBin) << std::endl;
+        LOG(debug) << "bin " << iBin << " value " << collBCArray->at(iBin);
       }
     }
 
     for (auto const& ft0 : ft0s) {
       auto bc = ft0.bc_as<BCsWithTimestamps>();
       if (!bc.timestamp())
-        continue;
+      {
+          continue;
+      }
       std::bitset<8> fT0Triggers = ft0.triggerMask();
-      [[maybe_unused]] auto localBC = bc.globalBC() % nBCsPerOrbit;
+      auto localBC = bc.globalBC() % nBCsPerOrbit;
       int index = FIND_INDEX(collBCArray, localBC);
       bool vertex = fT0Triggers[o2::fdd::Triggers::bitVertex];
       auto tsInSecond = ((bc.timestamp() * 1.e-3) - startTimeInS); // covert ts from ms to second
@@ -89,9 +92,11 @@ struct fitLumi {
     for (auto const& fdd : fdds) {
       auto bc = fdd.bc_as<BCsWithTimestamps>();
       if (!bc.timestamp())
-        continue;
+      {
+          continue;
+      }
       std::bitset<8> fddTriggers = fdd.triggerMask();
-      [[maybe_unused]] auto localBC = bc.globalBC() % nBCsPerOrbit;
+      auto localBC = bc.globalBC() % nBCsPerOrbit;
       int index = FIND_INDEX(collBCArray, localBC);
       bool vertex = fddTriggers[o2::fdd::Triggers::bitVertex];
       auto tsInSecond = ((bc.timestamp() * 1.e-3) - startTimeInS); // covert ts from ms to second
