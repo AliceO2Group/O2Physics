a480b6235d0b580bad8c3df0688a0b8fd8006eb8
creetz16 <carolina@reetz.name>
2023-10-24 14:04:52 +0200

    Add KF casc labels

--- PWGLF/TableProducer/cascadelabelbuilder.cxx
+++ PWGLF/TableProducer/cascadelabelbuilder.cxx
@@ -46,6 +46,7 @@ using LabeledTracks = soa::Join<aod::Tracks, aod::McTrackLabels>;
 //*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*
 struct cascadeLabelBuilder {
   Produces<aod::McCascLabels> casclabels; // MC labels for cascades
+  Produces<aod::McKFCascLabels> kfcasclabels; // MC labels for cascades
   Produces<aod::McTraCascLabels> tracasclabels; // MC labels for cascades
   Produces<aod::McCascBBTags> bbtags;           // MC labels for cascades
   void init(InitContext const&) {}
@@ -97,6 +98,52 @@ struct cascadeLabelBuilder {
       }     // end association check
       // Construct label table (note: this will be joinable with CascDatas)
       casclabels(
+        lLabel);
+    } // end casctable loop
+  }
+
+  //*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*
+  // build kf cascade labels
+  void processKFCascades(aod::KFCascDatas const& casctable, aod::V0sLinked const&, aod::McTrackLabels const&, aod::McParticles const&)
+  {
+    for (auto& casc : casctable) {
+
+      auto v0 = casc.v0_as<o2::aod::V0sLinked>();
+      int lLabel = -1;
+
+      // Acquire all three daughter tracks, please
+      auto lBachTrack = casc.bachelor_as<aod::McTrackLabels>();
+      auto lNegTrack = v0.negTrack_as<aod::McTrackLabels>();
+      auto lPosTrack = v0.posTrack_as<aod::McTrackLabels>();
+
+      // Association check
+      // There might be smarter ways of doing this in the future
+      if (lNegTrack.has_mcParticle() && lPosTrack.has_mcParticle() && lBachTrack.has_mcParticle()) {
+        auto lMCBachTrack = lBachTrack.mcParticle_as<aod::McParticles>();
+        auto lMCNegTrack = lNegTrack.mcParticle_as<aod::McParticles>();
+        auto lMCPosTrack = lPosTrack.mcParticle_as<aod::McParticles>();
+
+        // Step 1: check if the mother is the same, go up a level
+        if (lMCNegTrack.has_mothers() && lMCPosTrack.has_mothers()) {
+          for (auto& lNegMother : lMCNegTrack.mothers_as<aod::McParticles>()) {
+            for (auto& lPosMother : lMCPosTrack.mothers_as<aod::McParticles>()) {
+              if (lNegMother == lPosMother) {
+                // if we got to this level, it means the mother particle exists and is the same
+                // now we have to go one level up and compare to the bachelor mother too
+                for (auto& lV0Mother : lNegMother.mothers_as<aod::McParticles>()) {
+                  for (auto& lBachMother : lMCBachTrack.mothers_as<aod::McParticles>()) {
+                    if (lV0Mother == lBachMother) {
+                      lLabel = lV0Mother.globalIndex();
+                    }
+                  }
+                } // end conditional V0-bach pair
+              }   // end neg = pos mother conditional
+            }
+          } // end loop neg/pos mothers
+        }   // end conditional of mothers existing
+      }     // end association check
+      // Construct label table (note: this will be joinable with CascDatas)
+      kfcasclabels(
         lLabel);
     } // end casctable loop
   }
