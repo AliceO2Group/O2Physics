d88877a1414b28f9f9dfaa7aca5f82f749fee102
Sebastian Scheid <s.scheid@cern.ch>
2023-10-25 08:53:07 +0200

    ALICE3: Templated functions for pid

--- ALICE3/Tasks/alice3-dilepton.cxx
+++ ALICE3/Tasks/alice3-dilepton.cxx
@@ -618,7 +618,8 @@ struct Alice3Dilepton {
     } // end of collision loop
   }   // end of processRec
 
-  bool electronIDTOF()
+  template <typename TTrack>
+  bool electronIDTOF(TTrack const& track)
   {
     bool isElectron = false;
     bool isEleOuterTOF = std::abs(track.nSigmaElectronOuterTOF()) < nSigmaEleCutOuterTOF;
@@ -631,7 +632,8 @@ struct Alice3Dilepton {
     return isElectron;
   }
 
-  bool electronIDRICH()
+  template <typename TTrack>
+  bool electronIDRICH(TTrack const& track)
   {
     bool isElectron = false;
     bool isEleRICH = std::abs(track.nSigmaElectronOuterTOF()) < nSigmaElectronRich;
