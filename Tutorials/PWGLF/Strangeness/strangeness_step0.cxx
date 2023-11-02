2837b08a89b40d76226b7ea8e431476605cc6993
nepeivodaRS <roman.nepeivoda@hep.lu.se>
2023-11-02 14:26:10 +0100

    StrangenessTutorialUpdate
    Update of the Strangeness Tutorial

--- Tutorials/PWGLF/Strangeness/strangeness_step0.cxx
+++ Tutorials/PWGLF/Strangeness/strangeness_step0.cxx
@@ -10,8 +10,8 @@
 // or submit itself to any jurisdiction.
 ///
 /// \brief this is a starting point for the Strangeness tutorial
-/// \author
-/// \since 12/05/2023
+/// \author Nepeivoda Roman (roman.nepeivoda@cern.ch)
+/// \author Chiara De Martin (chiara.de.martin@cern.ch)
 
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
@@ -24,30 +24,51 @@ using namespace o2::framework::expressions;
 
 // STEP 0
 // Starting point: loop over all V0s and fill invariant mass histogram
-struct strangeness_tutorial {
 
-  // Configurable for number of bins
+struct strangeness_tutorial {
+  // Histograms are defined with HistogramRegistry
+  HistogramRegistry rEventSelection{"eventSelection", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
+  HistogramRegistry rKzeroShort{"kzeroShort", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
+
+  // Configurable for histograms
   Configurable<int> nBins{"nBins", 100, "N bins in all histos"};
 
-  // histogram defined with HistogramRegistry
-  HistogramRegistry registry{"registry",
-                             {{"hVertexZ", "hVertexZ", {HistType::kTH1F, {{nBins, -15., 15.}}}},
-                              {"hMassK0Short", "hMassK0Short", {HistType::kTH1F, {{200, 0.45f, 0.55f}}}}}};
+  // Configurable for event selection
+  Configurable<float> cutzvertex{"cutzvertex", 10.0f, "Accepted z-vertex range (cm)"};
+
+  void init(InitContext const&)
+  {
+    // Axes
+    AxisSpec K0ShortMassAxis = {200, 0.45f, 0.55f, "#it{M}_{inv} [GeV/#it{c}^{2}]"};
+    AxisSpec vertexZAxis = {nBins, -15., 15., "vrtx_{Z} [cm]"};
+
+    // Histograms
+    // Event selection
+    rEventSelection.add("hVertexZRec", "hVertexZRec", {HistType::kTH1F, {vertexZAxis}});
+
+    // K0s reconstruction
+    rKzeroShort.add("hMassK0Short", "hMassK0Short", {HistType::kTH1F, {K0ShortMassAxis}});
+  }
 
   // Defining filters for events (event selection)
-  // Processed events will be already fulfulling the event selection requirements
+  // Processed events will be already fulfilling the event selection requirements
   Filter eventFilter = (o2::aod::evsel::sel8 == true);
+  Filter posZFilter = (nabs(o2::aod::collision::posZ) < cutzvertex);
 
   void process(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& collision,
                aod::V0Datas const& V0s)
   {
     // Fill the event counter
-    registry.fill(HIST("hVertexZ"), collision.posZ());
+    rEventSelection.fill(HIST("hVertexZRec"), collision.posZ());
 
-    for (auto& v0 : V0s) {
-      registry.fill(HIST("hMassK0Short"), v0.mK0Short());
+    for (const auto& v0 : V0s) {
+      rKzeroShort.fill(HIST("hMassK0Short"), v0.mK0Short());
     }
   }
 };
 
-WorkflowSpec defineDataProcessing(ConfigContext const& cfgc) { return WorkflowSpec{adaptAnalysisTask<strangeness_tutorial>(cfgc)}; }
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc) 
+{ 
+  return WorkflowSpec{
+    adaptAnalysisTask<strangeness_tutorial>(cfgc)}; 
+}
--- Tutorials/PWGLF/Strangeness/strangeness_step3.cxx
+++ Tutorials/PWGLF/Strangeness/strangeness_step3.cxx
@@ -9,11 +9,9 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 ///
-/// \brief this is a starting point for the Strangeness tutorial
-/// \author
-/// \since 12/05/2023
-/// \file strangeness_step3.cxx
-///
+/// \brief Step3 of the Strangeness tutorial
+/// \author Nepeivoda Roman (roman.nepeivoda@cern.ch)
+/// \author Chiara De Martin (chiara.de.martin@cern.ch)
 
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
@@ -32,66 +30,98 @@ using namespace o2::framework::expressions;
 // STEP 2
 // Apply PID selections on V0 daughter tracks
 // STEP 3
-// Check the MC information of the V0s and verify with the PID information of daughter tracks
+// Check the MC information of the V0s
 
 struct strangeness_tutorial {
+  // Histograms are defined with HistogramRegistry
+  HistogramRegistry rEventSelection{"eventSelection", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
+  HistogramRegistry rKzeroShort{"kzeroShort", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
+  HistogramRegistry rGenParticles{"genParticles", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
 
-  // Configurable for number of bins
+  // Configurable for histograms
   Configurable<int> nBins{"nBins", 100, "N bins in all histos"};
 
-  // Configurables parameters for V0 selection
+  // Configurable for event selection
+  Configurable<float> cutzvertex{"cutzvertex", 10.0f, "Accepted z-vertex range (cm)"};
+
+  // Configurable parameters for V0 selection
   Configurable<float> v0setting_dcav0dau{"v0setting_dcav0dau", 1, "DCA V0 Daughters"};
   Configurable<float> v0setting_dcapostopv{"v0setting_dcapostopv", 0.06, "DCA Pos To PV"};
   Configurable<float> v0setting_dcanegtopv{"v0setting_dcanegtopv", 0.06, "DCA Neg To PV"};
   Configurable<double> v0setting_cospa{"v0setting_cospa", 0.98, "V0 CosPA"}; // double -> N.B. dcos(x)/dx = 0 at x=0
   Configurable<float> v0setting_radius{"v0setting_radius", 0.5, "v0radius"};
+
+  // Configurable parameters for PID selection
   Configurable<float> NSigmaTPCPion{"NSigmaTPCPion", 4, "NSigmaTPCPion"};
 
-  AxisSpec ptAxis = {100, 0.0f, 10.0f, "#it{p}_{T} (GeV/#it{c})"}; // Definition of axis
+  void init(InitContext const&)
+  {
+    // Axes
+    AxisSpec K0ShortMassAxis = {200, 0.45f, 0.55f, "#it{M}_{inv} [GeV/#it{c}^{2}]"};
+    AxisSpec vertexZAxis = {nBins, -15., 15., "vrtx_{Z} [cm]"};
+    AxisSpec ptAxis = {100, 0.0f, 10.0f, "#it{p}_{T} (GeV/#it{c})"};
 
-  // histogram defined with HistogramRegistry
-  HistogramRegistry registry{"registry",
-                             {{"hVertexZ", "hVertexZ", {HistType::kTH1F, {{nBins, -15., 15.}}}},
-                              {"hMassK0Short", "hMassK0Short", {HistType::kTH1F, {{200, 0.45f, 0.55f}}}},
-                              {"hMassK0ShortSelected", "hMassK0ShortSelected", {HistType::kTH1F, {{200, 0.45f, 0.55f}}}},
-                              {"hDCAV0Daughters", "hDCAV0Daughters", {HistType::kTH1F, {{55, 0.0f, 2.2f}}}},
-                              {"hV0CosPA", "hV0CosPA", {HistType::kTH1F, {{100, 0.95f, 1.f}}}},
-                              {"hNSigmaPosPionFromK0s", "hNSigmaPosPionFromK0s", {HistType::kTH2F, {{100, -5.f, 5.f}, {ptAxis}}}},
-                              {"hNSigmaNegPionFromK0s", "hNSigmaNegPionFromK0s", {HistType::kTH2F, {{100, -5.f, 5.f}, {ptAxis}}}},
-                              {"hMassK0ShortTruePions", "hMassK0ShortTruePions", {HistType::kTH1F, {{200, 0.45f, 0.55f}}}},
-                              {"hMassK0ShortMCTrue", "hMassK0ShortMCTrue", {HistType::kTH1F, {{200, 0.45f, 0.55f}}}},
-                              {"hPtK0ShortTrue", "hPtK0ShortTrue", {HistType::kTH1F, {{ptAxis}}}}}};
+    // Histograms
+    // Event selection
+    rEventSelection.add("hVertexZRec", "hVertexZRec", {HistType::kTH1F, {vertexZAxis}});
+
+    // K0s reconstruction
+      // Mass
+    rKzeroShort.add("hMassK0Short", "hMassK0Short", {HistType::kTH1F, {K0ShortMassAxis}});
+    rKzeroShort.add("hMassK0ShortSelected", "hMassK0ShortSelected", {HistType::kTH1F, {K0ShortMassAxis}});
+    rKzeroShort.add("hMassK0ShortSelectedTruePions", "hMassK0ShortSelectedTruePions", {HistType::kTH1F, {{200, 0.45f, 0.55f}}});
+    rKzeroShort.add("hMassK0ShortTrueRec", "hMassK0ShortTrueRec", {HistType::kTH1F, {{200, 0.45f, 0.55f}}});
+      // Pt
+    rKzeroShort.add("hPtK0ShortSelected", "hPtK0ShortSelected", {HistType::kTH1F, {{ptAxis}}});
+    rKzeroShort.add("hPtK0ShortTrueRec", "hPtK0ShortTrueRec", {HistType::kTH1F, {{ptAxis}}});
+
+    // K0s topological/PID cuts
+    rKzeroShort.add("hDCAV0Daughters", "hDCAV0Daughters", {HistType::kTH1F, {{55, 0.0f, 2.2f}}});
+    rKzeroShort.add("hV0CosPA", "hV0CosPA", {HistType::kTH1F, {{100, 0.95f, 1.f}}});
+    rKzeroShort.add("hNSigmaPosPionFromK0s", "hNSigmaPosPionFromK0s", {HistType::kTH2F, {{100, -5.f, 5.f}, {ptAxis}}});
+    rKzeroShort.add("hNSigmaNegPionFromK0s", "hNSigmaNegPionFromK0s", {HistType::kTH2F, {{100, -5.f, 5.f}, {ptAxis}}});
+
+    // Generated level histograms
+    rEventSelection.add("hVertexZGen", "hVertexZGen", {HistType::kTH1F, {vertexZAxis}});
+    rGenParticles.add("hPtK0ShortGen", "hPtK0ShortGen", {HistType::kTH1F, {{ptAxis}}});
+  }
 
   // Defining filters for events (event selection)
-  // Processed events will be already fulfulling the event selection requirements
+  // Processed events will be already fulfilling the event selection requirements
   Filter eventFilter = (o2::aod::evsel::sel8 == true);
+  Filter posZFilter = (nabs(o2::aod::collision::posZ) < cutzvertex);
+  Filter posZFilterMC = (nabs(o2::aod::mccollision::posZ) < cutzvertex);
 
   // Filters on V0s
-  // Cannot filter on dynamic columns, so we cut on DCA to PV and DCA between daughters only
-  Filter preFilterV0 = nabs(aod::v0data::dcapostopv) > v0setting_dcapostopv&& nabs(aod::v0data::dcanegtopv) > v0setting_dcanegtopv&& aod::v0data::dcaV0daughters < v0setting_dcav0dau;
+  // Cannot filter on dynamic columns
+  Filter preFilterV0 = (nabs(aod::v0data::dcapostopv) > v0setting_dcapostopv && 
+                        nabs(aod::v0data::dcanegtopv) > v0setting_dcanegtopv && 
+                        aod::v0data::dcaV0daughters < v0setting_dcav0dau);
 
   // Defining the type of the daughter tracks
   using DaughterTracks = soa::Join<aod::TracksIU, aod::TracksExtra, aod::pidTPCPi, aod::McTrackLabels>;
 
-  void process(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& collision,
-               soa::Filtered<soa::Join<aod::V0Datas, aod::McV0Labels>> const& V0s,
-               DaughterTracks const&, // no need to define a variable for tracks, if we don't access them directly
-               aod::McParticles const& mcParticles)
+  void processRecMC(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& collision,
+                    soa::Filtered<soa::Join<aod::V0Datas, aod::McV0Labels>> const& V0s,
+                    DaughterTracks const&, // no need to define a variable for tracks, if we don't access them directly
+                    aod::McParticles const&)
   {
     // Fill the event counter
-    registry.fill(HIST("hVertexZ"), collision.posZ());
+    rEventSelection.fill(HIST("hVertexZRec"), collision.posZ());
 
-    for (auto& v0 : V0s) {
-
+    // V0s
+    for (const auto& v0 : V0s) {
       const auto& posDaughterTrack = v0.posTrack_as<DaughterTracks>();
       const auto& negDaughterTrack = v0.negTrack_as<DaughterTracks>();
 
-      registry.fill(HIST("hMassK0Short"), v0.mK0Short());
+      rKzeroShort.fill(HIST("hMassK0Short"), v0.mK0Short());
 
+      // Cut on dynamic columns
       if (v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) < v0setting_cospa)
         continue;
       if (v0.v0radius() < v0setting_radius)
         continue;
+
       if (TMath::Abs(posDaughterTrack.tpcNSigmaPi()) > NSigmaTPCPion) {
         continue;
       }
@@ -99,39 +129,54 @@ struct strangeness_tutorial {
         continue;
       }
 
-      registry.fill(HIST("hMassK0ShortSelected"), v0.mK0Short());
-      registry.fill(HIST("hDCAV0Daughters"), v0.dcaV0daughters());
-      registry.fill(HIST("hV0CosPA"), v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()));
+      rKzeroShort.fill(HIST("hMassK0ShortSelected"), v0.mK0Short());
+      rKzeroShort.fill(HIST("hPtK0ShortSelected"), v0.pt());
+
+      rKzeroShort.fill(HIST("hDCAV0Daughters"), v0.dcaV0daughters());
+      rKzeroShort.fill(HIST("hV0CosPA"), v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()));
 
       // Filling the PID of the V0 daughters in the region of the K0 peak
-      if (0.45 > v0.mK0Short() && v0.mK0Short() < 0.55) {
-        registry.fill(HIST("hNSigmaPosPionFromK0s"), posDaughterTrack.tpcNSigmaPi(), posDaughterTrack.tpcInnerParam());
-        registry.fill(HIST("hNSigmaNegPionFromK0s"), negDaughterTrack.tpcNSigmaPi(), negDaughterTrack.tpcInnerParam());
+      if (0.45 < v0.mK0Short() && v0.mK0Short() < 0.55) {
+        rKzeroShort.fill(HIST("hNSigmaPosPionFromK0s"), posDaughterTrack.tpcNSigmaPi(), posDaughterTrack.tpcInnerParam());
+        rKzeroShort.fill(HIST("hNSigmaNegPionFromK0s"), negDaughterTrack.tpcNSigmaPi(), negDaughterTrack.tpcInnerParam());
       }
 
       if (posDaughterTrack.has_mcParticle() && negDaughterTrack.has_mcParticle()) { // Checking that the daughter tracks come from particles and are not fake
         auto posParticle = posDaughterTrack.mcParticle();
         auto negParticle = negDaughterTrack.mcParticle();
         if (posParticle.pdgCode() == 211 && negParticle.pdgCode() == -211) { // Checking that the daughter tracks are true pions
-          registry.fill(HIST("hMassK0ShortTruePions"), v0.mK0Short());
+          rKzeroShort.fill(HIST("hMassK0ShortSelectedTruePions"), v0.mK0Short());
         }
       }
 
-      // Checking that the V0 is a true K0s in the MC
+      // Checking that the V0 is a true K0s
       if (v0.has_mcParticle()) {
-        auto v0mcparticle = v0.mcParticle();
-        if (v0mcparticle.pdgCode() == 310) {
-          registry.fill(HIST("hMassK0ShortMCTrue"), v0.mK0Short());
-        }
-      }
-    }
-
-    for (const auto& mcParticle : mcParticles) {
-      if (mcParticle.pdgCode() == 310) {
-        registry.fill(HIST("hPtK0ShortTrue"), mcParticle.pt());
+        auto v0mcParticle = v0.mcParticle();
+        if (v0mcParticle.pdgCode() == 310) {
+          rKzeroShort.fill(HIST("hMassK0ShortTrueRec"), v0.mK0Short());
+          rKzeroShort.fill(HIST("hPtK0ShortTrueRec"), v0.pt()); // To mimic distribution after the signal extraction
+        }    
       }
     }
   }
+
+  void processGenMC(soa::Filtered<aod::McCollisions>::iterator const& mcCollision,
+                    aod::McParticles const& mcParticles)
+  {
+    rEventSelection.fill(HIST("hVertexZGen"), mcCollision.posZ());
+    for (const auto& mcParticle : mcParticles) {
+      if (mcParticle.pdgCode() == 310) {
+        rGenParticles.fill(HIST("hPtK0ShortGen"), mcParticle.pt());
+      }
+    }
+  }
+
+  PROCESS_SWITCH(strangeness_tutorial, processRecMC, "Process Run 3 mc, reconstructed", true);
+  PROCESS_SWITCH(strangeness_tutorial, processGenMC, "Process Run 3 mc, generated", true);
 };
 
-WorkflowSpec defineDataProcessing(ConfigContext const& cfgc) { return WorkflowSpec{adaptAnalysisTask<strangeness_tutorial>(cfgc)}; }
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc) 
+{ 
+  return WorkflowSpec{
+    adaptAnalysisTask<strangeness_tutorial>(cfgc)}; 
+}
