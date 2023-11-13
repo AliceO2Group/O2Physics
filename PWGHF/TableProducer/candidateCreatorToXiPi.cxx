905baccb8fec0f5649c6c2d0189259992e0a660d
ZFederica <zanone@physi.uni-heidelberg.de>
2023-11-13 12:37:54 +0100

    Temp

--- PWGHF/TableProducer/candidateCreatorToXiPi.cxx
+++ PWGHF/TableProducer/candidateCreatorToXiPi.cxx
@@ -114,7 +114,7 @@ struct HfCandidateCreatorToXiPi {
                MyV0Table const&,
                aod::V0sLinked const&)
   {
-
+      
       // 2-prong vertex fitter to build the omegac/xic vertex
       o2::vertexing::DCAFitterN<2> df;
       df.setBz(magneticField);
@@ -128,7 +128,7 @@ struct HfCandidateCreatorToXiPi {
       df.setUseAbsDCA(useAbsDCA);
       df.setWeightedFinalPCA(useWeightedFinalPCA);
       df.setRefitWithMatCorr(refitWithMatCorr);
-
+      
       double massPionFromPDG = o2::analysis::pdg::MassPiPlus;    // pdg code 211
       double massLambdaFromPDG = o2::analysis::pdg::MassLambda0; // pdg code 3122
       double massXiFromPDG = o2::analysis::pdg::MassXiMinus;     // pdg code 3312
