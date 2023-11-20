e190eb0995461194e03be76b4ef8ee156a8b417a
Johanna Lömker <jloemker@yahoo.de>
2023-11-20 16:22:21 +0100

    fix type

--- PWGJE/Tasks/trackJetqa.cxx
+++ PWGJE/Tasks/trackJetqa.cxx
@@ -155,8 +155,8 @@ struct TrackJetQa {
     histos.add("Centrality/FT0C", "CentFT0C", HistType::kTH1D, {axisPercentileFT0M});
     histos.add("Mult/NTracksPV", "MultNTracksPV", HistType::kTH1D, {axisMultiplicityPV});
     histos.add("Mult/FT0M", "MultFT0M", HistType::kTH1D, {axisMultiplicityFT0M});
-    histos.add("Mult/FT0M", "MultFT0A", HistType::kTH1D, {axisMultiplicityFT0A});
-    histos.add("Mult/FT0M", "MultFT0C", HistType::kTH1D, {axisMultiplicityFT0C}); // need to correct from up here
+    histos.add("Mult/FT0A", "MultFT0A", HistType::kTH1D, {axisMultiplicityFT0A});
+    histos.add("Mult/FT0C", "MultFT0C", HistType::kTH1D, {axisMultiplicityFT0C}); // need to correct from up here
     histos.add("Mult/MultCorrelations", "MultCorrelations", HistType::kTHnSparseD, {axisPercentileFT0A, axisPercentileFT0C, axisMultiplicityFT0A, axisMultiplicityFT0C, axisMultiplicityPV});
 
     histos.add("TrackEventPar/Sigma1PtFT0Mcent", "Sigma1Pt vs pT vs FT0M centrality", HistType::kTHnSparseD, {axisPercentileFT0M, axisPt, axisSigma1OverPt});
