c979e36b69af2c5336a0a4c12590bdf4af5ff4df
Sasha Bylinkin <37345380+abylinkin@users.noreply.github.com>
2023-11-21 11:40:25 +0100

    PWG-UD: Single Gap producer (#3879)
    * Single Gap producer, Paul's comments, mistype fixed
    
    * Please consider the following formatting changes
    
    * Update UDHelpers.h
    
    Removed CleanFitACollisions and CleanFitCCollision which are not ysed by the SGSelector
    
    ---------
    
    Co-authored-by: alexandr.bylinkin@cern.ch <albyl1023@ift-b7lb1t3.klientdrift.uib.no>
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

---
+++ PWGUD/Core/SGCutParHolder.cxx
@@ -0,0 +1,112 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "SGCutParHolder.h"
+
+// setter
+void SGCutParHolder::SetNDtcoll(int ndtcoll)
+{
+  mNDtcoll = ndtcoll;
+}
+void SGCutParHolder::SetMinNBCs(int nminbcs)
+{
+  mMinNBCs = nminbcs;
+}
+void SGCutParHolder::SetWithFwdTracks(bool withFwdTracks)
+{
+  mWithFwdTracks = withFwdTracks;
+}
+void SGCutParHolder::SetGlobalTracksOnly(bool globalTracksOnly)
+{
+  mGlobalTracksOnly = globalTracksOnly;
+}
+void SGCutParHolder::SetITSOnlyTracks(bool ITSonlyTracks)
+{
+  mITSOnlyTracks = ITSonlyTracks;
+}
+void SGCutParHolder::SetMinRgtrwTOF(float rgtrwTOF)
+{
+  mMinRgtrwTOF = rgtrwTOF;
+}
+void SGCutParHolder::SetNTracks(int MinNTracks, int MaxNTracks)
+{
+  mMinNTracks = MinNTracks;
+  mMaxNTracks = MaxNTracks;
+}
+void SGCutParHolder::SetNetCharges(std::vector<int> netCharges)
+{
+  mNetCharges = netCharges;
+}
+void SGCutParHolder::SetPidHypothesis(int pidHypo)
+{
+  mPidHypo = pidHypo;
+}
+void SGCutParHolder::SetPoszRange(float MinPosz, float MaxPosz)
+{
+  mMinVertexPosz = MinPosz;
+  mMaxVertexPosz = MaxPosz;
+}
+
+void SGCutParHolder::SetPtRange(float minPt, float maxPt)
+{
+  mMinPt = minPt;
+  mMaxPt = maxPt;
+}
+void SGCutParHolder::SetEtaRange(float minEta, float maxEta)
+{
+  mMinEta = minEta;
+  mMaxEta = maxEta;
+}
+void SGCutParHolder::SetIVMRange(float minIVM, float maxIVM)
+{
+  mMinIVM = minIVM;
+  mMaxIVM = maxIVM;
+}
+void SGCutParHolder::SetMaxNSigmaTPC(float maxnSigma)
+{
+  mMaxNSigmaTPC = maxnSigma;
+}
+void SGCutParHolder::SetMaxNSigmaTOF(float maxnSigma)
+{
+  mMaxNSigmaTOF = maxnSigma;
+}
+void SGCutParHolder::SetMaxFITtime(float maxFITtime)
+{
+  mMaxFITtime = maxFITtime;
+}
+void SGCutParHolder::SetFITAmpLimits(std::vector<float> FITAmpLimits)
+{
+  mFITAmpLimits = FITAmpLimits;
+}
+
+// getter
+int SGCutParHolder::NDtcoll() const { return mNDtcoll; }
+int SGCutParHolder::minNBCs() const { return mMinNBCs; }
+bool SGCutParHolder::withFwdTracks() const { return mWithFwdTracks; }
+bool SGCutParHolder::globalTracksOnly() const { return mGlobalTracksOnly; }
+bool SGCutParHolder::ITSOnlyTracks() const { return mITSOnlyTracks; }
+float SGCutParHolder::minRgtrwTOF() const { return mMinRgtrwTOF; }
+int SGCutParHolder::minNTracks() const { return mMinNTracks; }
+int SGCutParHolder::maxNTracks() const { return mMaxNTracks; }
+std::vector<int> SGCutParHolder::netCharges() const { return mNetCharges; }
+int SGCutParHolder::pidHypothesis() const { return mPidHypo; }
+float SGCutParHolder::minPosz() const { return mMinVertexPosz; }
+float SGCutParHolder::maxPosz() const { return mMaxVertexPosz; }
+float SGCutParHolder::minPt() const { return mMinPt; }
+float SGCutParHolder::maxPt() const { return mMaxPt; }
+float SGCutParHolder::minEta() const { return mMinEta; }
+float SGCutParHolder::maxEta() const { return mMaxEta; }
+float SGCutParHolder::minIVM() const { return mMinIVM; }
+float SGCutParHolder::maxIVM() const { return mMaxIVM; }
+float SGCutParHolder::maxNSigmaTPC() const { return mMaxNSigmaTPC; }
+float SGCutParHolder::maxNSigmaTOF() const { return mMaxNSigmaTOF; }
+float SGCutParHolder::maxFITtime() const { return mMaxFITtime; }
+std::vector<float> SGCutParHolder::FITAmpLimits() const { return mFITAmpLimits; }
