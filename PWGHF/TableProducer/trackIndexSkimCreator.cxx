714191324a883875cb558f928ffc20a9cb63dad4
ZFederica <zanone@physi.uni-heidelberg.de>
2023-11-24 20:51:28 +0100

    reinclude filter LfCascades

--- PWGHF/TableProducer/trackIndexSkimCreator.cxx
+++ PWGHF/TableProducer/trackIndexSkimCreator.cxx
@@ -3054,9 +3054,10 @@ struct HfTrackIndexSkimCreatorLfCascades {
   }
 
   Filter filterSelectCollisions = (aod::hf_sel_collision::whyRejectColl == 0);
+  Filter filterSelectTrackIds = (aod::hf_sel_track::isSelProng > 0);
 
   using SelectedCollisions = soa::Filtered<soa::Join<aod::Collisions, aod::HfSelCollision>>;
-  using SelectedHfTrackAssoc = soa::Join<aod::TrackAssoc, aod::HfSelTrack>;
+  using SelectedHfTrackAssoc = soa::Filtered<soa::Join<aod::TrackAssoc, aod::HfSelTrack>>;
   using CascFull = soa::Join<aod::CascDatas, aod::CascCovs>;
   using V0Full = soa::Join<aod::V0Datas, aod::V0Covs>;
 
