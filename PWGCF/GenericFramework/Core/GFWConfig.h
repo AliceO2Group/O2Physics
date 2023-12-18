8387b62044f5917fde587c0188cc177de6e027b8
Emil Gorm Nielsen <emilgormnielsen@cern-ch>
2023-12-18 14:50:52 +0100

    Templated reco/gen/data processing

--- PWGCF/GenericFramework/Core/GFWConfig.h
+++ PWGCF/GenericFramework/Core/GFWConfig.h
@@ -96,6 +96,9 @@ class GFWBinningCuts
   const auto& GetPtRefMin() const { return mPTrefmin; }
   const auto& GetPtRefMax() const { return mPTrefmax; }
 
+  const float& GetPtMin() const { return std::min(mPTrefmin, mPTpoimin); }
+  const float& GetPtMax() const { return std::max(mPTrefmax, mPTpoimax); }
+
   void SetNchBinning(int nchbins, float nchmin, float nchmax)
   {
     mNchbins = nchbins;
