3310fa80ef38fc045cfdfcd0a088248b95fb08e0
Emil Gorm Nielsen <emilgormnielsen@cern-ch>
2024-01-10 14:54:57 +0100

    copyright header

--- PWGCF/GenericFramework/Core/FlowPtContainer.cxx
+++ PWGCF/GenericFramework/Core/FlowPtContainer.cxx
@@ -1,9 +1,14 @@
-/*
-Author: Emil Gorm Nielsen
-Modified from AliCkContainer by Vytautas
-Container to store the terms required to calculate higher order moments of the pT spectrum.
-If used, modified, or distributed, please aknowledge the original author of this code.
-*/
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 #include "FlowPtContainer.h"
 
 FlowPtContainer::FlowPtContainer() : TNamed("name", "name"),
