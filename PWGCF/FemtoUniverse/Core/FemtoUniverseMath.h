aeafd69cb892ed30610ca63e72fa997ca0e6a3d6
prchakra <47203359+prchakra@users.noreply.github.com>
2024-01-10 13:32:47 +0100

    Adding function to compute 3d rel. pair momentum

--- PWGCF/FemtoUniverse/Core/FemtoUniverseMath.h
+++ PWGCF/FemtoUniverse/Core/FemtoUniverseMath.h
@@ -147,19 +147,18 @@ class FemtoUniverseMath
   /// \param part2 Particle 2
   /// \param mass2 Mass of particle 2
   /// \param isiden Identical or non-identical particle pair
-  /// \param islcms LCMS or PRF 
+  /// \param islcms LCMS or PRF
   template <typename T>
   static std::vector<double> getpairmom3d(const T& part1, const float mass1, const T& part2, const float mass2, bool isiden, bool islcms)
   {
-    
     const double E1 = sqrt(pow(part1.px(), 2) + pow(part1.py(), 2) + pow(part1.pz(), 2) + pow(mass1, 2));
     const double E2 = sqrt(pow(part2.px(), 2) + pow(part2.py(), 2) + pow(part2.pz(), 2) + pow(mass2, 2));
 
     const ROOT::Math::PxPyPzEVector vecpart1(part1.px(), part1.py(), part1.pz(), E1);
-    const ROOT::Math::PxPyPzEVector vecpart2(part2.px(), part2.py(), part2.pz(), E2);    
+    const ROOT::Math::PxPyPzEVector vecpart2(part2.px(), part2.py(), part2.pz(), E2);
     const ROOT::Math::PxPyPzEVector trackSum = vecpart1 + vecpart2;
 
-    std::vector<double> vect;    
+    std::vector<double> vect;
 
     const double tPx = trackSum.px();
     const double tPy = trackSum.py();
@@ -168,33 +167,33 @@ class FemtoUniverseMath
 
     const double tPt = trackSum.pt();
     const double tMt = trackSum.mt();
-    const double tPinv = std::sqrt((tMt*tMt) - (tPt*tPt)); 
+    const double tPinv = std::sqrt((tMt * tMt) - (tPt * tPt));
 
     float nullmass = 0.0;
     const double m1 = std::max(nullmass, mass1);
     const double m2 = std::max(nullmass, mass2);
 
-    const double tQinvL = std::pow((E1-E2),2) - std::pow((part1.px()-part2.px()),2) -
-    std::pow((part1.py()-part2.py()),2) - std::pow((part1.pz()-part2.pz()),2);
+    const double tQinvL = std::pow((E1 - E2), 2) - std::pow((part1.px() - part2.px()), 2) -
+                          std::pow((part1.py() - part2.py()), 2) - std::pow((part1.pz() - part2.pz()), 2);
 
-    double tQ = (m1 - m2)/tPinv;
-    tQ = ::sqrt( tQ*tQ - tQinvL);
+    double tQ = (m1 - m2) / tPinv;
+    tQ = ::sqrt(tQ * tQ - tQinvL);
 
-    const double fKStarCalc = tQ/2.0;
+    const double fKStarCalc = tQ / 2.0;
     vect.push_back(fKStarCalc);
 
-    //Boost to LCMS
+    // Boost to LCMS
 
-    const double beta = tPz/tPE;
-    const double gamma = tPE/tMt;
+    const double beta = tPz / tPE;
+    const double gamma = tPE / tMt;
 
-    const double px1L = (part1.px()*tPx + part1.py()*tPy)/tPt;
-    const double py1L = (-part1.px()*tPy + part1.py()*tPx)/tPt;
+    const double px1L = (part1.px() * tPx + part1.py() * tPy) / tPt;
+    const double py1L = (-part1.px() * tPy + part1.py() * tPx) / tPt;
     const double pz1L = gamma * (part1.pz() - beta * E1);
     const double pE1L = gamma * (E1 - beta * part1.pz());
 
-    const double px2L = (part2.px()*tPx + part2.py()*tPy)/tPt;
-    const double py2L = (-part2.px()*tPy + part2.py()*tPx)/tPt;
+    const double px2L = (part2.px() * tPx + part2.py() * tPy) / tPt;
+    const double py2L = (-part2.px() * tPy + part2.py() * tPx) / tPt;
     const double pz2L = gamma * (part2.pz() - beta * E2);
     const double pE2L = gamma * (E2 - beta * part2.pz());
 
@@ -206,49 +205,41 @@ class FemtoUniverseMath
     double fDKSidePRF;
     double fDKLongPRF;
 
-      if(!isiden)
-      {
-        fDKOutLCMS  = px1L;
-        fDKSideLCMS = py1L;
-        fDKLongLCMS = pz1L;
-      }    
-      else 
-      {
-        fDKOutLCMS  = px1L - px2L;
-        fDKSideLCMS = py1L - py2L;
-        fDKLongLCMS = pz1L - pz2L;
-      }
+    if (!isiden) {
+      fDKOutLCMS = px1L;
+      fDKSideLCMS = py1L;
+      fDKLongLCMS = pz1L;
+    } else {
+      fDKOutLCMS = px1L - px2L;
+      fDKSideLCMS = py1L - py2L;
+      fDKLongLCMS = pz1L - pz2L;
+    }
 
-      //Boost to PRF    
-      const double betaOut = tPt/tMt;
-      const double gammaOut = tMt/tPinv;
+    // Boost to PRF
+    const double betaOut = tPt / tMt;
+    const double gammaOut = tMt / tPinv;
 
-      if(!isiden){
-        fDKOutPRF  = gammaOut * (fDKOutLCMS - betaOut * pE1L);
-        fDKSidePRF = fDKSideLCMS;
-        fDKLongPRF = fDKLongLCMS;
-      }
-      else{
-        fDKOutPRF  = gammaOut * (fDKOutLCMS - betaOut * (pE1L - pE2L));
-        fDKSidePRF = fDKSideLCMS;
-        fDKLongPRF = fDKLongLCMS;
-      }
-    
-      if(islcms)
-      {
-        vect.push_back(fDKOutLCMS);
-        vect.push_back(fDKSideLCMS);
-        vect.push_back(fDKLongLCMS);
-      }
-      else
-      {
-        vect.push_back(fDKOutPRF);
-        vect.push_back(fDKSidePRF);
-        vect.push_back(fDKLongPRF);
-      }   
+    if (!isiden) {
+      fDKOutPRF = gammaOut * (fDKOutLCMS - betaOut * pE1L);
+      fDKSidePRF = fDKSideLCMS;
+      fDKLongPRF = fDKLongLCMS;
+    } else {
+      fDKOutPRF = gammaOut * (fDKOutLCMS - betaOut * (pE1L - pE2L));
+      fDKSidePRF = fDKSideLCMS;
+      fDKLongPRF = fDKLongLCMS;
+    }
+
+    if (islcms) {
+      vect.push_back(fDKOutLCMS);
+      vect.push_back(fDKSideLCMS);
+      vect.push_back(fDKLongLCMS);
+    } else {
+      vect.push_back(fDKOutPRF);
+      vect.push_back(fDKSidePRF);
+      vect.push_back(fDKLongPRF);
+    }
     return vect;
   }
-   
 };
 
 } // namespace o2::analysis::femtoUniverse
