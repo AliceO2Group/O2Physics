4718c8e71b55394e0fe411c381cd1fe23af32e10
prottayHEP <prottaydas0608@gmail.com>
2024-01-14 09:40:18 +0530

    tried to solve copyright issue

--- PWGLF/Tasks/chargedkstaranalysis.cxx
+++ PWGLF/Tasks/chargedkstaranalysis.cxx
@@ -1,5 +1,17 @@
-/// \code for Charged KStar analysis
-/// \author Prottay Das (prottay.das@cern.ch)
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+///
+/// \brief this is a code for the CKS resonance
+/// \author prottay das
+/// \since 13/01/2024
 
 #include <TDatabasePDG.h>
 #include <TDirectory.h>
