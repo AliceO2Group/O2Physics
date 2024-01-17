3299a5f1bbe3ab21172dad109c24eae065e462d1
R-Rohith <92912027+R-Rohith@users.noreply.github.com>
2024-01-17 15:41:18 +0530

    Update r2p2-4-id.cxx
    Added a PID column to the 'Flags' table.
    Avoided checking PID within the double loop, and now relying on PID Flags joined to the tracks table.
    Renamed namespace.

--- PWGCF/TwoParticleCorrelations/Tasks/r2p2-4-id.cxx
+++ PWGCF/TwoParticleCorrelations/Tasks/r2p2-4-id.cxx
@@ -88,24 +88,32 @@ bool (*pidarray[4])(T) = {NO_PID, PID_PION, PID_KAON, PID_PROTON}; // Array of P
 
 namespace o2::aod
 {
-namespace columns
+namespace idr2p2columns
 {
 DECLARE_SOA_COLUMN(BinFlag, binflag, bool); // To flag tracks without proper binning
-}
-DECLARE_SOA_TABLE(Flags, "AOD", "Flags", columns::BinFlag);
+DECLARE_SOA_COLUMN(PID, pid, int);          // Flag for type of particle
+} // namespace idr2p2columns
+DECLARE_SOA_TABLE(Flags, "AOD", "Flags", idr2p2columns::BinFlag, idr2p2columns::PID);
 } // namespace o2::aod
 struct FillFlagsTable {
   Produces<aod::Flags> ftable;
-  void process(aod::Tracks const& tracks)
+  void process(soa::Join<aod::Tracks, aod::TrackSelection, aod::pidTPCPi, aod::pidTOFPi, aod::pidTPCPr, aod::pidTOFPr, aod::pidTPCKa, aod::pidTOFKa, aod::pidTPCEl, aod::pidTOFbeta, aod::TracksExtra> const& tracks)
   {
-    int etabin, phibin;
+    bool binflag;
+    int etabin, phibin, pid;
     for (auto track : tracks) {
       etabin = (track.eta() + 0.8) * 15; // 15= 24/1.6
       phibin = 36 * track.phi() / (2 * constants::math::PI);
       if ((etabin < 0) || (etabin >= 24) || (phibin < 0) || (phibin >= 36))
-        ftable(false);
+        binflag = false;
       else
-        ftable(true);
+        binflag = true;
+      pid = 0;
+      for (int i = 0; i < 4; i++) {
+        if (pidarray<decltype(track)>[i](track))
+          pid = pid * 10 + i;
+      }
+      ftable(binflag, pid);
     }
   }
 };
@@ -117,7 +125,7 @@ struct r2p24id {
   Filter collisionFilter = (nabs(aod::collision::posZ) < 10.f);
   Filter ptfilter = aod::track::pt > minpT&& aod::track::pt < maxpT;
   Filter globalfilter = requireGlobalTrackInFilter();
-  Filter properbinfilter = aod::columns::binflag == true; //'aod::Flags::binflag == true;' did not work
+  Filter properbinfilter = aod::idr2p2columns::binflag == true;
   //---------------------------------------------------------------------------
   HistogramRegistry histos{"R2P2", {}, OutputObjHandlingPolicy::AnalysisObject};
 
@@ -129,12 +137,18 @@ struct r2p24id {
   void init(InitContext const&)
   {
     //-----Defining Histograms-------------------------------------------------
-    const AxisSpec phi{36, 0, 2.0 * constants::math::PI, "phi"}, eta{24, -0.8, 0.8, "eta"}, etaphi1{864, 0, 864, "etaphi1"}, etaphi2{864, 0, 864, "etaphi2"};
-    char name[4][3] = {"ch", "pi", "ka", "pr"}, histname[50];
+    const AxisSpec phi{36, 0, 2.0 * constants::math::PI, "phi"};
+    const AxisSpec eta{24, -0.8, 0.8, "eta"};
+    const AxisSpec etaphi1{864, 0, 864, "etaphi1"};
+    const AxisSpec etaphi2{864, 0, 864, "etaphi2"};
+    char histname[50];
+    char name[4][3] = {"ch", "pi", "ka", "pr"};
+
     histos.add("h1d_n1_phi", "#phi distribution", kTH1D, {phi});
     histos.add("h1d_n1_eta", "#eta distribution", kTH1D, {eta});
     histos.add("h1d_n1_pt", "p_T", kTH1D, {{100, 0, 5, "p_T"}});
     histos.add("h1i_n1_multPM", "Multiplicity", kTH1I, {{200, 0, 200, "Multiplicity"}});
+
     for (int i = 0; i < 7; i++) {
       if (i < 4) { // Single particle distribution histograms
         snprintf(histname, sizeof(histname), "h1d_n1_ptP_%s", name[i]);
@@ -154,11 +168,11 @@ struct r2p24id {
       int e = i, f = i;
       if (i > 3) {
         e = 1;
-        f = 2;
-        if (i == 5)
-          f = 3;
+        f = 3;
+        if (i == 4)
+          f = 2;
         else if (i == 6)
-          e = 3;
+          e = 2;
         // There are two kinds of  "********PM_*" histograms here, one is for when track1 is +ve the other is for when track2 is +ve.
         snprintf(histname, sizeof(histname), "h2d_n2_eta1Phi1Eta2Phi2PM_%s%s", name[e], name[f]);
         histos.add(histname, "#rho_2 for +ve_1 -ve_2 particles", kTH2D, {etaphi1, etaphi2});
@@ -213,18 +227,21 @@ struct r2p24id {
     hist.h2d_1p[0][0][1] = histos.template get<TH2>(HIST("h2d_pt_etaPhiM_ch"));
     hist.h2d_1p[0][1][0] = histos.template get<TH2>(HIST("h2d_n1_etaPhiP_ch"));
     hist.h2d_1p[0][1][1] = histos.template get<TH2>(HIST("h2d_pt_etaPhiP_ch"));
+
     hist.h1d_1p[1][0] = histos.template get<TH1>(HIST("h1d_n1_ptM_pi"));
     hist.h1d_1p[1][1] = histos.template get<TH1>(HIST("h1d_n1_ptP_pi"));
     hist.h2d_1p[1][0][0] = histos.template get<TH2>(HIST("h2d_n1_etaPhiM_pi"));
     hist.h2d_1p[1][0][1] = histos.template get<TH2>(HIST("h2d_pt_etaPhiM_pi"));
     hist.h2d_1p[1][1][0] = histos.template get<TH2>(HIST("h2d_n1_etaPhiP_pi"));
     hist.h2d_1p[1][1][1] = histos.template get<TH2>(HIST("h2d_pt_etaPhiP_pi"));
+
     hist.h1d_1p[2][0] = histos.template get<TH1>(HIST("h1d_n1_ptM_ka"));
     hist.h1d_1p[2][1] = histos.template get<TH1>(HIST("h1d_n1_ptP_ka"));
     hist.h2d_1p[2][0][0] = histos.template get<TH2>(HIST("h2d_n1_etaPhiM_ka"));
     hist.h2d_1p[2][0][1] = histos.template get<TH2>(HIST("h2d_pt_etaPhiM_ka"));
     hist.h2d_1p[2][1][0] = histos.template get<TH2>(HIST("h2d_n1_etaPhiP_ka"));
     hist.h2d_1p[2][1][1] = histos.template get<TH2>(HIST("h2d_pt_etaPhiP_ka"));
+
     hist.h1d_1p[3][0] = histos.template get<TH1>(HIST("h1d_n1_ptM_pr"));
     hist.h1d_1p[3][1] = histos.template get<TH1>(HIST("h1d_n1_ptP_pr"));
     hist.h2d_1p[3][0][0] = histos.template get<TH2>(HIST("h2d_n1_etaPhiM_pr"));
@@ -249,6 +266,7 @@ struct r2p24id {
     hist.h2d_2p[0][1][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PP_chch"));
     hist.h2d_2p[0][1][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PP_chch"));
     hist.h2d_2p[0][1][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PP_chch"));
+
     hist.h2d_2p[1][0][0][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2MM_pipi"));
     hist.h2d_2p[1][0][0][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2MM_pipi"));
     hist.h2d_2p[1][0][0][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2MM_pipi"));
@@ -265,6 +283,7 @@ struct r2p24id {
     hist.h2d_2p[1][1][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PP_pipi"));
     hist.h2d_2p[1][1][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PP_pipi"));
     hist.h2d_2p[1][1][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PP_pipi"));
+
     hist.h2d_2p[2][0][0][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2MM_kaka"));
     hist.h2d_2p[2][0][0][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2MM_kaka"));
     hist.h2d_2p[2][0][0][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2MM_kaka"));
@@ -281,6 +300,7 @@ struct r2p24id {
     hist.h2d_2p[2][1][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PP_kaka"));
     hist.h2d_2p[2][1][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PP_kaka"));
     hist.h2d_2p[2][1][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PP_kaka"));
+
     hist.h2d_2p[3][0][0][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2MM_prpr"));
     hist.h2d_2p[3][0][0][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2MM_prpr"));
     hist.h2d_2p[3][0][0][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2MM_prpr"));
@@ -297,6 +317,7 @@ struct r2p24id {
     hist.h2d_2p[3][1][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PP_prpr"));
     hist.h2d_2p[3][1][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PP_prpr"));
     hist.h2d_2p[3][1][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PP_prpr"));
+
     hist.h2d_2p[4][0][0][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2MM_pika"));
     hist.h2d_2p[4][0][0][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2MM_pika"));
     hist.h2d_2p[4][0][0][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2MM_pika"));
@@ -313,6 +334,7 @@ struct r2p24id {
     hist.h2d_2p[4][1][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PP_pika"));
     hist.h2d_2p[4][1][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PP_pika"));
     hist.h2d_2p[4][1][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PP_pika"));
+
     hist.h2d_2p[5][0][0][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2MM_pipr"));
     hist.h2d_2p[5][0][0][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2MM_pipr"));
     hist.h2d_2p[5][0][0][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2MM_pipr"));
@@ -329,28 +351,31 @@ struct r2p24id {
     hist.h2d_2p[5][1][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PP_pipr"));
     hist.h2d_2p[5][1][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PP_pipr"));
     hist.h2d_2p[5][1][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PP_pipr"));
-    hist.h2d_2p[6][0][0][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2MM_prka"));
-    hist.h2d_2p[6][0][0][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2MM_prka"));
-    hist.h2d_2p[6][0][0][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2MM_prka"));
-    hist.h2d_2p[6][0][0][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2MM_prka"));
-    hist.h2d_2p[6][0][1][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2PM_kapr"));
-    hist.h2d_2p[6][0][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PM_kapr"));
-    hist.h2d_2p[6][0][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PM_kapr"));
-    hist.h2d_2p[6][0][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PM_kapr"));
-    hist.h2d_2p[6][1][0][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2PM_prka"));
-    hist.h2d_2p[6][1][0][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PM_prka"));
-    hist.h2d_2p[6][1][0][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PM_prka"));
-    hist.h2d_2p[6][1][0][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PM_prka"));
-    hist.h2d_2p[6][1][1][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2PP_prka"));
-    hist.h2d_2p[6][1][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PP_prka"));
-    hist.h2d_2p[6][1][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PP_prka"));
-    hist.h2d_2p[6][1][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PP_prka"));
+
+    hist.h2d_2p[6][0][0][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2MM_kapr"));
+    hist.h2d_2p[6][0][0][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2MM_kapr"));
+    hist.h2d_2p[6][0][0][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2MM_kapr"));
+    hist.h2d_2p[6][0][0][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2MM_kapr"));
+    hist.h2d_2p[6][0][1][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2PM_prka"));
+    hist.h2d_2p[6][0][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PM_prka"));
+    hist.h2d_2p[6][0][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PM_prka"));
+    hist.h2d_2p[6][0][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PM_prka"));
+    hist.h2d_2p[6][1][0][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2PM_kapr"));
+    hist.h2d_2p[6][1][0][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PM_kapr"));
+    hist.h2d_2p[6][1][0][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PM_kapr"));
+    hist.h2d_2p[6][1][0][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PM_kapr"));
+    hist.h2d_2p[6][1][1][0] = histos.template get<TH2>(HIST("h2d_n2_eta1Phi1Eta2Phi2PP_kapr"));
+    hist.h2d_2p[6][1][1][1] = histos.template get<TH2>(HIST("h2d_npt_eta1Phi1Eta2Phi2PP_kapr"));
+    hist.h2d_2p[6][1][1][2] = histos.template get<TH2>(HIST("h2d_ptn_eta1Phi1Eta2Phi2PP_kapr"));
+    hist.h2d_2p[6][1][1][3] = histos.template get<TH2>(HIST("h2d_ptpt_eta1Phi1Eta2Phi2PP_kapr"));
     //-------------------------------------------------------------------------
     //-------------------------------------------------------------------------
   }
-  void process(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& filteredCollision, soa::Filtered<soa::Join<aod::Tracks, aod::TrackSelection, aod::pidTPCPi, aod::pidTOFPi, aod::pidTPCPr, aod::pidTOFPr, aod::pidTPCKa, aod::pidTOFKa, aod::pidTPCEl, aod::pidTOFbeta, aod::TracksExtra, aod::Flags>> const& tracks)
+  void process(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& filteredCollision, soa::Filtered<soa::Join<aod::Tracks, aod::TrackSelection, aod::TracksExtra, aod::Flags>> const& tracks)
   {
-    int sign1, sign2, mult = 0;
+    int mult = 0;
+    int pid1, pid2;
+    int sign1, sign2;
     int etabin1, phibin1, etabin2, phibin2;
 
     for (auto track1 : tracks) {
@@ -359,15 +384,15 @@ struct r2p24id {
       histos.fill(HIST("h1d_n1_eta"), track1.eta());
       histos.fill(HIST("h1d_n1_pt"), track1.pt());
       //---Single Particle Distribution----------------------------------------
-      for (int i = 0; i < 4; i++) // Filling histograms for different particle species
+      sign1 = (track1.sign() + 1) / 2;
+      pid1 = track1.pid();
+      for (int i = pid1;; i /= 10) // Filling histograms for different particle species; i%10 = 0 means ch, 1->pi, 2->ka, 3->pr.
       {
-        if (pidarray<decltype(track1)>[i](track1)) // PID function
-        {
-          sign1 = (track1.sign() + 1) / 2;
-          hist.h1d_1p[i][sign1]->Fill(track1.pt(), 1.0 / (2.0 * constants::math::PI * track1.pt())); // h1d_n1_pt*
-          hist.h2d_1p[i][sign1][0]->Fill(track1.eta(), track1.phi());                                // h2d_n1_etaphi*
-          hist.h2d_1p[i][sign1][1]->Fill(track1.eta(), track1.phi(), track1.pt());                   // h2d_pt_etaPhi*
-        }
+        hist.h1d_1p[i % 10][sign1]->Fill(track1.pt(), 1.0 / (2.0 * constants::math::PI * track1.pt())); // h1d_n1_pt*
+        hist.h2d_1p[i % 10][sign1][0]->Fill(track1.eta(), track1.phi());                                // h2d_n1_etaphi*
+        hist.h2d_1p[i % 10][sign1][1]->Fill(track1.eta(), track1.phi(), track1.pt());                   // h2d_pt_etaPhi*
+        if (i < 1)
+          break;
       }
       //-----------------------------------------------------------------------
       etabin1 = (track1.eta() + 0.8) * 15; // 15= 24/1.6
@@ -379,24 +404,29 @@ struct r2p24id {
         etabin2 = (track2.eta() + 0.8) * 15; // 15= 24/1.6
         phibin2 = 36 * track2.phi() / (2 * constants::math::PI);
         sign2 = (track2.sign() + 1) / 2;
+        pid2 = track2.pid();
         //-----Two Particle Distribution---------------------------------------
-        for (int i = 0; i < 7; i++) // iterating through different combination of particles
-        {
-          int e = i, f = i;
-          if (i > 3) { // combinations for cross correlation
-            e = 1;
-            f = 2;
-            if (i == 5)
-              f = 3;
-            else if (i == 6)
-              e = 3;
-          }
-          if ((pidarray<decltype(track1)>[e](track1)) && (pidarray<decltype(track2)>[f](track2))) {                                       // This If statement can be avoided by using a better PID Technique, where a PID flag can be assigned to the track and that flag can be used as an index for the histogram array. I am working on a better PID, but for now I have to stick with this.
+        int i, n1, n2;
+        for (int e = pid1;; e /= 10) {
+          n1 = e % 10;
+
+          for (int f = pid2; (f % 10) >= n1; f /= 10) {
+            if ((e == 0) && (f != 0))
+              continue; // No ch-pi or ch-ka or ch-pr pairs
+
+            n2 = f % 10;
+            i = ((n2 - n1) / 2 + (n2 - n1) % 2);
+            i = i + (n1 + n2) * (1 + i) / 2; // This formula gives 0 for chch, 1 for pipi, 2 for kaka, 3->prpr, 4->pik, 5->pip, 6->kp
+
             hist.h2d_2p[i][sign1][sign2][0]->Fill(36 * etabin1 + phibin1 + 0.5, 36 * etabin2 + phibin2 + 0.5);                            // h2d_n2_eta1Phi1Eta2Phi2*
             hist.h2d_2p[i][sign1][sign2][1]->Fill(36 * etabin1 + phibin1 + 0.5, 36 * etabin2 + phibin2 + 0.5, track2.pt());               // h2d_npt_eta1Phi1Eta2Phi2*
             hist.h2d_2p[i][sign1][sign2][2]->Fill(36 * etabin1 + phibin1 + 0.5, 36 * etabin2 + phibin2 + 0.5, track1.pt());               // h2d_ptn_eta1Phi1Eta2Phi2*
             hist.h2d_2p[i][sign1][sign2][3]->Fill(36 * etabin1 + phibin1 + 0.5, 36 * etabin2 + phibin2 + 0.5, track1.pt() * track2.pt()); // h2d_ptpt_eta1Phi1Eta2Phi2*
+            if (f < 1)
+              break;
           }
+          if (e < 1)
+            break;
         }
         //---------------------------------------------------------------------
       }
