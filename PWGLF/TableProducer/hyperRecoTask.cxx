7c23c66ab292f6f008586f0f3a85f3c4cba9972d
Francesco Mazzaschi <43742195+fmazzasc@users.noreply.github.com>
2024-01-22 17:38:47 +0100

    Clean filledMothers vector (#4419)
    * Clean filledMothers vector
    
    * Please consider the following formatting changes
    
    ---------
    
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

--- PWGLF/TableProducer/hyperRecoTask.cxx
+++ PWGLF/TableProducer/hyperRecoTask.cxx
@@ -530,6 +530,7 @@ struct hyperRecoTask {
 
   void processMC(CollisionsFull const& collisions, aod::V0s const& V0s, TracksFull const& tracks, aod::BCsWithTimestamps const&, aod::McTrackLabels const& trackLabelsMC, aod::McParticles const& particlesMC)
   {
+    filledMothers.clear();
 
     for (const auto& collision : collisions) {
       hyperCandidates.clear();
