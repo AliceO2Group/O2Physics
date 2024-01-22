e1eba0215d9000f9af300794aa22b9cfbdec2d0d
fmazzasc <fmazzasc@cern.ch>
2024-01-22 15:22:11 +0100

    Clean filledMothers vector

--- PWGLF/TableProducer/hyperRecoTask.cxx
+++ PWGLF/TableProducer/hyperRecoTask.cxx
@@ -530,7 +530,8 @@ struct hyperRecoTask {
 
   void processMC(CollisionsFull const& collisions, aod::V0s const& V0s, TracksFull const& tracks, aod::BCsWithTimestamps const&, aod::McTrackLabels const& trackLabelsMC, aod::McParticles const& particlesMC)
   {
-
+    filledMothers.clear();
+    
     for (const auto& collision : collisions) {
       hyperCandidates.clear();
       auto bc = collision.bc_as<aod::BCsWithTimestamps>();
