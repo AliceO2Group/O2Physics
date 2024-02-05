0d8c62fbeb8a0060cfafb89e9fa25061d1e5110a
Alice Caluisi <a.caluisi@students.uu.nl>
2024-02-05 13:16:49 +0100

    modifying the Lund declustering task

--- PWGJE/Tasks/jetLundReclustering.cxx
+++ PWGJE/Tasks/jetLundReclustering.cxx
@@ -62,24 +62,18 @@ struct JetLundReclustering {
   JetFinder jetReclusterer;
 
   Configurable<std::string> eventSelections{"eventSelections", "sel8", "choose event selection"};  
-  //Configurable<bool> evSel{"evSel", false, "to use event selection 8 for run3"};
   Configurable<float> jetPtMin{"jetPtMin", 5.0, "minimum jet pT cut"};
   Configurable<float> jetR{"jetR", 0.4, "jet resolution parameter"};
   Configurable<float> jet_min_eta{"jet_min_eta", -0.5, "minimum jet eta"};
   Configurable<float> jet_max_eta{"jet_max_eta", 0.5, "maximum jet eta"};
   Configurable<float> vertexZCut{"vertexZCut", 10.0f, "Accepted z-vertex range"};
-  
-  int eventSelection = -1;
-  void init(o2::framework::InitContext&)
-  {
-    eventSelection = jetderiveddatautilities::initialiseEventSelection(static_cast<std::string>(eventSelections));
-  }
 
-  Filter jetFilter = aod::jet::pt > jetPtMin && aod::jet::r == nround(jetR.node() * 100.0f) && aod::jet::eta > jet_min_eta && aod::jet::eta < jet_max_eta;
-  Filter collisionFilter = nabs(aod::jcollision::posZ) < vertexZCut;
+  int eventSelection = -1;
 
   void init(InitContext const&)
   {
+    eventSelection = jetderiveddatautilities::initialiseEventSelection(static_cast<std::string>(eventSelections));
+    
     registry.add("PrimaryLundPlane_kT_3D", "Primary Lund 3D plane;ln(R/Delta);ln(k_{t}/GeV);{p}_{t}", {HistType::kTH3F, {{100, 0, 20}, {100, -20, 20}, {20, 0, 200}}});
     registry.add("PrimaryLundPlane_kT_2D", "Primary Lund 2D plane;ln(R/Delta);ln(k_{t}/GeV)", {HistType::kTH2F, {{100, 0, 20}, {100, -20, 20}}});
     registry.add("PrimaryLundPlane_z_3D", "Primary Lund 3D plane;ln(R/Delta);ln(1/z);{p}_{t}", {HistType::kTH3F, {{100, 0, 20}, {100, -20, 20}, {20, 0, 200}}});
@@ -93,6 +87,9 @@ struct JetLundReclustering {
     jetReclusterer.isReclustering = true;
     jetReclusterer.algorithm = fastjet::JetAlgorithm::cambridge_algorithm;
   }
+
+  Filter jetFilter = aod::jet::pt > jetPtMin && aod::jet::r == nround(jetR.node() * 100.0f) && aod::jet::eta > jet_min_eta && aod::jet::eta < jet_max_eta;
+  Filter collisionFilter = nabs(aod::jcollision::posZ) < vertexZCut;
 
   // Reclustering function
   template <typename T>
@@ -137,31 +134,19 @@ struct JetLundReclustering {
     if (!jetderiveddatautilities::selectCollision(collision, eventSelection)) {
       return;
     }
-    /*
-    if (evSel == true) {
-      if (!jetderiveddatautilities::selectCollision(collision, jetderiveddatautilities::JCollisionSel::sel8)) {
-        return;
-      }
-      */
-      for (const auto& jet : jets) {
-        /*
-        if (jet.collisionId() != collision.globalIndex()) {
-        continue;
+    for (const auto& jet : jets) {
+      registry.fill(HIST("jet_pt"), jet.pt());
+      registry.fill(HIST("jet_eta"), jet.pt(), jet.eta());
+      registry.fill(HIST("jet_phi"), jet.pt(), jet.phi());
+      registry.fill(HIST("jet_etaVSphi"), jet.eta(), jet.phi());
+      registry.fill(HIST("jet_PtEtaPhi"), jet.pt(), jet.eta(), jet.phi());
+      jetConstituents.clear();
+        for (auto& jetConstituent : jet.tracks_as<JetTracks>()) {
+          fastjetutilities::fillTracks(jetConstituent, jetConstituents, jetConstituent.globalIndex());
         }
-        */
-        registry.fill(HIST("jet_pt"), jet.pt());
-        registry.fill(HIST("jet_eta"), jet.pt(), jet.eta());
-        registry.fill(HIST("jet_phi"), jet.pt(), jet.phi());
-        registry.fill(HIST("jet_etaVSphi"), jet.eta(), jet.phi());
-        registry.fill(HIST("jet_PtEtaPhi"), jet.pt(), jet.eta(), jet.phi());
-        jetConstituents.clear();
-          for (auto& jetConstituent : jet.tracks_as<JetTracks>()) {
-            fastjetutilities::fillTracks(jetConstituent, jetConstituents, jetConstituent.globalIndex());
-          }
-          // Perform jet reclustering
-          jetReclustering(jet);
+        // Perform jet reclustering
+        jetReclustering(jet);
       }
-    
   }
   PROCESS_SWITCH(JetLundReclustering, processChargedJets, "Process function for charged jets", false);
 };
