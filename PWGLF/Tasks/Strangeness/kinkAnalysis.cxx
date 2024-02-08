638739cdc71cecf77849dbe005bfd35ebcc9c4d0
mdiotti <mattydiotti190798@gmail.com>
2024-02-08 12:33:30 +0100

    Bug Fixed

--- PWGLF/Tasks/Strangeness/kinkAnalysis.cxx
+++ PWGLF/Tasks/Strangeness/kinkAnalysis.cxx
@@ -836,11 +836,12 @@ struct kinkAnalysis {
                 if (cfgIsMC) {
                   if (motherPdg == particlePdgCode)
                     histos.fill(HIST("hPtPlusRecMcTrth"), mass, sigmaPt);
-                  else if (motherPdg == 1010010030 && daughterPdg == 1000010030 ) {
+                  else if (motherPdg == 1010010030 && daughterPdg == 1000010030 )
                     histos.fill(HIST("hpRes"), sigmaPt, (mcMotherPt - sigmaPt) / mcMotherPt);
                 }
                 histos.fill(HIST("hMassPlusPt"), mass, sigmaPt);
-              }
+               }
+              
 
               if ((chargeM == -1) && (chargeD == 1)) {
                 histos.fill(HIST("hBackgroundNegPosPt"), mass, sigmaPt);
