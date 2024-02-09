ada6b86d1009afb28bce75053ccbb8d34bcba89c
Chiara De Martin <chiara.de.martin@cern.ch>
2024-02-09 10:34:49 +0100

    Formatting and fix cut counter
    Co-authored-by: Maximiliano Puccio <mpuccio@users.noreply.github.com>

--- PWGLF/TableProducer/cascadeflow.cxx
+++ PWGLF/TableProducer/cascadeflow.cxx
@@ -31,9 +31,10 @@ using namespace o2::framework;
 using namespace o2::framework::expressions;
 using std::array;
 
-using DauTracks = soa::Join<aod::DauTrackExtras, aod::DauTrackTPCPIDs>;  
+using DauTracks = soa::Join<aod::DauTrackExtras, aod::DauTrackTPCPIDs>;
 
-namespace cascadev2 {
+namespace cascadev2
+{
 enum species { Xi = 0,
                Omega = 1 };
 constexpr double massSigmaParameters[4][2]{
@@ -43,7 +44,7 @@ constexpr double massSigmaParameters[4][2]{
   {1.03468e-1, 0.1898}};
 static const std::vector<std::string> massSigmaParameterNames{"p0", "p1", "p2", "p3"};
 static const std::vector<std::string> speciesNames{"Xi", "Omega"};
-}
+} // namespace cascadev2
 
 struct cascadeFlow {
 
@@ -54,30 +55,28 @@ struct cascadeFlow {
   Configurable<float> nsigmatpcPr{"nsigmatpcPr", 5, "nsigmatpcPr"};
   Configurable<float> nsigmatpcPi{"nsigmatpcPi", 5, "nsigmatpcPi"};
   Configurable<float> mintpccrrows{"mintpccrrows", 3, "mintpccrrows"};
-  
+
   template <typename TCascade, typename TDaughter>
-  bool IsCascAccepted(TCascade casc, TDaughter negExtra, TDaughter posExtra, TDaughter bachExtra, Int_t counter) //loose cuts on topological selections of cascades
+  bool IsCascAccepted(TCascade casc, TDaughter negExtra, TDaughter posExtra, TDaughter bachExtra, int& counter) // loose cuts on topological selections of cascades
   {
-    
-    //TPC cuts as those implemented for the training of the signal
-    if (doNTPCSigmaCut) { 
-      if (casc.sign() < 0) { 
-	if (TMath::Abs(posExtra.tpcNSigmaPr()) > nsigmatpcPr || TMath::Abs(negExtra.tpcNSigmaPi()) > nsigmatpcPi)
-	  return false;
+    // TPC cuts as those implemented for the training of the signal
+    if (doNTPCSigmaCut) {
+      if (casc.sign() < 0) {
+        if (std::abs(posExtra.tpcNSigmaPr()) > nsigmatpcPr || std::abs(negExtra.tpcNSigmaPi()) > nsigmatpcPi)
+          return false;
       } else if (casc.sign() > 0) {
-	if (TMath::Abs(posExtra.tpcNSigmaPi()) > nsigmatpcPi || TMath::Abs(negExtra.tpcNSigmaPr()) > nsigmatpcPr)
-	  return false;
+        if (std::abs(posExtra.tpcNSigmaPi()) > nsigmatpcPi || std::abs(negExtra.tpcNSigmaPr()) > nsigmatpcPr)
+          return false;
       }
-    } 
+    }
     counter++;
 
-    if (posExtra.tpcCrossedRows() < mintpccrrows || negExtra.tpcCrossedRows() < mintpccrrows || bachExtra.tpcCrossedRows() < mintpccrrows) 
+    if (posExtra.tpcCrossedRows() < mintpccrrows || negExtra.tpcCrossedRows() < mintpccrrows || bachExtra.tpcCrossedRows() < mintpccrrows)
       return false;
 
     counter++;
     return true;
   }
-  
 
   HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
@@ -88,39 +87,40 @@ struct cascadeFlow {
     {cascadev2::massSigmaParameters[0], 4, 2,
      cascadev2::massSigmaParameterNames, cascadev2::speciesNames},
     "Mass resolution parameters: [0]*exp([1]*x)+[2]*exp([3]*x)"};
-  
+
   float getNsigmaMass(const cascadev2::species s, const float pt, const float nsigma = 6)
   {
     const auto sigma = parSigmaMass->get(0u, s) * exp(parSigmaMass->get(1, s) * pt) + parSigmaMass->get(2, s) * exp(parSigmaMass->get(3, s) * pt);
     return nsigma * sigma;
   }
 
-  template<class collision_t, class cascade_t>
-  void fillTrainingTable(collision_t coll, cascade_t casc, int pdgCode) {
-    trainingSample(coll.centFT0M(), 
-        casc.sign(), 
-        casc.pt(), 
-        casc.eta(), 
-        casc.mXi(), 
-        casc.mOmega(), 
-        casc.mLambda(), 
-        casc.cascradius(), 
-        casc.v0radius(),
-        casc.casccosPA(coll.posX(), coll.posY(), coll.posZ()),
-        casc.v0cosPA(coll.posX(), coll.posY(), coll.posZ()),
-        casc.dcapostopv(),
-        casc.dcanegtopv(),
-        casc.dcabachtopv(),
-        casc.dcacascdaughters(),
-        casc.dcaV0daughters(),
-        casc.dcav0topv(coll.posX(), coll.posY(), coll.posZ()),
-        casc.bachBaryonCosPA(),
-        casc.bachBaryonDCAxyToPV(),
-        pdgCode); 
+  template <class collision_t, class cascade_t>
+  void fillTrainingTable(collision_t coll, cascade_t casc, int pdgCode)
+  {
+    trainingSample(coll.centFT0M(),
+                   casc.sign(),
+                   casc.pt(),
+                   casc.eta(),
+                   casc.mXi(),
+                   casc.mOmega(),
+                   casc.mLambda(),
+                   casc.cascradius(),
+                   casc.v0radius(),
+                   casc.casccosPA(coll.posX(), coll.posY(), coll.posZ()),
+                   casc.v0cosPA(coll.posX(), coll.posY(), coll.posZ()),
+                   casc.dcapostopv(),
+                   casc.dcanegtopv(),
+                   casc.dcabachtopv(),
+                   casc.dcacascdaughters(),
+                   casc.dcaV0daughters(),
+                   casc.dcav0topv(coll.posX(), coll.posY(), coll.posZ()),
+                   casc.bachBaryonCosPA(),
+                   casc.bachBaryonDCAxyToPV(),
+                   pdgCode);
   }
 
-
-  void init(InitContext const&){
+  void init(InitContext const&)
+  {
     ConfigurableAxis vertexZ{"vertexZ", {20, -10, 10}, "vertex axis for histograms"};
     histos.add("hEventVertexZ", "hEventVertexZ", kTH1F, {vertexZ});
     histos.add("hEventCentrality", "hEventCentrality", kTH1F, {{101, 0, 101}});
@@ -129,8 +129,9 @@ struct cascadeFlow {
     histos.add("hCascadeSignal", "hCascadeSignal", HistType::kTH1D, {{6, -0.5, 5.5}});
   }
 
-  void processTrainingBackground(soa::Join<aod::StraCollisions, aod::StraCents, aod::StraEvSels>::iterator const& coll, soa::Join<aod::CascCollRefs, aod::CascCores, aod::CascExtras, aod::CascBBs> const& Cascades, DauTracks const&) {
-    
+  void processTrainingBackground(soa::Join<aod::StraCollisions, aod::StraCents, aod::StraEvSels>::iterator const& coll, soa::Join<aod::CascCollRefs, aod::CascCores, aod::CascExtras, aod::CascBBs> const& Cascades, DauTracks const&)
+  {
+
     int counter = 0;
 
     if (!coll.sel8() || std::abs(coll.posZ()) > 10.) {
@@ -141,15 +142,15 @@ struct cascadeFlow {
       if (gRandom->Uniform() > downsample) {
         continue;
       }
-      
+
       float sigmaRangeXi[2]{getNsigmaMass(cascadev2::Xi, casc.pt(), sideBandStart), getNsigmaMass(cascadev2::Xi, casc.pt(), sideBandEnd)};
       float sigmaRangeOmega[2]{getNsigmaMass(cascadev2::Omega, casc.pt(), sideBandStart), getNsigmaMass(cascadev2::Omega, casc.pt(), sideBandEnd)};
 
-      if ((std::abs(casc.mXi() - constants::physics::MassXiMinus) < sigmaRangeXi[0] || 
-	   std::abs(casc.mXi() - constants::physics::MassXiMinus) > sigmaRangeXi[1]) &&
-	  (std::abs(casc.mOmega() - constants::physics::MassOmegaMinus) < sigmaRangeOmega[0] || 
-	   std::abs(casc.mOmega() - constants::physics::MassOmegaMinus) > sigmaRangeOmega[1])) {
-	continue;
+      if ((std::abs(casc.mXi() - constants::physics::MassXiMinus) < sigmaRangeXi[0] ||
+           std::abs(casc.mXi() - constants::physics::MassXiMinus) > sigmaRangeXi[1]) &&
+          (std::abs(casc.mOmega() - constants::physics::MassOmegaMinus) < sigmaRangeOmega[0] ||
+           std::abs(casc.mOmega() - constants::physics::MassOmegaMinus) > sigmaRangeOmega[1])) {
+        continue;
       }
 
       /// Add some minimal cuts for single track variables (min number of TPC clusters)
@@ -159,11 +160,11 @@ struct cascadeFlow {
 
       if (doNTPCSigmaCut) {
         if (casc.sign() < 0) {
-          if (TMath::Abs(posExtra.tpcNSigmaPr()) > nsigmatpcPr || TMath::Abs(negExtra.tpcNSigmaPi()) > nsigmatpcPi)
+          if (std::abs(posExtra.tpcNSigmaPr()) > nsigmatpcPr || std::abs(negExtra.tpcNSigmaPi()) > nsigmatpcPi)
             continue;
-	  histos.fill(HIST("hCandidate"), ++counter);
+          histos.fill(HIST("hCandidate"), ++counter);
         } else if (casc.sign() > 0) {
-          if (TMath::Abs(posExtra.tpcNSigmaPi()) > nsigmatpcPi || TMath::Abs(negExtra.tpcNSigmaPr()) > nsigmatpcPr)
+          if (std::abs(posExtra.tpcNSigmaPi()) > nsigmatpcPi || std::abs(negExtra.tpcNSigmaPr()) > nsigmatpcPr)
             continue;
           histos.fill(HIST("hCandidate"), ++counter);
         }
@@ -173,24 +174,24 @@ struct cascadeFlow {
       if (posExtra.tpcCrossedRows() < mintpccrrows || negExtra.tpcCrossedRows() < mintpccrrows || bachExtra.tpcCrossedRows() < mintpccrrows)
         continue;
       histos.fill(HIST("hCandidate"), ++counter);
-      
-      fillTrainingTable(coll, casc, 0); 
+
+      fillTrainingTable(coll, casc, 0);
     }
   }
 
-  
-  void processTrainingSignal(soa::Join<aod::StraCollisions, aod::StraCents, aod::StraEvSels>::iterator const& coll, soa::Join<aod::CascCollRefs, aod::CascCores, aod::CascMCCores, aod::CascExtras, aod::CascBBs> const& Cascades, soa::Join<aod::DauTrackExtras, aod::DauTrackTPCPIDs> const&)  {
-    
+  void processTrainingSignal(soa::Join<aod::StraCollisions, aod::StraCents, aod::StraEvSels>::iterator const& coll, soa::Join<aod::CascCollRefs, aod::CascCores, aod::CascMCCores, aod::CascExtras, aod::CascBBs> const& Cascades, soa::Join<aod::DauTrackExtras, aod::DauTrackTPCPIDs> const&)
+  {
+
     if (!coll.sel8() || std::abs(coll.posZ()) > 10.) {
       return;
     }
 
     for (auto& casc : Cascades) {
       int pdgCode{casc.pdgCode()};
-      if (!(std::abs(pdgCode) == 3312 && std::abs(casc.pdgCodeV0()) == 3122 && std::abs(casc.pdgCodeBachelor() == 211)) //Xi
-	  && !(std::abs(pdgCode) == 3334 && std::abs(casc.pdgCodeV0()) == 3122 && std::abs(casc.pdgCodeBachelor() == 321))) //Omega
-	  continue;
-      
+      if (!(std::abs(pdgCode) == 3312 && std::abs(casc.pdgCodeV0()) == 3122 && std::abs(casc.pdgCodeBachelor() == 211))     // Xi
+          && !(std::abs(pdgCode) == 3334 && std::abs(casc.pdgCodeV0()) == 3122 && std::abs(casc.pdgCodeBachelor() == 321))) // Omega
+        continue;
+
       auto negExtra = casc.negTrackExtra_as<soa::Join<aod::DauTrackExtras, aod::DauTrackTPCPIDs>>();
       auto posExtra = casc.posTrackExtra_as<soa::Join<aod::DauTrackExtras, aod::DauTrackTPCPIDs>>();
       auto bachExtra = casc.bachTrackExtra_as<soa::Join<aod::DauTrackExtras, aod::DauTrackTPCPIDs>>();
@@ -199,12 +200,11 @@ struct cascadeFlow {
       IsCascAccepted(casc, negExtra, posExtra, bachExtra, counter);
       histos.fill(HIST("hCascadeSignal"), counter);
 
-      //PDG cascades 
-      fillTrainingTable(coll, casc, pdgCode); 
+      // PDG cascades
+      fillTrainingTable(coll, casc, pdgCode);
     }
   }
-  
-  
+
   PROCESS_SWITCH(cascadeFlow, processTrainingBackground, "Process to create the training dataset for the background", true);
   PROCESS_SWITCH(cascadeFlow, processTrainingSignal, "Process to create the training dataset for the signal", false);
 };
