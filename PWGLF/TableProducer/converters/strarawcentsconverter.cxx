f5fcfd12e70e240cd39ce92af24e88b5bdc807b8
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2024-02-13 18:18:36 +0100

    PWGLF: add minor converter for fast debug/QA (#4688)
    * PWGLF: add minor converter for fast debug/QA
    
    * Please consider the following formatting changes (#237)
    
    * Forgot git add...
    
    ---------
    
    Co-authored-by: ALICE Builder <alibuild@users.noreply.github.com>

---
+++ PWGLF/TableProducer/converters/strarawcentsconverter.cxx
@@ -0,0 +1,36 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+#include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/AnalysisDataModel.h"
+#include "PWGLF/DataModel/LFStrangenessTables.h"
+
+using namespace o2;
+using namespace o2::framework;
+
+// Converts V0 version 001 to 002
+
+struct strarawcentsconverter {
+  Produces<aod::StraRawCents_001> straRawCents_001;
+
+  void process(aod::StraRawCents_000 const& straRawCents_000)
+  {
+    for (auto& values : straRawCents_000) {
+      straRawCents_001(values.multFT0A(), values.multFT0C(), values.multFV0A(), values.multNTracksPVeta1(), 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+    }
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<strarawcentsconverter>(cfgc)};
+}
