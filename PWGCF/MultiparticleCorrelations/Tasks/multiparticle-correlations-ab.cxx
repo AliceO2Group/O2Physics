ec402f13ddd4cbca984b5f1972e204b1da2dc5de
abilandz <Ante.Bilandzic@cern.ch>
2024-02-13 18:50:43 +0100

    MegaLinter ´4

--- PWGCF/MultiparticleCorrelations/Tasks/multiparticle-correlations-ab.cxx
+++ PWGCF/MultiparticleCorrelations/Tasks/multiparticle-correlations-ab.cxx
@@ -9,9 +9,6 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include <vector>
-#include <string>
-
 // O2:
 #include <CCDB/BasicCCDBManager.h>
 #include "Framework/runDataProcessing.h"
