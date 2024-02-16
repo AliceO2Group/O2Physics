e7c9b13805cce3deb528c3ed383eae73ee12467d
Veronika Barbasova <vernika.barbasova@cern.ch>
2024-02-16 15:00:19 +0100

    update

--- PWGLF/Utils/rsnOutput.h
+++ PWGLF/Utils/rsnOutput.h
@@ -1,3 +1,22 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file rsnOutput.h
+/// \brief Dynamic sparse output for resonances for O2 analysis
+///
+/// Simply copied from FemtoDreamCollisionSelection.h
+/// original author: Laura Serksnyte, TU München
+///
+/// \author Veronika Barbasova <vbarbaso@cern.ch>
+
 #ifndef PWGLF_RSNOUTPUT_H_
 #define PWGLF_RSNOUTPUT_H_
 
