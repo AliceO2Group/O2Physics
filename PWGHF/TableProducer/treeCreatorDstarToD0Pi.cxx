eb68b0f5b8989080c05b308dd1a645d5a7dd2d78
Fabrizio <fabrizio.grosa@cern.ch>
2024-02-16 12:01:25 +0100

    Update PWGHF/TableProducer/treeCreatorDstarToD0Pi.cxx
    Co-authored-by: Vít Kučera <vit.kucera@cern.ch>

--- PWGHF/TableProducer/treeCreatorDstarToD0Pi.cxx
+++ PWGHF/TableProducer/treeCreatorDstarToD0Pi.cxx
@@ -20,7 +20,6 @@
 #include "Framework/AnalysisTask.h"
 #include "Framework/runDataProcessing.h"
 
-#include "PWGHF/Core/HfHelper.h"
 #include "PWGHF/DataModel/CandidateReconstructionTables.h"
 #include "PWGHF/DataModel/CandidateSelectionTables.h"
 
