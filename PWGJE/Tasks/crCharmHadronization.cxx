907e22e59e4fe992cd0646f50526e8b8be2c640d
Christian Reckziegel <christian.reckziegel@aluno.ufabc.edu.br>
2024-02-24 13:35:23 +0100

    First commit: copy of first o2 tutorial example to be modified

---
+++ PWGJE/Tasks/crCharmHadronization.cxx
@@ -0,0 +1,48 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+// charm lambda/d0 ratio hadronization task
+//
+/// \author Christian Reckziegel <christian.reckziegel@cern.ch>
+//
+
+#include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
+
+using namespace o2;
+using namespace o2::framework;
+
+struct crCharmHadronization {
+  // Histogram registry: an object to hold your histograms
+  HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
+
+  void init(InitContext const&)
+  {
+    // define axes you want to use
+    const AxisSpec axisEta{30, -1.5, +1.5, "#eta"};
+
+    // create histograms
+    histos.add("etaHistogram", "etaHistogram", kTH1F, {axisEta});
+  }
+
+  void process(aod::TracksIU const& tracks)
+  {
+    for (auto& track : tracks) {
+      histos.fill(HIST("etaHistogram"), track.eta());
+    }
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<crCharmHadronization>(cfgc)};
+}
