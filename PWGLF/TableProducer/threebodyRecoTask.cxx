eb9689845523ad3d5043d046493f71ad5a25d8a4
yuanzhe <yuanzhe.wang@cern.ch>
2024-02-27 16:52:24 +0100

    Formatting

--- PWGLF/TableProducer/threebodyRecoTask.cxx
+++ PWGLF/TableProducer/threebodyRecoTask.cxx
@@ -527,7 +527,7 @@ struct threebodyRecoTask {
     resetHistos();
 
     for (auto& cand3body : Candidates3body) {
-      outputDataTable(0, collision.posX(), collision.posY(), collision.posZ(), //centV0M() instead of 0. once available
+      outputDataTable(0, collision.posX(), collision.posY(), collision.posZ(), // centV0M() instead of 0. once available
                       cand3body.isMatter, cand3body.invmass, cand3body.lcand.P(), cand3body.lcand.Pt(), cand3body.ct,
                       cand3body.posSV[0], cand3body.posSV[1], cand3body.posSV[2],
                       cand3body.cosPA, cand3body.dcadaughters, cand3body.dcacandtopv,
@@ -610,7 +610,7 @@ struct threebodyRecoTask {
       resetHistos();
 
       for (auto& cand3body : Candidates3body) {
-        outputMCTable(0, collision.posX(), collision.posY(), collision.posZ(), //centV0M() instead of 0. once available
+        outputMCTable(0, collision.posX(), collision.posY(), collision.posZ(), // centV0M() instead of 0. once available
                       cand3body.isMatter, cand3body.invmass, cand3body.lcand.P(), cand3body.lcand.Pt(), cand3body.ct,
                       cand3body.posSV[0], cand3body.posSV[1], cand3body.posSV[2],
                       cand3body.cosPA, cand3body.dcadaughters, cand3body.dcacandtopv,
