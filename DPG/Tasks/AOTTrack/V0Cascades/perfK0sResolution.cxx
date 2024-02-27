e58f7cfb1e832570b2c2a8e34fc23659eeba1718
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2024-02-27 12:33:16 +0100

    Add extra cut on TPC clusters

--- DPG/Tasks/AOTTrack/V0Cascades/perfK0sResolution.cxx
+++ DPG/Tasks/AOTTrack/V0Cascades/perfK0sResolution.cxx
@@ -174,7 +174,7 @@ struct perfK0sResolution {
     if (ntrack.tpcNSigmaPi() > nSigTPC || ptrack.tpcNSigmaPi() > nSigTPC) {
       return false;
     }
-    if(ntrack.tpcNcls() < extraCutTPCClusters || ptrack.tpcNcls() < extraCutTPCClusters) {
+    if (ntrack.tpcNClsCrossedRows() < extraCutTPCClusters || ptrack.tpcNClsCrossedRows() < extraCutTPCClusters) {
       return false;
     }
     // TOF selection
