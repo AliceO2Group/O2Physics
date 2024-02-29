cbcd9b22073c9d90034a82f5aa4f7924b98cdf45
skundu692 <sourav.kundu@cern.ch>
2024-02-29 19:12:01 +0100

    Add rapidity cut

--- PWGLF/Tasks/Resonances/phipbpb.cxx
+++ PWGLF/Tasks/Resonances/phipbpb.cxx
@@ -30,6 +30,7 @@
 #include "Math/Vector3D.h"
 #include "Math/Vector4D.h"
 #include "Math/GenVector/Boost.h"
+#include "TF1.h"
 
 #include "PWGLF/DataModel/EPCalibrationTables.h"
 #include "Framework/runDataProcessing.h"
@@ -86,12 +87,7 @@ struct phipbpb {
   Configurable<bool> isDeepAngle{"isDeepAngle", false, "Deep Angle cut"};
   Configurable<bool> ispTdepPID{"ispTdepPID", true, "pT dependent PID"};
   Configurable<double> cfgDeepAngle{"cfgDeepAngle", 0.04, "Deep Angle cut value"};
-  Configurable<bool> cfgRemoveOutlier{"Removeoutlier", false, "Additional Event Selection"};
-  Configurable<double> cfgEvtSelpar0{"cfgEvtSelpar0", 0.0, "Event selection par0"};
-  Configurable<double> cfgEvtSelpar1{"cfgEvtSelpar1", 0.0, "Event selection par1"};
-  Configurable<double> cfgEvtSelpar2{"cfgEvtSelpar2", 0.0, "Event selection par2"};
-  Configurable<double> cfgEvtSelpar3{"cfgEvtSelpar3", 0.0, "Event selection par3"};
-
+  Configurable<double> confRapidity{"confRapidity", 0.5, "Rapidity cut"};
   ConfigurableAxis configThnAxisInvMass{"configThnAxisInvMass", {120, 0.98, 1.1}, "#it{M} (GeV/#it{c}^{2})"};
   ConfigurableAxis configThnAxisPt{"configThnAxisPt", {100, 0.0, 10.}, "#it{p}_{T} (GeV/#it{c})"};
   ConfigurableAxis configThnAxisCosThetaStar{"configThnAxisCosThetaStar", {10, 0.0, 1.}, "cos(#vartheta)"};
@@ -99,13 +95,15 @@ struct phipbpb {
   ConfigurableAxis configThnAxisPhiminusPsi{"configThnAxisPhiminusPsi", {6, 0.0, TMath::Pi()}, "#phi - #psi"};
   ConfigurableAxis configThnAxisV2{"configThnAxisV2", {200, -1, 1}, "V2"};
   ConfigurableAxis configThnAxisSA{"configThnAxisSA", {200, -1, 1}, "SA"};
+  Configurable<bool> additionalEvsel{"additionalEvsel", false, "Additional event selcection"};
+  Configurable<bool> timFrameEvsel{"timFrameEvsel", false, "TPC Time frame boundary cut"};
 
   Filter collisionFilter = nabs(aod::collision::posZ) < cfgCutVertex;
   Filter acceptanceFilter = (nabs(aod::track::eta) < cfgCutEta && nabs(aod::track::pt) > cfgCutPT);
   Filter DCAcutFilter = (nabs(aod::track::dcaXY) < cfgCutDCAxy) && (nabs(aod::track::dcaZ) < cfgCutDCAz);
   Filter PIDcutFilter = nabs(aod::pidtpc::tpcNSigmaKa) < nsigmaCutTPC;
 
-  using EventCandidates = soa::Filtered<soa::Join<aod::Collisions, aod::EvSels, aod::FT0Mults, aod::FV0Mults, aod::TPCMults, aod::CentFV0As, aod::CentFT0Ms, aod::CentFT0Cs, aod::CentFT0As, aod::EPCalibrationTables>>;
+  using EventCandidates = soa::Filtered<soa::Join<aod::Collisions, aod::EvSels, aod::FT0Mults, aod::FV0Mults, aod::TPCMults, aod::CentFV0As, aod::CentFT0Ms, aod::CentFT0Cs, aod::CentFT0As, aod::EPCalibrationTables, aod::Mults>>;
   using TrackCandidates = soa::Filtered<soa::Join<aod::Tracks, aod::TracksExtra, aod::TracksDCA, aod::TrackSelection, aod::pidTPCFullKa, aod::pidTOFFullKa>>;
 
   using EventCandidatesMC = soa::Join<aod::Collisions, aod::EvSels, aod::McCollisionLabels>;
@@ -116,6 +114,14 @@ struct phipbpb {
   Partition<TrackCandidates> negTracks = aod::track::signed1Pt < cfgCutCharge;
 
   HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
+
+  // Event selection cuts - Alex
+  TF1* fMultPVCutLow = nullptr;
+  TF1* fMultPVCutHigh = nullptr;
+  // TF1* fMultCutLow = nullptr;
+  // TF1* fMultCutHigh = nullptr;
+  // TF1* fMultMultPVCut = nullptr;
+
   void init(o2::framework::InitContext&)
   {
     const AxisSpec thnAxisInvMass{configThnAxisInvMass, "#it{M} (GeV/#it{c}^{2})"};
@@ -126,14 +132,10 @@ struct phipbpb {
     const AxisSpec thnAxisCentrality{configThnAxisCentrality, "Centrality (%)"};
     const AxisSpec thnAxisV2{configThnAxisV2, "V2"};
     const AxisSpec thnAxisSA{configThnAxisSA, "SA"};
-
-    histos.add("hFTOMvsTPC", "Mult correlation FT0M vs. TPC", kTH2F, {{600, -0.5f, 59999.5f}, {60, -0.5f, 5999.5f}});
-    histos.add("hFTOCvsTPC", "Mult correlation FT0C vs. TPC", kTH2F, {{600, -0.5f, 59999.5f}, {60, -0.5f, 5999.5f}});
-    histos.add("hFTOAvsTPC", "Mult correlation FT0A vs. TPC", kTH2F, {{600, -0.5f, 59999.5f}, {60, -0.5f, 5999.5f}});
-    histos.add("hFTOMvsTPCSelected", "Mult correlation FT0M vs. TPC after selection", kTH2F, {{600, -0.5f, 59999.5f}, {60, -0.5f, 5999.5f}});
-    histos.add("hFTOCvsTPCSelected", "Mult correlation FT0C vs. TPC after selection", kTH2F, {{600, -0.5f, 59999.5f}, {60, -0.5f, 5999.5f}});
-    histos.add("hFTOAvsTPCSelected", "Mult correlation FT0A vs. TPC after selection", kTH2F, {{600, -0.5f, 59999.5f}, {60, -0.5f, 5999.5f}});
-    histos.add("hCentrality", "Centrality distribution", kTH1F, {{201, -0.5, 200.5}});
+    histos.add("hpTvsRapidity", "pT vs Rapidity", kTH2F, {{100, 0.0f, 10.0f}, {300, -1.5f, 1.5f}});
+    histos.add("hFTOCvsTPC", "Mult correlation FT0C vs. TPC", kTH2F, {{80, 0.0f, 80.0f}, {600, -0.5f, 5999.5f}});
+    histos.add("hFTOCvsTPCSelected", "Mult correlation FT0C vs. TPC after selection", kTH2F, {{80, 0.0f, 80.0f}, {600, -0.5f, 5999.5f}});
+    histos.add("hCentrality", "Centrality distribution", kTH1F, {{200, 0.0, 200.0}});
     histos.add("hVtxZ", "Vertex distribution in Z;Z (cm)", kTH1F, {{400, -20.0, 20.0}});
     histos.add("hEta", "Eta distribution", kTH1F, {{200, -1.0f, 1.0f}});
     histos.add("hDcaxy", "Dcaxy distribution", kTH1F, {{200, -1.0f, 1.0f}});
@@ -144,9 +146,39 @@ struct phipbpb {
 
     histos.add("hSparseV2SASameEvent", "THn for V2 and SA in Same Event", HistType::kTHnSparseF, {thnAxisInvMass, thnAxisPt, thnAxisCosThetaStarOP, thnAxisCosThetaStarIP, thnAxisPhiminusPsi, thnAxisV2, thnAxisSA, thnAxisCentrality});
     histos.add("hSparseV2SAMixedEvent", "THn for V2 and SA in Mixed Event", HistType::kTHnSparseF, {thnAxisInvMass, thnAxisPt, thnAxisCosThetaStarOP, thnAxisCosThetaStarIP, thnAxisPhiminusPsi, thnAxisV2, thnAxisSA, thnAxisCentrality});
+
+    // Event selection cut additional - Alex
+    if (additionalEvsel) {
+      fMultPVCutLow = new TF1("fMultPVCutLow", "[0]+[1]*x+[2]*x*x+[3]*x*x*x - 2.5*([4]+[5]*x+[6]*x*x+[7]*x*x*x+[8]*x*x*x*x)", 0, 100);
+      fMultPVCutLow->SetParameters(2834.66, -87.0127, 0.915126, -0.00330136, 332.513, -12.3476, 0.251663, -0.00272819, 1.12242e-05);
+      fMultPVCutHigh = new TF1("fMultPVCutHigh", "[0]+[1]*x+[2]*x*x+[3]*x*x*x + 2.5*([4]+[5]*x+[6]*x*x+[7]*x*x*x+[8]*x*x*x*x)", 0, 100);
+      fMultPVCutHigh->SetParameters(2834.66, -87.0127, 0.915126, -0.00330136, 332.513, -12.3476, 0.251663, -0.00272819, 1.12242e-05);
+      // fMultCutLow = new TF1("fMultCutLow", "[0]+[1]*x+[2]*x*x+[3]*x*x*x - 2.5*([4]+[5]*x)", 0, 100);
+      // fMultCutLow->SetParameters(1893.94, -53.86, 0.502913, -0.0015122, 109.625, -1.19253);
+      // fMultCutHigh = new TF1("fMultCutHigh", "[0]+[1]*x+[2]*x*x+[3]*x*x*x + 3.*([4]+[5]*x)", 0, 100);
+      // fMultCutHigh->SetParameters(1893.94, -53.86, 0.502913, -0.0015122, 109.625, -1.19253);
+      // fMultMultPVCut = new TF1("fMultMultPVCut", "[0]+[1]*x+[2]*x*x", 0, 5000);
+      // fMultMultPVCut->SetParameters(-0.1, 0.785, -4.7e-05);
+    }
   }
 
   double massKa = o2::constants::physics::MassKPlus;
+
+  template <typename TCollision>
+  bool eventSelected(TCollision collision, const float& centrality)
+  {
+    if (collision.alias_bit(kTVXinTRD)) {
+      // TRD triggered
+      return 0;
+    }
+    auto multNTracksPV = collision.multNTracksPV();
+    if (multNTracksPV < fMultPVCutLow->Eval(centrality))
+      return 0;
+    if (multNTracksPV > fMultPVCutHigh->Eval(centrality))
+      return 0;
+
+    return 1;
+  }
 
   template <typename T>
   bool selectionTrack(const T& candidate)
@@ -229,37 +261,29 @@ struct phipbpb {
 
   void processSameEvent(EventCandidates::iterator const& collision, TrackCandidates const& tracks, aod::BCs const&)
   {
-    // auto bc = collision.bc_as<aod::BCsWithTimestamps>(); /// adding timestamp to access magnetic field later
-    // initCCDB(bc);
-
     if (!collision.sel8()) {
       return;
     }
+
     if (!collision.triggereventep()) {
+      return;
+    }
+    if (timFrameEvsel && !collision.selection_bit(aod::evsel::kNoTimeFrameBorder)) {
       return;
     }
     auto posThisColl = posTracks->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
     auto negThisColl = negTracks->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
     auto centrality = collision.centFT0C();
-    auto multFT0M = collision.multFT0M();
-    auto multFT0C = collision.multFT0C();
-    auto multFT0A = collision.multFT0A();
-    auto multTPC = collision.multTPC();
+    auto multTPC = collision.multNTracksPV();
     auto psiFT0C = collision.psiFT0C();
-    histos.fill(HIST("hFTOMvsTPC"), multFT0M, multTPC);
-    histos.fill(HIST("hFTOCvsTPC"), multFT0C, multTPC);
-    histos.fill(HIST("hFTOCvsTPC"), multFT0A, multTPC);
-    if (cfgRemoveOutlier) {
-      if ((multTPC > (cfgEvtSelpar0 * multFT0C + cfgEvtSelpar1)) || (multTPC < (cfgEvtSelpar2 * multFT0C + cfgEvtSelpar3))) {
-        return;
-      }
+    histos.fill(HIST("hFTOCvsTPC"), centrality, multTPC);
+    if (additionalEvsel && !eventSelected(collision, centrality)) {
+      return;
     }
+    histos.fill(HIST("hFTOCvsTPCSelected"), centrality, multTPC);
     histos.fill(HIST("hPsiFT0C"), centrality, psiFT0C);
     histos.fill(HIST("hCentrality"), centrality);
     histos.fill(HIST("hVtxZ"), collision.posZ());
-    histos.fill(HIST("hFTOMvsTPCSelected"), multFT0M, multTPC);
-    histos.fill(HIST("hFTOCvsTPCSelected"), multFT0C, multTPC);
-    histos.fill(HIST("hFTOCvsTPCSelected"), multFT0A, multTPC);
     for (auto track1 : posThisColl) {
       // track selection
       if (!selectionTrack(track1)) {
@@ -300,6 +324,10 @@ struct phipbpb {
         KaonPlus = ROOT::Math::PxPyPzMVector(track1.px(), track1.py(), track1.pz(), massKa);
         KaonMinus = ROOT::Math::PxPyPzMVector(track2.px(), track2.py(), track2.pz(), massKa);
         PhiMesonMother = KaonPlus + KaonMinus;
+        histos.fill(HIST("hpTvsRapidity"), PhiMesonMother.Pt(), PhiMesonMother.Rapidity());
+        if (TMath::Abs(PhiMesonMother.Rapidity()) > confRapidity) {
+          continue;
+        }
         ROOT::Math::Boost boost{PhiMesonMother.BoostToCM()};
         fourVecDauCM = boost(KaonMinus);
         threeVecDauCM = fourVecDauCM.Vect();
@@ -328,22 +356,20 @@ struct phipbpb {
       if (!collision1.triggereventep() || !collision2.triggereventep()) {
         return;
       }
+      if (timFrameEvsel && (!collision1.selection_bit(aod::evsel::kNoTimeFrameBorder) || !collision2.selection_bit(aod::evsel::kNoTimeFrameBorder))) {
+        return;
+      }
       auto posThisColl = posTracks->sliceByCached(aod::track::collisionId, collision1.globalIndex(), cache);
       auto negThisColl = negTracks->sliceByCached(aod::track::collisionId, collision2.globalIndex(), cache);
       auto centrality = collision1.centFT0C();
+      auto centrality2 = collision2.centFT0C();
       auto psiFT0C = collision1.psiFT0C();
-      auto multFT0C1 = collision1.multFT0C();
-      auto multTPC1 = collision1.multTPC();
-      auto multFT0C2 = collision2.multFT0C();
-      auto multTPC2 = collision2.multTPC();
 
-      if (cfgRemoveOutlier) {
-        if ((multTPC1 > (cfgEvtSelpar0 * multFT0C1 + cfgEvtSelpar1)) || (multTPC1 < (cfgEvtSelpar2 * multFT0C1 + cfgEvtSelpar3))) {
-          continue;
-        }
-        if ((multTPC2 > (cfgEvtSelpar0 * multFT0C2 + cfgEvtSelpar1)) || (multTPC2 < (cfgEvtSelpar2 * multFT0C2 + cfgEvtSelpar3))) {
-          continue;
-        }
+      if (additionalEvsel && !eventSelected(collision1, centrality)) {
+        return;
+      }
+      if (additionalEvsel && !eventSelected(collision2, centrality2)) {
+        return;
       }
       for (auto& [track1, track2] : o2::soa::combinations(o2::soa::CombinationsFullIndexPolicy(posThisColl, negThisColl))) {
         // track selection
@@ -363,6 +389,9 @@ struct phipbpb {
         KaonPlus = ROOT::Math::PxPyPzMVector(track1.px(), track1.py(), track1.pz(), massKa);
         KaonMinus = ROOT::Math::PxPyPzMVector(track2.px(), track2.py(), track2.pz(), massKa);
         PhiMesonMother = KaonPlus + KaonMinus;
+        if (TMath::Abs(PhiMesonMother.Rapidity()) > confRapidity) {
+          continue;
+        }
         ROOT::Math::Boost boost{PhiMesonMother.BoostToCM()};
         fourVecDauCM = boost(KaonMinus);
         threeVecDauCM = fourVecDauCM.Vect();
