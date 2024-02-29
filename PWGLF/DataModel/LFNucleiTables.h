309429cff201b75435dbf48c9010e7955c759f68
Giovanni Malfattore <giovanni.malfattore@cern.ch>
2024-02-29 18:19:54 +0100

    Fix formatting

--- PWGLF/DataModel/LFNucleiTables.h
+++ PWGLF/DataModel/LFNucleiTables.h
@@ -33,7 +33,7 @@ DECLARE_SOA_COLUMN(RunNumber, runNumber, int);
 DECLARE_SOA_COLUMN(CentFV0M, centFV0M, float);
 DECLARE_SOA_COLUMN(CentFT0M, centFT0M, float);
 DECLARE_SOA_DYNAMIC_COLUMN(Selection_Bit, selection_bit, //! Dummy
-                           [](o2::aod::evsel::EventSelectionFlags v) -> bool { return true ;});
+                           [](o2::aod::evsel::EventSelectionFlags v) -> bool { return true; });
 } // namespace fullEvent
 DECLARE_SOA_TABLE(LfNuclEvents, "AOD", "LFNUCLEvent",
                   o2::soa::Index<>,
