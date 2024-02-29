aa5ddf4670fc98558473d67aa582f67352b444cf
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2024-02-29 21:01:34 +0100

    Update multiplicityTable.cxx

--- Common/TableProducer/multiplicityTable.cxx
+++ Common/TableProducer/multiplicityTable.cxx
@@ -415,7 +415,7 @@ struct MultiplicityTableTaskIndexed {
           case kPVMults: // PV multiplicity
           {
             // use only one single grouping operation, then do loop
-            const auto& tracksThisCollision = pvContribTracksIUEta1.sliceByCached(perCol, collision.globalIndex(), cache);
+            const auto& tracksThisCollision = pvContribTracksIUEta1.sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
             multNContribsEta1 = tracksThisCollision.size();
             for (auto track : tracksThisCollision) {
               if (std::abs(track.eta()) < 0.8)
