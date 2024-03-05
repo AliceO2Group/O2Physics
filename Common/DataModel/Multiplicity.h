600d086fa1666843903e7a7e0a69bda29ed97c85
ALICE Builder <alibuild@users.noreply.github.com>
2024-03-05 17:32:40 +0100

    Please consider the following formatting changes (#251)

--- Common/DataModel/Multiplicity.h
+++ Common/DataModel/Multiplicity.h
@@ -104,7 +104,7 @@ DECLARE_SOA_TABLE(MultsExtra, "AOD", "MULTEXTRA", //!
                   mult::MultNTracksHasITS, mult::MultNTracksHasTPC, mult::MultNTracksHasTOF, mult::MultNTracksHasTRD,
                   mult::MultNTracksITSOnly, mult::MultNTracksTPCOnly, mult::MultNTracksITSTPC, mult::BCNumber);
 DECLARE_SOA_TABLE(MultSelections, "AOD", "MULTSELECTIONS", //!
-                  evsel::Selection); // for derived data / QA studies
+                  evsel::Selection);                       // for derived data / QA studies
 using MultExtra = MultsExtra::iterator;
 DECLARE_SOA_TABLE(MultsExtraMC, "AOD", "MULTEXTRAMC", //! Table for the MC information
                   mult::MultMCFT0A, mult::MultMCFT0C, mult::MultMCNParticlesEta05, mult::MultMCNParticlesEta08, mult::MultMCNParticlesEta10, o2::soa::Marker<1>);
