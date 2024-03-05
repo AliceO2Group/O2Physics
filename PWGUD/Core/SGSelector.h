c1be2285bf211164549f9aeca1e0ba4c8ef09fa3
Sasha Bylinkin <37345380+abylinkin@users.noreply.github.com>
2024-03-05 18:08:42 +0100

    Add files via upload

--- PWGUD/Core/SGSelector.h
+++ PWGUD/Core/SGSelector.h
@@ -1,14 +1,3 @@
-// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
-//
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
-
 #ifndef PWGUD_CORE_SGSELECTOR_H_
 #define PWGUD_CORE_SGSELECTOR_H_
 
@@ -19,101 +8,71 @@
 #include "PWGUD/Core/UDHelpers.h"
 #include "PWGUD/Core/SGCutParHolder.h"
 
-class SGSelector
-{
- public:
-  SGSelector() : fPDG(TDatabasePDG::Instance()) {}
+class SGSelector {
+public:
+    SGSelector() : fPDG(TDatabasePDG::Instance()) {}
 
-  template <typename CC, typename BCs, typename TCs, typename FWs>
-  int Print(SGCutParHolder diffCuts, CC& collision, BCs& bcRange, TCs& tracks, FWs& fwdtracks)
-  {
-    LOGF(info, "Size of array %i", collision.size());
-    return 1;
-  }
-
-  template <typename CC, typename BCs, typename TCs, typename FWs>
-  int IsSelected(SGCutParHolder diffCuts, CC& collision, BCs& bcRange, TCs& tracks, FWs& fwdtracks)
-  {
-    LOGF(debug, "Collision %f", collision.collisionTime());
-    LOGF(debug, "Number of close BCs: %i", bcRange.size());
-
-    bool gA = true, gC = true;
-    for (auto const& bc : bcRange) {
-      if (!udhelpers::cleanFITA(bc, diffCuts.maxFITtime(), diffCuts.FITAmpLimits()))
-        gA = false;
-      if (!udhelpers::cleanFITC(bc, diffCuts.maxFITtime(), diffCuts.FITAmpLimits()))
-        gC = false;
-    }
-    if (!gA && !gC)
-      return 3;
-
-    LOGF(debug, "FwdTracks %i", fwdtracks.size());
-    for (auto& fwdtrack : fwdtracks) {
-      if (fwdtrack.trackType() == 0 || fwdtrack.trackType() == 3) {
-        return 4;
-      }
+    template <typename CC, typename BCs, typename TCs, typename FWs>
+    int Print(SGCutParHolder diffCuts, CC& collision, BCs& bcRange, TCs& tracks, FWs& fwdtracks) {
+        LOGF(info, "Size of array %i", collision.size());
+        return 1;
     }
 
-    double rgtrwTOF = 0.0;
-    for (auto& track : tracks) {
-      if (track.isGlobalTrack() && !track.isPVContributor()) {
-        return 5;
-      }
-      if (diffCuts.globalTracksOnly() && track.isPVContributor() && !track.isGlobalTrack()) {
-        return 6;
-      }
-      if (!diffCuts.ITSOnlyTracks() && track.isPVContributor() && !track.hasTPC()) {
-        return 7;
-      }
-      if (track.isPVContributor() && track.hasTOF()) {
-        rgtrwTOF += 1.0;
-      }
-    }
-    if (collision.numContrib() > 0) {
-      rgtrwTOF /= static_cast<double>(collision.numContrib());
-    }
-    if (rgtrwTOF < diffCuts.minRgtrwTOF()) {
-      return 8;
-    }
+    template <typename CC, typename BCs>
+    //int IsSelected(SGCutParHolder diffCuts, CC& collision, BCs& bcRange, TCs& tracks, FWs& fwdtracks) {
+    int IsSelected(SGCutParHolder diffCuts, CC& collision, BCs& bcRange) {
+        LOGF(debug, "Collision %f", collision.collisionTime());
+        LOGF(debug, "Number of close BCs: %i", bcRange.size());
 
-    if (collision.numContrib() < diffCuts.minNTracks() || collision.numContrib() > diffCuts.maxNTracks()) {
-      return 9;
+        bool gA = true, gC = true;
+	int bA = 0, bC = 0;
+	int64_t bcA[5], bcC[5];
+        for (auto const& bc : bcRange) {
+	    bcA[bA] = 0;
+	    bcC[bC] = 0;
+            if (!udhelpers::cleanFITA(bc, diffCuts.maxFITtime(), diffCuts.FITAmpLimits())) {gA = false; bcA[bA] = bc.globalBC(); bA++;}
+            if (!udhelpers::cleanFITC(bc, diffCuts.maxFITtime(), diffCuts.FITAmpLimits())) {gC = false; bcC[bC] = bc.globalBC(); bC++;}
+        }
+        if (!gA && !gC) return 3;
+	if (bA > 1 || bC > 1){
+        LOGF(info, "Number of BCs with FT0 signal: %i, %i", bA, bC);
+	for (int i = 0; i < bA; i++){
+		LOGF(info, "Busy BC A-side: %i", bcA[i]);
+	}
+	for (int i = 0; i < bC; i++){
+		LOGF(info, "Busy BC C-side: %i", bcC[i]);
+	}
+	}
+        if (collision.numContrib() < diffCuts.minNTracks() || collision.numContrib() > diffCuts.maxNTracks()) {
+            return 4;
+        }
+        return gA && gC ? 2 : (gA ? 0 : 1);
+}
+  template <typename TFwdTrack>
+    int FwdTrkSelector(TFwdTrack const& fwdtrack) {
+    if (fwdtrack.trackType() == 0 || fwdtrack.trackType() == 3) return 1;
+    else return 0;        
     }
-
-    // PID, pt, and eta of tracks, invariant mass, and net charge
-    // consider only vertex tracks
-
-    // which particle hypothesis?
+  template <typename TTrack>
+    int TrkSelector(SGCutParHolder diffCuts, TTrack const& track) {
     auto mass2Use = 0.;
     TParticlePDG* pdgparticle = fPDG->GetParticle(diffCuts.pidHypothesis());
     if (pdgparticle != nullptr) {
       mass2Use = pdgparticle->Mass();
     }
-
-    auto netCharge = 0;
     auto lvtmp = TLorentzVector();
-    auto ivm = TLorentzVector();
-    for (auto& track : tracks) {
-      if (track.isPVContributor()) {
-        // pt
-        lvtmp.SetXYZM(track.px(), track.py(), track.pz(), mass2Use);
-        if (lvtmp.Perp() < diffCuts.minPt() || lvtmp.Perp() > diffCuts.maxPt()) {
-          return 10;
-        }
-        // eta
-        if (lvtmp.Eta() < diffCuts.minEta() || lvtmp.Eta() > diffCuts.maxEta()) {
-          return 11;
-        }
-        netCharge += track.sign();
-        ivm += lvtmp;
-      }
+    if (!track.isPVContributor()) return 1;
+    lvtmp.SetXYZM(track.px(), track.py(), track.pz(), mass2Use);
+    if (lvtmp.Perp() < diffCuts.minPt() || lvtmp.Perp() > diffCuts.maxPt()) return 2;
+    if (lvtmp.Eta() < diffCuts.minEta() || lvtmp.Eta() > diffCuts.maxEta()) return 3;
+    if (diffCuts.globalTracksOnly() && !track.isGlobalTrack()) return 4;
+    if (!diffCuts.ITSOnlyTracks() && !track.hasTPC()) return 5;
+    return 0;
     }
 
-    return gA && gC ? 2 : (gA ? 0 : 1);
-  }
-
- private:
-  TDatabasePDG* fPDG;
+private:
+    TDatabasePDG* fPDG;
 };
 
 #endif // PWGUD_CORE_SGSELECTOR_H_
+
