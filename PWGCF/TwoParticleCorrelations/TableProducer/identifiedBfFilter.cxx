e178f90036ece0ec8e8abe7e24cf3e632aafe303
bghanley1995 <bghanley1995@gmail.com>
2024-03-07 14:58:56 -0500

    Removed fhParticleSpecies which was leftover from testing

--- PWGCF/TwoParticleCorrelations/TableProducer/identifiedBfFilter.cxx
+++ PWGCF/TwoParticleCorrelations/TableProducer/identifiedBfFilter.cxx
@@ -80,7 +80,6 @@ TH1F* fhPtNegB = nullptr;
 TH1F* fhPtNegA[kIdBfNoOfSpecies] = {nullptr};
 TH2F* fhNPosNegA[kIdBfNoOfSpecies] = {nullptr};
 TH1F* fhDeltaNA[kIdBfNoOfSpecies] = {nullptr};
-TH1F* fhParticleSpecies = nullptr;
 
 TH1F* fhEtaB = nullptr;
 TH1F* fhEtaA = nullptr;
@@ -621,7 +620,6 @@ struct IdentifiedBfFilterTracks {
       fhDCAzB = new TH1F("DCAzB", "DCA_{z} distribution for reconstructed before;DCA_{z} (cm);counts", 1000, -4.0, 4.0);
       fhDCAzA = new TH1F("DCAzA", "DCA_{z} distribution for reconstructed;DCA_{z} (cm);counts", 1000, -4.0, 4.0);
       fhFineDCAzA = new TH1F("FineDCAzA", "DCA_{z} distribution for reconstructed;DCA_{z} (cm);counts", 4000, -1.0, 1.0);
-      fhParticleSpecies = new TH1F("fhParticleSpecies","Particle Species",(int)kIdBfNoOfSpecies,0,(int)kIdBfNoOfSpecies);
 
       if (checkAmbiguousTracks) {
         /* let's allocate the ambigous tracks tracking histograms*/
@@ -667,7 +665,6 @@ struct IdentifiedBfFilterTracks {
       fOutputList->Add(fhDCAzB);
       fOutputList->Add(fhDCAzA);
       fOutputList->Add(fhFineDCAzA);
-      fOutputList->Add(fhParticleSpecies);
       if (checkAmbiguousTracks) {
         fOutputList->Add(fhAmbiguousTrackType);
         fOutputList->Add(fhAmbiguousTrackPt);
@@ -1061,7 +1058,6 @@ inline MatchRecoGenSpecies IdentifiedBfFilterTracks::IdentifyTrack(TrackObject c
     if (doublematch) {
       return kWrongSpecies;
     } else {
-      fhParticleSpecies->Fill((int)sp_min_nsigma);
       return sp_min_nsigma;
     }
   } else {
