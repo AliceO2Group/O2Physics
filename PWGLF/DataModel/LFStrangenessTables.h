6eb8d11dcb56df8be0fbb1bacdc0e7d126252f70
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2024-03-07 11:36:47 -0300

    Common/LF: add some extras for QA of evsel based on PF info

--- PWGLF/DataModel/LFStrangenessTables.h
+++ PWGLF/DataModel/LFStrangenessTables.h
@@ -35,8 +35,17 @@ DECLARE_SOA_TABLE(StraCents, "AOD", "STRACENTS", //! centrality percentiles
 DECLARE_SOA_TABLE(StraRawCents_000, "AOD", "STRARAWCENTS", //! debug information
                   mult::MultFT0A, mult::MultFT0C, mult::MultFV0A, mult::MultNTracksPVeta1);
 DECLARE_SOA_TABLE_VERSIONED(StraRawCents_001, "AOD", "STRARAWCENTS", 1, //! debug information
-                            mult::MultFT0A, mult::MultFT0C, mult::MultFV0A, mult::MultNTracksPVeta1,
-                            mult::MultZNA, mult::MultZNC, mult::MultZEM1, mult::MultZEM2, mult::MultZPA, mult::MultZPC);
+                            mult::MultFT0A, mult::MultFT0C, mult::MultFV0A, // FIT detectors 
+                            mult::MultNTracksPVeta1, // track multiplicities
+                            mult::MultZNA, mult::MultZNC, mult::MultZEM1, // ZDC signals
+                            mult::MultZEM2, mult::MultZPA, mult::MultZPC);
+DECLARE_SOA_TABLE_VERSIONED(StraRawCents_002, "AOD", "STRARAWCENTS", 2, //! debug information
+                            mult::MultFT0A, mult::MultFT0C, mult::MultFV0A, // FIT detectors 
+                            mult::MultNTracksPVeta1, // track multiplicities with eta cut for INEL>0
+                            mult::MultNTracksTPCOnly, mult::MultNTracksITSTPC, // track multiplicities, PV contribs, no eta cut
+                            mult::MultAllTracksTPCOnly, mult::MultAllTracksITSTPC, // track multiplicities, all, no eta cut
+                            mult::MultZNA, mult::MultZNC, mult::MultZEM1, // ZDC signals
+                            mult::MultZEM2, mult::MultZPA, mult::MultZPC);
 DECLARE_SOA_TABLE(StraEvSels, "AOD", "STRAEVSELS", //! event selection: sel8
                   evsel::Sel8, evsel::Selection);
 DECLARE_SOA_TABLE(StraFT0AQVs, "AOD", "STRAFT0AQVS", //! t0a Qvec
@@ -50,7 +59,7 @@ DECLARE_SOA_TABLE(StraFV0AQVs, "AOD", "STRAFV0AQVS", //! v0a Qvec
 DECLARE_SOA_TABLE(StraStamps, "AOD", "STRASTAMPS", //! information for ID-ing mag field if needed
                   bc::RunNumber, timestamp::Timestamp);
 
-using StraRawCents = StraRawCents_001;
+using StraRawCents = StraRawCents_002;
 using StraCollision = StraCollisions::iterator;
 using StraCent = StraCents::iterator;
 
