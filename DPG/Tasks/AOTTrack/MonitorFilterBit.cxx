96656b0493648d39c5c5c3b1f77369d625bc2c37
Andrea <andrea.rossicern.ch>
2024-03-11 09:58:42 +0100

    fixing clang formatting

--- DPG/Tasks/AOTTrack/MonitorFilterBit.cxx
+++ DPG/Tasks/AOTTrack/MonitorFilterBit.cxx
@@ -151,9 +151,8 @@ struct CheckFilterBit {
     histos.add("Tracks/RecoMCPhysPrimCollMatch/histptMCTPConlyWithClusters", "TPConlyWithClusters;#it{p}_{T}^{gen} (GeV/#it{c});#it{#eta};#it{#varphi};NclustTPC", HistType::kTHnF, {axisPt, axisEta, axisPhi, axisNclustTPC});
     histos.add("Tracks/RecoMCPhysPrimCollMatch/histptTPConlyWithClusters", "TPConlyWithClusters;#it{p}_{T} (GeV/#it{c});#it{#eta};#it{#varphi};NclustTPC", HistType::kTHnF, {axisPt, axisEta, axisPhi, axisNclustTPC});
 
-    histos.add("Tracks/RecoMCPhysPrimCollMatch/histptMCFB0resolCurv", "FB0;#it{p}_{T}^{gen} (GeV/#it{c});#it{#eta};#it{p}_{T}^{gen}/#it{p}_{T}^{reco}-1", kTH3D, {{200,0,20},{10,-1,1},{200,-0.2,0.2}});
+    histos.add("Tracks/RecoMCPhysPrimCollMatch/histptMCFB0resolCurv", "FB0;#it{p}_{T}^{gen} (GeV/#it{c});#it{#eta};#it{p}_{T}^{gen}/#it{p}_{T}^{reco}-1", kTH3D, {{200, 0, 20}, {10, -1, 1}, {200, -0.2, 0.2}});
 
-    
     histos.add("Tracks/RecoMCRad1to15cmCollMatch/histptFB0", "FB0;#it{p}_{T} (GeV/#it{c});#it{#eta};#it{#varphi}", kTH3D, {axisPt, axisEta, axisPhi});
     histos.add("Tracks/RecoMCRad1to15cmCollMatch/histptFB1", "FB1;#it{p}_{T} (GeV/#it{c});#it{#eta};#it{#varphi}", kTH3D, {axisPt, axisEta, axisPhi});
     histos.add("Tracks/RecoMCRad1to15cmCollMatch/histptFB2", "FB2;#it{p}_{T} (GeV/#it{c});#it{#eta};#it{#varphi}", kTH3D, {axisPt, axisEta, axisPhi});
@@ -443,7 +442,7 @@ struct CheckFilterBit {
               if (track.isGlobalTrack()) {
                 histos.fill(HIST("Tracks/RecoMCPhysPrimCollMatch/histptFB0"), track.pt(), track.eta(), track.phi());
                 histos.fill(HIST("Tracks/RecoMCPhysPrimCollMatch/histptMCFB0"), mcparticle.pt(), track.eta(), track.phi());
-		histos.fill(HIST("Tracks/RecoMCPhysPrimCollMatch/histptMCFB0resolCurv"),mcparticle.pt(), track.eta(),(mcparticle.pt()/track.pt()-1.));// did not use signed1Pt just to avoid getting the charge for MCpart; last variable is relative difference on curvature (1/pt_reco - 1/pt_gen)/(1/pt_gen)
+                histos.fill(HIST("Tracks/RecoMCPhysPrimCollMatch/histptMCFB0resolCurv"), mcparticle.pt(), track.eta(), (mcparticle.pt() / track.pt() - 1.)); // did not use signed1Pt just to avoid getting the charge for MCpart; last variable is relative difference on curvature (1/pt_reco - 1/pt_gen)/(1/pt_gen)
               }
               if (track.itsChi2NCl() > 0. && track.tpcChi2NCl() < 0.) {
                 histos.fill(HIST("Tracks/RecoMCPhysPrimCollMatch/histptITSonly"), track.pt(), track.eta(), track.phi());
