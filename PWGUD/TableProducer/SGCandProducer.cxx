9b69ea3bdb5ce6409d8e37c6bbf0d47581a8beb5
Sasha Bylinkin <37345380+abylinkin@users.noreply.github.com>
2024-03-11 13:53:49 +0100

    Add files via upload

--- PWGUD/TableProducer/SGCandProducer.cxx
+++ PWGUD/TableProducer/SGCandProducer.cxx
@@ -9,7 +9,6 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include <cmath>
 #include "Framework/AnalysisDataModel.h"
 #include "Common/CCDB/EventSelectionParams.h"
 #include "Common/DataModel/EventSelection.h"
@@ -21,6 +20,7 @@
 #include "PWGUD/DataModel/UDTables.h"
 #include "PWGUD/Core/UPCHelpers.h"
 #include "PWGUD/Core/SGSelector.h"
+#include <cmath>
 
 using namespace o2;
 using namespace o2::framework;
@@ -30,7 +30,8 @@ struct SGCandProducer {
   // get an SGCutparHolder
   SGCutParHolder sameCuts = SGCutParHolder(); // SGCutparHolder
   Configurable<SGCutParHolder> SGCuts{"SGCuts", {}, "SG event cuts"};
-
+  Configurable<bool> saveAllTracks{"saveAllTracks", false, "save only PV contributors or all tracks associated to a collision"};
+  //Configurable<bool> rejectAtTFBoundary{"rejectAtTFBoundary", true, "reject collisions at a TF boundary"};
   // SG selector
   SGSelector sgSelector;
 
@@ -145,30 +146,36 @@ struct SGCandProducer {
                aod::Zdcs& zdcs, aod::FT0s& ft0s, aod::FV0As& fv0as, aod::FDDs& fdds)
   {
     LOGF(debug, "<SGCandProducer>  collision %d", collision.globalIndex());
+    registry.get<TH1>(HIST("reco/Stat"))->Fill(0., 1.);
+    // reject collisions at TF boundaries
+    //if (rejectAtTFBoundary && !collision.selection_bit(aod::evsel::kNoTimeFrameBorder)) {
+    //  return;
+    //}
+    //registry.get<TH1>(HIST("reco/Stat"))->Fill(1., 1.);
     // nominal BC
     if (!collision.has_foundBC()) {
       return;
     }
+    registry.get<TH1>(HIST("reco/Stat"))->Fill(2., 1.);
     auto bc = collision.foundBC_as<BCs>();
     auto newbc = bc;
 
     // obtain slice of compatible BCs
-    auto bcRange = udhelpers::compatibleBCs(collision, sameCuts.NDtcoll(), bcs, sameCuts.minNBCs());
-    auto isSGEvent = sgSelector.IsSelected(sameCuts, collision, bcRange, bc);
-    // auto isSGEvent = sgSelector.IsSelected(sameCuts, collision, bcRange, tracks);
+   auto bcRange = udhelpers::compatibleBCs(collision, sameCuts.NDtcoll(), bcs, sameCuts.minNBCs());
+   auto isSGEvent = sgSelector.IsSelected(sameCuts, collision, bcRange, bc);
+   //auto isSGEvent = sgSelector.IsSelected(sameCuts, collision, bcRange, tracks);
     int issgevent = isSGEvent.value;
-    if (isSGEvent.bc) {
-      newbc = *(isSGEvent.bc);
-    } else {
+      if (isSGEvent.bc){
+    newbc = *(isSGEvent.bc);
+      }
+      else {
       LOGF(info, "No Newbc %i", bc.globalBC());
-    }
-    registry.get<TH1>(HIST("reco/Stat"))->Fill(0., 1.);
-    registry.get<TH1>(HIST("reco/Stat"))->Fill(issgevent + 1, 1.);
+      }
+    registry.get<TH1>(HIST("reco/Stat"))->Fill(issgevent + 3, 1.);
     if (issgevent <= 2) {
-      //    LOGF(info, "Current BC: %i, %i, %i", bc.globalBC(), newbc.globalBC(), issgevent);
-      if (sameCuts.minRgtrwTOF()) {
-        if (udhelpers::rPVtrwTOF<true>(tracks, collision.numContrib()) < sameCuts.minRgtrwTOF())
-          return;
+  //    LOGF(info, "Current BC: %i, %i, %i", bc.globalBC(), newbc.globalBC(), issgevent);
+      if (sameCuts.minRgtrwTOF()){
+	      if (udhelpers::rPVtrwTOF<true>(tracks, collision.numContrib()) < sameCuts.minRgtrwTOF()) return;
       }
       upchelpers::FITInfo fitInfo{};
       udhelpers::getFITinfo(fitInfo, newbc.globalBC(), bcs, ft0s, fv0as, fdds);
@@ -176,7 +183,7 @@ struct SGCandProducer {
       outputCollisions(bc.globalBC(), bc.runNumber(),
                        collision.posX(), collision.posY(), collision.posZ(),
                        collision.numContrib(), udhelpers::netCharge<true>(tracks),
-                       1.); // rtrwTOF); //omit the calculation to speed up the things while skimming
+                       1.);//rtrwTOF); //omit the calculation to speed up the things while skimming
       outputSGCollisions(issgevent);
       outputCollisionsSels(fitInfo.ampFT0A, fitInfo.ampFT0C, fitInfo.timeFT0A, fitInfo.timeFT0C,
                            fitInfo.triggerMaskFT0,
@@ -188,24 +195,24 @@ struct SGCandProducer {
                            fitInfo.BBFDDApf, fitInfo.BBFDDCpf, fitInfo.BGFDDApf, fitInfo.BGFDDCpf);
       outputCollsLabels(collision.globalIndex());
       if (newbc.has_zdc()) {
-        auto zdc = newbc.zdc();
-        udZdcsReduced(outputCollisions.lastIndex(), zdc.timeZNA(), zdc.timeZNC(), zdc.energyCommonZNA(), zdc.energyCommonZNC());
-      } else {
-        udZdcsReduced(outputCollisions.lastIndex(), -999, -999, -999, -999);
+         auto zdc = newbc.zdc();
+         udZdcsReduced(outputCollisions.lastIndex(), zdc.timeZNA(), zdc.timeZNC(), zdc.energyCommonZNA(), zdc.energyCommonZNC());
+      }
+      else{
+         udZdcsReduced(outputCollisions.lastIndex(), -999, -999, -999, -999);
       }
       // update SGTracks tables
       for (auto& track : tracks) {
-        if (track.isPVContributor() && track.eta() > sameCuts.minEta() && track.eta() < sameCuts.maxEta())
-          updateUDTrackTables(outputCollisions.lastIndex(), track, bc.globalBC());
-        // if (track.isPVContributor())  updateUDTrackTables(outputCollisions.lastIndex(), track, bc.globalBC());
+         if (track.isPVContributor() || saveAllTracks){
+	    if( track.eta() > sameCuts.minEta() && track.eta() < sameCuts.maxEta())  updateUDTrackTables(outputCollisions.lastIndex(), track, bc.globalBC());
+ //if (track.isPVContributor())  updateUDTrackTables(outputCollisions.lastIndex(), track, bc.globalBC());
+	}
       }
-
       // update SGFwdTracks tables
-      if (sameCuts.withFwdTracks()) {
-        for (auto& fwdtrack : fwdtracks) {
-          if (!sgSelector.FwdTrkSelector(fwdtrack))
-            updateUDFwdTrackTables(fwdtrack, bc.globalBC());
-        }
+      if (sameCuts.withFwdTracks()){
+         for (auto& fwdtrack : fwdtracks) {
+           if (!sgSelector.FwdTrkSelector(fwdtrack)) updateUDFwdTrackTables(fwdtrack, bc.globalBC());
+         }
       }
     }
   }
