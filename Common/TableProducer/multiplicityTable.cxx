4600c505060a26ee225d888c7ff73fd0b47f251f
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2024-03-11 17:21:08 -0300

    Common: fix one more small recent bug (#5098)
    * Common: fix one more small recent bug
    
    * Update multiplicityTable.cxx

--- Common/TableProducer/multiplicityTable.cxx
+++ Common/TableProducer/multiplicityTable.cxx
@@ -253,10 +253,14 @@ struct MultiplicityTableTaskIndexed {
         case kMultsExtra: // Extra information
           tableExtra.reserve(collisions.size());
           break;
+        case kMultSelections: // Extra information
+          multSelections.reserve(collisions.size());
+          break;
         case kMultZeqs: // Equalized multiplicity
           tableMultZeq.reserve(collisions.size());
           break;
         case kMultsExtraMC: // MC extra information (nothing to do, this is data)
+          tableExtraMc.reserve(collisions.size());
           break;
         default:
           LOG(fatal) << "Unknown table requested: " << i;
