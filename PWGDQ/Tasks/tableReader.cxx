6fbf333153d34b13dae29393a2c86ad3754d6948
Zhenjun Xiong <108917659+zjxiongOvO@users.noreply.github.com>
2024-03-12 16:43:13 +0800

    fix naming issues

--- PWGDQ/Tasks/tableReader.cxx
+++ PWGDQ/Tasks/tableReader.cxx
@@ -1820,7 +1820,7 @@ void DefineHistograms(HistogramManager* histMan, TString histClasses, Configurab
     }
 
     if (classStr.Contains("Quadruplet")) {
-      dqhistograms::DefineHistograms(histMan, objArray->At(iclass)->GetName(), "dilepton-dihardon", histName);
+      dqhistograms::DefineHistograms(histMan, objArray->At(iclass)->GetName(), "dilepton-dihadron", histName);
     }
 
   } // end loop over histogram classes
