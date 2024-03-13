07ec324369586800c321b26b7ae592bb3143a50d
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2024-03-13 18:50:26 +0100

    EventFiltering/PWGHF: add configurable to force TOF proton PID in femto

--- EventFiltering/PWGHF/HFFilterHelpers.h
+++ EventFiltering/PWGHF/HFFilterHelpers.h
@@ -531,9 +531,10 @@ inline int8_t HfFilterHelper::isSelectedTrackForSoftPionOrBeauty(const T& track,
 /// \param activateQA flag to activate the filling of QA histos
 /// \param hProtonTPCPID histo with NsigmaTPC vs. p
 /// \param hProtonTOFPID histo with NsigmaTOF vs. p
+/// \param forceTof flag to force TOF PID
 /// \return true if track passes all cuts
 template <typename T1, typename T2, typename H2>
-inline bool HfFilterHelper::isSelectedProton4Femto(const T1& track, const T2& trackPar, const int& activateQA, H2 hProtonTPCPID, H2 hProtonTOFPID)
+inline bool HfFilterHelper::isSelectedProton4Femto(const T1& track, const T2& trackPar, const int& activateQA, H2 hProtonTPCPID, H2 hProtonTOFPID, bool forceTof)
 {
   float pt = trackPar.getPt();
   if (pt < mPtMinProtonForFemto || pt > mPtMaxProtonForFemto) {
@@ -550,6 +551,9 @@ inline bool HfFilterHelper::isSelectedProton4Femto(const T1& track, const T2& tr
 
   float NSigmaTPC = track.tpcNSigmaPr();
   float NSigmaTOF = track.tofNSigmaPr();
+  if (!forceTof && !track.hasTOF()) {
+    NSigmaTOF = 0.; // always accepted
+  }
 
   if (mTpcPidCalibrationOption == 1) {
     NSigmaTPC = getTPCPostCalib(track, kPr);
@@ -575,7 +579,9 @@ inline bool HfFilterHelper::isSelectedProton4Femto(const T1& track, const T2& tr
 
   if (activateQA > 1) {
     hProtonTPCPID->Fill(track.p(), NSigmaTPC);
-    hProtonTOFPID->Fill(track.p(), NSigmaTOF);
+    if (forceTof || track.hasTOF()) {
+      hProtonTOFPID->Fill(track.p(), NSigmaTOF);
+    }
   }
 
   return true;
