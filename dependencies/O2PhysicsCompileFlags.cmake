ea94e78ca492433b52d606eea403b08f503b1773
Vít Kučera <vit.kucera@cern.ch>
2024-03-24 00:39:22 +0100

    Fix parentheses

--- dependencies/O2PhysicsCompileFlags.cmake
+++ dependencies/O2PhysicsCompileFlags.cmake
@@ -11,7 +11,7 @@
 
 include_guard()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-error -Werror=unused")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-error -Werror=unused -Werror=parentheses")
 
 IF (ENABLE_TIMETRACE)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftime-trace")
