09b366419198e71b162fe2ada53d0c94b38592ae
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2024-03-24 21:02:58 +0100

    PWGLF: declarative eta window for speed in MC finding

--- PWGLF/TableProducer/lambdakzeromcfinder.cxx
+++ PWGLF/TableProducer/lambdakzeromcfinder.cxx
@@ -84,6 +84,7 @@ struct lambdakzeromcfinder {
   Configurable<bool> doSameCollisionOnly{"doSameCollisionOnly", false, "stick to decays in which tracks are assoc to same collision"};
   Configurable<int> qaNbins{"qaNbins", 200, "qa plots: binning"};
   Configurable<float> yPreFilter{"yPreFilter", 2.5, "broad y pre-filter for speed"};
+  Configurable<float> etaMax{"etaMax", 0.9, "broad daughter eta pre-filter for speed"};
   ConfigurableAxis axisPtQA{"axisPtQA", {VARIABLE_WIDTH, 0.0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f, 2.0f, 2.2f, 2.4f, 2.6f, 2.8f, 3.0f, 3.2f, 3.4f, 3.6f, 3.8f, 4.0f, 4.4f, 4.8f, 5.2f, 5.6f, 6.0f, 6.5f, 7.0f, 7.5f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 17.0f, 19.0f, 21.0f, 23.0f, 25.0f, 30.0f, 35.0f, 40.0f, 50.0f}, "pt axis for QA histograms"};
 
   Configurable<bool> doNotRequireTPC{"doNotRequireTPC", true, "do not require TPC for V0 MC finding"};
@@ -412,8 +413,8 @@ struct lambdakzeromcfinder {
 
   // this improved process function does not start from MC particles, as that would be too costly
   // rather, it starts from appropriately detected prongs of the correct charge.
-  Partition<LabeledTracks> posTracks = aod::track::signed1Pt > 0.0f;
-  Partition<LabeledTracks> negTracks = aod::track::signed1Pt < 0.0f;
+  Partition<LabeledTracks> posTracks = aod::track::signed1Pt > 0.0f && (aod::track::eta < etaMax) && (aod::track::eta > etaMax);
+  Partition<LabeledTracks> negTracks = aod::track::signed1Pt < 0.0f && (aod::track::eta < etaMax) && (aod::track::eta > etaMax);
 
   void processFromSingleProngs(aod::Collisions const& collisions, LabeledTracks const& tracks, soa::Join<aod::McCollisions, aod::McCollsExtra> const& mcCollisions, aod::McParticles const& allMcParticles)
   {
