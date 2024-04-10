dd6152ec0f870a691d621177990b0aeab264b286
zhangbiao-phy <biao.zhang@cern.ch>
2024-04-10 17:10:29 +0200

    EventFiltering[PWGHF]: Standardise scalers

--- EventFiltering/PWGHF/HFFilter.cxx
+++ EventFiltering/PWGHF/HFFilter.cxx
@@ -179,7 +179,10 @@ struct HfFilter { // Main struct for HF triggers
 
     hProcessedEvents = registry.add<TH1>("fProcessedEvents", "HF - event filtered;;counts", HistType::kTH1F, {{kNtriggersHF + 2, -0.5, +kNtriggersHF + 1.5}});
     for (auto iBin = 0; iBin < kNtriggersHF + 2; ++iBin) {
-      hProcessedEvents->GetXaxis()->SetBinLabel(iBin + 1, eventTitles[iBin].data());
+      if (iBin < 2)
+        hProcessedEvents->GetXaxis()->SetBinLabel(iBin + 1, eventTitles[iBin].data());
+      else
+        hProcessedEvents->GetXaxis()->SetBinLabel(iBin + 1, Form("o2::aod::filtering::%s::columnLabel()", hfTriggerNames[iBin - 2].data()));
     }
 
     if (activateQA) {
