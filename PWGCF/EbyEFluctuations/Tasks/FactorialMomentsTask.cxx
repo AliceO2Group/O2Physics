5e72e5206794d3d3aabd127ea404e61d27758fff
Salman Malik <salman.khurshid.malik@cern.ch>
2024-04-16 12:00:43 +0530

    chgdimpl

--- PWGCF/EbyEFluctuations/Tasks/FactorialMomentsTask.cxx
+++ PWGCF/EbyEFluctuations/Tasks/FactorialMomentsTask.cxx
@@ -25,49 +25,28 @@
 #include "Common/DataModel/Centrality.h"
 #include "Common/Core/TrackSelection.h"
 #include "Common/DataModel/TrackSelectionTables.h"
+#include "ReconstructionDataFormats/GlobalTrackID.h"
+#include "ReconstructionDataFormats/Track.h"
+#include "Framework/ASoAHelpers.h"
 
 using std::array;
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
-namespace o2::aod
-{
-namespace full
-{
-DECLARE_SOA_COLUMN(mEvMult, eventMult, Int_t);
-DECLARE_SOA_COLUMN(mVertexZ, vertexZ, Float_t);
-DECLARE_SOA_COLUMN(mCentrality, cent, Float_t);
-DECLARE_SOA_COLUMN(mBinMult, binmult, Int_t[5]);
-DECLARE_SOA_COLUMN(mAvbCon, avbcontent, Double_t[200]);
-DECLARE_SOA_COLUMN(mValM, m, Int_t[200]);
-DECLARE_SOA_COLUMN(mPtBin, ptbin, Int_t[200]);
-DECLARE_SOA_COLUMN(mF2, f2, Double_t[200]);
-DECLARE_SOA_COLUMN(mF3, f3, Double_t[200]);
-DECLARE_SOA_COLUMN(mF4, f4, Double_t[200]);
-DECLARE_SOA_COLUMN(mF5, f5, Double_t[200]);
-DECLARE_SOA_COLUMN(mF6, f6, Double_t[200]);
-DECLARE_SOA_COLUMN(mF7, f7, Double_t[200]);
-} // namespace full
-
-DECLARE_SOA_TABLE(NFMFULL, "AOD", "NFMFULL", o2::soa::Index<>, full::mEvMult, full::mVertexZ, full::mCentrality, full::mBinMult, full::mAvbCon, full::mValM, full::mPtBin, full::mF2, full::mF3, full::mF4, full::mF5, full::mF6, full::mF7);
-} // namespace o2::aod
-
-// Write information to the tree
 struct FactorialMoments {
-  Produces<o2::aod::NFMFULL> tableFacMoments;
 
   Configurable<Float_t> confEta{"centralEta", 0.8, "eta limit for tracks"};
-  Configurable<Int_t> confNumPt{"numPt", 5, "number of pT bins"};
+  Configurable<Int_t> confNumPt{"numPt", 1, "number of pT bins"};
   Configurable<Float_t> confPtMin{"ptMin", 0.2f, "lower pT cut"};
   Configurable<Float_t> confDCAxy{"dcaXY", 2.4f, "DCA xy cut"};
   Configurable<Float_t> confDCAz{"dcaZ", 3.2f, "DCA z cut"};
   Configurable<Float_t> confMinTPCCls{"minTPCCls", 70.0f, "minimum number of TPC clusters"};
   Configurable<std::vector<Int_t>> confCentCut{"centLimits", {0, 5}, "centrality min and max"};
   Configurable<std::vector<Float_t>> confVertex{"vertexXYZ", {0.3f, 0.4f, 10.0f}, "vertex cuts"};
-  Configurable<std::vector<Float_t>> confPtBins{"ptCuts", {0.4f, 1.0f, 0.4f, 0.6f, 0.8f, 1.0f, 0.4f, 2.0f, 0.4f, 5.0f}, "pT cuts"};
+  Configurable<std::vector<Float_t>> confPtBins{"ptCuts", {0.2f, 2.0f}, "pT cuts"};
 
-  Filter filterTracks = (nabs(aod::track::eta) < confEta) && (aod::track::pt > confPtMin) && (nabs(aod::track::dcaXY) < confDCAxy) && (nabs(aod::track::dcaZ) < confDCAz);
+  Filter filterTracks = (nabs(aod::track::eta) < confEta) && (aod::track::pt >= confPtMin) && (nabs(aod::track::dcaXY) < confDCAxy) && (nabs(aod::track::dcaZ) < confDCAz);
   Filter filterCollisions = (nabs(aod::collision::posZ) < confVertex.value[2]) && (nabs(aod::collision::posX) < confVertex.value[0]) && (nabs(aod::collision::posY) < confVertex.value[1]);
 
   // Histograms
@@ -105,27 +84,30 @@ struct FactorialMoments {
     },
     OutputObjHandlingPolicy::AnalysisObject,
     true};
-
-  array<Int_t, 40> binningM;
+  static const Int_t nBins = 52;
+  array<Int_t, nBins> binningM;
   array<Int_t, 5> countTracks{0, 0, 0, 0, 0};
-  array<array<array<Double_t, 40>, 5>, 6> fqEvent;
-  array<array<Double_t, 40>, 5> averagebinCon;
-  array<array<Int_t, 40>, 5> valueM;
-  array<array<Int_t, 40>, 5> mPt;
+  array<array<array<Double_t, nBins>, 5>, 6> fqEvent;
+  array<array<Double_t, nBins>, 5> binConEvent;
   std::vector<std::shared_ptr<TH2>> mHistArrReset;
-  std::vector<std::shared_ptr<TH2>> mHistArrUReset;
   std::vector<std::shared_ptr<TH1>> mHistArrQA;
+  std::vector<std::shared_ptr<TH1>> mFqBinFinal;
+  std::vector<std::shared_ptr<TH1>> mBinConFinal;
   // max number of bins restricted to 5
   static constexpr array<std::string_view, 5> mbinNames{"bin1/", "bin2/", "bin3/", "bin4/", "bin5/"};
 
   void init(o2::framework::InitContext&)
   {
+    // NOTE: check to make number of pt and the vector consistent
+    if (confNumPt != static_cast<int>(confPtBins.value.size()) / 2) {
+      for (int i = confNumPt; i < static_cast<int>(confPtBins.value.size() / 2); i++) {
+        confPtBins.value[2 * i] = 0;
+        confPtBins.value[2 * i + 1] = 0;
+      }
+    }
     AxisSpec axisPt[5] = {{100, -0.01, 3 * confPtBins.value[1], ""}, {100, -0.01, 3 * confPtBins.value[3], ""}, {100, -0.01, 3 * confPtBins.value[5], ""}, {100, -0.01, 3 * confPtBins.value[7], ""}, {100, -0.01, 3 * confPtBins.value[9], ""}}; // pT axis
 
-    if (confNumPt != static_cast<int>(confPtBins.value.size()) / 2) {
-      throw std::runtime_error("Number of pT bins in confNumPt does not match the number of pT cuts in config.json!");
-    }
-    for (auto iM = 0; iM < 40; ++iM) {
+    for (auto iM = 0; iM < nBins; ++iM) {
       binningM[iM] = 2 * (iM + 2);
     }
     for (auto iPt = 0; iPt < confNumPt; ++iPt) {
@@ -133,11 +115,15 @@ struct FactorialMoments {
       mHistArrQA.push_back(std::get<std::shared_ptr<TH1>>(histos.add(Form("bin%i/mPt", iPt + 1), Form("pT for bin %.2f-%.2f;pT", confPtBins.value[2 * iPt], confPtBins.value[2 * iPt + 1]), HistType::kTH1F, {axisPt[iPt]})));
       mHistArrQA.push_back(std::get<std::shared_ptr<TH1>>(histos.add(Form("bin%i/mPhi", iPt + 1), Form("#phi for bin %.2f-%.2f;#phi", confPtBins.value[2 * iPt], confPtBins.value[2 * iPt + 1]), HistType::kTH1F, {{1000, 0, 2 * TMath::Pi()}})));
       mHistArrQA.push_back(std::get<std::shared_ptr<TH1>>(histos.add(Form("bin%i/mMultiplicity", iPt + 1), Form("Multiplicity for bin %.2f-%.2f;Multiplicity", confPtBins.value[2 * iPt], confPtBins.value[2 * iPt + 1]), HistType::kTH1F, {{1000, 0, 8000}})));
-      for (auto iM = 0; iM < 40; ++iM) {
+      for (auto iM = 0; iM < nBins; ++iM) {
         auto mHistsR = std::get<std::shared_ptr<TH2>>(histos.add(Form("bin%i/Reset/mEtaPhi%i", iPt + 1, iM), Form("#eta#phi_%i for bin %.2f-%.2f;#eta;#phi", iM, confPtBins.value[2 * iPt], confPtBins.value[2 * iPt + 1]), HistType::kTH2F, {{binningM[iM], -0.8, 0.8}, {binningM[iM], 0, 2 * TMath::Pi()}}));
         mHistArrReset.push_back(mHistsR);
-        auto mHistsUR = std::get<std::shared_ptr<TH2>>(histos.add(Form("bin%i/UReset/mEtaPhi%i", iPt + 1, iM), Form("#eta#phi_%i for bin %.2f-%.2f;#eta;#phi", iM, confPtBins.value[2 * iPt], confPtBins.value[2 * iPt + 1]), HistType::kTH2F, {{binningM[iM], -0.8, 0.8}, {binningM[iM], 0, 2 * TMath::Pi()}}));
-        mHistArrUReset.push_back(mHistsUR);
+      }
+      for (auto i = 0; i < 6; ++i) {
+        auto mHistFq = std::get<std::shared_ptr<TH1>>(histos.add(Form("mFinalFq%i_bin%i", i + 2, iPt + 1), Form("Final F_%i for bin %.2f-%.2f;M", i + 2, confPtBins.value[2 * iPt], confPtBins.value[2 * iPt + 1]), HistType::kTH1F, {{nBins, -0.5, nBins - 0.5}}));
+        mFqBinFinal.push_back(mHistFq);
+        auto mHistAv = std::get<std::shared_ptr<TH1>>(histos.add(Form("mFinalAvBin%i_bin%i", i + 2, iPt + 1), Form("Final AvBin_%i for bin %.2f-%.2f;M", i + 2, confPtBins.value[2 * iPt], confPtBins.value[2 * iPt + 1]), HistType::kTH1F, {{nBins, -0.5, nBins - 0.5}}));
+        mBinConFinal.push_back(mHistAv);
       }
     }
   }
@@ -151,9 +137,8 @@ struct FactorialMoments {
         mHistArrQA[iPt * 4 + 1]->Fill(track.pt());
         mHistArrQA[iPt * 4 + 2]->Fill(track.phi());
         countTracks[iPt]++;
-        for (auto iM = 0; iM < 40; ++iM) {
-          mHistArrReset[iPt * 40 + iM]->Fill(track.eta(), track.phi());
-          mHistArrUReset[iPt * 40 + iM]->Fill(track.eta(), track.phi());
+        for (auto iM = 0; iM < nBins; ++iM) {
+          mHistArrReset[iPt * nBins + iM]->Fill(track.eta(), track.phi());
         }
       }
     }
@@ -164,14 +149,14 @@ struct FactorialMoments {
     Double_t binContent = 0;
     // Calculate the normalized factorial moments
     for (auto iPt = 0; iPt < confNumPt; ++iPt) {
-      for (auto iM = 0; iM < 40; ++iM) {
+      for (auto iM = 0; iM < nBins; ++iM) {
         binContent = 0;
         Double_t sumfqBin[6] = {0};
 
-        for (auto iEta = 1; iEta <= hist[iPt * 40 + iM]->GetNbinsX(); ++iEta) {
-          for (auto iPhi = 1; iPhi <= hist[iPt * 40 + iM]->GetNbinsY(); ++iPhi) {
+        for (auto iEta = 1; iEta <= hist[iPt * nBins + iM]->GetNbinsX(); ++iEta) {
+          for (auto iPhi = 1; iPhi <= hist[iPt * nBins + iM]->GetNbinsY(); ++iPhi) {
             double binconVal = 0;
-            binconVal = hist[iPt * 40 + iM]->GetBinContent(iEta, iPhi);
+            binconVal = hist[iPt * nBins + iM]->GetBinContent(iEta, iPhi);
             binContent += binconVal;
             for (auto iOrder = 0; iOrder < 6; ++iOrder) {
               Double_t fqBin = 0;
@@ -185,13 +170,13 @@ struct FactorialMoments {
             }
           }
         }
-        averagebinCon[iPt][iM] = binContent / (TMath::Power(binningM[iM], 2));
-        valueM[iPt][iM] = binningM[iM];
-        mPt[iPt][iM] = iPt;
+        binConEvent[iPt][iM] = binContent / (TMath::Power(binningM[iM], 2));
         for (auto iOrder = 0; iOrder < 6; ++iOrder) {
           if (sumfqBin[iOrder] > 0) {
             fqEvent[iOrder][iPt][iM] = sumfqBin[iOrder] / (TMath::Power(binningM[iM], 2));
           }
+          mFqBinFinal[iPt * 6 + iOrder]->Fill(iM, fqEvent[iOrder][iPt][iM]);
+          mBinConFinal[iPt * 6 + iOrder]->Fill(iM, binConEvent[iPt][iM]);
         }
       } // end of loop over M bins
     }   // end of loop over pT bins
@@ -220,9 +205,7 @@ struct FactorialMoments {
     }
     countTracks = {0, 0, 0, 0, 0};
     fqEvent = {0, 0, 0, 0, 0, 0};
-    averagebinCon = {0, 0, 0, 0, 0};
-    valueM = {0, 0, 0, 0, 0};
-    mPt = {0, 0, 0, 0, 0};
+    binConEvent = {0, 0, 0, 0, 0};
 
     for (auto const& track : tracks) {
       if ((track.pt() < confPtMin) || (!track.isGlobalTrack()) || (track.tpcNClsFindable() < confMinTPCCls)) {
@@ -257,37 +240,6 @@ struct FactorialMoments {
     }
     // Calculate the normalized factorial moments
     calculateMoments(mHistArrReset);
-    // Fill the table
-    Int_t binMult[5] = {0};
-    Double_t avBinconArr[200] = {0};
-    Int_t valueMArr[200] = {0};
-    Int_t mPtArr[200] = {0};
-    Double_t fqEventArr2[200] = {0};
-    Double_t fqEventArr3[200] = {0};
-    Double_t fqEventArr4[200] = {0};
-    Double_t fqEventArr5[200] = {0};
-    Double_t fqEventArr6[200] = {0};
-    Double_t fqEventArr7[200] = {0};
-    for (auto iPt = 0; iPt < confNumPt; ++iPt) {
-      binMult[iPt] = countTracks[iPt];
-      for (auto iM = 0; iM < 40; ++iM) {
-        for (auto iOrder = 0; iOrder < 6; ++iOrder) {
-          if (isinf(fqEvent[iOrder][iPt][iM])) {
-            fqEvent[iOrder][iPt][iM] = 0;
-          }
-        }
-        avBinconArr[iPt * 40 + iM] = averagebinCon[iPt][iM];
-        valueMArr[iPt * 40 + iM] = valueM[iPt][iM];
-        mPtArr[iPt * 40 + iM] = mPt[iPt][iM];
-        fqEventArr2[iPt * 40 + iM] = fqEvent[0][iPt][iM];
-        fqEventArr3[iPt * 40 + iM] = fqEvent[1][iPt][iM];
-        fqEventArr4[iPt * 40 + iM] = fqEvent[2][iPt][iM];
-        fqEventArr5[iPt * 40 + iM] = fqEvent[3][iPt][iM];
-        fqEventArr6[iPt * 40 + iM] = fqEvent[4][iPt][iM];
-        fqEventArr7[iPt * 40 + iM] = fqEvent[5][iPt][iM];
-      }
-    }
-    tableFacMoments(coll.multFT0C(), coll.posZ(), coll.centFT0C(), binMult, avBinconArr, valueMArr, mPtArr, fqEventArr2, fqEventArr3, fqEventArr4, fqEventArr5, fqEventArr6, fqEventArr7);
   }
   PROCESS_SWITCH(FactorialMoments, processRun3, "main process function", false);
 
@@ -309,9 +261,7 @@ struct FactorialMoments {
 
     countTracks = {0, 0, 0, 0, 0};
     fqEvent = {0, 0, 0, 0, 0, 0};
-    averagebinCon = {0, 0, 0, 0, 0};
-    valueM = {0, 0, 0, 0, 0};
-    mPt = {0, 0, 0, 0, 0};
+    binConEvent = {0, 0, 0, 0, 0};
 
     for (auto const& track : tracks) {
       if ((track.pt() < confPtMin) || (!track.isGlobalTrack()) || (track.tpcNClsFindable() < confMinTPCCls)) {
@@ -346,39 +296,8 @@ struct FactorialMoments {
     }
     // Calculate the normalized factorial moments
     calculateMoments(mHistArrReset);
-    // Fill the table
-    Int_t binMult[5] = {0};
-    Double_t avBinconArr[200] = {0};
-    Int_t valueMArr[200] = {0};
-    Int_t mPtArr[200] = {0};
-    Double_t fqEventArr2[200] = {0};
-    Double_t fqEventArr3[200] = {0};
-    Double_t fqEventArr4[200] = {0};
-    Double_t fqEventArr5[200] = {0};
-    Double_t fqEventArr6[200] = {0};
-    Double_t fqEventArr7[200] = {0};
-    for (auto iPt = 0; iPt < confNumPt; ++iPt) {
-      binMult[iPt] = countTracks[iPt];
-      for (auto iM = 0; iM < 40; ++iM) {
-        for (auto iOrder = 0; iOrder < 6; ++iOrder) {
-          if (isinf(fqEvent[iOrder][iPt][iM])) {
-            fqEvent[iOrder][iPt][iM] = 0;
-          }
-        }
-        avBinconArr[iPt * 40 + iM] = averagebinCon[iPt][iM];
-        valueMArr[iPt * 40 + iM] = valueM[iPt][iM];
-        mPtArr[iPt * 40 + iM] = mPt[iPt][iM];
-        fqEventArr2[iPt * 40 + iM] = fqEvent[0][iPt][iM];
-        fqEventArr3[iPt * 40 + iM] = fqEvent[1][iPt][iM];
-        fqEventArr4[iPt * 40 + iM] = fqEvent[2][iPt][iM];
-        fqEventArr5[iPt * 40 + iM] = fqEvent[3][iPt][iM];
-        fqEventArr6[iPt * 40 + iM] = fqEvent[4][iPt][iM];
-        fqEventArr7[iPt * 40 + iM] = fqEvent[5][iPt][iM];
-      }
-    }
-    tableFacMoments(coll.centRun2V0M(), coll.posZ(), coll.centRun2V0M(), binMult, avBinconArr, valueMArr, mPtArr, fqEventArr2, fqEventArr3, fqEventArr4, fqEventArr5, fqEventArr6, fqEventArr7);
   }
-  PROCESS_SWITCH(FactorialMoments, processRun2, "for RUN2", true);
+  PROCESS_SWITCH(FactorialMoments, processRun2, "for RUN2", false);
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
