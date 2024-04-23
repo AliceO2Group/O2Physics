f122a91a542ec3d1740cc1321a233d4df370426c
Sasha Bylinkin <37345380+abylinkin@users.noreply.github.com>
2024-04-23 14:58:26 +0200

    Update and rename sgD0_Analyzer.cxx to sgD0Analyzer.cxx

--- PWGUD/Tasks/sgD0Analyzer.cxx
+++ PWGUD/Tasks/sgD0Analyzer.cxx
@@ -30,7 +30,7 @@ using namespace o2::framework::expressions;
 #define mpion 0.1396
 #define mkaon 0.4937
 #define mproton 0.9383
-struct sgD0_Analyzer {
+struct sgD0Analyzer {
   SGSelector sgSelector;
   Configurable<float> FV0_cut{"FV0", 100., "FV0A threshold"};
   Configurable<float> FT0A_cut{"FT0A", 100., "FT0A threshold"};
@@ -193,5 +193,5 @@ struct sgD0_Analyzer {
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<sgD0_Analyzer>(cfgc)};
+    adaptAnalysisTask<sgD0Analyzer>(cfgc)};
 }
