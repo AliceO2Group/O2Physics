e4b5f4a02c7d1d789484d7f73286f593d8377eea
Sasha Bylinkin <37345380+abylinkin@users.noreply.github.com>
2024-04-23 16:42:57 +0200

    Update sgD0Analyzer.cxx

--- PWGUD/Tasks/sgD0Analyzer.cxx
+++ PWGUD/Tasks/sgD0Analyzer.cxx
@@ -30,7 +30,7 @@ using namespace o2::framework::expressions;
 #define mpion 0.1396
 #define mkaon 0.4937
 #define mproton 0.9383
-struct sgD0Analyzer {
+struct SGD0Analyzer {
   SGSelector sgSelector;
   Configurable<float> FV0_cut{"FV0", 100., "FV0A threshold"};
   Configurable<float> FT0A_cut{"FT0A", 100., "FT0A threshold"};
@@ -193,5 +193,5 @@ struct sgD0Analyzer {
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<sgD0Analyzer>(cfgc)};
+    adaptAnalysisTask<SGD0Analyzer>(cfgc)};
 }
