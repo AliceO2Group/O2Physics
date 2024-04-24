65e0ef8e9b143e212e6a899db08f8cef3a56f5e4
abilandz <Ante.Bilandzic@cern.ch>
2024-04-24 11:14:55 +0200

    support for internal validation against theoretical values

--- PWGCF/MultiparticleCorrelations/Core/MuPa-Configurables.h
+++ PWGCF/MultiparticleCorrelations/Core/MuPa-Configurables.h
@@ -17,84 +17,59 @@
 #include <string>
 
 // Task configuration:
-Configurable<string> cfTaskName{
-  "cfTaskName", "Default task name",
-  "set task name - use eventually to determine weights for this task"};
-Configurable<bool> cfDryRun{
-  "cfDryRun", false,
-  "book all histos and run without storing and calculating anything"};
-Configurable<bool> cfVerbose{
-  "cfVerbose", false,
-  "run or not in verbose mode (but not for function calls per particle)"};
-Configurable<bool> cfVerboseForEachParticle{
-  "cfVerboseForEachParticle", false,
-  "run or not in verbose mode (also for function calls per particle)"};
-Configurable<bool> cfDoAdditionalInsanityChecks{
-  "cfDoAdditionalInsanityChecks", false,
-  "do additional insanity checks at run time (this leads to small loss of performance)"};
-Configurable<bool> cfUseCCDB{
-  "cfUseCCDB", true,
-  "access personal files from CCDB or from home dir in AliEn"};
-Configurable<unsigned int> cfRandomSeed{
-  "cfRandomSeed", 0,
-  "0 = random seed is guaranteed to be unique in space and time"};
-Configurable<bool> cfUseFisherYates{
-  "cfUseFisherYates", false,
-  "use or not Fisher-Yates algorithm to randomize particle indices"};
-Configurable<int> cfFixedNumberOfRandomlySelectedTracks{
-  "cfFixedNumberOfRandomlySelectedTracks", -1,
-  "Set to some integer > 0, to apply and use. Set to <=0, to ignore."};
-Configurable<bool> cfUseStopwatch{
-  "cfUseStopwatch", false,
-  "If true, some basic info on time execution is printed, here and there."};
+Configurable<string> cfTaskName{"cfTaskName", "Default task name", "set task name - use eventually to determine weights for this task"};
+Configurable<bool> cfDryRun{"cfDryRun", false, "book all histos and run without storing and calculating anything"};
+Configurable<bool> cfVerbose{"cfVerbose", false, "run or not in verbose mode (but not for function calls per particle)"};
+Configurable<bool> cfVerboseForEachParticle{"cfVerboseForEachParticle", false, "run or not in verbose mode (also for function calls per particle)"};
+Configurable<bool> cfDoAdditionalInsanityChecks{"cfDoAdditionalInsanityChecks", false, "do additional insanity checks at run time (this leads to small loss of performance)"};
+Configurable<bool> cfInsanityCheckForEachParticle{"cfInsanityCheckForEachParticle", false, "do insanity checks at run time for each particle, at the expense of losing a lot of performance. Use only during debugging."};
+Configurable<bool> cfUseCCDB{"cfUseCCDB", true, "access personal files from CCDB or from home dir in AliEn"};
+Configurable<unsigned int> cfRandomSeed{"cfRandomSeed", 0, "0 = random seed is guaranteed to be unique in space and time"};
+Configurable<bool> cfUseFisherYates{"cfUseFisherYates", false, "use or not Fisher-Yates algorithm to randomize particle indices"};
+Configurable<int> cfFixedNumberOfRandomlySelectedTracks{"cfFixedNumberOfRandomlySelectedTracks", -1, "Set to some integer > 0, to apply and use. Set to <=0, to ignore."};
+Configurable<bool> cfUseStopwatch{"cfUseStopwatch", false, "If true, some basic info on time execution is printed, here and there."};
 
 // Default booking:
 Configurable<vector<int>> cfBookEventHistograms{"cfBookEventHistograms", {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, "Book (1) or do not book (0) event histogram, ordering is the same as in enum eEventHistograms"};
 Configurable<vector<int>> cfBookParticleHistograms{"cfBookParticleHistograms", {1, 1, 1, 1, 1, 1, 1}, "Book (1) or do not book (0) particle histogram, ordering is the same as in enum eParticleHistograms"};
+Configurable<vector<int>> cfBookParticleHistograms2D{"cfBookParticleHistograms2D", {1, 1}, "Book (1) or do not book (0) particle histogram, ordering is the same as in enum eParticleHistograms2D"};
 
 // Q-vector:
-Configurable<bool> cfCalculateQvectors{"cfCalculateQvectors", true,
-                                       "calculate or not Q-vectors (all, also diff. ones). If I want only to fill control histograms, then I can set here false"};
-
+Configurable<bool> cfCalculateQvectors{"cfCalculateQvectors", true, "calculate or not Q-vectors (all, also diff. ones). If I want only to fill control histograms, then I can set here false"};
 // Correlations:
-Configurable<bool> cfCalculateCorrelations{"cfCalculateCorrelations", false,
-                                           "calculate or not correlations"};
+Configurable<bool> cfCalculateCorrelations{"cfCalculateCorrelations", false, "calculate or not correlations"};
 
 // Test0:
-Configurable<bool> cfCalculateTest0{"cfCalculateTest0", false,
-                                    "calculate or not Test0"};
-Configurable<bool> cfCalculateTest0AsFunctionOfIntegrated{"cfCalculateTest0AsFunctionOfIntegrated", true,
-                                                          "calculate or not Test0 as a function of integrated"};
-Configurable<bool> cfCalculateTest0AsFunctionOfMultiplicity{"cfCalculateTest0AsFunctionOfMultiplicity", true,
-                                                            "calculate or not Test0 as a function of multiplicity"};
-Configurable<bool> cfCalculateTest0AsFunctionOfCentrality{"cfCalculateTest0AsFunctionOfCentrality", true,
-                                                          "calculate or not Test0 as a function of centrality"};
-Configurable<bool> cfCalculateTest0AsFunctionOfPt{"cfCalculateTest0AsFunctionOfPt", false,
-                                                  "calculate or not Test0 as a function of pt"};
-Configurable<bool> cfCalculateTest0AsFunctionOfEta{"cfCalculateTest0AsFunctionOfEta", false,
-                                                   "calculate or not Test0 as a function of eta"};
-Configurable<string> cfFileWithLabels{"cfFileWithLabels",
-                                      "/home/abilandz/DatasetsO2/labels.root", "path to external ROOT file which specifies all labels"}; // for AliEn file prepend "/alice/cern.ch/", for CCDB prepend "/alice-ccdb.cern.ch"
+Configurable<bool> cfCalculateTest0{"cfCalculateTest0", false, "calculate or not Test0"};
+Configurable<bool> cfCalculateTest0AsFunctionOfIntegrated{"cfCalculateTest0AsFunctionOfIntegrated", true, "calculate or not Test0 as a function of integrated"};
+Configurable<bool> cfCalculateTest0AsFunctionOfMultiplicity{"cfCalculateTest0AsFunctionOfMultiplicity", true, "calculate or not Test0 as a function of multiplicity"};
+Configurable<bool> cfCalculateTest0AsFunctionOfCentrality{"cfCalculateTest0AsFunctionOfCentrality", true, "calculate or not Test0 as a function of centrality"};
+Configurable<bool> cfCalculateTest0AsFunctionOfPt{"cfCalculateTest0AsFunctionOfPt", false, "calculate or not Test0 as a function of pt"};
+Configurable<bool> cfCalculateTest0AsFunctionOfEta{"cfCalculateTest0AsFunctionOfEta", false, "calculate or not Test0 as a function of eta"};
+Configurable<string> cfFileWithLabels{"cfFileWithLabels", "/home/abilandz/DatasetsO2/labels.root", "path to external ROOT file which specifies all labels"}; // for AliEn file prepend "/alice/cern.ch/", for CCDB prepend "/alice-ccdb.cern.ch"
 
 // Particle weights:
-Configurable<bool> cfUsePhiWeights{"cfUsePhiWeights", false,
-                                   "use or not phi weights"};
-Configurable<bool> cfUsePtWeights{"cfUsePtWeights", false,
-                                  "use or not pt weights"};
-Configurable<bool> cfUseEtaWeights{"cfUseEtaWeights", false,
-                                   "use or not eta weights"};
-Configurable<bool> cfUseDiffPhiPtWeights{"cfUseDiffPhiPtWeights", false,
-                                         "use or not differential phi(pt) weights"};
-Configurable<bool> cfUseDiffPhiEtaWeights{"cfUseDiffPhiEtaWeights", false,
-                                          "use or not differential phi(eta) weights"};
-Configurable<string> cfFileWithWeights{"cfFileWithWeights",
-                                       "/home/abilandz/DatasetsO2/weights.root", "path to external ROOT file which holds all particle weights in O2 format"}; // for AliEn file prepend "/alice/cern.ch/", for CCDB prepend "/alice-ccdb.cern.ch"
+Configurable<bool> cfUsePhiWeights{"cfUsePhiWeights", false, "use or not phi weights"};
+Configurable<bool> cfUsePtWeights{"cfUsePtWeights", false, "use or not pt weights"};
+Configurable<bool> cfUseEtaWeights{"cfUseEtaWeights", false, "use or not eta weights"};
+Configurable<bool> cfUseDiffPhiPtWeights{"cfUseDiffPhiPtWeights", false, "use or not differential phi(pt) weights"};
+Configurable<bool> cfUseDiffPhiEtaWeights{"cfUseDiffPhiEtaWeights", false, "use or not differential phi(eta) weights"};
+Configurable<string> cfFileWithWeights{"cfFileWithWeights", "/home/abilandz/DatasetsO2/weights.root", "path to external ROOT file which holds all particle weights in O2 format"}; // for AliEn file prepend "/alice/cern.ch/", for CCDB prepend "/alice-ccdb.cern.ch"
 
 // Nested loops:
-Configurable<bool> cfCalculateNestedLoops{"cfCalculateNestedLoops", false,
-                                          "cross-check for all events all correlations with nested loops"};
-Configurable<bool> cfCalculateCustomNestedLoops{"cfCalculateCustomNestedLoops", false,
-                                                "cross-check e-b-e all correlations with custom nested loops"};
+Configurable<bool> cfCalculateNestedLoops{"cfCalculateNestedLoops", false, "cross-check for all events all correlations with nested loops"};
+Configurable<bool> cfCalculateCustomNestedLoops{"cfCalculateCustomNestedLoops", false, "cross-check e-b-e all correlations with custom nested loops"};
+Configurable<bool> cfCalculateKineCustomNestedLoops{"cfCalculateKineCustomNestedLoops", false, "cross-check e-b-e all differential (vs. pt, eta, etc.) correlations with custom nested loops"};
+Configurable<int> cfMaxNestedLoop{"cfMaxNestedLoop", -1, "if set to e.g. 4, all nested loops beyond that, e.g. 6-p and 8-p, are NOT calculated"};
+
+// Internal validation:
+Configurable<bool> cfUseInternalValidation{"cfUseInternalValidation", false, "perform internal validation using flow analysis on-the-fly"};
+Configurable<unsigned int> cfnEventsInternalValidation{"cfnEventsInternalValidation", 0, "number of events simulated on-the-fly for internal validation"};
+Configurable<string> cfHarmonicsOptionInternalValidation{"cfHarmonicsOptionInternalValidation", "constant", "for internal validation, set whether flow amplitudes are \"constant\" or \"correlared\""};
+Configurable<bool> cfRescaleWithTheoreticalInput{"cfRescaleWithTheoreticalInput", false, "if kTRUE, all correlators are rescaled with theoretical input, so that all results in profiles are 1"};
+Configurable<vector<float>> cfInternalValidationAmplitudes{"cfInternalValidationAmplitudes", {0.0, 0.1, 0.2}, "{v1, v2, v3, ...} + has an effect only in combination with cfHarmonicsOptionInternalValidation = \"constant\". Max number of vn's is gMaxHarmonic."};
+Configurable<vector<float>> cfInternalValidationPlanes{"cfInternalValidationPlanes", {0.0, 0.0, 0.0}, "{Psi1, Psi2, Psi3, ...} + has an effect only in combination with cfHarmonicsOptionInternalValidation = \"constant\". Max number of Psin's is gMaxHarmonic."};
+Configurable<vector<int>> cfMultRangeInternalValidation{"cfMultRangeInternalValidation", {1000, 1001}, "{min, max}, with convention: min <= M < max"};
 
 // Event cuts:
 Configurable<string> cfTrigger{
@@ -102,61 +77,23 @@ Configurable<string> cfTrigger{
   "set here some supported trigger (kINT7, ...) "};
 Configurable<bool> cfUseSel7{"cfUseSel7", false, "use for Run 2 data and MC (see official doc)"};
 Configurable<bool> cfUseSel8{"cfUseSel8", false, "use for Run 3 data and MC (see official doc)"};
-Configurable<string> cfCentralityEstimator{
-  "cfCentralityEstimator", "some supported centrality estimator",
-  "set here some supported centrality estimator (CentFT0M, CentFV0A, CentNTPV, ... for Run 3, CentRun2V0M, CentRun2SPDTracklets, ..., for Run 2) "};
-
-Configurable<int> cNumberOfEvents_min{
-  "cNumberOfEvents_min", -1,
-  "minimum number of events to process (set to -1 to ignore)"};
-Configurable<int> cNumberOfEvents_max{"cNumberOfEvents_max", 1000000000,
-                                      "maximum number of events to process (use only in the local analysis, because after that BailOut() is forced)"};
-Configurable<int> cTotalMultiplicity_min{
-  "cTotalMultiplicity_min", -1,
-  "minimum total multiplicity of an event to be processed (set to -1 to "
-  "ignore)"};
-Configurable<int> cTotalMultiplicity_max{
-  "cTotalMultiplicity_max", 1000000000,
-  "maximum total multiplicity of an event to be processed"};
-Configurable<int> cSelectedTracks_min{
-  "cSelectedTracks_min", -1,
-  "minimum number of selected tracks (set to -1 to ignore)"};
-Configurable<int> cSelectedTracks_max{"cSelectedTracks_max", 1000000000,
-                                      "maximum number of selected tracks"};
-Configurable<float> cCentrality_min{"cCentrality_min", 0.,
-                                    "minimum centrality"};
-Configurable<float> cCentrality_max{"cCentrality_max", 100.,
-                                    "maximum centrality"};
-Configurable<float> cVertex_x_min{"cVertex_x_min", -10.0,
-                                  "minimum vertex x range [cm]"};
-Configurable<float> cVertex_x_max{"cVertex_x_max", 10.0,
-                                  "maximum vertex x range [cm]"};
-Configurable<float> cVertex_y_min{"cVertex_y_min", -10.0,
-                                  "minimum vertex y range [cm]"};
-Configurable<float> cVertex_y_max{"cVertex_y_max", 10.0,
-                                  "maximum vertex y range [cm]"};
-Configurable<float> cVertex_z_min{"cVertex_z_min", -10.0,
-                                  "minimum vertex z range [cm]"};
-Configurable<float> cVertex_z_max{"cVertex_z_max", 10.0,
-                                  "maximum vertex z range [cm]"};
-Configurable<int> cNContributors_min{
-  "cNContributors_min", -1,
-  "minimum number of vertex contributors (set to -1 to ignore)"};
-Configurable<int> cNContributors_max{"cNContributors_max", 1000000000,
-                                     "maximum number of vertex contributors"};
-Configurable<int> cImpactParameter_min{
-  "cImpactParameter_min", -1,
-  "minimum value of impact parameter (can be used only for sim) (set to -1 to ignore)"};
-Configurable<int> cImpactParameter_max{"cImpactParameter_max", 1000000000,
-                                       "maximum value of impact parameter (can be used osnly for sim)"};
+Configurable<string> cfCentralityEstimator{"cfCentralityEstimator", "some supported centrality estimator", "set here some supported centrality estimator (CentFT0M, CentFV0A, CentNTPV, ... for Run 3, CentRun2V0M, CentRun2SPDTracklets, ..., for Run 2) "};
+Configurable<vector<int>> cNumberOfEvents{"cNumberOfEvents", {-1, 1000000000}, "Number of events to process: {min, max}, with convention: min <= N < max"};
+Configurable<vector<int>> cTotalMultiplicity{"cTotalMultiplicity", {-1, 1000000000}, "Total multiplicity range: {min, max}, with convention: min <= M < max"};
+Configurable<vector<int>> cSelectedTracks{"cSelectedTracks", {-1, 1000000000}, "Selected tracks range: {min, max}, with convention: min <= M < max"};
+Configurable<vector<float>> cCentrality{"cCentrality", {-10., 110.}, "Centrality range: {min, max}, with convention: min <= cent < max"};
+Configurable<vector<float>> cVertex_x{"cVertex_x", {-10., 10.}, "Vertex x position range: {min, max}[cm], with convention: min <= Vx < max"};
+Configurable<vector<float>> cVertex_y{"cVertex_y", {-10., 10.}, "Vertex y position range: {min, max}[cm], with convention: min <= Vy < max"};
+Configurable<vector<float>> cVertex_z{"cVertex_z", {-10., 10.}, "Vertex z position range: {min, max}[cm], with convention: min <= Vz < max"};
+Configurable<vector<int>> cNContributors{"cNContributors", {-1, 1000000000}, "Number of vertex contributors: {min, max}, with convention: min <= IP < max"};
+Configurable<vector<float>> cImpactParameter{"cImpactParameter", {-1, 1000000000}, "Impact parameter range (can be used osnly for sim): {min, max}, with convention: min <= IP < max"};
 
 // Particle cuts:
 Configurable<float> pt_min{"pt_min", 0.2, "minimum track pt value [GeV/c]"};
 Configurable<float> pt_max{"pt_max", 5.0, "maximum track pt value [GeV/c]"};
 
 // Results histograms:
-Configurable<bool> cfSaveResultsHistograms{"cfSaveResultsHistograms", false,
-                                           "save or not results histograms"};
+Configurable<bool> cfSaveResultsHistograms{"cfSaveResultsHistograms", false, "save or not results histograms"};
 
 // Fixed-length binning (default):
 Configurable<vector<float>> cFixedLength_mult_bins{"cFixedLength_mult_bins", {2000, 0., 20000.}, "nMultBins, multMin, multMax"};
