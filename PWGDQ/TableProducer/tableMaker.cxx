2e8def93856fd9a7b59528537bec3f7067782943
Bhagyarathi Sahoo <125358702+bhsaho@users.noreply.github.com>
2024-04-29 20:36:31 +0530

    Update tableMaker.cxx

--- PWGDQ/TableProducer/tableMaker.cxx
+++ PWGDQ/TableProducer/tableMaker.cxx
@@ -1561,7 +1561,8 @@ struct TableMaker {
       auto muonIdsThisCollision = fwdtrackIndices.sliceBy(fwdtrackIndicesPerCollision, collision.globalIndex());
       fullSkimmingIndices<gkEventFillMapWithMult, 0u, gkMuonFillMapWithCovAmbi>(collision, bcs, nullptr, tracksMuon, nullptr, muonIdsThisCollision);
     }
-  }   
+  }
+                                              
   void processAssociatedMuonOnlyWithCovAndCent(MyEventsWithCent const& collisions, aod::BCsWithTimestamps const& bcs,
                                                soa::Filtered<MyMuonsCollWithCov> const& tracksMuon, aod::AmbiguousTracksFwd const&, aod::FwdTrackAssoc const& fwdtrackIndices)
   {
