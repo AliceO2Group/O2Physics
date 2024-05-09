c2caf4014536f464637c0db983ea5ba76667c7f5
Daniel Samitz <69901155+DanielSamitz@users.noreply.github.com>
2024-05-09 09:14:55 +0200

    PWGEM: lmeeHFCocktail.cxx: make axes configurable (#6079)
    * make axes configurable
    
    * use ConfigurableAxis

--- PWGEM/Dilepton/Tasks/lmeeHFCocktail.cxx
+++ PWGEM/Dilepton/Tasks/lmeeHFCocktail.cxx
@@ -48,10 +48,6 @@ DECLARE_SOA_TABLE(HfTable, "AOD", "HFTABLE",
                   hftable::IsHF);
 } // namespace o2::aod
 
-AxisSpec eta_axis = {100, -6., 6., "#eta"};
-AxisSpec pt_axis = {100, 0., 10., "#it{p}_{T} (GeV/c)"};
-AxisSpec mass_axis = {1000, 0., 10., "m_{ee} (GeV/c^{2})"};
-AxisSpec ptee_axis = {100, 0., 10., "#it{p}_{T,ee} (GeV/c)"};
 const char* stageNames[3] = {"gen", "eff", "eff_and_acc"};
 
 template <typename T>
@@ -136,6 +132,10 @@ struct lmeehfcocktailbeauty {
 
   Configurable<float> fConfigPtMin{"cfgPtMin", 0.2, "min. pT"};
   Configurable<float> fConfigEtaMax{"cfgEtaMax", 0.8, "max. |eta|"};
+  ConfigurableAxis fConfigPtBins{"cfgPtBins", {200, 0.f, 10.f}, "pT binning"};
+  ConfigurableAxis fConfigEtaBins{"cfgEtaBins", {200, -10.f, 10.f}, "eta binning"};
+  ConfigurableAxis fConfigMeeBins{"cfgMeeBins", {800, 0.f, 8.f}, "Mee binning"};
+  ConfigurableAxis fConfigPteeBins{"cfgPteeBins", {400, 0.f, 10.f}, "pTee binning"};
 
   Filter hfFilter = o2::aod::hftable::isHF == static_cast<int>(EFromHFType::kBE) || o2::aod::hftable::isHF == static_cast<int>(EFromHFType::kBCE);
   using MyFilteredMcParticlesSmeared = soa::Filtered<soa::Join<aod::McParticles, aod::SmearedTracks, aod::HfTable>>;
@@ -153,6 +153,11 @@ struct lmeehfcocktailbeauty {
     const char* typeNamesPairLS = "BCe_Be_DiffB";
     const char* typeNamesSingle[2] = {"be", "bce"};
     const char* typeTitlesSingle[2] = {"b->e", "b->c->e"};
+
+    AxisSpec eta_axis = {fConfigEtaBins, "#eta"};
+    AxisSpec pt_axis = {fConfigPtBins, "#it{p}_{T} (GeV/c)"};
+    AxisSpec mass_axis = {fConfigMeeBins, "m_{ee} (GeV/c^{2})"};
+    AxisSpec ptee_axis = {fConfigPteeBins, "#it{p}_{T,ee} (GeV/c)"};
 
     // single histograms
     for (int i = 0; i < 2; i++) {
@@ -251,8 +256,12 @@ struct lmeehfcocktailcharm {
   std::vector<std::shared_ptr<TH1>> hEta, hPt, hULS_Mee;
   std::vector<std::shared_ptr<TH2>> hPtEta, hULS_MeePtee;
 
-  Configurable<float> fConfigPtMin{"cfgPtMin", 0.2, "min. pT"};
-  Configurable<float> fConfigEtaMax{"cfgEtaMax", 0.8, "max. |eta|"};
+  Configurable<float> fConfigPtMin{"cfgPtMin", 0.2f, "min. pT"};
+  Configurable<float> fConfigEtaMax{"cfgEtaMax", 0.8f, "max. |eta|"};
+  ConfigurableAxis fConfigPtBins{"cfgPtBins", {200, 0.f, 10.f}, "pT binning"};
+  ConfigurableAxis fConfigEtaBins{"cfgEtaBins", {200, -10.f, 10.f}, "eta binning"};
+  ConfigurableAxis fConfigMeeBins{"cfgMeeBins", {800, 0.f, 8.f}, "Mee binning"};
+  ConfigurableAxis fConfigPteeBins{"cfgPteeBins", {400, 0.f, 10.f}, "pTee binning"};
 
   Filter hfFilter = o2::aod::hftable::isHF == static_cast<int>(EFromHFType::kCE);
   using MyFilteredMcParticlesSmeared = soa::Filtered<soa::Join<aod::McParticles, aod::SmearedTracks, aod::HfTable>>;
@@ -269,6 +278,11 @@ struct lmeehfcocktailcharm {
     const char* typeNamesPairULS = "Ce_Ce";
     const char* typeNamesSingle = "ce";
     const char* typeTitlesSingle = "c->e";
+
+    AxisSpec eta_axis = {fConfigEtaBins, "#eta"};
+    AxisSpec pt_axis = {fConfigPtBins, "#it{p}_{T} (GeV/c)"};
+    AxisSpec mass_axis = {fConfigMeeBins, "m_{ee} (GeV/c^{2})"};
+    AxisSpec ptee_axis = {fConfigPteeBins, "#it{p}_{T,ee} (GeV/c)"};
 
     // single histograms
     for (int j = 0; j < 3; j++) {
