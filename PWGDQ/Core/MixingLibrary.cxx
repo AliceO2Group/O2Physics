91ab65e04f3e55f71d006956859e3eb0b7f56d2a
Raphaelle Bailhache <raphaelle.bailhache@cern.ch>
2024-05-12 19:51:26 +0200

    gix

--- PWGDQ/Core/MixingLibrary.cxx
+++ PWGDQ/Core/MixingLibrary.cxx
@@ -77,27 +77,27 @@ void o2::aod::dqmixing::SetUpMixing(MixingHandler* mh, const char* mixingVarible
     mh->AddMixingVariable(VarManager::kVtxZ, fZLimsHashing.size(), fZLimsHashing);
   }
   if (!nameStr.compare("Psi2A1")) {
-    std::vector<float> fPsi2A = {0.0f, TMath::Pi()/2., TMath::Pi()};
+    std::vector<float> fPsi2A = {0.0f, TMath::Pi() / 2., TMath::Pi()};
     mh->AddMixingVariable(VarManager::kPsi2A, fPsi2A.size(), fPsi2A);
   }
   if (!nameStr.compare("Psi2A2")) {
-    std::vector<float> fPsi2A = {0.0f, TMath::Pi()/4., TMath::Pi()/2., 3.*TMath::Pi()/4., TMath::Pi()};
+    std::vector<float> fPsi2A = {0.0f, TMath::Pi() / 4., TMath::Pi() / 2., 3. * TMath::Pi() / 4., TMath::Pi()};
     mh->AddMixingVariable(VarManager::kPsi2A, fPsi2A.size(), fPsi2A);
   }
   if (!nameStr.compare("Psi2B1")) {
-    std::vector<float> fPsi2B = {0.0f, TMath::Pi()/2., TMath::Pi()};
+    std::vector<float> fPsi2B = {0.0f, TMath::Pi() / 2., TMath::Pi()};
     mh->AddMixingVariable(VarManager::kPsi2B, fPsi2B.size(), fPsi2B);
   }
   if (!nameStr.compare("Psi2B2")) {
-    std::vector<float> fPsi2B = {0.0f, TMath::Pi()/4., TMath::Pi()/2., 3.*TMath::Pi()/4., TMath::Pi()};
+    std::vector<float> fPsi2B = {0.0f, TMath::Pi() / 4., TMath::Pi() / 2., 3. * TMath::Pi() / 4., TMath::Pi()};
     mh->AddMixingVariable(VarManager::kPsi2B, fPsi2B.size(), fPsi2B);
   }
   if (!nameStr.compare("Psi2C1")) {
-    std::vector<float> fPsi2C = {0.0f, TMath::Pi()/2., TMath::Pi()};
+    std::vector<float> fPsi2C = {0.0f, TMath::Pi() / 2., TMath::Pi()};
     mh->AddMixingVariable(VarManager::kPsi2C, fPsi2C.size(), fPsi2C);
   }
   if (!nameStr.compare("Psi2C2")) {
-    std::vector<float> fPsi2C = {0.0f, TMath::Pi()/4., TMath::Pi()/2., 3.*TMath::Pi()/4., TMath::Pi()};
+    std::vector<float> fPsi2C = {0.0f, TMath::Pi() / 4., TMath::Pi() / 2., 3. * TMath::Pi() / 4., TMath::Pi()};
     mh->AddMixingVariable(VarManager::kPsi2C, fPsi2C.size(), fPsi2C);
   }
 }
