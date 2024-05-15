554a40061d0b992363c8ea2fdb2f19a54648a68e
Vít Kučera <vit.kucera@cern.ch>
2024-05-15 14:56:30 +0200

    Fill Y in JE

--- PWGJE/Core/JetHFUtilities.h
+++ PWGJE/Core/JetHFUtilities.h
@@ -488,7 +488,7 @@ template <bool isMc, typename T, typename U, typename V, typename M, typename N,
 void fillD0CandidateTable(T const& candidate, int32_t collisionIndex, U& D0BaseTable, V& D0ParTable, M& D0ParETable, N& D0SelectionFlagTable, O& D0MlTable, P& D0MCDTable, int32_t& D0CandidateTableIndex)
 {
 
-  D0BaseTable(collisionIndex, candidate.pt(), candidate.eta(), candidate.phi(), candidate.m());
+  D0BaseTable(collisionIndex, candidate.pt(), candidate.eta(), candidate.phi(), candidate.m(), candidate.y());
 
   D0ParTable(
     candidate.chi2PCA(),
@@ -560,7 +560,7 @@ template <bool isMc, typename T, typename U, typename V, typename M, typename N,
 void fillLcCandidateTable(T const& candidate, int32_t collisionIndex, U& LcBaseTable, V& LcParTable, M& LcParETable, N& LcSelectionFlagTable, O& LcMlTable, P& LcMCDTable, int32_t& LcCandidateTableIndex)
 {
 
-  LcBaseTable(collisionIndex, candidate.pt(), candidate.eta(), candidate.phi(), candidate.m());
+  LcBaseTable(collisionIndex, candidate.pt(), candidate.eta(), candidate.phi(), candidate.m(), candidate.y());
 
   LcParTable(
     candidate.chi2PCA(),
@@ -650,13 +650,13 @@ void fillCandidateTable(T const& candidate, int32_t collisionIndex, U& HFBaseTab
 template <typename T, typename U>
 void fillD0CandidateMcTable(T const& candidate, U& D0PBaseTable, int32_t& D0CandidateTableIndex)
 {
-  D0PBaseTable(candidate.hfD0McCollBaseId(), candidate.pt(), candidate.eta(), candidate.phi(), candidate.flagMcMatchGen(), candidate.originMcGen());
+  D0PBaseTable(candidate.hfD0McCollBaseId(), candidate.pt(), candidate.eta(), candidate.phi(), candidate.y(), candidate.flagMcMatchGen(), candidate.originMcGen());
   D0CandidateTableIndex = D0PBaseTable.lastIndex();
 }
 template <typename T, typename U>
 void fillLcCandidateMcTable(T const& candidate, U& LcPBaseTable, int32_t& LcCandidateTableIndex)
 {
-  LcPBaseTable(candidate.hf3PMcCollBaseId(), candidate.pt(), candidate.eta(), candidate.phi(), candidate.flagMcMatchGen(), candidate.originMcGen());
+  LcPBaseTable(candidate.hf3PMcCollBaseId(), candidate.pt(), candidate.eta(), candidate.phi(), candidate.y(), candidate.flagMcMatchGen(), candidate.originMcGen());
   LcCandidateTableIndex = LcPBaseTable.lastIndex();
 }
 
