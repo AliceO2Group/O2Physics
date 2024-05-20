c59cab1b4621c9f1d47b099892a6e9fd58ae04cd
sweta <sweta.singh@cern.ch>
2024-05-20 13:40:59 +0200

    MeanpT-Fluctuations

--- PWGCF/EbyEFluctuations/Tasks/IdentifiedMeanPtFluctuations.cxx
+++ PWGCF/EbyEFluctuations/Tasks/IdentifiedMeanPtFluctuations.cxx
@@ -33,7 +33,6 @@
 #include <TPDGCode.h>
 #include "Common/CCDB/TriggerAliases.h"
 
-
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
@@ -490,8 +489,7 @@ struct IdentifiedMeanPtFluctuations {
 
       // pion-TPC-----------------------------------------------------------------------------------
 
-      if ((track1.hasTPC() && (track1.p() < 0.7) && abs(track1.tpcNSigmaPi()) < 3. && (std::abs(track1.tpcNSigmaKa()) > 3.0 && std::abs(track1.tpcNSigmaPr()) > 3.0)))
-      {
+      if ((track1.hasTPC() && (track1.p() < 0.7) && abs(track1.tpcNSigmaPi()) < 3. && (std::abs(track1.tpcNSigmaKa()) > 3.0 && std::abs(track1.tpcNSigmaPr()) > 3.0))) {
 
         count_rec_pi++;
         histos.fill(HIST("ptHistogramPionrec"), track1.pt());
@@ -504,7 +502,8 @@ struct IdentifiedMeanPtFluctuations {
         histos.fill(HIST("NSigamaTOFpion_rec"), track1.p(), track1.tofNSigmaPi());
         histos.fill(HIST("NSigamaTPCTOFpion_rec"), track1.tpcNSigmaPi(), track1.tofNSigmaPi());
 
-        if (track1.beta() > 1)    continue;
+        if (track1.beta() > 1)
+          continue;
 
         histos.fill(HIST("hdEdx_afterselection_rec_afterpidcut"), track1.p(), track1.tpcSignal());
         histos.fill(HIST("hTOFbeta_afterselection_rec_afterpidcut"), track1.p(), track1.beta());
