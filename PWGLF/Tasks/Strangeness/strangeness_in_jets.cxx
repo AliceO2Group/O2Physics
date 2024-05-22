76f2fb94fccc0c8eae986d9f3cab7709879e3f60
Alberto Caliva <alberto.caliva@cern.ch>
2024-05-22 18:19:31 +0200

    fixed copyright

--- PWGLF/Tasks/Strangeness/strangeness_in_jets.cxx
+++ PWGLF/Tasks/Strangeness/strangeness_in_jets.cxx
@@ -1,6 +1,6 @@
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright
-// holders. All rights not expressly granted are reserved.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
@@ -8,9 +8,9 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-//
-// Authors: Alberto Caliva, Francesca Ercolessi
-// Date: 22.05.2024
+///
+/// \author Alberto Caliva (alberto.caliva@cern.ch), Francesca Ercolessi (francesca.ercolessi@cern.ch)
+/// \since May 22, 2024
 
 #include <TDatabasePDG.h>
 #include <TLorentzVector.h>
