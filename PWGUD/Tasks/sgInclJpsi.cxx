81988302b2eedf9c46d73d1c4c0a9cfe502d0cf1
Sasha Bylinkin <37345380+abylinkin@users.noreply.github.com>
2024-05-29 13:51:19 +0200

    Add files via upload

--- PWGUD/Tasks/sgInclJpsi.cxx
+++ PWGUD/Tasks/sgInclJpsi.cxx
@@ -38,7 +38,7 @@ struct SGInclJpsi {
   Configurable<float> FDDA_cut{"FDDA", 10000., "FDDA threshold"};
   Configurable<float> FDDC_cut{"FDDC", 10000., "FDDC threshold"};
   Configurable<float> ZDC_cut{"ZDC", 1., "ZDC threshold"};
-  // Track Selections
+  //Track Selections
   Configurable<float> PV_cut{"PV_cut", 1.0, "Use Only PV tracks"};
   Configurable<float> dcaZ_cut{"dcaZ_cut", 2.0, "dcaZ cut"};
   Configurable<float> dcaXY_cut{"dcaXY_cut", 0, "dcaXY cut (0 for Pt-function)"};
@@ -47,7 +47,10 @@ struct SGInclJpsi {
   Configurable<float> itsChi2_cut{"itsChi2_cut", 36, "Max itsChi2NCl"};
   Configurable<float> eta_cut{"eta_cut", 0.9, "Track Pseudorapidity"};
   Configurable<float> pt_cut{"pt_cut", 0.1, "Track Pt"};
-  // D0 Specific Cuts
+  //JPsi Specific Cuts
+  Configurable<float> nsigmatpc_cut{"nsigmatpc", 3.0, "nsigma tpc cut"};
+  Configurable<float> nsigmatof_cut{"nsigmatof", 9.0, "nsigma tof cut"};
+  Configurable<bool> use_tof{"Use_TOF", true, "TOF PID"};
   HistogramRegistry registry{
     "registry",
     {
@@ -89,10 +92,18 @@ struct SGInclJpsi {
       {"ss_mm_pT_3", "pT (GeV/c); Entries", {HistType::kTH1F, {{5000, 0, 10}}}},
       {"ss_mm_eTa_3", "eTa (GeV/c); Entries", {HistType::kTH1F, {{100, -1., 1.}}}},
       {"ss_mm_invm_3", "Mass (GeV/c^2); Entries", {HistType::kTH1F, {{5000, 0, 10}}}},
-      {"os_Ntr_mm_invm_0", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5}, {600, 2., 5.}}}},
-      {"os_Ntr_mm_invm_1", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5}, {600, 2., 5.}}}},
-      {"os_Ntr_mm_invm_2", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5}, {600, 2., 5.}}}},
-      {"os_Ntr_mm_invm_3", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5}, {600, 2., 5.}}}},
+      {"os_Ntr_mm_invm_0", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"os_Ntr_mm_invm_1", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"os_Ntr_mm_invm_2", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"os_Ntr_mm_invm_3", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"ss_Ntr_mm_invm_0", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"ss_Ntr_mm_invm_1", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"ss_Ntr_mm_invm_2", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"ss_Ntr_mm_invm_3", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"sss_Ntr_mm_invm_0", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"sss_Ntr_mm_invm_1", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"sss_Ntr_mm_invm_2", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"sss_Ntr_mm_invm_3", "N tracks vs #mu#mu Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
       {"os_ee_pT", "pT (GeV/c); Entries", {HistType::kTH1F, {{5000, 0, 10}}}},
       {"os_ee_eTa", "eTa (GeV/c); Entries", {HistType::kTH1F, {{100, -1., 1.}}}},
       {"os_ee_invm", "Mass (GeV/c^2); Entries", {HistType::kTH1F, {{5000, 0, 10}}}},
@@ -123,18 +134,42 @@ struct SGInclJpsi {
       {"ss_ee_pT_3", "pT (GeV/c); Entries", {HistType::kTH1F, {{5000, 0, 10}}}},
       {"ss_ee_eTa_3", "eTa (GeV/c); Entries", {HistType::kTH1F, {{100, -1., 1.}}}},
       {"ss_ee_invm_3", "Mass (GeV/c^2); Entries", {HistType::kTH1F, {{5000, 0, 10}}}},
-      {"os_ee_pt_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{500, 0, 10}, {600, 2., 5.}}}},
-      {"os_ee_pt_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{500, 0, 10}, {600, 2., 5.}}}},
-      {"os_ee_pt_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{500, 0, 10}, {600, 2., 5.}}}},
-      {"os_ee_pt_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{500, 0, 10}, {600, 2., 5.}}}},
-      {"os_mm_pt_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{500, 0, 10}, {600, 2., 5.}}}},
-      {"os_mm_pt_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{500, 0, 10}, {600, 2., 5.}}}},
-      {"os_mm_pt_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{500, 0, 10}, {600, 2., 5.}}}},
-      {"os_mm_pt_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{500, 0, 10}, {600, 2., 5.}}}},
-      {"os_Ntr_ee_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5}, {600, 2., 5.}}}},
-      {"os_Ntr_ee_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5}, {600, 2., 5.}}}},
-      {"os_Ntr_ee_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5}, {600, 2., 5.}}}},
-      {"os_Ntr_ee_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5}, {600, 2., 5.}}}},
+      {"os_ee_pt_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"os_ee_pt_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"os_ee_pt_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"os_ee_pt_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"os_mm_pt_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"os_mm_pt_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"os_mm_pt_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"os_mm_pt_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"os_Ntr_ee_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"os_Ntr_ee_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"os_Ntr_ee_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"os_Ntr_ee_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"ss_ee_pt_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"ss_ee_pt_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"ss_ee_pt_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"ss_ee_pt_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"ss_mm_pt_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"ss_mm_pt_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"ss_mm_pt_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"ss_mm_pt_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"ss_Ntr_ee_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"ss_Ntr_ee_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"ss_Ntr_ee_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"ss_Ntr_ee_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"sss_ee_pt_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"sss_ee_pt_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"sss_ee_pt_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"sss_ee_pt_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"sss_mm_pt_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"sss_mm_pt_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"sss_mm_pt_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"sss_mm_pt_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{500,0,10},{600, 2., 5.}}}},
+      {"sss_Ntr_ee_invm_0", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"sss_Ntr_ee_invm_1", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"sss_Ntr_ee_invm_2", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
+      {"sss_Ntr_ee_invm_3", "N tracks vs ee Mass", {HistType::kTH2F, {{48, 1.5, 49.5},{600, 2., 5.}}}},
     }};
   using udtracks = soa::Join<aod::UDTracks, aod::UDTracksExtra, aod::UDTracksPID>;
   using udtracksfull = soa::Join<aod::UDTracks, aod::UDTracksPID, aod::UDTracksExtra, aod::UDTracksFlags, aod::UDTracksDCA>;
@@ -160,26 +195,20 @@ struct SGInclJpsi {
     registry.fill(HIST("GapSide"), gapSide);
     registry.fill(HIST("TrueGapSide"), truegapSide);
     gapSide = truegapSide;
-    int pvtracks = 0;
+    int pvtracks=0;
     for (auto& t0 : tracks) {
-      if (trackselector(t0, parameters) && t0.isPVContributor())
-        pvtracks++;
+	    if (trackselector(t0, parameters) && t0.isPVContributor())pvtracks++;
     }
     registry.fill(HIST("Ntr"), pvtracks);
-    if (gapSide == 0)
-      registry.fill(HIST("Ntr_0"), pvtracks);
-    if (gapSide == 1)
-      registry.fill(HIST("Ntr_1"), pvtracks);
-    if (gapSide == 2)
-      registry.fill(HIST("Ntr_2"), pvtracks);
-    if (gapSide == -1)
-      registry.fill(HIST("Ntr_3"), pvtracks);
+    if (gapSide == 0) registry.fill(HIST("Ntr_0"), pvtracks);
+    if (gapSide == 1) registry.fill(HIST("Ntr_1"), pvtracks);
+    if (gapSide == 2) registry.fill(HIST("Ntr_2"), pvtracks);
+    if (gapSide == -1) registry.fill(HIST("Ntr_3"), pvtracks);
     // Look for D0 and D0bar
     for (auto& [t0, t1] : combinations(tracks, tracks)) {
       // PID cut - t0=K, t1=pi
-      if (!trackselector(t0, parameters) || !trackselector(t1, parameters))
-        continue;
-      if (std::abs(t0.tpcNSigmaMu()) < 3 && std::abs(t1.tpcNSigmaMu()) < 3) {
+      if (!trackselector(t0, parameters)||!trackselector(t1,parameters))continue;
+      if (selectionPIDMuon(t0, use_tof, nsigmatpc_cut, nsigmatof_cut) && selectionPIDMuon(t0, use_tof, nsigmatpc_cut, nsigmatof_cut)){
         // Apply pion hypothesis and create pairs
         v0.SetXYZM(t0.px(), t0.py(), t0.pz(), o2::constants::physics::MassMuon);
         v1.SetXYZM(t1.px(), t1.py(), t1.pz(), o2::constants::physics::MassMuon);
@@ -195,24 +224,32 @@ struct SGInclJpsi {
             registry.fill(HIST("os_mm_invm_0"), v01.M());
             registry.fill(HIST("os_Ntr_mm_invm_0"), tracks.size(), v01.M());
             registry.fill(HIST("os_mm_pt_invm_0"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_mm_invm_0"), tracks.size(), v01.M());
+            registry.fill(HIST("sss_mm_pt_invm_0"), v01.Pt(), v01.M());
           } else if (gapSide == 1) {
             registry.fill(HIST("os_mm_pT_1"), v01.Pt());
             registry.fill(HIST("os_mm_eTa_1"), v01.Eta());
             registry.fill(HIST("os_mm_invm_1"), v01.M());
             registry.fill(HIST("os_Ntr_mm_invm_1"), tracks.size(), v01.M());
             registry.fill(HIST("os_mm_pt_invm_1"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_mm_invm_1"), tracks.size(), v01.M());
+            registry.fill(HIST("sss_mm_pt_invm_1"), v01.Pt(), v01.M());
           } else if (gapSide == 2) {
             registry.fill(HIST("os_mm_pT_2"), v01.Pt());
             registry.fill(HIST("os_mm_eTa_2"), v01.Eta());
             registry.fill(HIST("os_mm_invm_2"), v01.M());
             registry.fill(HIST("os_Ntr_mm_invm_2"), tracks.size(), v01.M());
             registry.fill(HIST("os_mm_pt_invm_2"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_mm_invm_2"), tracks.size(), v01.M());
+            registry.fill(HIST("sss_mm_pt_invm_2"), v01.Pt(), v01.M());
           } else {
             registry.fill(HIST("os_mm_pT_3"), v01.Pt());
             registry.fill(HIST("os_mm_eTa_3"), v01.Eta());
             registry.fill(HIST("os_mm_invm_3"), v01.M());
             registry.fill(HIST("os_Ntr_mm_invm_3"), tracks.size(), v01.M());
             registry.fill(HIST("os_mm_pt_invm_3"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_mm_invm_3"), tracks.size(), v01.M());
+            registry.fill(HIST("sss_mm_pt_invm_3"), v01.Pt(), v01.M());
           }
         } else if (t0.sign() == t1.sign()) {
           registry.fill(HIST("ss_mm_pT"), v01.Pt());
@@ -222,20 +259,41 @@ struct SGInclJpsi {
             registry.fill(HIST("ss_mm_pT_0"), v01.Pt());
             registry.fill(HIST("ss_mm_eTa_0"), v01.Eta());
             registry.fill(HIST("ss_mm_invm_0"), v01.M());
+            registry.fill(HIST("ss_Ntr_mm_invm_0"), tracks.size(), v01.M());
+            registry.fill(HIST("ss_mm_pt_invm_0"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_mm_invm_0"), tracks.size(), v01.M(),-1);
+            registry.fill(HIST("sss_mm_pt_invm_0"), v01.Pt(), v01.M(),-1);
           }
           if (gapSide == 1) {
             registry.fill(HIST("ss_mm_pT_1"), v01.Pt());
             registry.fill(HIST("ss_mm_eTa_1"), v01.Eta());
             registry.fill(HIST("ss_mm_invm_1"), v01.M());
+            registry.fill(HIST("ss_Ntr_mm_invm_1"), tracks.size(), v01.M());
+            registry.fill(HIST("ss_mm_pt_invm_1"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_mm_invm_1"), tracks.size(), v01.M(),-1);
+            registry.fill(HIST("sss_mm_pt_invm_1"), v01.Pt(), v01.M(),-1);
           }
           if (gapSide == 2) {
             registry.fill(HIST("ss_mm_pT_2"), v01.Pt());
             registry.fill(HIST("ss_mm_eTa_2"), v01.Eta());
             registry.fill(HIST("ss_mm_invm_2"), v01.M());
+            registry.fill(HIST("ss_Ntr_mm_invm_2"), tracks.size(), v01.M());
+            registry.fill(HIST("ss_mm_pt_invm_2"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_mm_invm_2"), tracks.size(), v01.M(),-1);
+            registry.fill(HIST("sss_mm_pt_invm_2"), v01.Pt(), v01.M(),-1);
           }
-        }
+          if (gapSide == -1) {
+            registry.fill(HIST("ss_mm_pT_3"), v01.Pt());
+            registry.fill(HIST("ss_mm_eTa_3"), v01.Eta());
+            registry.fill(HIST("ss_mm_invm_3"), v01.M());
+            registry.fill(HIST("ss_Ntr_mm_invm_3"), tracks.size(), v01.M());
+            registry.fill(HIST("ss_mm_pt_invm_3"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_mm_invm_3"), tracks.size(), v01.M(),-1);
+            registry.fill(HIST("sss_mm_pt_invm_3"), v01.Pt(), v01.M(),-1);
+          }
       }
-      if (std::abs(t0.tpcNSigmaEl()) < 3 && std::abs(t1.tpcNSigmaEl()) < 3) {
+    }
+      if (selectionPIDElec(t0, use_tof, nsigmatpc_cut, nsigmatof_cut) && selectionPIDElec(t0, use_tof, nsigmatpc_cut, nsigmatof_cut)){
         // Apply pion hypothesis and create pairs
         v0.SetXYZM(t0.px(), t0.py(), t0.pz(), o2::constants::physics::MassElectron);
         v1.SetXYZM(t1.px(), t1.py(), t1.pz(), o2::constants::physics::MassElectron);
@@ -251,24 +309,32 @@ struct SGInclJpsi {
             registry.fill(HIST("os_ee_invm_0"), v01.M());
             registry.fill(HIST("os_Ntr_ee_invm_0"), tracks.size(), v01.M());
             registry.fill(HIST("os_ee_pt_invm_0"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_ee_pt_invm_0"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_ee_invm_0"), tracks.size(), v01.M());
           } else if (gapSide == 1) {
             registry.fill(HIST("os_ee_pT_1"), v01.Pt());
             registry.fill(HIST("os_ee_eTa_1"), v01.Eta());
             registry.fill(HIST("os_ee_invm_1"), v01.M());
             registry.fill(HIST("os_Ntr_ee_invm_1"), tracks.size(), v01.M());
             registry.fill(HIST("os_ee_pt_invm_1"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_ee_invm_1"), tracks.size(), v01.M());
+            registry.fill(HIST("sss_ee_pt_invm_1"), v01.Pt(), v01.M());
           } else if (gapSide == 2) {
             registry.fill(HIST("os_ee_pT_2"), v01.Pt());
             registry.fill(HIST("os_ee_eTa_2"), v01.Eta());
             registry.fill(HIST("os_ee_invm_2"), v01.M());
             registry.fill(HIST("os_Ntr_ee_invm_2"), tracks.size(), v01.M());
             registry.fill(HIST("os_ee_pt_invm_2"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_ee_invm_2"), tracks.size(), v01.M());
+            registry.fill(HIST("sss_ee_pt_invm_2"), v01.Pt(), v01.M());
           } else {
             registry.fill(HIST("os_ee_pT_3"), v01.Pt());
             registry.fill(HIST("os_ee_eTa_3"), v01.Eta());
             registry.fill(HIST("os_ee_invm_3"), v01.M());
             registry.fill(HIST("os_Ntr_ee_invm_3"), tracks.size(), v01.M());
             registry.fill(HIST("os_ee_pt_invm_3"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_ee_invm_3"), tracks.size(), v01.M());
+            registry.fill(HIST("sss_ee_pt_invm_3"), v01.Pt(), v01.M());
           }
         } else if (t0.sign() == t1.sign()) {
           registry.fill(HIST("ss_ee_pT"), v01.Pt());
@@ -278,21 +344,42 @@ struct SGInclJpsi {
             registry.fill(HIST("ss_ee_pT_0"), v01.Pt());
             registry.fill(HIST("ss_ee_eTa_0"), v01.Eta());
             registry.fill(HIST("ss_ee_invm_0"), v01.M());
+            registry.fill(HIST("ss_Ntr_ee_invm_0"), tracks.size(), v01.M());
+            registry.fill(HIST("ss_ee_pt_invm_0"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_ee_invm_0"), tracks.size(), v01.M(),-1);
+            registry.fill(HIST("sss_ee_pt_invm_0"), v01.Pt(), v01.M(),-1);
           }
           if (gapSide == 1) {
             registry.fill(HIST("ss_ee_pT_1"), v01.Pt());
             registry.fill(HIST("ss_ee_eTa_1"), v01.Eta());
             registry.fill(HIST("ss_ee_invm_1"), v01.M());
+            registry.fill(HIST("ss_Ntr_ee_invm_1"), tracks.size(), v01.M());
+            registry.fill(HIST("ss_ee_pt_invm_1"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_ee_invm_1"), tracks.size(), v01.M(),-1);
+            registry.fill(HIST("sss_ee_pt_invm_1"), v01.Pt(), v01.M(),-1);
           }
           if (gapSide == 2) {
             registry.fill(HIST("ss_ee_pT_2"), v01.Pt());
             registry.fill(HIST("ss_ee_eTa_2"), v01.Eta());
             registry.fill(HIST("ss_ee_invm_2"), v01.M());
+            registry.fill(HIST("ss_Ntr_ee_invm_2"), tracks.size(), v01.M());
+            registry.fill(HIST("ss_ee_pt_invm_2"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_ee_invm_2"), tracks.size(), v01.M(),-1);
+            registry.fill(HIST("sss_ee_pt_invm_2"), v01.Pt(), v01.M(),-1);
           }
-        }
+          if (gapSide == -1) {
+            registry.fill(HIST("ss_ee_pT_3"), v01.Pt());
+            registry.fill(HIST("ss_ee_eTa_3"), v01.Eta());
+            registry.fill(HIST("ss_ee_invm_3"), v01.M());
+            registry.fill(HIST("ss_Ntr_ee_invm_3"), tracks.size(), v01.M());
+            registry.fill(HIST("ss_ee_pt_invm_3"), v01.Pt(), v01.M());
+            registry.fill(HIST("sss_Ntr_ee_invm_3"), tracks.size(), v01.M(),-1);
+            registry.fill(HIST("sss_ee_pt_invm_3"), v01.Pt(), v01.M(),-1);
+          }
       }
     }
   }
+ }
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
