b45d0a55f28a76f2211d57903df23eb68a671826
Lars Jörgensen <lars.christian.joergensen@cern.ch>
2024-05-31 00:26:49 +0200

    added rudimentary preliminary track filter

--- PWGLF/Tasks/Nuspex/AngularCorrelationsInJets.cxx
+++ PWGLF/Tasks/Nuspex/AngularCorrelationsInJets.cxx
@@ -13,7 +13,7 @@
 // Date: 29.05.2024
 
 #include <cmath>
-#include <TLorentzVector.h>
+#include <vector>
 #include <TMath.h>
 #include <TObjArray.h>
 
@@ -21,11 +21,10 @@
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
 #include "Framework/ASoAHelpers.h"
-#include "Common/DataModel/TrackSelectionTables.h"
-#include "Common/DataModel/EventSelection.h"
-#include "Common/DataModel/Centrality.h"
 #include "Framework/runDataProcessing.h"
 #include "Framework/HistogramRegistry.h"
+#include "Common/DataModel/TrackSelectionTables.h"
+#include "Common/DataModel/EventSelection.h"
 #include "PWGDQ/DataModel/ReducedInfoTables.h"
 #include "fastjet/PseudoJet.hh"
 #include "fastjet/AreaDefinition.hh"
@@ -39,12 +38,13 @@ using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
+using SelectedCollisions = soa::Join<aod::Collisions, aod::EvSels>;
+using FullTracks = soa::Join<aod::Tracks, aod::TracksExtra, aod::TrackSelection, aod::TrackSelectionExtension, aod::TracksDCA, aod::pidTPCFullPr, aod::pidTPCFullDe, aod::pidTPCFullHe, aod::pidTOFFullPr, aod::pidTOFFullDe, aod::pidTOFFullHe>;
+
 struct AngularCorrelationsInJets {
 
 	// HistogramRegistry registryName{"folderTitle", {}, OutputObjHandlingPolicy::AnalysisObject, <sortHistograms:bool>, <createDir:bool>};
-  HistogramRegistry jetOutputReg{"jetOutput", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
-  HistogramRegistry nucleiOutputReg{"nucleiOutput", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
-  HistogramRegistry correlationsOutputReg{"correlationsOutput", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
+  HistogramRegistry registryData{"jetOutput", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
 	// OutputObj<Type> histogramName{Type("histogramName", "histogramTitle;Axis", nbins,minbin,maxbin)};
 
   void init(o2::framework::InitContext&)
@@ -60,15 +60,15 @@ struct AngularCorrelationsInJets {
 
 		// registryName.add("histogramName", "histogramTitle", HistType::Type, {{binningInfo}});
 		
-    jetOutputReg.add("hPtFullEvent", "p_{T} after basic cuts", HistType::kTH1F, {ptAxis});
-		jetOutputReg.add("hNumPartInEvent", "Number of particles per event", HistType::kTH1I, {{500,0,500}});
+    registryData.add("hPtFullEvent", "p_{T} after basic cuts", HistType::kTH1F, {ptAxis});
+		registryData.add("hNumPartInEvent", "Number of particles per event", HistType::kTH1I, {{500,0,500}});
   }
 
 	// Configurable<Type> cfgName{"nameOnHyperloop", value, "Flag shown on hyperloop"};
 	Configurable<float> ptMin{"ptMin", 0.5, "Minimum pT for particles"};
 
 	// Preliminary Cuts
-	Configurable<bool> TPCRefit{"TPCRefit", false, "Require TPC refit"};
+	Configurable<bool> fTPCRefit{"TPCRefit", false, "Require TPC refit"};
 	Configurable<bool> fITSRefit{"ITSRefit", false, "Require ITS refit"};
 	Configurable<float> fMinNCrossedRowsTPC{"minNCrossedRowsTPC", 70.0f, "min number of crossed rows TPC"}; 
 	Configurable<float> fMinReqClusterITS{"minReqClusterITS", 2.0, "min number of clusters required in ITS"};
@@ -131,13 +131,14 @@ struct AngularCorrelationsInJets {
       if (track.pt()<ptMin)
         continue;
 
-      jetOutputReg.fill(HIST("hPtFullEvent"), track.pt());
+      registryData.fill(HIST("hPtFullEvent"), track.pt());
     }
   }
 
   //****************************************************************************************************
 
-  void process_jet_reco(aod::Collision const& collision, soa::Join<aod::Tracks,aod::TracksExtra, aod::TracksDCA> const& tracks)
+	Filter prelimTrackCuts = (aod::track::TPCrefit == fTPCRefit && aod::track::ITSrefit == fITSRefit && aod::track::itsChi2NCl < fMaxChi2ITS && aod::track::tpcChi2NCl < fMaxChi2TPC); // refits are int
+  void process_jet_reco(aod::Collision const& collision, soa::Filtered<soa::Join<aod::Tracks,aod::TracksExtra, aod::TracksDCA>> const& tracks)
   {
     jetReco(collision, tracks);
   }
