aec28dfbeed1765f001cda322b889bff0477cc9f
Vít Kučera <vit.kucera@cern.ch>
2024-06-03 17:35:50 +0200

    Fix type of loop variable

--- PWGHF/Utils/utilsEvSelHf.h
+++ PWGHF/Utils/utilsEvSelHf.h
@@ -202,7 +202,7 @@ struct HfEventSelection : o2::framework::ConfigurableGroup {
     const float posZ = collision.posZ();
     hPosZBeforeEvSel->Fill(posZ);
 
-    for (size_t reason = 1; reason < EventRejection::NEventRejection; reason++) {
+    for (EventRejection reason = 1; reason < EventRejection::NEventRejection; reason++) {
       if (TESTBIT(rejectionMask, reason)) {
         return;
       }
@@ -297,7 +297,7 @@ struct HfEventSelectionMc {
   {
     hParticles->Fill(EventRejection::None);
 
-    for (size_t reason = 1; reason < EventRejection::NEventRejection; reason++) {
+    for (EventRejection reason = 1; reason < EventRejection::NEventRejection; reason++) {
       if (TESTBIT(rejectionMask, reason)) {
         return;
       }
