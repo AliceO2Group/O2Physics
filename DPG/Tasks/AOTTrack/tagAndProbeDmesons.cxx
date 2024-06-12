386fc45822242f0a35b85c4178aa6805d7335932
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2024-06-12 13:34:25 +0200

    DPG/AOTTrack: check proper process functions

--- DPG/Tasks/AOTTrack/tagAndProbeDmesons.cxx
+++ DPG/Tasks/AOTTrack/tagAndProbeDmesons.cxx
@@ -272,6 +272,10 @@ struct TagTwoProngDisplacedVertices {
 
   void init(InitContext&)
   {
+    if ((doprocessPiPiFromDplus && doprocessPiPiFromDplusMc) || (doprocessKaKaFromDsOrDplus && doprocessKaKaFromDsOrDplusMc) || (doprocessKaPiFromDstar && doprocessKaPiFromDstarMc) || (doprocessKaKaFromDzero && doprocessKaKaFromDzeroMc)) {
+      LOGP(fatal, "The process functions for the same channel with and without MC truth cannot be enabled at the same time! Please check your configuration");
+    }
+
     std::string ccdbUrl = "http://alice-ccdb.cern.ch";
     ccdb->setURL(ccdbUrl.data());
     ccdb->setCaching(true);
@@ -297,20 +301,20 @@ struct TagTwoProngDisplacedVertices {
     const AxisSpec axisMassKaKa{200, constants::physics::MassPhi - 0.05f, constants::physics::MassPhi + 0.05f};
     const AxisSpec axisMassKaPi{400, constants::physics::MassD0 - 0.2f, constants::physics::MassD0 + 0.2f};
 
-    if (doprocessPiPiFromDplus) {
+    if (doprocessPiPiFromDplus || doprocessPiPiFromDplusMc) {
       registry.add<TH2>("hMassPiPiVsPt", ";#it{p}_{T}(#pi#pi) (GeV/#it{c}); #it{M}(#pi#pi) (GeV/#it{c}^{2})", HistType::kTH2D, {axisPt, axisMassPiPi});
     }
-    if (doprocessKaKaFromDsOrDplus) {
+    if (doprocessKaKaFromDsOrDplus || doprocessKaKaFromDsOrDplusMc) {
       registry.add<TH2>("hMassKaKaVsPt", ";#it{p}_{T}(KK) (GeV/#it{c}); #it{M}(KK) (GeV/#it{c}^{2})", HistType::kTH2D, {axisPt, axisMassKaKa});
     }
-    if (doprocessKaPiFromDstar) {
+    if (doprocessKaPiFromDstar || doprocessKaPiFromDstarMc) {
       if (!studyDzeroReflections) {
         registry.add<TH2>("hMassKaPiVsPt", ";#it{p}_{T}(K#pi) (GeV/#it{c}); #it{M}(K#pi) (GeV/#it{c}^{2})", HistType::kTH2D, {axisPt, axisMassKaPi});
       } else {
         registry.add<THnSparse>("hMassKaPiVsPt", ";#it{p}_{T}(K#pi) (GeV/#it{c}); #it{M}(K#pi) (GeV/#it{c}^{2}); #it{M}(#piK) (GeV/#it{c}^{2}); ReflFag", HistType::kTHnSparseF, {axisPtDzeroRefl, axisMassKaPi, axisMassKaPi, axisReflFlag});
       }
     }
-    if (doprocessKaKaFromDzero) {
+    if (doprocessKaKaFromDzero || doprocessKaKaFromDzeroMc) {
       registry.add<TH2>("hMassDzeroKaKaVsPt", ";#it{p}_{T}(K#pi) (GeV/#it{c}); #it{M}(K#pi) (GeV/#it{c}^{2})", HistType::kTH2D, {axisPt, axisMassKaPi});
     }
 
