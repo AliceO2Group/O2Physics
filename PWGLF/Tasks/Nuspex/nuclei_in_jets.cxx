9a62f9ae2f0f587f93f63be61451982c5f86f604
Alberto Caliva <alberto.caliva@cern.ch>
2024-06-13 16:38:07 +0200

    fixed typo

--- PWGLF/Tasks/Nuspex/nuclei_in_jets.cxx
+++ PWGLF/Tasks/Nuspex/nuclei_in_jets.cxx
@@ -33,7 +33,7 @@
 #include "ReconstructionDataFormats/DCA.h"
 #include "Common/Core/trackUtilities.h"
 #include "Common/Core/TrackSelection.h"
-#include "Common/DataModel/TrackSelectionTables.h
+#include "Common/DataModel/TrackSelectionTables.h"
 #include "Common/DataModel/EventSelection.h"
 #include "Common/DataModel/Centrality.h"
 #include "Common/DataModel/PIDResponse.h"
