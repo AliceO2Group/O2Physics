ec540000690a9cc6a1a67810c0a04f2158dd1392
DanielSamitz <daniel.samitz@cern.ch>
2024-06-13 15:05:54 +0200

    ccdb for smearing task

--- PWGEM/Dilepton/Tasks/smearing.cxx
+++ PWGEM/Dilepton/Tasks/smearing.cxx
@@ -22,6 +22,8 @@
 #include "Framework/HistogramRegistry.h"
 #include "PWGDQ/DataModel/ReducedInfoTables.h"
 #include "PWGEM/Dilepton/Utils/MomentumSmearer.h"
+#include <CCDB/BasicCCDBManager.h>
+#include <chrono>
 
 using namespace o2;
 using namespace o2::framework;
@@ -41,8 +43,14 @@ struct ApplySmearing {
   Configurable<std::string> fConfigResPhiNegHistName{"cfgResPhiNegHistName", "PhiEleResArr", "hisogram for phi neg in resolution file"};
   Configurable<std::string> fConfigEffFileName{"cfgEffFileName", "", "name of efficiency file"};
   Configurable<std::string> fConfigEffHistName{"cfgEffHistName", "fhwEffpT", "name of efficiency histogram"};
+  Configurable<bool> fFromCcdb{"cfgFromCcdb", false, "get resolution and efficiency histos from CCDB"};
+  Configurable<std::string> fConfigCcdbPathRes{"cfgCcdbPathRes", "", "path to the ccdb object for resolution"};
+  Configurable<std::string> fConfigCcdbPathEff{"cfgCcdbPahtEff", "", "path to the ccdb object for efficiency"};
+  Configurable<std::string> fConfigCcdbUrl{"cfgCcdbUrl", "http://alice-ccdb.cern.ch", "url of the ccdb repository"};
+  Configurable<long> fConfigCcdbNoLaterThan{"cfgCcdbNoLaterThan", std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(), "latest acceptable timestamp of creation for the object"};
 
   MomentumSmearer smearer;
+  Service<ccdb::BasicCCDBManager> ccdb;
 
   void init(InitContext&)
   {
@@ -53,6 +61,16 @@ struct ApplySmearing {
     smearer.setResPhiNegHistName(TString(fConfigResPhiNegHistName));
     smearer.setEffFileName(TString(fConfigEffFileName));
     smearer.setEffHistName(TString(fConfigEffHistName));
+    if (fFromCcdb) {
+      ccdb->setURL(fConfigCcdbUrl);
+      ccdb->setCaching(true);
+      ccdb->setLocalObjectValidityChecking();
+      ccdb->setCreatedNotAfter(fConfigCcdbNoLaterThan);
+      smearer.setCcdbPathRes(TString(fConfigCcdbPathRes));
+      smearer.setCcdbPathEff(TString(fConfigCcdbPathEff));
+      smearer.setTimestamp(fConfigCcdbNoLaterThan);
+      smearer.setCcdb(ccdb);
+    }
     smearer.init();
   }
 
