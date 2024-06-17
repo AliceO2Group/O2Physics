43f7f6c5bc18124133a103715262a87f0ccf1edf
Fabrizio <fabrizio.grosa@cern.ch>
2024-06-17 16:01:25 +0200

    PWGHF: use ZORRO utility to selected triggered events in HF code (#6533)
    * PWGHF: use ZORRO utility to selected triggered events in HF code
    
    * Add one more counter to D2H reduced data models
    
    * Please consider the following formatting changes
    
    * Implement Vit's comment
    
    * Pass BC table type only and document parameter
    
    * Fix typo
    
    ---------
    
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

--- PWGHF/D2H/Utils/utilsRedDataFormat.h
+++ PWGHF/D2H/Utils/utilsRedDataFormat.h
@@ -16,6 +16,7 @@
 #ifndef PWGHF_D2H_UTILS_UTILSREDDATAFORMAT_H_
 #define PWGHF_D2H_UTILS_UTILSREDDATAFORMAT_H_
 
+#include "CCDB/BasicCCDBManager.h"
 #include "PWGHF/Core/CentralityEstimation.h"
 #include "PWGHF/Utils/utilsEvSelHf.h"
 
@@ -25,19 +26,26 @@ namespace o2::hf_evsel
 /// \tparam useEvSel use information from the EvSel table
 /// \tparam centEstimator centrality estimator
 /// \param collision collision to test against the selection criteria
-template <bool useEvSel, o2::hf_centrality::CentralityEstimator centEstimator, typename Coll>
-void checkEvSel(Coll const& collision, o2::hf_evsel::HfEventSelection& hfEvSel, int& zvtxColl, int& sel8Coll, int& zvtxAndSel8Coll, int& allSelColl)
+template <bool useEvSel, o2::hf_centrality::CentralityEstimator centEstimator, typename BCs, typename Coll>
+void checkEvSel(Coll const& collision, o2::hf_evsel::HfEventSelection& hfEvSel, int& zvtxColl, int& sel8Coll, int& zvtxAndSel8Coll, int& zvtxAndSel8CollAndSoftTrig, int& allSelColl, o2::framework::Service<o2::ccdb::BasicCCDBManager> const& ccdb)
 {
   float centrality{-1.f};
-  const auto rejectionMask = hfEvSel.getHfCollisionRejectionMask<useEvSel, o2::hf_centrality::CentralityEstimator::None>(collision, centrality);
-  if (!TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::Trigger))
+  const auto rejectionMask = hfEvSel.getHfCollisionRejectionMask<useEvSel, o2::hf_centrality::CentralityEstimator::None, BCs>(collision, centrality, ccdb);
+  if (!TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::Trigger)) {
     sel8Coll++;
-  if (!TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::PositionZ))
+  }
+  if (!TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::PositionZ)) {
     zvtxColl++;
-  if (!TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::PositionZ) && !TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::Trigger))
+  }
+  if (!TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::PositionZ) && !TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::Trigger)) {
     zvtxAndSel8Coll++;
-  if (rejectionMask == 0)
+  }
+  if (!TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::PositionZ) && !TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::Trigger) && !TESTBIT(rejectionMask, o2::hf_evsel::EventRejection::SoftwareTrigger)) {
+    zvtxAndSel8CollAndSoftTrig++;
+  }
+  if (rejectionMask == 0) {
     allSelColl++;
+  }
 }
 } // namespace o2::hf_evsel
 
