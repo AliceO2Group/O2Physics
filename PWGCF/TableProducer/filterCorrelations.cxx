f1f04b664fe4f75cda7b92f8948e1160949a85bf
Sushanta Tripathy <sushanta.tripathy@cern.ch>
2024-07-08 17:49:36 +0200

    Update filterCorrelations.cxx

--- PWGCF/TableProducer/filterCorrelations.cxx
+++ PWGCF/TableProducer/filterCorrelations.cxx
@@ -108,8 +108,8 @@ struct FilterCF {
       int occupancy = collision.trackOccupancyInTimeRange();
       if (occupancy >= 0 && occupancy < 500) {
         return collision.sel8() && collision.selection_bit(aod::evsel::kNoSameBunchPileup) && collision.selection_bit(aod::evsel::kIsGoodZvtxFT0vsPV) && collision.selection_bit(aod::evsel::kNoCollInTimeRangeStandard);
-      }
-      else return false;
+      } else 
+        return false;
     }
     return false;
   }
