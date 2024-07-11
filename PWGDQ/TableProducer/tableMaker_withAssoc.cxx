0ab60246053fbaa4307969624d4fd017e62a0149
mcoquet642 <74600025+mcoquet642@users.noreply.github.com>
2024-07-11 20:08:15 +0200

    Fix typo
    Co-authored-by: Sergio Garcia <47090312+singiamtel@users.noreply.github.com>

--- PWGDQ/TableProducer/tableMaker_withAssoc.cxx
+++ PWGDQ/TableProducer/tableMaker_withAssoc.cxx
@@ -212,7 +212,7 @@ struct TableMaker {
 
   // Muon related options
   Configurable<bool> fPropMuon{"cfgPropMuon", true, "Propagate muon tracks through absorber (do not use if applying pairing)"};
-  Configurable<bool> fRefitGlobalMuon{"cfgRefitGlobalMuon", true, "Propgate muon tracks through absorber (do not use if applying pairing)"};
+  Configurable<bool> fRefitGlobalMuon{"cfgRefitGlobalMuon", true, "Propagate muon tracks through absorber (do not use if applying pairing)"};
 
   Service<o2::ccdb::BasicCCDBManager> fCCDB;
 
