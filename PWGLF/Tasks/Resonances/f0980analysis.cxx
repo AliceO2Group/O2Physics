a333e97ca6f039163a85c77b737719b02d7fb24e
Yunseul <ybae@cern.ch>
2024-07-20 01:23:17 +0900

    Apply clang-format changes

--- PWGLF/Tasks/Resonances/f0980analysis.cxx
+++ PWGLF/Tasks/Resonances/f0980analysis.cxx
@@ -94,12 +94,8 @@ struct f0980analysis {
 
   // Axis
   ConfigurableAxis massAxis{"massAxis", {400, 0.2, 2.2}, "Invariant mass axis"};
-  ConfigurableAxis ptAxis{"ptAxis", {VARIABLE_WIDTH, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8,
-                                                        1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5,
-                                                        5.0, 6.0, 7.0, 8.0, 10.0, 13.0, 20.0},"Transverse momentum Binning"};
-  ConfigurableAxis centAxis{"centAxis", {VARIABLE_WIDTH, 0.0, 1.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0,
-                                                            40.0,45.0, 50.0, 55.0, 60.0, 65.0, 70.0,
-                                                            75.0, 80.0, 95.0, 100.0, 105.0, 110.0},"Centrality  Binning"};
+  ConfigurableAxis ptAxis{"ptAxis", {VARIABLE_WIDTH, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 6.0, 7.0, 8.0, 10.0, 13.0, 20.0}, "Transverse momentum Binning"};
+  ConfigurableAxis centAxis{"centAxis", {VARIABLE_WIDTH, 0.0, 1.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0, 65.0, 70.0, 75.0, 80.0, 95.0, 100.0, 105.0, 110.0}, "Centrality  Binning"};
   void init(o2::framework::InitContext&)
   {
     std::vector<double> lptBinning = {0, 5.0, 13.0, 20.0, 50.0, 1000.0};
