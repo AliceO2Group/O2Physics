0c6183e672ab6c448c8d3e4aa6b0ee58e07bf773
spolitan <59452587+stefanopolitano@users.noreply.github.com>
2024-07-23 13:14:23 +0200

    PWGHF: adding centrality selection on generated events in candidate creator (#6932)
    * PWGHF: adding HF selctions on generated mc particles in candidate creators
    
    * PWGHF: inherit centrality selection on generated events from reconstructed ones
    
    * PWGHF: implementing PR comments on candidate creator at generator and reco levels
    
    * Please consider the following formatting changes
    
    * PWGHF: addressing comments on reco collision multiplicity estimator
    
    ---------
    
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

--- PWGHF/TableProducer/candidateCreator3Prong.cxx
+++ PWGHF/TableProducer/candidateCreator3Prong.cxx
@@ -453,6 +453,9 @@ struct HfCandidateCreator3ProngExpressions {
   Produces<aod::HfCand3ProngMcRec> rowMcMatchRec;
   Produces<aod::HfCand3ProngMcGen> rowMcMatchGen;
 
+  // Configuration
+  o2::framework::Configurable<bool> rejectBackground{"rejectBackground", true, "Reject particles from background events"};
+
   bool createDplus{false};
   bool createDs{false};
   bool createLc{false};
@@ -461,6 +464,11 @@ struct HfCandidateCreator3ProngExpressions {
   HfEventSelectionMc hfEvSelMc; // mc event selection and monitoring
   using BCsInfo = soa::Join<aod::BCs, aod::Timestamps, aod::BcSels>;
   HistogramRegistry registry{"registry"};
+
+  using McCollisionsFT0Cs = soa::Join<aod::Collisions, aod::EvSels, aod::McCollisionLabels, aod::CentFT0Cs>;
+  using McCollisionsFT0Ms = soa::Join<aod::Collisions, aod::EvSels, aod::McCollisionLabels, aod::CentFT0Ms>;
+  PresliceUnsorted<McCollisionsFT0Cs> colPerMcCollisionFT0C = aod::mccollisionlabel::mcCollisionId;
+  PresliceUnsorted<McCollisionsFT0Ms> colPerMcCollisionFT0M = aod::mccollisionlabel::mcCollisionId;
 
   void init(InitContext& initContext)
   {
@@ -495,10 +503,12 @@ struct HfCandidateCreator3ProngExpressions {
   }
 
   /// Performs MC matching.
-  void processMc(aod::TracksWMc const& tracks,
-                 aod::McParticles const& mcParticles,
-                 aod::McCollisions const&,
-                 BCsInfo const&)
+  template <o2::hf_centrality::CentralityEstimator centEstimator, typename CCs>
+  void runCreator3ProngMc(aod::TracksWMc const& tracks,
+                          aod::McParticles const& mcParticles,
+                          CCs const& collInfos,
+                          aod::McCollisions const&,
+                          BCsInfo const&)
   {
     rowCandidateProng3->bindExternalIndices(&tracks);
 
@@ -526,6 +536,19 @@ struct HfCandidateCreator3ProngExpressions {
       arrDaughIndex.clear();
       std::vector<int> idxBhadMothers{};
       auto arrayDaughters = std::array{candidate.prong0_as<aod::TracksWMc>(), candidate.prong1_as<aod::TracksWMc>(), candidate.prong2_as<aod::TracksWMc>()};
+
+      // Check whether the particle is from background events. If so, reject it.
+      if (rejectBackground) {
+        for (const auto& daughter : arrayDaughters) {
+          if (daughter.has_mcParticle()) {
+            auto mcParticle = daughter.mcParticle();
+            if (mcParticle.fromBackgroundEvent()) {
+              rowMcMatchRec(flag, origin, swapping, channel, -1.f, 0);
+              continue;
+            }
+          }
+        }
+      }
 
       // D± → π± K∓ π±
       if (createDplus) {
@@ -615,15 +638,27 @@ struct HfCandidateCreator3ProngExpressions {
 
     // Match generated particles.
     for (const auto& particle : mcParticles) {
+      // Reject particles from background events
+      if (particle.fromBackgroundEvent() && rejectBackground) {
+        continue;
+      }
       flag = 0;
       origin = 0;
       channel = 0;
       arrDaughIndex.clear();
       std::vector<int> idxBhadMothers{};
 
+      // Slice the collisions table to get the collision info for the current MC collision
       auto mcCollision = particle.mcCollision();
-
-      const auto rejectionMask = hfEvSelMc.getHfMcCollisionRejectionMask<BCsInfo>(mcCollision);
+      float centrality{-1.f};
+      uint16_t rejectionMask{0};
+      if constexpr (centEstimator == CentralityEstimator::FT0C) {
+        const auto collSlice = collInfos.sliceBy(colPerMcCollisionFT0C, mcCollision.globalIndex());
+        rejectionMask = hfEvSelMc.getHfMcCollisionRejectionMask<BCsInfo, centEstimator>(mcCollision, collSlice, centrality);
+      } else if constexpr (centEstimator == CentralityEstimator::FT0M) {
+        const auto collSlice = collInfos.sliceBy(colPerMcCollisionFT0M, mcCollision.globalIndex());
+        rejectionMask = hfEvSelMc.getHfMcCollisionRejectionMask<BCsInfo, centEstimator>(mcCollision, collSlice, centrality);
+      }
       hfEvSelMc.fillHistograms(rejectionMask);
       if (rejectionMask != 0) {
         /// at least one event selection not satisfied --> reject the gen particle
@@ -709,7 +744,25 @@ struct HfCandidateCreator3ProngExpressions {
     }
   }
 
-  PROCESS_SWITCH(HfCandidateCreator3ProngExpressions, processMc, "Process MC", false);
+  void processMcCentFT0C(aod::TracksWMc const& tracks,
+                         aod::McParticles const& mcParticles,
+                         McCollisionsFT0Cs const& collInfos,
+                         aod::McCollisions const& mcCollisions,
+                         BCsInfo const& BCsInfo)
+  {
+    runCreator3ProngMc<CentralityEstimator::FT0C>(tracks, mcParticles, collInfos, mcCollisions, BCsInfo);
+  }
+  PROCESS_SWITCH(HfCandidateCreator3ProngExpressions, processMcCentFT0C, "Process MC - FT0c centrality", false);
+
+  void processMcCentFT0M(aod::TracksWMc const& tracks,
+                         aod::McParticles const& mcParticles,
+                         McCollisionsFT0Ms const& collInfos,
+                         aod::McCollisions const& mcCollisions,
+                         BCsInfo const& BCsInfo)
+  {
+    runCreator3ProngMc<CentralityEstimator::FT0M>(tracks, mcParticles, collInfos, mcCollisions, BCsInfo);
+  }
+  PROCESS_SWITCH(HfCandidateCreator3ProngExpressions, processMcCentFT0M, "Process MC - FT0m centrality", false);
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
