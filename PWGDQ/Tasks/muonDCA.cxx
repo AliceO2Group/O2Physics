2f4ea475a9879b031218ef76058e06e09875909a
Bhagyarathi Sahoo <125358702+bhsaho@users.noreply.github.com>
2024-07-26 14:08:01 +0530

    Update muonDCA.cxx

--- PWGDQ/Tasks/muonDCA.cxx
+++ PWGDQ/Tasks/muonDCA.cxx
@@ -131,7 +131,7 @@ struct muonExtrap {
 
       // Calculate DCA quantities (preferable to do it with VarManager)
       double dcax = muonTrackAtDCA.getX() - collision.posX();
-      double dcay = muonTrackAtDCA.getY() - collision.posY() ;
+      double dcay = muonTrackAtDCA.getY() - collision.posY();
       double dca = std::sqrt(dcax * dcax + dcay * dcay);
       double pdca = muonTrackAtVertex.getP() * dca;
       
