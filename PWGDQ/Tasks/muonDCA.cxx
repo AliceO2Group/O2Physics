f429862c21d1b606fb621968f397fafd2e999e90
Bhagyarathi Sahoo <Bhagyarathi.Sahoo@cern.ch>
2024-07-27 08:34:00 +0530

    Update muonDCA.cxx

--- PWGDQ/Tasks/muonDCA.cxx
+++ PWGDQ/Tasks/muonDCA.cxx
@@ -1,20 +1,16 @@
-// Copyright CERN and copyright holders of ALICE O2. 
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
-// 
-// This software is distributed under the terms of the GNU General Public 
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
-// See http://alice-o2.web.cern.ch/license for full licensing information.
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 //
-/// \file muonDCA.cxx
-/// \brief Task to compute and evaluate DCA quantities
-/// \author Nicolas Bizé <nicolas.bize@cern.ch>, SUBATECH
-
+// Contact: iarsene@cern.ch, i.c.arsene@fys.uio.no
+//
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/ASoAHelpers.h"
