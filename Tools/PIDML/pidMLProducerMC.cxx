f6383cf96c36b9de97f8d37d38d94d36853e093a
mytkom <marek.mytkowski.mm@gmail.com>
2024-08-02 08:25:26 +0200

    fix formatting

--- Tools/PIDML/pidMLProducerMC.cxx
+++ Tools/PIDML/pidMLProducerMC.cxx
@@ -45,106 +45,107 @@ struct PidMlProducerMc {
 
   HistogramRegistry registry{
     "registry",
-    {{"minus/hTPCSigvsP", "TPC signal vs #it{p};#it{p} (GeV/#it{c});TPC signal", {HistType::kTH2F, {{500, 0., 10.}, {1000, 0., 600.}}}},
-     {"minus/hTOFBetavsP", "TOF beta vs #it{p};#it{p} (GeV/#it{c});TOF #it{beta}", {HistType::kTH2F, {{500, 0., 10.}, {6000, -3., 3.}}}},
-     {"minus/hTOFSigvsP", "TOF signal vs #it{p};#it{p} (GeV/#it{c});TOF signal", {HistType::kTH2F, {{500, 0., 10.}, {10000, -5000., 80000.}}}},
-     {"minus/filtered/hTOFSigvsP", "TOF signal (filtered) vs #it{p};#it{p} (GeV/#it{c});TOF signal", {HistType::kTH2F, {{500, 0., 10.}, {10000, -5000., 80000.}}}},
-     {"minus/hTRDPattvsP", "TRD pattern vs #it{p};#it{p} (GeV/#it{c});TRD pattern", {HistType::kTH2F, {{500, 0., 10.}, {110, -10., 100.}}}},
-     {"minus/hTRDSigvsP", "TRD signal vs #it{p};#it{p} (GeV/#it{c});TRD signal", {HistType::kTH2F, {{500, 0., 10.}, {2500, -2., 100.}}}},
-     {"minus/hP", "#it{p};#it{p} (GeV/#it{c})", {HistType::kTH1F, {{500, 0., 6.}}}},
-     {"minus/hPt", "#it{p}_{t};#it{p}_{t} (GeV/#it{c})", {HistType::kTH1F, {{500, 0., 6.}}}},
-     {"minus/hPx", "#it{p}_{x};#it{p}_{x} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
-     {"minus/hPy", "#it{p}_{y};#it{p}_{y} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
-     {"minus/hPz", "#it{p}_{z};#it{p}_{z} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
-     {"minus/hX", "#it{x};#it{x}", {HistType::kTH1F, {{1000, -2., 2.}}}},
-     {"minus/hY", "#it{y};#it{y}", {HistType::kTH1F, {{1000, -2., 2.}}}},
-     {"minus/hZ", "#it{z};#it{z}", {HistType::kTH1F, {{1000, -10., 10.}}}},
-     {"minus/hAlpha", "#{alpha};#{alpha}", {HistType::kTH1F, {{1000, -5., 5.}}}},
-     {"minus/hTrackType", "Track Type;Track Type", {HistType::kTH1F, {{300, 0., 300.}}}},
-     {"minus/hTPCNClsShared", "hTPCNClsShared;hTPCNClsShared", {HistType::kTH1F, {{100, 0., 100.}}}},
-     {"minus/hDcaXY", "#it{DcaXY};#it{DcaXY}", {HistType::kTH1F, {{1000, -1., 1.}}}},
-     {"minus/hDcaZ", "#it{DcaZ};#it{DcaZ}", {HistType::kTH1F, {{1000, -1., 1.}}}},
-     {"minus/hPdgCode", "#it{PdgCode};#it{PdgCode}", {HistType::kTH1F, {{2500, 0., 2500.}}}},
-     {"minus/hIsPrimary", "#it{IsPrimary};#it{IsPrimary}", {HistType::kTH1F, {{4, -0.5, 1.5}}}},
-     {"plus/hTPCSigvsP", "TPC signal vs #it{p};#it{p} (GeV/#it{c});TPC signal", {HistType::kTH2F, {{500, 0., 10.}, {1000, 0., 600.}}}},
-     {"plus/hTOFBetavsP", "TOF beta vs #it{p};#it{p} (GeV/#it{c});TOF #it{beta}", {HistType::kTH2F, {{500, 0., 10.}, {6000, -3., 3.}}}},
-     {"plus/hTOFSigvsP", "TOF signal vs #it{p};#it{p} (GeV/#it{c});TOF signal", {HistType::kTH2F, {{500, 0., 10.}, {10000, -5000., 80000.}}}},
-     {"plus/filtered/hTOFSigvsP", "TOF signal (filtered) vs #it{p};#it{p} (GeV/#it{c});TOF signal", {HistType::kTH2F, {{500, 0., 10.}, {10000, -5000., 80000.}}}},
-     {"plus/hTRDPattvsP", "TRD pattern vs #it{p};#it{p} (GeV/#it{c});TRD pattern", {HistType::kTH2F, {{500, 0., 10.}, {110, -10., 100.}}}},
-     {"plus/hTRDSigvsP", "TRD signal vs #it{p};#it{p} (GeV/#it{c});TRD signal", {HistType::kTH2F, {{500, 0., 10.}, {2500, -2., 100.}}}},
-     {"plus/hP", "#it{p};#it{p} (GeV/#it{c})", {HistType::kTH1F, {{500, 0., 6.}}}},
-     {"plus/hPt", "#it{p}_{t};#it{p}_{t} (GeV/#it{c})", {HistType::kTH1F, {{500, 0., 6.}}}},
-     {"plus/hPx", "#it{p}_{x};#it{p}_{x} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
-     {"plus/hPy", "#it{p}_{y};#it{p}_{y} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
-     {"plus/hPz", "#it{p}_{z};#it{p}_{z} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
-     {"plus/hX", "#it{x};#it{x}", {HistType::kTH1F, {{1000, -2., 2.}}}},
-     {"plus/hY", "#it{y};#it{y}", {HistType::kTH1F, {{1000, -2., 2.}}}},
-     {"plus/hZ", "#it{z};#it{z}", {HistType::kTH1F, {{1000, -10., 10.}}}},
-     {"plus/hAlpha", "#{alpha};#{alpha}", {HistType::kTH1F, {{1000, -5., 5.}}}},
-     {"plus/hTrackType", "Track Type;Track Type", {HistType::kTH1F, {{300, 0., 300.}}}},
-     {"plus/hTPCNClsShared", "hTPCNClsShared;hTPCNClsShared", {HistType::kTH1F, {{100, 0., 100.}}}},
-     {"plus/hDcaXY", "#it{DcaXY};#it{DcaXY}", {HistType::kTH1F, {{1000, -1., 1.}}}},
-     {"plus/hDcaZ", "#it{DcaZ};#it{DcaZ}", {HistType::kTH1F, {{1000, -1., 1.}}}},
-     {"plus/hPdgCode", "#it{PdgCode};#it{PdgCode}", {HistType::kTH1F, {{2500, 0., 2500.}}}},
-     {"plus/hIsPrimary", "#it{IsPrimary};#it{IsPrimary}", {HistType::kTH1F, {{4, -0.5, 1.5}}}},
+    {
+      {"minus/hTPCSigvsP", "TPC signal vs #it{p};#it{p} (GeV/#it{c});TPC signal", {HistType::kTH2F, {{500, 0., 10.}, {1000, 0., 600.}}}},
+      {"minus/hTOFBetavsP", "TOF beta vs #it{p};#it{p} (GeV/#it{c});TOF #it{beta}", {HistType::kTH2F, {{500, 0., 10.}, {6000, -3., 3.}}}},
+      {"minus/hTOFSigvsP", "TOF signal vs #it{p};#it{p} (GeV/#it{c});TOF signal", {HistType::kTH2F, {{500, 0., 10.}, {10000, -5000., 80000.}}}},
+      {"minus/filtered/hTOFSigvsP", "TOF signal (filtered) vs #it{p};#it{p} (GeV/#it{c});TOF signal", {HistType::kTH2F, {{500, 0., 10.}, {10000, -5000., 80000.}}}},
+      {"minus/hTRDPattvsP", "TRD pattern vs #it{p};#it{p} (GeV/#it{c});TRD pattern", {HistType::kTH2F, {{500, 0., 10.}, {110, -10., 100.}}}},
+      {"minus/hTRDSigvsP", "TRD signal vs #it{p};#it{p} (GeV/#it{c});TRD signal", {HistType::kTH2F, {{500, 0., 10.}, {2500, -2., 100.}}}},
+      {"minus/hP", "#it{p};#it{p} (GeV/#it{c})", {HistType::kTH1F, {{500, 0., 6.}}}},
+      {"minus/hPt", "#it{p}_{t};#it{p}_{t} (GeV/#it{c})", {HistType::kTH1F, {{500, 0., 6.}}}},
+      {"minus/hPx", "#it{p}_{x};#it{p}_{x} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
+      {"minus/hPy", "#it{p}_{y};#it{p}_{y} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
+      {"minus/hPz", "#it{p}_{z};#it{p}_{z} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
+      {"minus/hX", "#it{x};#it{x}", {HistType::kTH1F, {{1000, -2., 2.}}}},
+      {"minus/hY", "#it{y};#it{y}", {HistType::kTH1F, {{1000, -2., 2.}}}},
+      {"minus/hZ", "#it{z};#it{z}", {HistType::kTH1F, {{1000, -10., 10.}}}},
+      {"minus/hAlpha", "#{alpha};#{alpha}", {HistType::kTH1F, {{1000, -5., 5.}}}},
+      {"minus/hTrackType", "Track Type;Track Type", {HistType::kTH1F, {{300, 0., 300.}}}},
+      {"minus/hTPCNClsShared", "hTPCNClsShared;hTPCNClsShared", {HistType::kTH1F, {{100, 0., 100.}}}},
+      {"minus/hDcaXY", "#it{DcaXY};#it{DcaXY}", {HistType::kTH1F, {{1000, -1., 1.}}}},
+      {"minus/hDcaZ", "#it{DcaZ};#it{DcaZ}", {HistType::kTH1F, {{1000, -1., 1.}}}},
+      {"minus/hPdgCode", "#it{PdgCode};#it{PdgCode}", {HistType::kTH1F, {{2500, 0., 2500.}}}},
+      {"minus/hIsPrimary", "#it{IsPrimary};#it{IsPrimary}", {HistType::kTH1F, {{4, -0.5, 1.5}}}},
+      {"plus/hTPCSigvsP", "TPC signal vs #it{p};#it{p} (GeV/#it{c});TPC signal", {HistType::kTH2F, {{500, 0., 10.}, {1000, 0., 600.}}}},
+      {"plus/hTOFBetavsP", "TOF beta vs #it{p};#it{p} (GeV/#it{c});TOF #it{beta}", {HistType::kTH2F, {{500, 0., 10.}, {6000, -3., 3.}}}},
+      {"plus/hTOFSigvsP", "TOF signal vs #it{p};#it{p} (GeV/#it{c});TOF signal", {HistType::kTH2F, {{500, 0., 10.}, {10000, -5000., 80000.}}}},
+      {"plus/filtered/hTOFSigvsP", "TOF signal (filtered) vs #it{p};#it{p} (GeV/#it{c});TOF signal", {HistType::kTH2F, {{500, 0., 10.}, {10000, -5000., 80000.}}}},
+      {"plus/hTRDPattvsP", "TRD pattern vs #it{p};#it{p} (GeV/#it{c});TRD pattern", {HistType::kTH2F, {{500, 0., 10.}, {110, -10., 100.}}}},
+      {"plus/hTRDSigvsP", "TRD signal vs #it{p};#it{p} (GeV/#it{c});TRD signal", {HistType::kTH2F, {{500, 0., 10.}, {2500, -2., 100.}}}},
+      {"plus/hP", "#it{p};#it{p} (GeV/#it{c})", {HistType::kTH1F, {{500, 0., 6.}}}},
+      {"plus/hPt", "#it{p}_{t};#it{p}_{t} (GeV/#it{c})", {HistType::kTH1F, {{500, 0., 6.}}}},
+      {"plus/hPx", "#it{p}_{x};#it{p}_{x} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
+      {"plus/hPy", "#it{p}_{y};#it{p}_{y} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
+      {"plus/hPz", "#it{p}_{z};#it{p}_{z} (GeV/#it{c})", {HistType::kTH1F, {{1000, -6., 6.}}}},
+      {"plus/hX", "#it{x};#it{x}", {HistType::kTH1F, {{1000, -2., 2.}}}},
+      {"plus/hY", "#it{y};#it{y}", {HistType::kTH1F, {{1000, -2., 2.}}}},
+      {"plus/hZ", "#it{z};#it{z}", {HistType::kTH1F, {{1000, -10., 10.}}}},
+      {"plus/hAlpha", "#{alpha};#{alpha}", {HistType::kTH1F, {{1000, -5., 5.}}}},
+      {"plus/hTrackType", "Track Type;Track Type", {HistType::kTH1F, {{300, 0., 300.}}}},
+      {"plus/hTPCNClsShared", "hTPCNClsShared;hTPCNClsShared", {HistType::kTH1F, {{100, 0., 100.}}}},
+      {"plus/hDcaXY", "#it{DcaXY};#it{DcaXY}", {HistType::kTH1F, {{1000, -1., 1.}}}},
+      {"plus/hDcaZ", "#it{DcaZ};#it{DcaZ}", {HistType::kTH1F, {{1000, -1., 1.}}}},
+      {"plus/hPdgCode", "#it{PdgCode};#it{PdgCode}", {HistType::kTH1F, {{2500, 0., 2500.}}}},
+      {"plus/hIsPrimary", "#it{IsPrimary};#it{IsPrimary}", {HistType::kTH1F, {{4, -0.5, 1.5}}}},
     }};
 
-  
+
   template <typename T>
   void fillHistos(const T& track, uint32_t pdgCode, uint8_t isPrimary) {
-      if(track.sign() < 0) {
-        registry.fill(HIST("minus/hTPCSigvsP"), track.p(), track.tpcSignal());
-        registry.fill(HIST("minus/hTOFBetavsP"), track.p(), track.beta());
-        registry.fill(HIST("minus/hTOFSigvsP"), track.p(), track.tofSignal());
-        if(TMath::Abs(track.beta() - kMissingBeta) >= kEps) {
-          registry.fill(HIST("minus/filtered/hTOFSigvsP"), track.p(), track.tofSignal());
-        } else {
-          registry.fill(HIST("minus/filtered/hTOFSigvsP"), track.p(), kMissingTOFSignal);
-        }
-        registry.fill(HIST("minus/hTRDPattvsP"), track.p(), track.trdPattern());
-        registry.fill(HIST("minus/hTRDSigvsP"), track.p(), track.trdSignal());
-        registry.fill(HIST("minus/hP"), track.p());
-        registry.fill(HIST("minus/hPt"), track.pt());
-        registry.fill(HIST("minus/hPx"), track.px());
-        registry.fill(HIST("minus/hPy"), track.py());
-        registry.fill(HIST("minus/hPz"), track.pz());
-        registry.fill(HIST("minus/hX"), track.x());
-        registry.fill(HIST("minus/hY"), track.y());
-        registry.fill(HIST("minus/hZ"), track.z());
-        registry.fill(HIST("minus/hAlpha"), track.alpha());
-        registry.fill(HIST("minus/hTrackType"), track.trackType());
-        registry.fill(HIST("minus/hTPCNClsShared"), track.tpcNClsShared());
-        registry.fill(HIST("minus/hDcaXY"), track.dcaXY());
-        registry.fill(HIST("minus/hDcaZ"), track.dcaZ());
-        registry.fill(HIST("minus/hPdgCode"), pdgCode);
-        registry.fill(HIST("minus/hIsPrimary"), isPrimary);
-      } else {
-        registry.fill(HIST("plus/hTPCSigvsP"), track.p(), track.tpcSignal());
-        registry.fill(HIST("plus/hTOFBetavsP"), track.p(), track.beta());
-        registry.fill(HIST("plus/hTOFSigvsP"), track.p(), track.tofSignal());
-        if(TMath::Abs(track.beta() - kMissingBeta) >= kEps) {
-          registry.fill(HIST("plus/filtered/hTOFSigvsP"), track.p(), track.tofSignal());
-        } else {
-          registry.fill(HIST("plus/filtered/hTOFSigvsP"), track.p(), kMissingTOFSignal);
-        }
-        registry.fill(HIST("plus/hTRDPattvsP"), track.p(), track.trdPattern());
-        registry.fill(HIST("plus/hTRDSigvsP"), track.p(), track.trdSignal());
-        registry.fill(HIST("plus/hP"), track.p());
-        registry.fill(HIST("plus/hPt"), track.pt());
-        registry.fill(HIST("plus/hPx"), track.px());
-        registry.fill(HIST("plus/hPy"), track.py());
-        registry.fill(HIST("plus/hPz"), track.pz());
-        registry.fill(HIST("plus/hX"), track.x());
-        registry.fill(HIST("plus/hY"), track.y());
-        registry.fill(HIST("plus/hZ"), track.z());
-        registry.fill(HIST("plus/hAlpha"), track.alpha());
-        registry.fill(HIST("plus/hTrackType"), track.trackType());
-        registry.fill(HIST("plus/hTPCNClsShared"), track.tpcNClsShared());
-        registry.fill(HIST("plus/hDcaXY"), track.dcaXY());
-        registry.fill(HIST("plus/hDcaZ"), track.dcaZ());
-        registry.fill(HIST("plus/hPdgCode"), pdgCode);
-        registry.fill(HIST("plus/hIsPrimary"), isPrimary);
-      }
+     if (track.sign() < 0) {
+       registry.fill(HIST("minus/hTPCSigvsP"), track.p(), track.tpcSignal());
+       registry.fill(HIST("minus/hTOFBetavsP"), track.p(), track.beta());
+       registry.fill(HIST("minus/hTOFSigvsP"), track.p(), track.tofSignal());
+       if (TMath::Abs(track.beta() - kMissingBeta) >= kEps) {
+         registry.fill(HIST("minus/filtered/hTOFSigvsP"), track.p(), track.tofSignal());
+       } else {
+         registry.fill(HIST("minus/filtered/hTOFSigvsP"), track.p(), kMissingTOFSignal);
+       }
+       registry.fill(HIST("minus/hTRDPattvsP"), track.p(), track.trdPattern());
+       registry.fill(HIST("minus/hTRDSigvsP"), track.p(), track.trdSignal());
+       registry.fill(HIST("minus/hP"), track.p());
+       registry.fill(HIST("minus/hPt"), track.pt());
+       registry.fill(HIST("minus/hPx"), track.px());
+       registry.fill(HIST("minus/hPy"), track.py());
+       registry.fill(HIST("minus/hPz"), track.pz());
+       registry.fill(HIST("minus/hX"), track.x());
+       registry.fill(HIST("minus/hY"), track.y());
+       registry.fill(HIST("minus/hZ"), track.z());
+       registry.fill(HIST("minus/hAlpha"), track.alpha());
+       registry.fill(HIST("minus/hTrackType"), track.trackType());
+       registry.fill(HIST("minus/hTPCNClsShared"), track.tpcNClsShared());
+       registry.fill(HIST("minus/hDcaXY"), track.dcaXY());
+       registry.fill(HIST("minus/hDcaZ"), track.dcaZ());
+       registry.fill(HIST("minus/hPdgCode"), pdgCode);
+       registry.fill(HIST("minus/hIsPrimary"), isPrimary);
+     } else {
+       registry.fill(HIST("plus/hTPCSigvsP"), track.p(), track.tpcSignal());
+       registry.fill(HIST("plus/hTOFBetavsP"), track.p(), track.beta());
+       registry.fill(HIST("plus/hTOFSigvsP"), track.p(), track.tofSignal());
+       if (TMath::Abs(track.beta() - kMissingBeta) >= kEps) {
+         registry.fill(HIST("plus/filtered/hTOFSigvsP"), track.p(), track.tofSignal());
+       } else {
+         registry.fill(HIST("plus/filtered/hTOFSigvsP"), track.p(), kMissingTOFSignal);
+       }
+       registry.fill(HIST("plus/hTRDPattvsP"), track.p(), track.trdPattern());
+       registry.fill(HIST("plus/hTRDSigvsP"), track.p(), track.trdSignal());
+       registry.fill(HIST("plus/hP"), track.p());
+       registry.fill(HIST("plus/hPt"), track.pt());
+       registry.fill(HIST("plus/hPx"), track.px());
+       registry.fill(HIST("plus/hPy"), track.py());
+       registry.fill(HIST("plus/hPz"), track.pz());
+       registry.fill(HIST("plus/hX"), track.x());
+       registry.fill(HIST("plus/hY"), track.y());
+       registry.fill(HIST("plus/hZ"), track.z());
+       registry.fill(HIST("plus/hAlpha"), track.alpha());
+       registry.fill(HIST("plus/hTrackType"), track.trackType());
+       registry.fill(HIST("plus/hTPCNClsShared"), track.tpcNClsShared());
+       registry.fill(HIST("plus/hDcaXY"), track.dcaXY());
+       registry.fill(HIST("plus/hDcaZ"), track.dcaZ());
+       registry.fill(HIST("plus/hPdgCode"), pdgCode);
+       registry.fill(HIST("plus/hIsPrimary"), isPrimary);
+     }
   }
 
   void processML(MyCollisionML const& /*collision*/, BigTracksML const& tracks, aod::McParticles const& /*mctracks*/)
