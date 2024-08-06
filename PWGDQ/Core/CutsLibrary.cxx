276718f25a452122e6d012d8bb7c6a06307d47c0
Jerome Jung <jung@ikf.uni-frankfurt.de>
2024-08-06 17:00:16 +0200

    PWGDQ:LMee: added new occupancy cut

--- PWGDQ/Core/CutsLibrary.cxx
+++ PWGDQ/Core/CutsLibrary.cxx
@@ -3204,6 +3204,18 @@ AnalysisCut* o2::aod::dqcuts::GetAnalysisCut(const char* cutName)
 
     return cut;
   }
+  if (!nameStr.compare("eventStandardSel8PbPbQualityFirmTrackOccupancy")) {
+    cut->AddCut(VarManager::kVtxZ, -10.0, 10.0);
+    cut->AddCut(VarManager::kIsSel8, 0.5, 1.5);
+    cut->AddCut(VarManager::kIsNoTFBorder, 0.5, 1.5);
+    cut->AddCut(VarManager::kIsNoITSROFBorder, 0.5, 1.5);
+    cut->AddCut(VarManager::kIsNoSameBunch, 0.5, 1.5);
+    cut->AddCut(VarManager::kIsGoodZvtxFT0vsPV, 0.5, 1.5);
+    cut->AddCut(VarManager::kCentFT0C, 0.0, 90.0);
+    cut->AddCut(VarManager::kTrackOccupancyInTimeRange, 0., 2000);
+
+    return cut;
+  }
 
   if (!nameStr.compare("eventStandardSel8PbPbQualityLooseTrackOccupancy")) {
     cut->AddCut(VarManager::kVtxZ, -10.0, 10.0);
