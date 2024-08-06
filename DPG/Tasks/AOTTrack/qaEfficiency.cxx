bcaf13a111d79557499fb7abec26f74ac0190594
upasanasharma31 <84681984+upasanasharma31@users.noreply.github.com>
2024-08-06 14:01:20 +0530

    Fix the filled efficiency in  2d plot Pt vs radius

--- DPG/Tasks/AOTTrack/qaEfficiency.cxx
+++ DPG/Tasks/AOTTrack/qaEfficiency.cxx
@@ -1016,9 +1016,9 @@ struct QaEfficiency {
         }
       }
       if (doPtRadius) {
-        hPtRadiusItsTpc[histogramIndex]->Fill(mcParticle.pt(), radius);
+        hPtRadiusItsTpc[histogramIndex]->Fill(mcParticle.pt(), mcParticle.radius());
         if (passedTOF) {
-          hPtRadiusItsTpcTof[histogramIndex]->Fill(mcParticle.pt(), radius);
+          hPtRadiusItsTpcTof[histogramIndex]->Fill(mcParticle.pt(), mcParticle.radius());
         }
       }
     }
@@ -1180,7 +1180,7 @@ struct QaEfficiency {
       hPtEtaGenerated[histogramIndex]->Fill(mcParticle.pt(), mcParticle.eta());
     }
     if (doPtRadius) {
-      hPtRadiusGenerated[histogramIndex]->Fill(mcParticle.pt(), radius);
+      hPtRadiusGenerated[histogramIndex]->Fill(mcParticle.pt(), mcParticle.radius());
     }
   }
 
