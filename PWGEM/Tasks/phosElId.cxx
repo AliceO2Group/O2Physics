80ad54d3d0c1bb5e4ea106f3a368e42194675660
yhambard <127940767+yhambard@users.noreply.github.com>
2024-08-06 12:44:35 +0300

    Fix momentum binning

--- PWGEM/Tasks/phosElId.cxx
+++ PWGEM/Tasks/phosElId.cxx
@@ -118,9 +118,9 @@ struct phosElId {
   {
     LOG(info) << "Initializing PHOS electron identification analysis task ...";
 
-    std::vector<double> momentum_binning = {0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0,
+    std::vector<double> momentum_binning = {0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0,
                                             1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0,
-                                            4.5, 5.0, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 10.};
+                                            4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 10.};
     std::vector<float> parameters_sigma_dz = pSigma_dz;
     std::vector<float> parameters_sigma_dx = pSigma_dx;
     std::vector<float> vPhosShiftX = pPhosShiftX;
