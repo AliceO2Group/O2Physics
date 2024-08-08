ac1ba4584e5db014c2a2323ef76e31561711df27
Nicolò Jacazio <njacazio@users.noreply.github.com>
2024-08-08 14:36:33 +0200

    Update eventSelection.cxx

--- Common/TableProducer/eventSelection.cxx
+++ Common/TableProducer/eventSelection.cxx
@@ -473,8 +473,7 @@ struct EventSelectionTask {
   void init(InitContext&)
   {
     if (metadataInfo.isFullyDefined()) { // Check if the metadata is initialized (only if not forced from the workflow configuration)
-      if (!doprocessRun2 && !doprocessRun3))
-        {
+      if (!doprocessRun2 && !doprocessRun3)        {
           LOG(info) << "Autosetting the processing mode (Run2 or Run3) based on metadata";
           if (metadataInfo.isRun3()) {
             doprocessRun3.value = true;
