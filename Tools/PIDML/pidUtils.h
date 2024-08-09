ed8722d19ec36ec5a921c5242f776d23de1b4a9f
mytkom <marek.mytkowski.mm@gmail.com>
2024-08-09 09:38:46 +0200

    remove almostEqual - it is not used anymore

--- Tools/PIDML/pidUtils.h
+++ Tools/PIDML/pidUtils.h
@@ -23,12 +23,6 @@ namespace pidml::pidutils
 {
 constexpr double kTOFMissingSignal = -999.0f;
 constexpr double kGlobalEtaCut = 0.8f;
-constexpr double kEpsilon = 1e-6f;
-
-bool almostEqual(double a, double b, double eps = kEpsilon)
-{
-  return std::abs(a - b) <= eps;
-}
 
 template <typename T>
 bool trdMissing(const T& track)
