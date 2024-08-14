291f18619f78c5ef7868149a838b4f13381ed958
yuanzhe <yuanzhe.wang@cern.ch>
2024-08-14 18:45:33 +0200

    Restore changes

--- PWGLF/DataModel/Vtx3BodyTables.h
+++ PWGLF/DataModel/Vtx3BodyTables.h
@@ -17,29 +17,6 @@
 #include "Common/Core/RecoDecay.h"
 #include "CommonConstants/PhysicsConstants.h"
 
-// Table for the event time. One entry per collision
-namespace o2::aod
-{
-namespace evtime
-{
-
-DECLARE_SOA_COLUMN(EvTime, evTime, float);             //! Event time. Can be obtained via a combination of detectors e.g. TOF, FT0A, FT0C
-DECLARE_SOA_COLUMN(EvTimeErr, evTimeErr, float);       //! Error of event time. Can be obtained via a combination of detectors e.g. TOF, FT0A, FT0C
-DECLARE_SOA_COLUMN(EvTimeTOF, evTimeTOF, float);       //! Event time computed with the TOF detector
-DECLARE_SOA_COLUMN(EvTimeTOFErr, evTimeTOFErr, float); //! Error of the event time computed with the TOF detector
-DECLARE_SOA_COLUMN(EvTimeFT0, evTimeFT0, float);       //! Event time computed with the FT0 detector
-DECLARE_SOA_COLUMN(EvTimeFT0Err, evTimeFT0Err, float); //! Error of the event time computed with the FT0 detector
-} // namespace evtime
-
-DECLARE_SOA_TABLE(EvTimeTOFFT0, "AOD", "EvTimeTOFFT0", //! Table of the TOF event time. One entry per collision.
-                  evtime::EvTime,
-                  evtime::EvTimeErr,
-                  evtime::EvTimeTOF,
-                  evtime::EvTimeTOFErr,
-                  evtime::EvTimeFT0,
-                  evtime::EvTimeFT0Err);
-} // namespace o2::aod
-
 namespace o2::aod
 {
 namespace vtx3body
