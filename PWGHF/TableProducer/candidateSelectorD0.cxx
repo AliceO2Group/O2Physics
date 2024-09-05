81f55bb816d59ac0e0e18d8202b425a016d537f3
Chuntai <48704924+Libeiyu@users.noreply.github.com>
2024-09-05 18:32:36 +0800

    Update candidateSelectorD0.cxx

--- PWGHF/TableProducer/candidateSelectorD0.cxx
+++ PWGHF/TableProducer/candidateSelectorD0.cxx
@@ -158,7 +158,9 @@ struct HfCandidateSelectorD0 {
       return false;
     }
     // candidate DCA
-    if (candidate.impactParameterXY() > cuts->get(pTBin, "DCA") return false;
+    if (candidate.impactParameterXY() > cuts->get(pTBin, "DCA")) {
+      return false;
+    }
 
     // candidate topological chi2 over ndf when using KFParticle, need to add this selection to the SelectorCuts.h
     // if constexpr (reconstructionType == aod::hf_cand::VertexerType::KfParticle) {
