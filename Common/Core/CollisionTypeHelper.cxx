a7c439c178d596cd5657126635022da089e17fbc
Nicolò Jacazio <njacazio@users.noreply.github.com>
2024-09-06 11:03:28 +0200

    [TOF] switch based on coll system (#7580)

---
+++ Common/Core/CollisionTypeHelper.cxx
@@ -0,0 +1,58 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   CollisionTypeHelper.h
+/// \author Nicolò Jacazio nicolo.jacazio@cern.ch
+/// \brief  Utility to handle the collision type from the GRP information
+///
+
+#include "Common/Core/CollisionTypeHelper.h"
+#include <fairlogger/Logger.h>
+#include <string>
+#include "DataFormatsParameters/GRPLHCIFData.h"
+
+std::string CollisionSystemType::getCollisionSystemName(collType collSys)
+{
+  switch (collSys) {
+    case kCollSyspp:
+      return "pp";
+    case kCollSysPbPb:
+      return "PbPb";
+    case kCollSysXeXe:
+      return "XeXe";
+    case kCollSyspPb:
+      return "pPb";
+    default:
+      LOG(fatal) << "Undefined collision system";
+      return "Undefined";
+  }
+}
+
+int CollisionSystemType::getCollisionTypeFromGrp(o2::parameters::GRPLHCIFData* grplhcif)
+{
+  const int ZBeamA = grplhcif->getBeamZ(o2::constants::lhc::BeamDirection::BeamA);
+  const int ZBeamC = grplhcif->getBeamZ(o2::constants::lhc::BeamDirection::BeamC);
+  switch (ZBeamA * ZBeamC) {
+    case 1: // pp 1*1
+      return kCollSyspp;
+    case 6724: // Pb-Pb 82*82
+      return kCollSysPbPb;
+    case 225: // Xe-Xe 54*54
+      return kCollSysXeXe;
+    case 82: // p-Pb 82*1
+      return kCollSyspPb;
+    default:
+      LOG(fatal) << "Undefined collision system";
+      return kCollSysUndef;
+  }
+  return kCollSysUndef;
+}
