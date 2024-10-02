53565fdaad794fca4ee2d40d553c88826f87ffb4
Rashi gupta <167059733+rashigupt@users.noreply.github.com>
2024-10-02 12:35:58 +0530

    use correct track id for correct build

--- PWGHF/HFL/DataModel/ElectronSelectionTable.h
+++ PWGHF/HFL/DataModel/ElectronSelectionTable.h
@@ -26,7 +26,7 @@ namespace o2::aod
 namespace hf_sel_electron
 {
 DECLARE_SOA_INDEX_COLUMN(Collision, collision);                //! collisioniD of the electron track
-DECLARE_SOA_INDEX_COLUMN(Track, track);                        //! trackid of of the electron track
+DECLARE_SOA_COLUMN(GTrackId, gtrackId, int);                   //! trackid of of the electron track
 DECLARE_SOA_COLUMN(EtaTrack, etaTrack, float);                 //! pseudorapidity of the electron track
 DECLARE_SOA_COLUMN(PhiTrack, phiTrack, float);                 //! azimuth of the electron track
 DECLARE_SOA_COLUMN(PtTrack, ptTrack, float);                   //! transverse momentum of the electron track
@@ -54,7 +54,7 @@ DECLARE_SOA_COLUMN(ISEmcal, isEmcal, bool);              //! electron informatio
 DECLARE_SOA_TABLE(HfSelEl, "AOD", "HFSELEL", //! Electron Informations
                   o2::soa::Index<>,
                   hf_sel_electron::CollisionId,
-                  hf_sel_electron::TrackId,
+                  hf_sel_electron::GTrackId,
                   hf_sel_electron::EtaTrack,
                   hf_sel_electron::PhiTrack,
                   hf_sel_electron::PtTrack,
