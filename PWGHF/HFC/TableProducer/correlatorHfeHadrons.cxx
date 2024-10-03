025b5359fdc67dcf9494bf33db72dc72ff3a1d7a
Rashi gupta <167059733+rashigupt@users.noreply.github.com>
2024-10-03 10:15:44 +0530

    correcttrack id build

--- PWGHF/HFC/TableProducer/correlatorHfeHadrons.cxx
+++ PWGHF/HFC/TableProducer/correlatorHfeHadrons.cxx
@@ -119,22 +119,24 @@ struct HfCorrelatorHfeHadrons {
     double ptElectron = -999;
     double phiElectron = -999;
     double etaElectron = -999;
+    int electronTrackId = 0;
 
     for (const auto& eTrack : electron) {
       ptElectron = eTrack.ptTrack();
       phiElectron = eTrack.phiTrack();
       etaElectron = eTrack.etaTrack();
+      electronTrackId = eTrack.gtrackId();
       double deltaPhi = -999;
       double deltaEta = -999;
       double ptHadron = -999;
       double etaHadron = -999;
       double phiHadron = -999;
+      int hadronTrackId = 0;
       if (!eTrack.isEmcal())
         continue;
 
       registry.fill(HIST("hptElectron"), ptElectron);
       for (const auto& hTrack : tracks) {
-        electronTrackId = eTrack.gtrackId();
         hadronTrackId = hTrack.globalIndex();
         if (hadronTrackId == electronTrackId)
           continue;
