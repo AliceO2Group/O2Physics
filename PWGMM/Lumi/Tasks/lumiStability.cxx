7503f6ebdfcfce9c1b4344de5d429f4632aad903
Josue-MiniPC <martinezgarciajosue54@gmail.com>
2024-10-07 10:31:41 -0600

    correcting the histo of time for fdd

--- PWGMM/Lumi/Tasks/lumiStability.cxx
+++ PWGMM/Lumi/Tasks/lumiStability.cxx
@@ -530,14 +530,6 @@ struct LumiStabilityTask {
               }
             }
           }
-          if (bcPatternB[localBC]) {
-            histos.fill(HIST("FDD/timeACbcBVertex"), fdd.timeA(), fdd.timeC());
-            histos.fill(HIST("FDD/hBcBVertex"), localBC);
-          }
-          if (bcPatternE[localBC]) {
-            histos.fill(HIST("FDD/timeACbcEVertex"), fdd.timeA(), fdd.timeC());
-            histos.fill(HIST("FDD/hBcEVertex"), localBC);
-          }
         }
 
 
@@ -557,8 +549,8 @@ struct LumiStabilityTask {
           if (bcPatternB[localBC]) {
             histos.fill(HIST("FDD/timeACbcB"), fdd.timeA(), fdd.timeC());
             histos.fill(HIST("FDD/hBcB"), localBC);
-            histos.fill(HIST("FDD/hTimeA"), fdd.timeA());
-            histos.fill(HIST("FDD/hTimeC"), fdd.timeC());
+            histos.fill(HIST("FDD/hTimeACoinc"), fdd.timeA());
+            histos.fill(HIST("FDD/hTimeCCoinc"), fdd.timeC());
 
           }
           if (bcPatternE[localBC]) {
