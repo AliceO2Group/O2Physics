e65bacf5fa4a1e53510b79a81a415761ae9cc4ad
spolitan <59452587+stefanopolitano@users.noreply.github.com>
2024-10-11 09:20:26 +0200

    Update PWGHF/TableProducer/candidateSelectorDplusToPiKPi.cxx
    Co-authored-by: Fabio Catalano <fabio.catalano372@gmail.com>

--- PWGHF/TableProducer/candidateSelectorDplusToPiKPi.cxx
+++ PWGHF/TableProducer/candidateSelectorDplusToPiKPi.cxx
@@ -61,7 +61,7 @@ struct HfCandidateSelectorDplusToPiKPi {
   // QA switch
   Configurable<bool> activateQA{"activateQA", false, "Flag to enable QA histogram"};
   // Correlated background from Ds and D+
-  Configurable<bool> storeDsDplusBkg{"storeDsBkg", false, "Flag to store correlated background from misidentified product of Ds and D+ decay"};
+  Configurable<bool> storeDsDplusBkg{"storeDsDplusBkg", false, "Flag to store correlated background from misidentified product of Ds and D+ decay"};
   // ML inference
   Configurable<bool> applyMl{"applyMl", false, "Flag to apply ML selections"};
   Configurable<std::vector<double>> binsPtMl{"binsPtMl", std::vector<double>{hf_cuts_ml::vecBinsPt}, "pT bin limits for ML application"};
