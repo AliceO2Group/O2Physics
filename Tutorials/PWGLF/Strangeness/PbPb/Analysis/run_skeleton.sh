92d7cb38f636c47ebadeaf49444d7c0e9e65b571
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2024-10-11 15:22:46 +0200

    U

--- Tutorials/PWGLF/Strangeness/PbPb/Analysis/run_skeleton.sh
+++ Tutorials/PWGLF/Strangeness/PbPb/Analysis/run_skeleton.sh
@@ -8,15 +8,15 @@ DIR_THIS=$PWD
 
 OPTION="-b --configuration json://configuration_skeleton.json"
 
-o2-analysistutorial-lf-strangeness-pbpb-skeleton ${OPTION} --aod-file @input_data.txt > "$LOGFILE" 2>&1
+o2-analysistutorial-lf-strangeness-pbpb-skeleton "${OPTION}" --aod-file @input_data.txt > "$LOGFILE" 2>&1
 
 # report status
 rc=$?
 if [ $rc -eq 0 ]; then
     echo "No problems!"
-    mkdir -p ${DIR_THIS}/results/${STEP}
-    mv AnalysisResults.root ${DIR_THIS}/results/${STEP}/AnalysisResults.root
-    mv dpl-config.json ${DIR_THIS}/results/${STEP}/${STEP}.json
+    mkdir -p "${DIR_THIS}/results/${STEP}"
+    mv AnalysisResults.root "${DIR_THIS}/results/${STEP}/AnalysisResults.root"
+    mv dpl-config.json "${DIR_THIS}/results/${STEP}/${STEP}.json"
 else
     echo "Error: Exit code ${rc}"
     echo "Check the log file ${LOGFILE}"
--- Tutorials/PWGLF/Strangeness/PbPb/Analysis/run_step3.sh
+++ Tutorials/PWGLF/Strangeness/PbPb/Analysis/run_step3.sh
@@ -8,15 +8,15 @@ DIR_THIS=$PWD
 
 OPTION="-b --configuration json://configuration_step3.json"
 
-o2-analysis-lf-cascadepid ${OPTION} | o2-analysis-lf-cascadespawner ${OPTION} | o2-analysistutorial-lf-strangeness-pbpb-step3 ${OPTION} --aod-file @input_data.txt > "$LOGFILE" 2>&1
+o2-analysis-lf-cascadepid "${OPTION}" | o2-analysis-lf-cascadespawner "${OPTION}" | o2-analysistutorial-lf-strangeness-pbpb-step3 "${OPTION}" --aod-file @input_data.txt > "$LOGFILE" 2>&1
 
 # report status
 rc=$?
 if [ $rc -eq 0 ]; then
     echo "No problems!"
-    mkdir -p ${DIR_THIS}/results/step${STEP}
-    mv AnalysisResults.root ${DIR_THIS}/results/step${STEP}/AnalysisResults.root
-    mv dpl-config.json ${DIR_THIS}/results/step${STEP}/step${STEP}.json
+    mkdir -p "${DIR_THIS}/results/step${STEP}"
+    mv AnalysisResults.root "${DIR_THIS}/results/step${STEP}/AnalysisResults.root"
+    mv dpl-config.json  "${DIR_THIS}/results/step${STEP}/step${STEP}.json"
 else
     echo "Error: Exit code ${rc}"
     echo "Check the log file ${LOGFILE}"
