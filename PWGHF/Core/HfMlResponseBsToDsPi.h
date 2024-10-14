939856b1559487c72e66642a92673cce6d94c17c
ALICE Builder <alibuild@users.noreply.github.com>
2024-10-14 17:20:08 +0200

    Please consider the following formatting changes (#8)

--- PWGHF/Core/HfMlResponseBsToDsPi.h
+++ PWGHF/Core/HfMlResponseBsToDsPi.h
@@ -26,9 +26,10 @@
 // Fill the map of available input features
 // the key is the feature's name (std::string)
 // the value is the corresponding value in EnumInputFeatures
-#define FILL_MAP_BS(FEATURE) \
-  {                          \
-    #FEATURE, static_cast<uint8_t>(InputFeaturesBsToDsPi::FEATURE)}
+#define FILL_MAP_BS(FEATURE)                                       \
+  {                                                                \
+    #FEATURE, static_cast<uint8_t>(InputFeaturesBsToDsPi::FEATURE) \
+  }
 
 // Check if the index of mCachedIndices (index associated to a FEATURE)
 // matches the entry in EnumInputFeatures associated to this FEATURE
