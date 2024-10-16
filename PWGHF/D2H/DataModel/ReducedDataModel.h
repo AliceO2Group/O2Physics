56f6ffc5de2ae47a4843f10ae0c35c62e62217d3
cterrevo <cristina.terrevoli@cern.ch>
2024-10-17 01:00:08 +0200

    [PWGHF] adding DstarTrack resonance (#8014)
    * [PWGHF] adding DstarTrack resonance
    
    * [PWGHF] removing index and renaming candV0Tr
    
    * [PWGHF] fixing indeces
    
    * [PWGHF] fixing bug candV0Tr
    
    * Please consider the following formatting changes
    
    ---------
    
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

--- PWGHF/D2H/DataModel/ReducedDataModel.h
+++ PWGHF/D2H/DataModel/ReducedDataModel.h
@@ -756,6 +756,15 @@ DECLARE_SOA_TABLE(HfMcGenRedResos, "AOD", "HFMCGENREDRESO", //! Generation-level
                   hf_b0_mc::YProng1,
                   hf_b0_mc::EtaProng1,
                   o2::soa::Marker<1>);
+
+DECLARE_SOA_TABLE(HfCandChaResTr, "AOD", "HFCANDCHARESTR", //! Table with Resonance candidate information for resonances plus tracks reduced workflow
+                                                           // Static
+                  hf_cand::PxProng0, hf_cand::PyProng0, hf_cand::PzProng0,
+                  hf_cand::PxProng1, hf_cand::PyProng1, hf_cand::PzProng1,
+                  hf_reso_cand_reduced::InvMass,
+                  hf_reso_cand_reduced::InvMassProng0,
+                  // Dynamic
+                  hf_reso_cand_reduced::PtProng0<hf_cand::PxProng0, hf_cand::PyProng0>);
 } // namespace aod
 
 namespace soa
