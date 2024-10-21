ea922750a23c4dbe567e5656b78af6066e4e8e9b
scattaru <scattaru@cern.ch>
2024-10-21 17:31:07 +0200

    Implementation of derived data for Ds-h corelation for ME

--- PWGHF/HFC/TableProducer/correlatorDsHadrons.cxx
+++ PWGHF/HFC/TableProducer/correlatorDsHadrons.cxx
@@ -29,6 +29,7 @@
 #include "PWGHF/DataModel/CandidateReconstructionTables.h"
 #include "PWGHF/DataModel/CandidateSelectionTables.h"
 #include "PWGHF/HFC/DataModel/CorrelationTables.h"
+#include "PWGHF/HFC/DataModel/DerivedDataCorrelationTables.h"
 
 using namespace o2;
 using namespace o2::analysis;
@@ -135,6 +136,9 @@ struct HfCorrelatorDsHadrons {
   Produces<aod::DsCandRecoInfo> entryDsCandRecoInfo;
   Produces<aod::DsCandGenInfo> entryDsCandGenInfo;
   Produces<aod::TrackRecoInfo> entryTrackRecoInfo;
+  Produces<aod::HfRedCollisions> collReduced;
+  Produces<aod::DsCandReduced> candReduced;
+  Produces<aod::AssocTrackReduced> assocTrackReduced;
 
   Configurable<bool> fillHistoData{"fillHistoData", true, "Flag for filling histograms in data processes"};
   Configurable<bool> fillHistoMcRec{"fillHistoMcRec", true, "Flag for filling histograms in MC Rec processes"};
@@ -679,6 +683,25 @@ struct HfCorrelatorDsHadrons {
     }   // end loop generated collision
   }
   PROCESS_SWITCH(HfCorrelatorDsHadrons, processMcGen, "Process MC Gen mode", false);
+  
+  void processDerivedDataDs(SelCollisionsWithDs::iterator const& collision,
+                            CandDsData const& candidates,
+                            MyTracksData const& tracks)
+  {
+    collReduced(collision.multFV0M(), collision.posZ());
+    // Ds fill histograms and Ds candidates information stored
+    for (const auto& candidate : candidates) {
+          
+      // candidate selected with rectangular selections
+      candReduced(collReduced.lastIndex(), candidate.phi(), candidate.eta(), candidate.pt());
+
+      // tracks information
+      for (const auto& track : tracks) {
+        assocTrackReduced(collReduced.lastIndex(), track.phi(), track.eta(), track.pt());
+      }
+    }
+  }
+  PROCESS_SWITCH(HfCorrelatorDsHadrons, processDerivedDataDs, "Process derived data Ds", false);
 
   // Event Mixing
   void processDataME(SelCollisionsWithDs const& collisions,
