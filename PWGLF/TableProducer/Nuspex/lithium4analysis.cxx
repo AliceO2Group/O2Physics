256cbcbae0f76a23abf8f3da9bdda3c93c3ca51d
Francesco Mazzaschi <43742195+fmazzasc@users.noreply.github.com>
2024-10-23 19:58:05 +0200

    PWGLF: make the EM faster in li4 analysis (#8114)
    Co-authored-by: Francesco Mazzaschi <fmazzasc@alipap1.cern.ch>

--- PWGLF/TableProducer/Nuspex/lithium4analysis.cxx
+++ PWGLF/TableProducer/Nuspex/lithium4analysis.cxx
@@ -410,7 +410,6 @@ struct lithium4analysis {
   {
     bool heliumPID = candidate.pidForTracking() == o2::track::PID::Helium3 || candidate.pidForTracking() == o2::track::PID::Alpha;
     float correctedTPCinnerParam = (heliumPID && setting_compensatePIDinTracking) ? candidate.tpcInnerParam() / 2.f : candidate.tpcInnerParam();
-    m_qaRegistry.fill(HIST("h2dEdxHe3candidates"), correctedTPCinnerParam * 2.f, candidate.tpcSignal());
 
     if (correctedTPCinnerParam < setting_cutRigidityMinHe3) {
       return false;
@@ -436,6 +435,7 @@ struct lithium4analysis {
       return false;
     }
 
+    m_qaRegistry.fill(HIST("h2dEdxHe3candidates"), candidate.sign() * correctedTPCinnerParam, candidate.tpcSignal());
     m_qaRegistry.fill(HIST("h2NsigmaHe3TPC"), candidate.sign() * correctedTPCinnerParam, nSigmaHe3);
     m_qaRegistry.fill(HIST("h2ClSizeCosLamHe3"), nSigmaHe3, clsizeCoslIts);
     return true;
@@ -610,28 +610,15 @@ struct lithium4analysis {
     }
   }
 
-  void pairTracksEventMixing()
+  template <typename T>
+  void pairTracksEventMixing(T& he3Cands, T& protonCands)
   {
-
-    for (auto& [c1, tracks1, c2, tracks2] : m_pair) {
-      if (!c1.sel8() || !c2.sel8()) {
+    for (auto& he3Cand : he3Cands) {
+      if (!selectTrack(he3Cand) || !selectionPIDHe3(he3Cand)) {
         continue;
       }
-      m_qaRegistry.fill(HIST("hNcontributor"), c1.numContrib());
-      m_qaRegistry.fill(HIST("hVtxZ"), c1.posZ());
-
-      for (auto& [t1, t2] : o2::soa::combinations(o2::soa::CombinationsFullIndexPolicy(tracks1, tracks2))) {
-
-        if (!selectTrack(t1) || !selectTrack(t2)) {
-          continue;
-        }
-
-        TrackCandidates::iterator he3Cand, protonCand;
-        if (selectionPIDHe3(t1) && selectionPIDProton(t2)) {
-          he3Cand = t1, protonCand = t2;
-        } else if (selectionPIDHe3(t2) && selectionPIDProton(t1)) {
-          he3Cand = t2, protonCand = t1;
-        } else {
+      for (auto& protonCand : protonCands) {
+        if (!selectTrack(protonCand) || !selectionPIDProton(protonCand)) {
           continue;
         }
 
@@ -774,9 +761,20 @@ struct lithium4analysis {
 
   void processMixedEvent(const CollisionsFull& collisions, const TrackCandidates& tracks)
   {
-    LOG(info) << "Processing mixed event";
+    LOG(debug) << "Processing mixed event";
     m_trackPairs.clear();
-    pairTracksEventMixing();
+
+    for (auto& [c1, tracks1, c2, tracks2] : m_pair) {
+      if (!c1.sel8() || !c2.sel8()) {
+        continue;
+      }
+
+      m_qaRegistry.fill(HIST("hNcontributor"), c1.numContrib());
+      m_qaRegistry.fill(HIST("hVtxZ"), c1.posZ());
+
+      pairTracksEventMixing(tracks1, tracks2);
+      pairTracksEventMixing(tracks2, tracks1);
+    }
 
     for (auto& trackPair : m_trackPairs) {
 
