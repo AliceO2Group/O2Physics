3d052fb0ba14759c02e34ef16183dc48d8bab635
Maria Paula Martins Palhares <maria.paula.palhares@cern.ch>
2024-10-28 19:26:39 +0100

    Revert "PWGLF/NuSpEx: nSigmaCutTOF add"
    This reverts commit 9fdd3fc627658529af7da4019ef31d13ff51cf0a.

--- PWGLF/TableProducer/Nuspex/lnnRecoTask.cxx
+++ PWGLF/TableProducer/Nuspex/lnnRecoTask.cxx
@@ -151,7 +151,6 @@ struct lnnRecoTask {
   Configurable<float> TPCRigidityMin3H{"TPCRigidityMin3H", 0.2, "Minimum rigidity of the triton candidate"};
   Configurable<float> nSigmaCutMinTPC{"nSigmaCutMinTPC", -5, "triton dEdx cut (n sigma)"};
   Configurable<float> nSigmaCutMaxTPC{"nSigmaCutMaxTPC", 5, "triton dEdx cut (n sigma)"};
-  Configurable<float> nSigmaCutTOF{"nSigmaCutTOF", 3, "triton cut (n sigma) TOF"};
   Configurable<float> nTPCClusMin3H{"nTPCClusMin3H", 80, "triton NTPC clusters cut"};
   Configurable<float> ptMinTOF{"ptMinTOF", 0.8, "minimum pt for TOF cut"};
   Configurable<float> TrTOFMass2Cut{"TrTOFMass2Cut", 5.5, "minimum Triton mass square to TOF"};
@@ -400,9 +399,6 @@ struct lnnRecoTask {
         beta = h3track.beta();
         lnnCand.mass2TrTOF = h3track.mass() * h3track.mass();
         if (lnnCand.mass2TrTOF < TrTOFMass2Cut) {
-          continue;
-        }
-        if (std::abs(h3track.tofNSigmaTr()) > nSigmaCutTOF) {
           continue;
         }
       }
