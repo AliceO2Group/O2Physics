2a6c021a79b34a045a4a64e77963b8196dfad3fd
zjxiongOvO <kausorxiongzj@mail.ustc.edu.cn>
2024-10-29 20:48:51 +0800

    update HistogramManager for Run-by-Run QA

--- PWGDQ/Core/HistogramManager.h
+++ PWGDQ/Core/HistogramManager.h
@@ -67,14 +67,14 @@ class HistogramManager : public TNamed
                     int nYbins = 0, double ymin = 0, double ymax = 0, int varY = -1,
                     int nZbins = 0, double zmin = 0, double zmax = 0, int varZ = -1,
                     const char* xLabels = "", const char* yLabels = "", const char* zLabels = "",
-                    int varT = -1, int varW = -1, bool isdouble = false);
+                    int varT = -1, int varW = -1, bool isdouble = false, bool isFillLabelx = false);
   // Similar to the above function, with the difference that the user can specify non-equidistant binning
   void AddHistogram(const char* histClass, const char* name, const char* title, bool isProfile,
                     int nXbins, double* xbins, int varX,
                     int nYbins = 0, double* ybins = nullptr, int varY = -1,
                     int nZbins = 0, double* zbins = nullptr, int varZ = -1,
                     const char* xLabels = "", const char* yLabels = "", const char* zLabels = "",
-                    int varT = -1, int varW = -1, bool isdouble = false);
+                    int varT = -1, int varW = -1, bool isdouble = false, bool isFillLabelx = false);
   // Create a THn histogram (either THnF or THnSparse) with equidistant binning
   void AddHistogram(const char* histClass, const char* name, const char* title,
                     int nDimensions, int* vars, int* nBins, double* xmin, double* xmax,
