51cf5bec06495aa2f38c94f231ac946922a00d5e
Xu wang <113404602+1481014945@users.noreply.github.com>
2024-10-31 16:33:43 +0800

    Update utilsCorrelations.h

--- PWGHF/HFC/Utils/utilsCorrelations.h
+++ PWGHF/HFC/Utils/utilsCorrelations.h
@@ -16,6 +16,7 @@
 #ifndef PWGHF_HFC_UTILS_UTILSCORRELATIONS_H_
 #define PWGHF_HFC_UTILS_UTILSCORRELATIONS_H_
 
+#include "CommonConstants/PhysicsConstants.h"
 #include <TPDGCode.h>
 #include <cmath>
 
