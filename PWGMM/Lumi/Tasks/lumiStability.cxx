053ba2d2dd91c85fad29ef3f3cfea2b3b1140917
Josue-MiniPC <martinezgarciajosue54@gmail.com>
2024-10-31 00:45:38 -0600

    resolving issues

--- PWGMM/Lumi/Tasks/lumiStability.cxx
+++ PWGMM/Lumi/Tasks/lumiStability.cxx
@@ -35,6 +35,11 @@
 #include "DataFormatsParameters/GRPLHCIFData.h"
 #include "DataFormatsParameters/GRPECSObject.h"
 
+#include <utility>
+#include <map>
+#include <string>
+#include <vector>
+
 using namespace o2;
 using namespace o2::framework;
 
