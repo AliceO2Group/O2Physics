e5e0871acda2f5509cad8477bcd028bacd2fa704
Xu wang <113404602+1481014945@users.noreply.github.com>
2024-10-31 21:37:06 +0800

    Update utilsCorrelations.h

--- PWGHF/HFC/Utils/utilsCorrelations.h
+++ PWGHF/HFC/Utils/utilsCorrelations.h
@@ -13,7 +13,7 @@
 /// \brief Utilities for HFC analyses
 /// \author Xu Wang <waxu@cern.ch>
 
-#  ifndef PWGHF_HFC_UTILS_UTILSCORRELATIONS_H_
+#ifndef PWGHF_HFC_UTILS_UTILSCORRELATIONS_H_
 #define PWGHF_HFC_UTILS_UTILSCORRELATIONS_H_
 
 #include <cmath>
