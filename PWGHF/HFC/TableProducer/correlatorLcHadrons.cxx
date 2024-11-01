a3bb0c32b677c90941e8aa77e2260482a9e42b92
Xu wang <113404602+1481014945@users.noreply.github.com>
2024-11-01 10:32:28 +0800

    Update correlatorLcHadrons.cxx

--- PWGHF/HFC/TableProducer/correlatorLcHadrons.cxx
+++ PWGHF/HFC/TableProducer/correlatorLcHadrons.cxx
@@ -15,6 +15,8 @@
 /// \author Marianna Mazzilli <marianna.mazzilli@cern.ch>
 /// \author Zhen Zhang <zhenz@cern.ch>
 
+#include <vector>
+
 #include "CommonConstants/PhysicsConstants.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/HistogramRegistry.h"
