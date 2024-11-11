0080f5d008ef44693a86d2bfd0ebc840e5505d6b
Iris_Likmeta <iris.likmeta@cern.ch>
2024-11-11 11:36:07 -0600

    [PWGCF] Fix Megalinter error

--- PWGCF/Flow/Tasks/GFWEfficiency.cxx
+++ PWGCF/Flow/Tasks/GFWEfficiency.cxx
@@ -23,7 +23,6 @@
 
 #include "Common/DataModel/EventSelection.h"
 #include "Common/Core/TrackSelection.h"
-#include "Common/DataModel/TrackSelectionTables.h"
 #include "Common/DataModel/Centrality.h"
 #include "Common/DataModel/Multiplicity.h"
 
