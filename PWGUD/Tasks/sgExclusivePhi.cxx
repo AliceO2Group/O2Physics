6f2ccfcad133cc12f6219a6ef021ee6bcbdec5b5
Sasha Bylinkin <37345380+abylinkin@users.noreply.github.com>
2024-11-13 09:22:51 +0100

    Update sgExclusivePhi.cxx
    MegaLinter include

--- PWGUD/Tasks/sgExclusivePhi.cxx
+++ PWGUD/Tasks/sgExclusivePhi.cxx
@@ -9,6 +9,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 //
+#include <vector>
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
