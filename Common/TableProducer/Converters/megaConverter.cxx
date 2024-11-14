fabcc4833b9a5f1b9c97b911c5a1cf2fdb14b5c6
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2024-11-14 10:35:22 +0100

    Update

--- Common/TableProducer/Converters/megaConverter.cxx
+++ Common/TableProducer/Converters/megaConverter.cxx
@@ -34,14 +34,15 @@ using namespace o2::zdc;
 // Converts bc_000 into bc_001
 struct bcConverter {
   Produces<aod::BCs_001> bc_001;
-
   void init(o2::framework::InitContext& initContext)
   {
     if (!doprocessConverter) {
       doprocessConverter.value = isTableRequiredInWorkflow(initContext, "BCs_001");
     }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table BCs_001";
+    }
   }
-  void process(aod::Timestamps const&) {}
   void processConverter(aod::BCs_000 const& bcTable)
   {
     for (auto& bc : bcTable) {
@@ -63,13 +64,18 @@ struct collisionConverter {
 
   HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
-  void init(InitContext const&)
+  void init(o2::framework::InitContext& initContext)
   {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "Collisions_001");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table Collisions_001";
+    }
     const AxisSpec axisCYYdebug{nbins, -1.0f, +1.0f, ""};
     histos.add("hCYY", "hCYY", kTH1F, {axisCYYdebug});
   }
-
-  void process(aod::Collisions_000 const& collisionTable)
+  void processConverter(aod::Collisions_000 const& collisionTable)
   {
     float negtolerance = -1.0f * tolerance;
     for (auto& collision : collisionTable) {
@@ -119,13 +125,23 @@ struct collisionConverter {
         collision.collisionTime(), collision.collisionTimeRes());
     }
   }
+  PROCESS_SWITCH(collisionConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 // Converts FDD table from version 000 to 001
 struct FddConverter {
   Produces<aod::FDDs_001> fdd_001;
 
-  void process(aod::FDDs_000 const& fdd_000)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "FDDs_001");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table FDDs_001";
+    }
+  }
+  void processConverter(aod::FDDs_000 const& fdd_000)
   {
     for (auto& p : fdd_000) {
       int16_t chargeA[8] = {0u};
@@ -143,12 +159,22 @@ struct FddConverter {
               p.timeA(), p.timeC(), p.triggerMask());
     }
   }
+  PROCESS_SWITCH(FddConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 struct hmpConverter {
   Produces<aod::HMPID_001> HMPID_001;
 
-  void process(aod::HMPID_000 const& hmpLegacy, aod::Tracks const&)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "HMPID_001");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table HMPID_001";
+    }
+  }
+  void processConverter(aod::HMPID_000 const& hmpLegacy, aod::Tracks const&)
   {
     for (auto& hmpData : hmpLegacy) {
 
@@ -178,13 +204,22 @@ struct hmpConverter {
                 hmpidPhotsCharge);
     }
   }
+  PROCESS_SWITCH(hmpConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 // Converts the old McCaloLabels_000 table to the new McCaloLabels_001 table where we have a variable size array for associated MCParticles for each calo cell
 struct caloLabelConverter {
   Produces<aod::McCaloLabels_001> McCaloLabels_001;
-
-  void process(aod::McCaloLabels_000 const& mccalolabelTable)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "McCaloLabels_001");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table BCs_001";
+    }
+  }
+  void processConverter(aod::McCaloLabels_000 const& mccalolabelTable)
   {
     std::vector<float> amplitude = {0};
     std::vector<int32_t> particleId = {0};
@@ -196,13 +231,22 @@ struct caloLabelConverter {
         amplitude);
     }
   }
+  PROCESS_SWITCH(caloLabelConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 // Converts MCParticle table from version 000 to 001
 struct McConverter {
   Produces<aod::StoredMcParticles_001> mcParticles_001;
-
-  void process(aod::StoredMcParticles_000 const& mcParticles_000)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "StoredMcParticles_001");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table StoredMcParticles_001";
+    }
+  }
+  void processConverter(aod::StoredMcParticles_000 const& mcParticles_000)
   {
     for (auto& p : mcParticles_000) {
 
@@ -228,11 +272,21 @@ struct McConverter {
                       p.vx(), p.vy(), p.vz(), p.vt());
     }
   }
+  PROCESS_SWITCH(McConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 struct TracksExtraConverter {
   Produces<aod::StoredTracksExtra_001> tracksExtra_001;
-  void process(aod::TracksExtra_000 const& tracksExtra_000)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "StoredTracksExtra_002");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table StoredTracksExtra_002";
+    }
+  }
+  void processConverter(aod::TracksExtra_000 const& tracksExtra_000)
   {
 
     for (const auto& track0 : tracksExtra_000) {
@@ -266,12 +320,21 @@ struct TracksExtraConverter {
                       track0.trackTimeRes());
     }
   }
+  PROCESS_SWITCH(TracksExtraConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 struct zdcConverter {
   Produces<aod::Zdcs_001> Zdcs_001;
-
-  void process(aod::Zdcs_000 const& zdcLegacy, aod::BCs const&)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "Zdcs_001");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table Zdcs_001";
+    }
+  }
+  void processConverter(aod::Zdcs_000 const& zdcLegacy, aod::BCs const&)
   {
     for (auto& zdcData : zdcLegacy) {
       // Get legacy information, please
@@ -353,12 +416,21 @@ struct zdcConverter {
                zdcChannelsT);
     }
   }
+  PROCESS_SWITCH(zdcConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 struct mcCollisionConverter {
   Produces<aod::McCollisions_001> mcCollisions_001;
-
-  void process(aod::McCollisions_000 const& mcCollisionTable)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "McCollisions_001");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table McCollisions_001";
+    }
+  }
+  void processConverter(aod::McCollisions_000 const& mcCollisionTable)
   {
     for (auto& mcCollision : mcCollisionTable) {
 
@@ -372,11 +444,21 @@ struct mcCollisionConverter {
         0.0f); // dummy event plane, not available in _000
     }
   }
+  PROCESS_SWITCH(mcCollisionConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 struct MftTracksConverter {
   Produces<aod::StoredMFTTracks_001> mftTracks_001;
-  void process(aod::MFTTracks_000 const& mftTracks_000)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "StoredMFTTracks_001");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table StoredMFTTracks_001";
+    }
+  }
+  void processConverter(aod::MFTTracks_000 const& mftTracks_000)
   {
 
     for (const auto& track0 : mftTracks_000) {
@@ -401,6 +483,7 @@ struct MftTracksConverter {
                     track0.trackTimeRes());
     }
   }
+  PROCESS_SWITCH(MftTracksConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 /// Spawn the extended table for MFTTracks001 to avoid the call to the internal spawner and a consequent circular dependency
@@ -410,7 +493,16 @@ struct MFTTracksSpawner {
 
 struct MultsExtraConverter {
   Produces<aod::MultsExtra_001> multsExtra_001;
-  void process(aod::MultsExtra_000 const& multsExtra_000)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "MultsExtra_001");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table MultsExtra_001";
+    }
+  }
+  void processConverter(aod::MultsExtra_000 const& multsExtra_000)
   {
     for (const auto& r : multsExtra_000) {
       multsExtra_001(r.multPVTotalContributors(), r.multPVChi2(),
@@ -424,71 +516,120 @@ struct MultsExtraConverter {
                      r.flags());
     }
   }
+  PROCESS_SWITCH(MultsExtraConverter, processConverter, "Process converter (autoset if false)", false);
 };
 
 struct V0Converter {
   Produces<aod::V0s_002> v0s_002;
-
-  void process(aod::V0s_001 const& v0s)
+  void init(o2::framework::InitContext& initContext)
+  {
+    if (!doprocessConverter) {
+      doprocessConverter.value = isTableRequiredInWorkflow(initContext, "V0s_002");
+    }
+    if(doprocessConverter){
+      LOG(info) << "Enabling converter for table V0s_002";
+    }
+  }
+  void processConverter(aod::V0s_001 const& v0s)
   {
     for (auto& v0 : v0s) {
       uint8_t bitMask = static_cast<uint8_t>(1); // first bit on
       v0s_002(v0.collisionId(), v0.posTrackId(), v0.negTrackId(), bitMask);
     }
   }
+  PROCESS_SWITCH(V0Converter, processConverter, "Process converter (autoset if false)", false);
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   auto workflow = WorkflowSpec{};
+  if (0) {
+    for (auto c : cfgc.options().specs()) {
+      LOG(info) << "Option: " << c.name << " " << static_cast<int>(c.type);
+      // LOG(info) << "Option: " << c.name << " " << cfgc.options().get<c.type>(c.name);
+    }
+  }
   if (cfgc.options().hasOption("aod-metadata-tables")) {
     const std::vector<std::string> tables = cfgc.options().get<std::vector<std::string>>("aod-metadata-tables");
     for (auto t : tables) {
+      // if (t.find("_0") == std::string::npos) {
+      //   continue;
+      // }
+      LOG(info) << "AOD converter: Table " << t << " checking for converters";
       if (t == "O2cascade_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2cascade_000") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2bc_001") {
-      } else if (t == "O2bc_000") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
+      } else if (t == "O2bc_000" || t == "O2bc") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<bcConverter>(cfgc));
       } else if (t == "O2collision_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2collision_000") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<collisionConverter>(cfgc));
       } else if (t == "O2fdd_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2fdd_000") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<FddConverter>(cfgc));
       } else if (t == "O2hmpid_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2hmpid_000") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<hmpConverter>(cfgc));
       } else if (t == "O2mccalolabel_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2mccalolabel_000") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<caloLabelConverter>(cfgc));
       } else if (t == "O2mfttrack_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2mfttrack_000") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<MftTracksConverter>(cfgc));
         workflow.push_back(adaptAnalysisTask<MFTTracksSpawner>(cfgc));
       } else if (t == "O2multextra_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2multextra_000") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<MultsExtraConverter>(cfgc));
       } else if (t == "O2v0_002") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2v0_001") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<V0Converter>(cfgc));
       } else if (t == "O2v0_000") {
-        workflow.push_back(adaptAnalysisTask<V0Converter>(cfgc));
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2mccollision_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2mccollision_000") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<mcCollisionConverter>(cfgc));
       } else if (t == "O2mcparticle_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2mcparticle_000") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<McConverter>(cfgc));
       } else if (t == "O2trackextra_001") {
-      } else if (t == "O2trackextra_000") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
+      } else if (t == "O2trackextra_000" || t == "O2trackextra") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<TracksExtraConverter>(cfgc));
+        workflow.push_back(adaptAnalysisTask<TracksExtraSpawner>(cfgc));
       } else if (t == "O2zdc_001") {
+        LOG(info) << "  - AOD converter: Table " << t << " needs no converter";
       } else if (t == "O2zdc_000") {
+        LOG(info) << "  + AOD converter: Table " << t << " found, adding converter task";
         workflow.push_back(adaptAnalysisTask<zdcConverter>(cfgc));
-      } else if (t.find("_0") != std::string::npos) {
-        LOG(warning) << "Versioned table not covered " << t;
+      } else {
+        LOG(warning) << "AOD converter: Versioned table not covered " << t;
       }
     }
+  } else {
+    LOG(warning) << "AOD converter: No tables found in the workflow";
   }
   return workflow;
 }
