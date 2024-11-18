b5d3760d634ea1fbe96775499ec2c669dd043cc0
Nicolas Strangmann <nicolas.strangmann@.cern.ch>
2024-11-18 17:12:26 +0100

    Fix mistake in first line

--- PWGJE/TableProducer/emcalCorrectionTask.cxx
+++ PWGJE/TableProducer/emcalCorrectionTask.cxx
@@ -1,4 +1,3 @@
-PWGJE/TableProducer/emcalCorrectionTask.cxx
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
