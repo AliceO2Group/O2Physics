beef1d581110cb3fe9a8978b8549576959799a22
fchinu <fachi2001@gmail.com>
2024-11-22 14:49:14 +0100

    Format changes

--- PWGHF/Tasks/pidStudies.cxx
+++ PWGHF/Tasks/pidStudies.cxx
@@ -15,7 +15,7 @@
 /// \author Fabrizio Chinu <fabrizio.chinu@cern.ch>, Università and INFN Torino
 /// \author Stefano Politanò <stefano.politano@cern.ch>, INFN Torino
 /// \author Marcello Di Costanzo <marcello.di.costanzo@cern.ch>, Politecnico and INFN Torino
-/// \author Marcello Di Costanzo <luca.aglietta@unito.it>, Università and INFN Torino
+/// \author Luca Aglietta <luca.aglietta@unito.it>, Università and INFN Torino
 
 #include "TPDGCode.h"
 
@@ -34,47 +34,49 @@ using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
-
 namespace o2::aod
 {
 namespace pid_studies
 {
-enum Particle {NotMatched=0, K0s, Lambda, Omega};
+enum Particle { NotMatched = 0,
+                K0s,
+                Lambda,
+                Omega };
 // V0s
-DECLARE_SOA_COLUMN(MassK0, massK0, float);                  //! Candidate mass
-DECLARE_SOA_COLUMN(MassLambda, massLambda, float);          //! Candidate mass
-DECLARE_SOA_COLUMN(MassAntiLambda, massAntiLambda, float);  //! Candidate mass
-DECLARE_SOA_COLUMN(Pt, pt, float);                          //! Transverse momentum of the candidate (GeV/c)
-DECLARE_SOA_COLUMN(PtPos, ptPos, float);                    //! Transverse momentum of positive track (GeV/c)
-DECLARE_SOA_COLUMN(PtNeg, ptNeg, float);                    //! Transverse momentum of negative track (GeV/c)
-DECLARE_SOA_COLUMN(Radius, radius, float);                  //! Radius
-DECLARE_SOA_COLUMN(Cpa, cpa, float);                        //! Cosine of pointing angle
-DECLARE_SOA_COLUMN(NSigmaTpcPosPi, nSigmaTpcPosPi, float);  //! nSigmaTPC of positive track with pion hypothesis
-DECLARE_SOA_COLUMN(NSigmaTpcNegPi, nSigmaTpcNegPi, float);  //! nSigmaTPC of negative track with pion hypothesis
-DECLARE_SOA_COLUMN(NSigmaTpcPosPr, nSigmaTpcPosPr, float);  //! nSigmaTPC of positive track with proton hypothesis
-DECLARE_SOA_COLUMN(NSigmaTpcNegPr, nSigmaTpcNegPr, float);  //! nSigmaTPC of negative track with proton hypothesis
-DECLARE_SOA_COLUMN(NSigmaTofPosPi, nSigmaTofPosPi, float);  //! nSigmaTOF of positive track with pion hypothesis
-DECLARE_SOA_COLUMN(NSigmaTofNegPi, nSigmaTofNegPi, float);  //! nSigmaTOF of negative track with pion hypothesis
-DECLARE_SOA_COLUMN(NSigmaTofPosPr, nSigmaTofPosPr, float);  //! nSigmaTOF of positive track with proton hypothesis
-DECLARE_SOA_COLUMN(NSigmaTofNegPr, nSigmaTofNegPr, float);  //! nSigmaTOF of negative track with proton hypothesis
-DECLARE_SOA_COLUMN(AlphaArm, alphaArm, float);              //! Armenteros alpha
-DECLARE_SOA_COLUMN(QtArm, qtArm, float);                    //! Armenteros Qt
+DECLARE_SOA_COLUMN(MassK0, massK0, float);                 //! Candidate mass
+DECLARE_SOA_COLUMN(MassLambda, massLambda, float);         //! Candidate mass
+DECLARE_SOA_COLUMN(MassAntiLambda, massAntiLambda, float); //! Candidate mass
+DECLARE_SOA_COLUMN(Pt, pt, float);                         //! Transverse momentum of the candidate (GeV/c)
+DECLARE_SOA_COLUMN(PtPos, ptPos, float);                   //! Transverse momentum of positive track (GeV/c)
+DECLARE_SOA_COLUMN(PtNeg, ptNeg, float);                   //! Transverse momentum of negative track (GeV/c)
+DECLARE_SOA_COLUMN(Radius, radius, float);                 //! Radius
+DECLARE_SOA_COLUMN(Cpa, cpa, float);                       //! Cosine of pointing angle
+DECLARE_SOA_COLUMN(NSigmaTpcPosPi, nSigmaTpcPosPi, float); //! nSigmaTPC of positive track with pion hypothesis
+DECLARE_SOA_COLUMN(NSigmaTpcNegPi, nSigmaTpcNegPi, float); //! nSigmaTPC of negative track with pion hypothesis
+DECLARE_SOA_COLUMN(NSigmaTpcPosPr, nSigmaTpcPosPr, float); //! nSigmaTPC of positive track with proton hypothesis
+DECLARE_SOA_COLUMN(NSigmaTpcNegPr, nSigmaTpcNegPr, float); //! nSigmaTPC of negative track with proton hypothesis
+DECLARE_SOA_COLUMN(NSigmaTofPosPi, nSigmaTofPosPi, float); //! nSigmaTOF of positive track with pion hypothesis
+DECLARE_SOA_COLUMN(NSigmaTofNegPi, nSigmaTofNegPi, float); //! nSigmaTOF of negative track with pion hypothesis
+DECLARE_SOA_COLUMN(NSigmaTofPosPr, nSigmaTofPosPr, float); //! nSigmaTOF of positive track with proton hypothesis
+DECLARE_SOA_COLUMN(NSigmaTofNegPr, nSigmaTofNegPr, float); //! nSigmaTOF of negative track with proton hypothesis
+DECLARE_SOA_COLUMN(AlphaArm, alphaArm, float);             //! Armenteros alpha
+DECLARE_SOA_COLUMN(QtArm, qtArm, float);                   //! Armenteros Qt
 
 // Cascades
-DECLARE_SOA_COLUMN(MassOmega, massOmega, float);              //! Candidate mass
-DECLARE_SOA_COLUMN(MassXi, massXi, float);                    //! Candidate mass
-DECLARE_SOA_COLUMN(CascCosPA, casccosPA, float);              //! Cascade CPA
-DECLARE_SOA_COLUMN(DCAV0daughters, dcaV0daughters, float);    //! DCA of V0 daughters
-DECLARE_SOA_COLUMN(DCAv0topv, dcav0topv, float);              //! V0 DCA to PV
-DECLARE_SOA_COLUMN(NSigmaTpcBachKa, nSigmaTpcBachKa, float);  //! nSigmaTPC of positive bachelor with kaon hypothesis
-DECLARE_SOA_COLUMN(NSigmaTofBachKa, nSigmaTofBachKa, float);  //! nSigmaTPC of negative bachelor with kaon hypothesis
+DECLARE_SOA_COLUMN(MassOmega, massOmega, float);             //! Candidate mass
+DECLARE_SOA_COLUMN(MassXi, massXi, float);                   //! Candidate mass
+DECLARE_SOA_COLUMN(CascCosPA, casccosPA, float);             //! Cascade CPA
+DECLARE_SOA_COLUMN(DCAV0daughters, dcaV0daughters, float);   //! DCA of V0 daughters
+DECLARE_SOA_COLUMN(DCAv0topv, dcav0topv, float);             //! V0 DCA to PV
+DECLARE_SOA_COLUMN(NSigmaTpcBachKa, nSigmaTpcBachKa, float); //! nSigmaTPC of positive bachelor with kaon hypothesis
+DECLARE_SOA_COLUMN(NSigmaTofBachKa, nSigmaTofBachKa, float); //! nSigmaTPC of negative bachelor with kaon hypothesis
 
 // Common columns
-DECLARE_SOA_COLUMN(OccupancyFt0c, occupancyFt0c, float);    //! Occupancy from FT0C
-DECLARE_SOA_COLUMN(OccupancyIts, occupancyIts, float);      //! Occupancy from ITS
-DECLARE_SOA_COLUMN(CentralityFT0C, centralityFT0C, float);  //! Centrality from FT0C
-DECLARE_SOA_COLUMN(CentralityFT0M, centralityFT0M, float);  //! Centrality from FT0M
-DECLARE_SOA_COLUMN(CandFlag, candFlag, int);                //! Flag for MC matching
+DECLARE_SOA_COLUMN(OccupancyFt0c, occupancyFt0c, float);   //! Occupancy from FT0C
+DECLARE_SOA_COLUMN(OccupancyIts, occupancyIts, float);     //! Occupancy from ITS
+DECLARE_SOA_COLUMN(CentralityFT0C, centralityFT0C, float); //! Centrality from FT0C
+DECLARE_SOA_COLUMN(CentralityFT0M, centralityFT0M, float); //! Centrality from FT0M
+DECLARE_SOA_COLUMN(CandFlag, candFlag, int);               //! Flag for MC matching
 } // namespace pid_studies
 
 DECLARE_SOA_TABLE(pidV0s, "AOD", "PIDV0S", //! Table with PID information
@@ -98,10 +100,9 @@ DECLARE_SOA_TABLE(pidV0s, "AOD", "PIDV0S", //! Table with PID information
                   pid_studies::QtArm,
                   pid_studies::OccupancyFt0c,
                   pid_studies::OccupancyIts,
-                  pid_studies::CentralityFT0C,                  
-                  pid_studies::CentralityFT0M,                  
-                  pid_studies::CandFlag                  
-                  );
+                  pid_studies::CentralityFT0C,
+                  pid_studies::CentralityFT0M,
+                  pid_studies::CandFlag);
 
 DECLARE_SOA_TABLE(pidCascades, "AOD", "PIDCASCADES", //! Table with PID information
                   pid_studies::MassOmega,
@@ -114,20 +115,19 @@ DECLARE_SOA_TABLE(pidCascades, "AOD", "PIDCASCADES", //! Table with PID informat
                   pid_studies::NSigmaTofBachKa,
                   pid_studies::OccupancyFt0c,
                   pid_studies::OccupancyIts,
-                  pid_studies::CentralityFT0C,                  
-                  pid_studies::CentralityFT0M,                  
-                  pid_studies::CandFlag                  
-                  );
+                  pid_studies::CentralityFT0C,
+                  pid_studies::CentralityFT0M,
+                  pid_studies::CandFlag);
 } // namespace o2::aod
 
 struct pidStudies {
-  Produces <o2::aod::pidV0s> pidV0;
-  Produces <o2::aod::pidCascades> pidCascade;
+  Produces<o2::aod::pidV0s> pidV0;
+  Produces<o2::aod::pidCascades> pidCascade;
   HistogramRegistry registry{"registry", {}};
 
   using PIDTracks = soa::Join<aod::Tracks, aod::TracksExtra,
-                            aod::pidTPCFullPi, aod::pidTPCFullKa, aod::pidTPCFullPr,
-                            aod::pidTOFFullPi, aod::pidTOFFullKa, aod::pidTOFFullPr>;
+                              aod::pidTPCFullPi, aod::pidTPCFullKa, aod::pidTPCFullPr,
+                              aod::pidTOFFullPi, aod::pidTOFFullKa, aod::pidTOFFullPr>;
   using CollSels = soa::Join<aod::Collisions, aod::EvSels, aod::CentFT0Cs, aod::CentFT0Ms>;
   using V0sMCRec = soa::Join<aod::V0Datas, aod::V0CoreMCLabels>;
   using CascsMCRec = soa::Join<aod::CascDatas, aod::CascCoreMCLabels>;
@@ -180,8 +180,7 @@ struct pidStudies {
         coll.trackOccupancyInTimeRange(),
         coll.centFT0C(),
         coll.centFT0M(),
-        flag
-      );
+        flag);
     } else {
       const auto& bachTrack = candidate.template bachelor_as<PIDTracks>();
       pidCascade(
@@ -197,77 +196,71 @@ struct pidStudies {
         coll.trackOccupancyInTimeRange(),
         coll.centFT0C(),
         coll.centFT0M(),
-        flag
-      );      
+        flag);
     }
-  } 
+  }
 
   template <typename T1>
-  int isMatched(const T1& cand) {
+  int isMatched(const T1& cand)
+  {
     if constexpr (std::is_same<T1, V0sMCRec::iterator>::value) {
       if (!cand.has_v0MCCore()) {
-        return aod::pid_studies::NotMatched;
+        return aod::pid_studies::Particle::NotMatched;
       }
       auto v0MC = cand.template v0MCCore_as<aod::V0MCCores>();
-      if (v0MC.pdgCode() == kK0Short && v0MC.pdgCodeNegative() == -kPiPlus 
-          && v0MC.pdgCodePositive() == kPiPlus) {
-          return aod::pid_studies::K0s;
-      } 
-      if (v0MC.pdgCode() == -kK0Short && v0MC.pdgCodeNegative() == -kPiPlus 
-          && v0MC.pdgCodePositive() == kPiPlus) {
-          return -aod::pid_studies::K0s;
+      if (v0MC.pdgCode() == kK0Short && v0MC.pdgCodeNegative() == -kPiPlus && v0MC.pdgCodePositive() == kPiPlus) {
+        return aod::pid_studies::Particle::K0s;
       }
-      if (v0MC.pdgCode() == kLambda0 && v0MC.pdgCodeNegative() == -kPiPlus 
-          && v0MC.pdgCodePositive() == kProton) {
-          return aod::pid_studies::Lambda;
+      if (v0MC.pdgCode() == -kK0Short && v0MC.pdgCodeNegative() == -kPiPlus && v0MC.pdgCodePositive() == kPiPlus) {
+        return -aod::pid_studies::Particle::K0s;
       }
-      if (v0MC.pdgCode() == -kLambda0 && v0MC.pdgCodeNegative() == -kProton 
-          && v0MC.pdgCodePositive() == kPiPlus) {
-          return -aod::pid_studies::Lambda;
+      if (v0MC.pdgCode() == kLambda0 && v0MC.pdgCodeNegative() == -kPiPlus && v0MC.pdgCodePositive() == kProton) {
+        return aod::pid_studies::Particle::Lambda;
+      }
+      if (v0MC.pdgCode() == -kLambda0 && v0MC.pdgCodeNegative() == -kProton && v0MC.pdgCodePositive() == kPiPlus) {
+        return -aod::pid_studies::Particle::Lambda;
       }
     }
     if constexpr (std::is_same<T1, CascsMCRec::iterator>::value) {
       if (!cand.has_cascMCCore()) {
-        return aod::pid_studies::NotMatched;
+        return aod::pid_studies::Particle::NotMatched;
       }
-      auto cascMC = cand.template cascMCCore_as<aod::CascMCCores>();     
-      if (cascMC.pdgCode() > 0) {
-        if (cascMC.pdgCode() == kOmegaMinus && 
-            cascMC.pdgCodeBachelor() == -kKPlus && 
-            cascMC.pdgCodeV0() == kLambda0 &&
-            cascMC.pdgCodePositive() == kProton &&
-            cascMC.pdgCodeNegative() == -kPiPlus) {
-            return aod::pid_studies::Omega;
-        }
-      } else {
-        if (cascMC.pdgCode() == -kOmegaMinus && 
-            cascMC.pdgCodeBachelor() == kKPlus && 
-            cascMC.pdgCodeV0() == -kLambda0 &&
-            cascMC.pdgCodePositive() == kPiPlus &&
-            cascMC.pdgCodeNegative() == -kProton) {
-            return -aod::pid_studies::Omega;
-        }
+      auto cascMC = cand.template cascMCCore_as<aod::CascMCCores>();
+      if (cascMC.pdgCode() == kOmegaMinus &&
+          cascMC.pdgCodeBachelor() == -kKPlus &&
+          cascMC.pdgCodeV0() == kLambda0 &&
+          cascMC.pdgCodePositive() == kProton &&
+          cascMC.pdgCodeNegative() == -kPiPlus) {
+        return aod::pid_studies::Particle::Omega;
+      }
+      if (cascMC.pdgCode() == -kOmegaMinus &&
+          cascMC.pdgCodeBachelor() == kKPlus &&
+          cascMC.pdgCodeV0() == -kLambda0 &&
+          cascMC.pdgCodePositive() == kPiPlus &&
+          cascMC.pdgCodeNegative() == -kProton) {
+        return -aod::pid_studies::Particle::Omega;
       }
     }
-    return aod::pid_studies::NotMatched;
+    return aod::pid_studies::Particle::NotMatched;
   }
 
-  void processMC(V0sMCRec const& V0s, aod::V0MCCores const&, CascsMCRec const& cascades, 
-                 aod::CascMCCores const&, CollSels const&, PIDTracks const&)  {
+  void processMC(V0sMCRec const& V0s, aod::V0MCCores const&, CascsMCRec const& cascades,
+                 aod::CascMCCores const&, CollSels const&, PIDTracks const&)
+  {
     for (const auto& v0 : V0s) {
-      if (v0.mK0Short() > massK0Min && v0.mK0Short() < massK0Max ||
-          v0.mLambda() > massLambdaMin && v0.mLambda() < massLambdaMax ||
-          v0.mAntiLambda() > massLambdaMin && v0.mAntiLambda() < massLambdaMax) {
+      if ((v0.mK0Short() > massK0Min && v0.mK0Short() < massK0Max) ||
+          (v0.mLambda() > massLambdaMin && v0.mLambda() < massLambdaMax) ||
+          (v0.mAntiLambda() > massLambdaMin && v0.mAntiLambda() < massLambdaMax)) {
         int matched = isMatched(v0);
-        if(matched != aod::pid_studies::NotMatched) {
+        if (matched != aod::pid_studies::Particle::NotMatched) {
           fillTree<true>(v0, matched);
         }
       }
     }
     for (const auto& casc : cascades) {
       if (casc.mOmega() > massOmegaMin && casc.mOmega() < massOmegaMax) {
-        int matched = isMatched(casc); 
-        if(matched != aod::pid_studies::NotMatched) {
+        int matched = isMatched(casc);
+        if (matched != aod::pid_studies::Particle::NotMatched) {
           fillTree<false>(casc, matched);
         }
       }
@@ -278,15 +271,15 @@ struct pidStudies {
   void processData(aod::V0Datas const& V0s, aod::CascDatas const& cascades, CollSels const&, PIDTracks const&)
   {
     for (const auto& v0 : V0s) {
-      if (v0.mK0Short() > massK0Min && v0.mK0Short() < massK0Max ||
-          v0.mLambda() > massLambdaMin && v0.mLambda() < massLambdaMax ||
-          v0.mAntiLambda() > massLambdaMin && v0.mAntiLambda() < massLambdaMax) {
-        fillTree<true>(v0, aod::pid_studies::NotMatched);
+      if ((v0.mK0Short() > massK0Min && v0.mK0Short() < massK0Max) ||
+          (v0.mLambda() > massLambdaMin && v0.mLambda() < massLambdaMax) ||
+          (v0.mAntiLambda() > massLambdaMin && v0.mAntiLambda() < massLambdaMax)) {
+        fillTree<true>(v0, aod::pid_studies::Particle::NotMatched);
       }
     }
     for (const auto& casc : cascades) {
       if (casc.mOmega() > massOmegaMin && casc.mOmega() < massOmegaMax) {
-        fillTree<false>(casc, aod::pid_studies::NotMatched);
+        fillTree<false>(casc, aod::pid_studies::Particle::NotMatched);
       }
     }
   }
