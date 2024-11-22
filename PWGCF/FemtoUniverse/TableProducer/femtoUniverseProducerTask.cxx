84e1052e8df4702cab49c1b6df3022386faa66fe
Katarzyna Gwizdziel <katarzyna.gwizdziel@cern.ch>
2024-11-22 12:13:58 +0100

    PWCF:FemtoUniverse - fixing whitespaces

--- PWGCF/FemtoUniverse/TableProducer/femtoUniverseProducerTask.cxx
+++ PWGCF/FemtoUniverse/TableProducer/femtoUniverseProducerTask.cxx
@@ -777,7 +777,6 @@ struct femtoUniverseProducerTask {
     if (!colCuts.isSelected(col)) {
       return;
     }
-    
     if (!ConfIsUsePileUp) {
       if (ConfDoSpher) {
         outputCollision(vtxZ, mult, multNtr, colCuts.computeSphericity(col, tracks), mMagField);
