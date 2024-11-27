c5d758ce568801a789d7d9250358e46d87d0eedb
upasanasharma31 <84681984+upasanasharma31@users.noreply.github.com>
2024-11-27 15:48:33 +0530

    Update qaEfficiency.cxx

--- DPG/Tasks/AOTTrack/qaEfficiency.cxx
+++ DPG/Tasks/AOTTrack/qaEfficiency.cxx
@@ -333,7 +333,7 @@ struct QaEfficiency {
                                   phiMin, phiMax,
                                   yMin, yMax);
     const int histogramIndex = id + pdgSign * nSpecies;
-    hPtmotherGenerated = histos.add<TH1>("MC/mother/pt/generated","Generated pT of mother Lambda or Xi ",+ tagPt, kTH1D, {axisPt}); 
+    hPtmotherGenerated = histos.add<TH1>("MC/mother/pt/generated", "Generated pT of mother Lambda or Xi ", +tagPt, kTH1D, {axisPt}); 
    
     // Pt
     hPtIts[histogramIndex] = histos.add<TH1>(Form("MC/pdg%i/pt/its", PDGs[histogramIndex]), "ITS tracks " + tagPt, kTH1D, {axisPt});
