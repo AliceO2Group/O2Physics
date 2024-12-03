6498c28dde2c7f62fc6ce62ca1a8ee5368e60d87
victorvalenciatorres <valencia@subatech.in2p3.fr>
2024-12-03 23:59:24 +0100

    Adding include inside HistogramsLibrary.h

--- PWGDQ/Core/HistogramsLibrary.cxx
+++ PWGDQ/Core/HistogramsLibrary.cxx
@@ -15,9 +15,6 @@
 #include "VarManager.h"
 #include "CommonConstants/MathConstants.h"
 
-using namespace o2::framework;
-using namespace o2::framework::expressions;
-
 void o2::aod::dqhistograms::DefineHistograms(HistogramManager* hm, const char* histClass, const char* groupName, const char* subGroupName)
 {
   //
