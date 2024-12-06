de16a977926f0076a0b086d966c782b14f35d196
Chiara Oppedisano <chiara.oppedisano@to.infn.it>
2024-12-06 15:31:18 +0100

    coding convention

--- Common/DataModel/ZDCInterCalib.h
+++ Common/DataModel/ZDCInterCalib.h
@@ -20,18 +20,18 @@ namespace o2::aod
 {
 namespace znoutput
 {
-DECLARE_SOA_COLUMN(ZNAtdc, znatdc, float);     //! ZNA tdc value
-DECLARE_SOA_COLUMN(ZNCtdc, znctdc, float);     //! ZNC tdc value
+DECLARE_SOA_COLUMN(ZNAtdc, znatdc, float); //! ZNA tdc value
+DECLARE_SOA_COLUMN(ZNCtdc, znctdc, float); //! ZNC tdc value
 DECLARE_SOA_COLUMN(ZNApmc, znapmc, float); //! PMC ZNA
-DECLARE_SOA_COLUMN(ZNApm1, znapm1, float);      //! PM1 ZNA
-DECLARE_SOA_COLUMN(ZNApm2, znapm2, float);      //! PM2 ZNA
-DECLARE_SOA_COLUMN(ZNApm3, znapm3, float);      //! PM3 ZNA
-DECLARE_SOA_COLUMN(ZNApm4, znapm4, float);      //! PM4 ZNA
+DECLARE_SOA_COLUMN(ZNApm1, znapm1, float); //! PM1 ZNA
+DECLARE_SOA_COLUMN(ZNApm2, znapm2, float); //! PM2 ZNA
+DECLARE_SOA_COLUMN(ZNApm3, znapm3, float); //! PM3 ZNA
+DECLARE_SOA_COLUMN(ZNApm4, znapm4, float); //! PM4 ZNA
 DECLARE_SOA_COLUMN(ZNCpmc, zncpmc, float); //! PMC ZNC
-DECLARE_SOA_COLUMN(ZNCpm1, zncpm1, float);      //! PM1 ZNC
-DECLARE_SOA_COLUMN(ZNCpm2, zncpm2, float);      //! PM2 ZNC
-DECLARE_SOA_COLUMN(ZNCpm3, zncpm3, float);      //! PM3 ZNC
-DECLARE_SOA_COLUMN(ZNCpm4, zncpm4, float);      //! PM4 ZNC
+DECLARE_SOA_COLUMN(ZNCpm1, zncpm1, float); //! PM1 ZNC
+DECLARE_SOA_COLUMN(ZNCpm2, zncpm2, float); //! PM2 ZNC
+DECLARE_SOA_COLUMN(ZNCpm3, zncpm3, float); //! PM3 ZNC
+DECLARE_SOA_COLUMN(ZNCpm4, zncpm4, float); //! PM4 ZNC
 
 } // namespace znoutput
 
