9b95710eee2bb9b7fa4b57721cb6abcdea6479a5
Tanu Gahlaut <154991749+TGahlaut1@users.noreply.github.com>
2024-12-07 02:55:34 +0100

    Fixed O2 linter issues

--- PWGCF/EbyEFluctuations/Tasks/meanPtFlucId.cxx
+++ PWGCF/EbyEFluctuations/Tasks/meanPtFlucId.cxx
@@ -9,7 +9,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file MeanPtFlucIdentified.cxx
+/// \file meanPtFlucId.cxx
 /// \brief Calculate EbyE <pt> fluctuations with cumulant method.
 ///        For charged particles and identified particles.
 ///        For RUN-3
