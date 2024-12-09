97663f2cc6d43f06625a324a073f7543e44dd229
Zhenjun Xiong <108917659+zjxiongOvO@users.noreply.github.com>
2024-12-10 00:49:04 +0800

    Update ReducedInfoTables.h

--- PWGDQ/DataModel/ReducedInfoTables.h
+++ PWGDQ/DataModel/ReducedInfoTables.h
@@ -43,17 +43,6 @@ namespace reducedevent
 {
 
 // basic event information
-DECLARE_SOA_INDEX_COLUMN(Collision, collision);                  //!
-DECLARE_SOA_BITMAP_COLUMN(Tag, tag, 64);                         //!  Bit-field for storing event information (e.g. high level info, cut decisions)
-DECLARE_SOA_COLUMN(MCPosX, mcPosX, float);                       //!  MC event position X
-DECLARE_SOA_COLUMN(MCPosY, mcPosY, float);                       //!  MC event position Y
-DECLARE_SOA_COLUMN(MCPosZ, mcPosZ, float);                       //!  MC event position Z
-DECLARE_SOA_COLUMN(NTPCpileupContribA, nTPCpileupContribA, int); //!  Number of TPC pileup tracks on A side
-DECLARE_SOA_COLUMN(NTPCpileupContribC, nTPCpileupContribC, int); //!  Number of TPC pileup tracks on C side
-DECLARE_SOA_COLUMN(NTPCpileupZA, nTPCpileupZA, float);           //!  Median Z position of pileup tracks on A side
-DECLARE_SOA_COLUMN(NTPCpileupZC, nTPCpileupZC, float);           //!  Median Z position of pileup tracks on C side
-DECLARE_SOA_COLUMN(NTPCtracksInPast, nTPCtracksInPast, int);     //!  Number of TPC tracks in the past events (configurable, but e.g. one drift time)
-DECLARE_SOA_COLUMN(NTPCtracksInFuture, nTPCtracksInFuture, int); //!  Number of TPC tracks in the future events (configurable, but e.g. one drift time)
 DECLARE_SOA_INDEX_COLUMN(Collision, collision); //!
 DECLARE_SOA_BITMAP_COLUMN(Tag, tag, 64);       //!  Bit-field for storing event information (e.g. high level info, cut decisions)
 DECLARE_SOA_COLUMN(MCPosX, mcPosX, float);     //!  MC event position X
