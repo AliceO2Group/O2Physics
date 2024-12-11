4ffef41009440f5a8158c2d49e352f7db58e3495
Vít Kučera <vit.kucera@cern.ch>
2024-12-11 13:37:41 +0100

    [PWGLF] Fix compilation warnings (#8921)

--- PWGLF/TableProducer/Resonances/resonanceInitializer.cxx
+++ PWGLF/TableProducer/Resonances/resonanceInitializer.cxx
@@ -1042,7 +1042,7 @@ struct ResonanceInitializer {
     LOGF(info, "Bz set to %f for run: ", dBz, mRunNumber);
   }
 
-  void processDummy(ResoRun2Events const& collisions)
+  void processDummy(ResoRun2Events const& /*collisions*/)
   {
   }
   PROCESS_SWITCH(ResonanceInitializer, processDummy, "Process for dummy", true);
