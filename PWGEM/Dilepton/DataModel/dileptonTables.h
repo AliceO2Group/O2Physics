985c34347b02a90d390276cebe6b1f060d7dec26
Daiki Sekihata <daiki.sekihata@cern.ch>
2024-12-17 16:36:28 +0900

    [PWGEM/Dilepton] add cent info in norm table (#9015)

--- PWGEM/Dilepton/DataModel/dileptonTables.h
+++ PWGEM/Dilepton/DataModel/dileptonTables.h
@@ -212,7 +212,7 @@ DECLARE_SOA_TABLE(EMEoIs, "AOD", "EMEOI", //! joinable to aod::Collisions in cre
 using EMEoI = EMEoIs::iterator;
 
 DECLARE_SOA_TABLE(EMEventNormInfos, "AOD", "EMEVENTNORMINFO", //! event information for normalization
-                  o2::soa::Index<>, evsel::Alias, evsel::Selection, emevent::PosZint16, emevent::PosZ<emevent::PosZint16>, emevent::Sel8<evsel::Selection>);
+                  o2::soa::Index<>, evsel::Alias, evsel::Selection, emevent::PosZint16, cent::CentFT0C, emevent::PosZ<emevent::PosZint16>, emevent::Sel8<evsel::Selection>);
 using EMEventNormInfo = EMEventNormInfos::iterator;
 
 namespace emmcevent
