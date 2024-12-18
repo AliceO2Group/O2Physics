4f9e3f8afeb9aa3603598777964abedd48195536
nzardosh <nima.zardoshti@cern.ch>
2024-12-18 17:10:59 +0100

    [PWGJE] adding bplusSels table to dummy tables (#9048)

--- PWGJE/TableProducer/derivedDataProducerDummyD0.cxx
+++ PWGJE/TableProducer/derivedDataProducerDummyD0.cxx
@@ -44,6 +44,7 @@ struct JetDerivedDataProducerDummyD0Task {
   Produces<aod::HfBplusPars> bplusParsTable;
   Produces<aod::HfBplusParEs> bplusParExtrasTable;
   Produces<aod::HfBplusParD0s> bplusParD0sTable;
+  Produces<aod::HfBplusSels> bplusSelsTable;
   Produces<aod::HfBplusMls> bplusMlsTable;
   Produces<aod::HfBplusMlD0s> bplusMlD0sTable;
   Produces<aod::HfBplusMcs> bplusMcsTable;
