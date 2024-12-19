07977b7ba4d935c8cf380edb44ab01308fc12536
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2024-12-19 20:22:42 +0100

    Update run2bcinfosConverter.cxx

--- Common/TableProducer/Converters/run2bcinfosConverter.cxx
+++ Common/TableProducer/Converters/run2bcinfosConverter.cxx
@@ -9,16 +9,6 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file tracksExtraConverter.cxx
-/// \brief Converts TracksExtra table from version 000 to 001
-
-/// This task allows for the conversion of the TracksExtra table from version 000 to 001.
-/// The conversion is needed because the table has been extended with the ITSClusterSize column
-/// and the ITSClusterMap column is evaluated dynamically from it.
-/// In the converter a dummy ITSClusterSize column is filled with overflows if a hit in the layer is present
-
-/// \author F.Mazzaschi <fmazzasc@cern.ch>
-
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
@@ -27,18 +17,18 @@ using namespace o2;
 using namespace o2::framework;
 
 struct Run2BCInfosConverter {
-  Produces<aod::Run2TrackExtras_001> Run2TrackExtras_001;
+  Produces<aod::Run2BCInfos_001> Run2BCInfos_001;
   void process(aod::Run2BCInfos_000 const& Run2BCInfos_000)
   {
 
     for (const auto& entry : Run2BCInfos_000) {
-      Run2TrackExtras_001(entry.eventCuts(),
-                          entry.triggerMaskNext50(), entry.l0TriggerInputMask(),
-                          entry.spdClustersL0(), entry.spdClustersL1(),
-                          entry.spdFiredChipsL0(), entry.spdFiredChipsL1(),
-                          entry.spdFiredFastOrL0(), entry.spdFiredFastOrL1(),
-                          entry.v0TriggerChargeA(), entry.v0TriggerChargeC(),
-                          0, 0);
+      Run2BCInfos_001(entry.eventCuts(),
+                      entry.triggerMaskNext50(), entry.l0TriggerInputMask(),
+                      entry.spdClustersL0(), entry.spdClustersL1(),
+                      entry.spdFiredChipsL0(), entry.spdFiredChipsL1(),
+                      entry.spdFiredFastOrL0(), entry.spdFiredFastOrL1(),
+                      entry.v0TriggerChargeA(), entry.v0TriggerChargeC(),
+                      0, 0);
     }
   }
 };
