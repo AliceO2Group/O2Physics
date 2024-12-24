0ffc9ed5bd6d8a1cb8f460085c19542417e32a8c
Josue-MiniPC <martinezgarciajosue54@gmail.com>
2024-12-23 23:50:02 -0600

    new changes

--- PWGMM/Lumi/Tasks/lumiStability.cxx
+++ PWGMM/Lumi/Tasks/lumiStability.cxx
@@ -1,3 +1,9 @@
+/**
+ * \file lumiStability.cxx
+ * \brief Analysis over BCs to study the luminosity stability along time.
+ * \author josuem@cern.ch
+ */
+
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
@@ -8,10 +14,6 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-
-// \file lumiStability.cxx
-// \brief Analysis over BCs to study the luminosity stability along time.
-// \author josuem@cern.ch
 
 #include <utility>
 #include <map>
