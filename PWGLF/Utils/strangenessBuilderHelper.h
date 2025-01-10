eb16bb283b0fb1927c32d0bcb266b7902e029b55
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2025-01-10 13:37:13 +0100

    Move all to header, do not compile lib

--- PWGLF/Utils/strangenessBuilderHelper.h
+++ PWGLF/Utils/strangenessBuilderHelper.h
@@ -17,6 +17,10 @@
 #include <array>
 #include "DCAFitter/DCAFitterN.h"
 #include "Framework/AnalysisDataModel.h"
+#include "ReconstructionDataFormats/Track.h"
+#include "DetectorsBase/GeometryManager.h"
+#include "CommonConstants/PhysicsConstants.h"
+#include "Common/Core/trackUtilities.h"
 
 namespace o2
 {
@@ -108,13 +112,108 @@ struct cascadeCandidate {
 class strangenessBuilderHelper
 {
   public:
-    strangenessBuilderHelper();
+    strangenessBuilderHelper(){
+      // standards hardcoded in builder ...
+      // ...but can be changed easily since fitter is public 
+      fitter.setPropagateToPCA(true);
+      fitter.setMaxR(200.);
+      fitter.setMinParamChange(1e-3);
+      fitter.setMinRelChi2Change(0.9);
+      fitter.setMaxDZIni(1e9);
+      fitter.setMaxDXYIni(4.0f);
+      fitter.setMaxChi2(1e9);
+      fitter.setUseAbsDCA(true);
+      fitter.setWeightedFinalPCA(false);
+
+      // LUT has to be loaded later
+      lut = nullptr;
+      fitter.setMatCorrType(o2::base::Propagator::MatCorrType::USEMatCorrLUT);
+
+      // mag field has to be set later
+      fitter.setBz(-999.9f); // will NOT make sense if not changed
+    };
     
     template <typename TTrack>
     bool buildV0Candidate(o2::aod::Collision const& collision,
                           TTrack const& positiveTrack, 
                           TTrack const& negativeTrack, 
-                          bool useCollinearFit = false);
+                          bool useCollinearFit){
+      // Calculate DCA with respect to the collision associated to the V0, not individual tracks
+      gpu::gpustd::array<float, 2> dcaInfo;
+
+      auto posTrackPar = getTrackPar(positiveTrack);
+      o2::base::Propagator::Instance()->propagateToDCABxByBz({collision.posX(), collision.posY(), collision.posZ()}, posTrackPar, 2.f, fitter.getMatCorrType(), &dcaInfo);
+      v0.positiveDCAxy = dcaInfo[0];
+
+      auto negTrackPar = getTrackPar(negativeTrack);
+      o2::base::Propagator::Instance()->propagateToDCABxByBz({collision.posX(), collision.posY(), collision.posZ()}, negTrackPar, 2.f, fitter.getMatCorrType(), &dcaInfo);
+      v0.negativeDCAxy = dcaInfo[0];
+
+      o2::track::TrackParCov positiveTrackParam = getTrackParCov(positiveTrack);
+      o2::track::TrackParCov negativeTrackParam = getTrackParCov(negativeTrack);
+
+      // Perform DCA fit
+      int nCand = 0;
+      fitter.setCollinear(useCollinearFit);
+      try {
+        nCand = fitter.process(positiveTrackParam, negativeTrackParam);
+      } catch (...) {
+        return false;
+      }
+      if (nCand == 0) {
+        return false;
+      }
+
+      v0.positiveTrackX = fitter.getTrack(0).getX();
+      v0.negativeTrackX = fitter.getTrack(1).getX();
+      positiveTrackParam = fitter.getTrack(0);
+      negativeTrackParam = fitter.getTrack(1);
+      positiveTrackParam.getPxPyPzGlo(v0.positiveMomentum);
+      negativeTrackParam.getPxPyPzGlo(v0.negativeMomentum);
+      positiveTrackParam.getXYZGlo(v0.positivePosition);
+      negativeTrackParam.getXYZGlo(v0.negativePosition);
+
+      // get decay vertex coordinates
+      const auto& vtx = fitter.getPCACandidate();
+      for (int i = 0; i < 3; i++) {
+        v0.position[i] = vtx[i];
+      }
+
+      v0.daughterDCA = TMath::Sqrt(fitter.getChi2AtPCACandidate());
+      v0.pointingAngle = TMath::ACos(RecoDecay::cpa(
+        std::array{collision.posX(), collision.posY(), collision.posZ()}, 
+        std::array{v0.position[0], v0.position[1], v0.position[2]}, 
+        std::array{v0.positiveMomentum[0] + v0.negativeMomentum[0], v0.positiveMomentum[1] + v0.negativeMomentum[1], v0.positiveMomentum[2] + v0.negativeMomentum[2]}
+      ));
+
+      v0.dcaXY = CalculateDCAStraightToPV(
+        v0.position[0], v0.position[1], v0.position[2],
+        v0.positiveMomentum[0] + v0.negativeMomentum[0],
+        v0.positiveMomentum[1] + v0.negativeMomentum[1],
+        v0.positiveMomentum[2] + v0.negativeMomentum[2],
+        collision.posX(), collision.posY(), collision.posZ());
+
+      // Calculate masses
+      v0.massGamma = RecoDecay::m(std::array{
+        std::array{v0.positiveMomentum[0], v0.positiveMomentum[1], v0.positiveMomentum[2]}, 
+        std::array{v0.negativeMomentum[0], v0.negativeMomentum[1], v0.negativeMomentum[2]}}, 
+        std::array{o2::constants::physics::MassElectron, o2::constants::physics::MassElectron});
+      v0.massK0Short = RecoDecay::m(std::array{
+        std::array{v0.positiveMomentum[0], v0.positiveMomentum[1], v0.positiveMomentum[2]}, 
+        std::array{v0.negativeMomentum[0], v0.negativeMomentum[1], v0.negativeMomentum[2]}}, 
+        std::array{o2::constants::physics::MassPionCharged, o2::constants::physics::MassPionCharged});
+      v0.massLambda = RecoDecay::m(std::array{
+        std::array{v0.positiveMomentum[0], v0.positiveMomentum[1], v0.positiveMomentum[2]}, 
+        std::array{v0.negativeMomentum[0], v0.negativeMomentum[1], v0.negativeMomentum[2]}}, 
+        std::array{o2::constants::physics::MassProton, o2::constants::physics::MassPionCharged});
+      v0.massAntiLambda = RecoDecay::m(std::array{
+        std::array{v0.positiveMomentum[0], v0.positiveMomentum[1], v0.positiveMomentum[2]}, 
+        std::array{v0.negativeMomentum[0], v0.negativeMomentum[1], v0.negativeMomentum[2]}}, 
+        std::array{o2::constants::physics::MassPionCharged, o2::constants::physics::MassProton});
+
+      // information validated, V0 built successfully. Signal OK
+      return true;   
+    }
 
     o2::base::MatLayerCylSet* lut; // material LUT for DCA fitter
     o2::vertexing::DCAFitterN<2> fitter; // 2-prong o2 dca fitter
@@ -124,7 +223,9 @@ class strangenessBuilderHelper
 
   private: 
     // internal helper to calculate DCAxy of a straight line to a given PV analytically
-    float CalculateDCAStraightToPV(float X, float Y, float Z, float Px, float Py, float Pz, float pvX, float pvY, float pvZ);
+    float CalculateDCAStraightToPV(float X, float Y, float Z, float Px, float Py, float Pz, float pvX, float pvY, float pvZ){
+      return std::sqrt((std::pow((pvY - Y) * Pz - (pvZ - Z) * Py, 2) + std::pow((pvX - X) * Pz - (pvZ - Z) * Px, 2) + std::pow((pvX - X) * Py - (pvY - Y) * Px, 2)) / (Px * Px + Py * Py + Pz * Pz));
+    }
 };
 
 } // namespace pwglf
@@ -132,18 +233,17 @@ class strangenessBuilderHelper
 
 // specific specializations that are foreseen
 // Run 3 case: tracks at IU
-extern template<o2::soa::Join<o2::aod::TracksIU, o2::aod::TracksExtra, o2::aod::TracksCovIU>::iterator>
-bool o2::pwglf::strangenessBuilderHelper::buildV0Candidate(o2::aod::Collision const& collision,
-                                                           o2::soa::Join<o2::aod::TracksIU, o2::aod::TracksExtra, o2::aod::TracksCovIU>::iterator const& positiveTrack, 
-                                                           o2::soa::Join<o2::aod::TracksIU, o2::aod::TracksExtra, o2::aod::TracksCovIU>::iterator const& negativeTrack, 
-                                                           bool useCollinearFit);
+extern template
+bool o2::pwglf::strangenessBuilderHelper::buildV0Candidate<o2::soa::Join<o2::aod::TracksIU, o2::aod::TracksExtra, o2::aod::TracksCovIU>::iterator>(o2::aod::Collision const& collision,
+                                                                                                                                                   o2::soa::Join<o2::aod::TracksIU, o2::aod::TracksExtra, o2::aod::TracksCovIU>::iterator const& positiveTrack, 
+                                                                                                                                                   o2::soa::Join<o2::aod::TracksIU, o2::aod::TracksExtra, o2::aod::TracksCovIU>::iterator const& negativeTrack, 
+                                                                                                                                                   bool useCollinearFit);
 
 // Run 2 case: tracks at PV
-extern template<o2::soa::Join<o2::aod::Tracks, o2::aod::TracksExtra, o2::aod::TracksCov>::iterator>
-bool o2::pwglf::strangenessBuilderHelper::buildV0Candidate(o2::aod::Collision const& collision,
-                                                           o2::soa::Join<o2::aod::Tracks, o2::aod::TracksExtra, o2::aod::TracksCov>::iterator const& positiveTrack, 
-                                                           o2::soa::Join<o2::aod::Tracks, o2::aod::TracksExtra, o2::aod::TracksCov>::iterator const& negativeTrack, 
-                                                           bool useCollinearFit);
-
+extern template
+bool o2::pwglf::strangenessBuilderHelper::buildV0Candidate<o2::soa::Join<o2::aod::Tracks, o2::aod::TracksExtra, o2::aod::TracksCov>::iterator>(o2::aod::Collision const& collision,
+                                                                                                                                               o2::soa::Join<o2::aod::Tracks, o2::aod::TracksExtra, o2::aod::TracksCov>::iterator const& positiveTrack, 
+                                                                                                                                               o2::soa::Join<o2::aod::Tracks, o2::aod::TracksExtra, o2::aod::TracksCov>::iterator const& negativeTrack, 
+                                                                                                                                               bool useCollinearFit);
 
 #endif // PWGLF_UTILS_STRANGENESSBUILDERHELPER_H_
