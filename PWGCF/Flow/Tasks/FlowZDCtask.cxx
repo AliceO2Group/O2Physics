492fe4d18e03a6115c47fb14ab1a1bcbcdc9907f
Sabrina Hernandez <sabrina.gabrielle.hernandez@cern.ch>
2025-01-15 15:07:14 -0600

    Fixed linter warnings for FlowZDCtask

--- PWGCF/Flow/Tasks/FlowZDCtask.cxx
+++ PWGCF/Flow/Tasks/FlowZDCtask.cxx
@@ -64,7 +64,7 @@ struct FlowZDCtask {
 
   Configurable<int> nBinsPt{"nBinsPt", 100, "N bins in pT histo"};
   Configurable<int> eventSelection{"eventSelection", 1, "event selection"};
-  Configurable<float> maxZp{"maxZP", 3099.5, "Max ZP signal"};
+  Configurable<float> maxZp{"maxZp", 3099.5, "Max ZP signal"};
   Configurable<float> vtxCut{"vtxCut", 10.0, "Z vertex cut"};
   Configurable<float> etaCut{"etaCut", 0.8, "Eta cut"};
   Configurable<float> etaGap{"etaGap", 0.5, "Eta gap"};
@@ -87,7 +87,7 @@ struct FlowZDCtask {
   ConfigurableAxis axisPt{"axisPt", {VARIABLE_WIDTH, 0.2, 0.25, 0.30, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00, 1.10, 1.20, 1.30, 1.40, 1.50, 1.60, 1.70, 1.80, 1.90, 2.00, 2.20, 2.40, 2.60, 2.80, 3.00}, "pt axis for histograms"};
   ConfigurableAxis axisMultiplicity{"axisMultiplicity", {3500, 0, 3500}, "centrality axis for histograms"};
   ConfigurableAxis axisEnergy{"axisEnergy", {100, 0, 700}, "energy axis for zdc histos"};
-  ConfigurableAxis axisMultTpc{"axisaxisMultTPC", {1000, -0.5f, 1999.5f}, "TPCmultiplicity"};
+  ConfigurableAxis axisMultTpc{"axisMultTpc", {1000, -0.5f, 1999.5f}, "TPCmultiplicity"};
   ConfigurableAxis axisZN{"axisZN", {5000, 0, 500}, "axisZN"};
   ConfigurableAxis axisZP{"axisZP", {5000, 0, 500}, "axisZP"};
   ConfigurableAxis axisFT0CAmp{"axisFT0CAmp", {60000, 0, 60000}, "axisFT0CAmp"};
