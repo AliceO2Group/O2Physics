733989ad4b94d1fdc3d4b115744382848430e520
Luigi Dello Stritto <ldellost@alicecerno2.cern.ch>
2025-01-15 16:53:16 +0100

    Apply event selection before filling the hGenCollisionCent histo

--- PWGHF/Utils/utilsEvSelHf.h
+++ PWGHF/Utils/utilsEvSelHf.h
@@ -430,10 +430,14 @@ struct HfEventSelectionMc {
   template <o2::hf_centrality::CentralityEstimator centEstimator, typename Coll>
   void fillHistograms(Coll const& mcCollision, const uint16_t rejectionMask)
   {
+    hParticles->Fill(EventRejection::None);
+
     if constexpr (centEstimator == o2::hf_centrality::CentralityEstimator::FT0M) {
+      if (TESTBIT(rejectionMask, EventRejection::TimeFrameBorderCut) || TESTBIT(rejectionMask, EventRejection::ItsRofBorderCut) || TESTBIT(rejectionMask, EventRejection::PositionZ)) {
+        return;
+      }
       hGenCollisionsCent->Fill(mcCollision.centFT0M());
     }
-    hParticles->Fill(EventRejection::None);
 
     for (std::size_t reason = 1; reason < EventRejection::NEventRejection; reason++) {
       if (TESTBIT(rejectionMask, reason)) {
