a35c90bbdbc97f4976b24b8b97d43773a80283a5
Gyula Bencedi <gyula.bencedi@cern.ch>
2025-01-17 20:21:32 +0100

    Update dndetaMFTPbPb.cxx
    new try to fix copyright header

--- PWGMM/Mult/Tasks/dndetaMFTPbPb.cxx
+++ PWGMM/Mult/Tasks/dndetaMFTPbPb.cxx
@@ -1,6 +1,6 @@
-// Copyright 2020-2022 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright
-// holders. All rights not expressly granted are reserved.
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
@@ -8,6 +8,7 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
+//
 ///
 /// \brief  Task for calculating dNdeta in Pb-Pb collisions using MFT detector
 /// \author Gyula Bencedi, gyula.bencedi@cern.ch
