48de47b8a1371a755a56706a8894dafe87a846c2
mherzer <96999709+mherzer28@users.noreply.github.com>
2025-01-20 14:24:44 +0100

    Update copyright trHeAnalysis.cxx

--- PWGLF/TableProducer/Nuspex/trHeAnalysis.cxx
+++ PWGLF/TableProducer/Nuspex/trHeAnalysis.cxx
@@ -1,6 +1,6 @@
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright
-// holders. All rights not expressly granted are reserved.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
@@ -8,12 +8,14 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
+
 ///
 /// \file trHeAnalysis.cxx
+///
 /// \brief Triton and Helion Analysis on pp Data
-/// \author Matthias Herzer <matthias.herzer@cern.ch>, Goethe University
-/// Frankfurt
-
+///
+/// \author Matthias Herzer <matthias.herzer@cern.ch>, Goethe University Frankfurt
+///
 #include "Common/CCDB/EventSelectionParams.h"
 #include "Common/Core/PID/TPCPIDResponse.h"
 #include "Common/Core/trackUtilities.h"
