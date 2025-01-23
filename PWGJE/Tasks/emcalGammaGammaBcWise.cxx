d8d763db128f9b502e8e89162610a604571be291
Nicolas Strangmann <nicolas.strangmann@.cern.ch>
2025-01-23 11:58:08 +0100

    Make MegaLinter happy

--- PWGJE/Tasks/emcalGammaGammaBcWise.cxx
+++ PWGJE/Tasks/emcalGammaGammaBcWise.cxx
@@ -27,8 +27,6 @@
 #include "Common/DataModel/EventSelection.h"
 #include "PWGJE/DataModel/EMCALClusters.h"
 
-#include "DataFormatsFT0/Digit.h"
-
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
