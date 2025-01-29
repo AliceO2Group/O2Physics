e838e17e35331dfcf2b89e1b9b0c4cc5fc34ca61
sofiatomassini <122356048+sofiatomassini@users.noreply.github.com>
2025-01-29 16:35:37 +0100

    STS: adding tables correclty

--- PWGCF/Femto3D/Tasks/femto3dPairTask.cxx
+++ PWGCF/Femto3D/Tasks/femto3dPairTask.cxx
@@ -121,7 +121,7 @@ struct FemtoCorrelations {
   std::pair<int, std::vector<float>> TOFcuts_2;
 
   using FilteredCollisions = soa::Join<aod::SingleCollSels, aod::SingleCollExtras>;
-  using FilteredTracks = soa::Join<aod::SingleTrackSels, aod::SinglePIDPis, aod::SinglePIDKas, aod::SinglePIDPrs, aod::SinglePIDDes>;
+  using FilteredTracks = soa::Join<aod::SingleTrackSels, aod::SinglePIDPis, aod::SinglePIDKas, aod::SinglePIDPrs, aod::SinglePIDDes, aod::SinglePIDTrs, aod::SinglePIDHes>;
 
   typedef std::shared_ptr<soa::Filtered<FilteredTracks>::iterator> trkType;
   typedef std::shared_ptr<soa::Filtered<FilteredCollisions>::iterator> colType;
