49c41171d95c4e2916841bbbe4ca8da80089768f
nzardosh <nima.zardoshti@cern.ch>
2025-01-31 17:24:57 +0100

    [PWGHF] Add McGenOnly process function to the init check in derived data creators (#9648)

--- PWGHF/TableProducer/derivedDataCreatorLcToPKPi.cxx
+++ PWGHF/TableProducer/derivedDataCreatorLcToPKPi.cxx
@@ -108,7 +108,7 @@ struct HfDerivedDataCreatorLcToPKPi {
 
   void init(InitContext const&)
   {
-    std::array<bool, 8> doprocess{doprocessData, doprocessMcSig, doprocessMcBkg, doprocessMcAll, doprocessDataMl, doprocessMcMlSig, doprocessMcMlBkg, doprocessMcMlAll};
+    std::array<bool, 9> doprocess{doprocessData, doprocessMcSig, doprocessMcBkg, doprocessMcAll, doprocessDataMl, doprocessMcMlSig, doprocessMcMlBkg, doprocessMcMlAll, doprocessMcGenOnly};
     if (std::accumulate(doprocess.begin(), doprocess.end(), 0) != 1) {
       LOGP(fatal, "Only one process function can be enabled at a time.");
     }
