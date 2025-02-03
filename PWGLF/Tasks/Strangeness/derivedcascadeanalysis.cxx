881fb6d30ed7eafc3a38072a8d0a92649757512c
Lucia Anna Tarasovicova <husovalucia@gmail.com>
2025-02-03 14:50:30 +0100

    [PWGLF] adding the new centrality estimators (#9605)
    Co-authored-by: Lucia Anna Tarasovicova <lucia.anna.husova@cern.ch>
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

--- PWGLF/Tasks/Strangeness/derivedcascadeanalysis.cxx
+++ PWGLF/Tasks/Strangeness/derivedcascadeanalysis.cxx
@@ -74,7 +74,9 @@ struct Derivedcascadeanalysis {
   ConfigurableAxis axisMass{"axisMass", {200, 1.222f, 1.422f}, "range of invariant mass, in case of omega take 1.572f, 1.772f"};
 
   Configurable<bool> isXi{"isXi", 1, "Apply cuts for Xi identification"};
-  Configurable<bool> usePbPbCentrality{"usePbPbCentrality", 0, "If true, use centFt0C, else use centFT0M"};
+  Configurable<bool> useCentralityFT0M{"useCentralityFT0M", 0, "If true, use centFT0M"};
+  Configurable<bool> useCentralityFT0A{"useCentralityFT0A", 0, "If true, use centFT0A"};
+  Configurable<bool> useCentralityFT0Cvar1{"useCentralityFT0Cvar1", 0, "If true, use centFT0FT0Cvar1"};
   Configurable<int> minOccupancy{"minOccupancy", -1, "Minimal occupancy"};
   Configurable<int> maxOccupancy{"maxOccupancy", -1, "Maximal occupancy"};
   Configurable<float> minOccupancyFT0{"minOccupancyFT0", -1, "Minimal occupancy"};
@@ -375,6 +377,11 @@ struct Derivedcascadeanalysis {
       histos.add("h2dGenOmegaMinusVsCentOccupancy", "h2dGenOmegaMinusVsCentOccupancy", kTH3D, {axisPt, {101, 0.0f, 101.0f}, axisOccupancy});
       histos.add("h2dGenOmegaPlusVsCentOccupancy", "h2dGenOmegaPlusVsCentOccupancy", kTH3D, {axisPt, {101, 0.0f, 101.0f}, axisOccupancy});
 
+      histos.add("h2dGenXiMinusVsNchVsOccupancy", "h2dGenXiMinusVsNchVsOccupancy", kTH3D, {axisPt, axisNch, axisOccupancy});
+      histos.add("h2dGenXiPlusVsNchVsOccupancy", "h2dGenXiPlusVsNchVsOccupancy", kTH3D, {axisPt, axisNch, axisOccupancy});
+      histos.add("h2dGenOmegaMinusVsNchVsOccupancy", "h2dGenOmegaMinusVsNchVsOccupancy", kTH3D, {axisPt, axisNch, axisOccupancy});
+      histos.add("h2dGenOmegaPlusVsNchVsOccupancy", "h2dGenOmegaPlusVsNchVsOccupancy", kTH3D, {axisPt, axisNch, axisOccupancy});
+
       histos.add("h2dGenXiMinusVsMultMC", "h2dGenXiMinusVsMultMC", kTH2D, {axisNch, axisPt});
       histos.add("h2dGenXiPlusVsMultMC", "h2dGenXiPlusVsMultMC", kTH2D, {axisNch, axisPt});
       histos.add("h2dGenOmegaMinusVsMultMC", "h2dGenOmegaMinusVsMultMC", kTH2D, {axisNch, axisPt});
@@ -426,8 +433,12 @@ struct Derivedcascadeanalysis {
     if (fillHists)
       histos.fill(HIST("hEventSelection"), 0.5 /* all collisions */);
     float centrality = coll.centFT0C();
-    if (!usePbPbCentrality)
+    if (useCentralityFT0M)
       centrality = coll.centFT0M();
+    if (useCentralityFT0A)
+      centrality = coll.centFV0A();
+    if (useCentralityFT0Cvar1)
+      centrality = coll.centFT0CVariant1();
 
     if (qaFlags.doBefSelEventMultCorr) {
       histos.fill(HIST("hEventNchCorrelationBefCuts"), coll.multNTracksPVeta1(), coll.multNTracksGlobal());
@@ -743,8 +754,12 @@ struct Derivedcascadeanalysis {
       return;
 
     float centrality = coll.centFT0C();
-    if (!usePbPbCentrality)
+    if (useCentralityFT0M)
       centrality = coll.centFT0M();
+    if (useCentralityFT0A)
+      centrality = coll.centFV0A();
+    if (useCentralityFT0Cvar1)
+      centrality = coll.centFT0CVariant1();
 
     for (const auto& casc : Cascades) {
 
@@ -1033,8 +1048,13 @@ struct Derivedcascadeanalysis {
     if (!isEventAccepted(coll, true))
       return;
     float centrality = coll.centFT0C();
-    if (!usePbPbCentrality)
+    if (useCentralityFT0M)
       centrality = coll.centFT0M();
+    if (useCentralityFT0A)
+      centrality = coll.centFV0A();
+    if (useCentralityFT0Cvar1)
+      centrality = coll.centFT0CVariant1();
+
     float nChEta05 = coll.multNTracksPVeta1();
     for (const auto& casc : Cascades) {
       float mass = -1;
@@ -1375,8 +1395,12 @@ struct Derivedcascadeanalysis {
       if (listBestCollisionIdx[mcCollision.globalIndex()] > -1) {
         auto collision = collisions.iteratorAt(listBestCollisionIdx[mcCollision.globalIndex()]);
         centrality = collision.centFT0C();
-        if (!usePbPbCentrality)
+        if (useCentralityFT0M)
           centrality = collision.centFT0M();
+        if (useCentralityFT0A)
+          centrality = collision.centFV0A();
+        if (useCentralityFT0Cvar1)
+          centrality = collision.centFT0CVariant1();
         if (useTrackOccupancyDef)
           occupancy = collision.trackOccupancyInTimeRange();
         if (useFT0OccupancyDef)
@@ -1393,12 +1417,14 @@ struct Derivedcascadeanalysis {
         histos.fill(HIST("h2dGenXiMinusEtaBach"), RecoDecay::eta(std::array{cascMC.pxBachMC(), cascMC.pyBachMC(), cascMC.pzBachMC()}));
         histos.fill(HIST("h2dGenXiMinusVsMultMC"), mcCollision.multMCNParticlesEta05(), ptmc);
         histos.fill(HIST("h2dGenXiMinusVsCentOccupancy"), ptmc, centrality, occupancy);
+        histos.fill(HIST("h2dGenXiMinusVsNchVsOccupancy"), ptmc, nChEta05, occupancy);
       }
       if (cascMC.pdgCode() == -3312) {
         histos.fill(HIST("h2dGenXiPlus"), centrality, ptmc);
         histos.fill(HIST("h2dGenXiPlusVsNch"), nChEta05, ptmc);
         histos.fill(HIST("h2dGenXiPlusVsMultMC"), mcCollision.multMCNParticlesEta05(), ptmc);
         histos.fill(HIST("h2dGenXiPlusVsCentOccupancy"), ptmc, centrality, occupancy);
+        histos.fill(HIST("h2dGenXiPlusVsNchVsOccupancy"), ptmc, nChEta05, occupancy);
       }
       if (cascMC.pdgCode() == 3334) {
         histos.fill(HIST("h2dGenOmegaMinus"), centrality, ptmc);
@@ -1409,12 +1435,14 @@ struct Derivedcascadeanalysis {
         histos.fill(HIST("h2dGenOmegaMinusEtaBach"), RecoDecay::eta(std::array{cascMC.pxBachMC(), cascMC.pyBachMC(), cascMC.pzBachMC()}));
         histos.fill(HIST("h2dGenOmegaMinusVsMultMC"), mcCollision.multMCNParticlesEta05(), ptmc);
         histos.fill(HIST("h2dGenOmegaMinusVsCentOccupancy"), ptmc, centrality, occupancy);
+        histos.fill(HIST("h2dGenOmegaMinusVsNchVsOccupancy"), ptmc, nChEta05, occupancy);
       }
       if (cascMC.pdgCode() == -3334) {
         histos.fill(HIST("h2dGenOmegaPlus"), centrality, ptmc);
         histos.fill(HIST("h2dGenOmegaPlusVsNch"), nChEta05, ptmc);
         histos.fill(HIST("h2dGenOmegaPlusVsMultMC"), mcCollision.multMCNParticlesEta05(), ptmc);
         histos.fill(HIST("h2dGenOmegaPlusVsCentOccupancy"), ptmc, centrality, occupancy);
+        histos.fill(HIST("h2dGenOmegaPlusVsNchVsOccupancy"), ptmc, nChEta05, occupancy);
       }
     }
   }
@@ -1445,9 +1473,12 @@ struct Derivedcascadeanalysis {
           biggestNContribs = collision.multPVTotalContributors();
           bestCollisionIndex = collision.globalIndex();
           centrality = collision.centFT0C();
-          nChEta05 = collision.multNTracksPVeta1();
-          if (!usePbPbCentrality)
+          if (useCentralityFT0M)
             centrality = collision.centFT0M();
+          if (useCentralityFT0A)
+            centrality = collision.centFV0A();
+          if (useCentralityFT0Cvar1)
+            centrality = collision.centFT0CVariant1();
         }
         nCollisions++;
 
