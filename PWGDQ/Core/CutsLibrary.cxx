4549777eb3dcf5ea8b2081365416f24566438bee
Yuanjing Ji <yji@lxbk0905.gsi.de>
2025-02-04 15:09:16 +0100

    formating

--- PWGDQ/Core/CutsLibrary.cxx
+++ PWGDQ/Core/CutsLibrary.cxx
@@ -4549,7 +4549,7 @@ AnalysisCut* o2::aod::dqcuts::GetAnalysisCut(const char* cutName)
     cut->AddCut(VarManager::kTPCnclsCR, 70, 161);
     return cut;
   }
-  
+
   if (!nameStr.compare("protonPVcut")) {
     cut->AddCut(VarManager::kTrackDCAxy, -0.1, 0.1);
     cut->AddCut(VarManager::kTrackDCAz, -0.15, 0.15);
