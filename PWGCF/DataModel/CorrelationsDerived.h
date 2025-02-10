2b8f2654dca5966e7705da89ab21470ddc4408c6
jikim1290 <jikim1290@gmail.com>
2025-02-10 23:08:59 +0900

    delete redefinition

--- PWGCF/DataModel/CorrelationsDerived.h
+++ PWGCF/DataModel/CorrelationsDerived.h
@@ -121,13 +121,6 @@ using CF2ProngTrack = CF2ProngTracks::iterator;
 
 namespace cf2prongtrackml
 {
-DECLARE_SOA_INDEX_COLUMN_FULL(CFTrackProng0, cfTrackProng0, int, CFTracks, "_0"); //! Index to prong 1 CFTrack
-DECLARE_SOA_INDEX_COLUMN_FULL(CFTrackProng1, cfTrackProng1, int, CFTracks, "_1"); //! Index to prong 2 CFTrack
-DECLARE_SOA_COLUMN(Pt, pt, float);                                                //! pT (GeV/c)
-DECLARE_SOA_COLUMN(Eta, eta, float);                                              //! Pseudorapidity
-DECLARE_SOA_COLUMN(Phi, phi, float);                                              //! Phi angle
-DECLARE_SOA_COLUMN(InvMass, invMass, float);                                      //! Invariant mass
-DECLARE_SOA_COLUMN(Decay, decay, uint8_t);                                        //! Particle decay
 DECLARE_SOA_COLUMN(MlProbD0, mlProbD0, std::vector<float>);                       //!
 DECLARE_SOA_COLUMN(MlProbD0bar, mlProbD0bar, std::vector<float>);                 //!
 enum ParticleDecay {
@@ -140,9 +133,9 @@ enum ParticleDecay {
 DECLARE_SOA_TABLE(CF2ProngTrackmls, "AOD", "CF2PRONGTRACKML", //! Reduced track table
                   o2::soa::Index<>,
                   cftrack::CFCollisionId,
-                  cf2prongtrackml::CFTrackProng0Id,
-                  cf2prongtrackml::CFTrackProng1Id,
-                  cf2prongtrackml::Pt, cf2prongtrackml::Eta, cf2prongtrackml::Phi, cf2prongtrackml::InvMass, cf2prongtrackml::Decay,
+                  cf2prongtrack::CFTrackProng0Id,
+                  cf2prongtrack::CFTrackProng1Id,
+                  cf2prongtrack::Pt, cf2prongtrack::Eta, cf2prongtrack::Phi, cf2prongtrack::InvMass, cf2prongtrack::Decay,
                   cf2prongtrackml::MlProbD0, cf2prongtrackml::MlProbD0bar);
 using CF2ProngTrackml = CF2ProngTrackmls::iterator;
 //------
