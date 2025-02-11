f54629e8efebdccd055eefc2cc608245f7090250
prottayCMT <61418725+prottayCMT@users.noreply.github.com>
2025-02-11 20:47:54 +0100

    [PWGLF] fixed overwriting of q vectors (#9859)
    Co-authored-by: Prottay Das <prottay@alipap1.cern.ch>

--- PWGLF/Tasks/Strangeness/lambdapolsp.cxx
+++ PWGLF/Tasks/Strangeness/lambdapolsp.cxx
@@ -622,14 +622,24 @@ struct lambdapolsp {
     auto psiZDCC = collision.psiZDCC();
     auto psiZDCA = collision.psiZDCA();
 
+    double modqxZDCA;
+    double modqyZDCA;
+    double modqxZDCC;
+    double modqyZDCC;
+
     if (cqvas) {
-      qxZDCA = TMath::Sqrt((qxZDCA * qxZDCA) + (qyZDCA * qyZDCA)) * TMath::Cos(psiZDCA);
-      qyZDCA = TMath::Sqrt((qxZDCA * qxZDCA) + (qyZDCA * qyZDCA)) * TMath::Sin(psiZDCA);
-      qxZDCC = TMath::Sqrt((qxZDCC * qxZDCC) + (qyZDCC * qyZDCC)) * TMath::Cos(psiZDCC);
-      qyZDCC = TMath::Sqrt((qxZDCC * qxZDCC) + (qyZDCC * qyZDCC)) * TMath::Sin(psiZDCC);
+      modqxZDCA = TMath::Sqrt((qxZDCA * qxZDCA) + (qyZDCA * qyZDCA)) * TMath::Cos(psiZDCA);
+      modqyZDCA = TMath::Sqrt((qxZDCA * qxZDCA) + (qyZDCA * qyZDCA)) * TMath::Sin(psiZDCA);
+      modqxZDCC = TMath::Sqrt((qxZDCC * qxZDCC) + (qyZDCC * qyZDCC)) * TMath::Cos(psiZDCC);
+      modqyZDCC = TMath::Sqrt((qxZDCC * qxZDCC) + (qyZDCC * qyZDCC)) * TMath::Sin(psiZDCC);
+    } else {
+      modqxZDCA = qxZDCA;
+      modqyZDCA = qyZDCA;
+      modqxZDCC = qxZDCC;
+      modqyZDCC = qyZDCC;
     }
 
-    auto psiZDC = TMath::ATan2((qyZDCC - qyZDCA), (qxZDCC - qxZDCA)); // full event plane
+    auto psiZDC = TMath::ATan2((modqyZDCC - modqyZDCA), (modqxZDCC - modqxZDCA)); // full event plane
     if (useonlypsis) {
       psiZDC = psiZDCC - psiZDCA;
     }
@@ -652,11 +662,11 @@ struct lambdapolsp {
     ///////////checking v1////////////////////////////////
     if (checkwithpub) {
 
-      auto QxtQxp = qxZDCA * qxZDCC;
-      auto QytQyp = qyZDCA * qyZDCC;
+      auto QxtQxp = modqxZDCA * modqxZDCC;
+      auto QytQyp = modqyZDCA * modqyZDCC;
       auto Qxytp = QxtQxp + QytQyp;
-      auto QxpQyt = qxZDCA * qyZDCC;
-      auto QxtQyp = qxZDCC * qyZDCA;
+      auto QxpQyt = modqxZDCA * modqyZDCC;
+      auto QxtQyp = modqxZDCC * modqyZDCA;
 
       histos.fill(HIST("hpQxtQxpvscent"), centrality, QxtQxp);
       histos.fill(HIST("hpQytQypvscent"), centrality, QytQyp);
@@ -664,10 +674,10 @@ struct lambdapolsp {
       histos.fill(HIST("hpQxpQytvscent"), centrality, QxpQyt);
       histos.fill(HIST("hpQxtQypvscent"), centrality, QxtQyp);
 
-      histos.fill(HIST("hpQxpvscent"), centrality, qxZDCA);
-      histos.fill(HIST("hpQxtvscent"), centrality, qxZDCC);
-      histos.fill(HIST("hpQypvscent"), centrality, qyZDCA);
-      histos.fill(HIST("hpQytvscent"), centrality, qyZDCC);
+      histos.fill(HIST("hpQxpvscent"), centrality, modqxZDCA);
+      histos.fill(HIST("hpQxtvscent"), centrality, modqxZDCC);
+      histos.fill(HIST("hpQypvscent"), centrality, modqyZDCA);
+      histos.fill(HIST("hpQytvscent"), centrality, modqyZDCC);
 
       for (auto track : tracks) {
         if (!selectionTrack(track)) {
@@ -682,24 +692,24 @@ struct lambdapolsp {
         auto uy = TMath::Sin(GetPhiInRange(track.phi()));
         // auto py=track.py();
 
-        auto uxQxp = ux * qxZDCA;
-        auto uyQyp = uy * qyZDCA;
+        auto uxQxp = ux * modqxZDCA;
+        auto uyQyp = uy * modqyZDCA;
         auto uxyQxyp = uxQxp + uyQyp;
-        auto uxQxt = ux * qxZDCC;
-        auto uyQyt = uy * qyZDCC;
+        auto uxQxt = ux * modqxZDCC;
+        auto uyQyt = uy * modqyZDCC;
         auto uxyQxyt = uxQxt + uyQyt;
-        auto oddv1 = ux * (qxZDCA - qxZDCC) + uy * (qyZDCA - qyZDCC);
-        auto evenv1 = ux * (qxZDCA + qxZDCC) + uy * (qyZDCA + qyZDCC);
+        auto oddv1 = ux * (modqxZDCA - modqxZDCC) + uy * (modqyZDCA - modqyZDCC);
+        auto evenv1 = ux * (modqxZDCA + modqxZDCC) + uy * (modqyZDCA + modqyZDCC);
         auto v21 = TMath::Cos(2 * (GetPhiInRange(track.phi()) - psiZDCA - psiZDCC));
         auto v22 = TMath::Cos(2 * (GetPhiInRange(track.phi()) + psiZDCA - psiZDCC));
         auto v23 = TMath::Cos(2 * (GetPhiInRange(track.phi()) - psiZDC));
 
-        auto x2Tx1Ax1C = TMath::Cos(2 * GetPhiInRange(track.phi())) * qxZDCA * qxZDCC;
-        auto x2Ty1Ay1C = TMath::Cos(2 * GetPhiInRange(track.phi())) * qyZDCA * qyZDCC;
-        auto y2Tx1Ay1C = TMath::Sin(2 * GetPhiInRange(track.phi())) * qxZDCA * qyZDCC;
-        auto y2Ty1Ax1C = TMath::Sin(2 * GetPhiInRange(track.phi())) * qyZDCA * qxZDCC;
-        auto x1Ax1C = qxZDCA * qxZDCC;
-        auto y1Ay1C = qyZDCA * qyZDCC;
+        auto x2Tx1Ax1C = TMath::Cos(2 * GetPhiInRange(track.phi())) * modqxZDCA * modqxZDCC;
+        auto x2Ty1Ay1C = TMath::Cos(2 * GetPhiInRange(track.phi())) * modqyZDCA * modqyZDCC;
+        auto y2Tx1Ay1C = TMath::Sin(2 * GetPhiInRange(track.phi())) * modqxZDCA * modqyZDCC;
+        auto y2Ty1Ax1C = TMath::Sin(2 * GetPhiInRange(track.phi())) * modqyZDCA * modqxZDCC;
+        auto x1Ax1C = modqxZDCA * modqxZDCC;
+        auto y1Ay1C = modqyZDCA * modqyZDCC;
 
         if (globalpt) {
           // if (sign > 0) {
