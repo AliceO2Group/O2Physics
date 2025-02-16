28dda26aba0b6464d5a527b595bcd19ee8fa3e8b
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2025-02-16 16:58:15 +0100

    Update flowTest.cxx

--- Common/Tasks/flowTest.cxx
+++ Common/Tasks/flowTest.cxx
@@ -152,7 +152,7 @@ struct flowTest {
               }
             }
 
-            if (track.tpcNClsFound() > analysisMinimumTPCClusters && track.itsNCls() > analysisMinimumITSClusters) {
+            if (track.tpcNClsFound() >= analysisMinimumTPCClusters && track.itsNCls() >= analysisMinimumITSClusters) {
               validAnalysisTrack = true;
               if (isITSFake) {
                 validAnalysisTrackFake = true;
