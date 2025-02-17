b512823e9b21ff746c187eb47465c5d6af3b8bae
victorvalenciatorres <valencia@subatech.in2p3.fr>
2025-02-17 09:30:16 +0100

    [PWGDQ]: fix dimuon multiplicity for event mixing

--- PWGDQ/Tasks/tableReader.cxx
+++ PWGDQ/Tasks/tableReader.cxx
@@ -775,9 +775,7 @@ struct AnalysisEventMixing {
       histNames = fTrackMuonHistNames;
     }
 
-    uint32_t twoTrackFilter = 0;
-
-    if (fConfigSingleMuCumulants) {
+      uint32_t twoTrackFilter = 0;
       uint32_t mult_dimuons = 0;
       for (auto& track1 : tracks1) {
         for (auto& track2 : tracks2) {
@@ -790,7 +788,6 @@ struct AnalysisEventMixing {
         } // end for (track2)
       } // end for (track1)
       VarManager::fgValues[VarManager::kMultDimuonsME] = mult_dimuons;
-    }
 
     twoTrackFilter = 0;
     for (auto& track1 : tracks1) {
