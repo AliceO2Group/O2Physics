1bb9f5b04b2615a6aee46c1e77126c2d385fd467
Nicolò Jacazio <njacazio@users.noreply.github.com>
2025-02-22 15:20:20 +0100

    Update strangenessInJets.cxx

--- PWGLF/Tasks/Strangeness/strangenessInJets.cxx
+++ PWGLF/Tasks/Strangeness/strangenessInJets.cxx
@@ -1066,7 +1066,7 @@ struct StrangenessInJets {
       isAtLeastOneJetSelected = true;
 
       // perpendicular cone
-      double coneRadius = std::sqrt(jet.area() / PI);
+      // double coneRadius = std::sqrt(jet.area() / PI);
       TVector3 jetAxis(jet.px(), jet.py(), jet.pz());
       TVector3 ueAxis1(0, 0, 0);
       TVector3 ueAxis2(0, 0, 0);
