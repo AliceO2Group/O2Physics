8a6920d32332899ef57352843886c56d3f7519cf
Preet Pati <preet@preet-2.local>
2025-02-25 14:27:24 +0100

    recommiting

--- PWGCF/Flow/Tasks/resonancesGfwFlow.cxx
+++ PWGCF/Flow/Tasks/resonancesGfwFlow.cxx
@@ -218,8 +218,8 @@ struct ResonancesGfwFlow {
 
     o2::framework::AxisSpec axisLambdasMass = axisLambdaMass;
     int nLambdaMassBins = axisLambdasMass.binEdges.size() - 1;
-    double* lambdaMassBins = &(axisLambdasMass.binEdges)[0];
-    fLambdaMassAxis = new TAxis(nLambdaMassBins, lambdaMassBins);
+    double* LambdaMassBins = &(axisLambdasMass.binEdges)[0];
+    fLambdaMassAxis = new TAxis(nLambdaMassBins, LambdaMassBins);
 
     //********** Defining the regions  **********
     // reference particles
@@ -567,7 +567,7 @@ struct ResonancesGfwFlow {
     for (auto const& track : tracks) {
       double pt = track.pt();
       bool withinPtRef = (cfgCutPtMin < pt) && (pt < cfgCutPtMax);
-      
+
       if (withinPtRef)
         fGFW->Fill(track.eta(), fPtAxis->FindBin(pt) - 1, track.phi(), wacc * weff, 1);
     }
@@ -576,7 +576,7 @@ struct ResonancesGfwFlow {
     auto negSlicedTracks = negTracks->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
 
     resurrectPhi(posSlicedTracks, negSlicedTracks, kaonPlus, kaonMinus, phiMom, massKaPlus, HIST("hPhiMass_sparse"), cent, weff, wacc);
-    
+
     for (auto const& v0s : V0s) {
       if (selectionLambda(collision, v0s) == true)
         histos.fill(HIST("hLambdaCount"), 1);
