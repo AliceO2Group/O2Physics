607e4d1e3be31f03b0ea724d7e6998422321df62
jaelpark <jasper.parkkila@cern.ch>
2025-03-04 23:01:39 +0800

    [PWGCF] correlations task 2-prong track efficiency (#10307)

--- PWGCF/TableProducer/filter2Prong.cxx
+++ PWGCF/TableProducer/filter2Prong.cxx
@@ -40,6 +40,8 @@ struct Filter2Prong {
   Produces<aod::CF2ProngTracks> output2ProngTracks;
   Produces<aod::CF2ProngTrackmls> output2ProngTrackmls;
 
+  Produces<aod::CF2ProngMcParts> output2ProngMcParts;
+
   std::vector<float> mlvecd{};
   std::vector<float> mlvecdbar{};
 
@@ -81,13 +83,11 @@ struct Filter2Prong {
                            prongCFId[0], prongCFId[1], c.pt(), c.eta(), c.phi(), hfHelper.invMassD0ToPiK(c), aod::cf2prongtrack::D0ToPiK);
         if constexpr (std::experimental::is_detected<HasMLProb, typename HFCandidatesType::iterator>::value) {
           mlvecd.clear();
-          for (float val : c.mlProbD0()) {
+          for (float val : c.mlProbD0())
             mlvecd.push_back(val);
-          }
           mlvecdbar.clear();
-          for (float val : c.mlProbD0bar()) {
+          for (float val : c.mlProbD0bar())
             mlvecdbar.push_back(val);
-          }
           output2ProngTrackmls(cfcollisions.begin().globalIndex(), mlvecd, mlvecdbar);
         }
       }
@@ -97,30 +97,51 @@ struct Filter2Prong {
                            prongCFId[0], prongCFId[1], c.pt(), c.eta(), c.phi(), hfHelper.invMassD0barToKPi(c), aod::cf2prongtrack::D0barToKPi);
         if constexpr (std::experimental::is_detected<HasMLProb, typename HFCandidatesType::iterator>::value) {
           mlvecd.clear();
-          for (float val : c.mlProbD0()) {
+          for (float val : c.mlProbD0())
             mlvecd.push_back(val);
-          }
           mlvecdbar.clear();
-          for (float val : c.mlProbD0bar()) {
+          for (float val : c.mlProbD0bar())
             mlvecdbar.push_back(val);
-          }
           output2ProngTrackmls(cfcollisions.begin().globalIndex(), mlvecd, mlvecdbar);
         }
       }
     }
   }
 
-  void processDataML(aod::Collisions::iterator const& cols, aod::BCsWithTimestamps const& bcs, aod::CFCollRefs const& cfcollisions, aod::CFTrackRefs const& cftracks, HFCandidatesML const& candidates)
+  void processDataML(aod::Collisions::iterator const& col, aod::BCsWithTimestamps const& bcs, aod::CFCollRefs const& cfcollisions, aod::CFTrackRefs const& cftracks, HFCandidatesML const& candidates)
   {
-    processDataT(cols, bcs, cfcollisions, cftracks, candidates);
+    processDataT(col, bcs, cfcollisions, cftracks, candidates);
   }
   PROCESS_SWITCH(Filter2Prong, processDataML, "Process data D0 candidates with ML", false);
 
-  void processData(aod::Collisions::iterator const& cols, aod::BCsWithTimestamps const& bcs, aod::CFCollRefs const& cfcollisions, aod::CFTrackRefs const& cftracks, HFCandidates const& candidates)
+  void processData(aod::Collisions::iterator const& col, aod::BCsWithTimestamps const& bcs, aod::CFCollRefs const& cfcollisions, aod::CFTrackRefs const& cftracks, HFCandidates const& candidates)
   {
-    processDataT(cols, bcs, cfcollisions, cftracks, candidates);
+    processDataT(col, bcs, cfcollisions, cftracks, candidates);
   }
   PROCESS_SWITCH(Filter2Prong, processData, "Process data D0 candidates", true);
+
+  void processMC(aod::McCollisions::iterator const&, aod::CFMcParticleRefs const& cfmcparticles, aod::McParticles const& mcparticles)
+  {
+    // The main filter outputs the primary MC particles. Here we just resolve the daughter indices that are needed for the efficiency matching.
+    for (auto& r : cfmcparticles) {
+      const auto& mcParticle = mcparticles.iteratorAt(r.mcParticleId());
+      if (mcParticle.daughtersIds().size() != 2) {
+        output2ProngMcParts(-1, -1);
+        continue;
+      }
+      int prongCFId[2] = {-1, -1};
+      for (uint i = 0; i < 2; ++i) {
+        for (auto& cfmcpart : cfmcparticles) {
+          if (mcParticle.daughtersIds()[i] == cfmcpart.mcParticleId()) {
+            prongCFId[i] = cfmcpart.globalIndex();
+            break;
+          }
+        }
+      }
+      output2ProngMcParts(prongCFId[0], prongCFId[1]);
+    }
+  }
+  PROCESS_SWITCH(Filter2Prong, processMC, "Process MC 2-prong daughters", false);
 }; // struct
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
