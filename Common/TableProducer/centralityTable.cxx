cbe3922b43f8044dc00c66b8721170c9f8283dd2
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2025-03-10 16:11:25 +0100

    [Common] Improve warning message for estimator not available (#10426)
    Co-authored-by: ALICE Builder <alibuild@users.noreply.github.com>

--- Common/TableProducer/centralityTable.cxx
+++ Common/TableProducer/centralityTable.cxx
@@ -590,7 +590,7 @@ struct CentralityTable {
               estimator.mCalibrationStored = true;
               estimator.isSane();
             } else {
-              LOGF(error, "Calibration information from %s for run %d not available", estimator.name.c_str(), bc.runNumber());
+              LOGF(info, "Calibration information from %s for run %d not available, will fill this estimator with invalid values and continue (no crash).", estimator.name.c_str(), bc.runNumber());
             }
           };
 
