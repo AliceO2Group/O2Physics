cbfd04ebb263e9dd382e0f70a9d8dbb034eb0710
Rrantu <ratufdu@163.com>
2025-03-11 20:35:34 +0800

    add braces around blocks

--- PWGHF/TableProducer/candidateCreatorXic0Omegac0.cxx
+++ PWGHF/TableProducer/candidateCreatorXic0Omegac0.cxx
@@ -2215,7 +2215,7 @@ struct HfCandidateCreatorXic0Omegac0Mc {
           if (origin == RecoDecay::OriginType::NonPrompt) {
             rowMCMatchGenXicToXiPi(flag, debugGenCharmBar, debugGenCasc, debugGenLambda, ptCharmBaryonGen, rapidityCharmBaryonGen, origin, idxBhadMothers[0]);
           } else {
-            if (rejectOriginNone && origin == RecoDecay::OriginType::None)
+            if (rejectOriginNone && (origin == RecoDecay::OriginType::None))
               continue;
             rowMCMatchGenXicToXiPi(flag, debugGenCharmBar, debugGenCasc, debugGenLambda, ptCharmBaryonGen, rapidityCharmBaryonGen, origin, -1);
           }
@@ -2259,7 +2259,7 @@ struct HfCandidateCreatorXic0Omegac0Mc {
           if (origin == RecoDecay::OriginType::NonPrompt) {
             rowMCMatchGenOmegacToXiPi(flag, debugGenCharmBar, debugGenCasc, debugGenLambda, ptCharmBaryonGen, rapidityCharmBaryonGen, origin, idxBhadMothers[0]);
           } else {
-            if (rejectOriginNone && origin == RecoDecay::OriginType::None)
+            if (rejectOriginNone && (origin == RecoDecay::OriginType::None))
               continue;
             rowMCMatchGenOmegacToXiPi(flag, debugGenCharmBar, debugGenCasc, debugGenLambda, ptCharmBaryonGen, rapidityCharmBaryonGen, origin, -1);
           }
@@ -2303,7 +2303,7 @@ struct HfCandidateCreatorXic0Omegac0Mc {
           if (origin == RecoDecay::OriginType::NonPrompt) {
             rowMCMatchGenToOmegaPi(flag, debugGenCharmBar, debugGenCasc, debugGenLambda, ptCharmBaryonGen, rapidityCharmBaryonGen, origin, idxBhadMothers[0]);
           } else {
-            if (rejectOriginNone && origin == RecoDecay::OriginType::None)
+            if (rejectOriginNone && (origin == RecoDecay::OriginType::None))
               continue;
             rowMCMatchGenToOmegaPi(flag, debugGenCharmBar, debugGenCasc, debugGenLambda, ptCharmBaryonGen, rapidityCharmBaryonGen, origin, -1);
           }
@@ -2347,7 +2347,7 @@ struct HfCandidateCreatorXic0Omegac0Mc {
           if (origin == RecoDecay::OriginType::NonPrompt) {
             rowMCMatchGenToOmegaK(flag, debugGenCharmBar, debugGenCasc, debugGenLambda, ptCharmBaryonGen, rapidityCharmBaryonGen, origin, idxBhadMothers[0]);
           } else {
-            if (rejectOriginNone && origin == RecoDecay::OriginType::None)
+            if (rejectOriginNone && (origin == RecoDecay::OriginType::None))
               continue;
             rowMCMatchGenToOmegaK(flag, debugGenCharmBar, debugGenCasc, debugGenLambda, ptCharmBaryonGen, rapidityCharmBaryonGen, origin, -1);
           }
