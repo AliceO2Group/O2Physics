c647e845f20e05ec560e295f9be09782fbb4abeb
Ana Marin <a.marin@gsi.de>
2025-03-12 12:12:29 +0100

    correcting some more formatting errors

--- DPG/Tasks/TPC/tpcSkimsTableCreator.cxx
+++ DPG/Tasks/TPC/tpcSkimsTableCreator.cxx
@@ -678,7 +678,7 @@ struct TreeWriterTPCTOF {
               ((trackSelection == 2) && trk.isGlobalTrackWoPtEta()) ||
               ((trackSelection == 3) && trk.isGlobalTrackWoDCA()) ||
               ((trackSelection == 4) && trk.isQualityTrack()) ||
-              ((trackSelection == 5) && trk.isInAcceptanceTrack()))){
+              ((trackSelection == 5) && trk.isInAcceptanceTrack()))) {
           continue;
         }
         // get the corresponding trackQA using labelTracks2TracKQA and get variables of interest
