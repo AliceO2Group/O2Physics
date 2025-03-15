06b4e5b3fb033767974f55a748bed3e1c95c6c04
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2025-03-15 12:49:01 +0800

    clang formatted

--- PWGUD/Tasks/flowCumulantsUpc.cxx
+++ PWGUD/Tasks/flowCumulantsUpc.cxx
@@ -778,8 +778,10 @@ struct FlowCumulantsUpc {
       TVector3 momentum(track.px(), track.py(), track.pz());
       double pt = momentum.Perp();
       double phi = momentum.Phi();
-      if (phi < 0) phi += 2*o2::constants::math::PI;
-      if (phi > 2*o2::constants::math::PI) phi -= 2*o2::constants::math::PI;
+      if (phi < 0)
+        phi += 2 * o2::constants::math::PI;
+      if (phi > 2 * o2::constants::math::PI)
+        phi -= 2 * o2::constants::math::PI;
       double eta = momentum.PseudoRapidity();
       bool withinPtPOI = (cfgCutPtPOIMin < pt) && (pt < cfgCutPtPOIMax); // within POI pT range
       bool withinPtRef = (cfgCutPtRefMin < pt) && (pt < cfgCutPtRefMax); // within RF pT range
