69c0b7066d3f7d0e37b69ed80869ceeead2b37dd
Lucia Anna Tarasovicova <husovalucia@gmail.com>
2025-04-03 18:28:54 +0200

    [PWGLF] change in hStrangeCorrelation.cxx (#10707)
    Co-authored-by: Lucia Anna Tarasovicova <lucia.anna.husova@cern.ch>

--- PWGLF/Tasks/Strangeness/hStrangeCorrelation.cxx
+++ PWGLF/Tasks/Strangeness/hStrangeCorrelation.cxx
@@ -843,6 +843,14 @@ struct HStrangeCorrelation {
         }
       }
     }
+
+    if (TESTBIT(doCorrelation, 8)) {
+      histos.add("hAsssocTrackEtaVsPtVsPhi", "", kTH3F, {axisPtQA, axisEta, axisPhi});
+      histos.add("hAssocPrimaryEtaVsPt", "", kTH3F, {axisPtQA, axisEta, axisMultNDim});
+      histos.add("hAssocHadronsAllSelectedEtaVsPt", "", kTH3F, {axisPtQA, axisEta, axisMultNDim});
+      histos.add("hAssocPtResolution", ";p_{T}^{reconstructed} (GeV/c); p_{T}^{generated} (GeV/c)", kTH2F, {axisPtQA, axisPtQA});
+    }
+
     if (hStrange) {
       histos.addClone("sameEvent/Signal/", "sameEvent/LeftBg/");
       histos.addClone("sameEvent/Signal/", "sameEvent/RightBg/");
