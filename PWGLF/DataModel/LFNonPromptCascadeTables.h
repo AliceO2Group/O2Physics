abdec46d31b8dba1e0e41fa822761e3be2179fdb
Roman Lietava <roman.lietava@cern.ch>
2025-04-03 09:30:58 +0200

    [PWGLF,Trigger] dev: 1 added counter for negative amps filter 2 mult added to nonpromtcascade task (#10742)

--- PWGLF/DataModel/LFNonPromptCascadeTables.h
+++ PWGLF/DataModel/LFNonPromptCascadeTables.h
@@ -26,7 +26,6 @@ namespace NPCascadeTable
 {
 DECLARE_SOA_COLUMN(MatchingChi2, matchingChi2, float);
 DECLARE_SOA_COLUMN(DeltaPtITSCascade, deltaPtITSCascade, float);
-DECLARE_SOA_COLUMN(DeltaPtCascade, deltaPtCascade, float);
 DECLARE_SOA_COLUMN(ITSClusSize, itsClusSize, float);
 DECLARE_SOA_COLUMN(HasReassociatedCluster, hasReassociatedCluster, bool);
 DECLARE_SOA_COLUMN(IsGoodMatch, isGoodMatch, bool);
@@ -104,14 +103,15 @@ DECLARE_SOA_COLUMN(DCAxMC, dcaXmc, float);
 DECLARE_SOA_COLUMN(DCAyMC, dcaYmc, float);
 DECLARE_SOA_COLUMN(DCAzMC, dcaZmc, float);
 DECLARE_SOA_COLUMN(MCcollisionMatch, mcCollisionMatch, bool);
-DECLARE_SOA_COLUMN(HasFakeReassociation, hasFakeReassociation, bool);
-DECLARE_SOA_COLUMN(MotherDecayDaughters, motherDecayDaughters, int8_t);
+
+DECLARE_SOA_COLUMN(Sel8, sel8, bool);
+DECLARE_SOA_COLUMN(MultFT0C, multFT0C, float);
+DECLARE_SOA_COLUMN(MultFT0A, multFT0A, float);
 
 } // namespace NPCascadeTable
 DECLARE_SOA_TABLE(NPCascTable, "AOD", "NPCASCTABLE",
                   NPCascadeTable::MatchingChi2,
                   NPCascadeTable::DeltaPtITSCascade,
-                  NPCascadeTable::DeltaPtCascade,
                   NPCascadeTable::ITSClusSize,
                   NPCascadeTable::HasReassociatedCluster,
                   aod::collision::NumContrib,
@@ -162,12 +162,14 @@ DECLARE_SOA_TABLE(NPCascTable, "AOD", "NPCASCTABLE",
                   NPCascadeTable::ProtonTOFNSigma,
                   NPCascadeTable::PionTOFNSigma,
                   NPCascadeTable::BachKaonTOFNSigma,
-                  NPCascadeTable::BachPionTOFNSigma)
+                  NPCascadeTable::BachPionTOFNSigma,
+                  NPCascadeTable::Sel8,
+                  NPCascadeTable::MultFT0C,
+                  NPCascadeTable::MultFT0A)
 
 DECLARE_SOA_TABLE(NPCascTableNT, "AOD", "NPCASCTABLENT",
                   NPCascadeTable::MatchingChi2,
                   NPCascadeTable::DeltaPtITSCascade,
-                  NPCascadeTable::DeltaPtCascade,
                   NPCascadeTable::ITSClusSize,
                   NPCascadeTable::HasReassociatedCluster,
                   aod::collision::NumContrib,
@@ -218,12 +220,14 @@ DECLARE_SOA_TABLE(NPCascTableNT, "AOD", "NPCASCTABLENT",
                   NPCascadeTable::ProtonTOFNSigma,
                   NPCascadeTable::PionTOFNSigma,
                   NPCascadeTable::BachKaonTOFNSigma,
-                  NPCascadeTable::BachPionTOFNSigma)
+                  NPCascadeTable::BachPionTOFNSigma,
+                  NPCascadeTable::Sel8,
+                  NPCascadeTable::MultFT0C,
+                  NPCascadeTable::MultFT0A)
 
 DECLARE_SOA_TABLE(NPCascTableMC, "AOD", "NPCASCTABLEMC",
                   NPCascadeTable::MatchingChi2,
                   NPCascadeTable::DeltaPtITSCascade,
-                  NPCascadeTable::DeltaPtCascade,
                   NPCascadeTable::ITSClusSize,
                   NPCascadeTable::HasReassociatedCluster,
                   NPCascadeTable::IsGoodMatch,
@@ -289,13 +293,13 @@ DECLARE_SOA_TABLE(NPCascTableMC, "AOD", "NPCASCTABLEMC",
                   NPCascadeTable::DCAyMC,
                   NPCascadeTable::DCAzMC,
                   NPCascadeTable::MCcollisionMatch,
-                  NPCascadeTable::HasFakeReassociation,
-                  NPCascadeTable::MotherDecayDaughters)
+                  NPCascadeTable::Sel8,
+                  NPCascadeTable::MultFT0C,
+                  NPCascadeTable::MultFT0A)
 
 DECLARE_SOA_TABLE(NPCascTableMCNT, "AOD", "NPCASCTABLEMCNT",
                   NPCascadeTable::MatchingChi2,
                   NPCascadeTable::DeltaPtITSCascade,
-                  NPCascadeTable::DeltaPtCascade,
                   NPCascadeTable::ITSClusSize,
                   NPCascadeTable::HasReassociatedCluster,
                   NPCascadeTable::IsGoodMatch,
@@ -361,8 +365,9 @@ DECLARE_SOA_TABLE(NPCascTableMCNT, "AOD", "NPCASCTABLEMCNT",
                   NPCascadeTable::DCAyMC,
                   NPCascadeTable::DCAzMC,
                   NPCascadeTable::MCcollisionMatch,
-                  NPCascadeTable::HasFakeReassociation,
-                  NPCascadeTable::MotherDecayDaughters)
+                  NPCascadeTable::Sel8,
+                  NPCascadeTable::MultFT0C,
+                  NPCascadeTable::MultFT0A)
 
 DECLARE_SOA_TABLE(NPCascTableGen, "AOD", "NPCASCTABLEGen",
                   NPCascadeTable::gPt,
@@ -374,8 +379,7 @@ DECLARE_SOA_TABLE(NPCascTableGen, "AOD", "NPCASCTABLEGen",
                   NPCascadeTable::DCAyMC,
                   NPCascadeTable::DCAzMC,
                   NPCascadeTable::IsFromBeauty,
-                  NPCascadeTable::IsFromCharm,
-                  NPCascadeTable::MotherDecayDaughters)
+                  NPCascadeTable::IsFromCharm)
 
 } // namespace o2::aod
 
