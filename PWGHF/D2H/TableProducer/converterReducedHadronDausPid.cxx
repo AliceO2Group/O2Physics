b762e44c83689a7656a8acdf1129706bc27c7cb4
BiaoZhang (张彪) <52267892+zhangbiao-phy@users.noreply.github.com>
2025-04-28 16:11:18 +0200

    Create converterReducedHadronDausPid.cxx

---
+++ PWGHF/D2H/TableProducer/converterReducedHadronDausPid.cxx
@@ -0,0 +1,46 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file converterReducedHadronDausPid.cxx
+/// \brief Task for conversion of  daughters pid to version 001
+///
+/// \author Biao Zhang <biao.zhang@cern.ch>, Heidelberg University
+
+#include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/AnalysisDataModel.h"
+
+#include "PWGHF/D2H/DataModel/ReducedDataModel.h"
+#include "PWGHF/DataModel/CandidateSelectionTables.h"
+
+using namespace o2;
+using namespace o2::framework;
+
+struct HfConverterReducedHadronDausPid {
+  Produces<aod::HfRedPidDau0s_001> hfRedPidDau0s;
+  Produces<aod::HfRedPidDau1s_001> hfRedPidDau1s;
+  Produces<aod::HfRedPidDau2s_001> hfRedPidDau2s;
+  using HfRedPidDaus = soa::Join<aod::HfRedTrkNoParams, aod::HfRedPidDau0s_000, aod::HfRedPidDau1s_000, aod::HfRedPidDau2s_000>;
+  void process(HfRedPidDaus::iterator const& hfCandPidProngs)
+  {
+
+    hfRedPidDau0s(hfCandPidProngs.tpcNSigmaPiProng0(), hfCandPidProngs.tofNSigmaPiProng0(), hfCandPidProngs.tpcNSigmaKaProng0(), hfCandPidProngs.tpcNSigmaKaProng0(), hfCandPidProngs.tpcNSigmaPr(), hfCandPidProngs.tofNSigmaPr(), hfCandPidProngs.hasTOFProng0(), hfCandPidProngs.hasTPCProng0());
+    hfRedPidDau1s(hfCandPidProngs.tpcNSigmaPiProng1(), hfCandPidProngs.tofNSigmaPiProng1(), hfCandPidProngs.tpcNSigmaKaProng1(), hfCandPidProngs.tpcNSigmaKaProng1(), hfCandPidProngs.tpcNSigmaPr(), hfCandPidProngs.tofNSigmaPr(), hfCandPidProngs.hasTOFProng1(), hfCandPidProngs.hasTPCProng1());
+    hfRedPidDau2s(hfCandPidProngs.tpcNSigmaPiProng2(), hfCandPidProngs.tofNSigmaPiProng2(), hfCandPidProngs.tpcNSigmaKaProng2(), hfCandPidProngs.tpcNSigmaKaProng2(), hfCandPidProngs.tpcNSigmaPr(), hfCandPidProngs.tofNSigmaPr(), hfCandPidProngs.hasTOFProng2(), hfCandPidProngs.hasTPCProng2());
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<HfConverterReducedHadronDausPid>(cfgc),
+  };
+}
