95e293a50716887179640c01c7bb6082c875f6a6
Rashi gupta <167059733+rashigupt@users.noreply.github.com>
2025-05-02 05:51:27 +0530

    fix nae

--- PWGHF/HFC/Tasks/taskCorrelationHfeHadrons.cxx
+++ PWGHF/HFC/Tasks/taskCorrelationHfeHadrons.cxx
@@ -68,14 +68,14 @@ struct HfTaskCorrelationHfeHadrons {
       ptHadron = pairEntry.ptHadron();
 
       registry.fill(HIST("hInclusiveEHCorrel"), ptElectron, ptHadron, deltaPhi, deltaEta);
-      if (pairEntry.lSPairEHCount() > 0) {
-        for (int i = 0; i < pairEntry.lSPairEHCount(); ++i) {
+      if (pairEntry.lsPairEHCount() > 0) {
+        for (int i = 0; i < pairEntry.lsPairEHCount(); ++i) {
 
           registry.fill(HIST("hLikeSignEHCorrel"), ptElectron, ptHadron, deltaPhi, deltaEta);
         }
       }
-      if (pairEntry.uLSPairEHCount() > 0) {
-        for (int i = 0; i < pairEntry.uLSPairEHCount(); ++i) {
+      if (pairEntry.ulsPairEHCount() > 0) {
+        for (int i = 0; i < pairEntry.ulsPairEHCount(); ++i) {
 
           registry.fill(HIST("hUnlikeSignEHCorrel"), ptElectron, ptHadron, deltaPhi, deltaEta);
         }
