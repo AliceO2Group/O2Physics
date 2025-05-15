d3095349286502c3327781506815aec13bd7c1c9
Nicolò Jacazio <njacazio@users.noreply.github.com>
2025-05-15 13:05:50 +0200

    [ALICE3] A3PID: add parametric getters (#11183)

--- ALICE3/TableProducer/OTF/onTheFlyTofPid.cxx
+++ ALICE3/TableProducer/OTF/onTheFlyTofPid.cxx
@@ -145,7 +145,7 @@ struct OnTheFlyTofPid {
       if (lut.value != "inherit") {
         return;
       }
-      if (!getTaskOptionValue(initContext, "on-the-fly-tracker", lut.name, lut.value, true)) {
+      if (!getTaskOptionValue(initContext, "on-the-fly-tracker", lut, false)) {
         LOG(fatal) << "Could not get " << lut.name << " from on-the-fly-tracker task";
       }
     };
