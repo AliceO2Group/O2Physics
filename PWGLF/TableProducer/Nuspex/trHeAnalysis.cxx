fe0454ca9388ddc1d239d1ec7b4d17df4ae45b15
mherzer28 <herzer.matt@gmail.com>
2025-05-27 22:19:27 +0200

    linter fix

--- PWGLF/TableProducer/Nuspex/trHeAnalysis.cxx
+++ PWGLF/TableProducer/Nuspex/trHeAnalysis.cxx
@@ -648,11 +648,11 @@ struct TrHeAnalysis {
   float betheBlochAleph(Particle const& particle, T const& rigidity)
   {
     double bg = particle.charge*rigidity / particle.mass;
-    double beta = bg / TMath::Sqrt(1. + bg*bg);
-    double aa = TMath::Power(beta, particle.betheParams[3]);
-    double bb = TMath::Power(1. / bg, particle.betheParams[4]);
+    double beta = bg / std::Sqrt(1. + bg*bg);
+    double aa = std::Power(beta, particle.betheParams[3]);
+    double bb = std::Power(1. / bg, particle.betheParams[4]);
     if ((particle.betheParams[2] + bb) <= 0) return 0;
-    bb = TMath::Log(particle.betheParams[2] + bb);
+    bb = std::Log(particle.betheParams[2] + bb);
     return std::pow(particle.charge, particle.chargeFactor) * 50 * (particle.betheParams[1] - aa - bb)*particle.betheParams[0] / aa; 
   }
 
@@ -684,8 +684,8 @@ struct TrHeAnalysis {
   {
     const float beta = track.beta();
     const float rigidity = getRigidity(track);
-    float gamma = 1 / TMath::Sqrt(1-beta*beta);
-    float mass = (rigidity / TMath::Sqrt(gamma * gamma - 1));
+    float gamma = 1 / std::Sqrt(1-beta*beta);
+    float mass = (rigidity / std::Sqrt(gamma * gamma - 1));
     return mass;
   }
 };
