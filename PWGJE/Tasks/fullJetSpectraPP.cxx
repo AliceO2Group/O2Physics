b3d8ca6996e75e5717beff33df4778a65eeb2309
Archita-Dash <archita.rani.dash@cern.ch>
2025-05-30 16:03:39 +0200

    Fixed Copyright Notice Format

--- PWGJE/Tasks/fullJetSpectraPP.cxx
+++ PWGJE/Tasks/fullJetSpectraPP.cxx
@@ -1,8 +1,3 @@
-/**
- * \file FullJetSpectraPP.cxx
- * \brief Full Jet Spectra analysis in pp collisions.
- * \author Archita Rani Dash <archita.rani.dash@cern.ch>
- */
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
@@ -13,6 +8,11 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
+//
+/// \file fullJetSpectraPP.cxx
+/// \brief Task for full jet spectra studies in pp collisions.
+/// \author Archita Rani Dash <archita.rani.dash@cern.ch>
+
 #include <vector>
 #include <iostream>
 #include <utility>
