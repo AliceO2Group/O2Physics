c43b13030213e41f5ea2473bf56c246e97f8b03b
Ravindra Singh <56298081+singhra1994@users.noreply.github.com>
2025-06-02 01:02:49 +0200

    Update correlatorLcScHadrons.cxx

--- PWGHF/HFC/TableProducer/correlatorLcScHadrons.cxx
+++ PWGHF/HFC/TableProducer/correlatorLcScHadrons.cxx
@@ -33,6 +33,7 @@
 #include "CommonConstants/MathConstants.h"
 
 #include "PWGHF/Core/HfHelper.h"
+#include "PWGHF/Core/SelectorCuts.h"
 #include "PWGHF/DataModel/CandidateReconstructionTables.h"
 #include "PWGHF/DataModel/CandidateSelectionTables.h"
 #include "PWGHF/HFC/DataModel/CorrelationTables.h"
