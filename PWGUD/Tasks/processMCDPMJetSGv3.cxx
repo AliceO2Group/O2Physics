27b2583122d825a6efd8ea9c9b2b88c3cd24ccc5
siragoni <simone.ragoni@cern.ch>
2025-06-06 15:08:12 +0200

    Fix error messages relevant to name of variables

--- PWGUD/Tasks/processMCDPMJetSGv3.cxx
+++ PWGUD/Tasks/processMCDPMJetSGv3.cxx
@@ -1,3 +1,7 @@
+/// \file ProcessMCDPMJetSGv3.cxx
+/// \brief Process MC DPMJet events for exclusive studies
+/// \author Simone Ragoni
+/// \since 2025-06-06
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
@@ -8,9 +12,6 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-/// \file Task for the analysis of inclusive MC for spectra.
-/// \brief Task for the analysis of inclusive MC for spectra.
-/// \author Simone Ragoni
 
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
