4f1d409d7da248251ea3b097a9f277d6858891f1
Yiping Wang <yipwang@lxbk1131.gsi.de>
2025-06-17 10:08:23 +0200

    copyright

--- PWGDQ/Tasks/ModelConverterEventExtended.cxx
+++ PWGDQ/Tasks/ModelConverterEventExtended.cxx
@@ -1,3 +1,20 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+//
+// Contact: iarsene@cern.ch, i.c.arsene@fys.uio.no
+//
+// Task used to convert the data model from the old format to the new format. To avoid
+// the conflict with the old data model.
+
+// other includes
 #include <THashList.h>
 #include <TList.h>
 #include <TString.h>
