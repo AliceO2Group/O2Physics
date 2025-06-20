074758faf5129534f30085f4307389c4ac59e11f
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-06-20 13:24:09 +0200

    address O2 linter error

--- PWGHF/D2H/Macros/HFInvMassFitterLinkDef.h
+++ PWGHF/D2H/Macros/HFInvMassFitterLinkDef.h
@@ -9,6 +9,15 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
+/// \file HFInvMassFitterLinkDef.h
+/// \brief HFInvMassFitter dictionary definition file
+///
+/// \author Zhen Zhang <zhenz@cern.ch>
+/// \author Mingyu Zhang <mingyu.zang@cern.ch>
+/// \author Xinye Peng  <xinye.peng@cern.ch>
+/// \author Biao Zhang <biao.zhang@cern.ch>
+/// \author Oleksii Lubynets <oleksii.lubynets@cern.ch>
+
 #ifndef PWGHF_D2H_MACROS_HFINVMASSFITTERLINKDEF_H_
 #define PWGHF_D2H_MACROS_HFINVMASSFITTERLINKDEF_H_
 
