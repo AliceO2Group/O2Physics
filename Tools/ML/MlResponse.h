774e2ce55fb0b8f3859bd6df1fff8e6e79ccd403
Vít Kučera <vit.kucera@cern.ch>
2025-06-24 11:25:44 +0200

    [Tools] Include What You Use (#11552)

--- Tools/ML/MlResponse.h
+++ Tools/ML/MlResponse.h
@@ -17,16 +17,17 @@
 #ifndef TOOLS_ML_MLRESPONSE_H_
 #define TOOLS_ML_MLRESPONSE_H_
 
-#include <onnxruntime_cxx_api.h>
+#include "Tools/ML/model.h"
 
+#include <CCDB/CcdbApi.h>
+#include <Framework/Array2D.h>
+#include <Framework/Logger.h>
+
+#include <cstddef>
+#include <cstdint>
 #include <map>
 #include <string>
 #include <vector>
-
-#include "CCDB/CcdbApi.h"
-#include "Framework/Array2D.h"
-
-#include "Tools/ML/model.h"
 
 namespace o2
 {
