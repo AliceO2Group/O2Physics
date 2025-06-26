f1d4570e879e235fef43c1c3edd4304f9e12bbe6
amaringarcia <a.marin@gsi.de>
2025-06-26 09:42:04 +0200

    [Common,DPG] [DPG][Common] Test of dE/dx correction for occupancy (#11747)

--- Common/TableProducer/PID/pidTPCBase.h
+++ Common/TableProducer/PID/pidTPCBase.h
@@ -21,13 +21,22 @@
 #include "Common/DataModel/Multiplicity.h"
 #include "Common/DataModel/PIDResponseTPC.h"
 
+#include "TMatrixD.h"
+
 namespace o2::aod
 {
+
+namespace pid
+{
+DECLARE_SOA_COLUMN(TpcSignalCorrected, tpcSignalCorrected, float); //!
+}; // namespace pid
 
 DECLARE_SOA_TABLE(PIDMults, "AOD", "PIDMults", //! TPC auxiliary table for the PID
                   o2::soa::Marker<1>,
                   mult::MultTPC);
+DECLARE_SOA_TABLE_FULL(DEdxsCorrected, "DEdxsCorrected", "AOD", "DEDXCORR", pid::TpcSignalCorrected); //!
 using PIDMult = PIDMults::iterator;
+using DEdxCorrected = DEdxsCorrected::iterator; //!
 
 } // namespace o2::aod
 
@@ -56,5 +65,42 @@ int getPIDIndex(const int pdgCode) // Get O2 PID index corresponding to MC PDG c
       return o2::track::PID::Pion;
   }
 }
+
+typedef struct Str_dEdx_correction {
+  TMatrixD fMatrix;
+  bool warning = true;
+
+  // void init(std::vector<double>& params)
+  void init()
+  {
+    double elements[32] = {0.99091, -0.015053, 0.0018912, -0.012305,
+                           0.081387, 0.003205, -0.0087404, -0.0028608,
+                           0.013066, 0.017012, -0.0018469, -0.0052177,
+                           -0.0035655, 0.0017846, 0.0019127, -0.00012964,
+                           0.0049428, 0.0055592, -0.0010618, -0.0016134,
+                           -0.0059098, 0.0013335, 0.00052133, 3.1119e-05,
+                           -0.004882, 0.00077317, -0.0013827, 0.003249,
+                           -0.00063689, 0.0016218, -0.00045215, -1.5815e-05};
+    fMatrix.ResizeTo(4, 8);
+    fMatrix.SetMatrixArray(elements);
+  }
+
+  float fReal_fTPCSignalN(std::vector<float>& vec1, std::vector<float>& vec2)
+  {
+    float result = 0.f;
+    // push 1.
+    vec1.insert(vec1.begin(), 1.0);
+    vec2.insert(vec2.begin(), 1.0);
+    for (int i = 0; i < fMatrix.GetNrows(); i++) {
+      for (int j = 0; j < fMatrix.GetNcols(); j++) {
+        double param = fMatrix(i, j);
+        double value1 = i > static_cast<int>(vec1.size()) ? 0 : vec1[i];
+        double value2 = j > static_cast<int>(vec2.size()) ? 0 : vec2[j];
+        result += param * value1 * value2;
+      }
+    }
+    return result;
+  }
+} Str_dEdx_correction;
 
 #endif // COMMON_TABLEPRODUCER_PID_PIDTPCBASE_H_
