1c4650fdca8a320e7114c43746d19633289383f1
Yunseul <qothdbs2001@gmail.com>
2025-06-30 23:24:15 +0900

    [PWGLF] Apply O2 linter

--- PWGLF/Tasks/Resonances/f0980analysis.cxx
+++ PWGLF/Tasks/Resonances/f0980analysis.cxx
@@ -17,6 +17,7 @@
 #include "Common/DataModel/EventSelection.h"
 #include "Common/DataModel/PIDResponse.h"
 
+#include "CommonConstants/MathConstants.h"
 #include "CommonConstants/PhysicsConstants.h"
 #include "DataFormatsParameters/GRPObject.h"
 #include "Framework/ASoAHelpers.h"
@@ -25,14 +26,8 @@
 #include <Framework/Configurable.h>
 
 #include "TVector2.h"
-#include <TLorentzVector.h>
-#include <vector>
 
-using namespace o2;
-using namespace o2::framework;
-using namespace o2::framework::expressions;
-using namespace o2::soa;
-using namespace o2::constants::physics;
+#include <vector>
 
 struct f0980analysis {
   SliceCache cache;
@@ -98,9 +93,9 @@ struct f0980analysis {
 
     AxisSpec PIDqaAxis = {120, -6, 6};
     AxisSpec pTqaAxis = {200, 0, 20};
-    AxisSpec phiqaAxis = {72, 0., 2.0 * constants::math::PI};
-    AxisSpec EPAxis = {10, 0, constants::math::PI};
-    AxisSpec EPqaAxis = {200, -constants::math::PI, constants::math::PI};
+    AxisSpec phiqaAxis = {72, 0., 2.0 * o2::constants::math::PI};
+    AxisSpec EPAxis = {10, 0, o2::constants::math::PI};
+    AxisSpec EPqaAxis = {200, -o2::constants::math::PI, o2::constants::math::PI};
     AxisSpec EPresAxis = {200, -2, 2};
 
     if (cfgFindRT) {
@@ -144,11 +139,11 @@ struct f0980analysis {
   int RTIndex(double pairphi, double lhphi)
   {
     double dphi = std::fabs(TVector2::Phi_mpi_pi(lhphi - pairphi));
-    if (dphi < constants::math::PI / 3.0)
+    if (dphi < o2::constants::math::PI / 3.0)
       return 0;
-    if (dphi < 2.0 * constants::math::PI / 3.0 && dphi > constants::math::PI / 3.0)
+    if (dphi < 2.0 * o2::constants::math::PI / 3.0 && dphi > o2::constants::math::PI / 3.0)
       return 1;
-    if (dphi > 2.0 * constants::math::PI / 3.0)
+    if (dphi > 2.0 * o2::constants::math::PI / 3.0)
       return 2;
 
     return -1;
@@ -234,7 +229,7 @@ struct f0980analysis {
     histos.fill(HIST("QA/hEPResBC"), collision.cent(), collision.evtPlResAC());
     histos.fill(HIST("QA/LTpt"), LHpt, collision.cent(), LHphi);
 
-    TLorentzVector Pion1, Pion2, Reco;
+    ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float>> Pion1, Pion2, Reco;
     for (auto& [trk1, trk2] :
          combinations(CombinationsStrictlyUpperIndexPolicy(dTracks, dTracks))) {
 
@@ -256,8 +251,8 @@ struct f0980analysis {
         continue;
 
       relPhi = TVector2::Phi_0_2pi(Reco.Phi() - collision.evtPl());
-      if (relPhi > constants::math::PI) {
-        relPhi -= constants::math::PI;
+      if (relPhi > o2::constants::math::PI) {
+        relPhi -= o2::constants::math::PI;
       }
 
       if (trk1.sign() * trk2.sign() < 0) {
@@ -268,11 +263,11 @@ struct f0980analysis {
         histos.fill(HIST("hInvMass_f0980_US_EPA"), Reco.M(), Reco.Pt(),
                     collision.cent(), relPhi);
         if constexpr (IsMC) {
-          if (abs(trk1.pdgCode()) != 211 || abs(trk2.pdgCode()) != 211)
+          if (std::abs(trk1.pdgCode()) != 211 || std::abs(trk2.pdgCode()) != 211)
             continue;
           if (trk1.motherId() != trk2.motherId())
             continue;
-          if (abs(trk1.motherPDG()) != 9010221)
+          if (std::abs(trk1.motherPDG()) != 9010221)
             continue;
           histos.fill(HIST("MCL/hpT_f0980_REC"), Reco.M(), Reco.Pt(),
                       collision.cent());
@@ -313,7 +308,7 @@ struct f0980analysis {
   void processMCTrue(aod::ResoMCParents& resoParents)
   {
     for (auto& part : resoParents) { // loop over all pre-filtered MC particles
-      if (abs(part.pdgCode()) != 9010221)
+      if (std::abs(part.pdgCode()) != 9010221)
         continue;
       if (!part.producedByGenerator())
         continue;
@@ -321,8 +316,8 @@ struct f0980analysis {
         continue;
       }
       bool pass = false;
-      if ((abs(part.daughterPDG1()) == 211 &&
-           abs(part.daughterPDG2()) == 211)) {
+      if ((std::abs(part.daughterPDG1()) == 211 &&
+           std::abs(part.daughterPDG2()) == 211)) {
         pass = true;
       }
       if (!pass) // If we have both decay products
