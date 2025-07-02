105db492889ab86af2316ccae939607d5d286a74
Nicolò Jacazio <njacazio@users.noreply.github.com>
2025-07-02 16:20:08 +0200

    [PWGHF] Use namespace for MetadataHelper (#11873)

--- PWGHF/TableProducer/mcPidTof.cxx
+++ PWGHF/TableProducer/mcPidTof.cxx
@@ -66,7 +66,7 @@ using namespace o2::pid;
 using namespace o2::framework::expressions;
 using namespace o2::track;
 
-MetadataHelper metadataInfo;
+o2::common::core::MetadataHelper metadataInfo;
 
 // Input data types
 using Trks = o2::soa::Join<aod::TracksIU, aod::TracksExtra>;
