ccf47b01a12ee3980328d6dd834b51b53538a55b
Jaideep Tanwar <141036812+jtanwar2212@users.noreply.github.com>
2025-07-07 18:13:46 +0530

    Update nucleitpcpbpb.cxx

--- PWGLF/Tasks/Nuspex/nucleitpcpbpb.cxx
+++ PWGLF/Tasks/Nuspex/nucleitpcpbpb.cxx
@@ -1,7 +1,3 @@
-/// \file NucleitpcPbPb.cxx
-/// \brief nuclei analysis
-/// \author Jaideep Tanwar <jaideep.tanwar@cern.ch>
-
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
@@ -12,6 +8,10 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
+
+/// \file NucleitpcPbPb.cxx
+/// \brief nuclei analysis
+/// \author Jaideep Tanwar <jaideep.tanwar@cern.ch>
 
 #include "Common/Core/PID/PIDTOF.h"
 #include "Common/Core/PID/TPCPIDResponse.h"
