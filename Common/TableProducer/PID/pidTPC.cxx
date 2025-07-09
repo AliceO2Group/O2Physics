afbd3b9cdf7ecb180b46ac87da414d220d422549
Nicolò Jacazio <njacazio@users.noreply.github.com>
2025-07-09 14:06:01 +0200

    Merge the base and non base TPC tasks (as exectuable)

--- Common/TableProducer/PID/pidTPC.cxx
+++ Common/TableProducer/PID/pidTPC.cxx
@@ -9,7 +9,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-///
+
 /// \file   pidTPC.cxx
 /// \author Nicolò Jacazio nicolo.jacazio@cern.ch
 /// \author Christian Sonnabend christian.sonnabend@cern.ch
@@ -17,18 +17,21 @@
 /// \author Jeremy Wilkinson jeremy.wilkinson@cern.ch
 /// \brief  Task to produce PID tables for TPC split for each particle.
 ///         Only the tables for the mass hypotheses requested are filled, and only for the requested table size ("Full" or "Tiny"). The others are sent empty.
-///
+
+
 #include <map>
 #include <memory>
 #include <string>
 #include <utility>
 #include <vector>
-// ROOT includes
-#include "TFile.h"
-#include "TRandom.h"
-#include "TSystem.h"
+#include <string>
+#include <utility>
+#include <vector>
 
-// O2 includes
+#include <TFile.h>
+#include <TRandom.h>
+#include <TSystem.h>
+
 #include "MetadataHelper.h"
 #include "TableHelper.h"
 #include "pidTPCBase.h"
@@ -47,6 +50,17 @@
 #include "Framework/runDataProcessing.h"
 #include "ReconstructionDataFormats/Track.h"
 
+#include "TableHelper.h"
+#include "pidTPCBase.h"
+
+#include "Common/CCDB/ctpRateFetcher.h"
+#include "Common/DataModel/FT0Corrected.h"
+
+#include "CCDB/BasicCCDBManager.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/runDataProcessing.h"
+#include "ReconstructionDataFormats/Track.h"
+
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::pid;
@@ -57,11 +71,145 @@ using namespace o2::ml;
 
 MetadataHelper metadataInfo; // Metadata helper
 
-void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
-{
-  std::vector<ConfigParamSpec> options{{"add-qa", VariantType::Int, 0, {"Legacy. No effect."}}};
-  std::swap(workflowOptions, options);
-}
+struct PidMultiplicity {
+  SliceCache cache;
+  Produces<aod::PIDMults> mult;
+
+  bool enableTable = false;
+  void init(InitContext& initContext)
+  {
+    LOG(info) << "Initializing PID Mult Task";
+    // Checking that the table is requested in the workflow and enabling it
+    enableTable = isTableRequiredInWorkflow(initContext, "PIDMults");
+    if (enableTable) {
+      LOG(info) << "Table TPC PID Multiplicity enabled!";
+    }
+    if (doprocessStandard == true && doprocessIU == true) {
+      LOG(fatal) << "Both processStandard and processIU are enabled, pick one!";
+    }
+  }
+
+  using TrksIU = soa::Join<aod::TracksIU, aod::TracksExtra>;
+  Partition<TrksIU> tracksWithTPCIU = (aod::track::tpcNClsFindable > (uint8_t)0);
+  void processIU(aod::Collision const& collision, TrksIU const&)
+  {
+    if (!enableTable) {
+      return;
+    }
+    auto tracksGrouped = tracksWithTPCIU->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
+    mult(tracksGrouped.size());
+  }
+  PROCESS_SWITCH(PidMultiplicity, processIU, "Process with IU tracks, faster but works on Run3 only", false);
+
+  using Trks = soa::Join<aod::Tracks, aod::TracksExtra>;
+  Partition<Trks> tracksWithTPC = (aod::track::tpcNClsFindable > (uint8_t)0);
+  void processStandard(aod::Collision const& collision, Trks const&)
+  {
+    if (!enableTable) {
+      return;
+    }
+    auto tracksGrouped = tracksWithTPC->sliceByCached(aod::track::collisionId, collision.globalIndex(), cache);
+    mult(tracksGrouped.size());
+  }
+  PROCESS_SWITCH(PidMultiplicity, processStandard, "Process with tracks, needs propagated tracks", true);
+};
+
+struct DeDxCorrection {
+  Produces<aod::DEdxsCorrected> dEdxCorrected;
+  using BCsRun3 = soa::Join<aod::BCs, aod::Timestamps, aod::BcSels, aod::Run3MatchedToBCSparse>;
+  using ColEvSels = soa::Join<aod::Collisions, aod::EvSels, aod::Mults>;
+  using FullTracksIU = soa::Join<aod::TracksIU, aod::TracksExtra>;
+
+  uint64_t minGlobalBC = 0;
+  Service<o2::ccdb::BasicCCDBManager> ccdb;
+  ctpRateFetcher mRateFetcher;
+
+  Str_dEdx_correction str_dedx_correction;
+
+  // void init(InitContext& initContext)
+  void init(o2::framework::InitContext&)
+  {
+    ccdb->setURL("http://alice-ccdb.cern.ch");
+    ccdb->setCaching(true);
+    ccdb->setLocalObjectValidityChecking();
+    str_dedx_correction.init();
+  }
+
+  void processRun3(
+    ColEvSels const& cols,
+    FullTracksIU const& tracks,
+    aod::BCsWithTimestamps const& bcs)
+  {
+    const uint64_t outTable_size = tracks.size();
+    dEdxCorrected.reserve(outTable_size);
+
+    for (auto const& trk : tracks) {
+      double hadronicRate;
+      int multTPC;
+      int occupancy;
+      if (trk.has_collision()) {
+        auto collision = cols.iteratorAt(trk.collisionId());
+        auto bc = collision.bc_as<aod::BCsWithTimestamps>();
+        const int runnumber = bc.runNumber();
+        hadronicRate = mRateFetcher.fetch(ccdb.service, bc.timestamp(), runnumber, "ZNC hadronic") * 1.e-3; // kHz
+        multTPC = collision.multTPC();
+        occupancy = collision.trackOccupancyInTimeRange();
+      } else {
+        auto bc = bcs.begin();
+        const int runnumber = bc.runNumber();
+        hadronicRate = mRateFetcher.fetch(ccdb.service, bc.timestamp(), runnumber, "ZNC hadronic") * 1.e-3; // kHz
+        multTPC = 0;
+        occupancy = 0;
+      }
+
+      float fTPCSignal = trk.tpcSignal();
+      float fNormMultTPC = multTPC / 11000.;
+
+      float fTrackOccN = occupancy / 1000.;
+      float fOccTPCN = fNormMultTPC * 10; //(fNormMultTPC*10).clip(0,12)
+      if (fOccTPCN > 12)
+        fOccTPCN = 12;
+      else if (fOccTPCN < 0)
+        fOccTPCN = 0;
+
+      float fTrackOccMeanN = hadronicRate / 5;
+      float side = trk.tgl() > 0 ? 1 : 0;
+      float a1pt = std::abs(trk.signed1Pt());
+      float a1pt2 = a1pt * a1pt;
+      float atgl = std::abs(trk.tgl());
+      float mbb0R = 50 / fTPCSignal;
+      if (mbb0R > 1.05)
+        mbb0R = 1.05;
+      else if (mbb0R < 0.05)
+        mbb0R = 0.05;
+      // float mbb0R =  max(0.05,  min(50 / fTPCSignal, 1.05));
+      float a1ptmbb0R = a1pt * mbb0R;
+      float atglmbb0R = atgl * mbb0R;
+
+      std::vector<float> vec_occu = {fTrackOccN, fOccTPCN, fTrackOccMeanN};
+      std::vector<float> vec_track = {mbb0R, a1pt, atgl, atglmbb0R, a1ptmbb0R, side, a1pt2};
+
+      float fTPCSignalN_CR0 = str_dedx_correction.fReal_fTPCSignalN(vec_occu, vec_track);
+
+      float mbb0R1 = 50 / (fTPCSignal / fTPCSignalN_CR0);
+      if (mbb0R1 > 1.05)
+        mbb0R1 = 1.05;
+      else if (mbb0R1 < 0.05)
+        mbb0R1 = 0.05;
+
+      std::vector<float> vec_track1 = {mbb0R1, a1pt, atgl, atgl * mbb0R1, a1pt * mbb0R1, side, a1pt2};
+      float fTPCSignalN_CR1 = str_dedx_correction.fReal_fTPCSignalN(vec_occu, vec_track1);
+
+      float corrected_dEdx = fTPCSignal / fTPCSignalN_CR1;
+      dEdxCorrected(corrected_dEdx);
+    }
+  }
+  PROCESS_SWITCH(DeDxCorrection, processRun3, "dEdx correction process", false);
+
+  void processDummy(aod::Collisions const&) {}
+
+  PROCESS_SWITCH(DeDxCorrection, processDummy, "Do nothing", true);
+};
 
 /// Task to produce the response table
 struct tpcPid {
@@ -774,5 +922,8 @@ struct tpcPid {
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   metadataInfo.initMetadata(cfgc); // Parse AO2D metadata
-  return WorkflowSpec{adaptAnalysisTask<tpcPid>(cfgc)};
+
+  return WorkflowSpec{adaptAnalysisTask<PidMultiplicity>(cfgc),
+                      adaptAnalysisTask<DeDxCorrection>(cfgc),
+                      adaptAnalysisTask<tpcPid>(cfgc)};
 }
