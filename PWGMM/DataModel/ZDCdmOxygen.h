76906129fb4a5cdf01c77fb0b23f61696ebbf4ea
Chiara Oppedisano <chiara.oppedisano@to.infn.it>
2025-07-12 09:28:48 +0200

    Adding task for ZDC in O-O/Ne-Ne/p-O

--- PWGMM/DataModel/ZDCdmOxygen.h
+++ PWGMM/DataModel/ZDCdmOxygen.h
@@ -41,13 +41,13 @@ DECLARE_SOA_COLUMN(ZncPm4, zncPM4, float);                 //! ADC PM4 ZNC      
 DECLARE_SOA_COLUMN(ZpaTdc, zpaTDC, float);                 //! TDC ZPA           // o2-linter: disable=name/o2-column
 DECLARE_SOA_COLUMN(ZpaAmpl, zpAmplc, float);               //! amplitude ZPA     // o2-linter: disable=name/o2-column
 DECLARE_SOA_COLUMN(ZpaPmc, zpaPMC, float);                 //! ADC PMC ZPA       // o2-linter: disable=name/o2-column
-DECLARE_SOA_COLUMN(ZpaTdc, zpcTDC, float);                 //! TDC ZPC           // o2-linter: disable=name/o2-column
-DECLARE_SOA_COLUMN(ZpaAmpl, zpcAmpl, float);               //! amplitude ZPA     // o2-linter: disable=name/o2-column
-DECLARE_SOA_COLUMN(ZpaPmc, zpcPMC, float);                 //! ADC PMC ZPA       // o2-linter: disable=name/o2-column
+DECLARE_SOA_COLUMN(ZpcTdc, zpcTDC, float);                 //! TDC ZPC           // o2-linter: disable=name/o2-column
+DECLARE_SOA_COLUMN(ZpcAmpl, zpcAmpl, float);               //! amplitude ZPA     // o2-linter: disable=name/o2-column
+DECLARE_SOA_COLUMN(ZpcPmc, zpcPMC, float);                 //! ADC PMC ZPA       // o2-linter: disable=name/o2-column
 DECLARE_SOA_COLUMN(Zem1Tdc, zem1TDC, float);               //! TDC ZEM1          // o2-linter: disable=name/o2-column
 DECLARE_SOA_COLUMN(Zem1Ampl, zemampl, float);              //! amplitude ZEM1    // o2-linter: disable=name/o2-column
 DECLARE_SOA_COLUMN(Zem2Tdc, zem2TDC, float);               //! TDC ZEM2          // o2-linter: disable=name/o2-column
-DECLARE_SOA_COLUMN(Zem2ampl, zem2Ampl, float);             //! amplitude ZEM2    // o2-linter: disable=name/o2-column
+DECLARE_SOA_COLUMN(Zem2Ampl, zem2Ampl, float);             //! amplitude ZEM2    // o2-linter: disable=name/o2-column
 DECLARE_SOA_COLUMN(MultFt0A, multFT0A, float);             //! mult. FIT-A       // o2-linter: disable=name/o2-column
 DECLARE_SOA_COLUMN(MultFt0C, multFT0C, float);             //! mult. FIT-C       // o2-linter: disable=name/o2-column
 DECLARE_SOA_COLUMN(MultV0A, multV0A, float);               //! mult. V0-A        // o2-linter: disable=name/o2-column
@@ -59,35 +59,35 @@ DECLARE_SOA_COLUMN(SelectionBits, selectionBits, uint8_t); //! Selection Flags  
 } // namespace zdctable
 
 DECLARE_SOA_TABLE(ZdcTable, "AOD", "ZdcTeble",
-                  zdcTableOO::ZNAtdc,
-                  zdcTableOO::ZNAampl,
-                  zdcTableOO::ZNApmc,
-                  zdcTableOO::ZNApm1,
-                  zdcTableOO::ZNApm2,
-                  zdcTableOO::ZNApm3,
-                  zdcTableOO::ZNApm4,
-                  zdcTableOO::ZNCtdc,
-                  zdcTableOO::ZNCampl,
-                  zdcTableOO::ZNCpmc,
-                  zdcTableOO::ZNCpm1,
-                  zdcTableOO::ZNCpm2,
-                  zdcTableOO::ZNCpm3,
-                  zdcTableOO::ZNCpm4,
-                  zdcTableOO::ZPAtdc,
-                  zdcTableOO::ZPAampl,
-                  zdcTableOO::ZPCtdc,
-                  zdcTableOO::ZPCampl,
-                  zdcTableOO::ZEM1tdc,
-                  zdcTableOO::ZEM1ampl,
-                  zdcTableOO::ZEM2tdc,
-                  zdcTableOO::ZEM2ampl,
-                  zdcTableOO::MultFT0A,
-                  zdcTableOO::MultFT0C,
+                  zdcTableOO::ZnaTdc,
+                  zdcTableOO::ZnaAmpl,
+                  zdcTableOO::ZnaPmc,
+                  zdcTableOO::ZnaPm1,
+                  zdcTableOO::ZnaPm2,
+                  zdcTableOO::ZnaPm3,
+                  zdcTableOO::ZnaPm4,
+                  zdcTableOO::ZncTdc,
+                  zdcTableOO::ZncAmpl,
+                  zdcTableOO::ZncPmc,
+                  zdcTableOO::ZncPm1,
+                  zdcTableOO::ZncPm2,
+                  zdcTableOO::ZncPm3,
+                  zdcTableOO::ZncPm4,
+                  zdcTableOO::ZpaTdc,
+                  zdcTableOO::ZpaAmpl,
+                  zdcTableOO::ZpcTdc,
+                  zdcTableOO::ZpcAmpl,
+                  zdcTableOO::Zem1Tdc,
+                  zdcTableOO::Zem1Ampl,
+                  zdcTableOO::Zem2Tdc,
+                  zdcTableOO::Zem2Ampl,
+                  zdcTableOO::MultFt0A,
+                  zdcTableOO::MultFt0C,
                   zdcTableOO::MultV0A,
                   zdcTableOO::Zvertex,
-                  zdcTableOO::CentralityFT0C,
-                  zdcTableOO::CentralityFT0A,
-                  zdcTableOO::CentralityFT0M,
+                  zdcTableOO::CentralityFt0C,
+                  zdcTableOO::CentralityFt0A,
+                  zdcTableOO::CentralityFt0M,
                   zdcTableOO::SelectionBits);
 } // namespace o2::aod
 
