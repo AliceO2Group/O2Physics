403a7270d15ccd2fc4119565a1ea528035e325ef
Luca Aglietta <75362880+Luca610@users.noreply.github.com>
2025-07-16 18:02:36 +0200

    [PWGHF] Refactor of resonances workflow, pt 2 (#12014)
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

--- PWGHF/Core/DecayChannels.h
+++ PWGHF/Core/DecayChannels.h
@@ -219,6 +219,40 @@ enum DecayChannelToJpsiResonant : int8_t {
   NChannelsToJpsiResonant = BsToJpsiPhi // last channel
 };
 } // namespace hf_cand_beauty
+
+namespace hf_cand_reso
+{
+/// @brief resonance candidates: main channels
+enum DecayChannelMain : int8_t {
+  // D1(2420)0
+  D1zeroToDstarPi = 1, // D*+ π-
+  // D2*(2460)0
+  D2starzeroToDplusPi = 2, // D+ π−
+  D2starzeroToDstarPi = 3, // D*+ π-
+  // D2*(2460)+
+  D2starplusToD0Pi = 4, // D0 π+
+  // Ds1(2536)+
+  Ds1ToDstarK0s = 5, // D*+ K0s
+  // Ds2*(2573)+
+  Ds2starToD0Kplus = 6,  // D0 K+
+  Ds2starToDplusK0s = 7, // D+ K0s
+  Ds2starToDstarK0s = 8, // D*+ K0s
+  // Ds1*(2700)+
+  Ds1star2700ToDstarK0s = 9, // D*+ K0s
+  // Ds1*(2860)+
+  Ds1star2860ToDstarK0s = 10, // D*+ K0s
+  // Ds3*(2860)+
+  Ds3star2860ToDstarK0s = 11, // D*+ K0s
+  // Xic(3055)0
+  Xic3055zeroToD0Lambda = 12, // D0 Λ
+  // Xic(3055)+
+  Xic3055plusToDplusLambda = 13, // D+ Λ
+  // Xic(3080)0
+  Xic3080zeroToD0Lambda = 14, // D0 Λ
+  // Xic(3080)+
+  Xic3080plusToDplusLambda = 15 // D+ Λ
+};
+} // namespace hf_cand_reso
 } // namespace o2::hf_decay
 
 #endif // PWGHF_CORE_DECAYCHANNELS_H_
