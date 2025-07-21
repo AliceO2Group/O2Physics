28126ae4d7ef15d19b319f3b052eddfb29a25c9f
Chiara Oppedisano <chiara.oppedisano@to.infn.it>
2025-07-21 17:23:33 +0200

    Trying to fix linter related issues

--- PWGMM/DataModel/ZDCdmOxygen.h
+++ PWGMM/DataModel/ZDCdmOxygen.h
@@ -27,14 +27,14 @@ namespace zdcTableLiIo
 {
 DECLARE_SOA_COLUMN(ZnaTdc, znaTdc, float);                 //! Tdc ZNA
 DECLARE_SOA_COLUMN(ZnaAmpl, znaAmpl, float);               //! amplitude ZNA
-DECLARE_SOA_COLUMN(ZnaPmc, znaPmC, float);                 //! ADC PmC ZNA
+DECLARE_SOA_COLUMN(ZnaPmc, znaPmc, float);                 //! ADC PmC ZNA
 DECLARE_SOA_COLUMN(ZnaPm1, znaPm1, float);                 //! ADC Pm1 ZNA
 DECLARE_SOA_COLUMN(ZnaPm2, znaPm2, float);                 //! ADC Pm2 ZNA
 DECLARE_SOA_COLUMN(ZnaPm3, znaPm3, float);                 //! ADC Pm3 ZNA
 DECLARE_SOA_COLUMN(ZnaPm4, znaPm4, float);                 //! ADC Pm4 ZNA
 DECLARE_SOA_COLUMN(ZncTdc, zncTdc, float);                 //! Tdc ZNC
 DECLARE_SOA_COLUMN(ZncAmpl, zncAmpl, float);               //! amplitude ZNC
-DECLARE_SOA_COLUMN(ZncPmc, zncPmC, float);                 //! ADC PmC ZNC
+DECLARE_SOA_COLUMN(ZncPmc, zncPmc, float);                 //! ADC PmC ZNC
 DECLARE_SOA_COLUMN(ZncPm1, zncPm1, float);                 //! ADC Pm1 ZNC
 DECLARE_SOA_COLUMN(ZncPm2, zncPm2, float);                 //! ADC Pm2 ZNC
 DECLARE_SOA_COLUMN(ZncPm3, zncPm3, float);                 //! ADC Pm3 ZNC
@@ -44,18 +44,18 @@ DECLARE_SOA_COLUMN(ZpaAmpl, zpAmplc, float);               //! amplitude ZPA
 DECLARE_SOA_COLUMN(ZpaPmc, zpaPmC, float);                 //! ADC PmC ZPA
 DECLARE_SOA_COLUMN(ZpcTdc, zpcTdc, float);                 //! Tdc ZPC
 DECLARE_SOA_COLUMN(ZpcAmpl, zpcAmpl, float);               //! amplitude ZPA
-DECLARE_SOA_COLUMN(ZpcPmc, zpcPmC, float);                 //! ADC PmC ZPA
+DECLARE_SOA_COLUMN(ZpcPmc, zpcPmc, float);                 //! ADC PmC ZPA
 DECLARE_SOA_COLUMN(Zem1Tdc, zem1Tdc, float);               //! Tdc ZEM1
 DECLARE_SOA_COLUMN(Zem1Ampl, zemampl, float);              //! amplitude ZEM1
 DECLARE_SOA_COLUMN(Zem2Tdc, zem2Tdc, float);               //! Tdc ZEM2
 DECLARE_SOA_COLUMN(Zem2Ampl, zem2Ampl, float);             //! amplitude ZEM2
-DECLARE_SOA_COLUMN(MultFt0A, multFT0A, float);             //! mult. FIT-A
-DECLARE_SOA_COLUMN(MultFt0C, multFT0C, float);             //! mult. FIT-C
-DECLARE_SOA_COLUMN(MultV0A, multV0A, float);               //! mult. V0-A
+DECLARE_SOA_COLUMN(MultFt0a, multFt0a, float);             //! mult. FIT-A
+DECLARE_SOA_COLUMN(MultFt0c, multFt0c, float);             //! mult. FIT-C
+DECLARE_SOA_COLUMN(MultV0a, multV0a, float);               //! mult. V0-A
 DECLARE_SOA_COLUMN(Zvertex, zVertex, float);               //! Z vertex
-DECLARE_SOA_COLUMN(CentralityFt0C, centralityFT0C, float); //! Centrality
-DECLARE_SOA_COLUMN(CentralityFt0A, centralityFT0A, float); //! Centrality
-DECLARE_SOA_COLUMN(CentralityFt0M, centralityFT0M, float); //! Centrality
+DECLARE_SOA_COLUMN(CentralityFt0c, centralityFt0c, float); //! Centrality
+DECLARE_SOA_COLUMN(CentralityFt0a, centralityFt0a, float); //! Centrality
+DECLARE_SOA_COLUMN(CentralityFt0m, centralityFt0m, float); //! Centrality
 DECLARE_SOA_COLUMN(SelectionBits, selectionBits, uint8_t); //! Selection Flags
 } // namespace zdcTableLiIo
 
@@ -82,13 +82,13 @@ DECLARE_SOA_TABLE(ZdcTable, "AOD", "ZdcTeble",
                   zdcTableLiIo::Zem1Ampl,
                   zdcTableLiIo::Zem2Tdc,
                   zdcTableLiIo::Zem2Ampl,
-                  zdcTableLiIo::MultFt0A,
-                  zdcTableLiIo::MultFt0C,
-                  zdcTableLiIo::MultV0A,
+                  zdcTableLiIo::MultFt0a,
+                  zdcTableLiIo::MultFt0c,
+                  zdcTableLiIo::MultV0a,
                   zdcTableLiIo::Zvertex,
-                  zdcTableLiIo::CentralityFt0C,
-                  zdcTableLiIo::CentralityFt0A,
-                  zdcTableLiIo::CentralityFt0M,
+                  zdcTableLiIo::CentralityFt0c,
+                  zdcTableLiIo::CentralityFt0a,
+                  zdcTableLiIo::CentralityFt0m,
                   zdcTableLiIo::SelectionBits);
 } // namespace o2::aod
 
