c8044540212dab2e3ce3de27d456a310c8a8ace2
INFNuser <INFNuser@mtcamerl.ba.infn.it>
2025-07-30 11:32:44 +0200

    Cleaned inclusion of AE model inference

--- PWGHF/Core/HfAeToMseXicToPKPi.h
+++ PWGHF/Core/HfAeToMseXicToPKPi.h
@@ -66,7 +66,7 @@ class HfAeToMseXicToPKPi : public HfMlResponse<TypeOutputScore>
   void unsetMinMaxScaling(std::vector<float>& yOut, std::vector<float> yIn, std::vector<float> scaleMin, std::vector<float> scaleMax)
   {
     yOut.clear();                             // initial clear to avoid multiple filling if setMinMax o setScaling are called more than once
-    for (size_t j = 0; j < yIn.size(); ++j ) { // loop for over the features
+    for (size_t j = 0; j < yIn.size(); ++j) { // loop for over the features
       // MinMax scaling of the input features
       LOG(debug) << "--------------> MinMax unscaling Debug \t" << scaleMin.at(j) << "\t" << scaleMax.at(j);
       yOut.push_back(yIn.at(j) * (scaleMax.at(j) - scaleMin.at(j)) + scaleMin.at(j));
@@ -84,36 +84,35 @@ class HfAeToMseXicToPKPi : public HfMlResponse<TypeOutputScore>
       if (scaleType == 1) {
         LOG(debug) << "MinMax unscaling will be applied";
         unsetMinMaxScaling(yOutRescaled, yIn, scaleMin, scaleMax);
-      }  //... with scaleType > 1 we could add other preprocessing trasformations
+      } //... with scaleType > 1 we could add other preprocessing trasformations
     }
   }
 
   std::vector<float> getPostprocessedOutput()
   {
     for (size_t j = 0; j < yOutRescaled.size(); ++j)
-      LOG(debug)<<"Global rescaled AE output = "<< yOutRescaled.at(j);
+      LOG(debug) << "Global rescaled AE output = " << yOutRescaled.at(j);
     return yOutRescaled;
   }
   //---- MSE function
   float getMse(std::vector<float> yTrue, std::vector<float> yPred)
   {
     LOG(debug) << "Inside getMse sizes " << yTrue.size() << "\t" << yPred.size();
-    float mse= 0.0f;
+    float mse = 0.0f;
     float sum = 0.0f;
     for (size_t j = 0; j < yTrue.size(); ++j)
       LOG(debug) << "Local Feature = " << j << " ----> input = " << yTrue.at(j) << " scaled feature = " << yPred.at(j);
     std::vector<float> yTrueScaled = getPreprocessedFeatures(); // to make the input features adimensional
-    if (yTrue.size() != yPred.size())
-    {
+    if (yTrue.size() != yPred.size()) {
       LOG(debug) << "size of input vector =" << yTrue.size();
       LOG(debug) << "size of AE output vector =" << yPred.size();
       LOG(fatal) << "vectors of input and predictions don't have the same size";
-    } else { //MSE
+    } else { // MSE
       for (size_t j = 0; j < yPred.size(); ++j) {
-        sum += std::pow(((yTrueScaled).at(j) - (yPred).at(j)), 2); //AE model gives adimensional predictions by design choice
+        sum += std::pow(((yTrueScaled).at(j) - (yPred).at(j)), 2); // AE model gives adimensional predictions by design choice
         LOG(debug) << "getMse Local feature = " << j << " ----> input = " << yTrueScaled.at(j) << " AE prediction = " << yPred.at(j);
       }
-      mse = sum/yPred.size(); // MSE of a candidate
+      mse = sum / yPred.size(); // MSE of a candidate
       LOG(debug) << "Local mse " << mse;
     }
     return mse;
