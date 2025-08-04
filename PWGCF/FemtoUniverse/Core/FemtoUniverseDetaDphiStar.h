f19f5d8f9919cb88121a24b7ac570b921d885956
Pritam Chakraborty <47203359+prchakra@users.noreply.github.com>
2025-08-04 11:44:47 +0200

    Fixing megalinter error

--- PWGCF/FemtoUniverse/Core/FemtoUniverseDetaDphiStar.h
+++ PWGCF/FemtoUniverse/Core/FemtoUniverseDetaDphiStar.h
@@ -447,7 +447,9 @@ class FemtoUniverseDetaDphiStar
           LOG(fatal) << "FemtoUniverseDetaDphiStar: passed arguments don't agree with FemtoUniverseDetaDphiStar's type of events! Please provide same or mixed.";
         }
         return false;
-      }
+      } else {
+      LOG(fatal) << "FemtoUniversePairCleaner: Combination of objects not defined - quitting!";
+      return false;
     }
   }
 
