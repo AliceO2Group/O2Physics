c923c553bd96dcd8692580e3134890c2bb4a13c0
Yunfan Liu <yunfan.liu@cern.ch>
2025-08-05 08:43:43 +0800

    Update femtoDreamContainer.h

--- PWGCF/FemtoDream/Core/femtoDreamContainer.h
+++ PWGCF/FemtoDream/Core/femtoDreamContainer.h
@@ -203,7 +203,7 @@ class FemtoDreamContainer
     const float kT = FemtoDreamMath::getkT(part1, mMassOne, part2, mMassTwo);
     if constexpr (isHF) {
       float mP2;
-      if (candidateSelFlag() == o2::aod::fdhf::dplusToPiKPi) {
+      if (part2.candidateSelFlag() == o2::aod::fdhf::dplusToPiKPi) {
         mP2 = part2.m(std::array{o2::constants::physics::MassPiPlus, o2::constants::physics::MassKPlus, o2::constants::physics::MassPiPlus});
       } else if (part2.candidateSelFlag() == o2::aod::fdhf::lcToPKPi) {
         mP2 = part2.m(std::array{o2::constants::physics::MassProton, o2::constants::physics::MassKPlus, o2::constants::physics::MassPiPlus});
