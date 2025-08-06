c5fff20e8c636e8d2582b18bfdcf43eb7cc19376
Bong-Hwi Lim <bong-hwi.lim@cern.ch>
2025-08-06 15:21:35 +0900

    Avoid magic number issue

--- PWGCF/JCorran/Tasks/jFlucEfficiencyTask.cxx
+++ PWGCF/JCorran/Tasks/jFlucEfficiencyTask.cxx
@@ -49,6 +49,8 @@ struct JFlucEfficiencyTask {
     170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0, 250.0, 260.0,
     270.0, 280.0, 290.0, 300.0};
 
+    constexpr double kChargeThreshold = 3.0; // PDG charge units: 3 = |e|
+
   // Update the axisPt configuration with proper vector initialization
   ConfigurableAxis axisPt{"axisPt", std::vector<double>(PttJacek.begin(), PttJacek.end()), "pT axis"};
 
@@ -274,7 +276,7 @@ struct JFlucEfficiencyTask {
     if (p != nullptr) {
       charge = p->Charge();
     }
-    return std::abs(charge) >= 3.;
+    return std::abs(charge) >= kChargeThreshold;
   }
   // Track selection
   template <typename TrackType>
