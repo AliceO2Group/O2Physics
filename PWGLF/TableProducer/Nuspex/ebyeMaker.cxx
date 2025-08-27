3e2bf4c2a07f9fa6e2611ea31ea25bf92dc1c8ea
Mario Ciacco <mario.ciacco@cern.ch>
2025-08-27 12:07:32 +0200

    fix conditional statement

--- PWGLF/TableProducer/Nuspex/ebyeMaker.cxx
+++ PWGLF/TableProducer/Nuspex/ebyeMaker.cxx
@@ -855,7 +855,7 @@ struct EbyeMaker {
       auto pdgCode = mcPart.pdgCode();
       auto genPt = std::hypot(mcPart.px(), mcPart.py());
       if ((std::abs(pdgCode) == PDG_t::kPiPlus || std::abs(pdgCode) == PDG_t::kElectron || std::abs(pdgCode) == PDG_t::kMuonMinus || std::abs(pdgCode) == PDG_t::kKPlus || std::abs(pdgCode) == PDG_t::kProton) && mcPart.isPhysicalPrimary() && genPt > ptMin[0] && genPt < ptMax[0]) {
-        int ch = (pdgCode == PDG_t::kPiPlus || pdgCode == -PDG_t::kElectron || pdgCode == -PDG_t::kMuonMinus || pdgCode == PDG_t::kKPlus || PDG_t::kProton) ? 1 : -1;
+        int ch = (pdgCode == PDG_t::kPiPlus || pdgCode == -PDG_t::kElectron || pdgCode == -PDG_t::kMuonMinus || pdgCode == PDG_t::kKPlus || pdgCode == PDG_t::kProton) ? 1 : -1;
         if ((ch < 0 && countOnlyNegTrk) || !countOnlyNegTrk)
           nChPartGen++;
       }
