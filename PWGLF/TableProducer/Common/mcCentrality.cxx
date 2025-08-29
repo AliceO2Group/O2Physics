71ed16d4f5bfacea24d2cf9122156fb73e02cdbc
Nicolò Jacazio <njacazio@users.noreply.github.com>
2025-08-29 14:37:42 +0200

    Allow FT0A and FT0C
    Refactor histogram retrieval and configuration for FT0A and FT0C.

--- PWGLF/TableProducer/Common/mcCentrality.cxx
+++ PWGLF/TableProducer/Common/mcCentrality.cxx
@@ -18,21 +18,25 @@
 ///
 
 // O2 includes
-#include "CCDB/BasicCCDBManager.h"
-#include "ReconstructionDataFormats/Track.h"
-#include "CCDB/CcdbApi.h"
+#include "PWGLF/DataModel/mcCentrality.h"
+
+#include "TableHelper.h"
+
+#include "PWGLF/Utils/inelGt.h"
+
+#include "Common/DataModel/Centrality.h"
 #include "Common/DataModel/TrackSelectionTables.h"
-#include "Framework/AnalysisTask.h"
+
+#include "CCDB/BasicCCDBManager.h"
+#include "CCDB/CcdbApi.h"
 #include "Framework/AnalysisDataModel.h"
-#include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
 #include "Framework/HistogramRegistry.h"
+#include "Framework/O2DatabasePDGPlugin.h"
 #include "Framework/RunningWorkflowInfo.h"
 #include "Framework/StaticFor.h"
-#include "TableHelper.h"
-#include "Framework/O2DatabasePDGPlugin.h"
-#include "Common/DataModel/Centrality.h"
-#include "PWGLF/DataModel/mcCentrality.h"
-#include "PWGLF/Utils/inelGt.h"
+#include "Framework/runDataProcessing.h"
+#include "ReconstructionDataFormats/Track.h"
 
 using namespace o2;
 using namespace o2::framework;
@@ -58,14 +62,17 @@ struct mcCentrality {
   Configurable<bool> selectPrimaries{"selectPrimaries", true, "Select only primary particles"};
   Service<o2::framework::O2DatabasePDG> pdgDB;
   ConfigurableAxis binsPercentile{"binsPercentile", {VARIABLE_WIDTH, 0, 0.001, 0.01, 1.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0}, "Binning of the percentile axis"};
+  ConfigurableAxis binsMultiplicity{"binsMultiplicity", {1000, 0, 5000}, "Binning of the multiplicity axis"};
+  Configurable<bool> fillFt0A{"fillFt0A", false, "Fills the FT0A histogram"};
+  Configurable<bool> fillFt0C{"fillFt0C", false, "Fills the FT0C histogram"};
 
   HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
   TH1F* h1dFT0M;
-  /*TH1F* h1dFT0A;
+  TH1F* h1dFT0A;
   TH1F* h1dFT0C;
-  TH1F* h1dFDD;
-  TH1F* h1dNTP;*/
+  // TH1F* h1dFDD;
+  // TH1F* h1dNTP;
 
   o2::pwglf::ParticleCounter<o2::framework::O2DatabasePDG> mCounter;
 
@@ -81,7 +88,15 @@ struct mcCentrality {
     mCounter.mPdgDatabase = pdgDB.service;
     mCounter.mSelectPrimaries = selectPrimaries.value;
     histos.add("FT0M/percentile", "FT0M percentile.", HistType::kTH1D, {{binsPercentile, "FT0M percentile"}});
-    histos.add("FT0M/percentilevsMult", "FT0M percentile.", HistType::kTH2D, {{binsPercentile, "FT0M percentile"}, {1000, 0, 5000, "FT0M mult."}});
+    histos.add("FT0M/percentilevsMult", "FT0M percentile.", HistType::kTH2D, {{binsPercentile, "FT0M percentile"}, {binsMultiplicity, "FT0M mult."}});
+    if (fillFt0A) {
+      histos.add("FT0A/percentile", "FT0A percentile.", HistType::kTH1D, {{binsPercentile, "FT0A percentile"}});
+      histos.add("FT0A/percentilevsMult", "FT0A percentile.", HistType::kTH2D, {{binsPercentile, "FT0A percentile"}, {binsMultiplicity, "FT0A mult."}});
+    }
+    if (fillFt0C) {
+      histos.add("FT0C/percentile", "FT0C percentile.", HistType::kTH1D, {{binsPercentile, "FT0C percentile"}});
+      histos.add("FT0C/percentilevsMult", "FT0C percentile.", HistType::kTH2D, {{binsPercentile, "FT0C percentile"}, {binsMultiplicity, "FT0C mult."}});
+    }
 
     TList* lOfInput;
     if (path.value.rfind("ccdb://", 0) == 0) { // Getting post calib. from CCDB
@@ -105,11 +120,20 @@ struct mcCentrality {
         LOG(fatal) << "The input file " << path.value << " does not contain the TList ccdb_object";
       }
     }
-    h1dFT0M = static_cast<TH1F*>(lOfInput->FindObject("h1dFT0M"));
-    if (!h1dFT0M) {
-      lOfInput->ls();
-      LOG(fatal) << "Could not open histogram h1dFT0M from TList";
-      return;
+    auto getHist = [lOfInput](const char* name) -> TH1F* {
+      auto hist = static_cast<TH1F*>(lOfInput->FindObject(name));
+      if (!hist) {
+        lOfInput->ls();
+        LOG(fatal) << "Could not open histogram " << name << " from TList";
+      }
+      return hist;
+    };
+    h1dFT0M = getHist(lOfInput, "h1dFT0M");
+    if (fillFt0A) {
+      h1dFT0A = getHist("h1dFT0A");
+    }
+    if (fillFt0C) {
+      h1dFT0C = getHist("h1dFT0C");
     }
   }
 
@@ -117,20 +141,24 @@ struct mcCentrality {
   void process(aod::McCollision const& /*mcCollision*/,
                aod::McParticles const& mcParticles)
   {
-    const float nFT0M = mCounter.countFT0A(mcParticles) + mCounter.countFT0C(mcParticles);
-    /*const float nFT0A = mCounter.countFT0A(mcParticles);
+    const float nFT0A = mCounter.countFT0A(mcParticles);
     const float nFT0C = mCounter.countFT0C(mcParticles);
-    const float nFV0A = mCounter.countFV0A(mcParticles);*/
+    const float nFT0M = nFT0A + nFT0C;
+    // const float nFV0A = mCounter.countFV0A(mcParticles);
 
     const float valueCentFT0M = h1dFT0M->GetBinContent(h1dFT0M->FindBin(nFT0M));
-    /*const float valueCentFT0A = h1dFT0M->GetBinContent(h1dFT0M->FindBin(nFT0A));
-    const float valueCentFT0C = h1dFT0M->GetBinContent(h1dFT0M->FindBin(nFT0C));
-    const float valueCentFV0A = h1dFT0M->GetBinContent(h1dFT0M->FindBin(nFV0A));*/
+    if (fillFt0A) {
+      const float valueCentFT0A = h1dFT0M->GetBinContent(h1dFT0M->FindBin(nFT0A));
+      centFT0A(valueCentFT0A);
+    }
+    if (fillFt0C) {
+      const float valueCentFT0C = h1dFT0M->GetBinContent(h1dFT0M->FindBin(nFT0C));
+      centFT0C(valueCentFT0C);
+    }
+    // const float valueCentFV0A = h1dFT0M->GetBinContent(h1dFT0M->FindBin(nFV0A));
 
     centFT0M(valueCentFT0M);
-    /*centFT0A(valueCentFT0A);
-    centFT0C(valueCentFT0C);
-    centFV0A(valueCentFV0A);*/
+    // centFV0A(valueCentFV0A);
     histos.fill(HIST("FT0M/percentile"), valueCentFT0M);
     histos.fill(HIST("FT0M/percentilevsMult"), valueCentFT0M, nFT0M);
   }
