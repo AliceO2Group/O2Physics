29a2e30f3653d9b49a8d99c0d6169029e87dde74
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2025-09-08 20:55:41 +0200

    [Common] Improve / fix printout conditionals

--- Common/Tools/TrackPropagationModule.h
+++ Common/Tools/TrackPropagationModule.h
@@ -115,7 +115,7 @@ class TrackPropagationModule
     fillTracksDCACov = isTableRequiredInWorkflow(initContext, "TracksDCACov");
 
     // enable Tracks in case Tracks have been requested
-    if (fillTracksDCA) {
+    if (fillTracksDCA && !fillTracks) {
       LOGF(info, "******************************************************************");
       LOGF(info, " There is no task subscribed to Tracks, but I have detected a");
       LOGF(info, " subscription to TracksDCA. Now enabling tracks as algorithmic");
