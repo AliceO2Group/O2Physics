131d5529be292bb35c9d0cd6f2b3cb43a6cef38f
Nicolò Jacazio <njacazio@users.noreply.github.com>
2025-09-08 16:30:58 +0200

    Refactor ft0CorrectedTable by removing unused processes
    Removed the processStandard and processWithBypassFT0timeInMC functions, along with related logging and histogram additions. Updated the function signature to process.

--- Common/TableProducer/ft0CorrectedTable.cxx
+++ Common/TableProducer/ft0CorrectedTable.cxx
@@ -34,6 +34,9 @@
 #include <cstdint>
 #include <string>
 
+#include <bitset>
+#include <string>
+
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::aod;
@@ -58,12 +61,6 @@ struct ft0CorrectedTable {
   HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
   void init(o2::framework::InitContext&)
   {
-    if (doprocessStandard && doprocessWithBypassFT0timeInMC) {
-      LOG(fatal) << "Both processStandard and processWithBypassFT0timeInMC are enabled. Pick one of the two";
-    }
-    if (!doprocessStandard && !doprocessWithBypassFT0timeInMC) {
-      LOG(fatal) << "No process is enabled. Pick one";
-    }
     ccdb->setURL(cfgUrl);
     ccdb->setTimestamp(cfgTimestamp);
     ccdb->setCaching(true);
@@ -71,11 +68,6 @@ struct ft0CorrectedTable {
     // Not later than now objects
     ccdb->setCreatedNotAfter(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
 
-    if (doprocessWithBypassFT0timeInMC) {
-      // From ps to ns
-      resoFT0A.value = resoFT0A.value / 1000.f;
-      resoFT0C.value = resoFT0C.value / 1000.f;
-    }
     if (!addHistograms) {
       return;
     }
@@ -84,16 +76,11 @@ struct ft0CorrectedTable {
     histos.add("t0AC", "t0AC", kTH1D, {{1000, -1000, 1000, "t0AC (ns)"}});
     histos.add("deltat0AC", "deltat0AC", kTH1D, {{1000, -1, 1, "#Deltat0AC (ns)"}});
     histos.add("deltat0ACps", "deltat0ACps", kTH1D, {{1000, -1000, 1000, "#Deltat0AC (ps)"}});
-    if (doprocessWithBypassFT0timeInMC) {
-      histos.add("MC/deltat0A", "t0A", kTH1D, {{1000, -50, 50, "t0A (ps)"}});
-      histos.add("MC/deltat0C", "t0C", kTH1D, {{1000, -50, 50, "t0C (ps)"}});
-      histos.add("MC/deltat0AC", "t0AC", kTH1D, {{1000, -50, 50, "t0AC (ps)"}});
-    }
   }
 
-  void processStandard(soa::Join<aod::Collisions, aod::EvSels> const& collisions,
-                       BCsWithMatchings const&,
-                       aod::FT0s const&)
+  void process(soa::Join<aod::Collisions, aod::EvSels> const& collisions,
+               BCsWithMatchings const&,
+               aod::FT0s const&)
   {
     table.reserve(collisions.size());
     float t0A = 1e10f;
@@ -131,94 +118,6 @@ struct ft0CorrectedTable {
       table(t0A, t0C);
     }
   }
-  PROCESS_SWITCH(ft0CorrectedTable, processStandard, "Process standard table (default)", true);
-
-  void processWithBypassFT0timeInMC(soa::Join<aod::Collisions, aod::EvSels, aod::McCollisionLabels> const& collisions,
-                                    soa::Join<BCsWithMatchings, aod::Timestamps> const& bcs,
-                                    aod::FT0s const&,
-                                    aod::McCollisions const&)
-  {
-    if (cfgCollisionSystem.value == -1) {
-      o2::parameters::GRPLHCIFData* grpo = ccdb->template getForTimeStamp<o2::parameters::GRPLHCIFData>(cfgPathGrpLhcIf,
-                                                                                                        bcs.iteratorAt(0).timestamp());
-      cfgCollisionSystem.value = CollisionSystemType::getCollisionTypeFromGrp(grpo);
-      switch (cfgCollisionSystem.value) {
-        case CollisionSystemType::kCollSyspp:
-          resoFT0A.value = 24.f;
-          resoFT0C.value = 24.f;
-          break;
-        case CollisionSystemType::kCollSysPbPb:
-          resoFT0A.value = 5.65f;
-          resoFT0C.value = 5.65f;
-          break;
-        default:
-          break;
-      }
-      // Resolution is given in ps
-      resoFT0A.value = resoFT0A.value / 1000.f;
-      resoFT0C.value = resoFT0C.value / 1000.f;
-    }
-    table.reserve(collisions.size());
-    float t0A = 1e10f;
-    float t0C = 1e10f;
-    float eventtimeMC = 1e10f;
-    float posZMC = 0;
-    bool hasMCcoll = false;
-
-    for (const auto& collision : collisions) {
-      hasMCcoll = false;
-      eventtimeMC = 1e10f;
-      t0A = 1e10f;
-      t0C = 1e10f;
-      posZMC = 0;
-      const float vertexPV = collision.posZ();
-      const float vertex_corr = vertexPV * invLightSpeedCm2NS;
-      constexpr float dummyTime = 30.; // Due to HW limitations time can be only within range (-25,25) ns, dummy time is around 32 ns
-      if (collision.has_mcCollision()) {
-        hasMCcoll = true;
-        const auto& collisionMC = collision.mcCollision();
-        eventtimeMC = collisionMC.t();
-        posZMC = collisionMC.posZ();
-      }
-      if (collision.has_foundFT0()) {
-        const auto& ft0 = collision.foundFT0();
-        const std::bitset<8>& triggers = ft0.triggerMask();
-        const bool ora = triggers[o2::ft0::Triggers::bitA];
-        const bool orc = triggers[o2::ft0::Triggers::bitC];
-
-        if (ora && ft0.timeA() < dummyTime) {
-          t0A = ft0.timeA();
-          if (hasMCcoll) {
-            const float diff = eventtimeMC - posZMC * invLightSpeedCm2NS + gRandom->Gaus(0.f, resoFT0A);
-            t0A = diff;
-          }
-          t0A += vertex_corr;
-        }
-        if (orc && ft0.timeC() < dummyTime) {
-          t0C = ft0.timeC();
-          if (hasMCcoll) {
-            const float diff = eventtimeMC + posZMC * invLightSpeedCm2NS + gRandom->Gaus(0.f, resoFT0C);
-            t0C = diff;
-          }
-          t0C -= vertex_corr;
-        }
-      }
-      LOGF(debug, " T0 collision time T0A = %f, T0C = %f", t0A, t0C);
-      if (addHistograms) {
-        histos.fill(HIST("t0A"), t0A);
-        histos.fill(HIST("t0C"), t0C);
-        histos.fill(HIST("t0AC"), (t0A + t0C) * 0.5f);
-        histos.fill(HIST("deltat0AC"), t0A - t0C);
-        if (hasMCcoll) {
-          histos.fill(HIST("MC/deltat0A"), (t0A - eventtimeMC) * 1000.f);
-          histos.fill(HIST("MC/deltat0C"), (t0C - eventtimeMC) * 1000.f);
-          histos.fill(HIST("MC/deltat0AC"), ((t0A + t0C) * 0.5f - eventtimeMC) * 1000.f);
-        }
-      }
-      table(t0A, t0C);
-    }
-  }
-  PROCESS_SWITCH(ft0CorrectedTable, processWithBypassFT0timeInMC, "Process MC with bypass of the AO2D information. Use with care!", false);
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc) { return WorkflowSpec{adaptAnalysisTask<ft0CorrectedTable>(cfgc)}; }
