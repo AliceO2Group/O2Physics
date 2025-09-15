b5e37a354c04011c4f9a318e5c9101128d0776dd
Shingo Sakai <shingo.sakai@cern.ch>
2025-09-15 22:45:11 +0900

    use o2::constants::math::SectorSpanRad

--- PWGHF/HFL/Tasks/taskElectronWeakBoson.cxx
+++ PWGHF/HFL/Tasks/taskElectronWeakBoson.cxx
@@ -735,12 +735,9 @@ struct HfTaskElectronWeakBoson {
             auto child2 = RecoDecayPtEtaPhi::pVector(trackPos.pt, trackPos.eta, trackPos.phi);
             double invMass = RecoDecay::m(std::array{child1, child2}, std::array{o2::constants::physics::MassElectron, o2::constants::physics::MassElectron});
             if (invMass > massZMinQA) {
-              float sectorneg = 18.0 * trackEle.phi / (o2::constants::math::TwoPI);
-              float sectorpos = 18.0 * trackPos.phi / (o2::constants::math::TwoPI);
-              // float dsectorneg = std::abs(sectorneg - TMath::Nint(sectorneg));
-              // float dsectorpos = std::abs(sectorpos - TMath::Nint(sectorpos));
+              float sectorneg = trackEle.phi / o2::constants::math::SectorSpanRad;
+              float sectorpos = trackPos.phi / o2::constants::math::SectorSpanRad;
               // LOG(info) << "TPC sector= " << sectorneg << " ; " << sectorpos;
-              // LOG(info) << "TPC sector= " << dsectorneg << " ; " << dsectorpos;
               registry.fill(HIST("hInvMassZeeQA"), invMass, trackEle.pt, trackPos.pt, trackEle.dcaxyTrk, trackPos.dcaxyTrk, trackPos.dcazTrk, trackEle.nclusterTPC, trackPos.nclusterTPC, trackEle.nclusterITS, trackPos.nclusterITS, sectorneg, sectorpos, trackEle.eop, trackPos.eop, trackEle.energyIso, trackPos.energyIso, trackEle.momIso, trackPos.momIso, trackEle.ntrackIso, trackPos.ntrackIso);
             }
           }
