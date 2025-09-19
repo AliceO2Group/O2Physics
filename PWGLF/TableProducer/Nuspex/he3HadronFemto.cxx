b043d4fa3ec590963c3f6e7321d068b41cac98f6
blacwovie <meiyi.chen@cern.ch>
2025-09-19 12:08:58 +0800

    Update he3HadronFemto.cxx

--- PWGLF/TableProducer/Nuspex/he3HadronFemto.cxx
+++ PWGLF/TableProducer/Nuspex/he3HadronFemto.cxx
@@ -440,8 +440,8 @@ struct he3HadronFemto {
         candidate.tpcNClsCrossedRows() < crossedRowsToFindableRatio * candidate.tpcNClsFindable() ||
         candidate.tpcChi2NCl() > maxChi2NCl ||
         candidate.tpcChi2NCl() < settingCutChi2tpcLow ||
-        candidate.itsChi2NCl() > settingCutChi2NClITS || candidate.dcaXY() > settingCutDCAxy ||
-        candidate.dcaZ() > settingCutDCAz) {
+        candidate.itsChi2NCl() > settingCutChi2NClITS || std::abs(candidate.dcaXY()) > settingCutDCAxy ||
+        std::abs(candidate.dcaZ()) > settingCutDCAz) {
       return false;
     }
 
@@ -622,7 +622,7 @@ struct he3HadronFemto {
       return false;
     }
 
-    if (he3Hadcand.recoPtHad() < settingCutPtMinHad || he3Hadcand.recoPtHad() > settingCutPtMaxHad)
+    if (std::abs(he3Hadcand.recoPtHad()) < settingCutPtMinHad || std::abs(he3Hadcand.recoPtHad()) > settingCutPtMaxHad)
       return false;
 
     he3Hadcand.signHe3 = trackHe3.sign();
