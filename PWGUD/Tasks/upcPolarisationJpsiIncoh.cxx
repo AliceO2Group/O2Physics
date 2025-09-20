24f470c670220622eb0e4e5163a76194864823e9
Niveditha Ramasubramanian <nramasubramanian@lyoalice02.local>
2025-09-20 16:30:08 +0200

    physics constants

--- PWGUD/Tasks/upcPolarisationJpsiIncoh.cxx
+++ PWGUD/Tasks/upcPolarisationJpsiIncoh.cxx
@@ -18,7 +18,8 @@
 #include "PWGUD/DataModel/UDTables.h"
 
 #include "Common/Core/RecoDecay.h"
-#include "Common/PhysicsConstants.h"
+#include "CommonConstants/PhysicsConstants.h"
+
 #include "CCDB/BasicCCDBManager.h"
 #include "CommonConstants/PhysicsConstants.h"
 #include "DataFormatsParameters/GRPECSObject.h"
