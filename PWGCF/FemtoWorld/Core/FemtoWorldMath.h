3415af11852fd1d7d4a37f93bfd05d523961bd55
blacwovie <meiyi.chen@cern.ch>
2025-09-23 17:15:12 +0800

    Update FemtoWorldMath.h

--- PWGCF/FemtoWorld/Core/FemtoWorldMath.h
+++ PWGCF/FemtoWorld/Core/FemtoWorldMath.h
@@ -39,10 +39,10 @@ class FemtoWorldMath
   /// \param part2 Particle 2
   /// \param mass2 Mass of particle 2
   template <typename T>
-  static float getkstar(const T& part1, const float mass1, const T& part2, const float mass2)
+  static float getkstar(const T& part1, const float mass1, const T& part2, const float mass2, const float z1=1.f, const float z2=1.f)
   {
-    const ROOT::Math::PtEtaPhiMVector vecpart1(part1.pt(), part1.eta(), part1.phi(), mass1);
-    const ROOT::Math::PtEtaPhiMVector vecpart2(part2.pt(), part2.eta(), part2.phi(), mass2);
+    const ROOT::Math::PtEtaPhiMVector vecpart1(part1.pt() * z1, part1.eta(), part1.phi(), mass1);
+    const ROOT::Math::PtEtaPhiMVector vecpart2(part2.pt() * z2, part2.eta(), part2.phi(), mass2);
     const ROOT::Math::PtEtaPhiMVector trackSum = vecpart1 + vecpart2;
 
     const float beta = trackSum.Beta();
