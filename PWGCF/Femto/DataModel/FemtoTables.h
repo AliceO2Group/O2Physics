2c3a7e7c3bbb279ad32b5fc0ef5eda9d6b263112
ariedel-cern <85537041+ariedel-cern@users.noreply.github.com>
2025-09-23 23:45:44 +0200

    [PWGCF] add generic pair tasks (#13096)

--- PWGCF/Femto/DataModel/FemtoTables.h
+++ PWGCF/Femto/DataModel/FemtoTables.h
@@ -34,7 +34,7 @@ namespace o2::aod
 
 namespace femtocollisions
 {
-DECLARE_SOA_COLUMN(CollisionMask, collisionMask, femtodatatypes::CollsionsMaskType); //! Bitmask for collision selections
+DECLARE_SOA_COLUMN(CollisionMask, collisionMask, femtodatatypes::CollisionMaskType); //! Bitmask for collision selections
 
 DECLARE_SOA_COLUMN(PosX, posX, float);             //! x coordinate of vertex
 DECLARE_SOA_COLUMN(PosY, posY, float);             //! y coordinate of vertex
@@ -43,7 +43,6 @@ DECLARE_SOA_COLUMN(Mult, mult, float);             //! Multiplicity estimator se
 DECLARE_SOA_COLUMN(Cent, cent, float);             //! Centrality (~= multiplicity percentile) estimator set by producer
 DECLARE_SOA_COLUMN(MagField, magField, float);     //! Magnetic field of the event
 DECLARE_SOA_COLUMN(Sphericity, sphericity, float); //! Sphericity of the event
-DECLARE_SOA_COLUMN(Occupancy, occupancy, float);   //! occupancy of the event
 DECLARE_SOA_COLUMN(Qn, qn, float);                 //! qn bins for dividing events
 } // namespace femtocollisions
 
@@ -61,11 +60,6 @@ using FCols = FCols_001;
 DECLARE_SOA_TABLE_STAGED_VERSIONED(FColMasks_001, "FCOLMASK", 1, //! track masks
                                    femtocollisions::CollisionMask);
 using FColMasks = FColMasks_001;
-
-// table for occupancy
-DECLARE_SOA_TABLE_STAGED_VERSIONED(FColOccs_001, "FCOLOCC", 1, //! occupancy
-                                   femtocollisions::Occupancy);
-using FColOccs = FColOccs_001;
 
 // table for qn values
 DECLARE_SOA_TABLE_STAGED_VERSIONED(FColQns_001, "FCOLQN", 1, //! qn vector
@@ -274,50 +268,50 @@ DECLARE_SOA_TABLE_STAGED_VERSIONED(FElectronPids_001, "FELECTRONPID", 1, //! ful
                                    femtotracks::ItsNSigmaEl,
                                    femtotracks::TpcNSigmaEl,
                                    femtotracks::TofNSigmaEl,
-                                   femtotracks::TpcitsNSigmaEl<femtotracks::TpcNSigmaEl, femtotracks::TofNSigmaEl>,
-                                   femtotracks::TpctofNSigmaEl<femtotracks::TpcNSigmaEl, femtotracks::ItsNSigmaEl>);
+                                   femtotracks::TpcitsNSigmaEl<femtotracks::TpcNSigmaEl, femtotracks::ItsNSigmaEl>,
+                                   femtotracks::TpctofNSigmaEl<femtotracks::TpcNSigmaEl, femtotracks::TofNSigmaEl>);
 using FElectronPids = FElectronPids_001;
 DECLARE_SOA_TABLE_STAGED_VERSIONED(FPionPids_001, "FPIONPID", 1, //! full pion pid
                                    femtotracks::ItsNSigmaPi,
                                    femtotracks::TpcNSigmaPi,
                                    femtotracks::TofNSigmaPi,
-                                   femtotracks::TpcitsNSigmaPi<femtotracks::TpcNSigmaPi, femtotracks::TofNSigmaPi>,
-                                   femtotracks::TpctofNSigmaPi<femtotracks::TpcNSigmaPi, femtotracks::ItsNSigmaPi>);
+                                   femtotracks::TpcitsNSigmaPi<femtotracks::TpcNSigmaPi, femtotracks::ItsNSigmaPi>,
+                                   femtotracks::TpctofNSigmaPi<femtotracks::TpcNSigmaPi, femtotracks::TofNSigmaPi>);
 using FPionPids = FPionPids_001;
 DECLARE_SOA_TABLE_STAGED_VERSIONED(FKaonPids_001, "FKAONPID", 1, //! full kaon pid
                                    femtotracks::ItsNSigmaKa,
                                    femtotracks::TpcNSigmaKa,
                                    femtotracks::TofNSigmaKa,
-                                   femtotracks::TpcitsNSigmaKa<femtotracks::TpcNSigmaKa, femtotracks::TofNSigmaKa>,
-                                   femtotracks::TpctofNSigmaKa<femtotracks::TpcNSigmaKa, femtotracks::ItsNSigmaKa>);
+                                   femtotracks::TpcitsNSigmaKa<femtotracks::TpcNSigmaKa, femtotracks::ItsNSigmaKa>,
+                                   femtotracks::TpctofNSigmaKa<femtotracks::TpcNSigmaKa, femtotracks::TofNSigmaKa>);
 using FKaonPids = FKaonPids_001;
 DECLARE_SOA_TABLE_STAGED_VERSIONED(FProtonPids_001, "FPROTONPID", 1, //! full proton pid
                                    femtotracks::ItsNSigmaPr,
                                    femtotracks::TpcNSigmaPr,
                                    femtotracks::TofNSigmaPr,
-                                   femtotracks::TpcitsNSigmaPr<femtotracks::TpcNSigmaPr, femtotracks::TofNSigmaPr>,
-                                   femtotracks::TpctofNSigmaPr<femtotracks::TpcNSigmaPr, femtotracks::ItsNSigmaPr>);
+                                   femtotracks::TpcitsNSigmaPr<femtotracks::TpcNSigmaPr, femtotracks::ItsNSigmaPr>,
+                                   femtotracks::TpctofNSigmaPr<femtotracks::TpcNSigmaPr, femtotracks::TofNSigmaPr>);
 using FProtonPids = FProtonPids_001;
 DECLARE_SOA_TABLE_STAGED_VERSIONED(FDeuteronPids_001, "FDEUTERONPID", 1, //! full deuteron pid
                                    femtotracks::ItsNSigmaDe,
                                    femtotracks::TpcNSigmaDe,
                                    femtotracks::TofNSigmaDe,
-                                   femtotracks::TpcitsNSigmaDe<femtotracks::TpcNSigmaDe, femtotracks::TofNSigmaDe>,
-                                   femtotracks::TpctofNSigmaDe<femtotracks::TpcNSigmaDe, femtotracks::ItsNSigmaDe>);
+                                   femtotracks::TpcitsNSigmaDe<femtotracks::TpcNSigmaDe, femtotracks::ItsNSigmaDe>,
+                                   femtotracks::TpctofNSigmaDe<femtotracks::TpcNSigmaDe, femtotracks::TofNSigmaDe>);
 using FDeuteronPids = FDeuteronPids_001;
 DECLARE_SOA_TABLE_STAGED_VERSIONED(FTritonPids_001, "FTRITONPID", 1, //! full triton pid
                                    femtotracks::ItsNSigmaTr,
                                    femtotracks::TpcNSigmaTr,
                                    femtotracks::TofNSigmaTr,
-                                   femtotracks::TpcitsNSigmaTr<femtotracks::TpcNSigmaTr, femtotracks::TofNSigmaTr>,
-                                   femtotracks::TpctofNSigmaTr<femtotracks::TpcNSigmaTr, femtotracks::ItsNSigmaTr>);
+                                   femtotracks::TpcitsNSigmaTr<femtotracks::TpcNSigmaTr, femtotracks::ItsNSigmaTr>,
+                                   femtotracks::TpctofNSigmaTr<femtotracks::TpcNSigmaTr, femtotracks::TofNSigmaTr>);
 using FTritonPids = FTritonPids_001;
 DECLARE_SOA_TABLE_STAGED_VERSIONED(FHeliumPids_001, "FHELIUMPID", 1, //! full helium3 pid
                                    femtotracks::ItsNSigmaHe,
                                    femtotracks::TpcNSigmaHe,
                                    femtotracks::TofNSigmaHe,
-                                   femtotracks::TpcitsNSigmaHe<femtotracks::TpcNSigmaHe, femtotracks::TofNSigmaHe>,
-                                   femtotracks::TpctofNSigmaHe<femtotracks::TpcNSigmaHe, femtotracks::ItsNSigmaHe>);
+                                   femtotracks::TpcitsNSigmaHe<femtotracks::TpcNSigmaHe, femtotracks::ItsNSigmaHe>,
+                                   femtotracks::TpctofNSigmaHe<femtotracks::TpcNSigmaHe, femtotracks::TofNSigmaHe>);
 using FHeliumPids = FHeliumPids_001;
 
 using FTrackPids = soa::Join<FElectronPids, FPionPids, FKaonPids, FProtonPids, FDeuteronPids, FTritonPids, FHeliumPids>;
