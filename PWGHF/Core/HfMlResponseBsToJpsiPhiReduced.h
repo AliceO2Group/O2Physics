097b0095d84393842157f648458a7c858cef797f
ALICE Builder <alibuild@users.noreply.github.com>
2025-10-02 13:39:26 +0200

    Please consider the following formatting changes to #13206 (#134)

--- PWGHF/Core/HfMlResponseBsToJpsiPhiReduced.h
+++ PWGHF/Core/HfMlResponseBsToJpsiPhiReduced.h
@@ -29,9 +29,10 @@
 // Fill the map of available input features
 // the key is the feature's name (std::string)
 // the value is the corresponding value in EnumInputFeatures
-#define FILL_MAP_BS(FEATURE) \
-  {                          \
-    #FEATURE, static_cast<uint8_t>(InputFeaturesBsToJpsiPhiReduced::FEATURE)}
+#define FILL_MAP_BS(FEATURE)                                                 \
+  {                                                                          \
+    #FEATURE, static_cast<uint8_t>(InputFeaturesBsToJpsiPhiReduced::FEATURE) \
+  }
 
 // Check if the index of mCachedIndices (index associated to a FEATURE)
 // matches the entry in EnumInputFeatures associated to this FEATURE
