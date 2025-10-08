fe907450c1ff9c055e2c2ccf45e299321ece3309
Swadhinkumar49 <108603227+Swadhinkumar49@users.noreply.github.com>
2025-10-08 12:23:45 +0530

    Update phi1020SpherocityAnalysis.cxx
    Copyright information updated

--- PWGLF/Tasks/Resonances/phi1020SpherocityAnalysis.cxx
+++ PWGLF/Tasks/Resonances/phi1020SpherocityAnalysis.cxx
@@ -1,4 +1,9 @@
-
+/*
+ * Copyright CERN for the benefit of the ALICE Collaboration
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * nor submit itself to any jurisdiction.
+ */
 // Copyright 2019-2025 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
