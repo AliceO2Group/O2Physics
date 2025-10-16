d3ceec35b1abcb0912e35374b436a5d467c2b3e9
Uliana Dmitrieva <uliana.dmitrieva@cern.ch>
2025-10-16 13:00:11 +0200

    Add centroid calculations, centroid histos and new columns to the produced table; fix typos

--- Common/DataModel/ZDCExtra.h
+++ Common/DataModel/ZDCExtra.h
@@ -31,13 +31,20 @@ DECLARE_SOA_COLUMN(ZnaTow2, znaTow2, float);                //! Tower 2 ZNA
 DECLARE_SOA_COLUMN(ZnaTow3, znaTow3, float);                //! Tower 3 ZNA
 DECLARE_SOA_COLUMN(ZnaTow4, znaTow4, float);                //! Tower 4 ZNA
 DECLARE_SOA_COLUMN(ZnaTdc, znaTdc, float);                  //! TDC ZNA
+DECLARE_SOA_COLUMN(ZnaQx, znaQx, float);                    //! Q-vector X ZNA
+DECLARE_SOA_COLUMN(ZnaQy, znaQy, float);                    //! Q-vector Y ZNA
 DECLARE_SOA_COLUMN(ZncTowC, zncTowC, float);                //! Common tower ZNC
 DECLARE_SOA_COLUMN(ZncTow1, zncTow1, float);                //! Tower 1 ZNC
 DECLARE_SOA_COLUMN(ZncTow2, zncTow2, float);                //! Tower 2 ZNC
 DECLARE_SOA_COLUMN(ZncTow3, zncTow3, float);                //! Tower 3 ZNC
 DECLARE_SOA_COLUMN(ZncTow4, zncTow4, float);                //! Tower 4 ZNC
 DECLARE_SOA_COLUMN(ZncTdc, zncTdc, float);                  //! TDC ZNC
+DECLARE_SOA_COLUMN(ZncQx, zncQx, float);                    //! Q-vector X ZNC
+DECLARE_SOA_COLUMN(ZncQy, zncQy, float);                    //! Q-vector Y ZNC
 DECLARE_SOA_COLUMN(Centrality, centrality, float);          //! Centrality
+DECLARE_SOA_COLUMN(Vx, vx, float);                          //! Vertex X
+DECLARE_SOA_COLUMN(Vy, vy, float);                          //! Vertex Y
+DECLARE_SOA_COLUMN(Vz, vz, float);                          //! Vertex Z
 DECLARE_SOA_COLUMN(Timestamp, timestamp, uint64_t);         //! Timestamp
 DECLARE_SOA_COLUMN(SelectionBits, selectionBits, uint8_t);  //! Selection Flags
 } // namespace zdcextra
@@ -49,13 +56,20 @@ DECLARE_SOA_TABLE(ZdcExtras, "AOD", "ZDCEXTRA", o2::soa::Index<>,
                   zdcextra::ZnaTow3,
                   zdcextra::ZnaTow4,
                   zdcextra::ZnaTdc,
+                  zdcextra::ZnaQx,
+                  zdcextra::ZnaQy,
                   zdcextra::ZncTowC,
                   zdcextra::ZncTow1,
                   zdcextra::ZncTow2,
                   zdcextra::ZncTow3,
                   zdcextra::ZncTow4,
                   zdcextra::ZncTdc,
+                  zdcextra::ZncQx,
+                  zdcextra::ZncQy,
                   zdcextra::Centrality,
+                  zdcextra::Vx,
+                  zdcextra::Vy,
+                  zdcextra::Vz,
                   zdcextra::Timestamp,
                   zdcextra::SelectionBits);
 } // namespace o2::aod
