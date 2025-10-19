cfbb038fd6aee8a6ee2e260e0189aae9ee9c13ea
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-10-19 22:58:56 +0200

    require 1 and only 1 process

--- DPG/Tasks/TPC/tpcSkimsTableCreator.cxx
+++ DPG/Tasks/TPC/tpcSkimsTableCreator.cxx
@@ -446,6 +446,11 @@ struct TreeWriterTpcV0 {
 
   void init(o2::framework::InitContext&)
   {
+    const std::array<bool, 7> doprocess{doprocessStandard, doprocessStandardWithCorrecteddEdx, doprocessWithdEdxTrQA, doprocessWithdEdxTrQAWithCorrecteddEdx, doprocessWithTrQA, doprocessWithTrQAWithCorrecteddEdx, doprocessDummy};
+    if (std::accumulate(doprocess.begin(), doprocess.end(), 0) != 1) {
+      LOGP(fatal, "One and only one process function should be enabled");
+    }
+
     ccdb->setURL("http://alice-ccdb.cern.ch");
     ccdb->setCaching(true);
     ccdb->setFatalWhenNull(false);
@@ -968,6 +973,11 @@ struct TreeWriterTPCTOF {
 
   void init(o2::framework::InitContext&)
   {
+    const std::array<bool, 7> doprocess{doprocessStandard, doprocessStandardWithCorrecteddEdx, doprocessWithdEdxTrQA, doprocessWithdEdxTrQAWithCorrecteddEdx, doprocessWithTrQA, doprocessWithTrQAWithCorrecteddEdx, doprocessDummy};
+    if (std::accumulate(doprocess.begin(), doprocess.end(), 0) != 1) {
+      LOGP(fatal, "One and only one process function should be enabled");
+    }
+
     ccdb->setURL("http://alice-ccdb.cern.ch");
     ccdb->setCaching(true);
     ccdb->setFatalWhenNull(false);
