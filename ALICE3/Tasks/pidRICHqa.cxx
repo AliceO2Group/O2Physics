04c7b1f84446a9930f568ad39aa9bf1578df7163
Nicolò Jacazio <njacazio@users.noreply.github.com>
2025-11-02 18:40:37 +0100

    [ALICE3] Clean PIDResponse in ALICE3 (#13637)

--- ALICE3/Tasks/pidRICHqa.cxx
+++ ALICE3/Tasks/pidRICHqa.cxx
@@ -16,9 +16,10 @@
 ///
 
 // O2 includes
-#include "Framework/AnalysisTask.h"
 #include "ALICE3/DataModel/RICH.h"
-#include "Common/DataModel/PIDResponse.h"
+#include "Common/DataModel/PIDResponseTOF.h"
+
+#include "Framework/AnalysisTask.h"
 #include "ReconstructionDataFormats/PID.h"
 
 using namespace o2;
