ce8b2901584b4fcd85762b5f77ecc84ad35b6cf9
joachimckh <jh@joachimhansen.dk>
2025-11-03 20:15:52 +0100

    mean-pt shape selection tables

--- Common/Core/FFitWeights.cxx
+++ Common/Core/FFitWeights.cxx
@@ -19,6 +19,7 @@
 #include <TCollection.h>
 #include <TH1.h>
 #include <TH2.h>
+#include <TProfile.h>
 #include <TNamed.h>
 #include <TObjArray.h>
 #include <TSpline.h>
@@ -35,18 +36,22 @@ ClassImp(FFitWeights)
 
   FFitWeights::FFitWeights() : TNamed("", ""),
                                fW_data{nullptr},
-                               CentBin{100},
+                               centBin{100},
                                qAxis{nullptr},
                                nResolution{3000},
+                               ptBin{100},
+                               ptAxis{nullptr},
                                qnTYPE{0}
 {
 }
 
 FFitWeights::FFitWeights(const char* name) : TNamed(name, name),
                                              fW_data{nullptr},
-                                             CentBin{100},
+                                             centBin{100},
                                              qAxis{nullptr},
                                              nResolution{3000},
+                                             ptBin{100},
+                                             ptAxis{nullptr},
                                              qnTYPE{0} {}
 
 FFitWeights::~FFitWeights()
@@ -54,6 +59,8 @@ FFitWeights::~FFitWeights()
   delete fW_data;
   if (qAxis)
     delete qAxis;
+  if (ptAxis)
+    delete ptAxis;
 };
 
 void FFitWeights::init()
@@ -65,8 +72,14 @@ void FFitWeights::init()
   if (!qAxis)
     this->setBinAxis(500, 0, 25);
   for (const auto& qn : qnTYPE) {
-    fW_data->Add(new TH2D(this->getQName(qn.first, qn.second.c_str()), this->getAxisName(qn.first, qn.second.c_str()), CentBin, 0, CentBin, qAxis->GetNbins(), qAxis->GetXmin(), qAxis->GetXmax()));
+    fW_data->Add(new TH2D(this->getQName(qn.first, qn.second.c_str()), this->getAxisName(qn.first, qn.second.c_str()), centBin, 0, centBin, qAxis->GetNbins(), qAxis->GetXmin(), qAxis->GetXmax()));
   }
+
+  if (!ptAxis)
+    this->setPtAxis(3000, -3, 3);
+  //fW_data->Add(new TH2D("hPtWeight", "", centBin, 0, centBin, ptBin, ptAxis->GetXmin(), ptAxis->GetXmax()));
+  fW_data->Add(new TProfile("pMeanPt","", centBin, 0, centBin));
+  fW_data->Add(new TH2D("hPtWeight", "", centBin, 0, centBin, ptBin, ptAxis->GetXmin(), ptAxis->GetXmax()));
 };
 
 void FFitWeights::fillWeights(float centrality, float qn, int nh, const char* pf)
@@ -79,10 +92,46 @@ void FFitWeights::fillWeights(float centrality, float qn, int nh, const char* pf
 
   TH2D* th2 = reinterpret_cast<TH2D*>(tar->FindObject(this->getQName(nh, pf)));
   if (!th2) {
-    tar->Add(new TH2D(this->getQName(nh, pf), this->getAxisName(nh, pf), CentBin, 0, CentBin, qAxis->GetNbins(), qAxis->GetXmin(), qAxis->GetXmax()));
+    tar->Add(new TH2D(this->getQName(nh, pf), this->getAxisName(nh, pf), centBin, 0, centBin, qAxis->GetNbins(), qAxis->GetXmin(), qAxis->GetXmax()));
     th2 = reinterpret_cast<TH2D*>(tar->At(tar->GetEntries() - 1));
   }
   th2->Fill(centrality, qn);
+};
+void FFitWeights::fillPt(float centrality, float pt, bool first)
+{
+  TObjArray* tar{nullptr};
+  tar = fW_data;
+  if (!tar)
+    return;
+  if (first) {
+    auto tp = reinterpret_cast<TProfile*>(tar->FindObject("pMeanPt"));
+    if (!tp) {
+      tar->Add(new TProfile("pMeanPt", "", centBin, 0, centBin));
+      tp = reinterpret_cast<TProfile*>(tar->At(tar->GetEntries() - 1));
+    }
+    tp->Fill(centrality, pt);
+  }
+  else {
+    auto th2 = reinterpret_cast<TH2D*>(tar->FindObject("hPtWeight"));
+    if (!th2) {
+      tar->Add(new TH2D("hPtWeight", "", centBin, 0, centBin, ptBin, ptAxis->GetXmin(), ptAxis->GetXmax()));
+      th2 = reinterpret_cast<TH2D*>(tar->At(tar->GetEntries() - 1));
+    }
+    th2->Fill(centrality, pt);
+  }
+};
+float FFitWeights::getPtMult(float centrality)
+{
+  TObjArray* tar{nullptr};
+  tar = fW_data;
+  if (!tar)
+    return -1;
+
+  auto tp = reinterpret_cast<TProfile*>(tar->FindObject("pMeanPt"));
+  if (!tp) {
+    return -1;
+  }
+  return tp->GetBinContent(tp->FindBin(centrality));
 };
 
 Long64_t FFitWeights::Merge(TCollection* collist)
@@ -93,10 +142,10 @@ Long64_t FFitWeights::Merge(TCollection* collist)
     fW_data->SetName("FFitWeights_Data");
     fW_data->SetOwner(kTRUE);
   }
-  FFitWeights* l_w = 0;
-  TIter all_w(collist);
-  while ((l_w = (reinterpret_cast<FFitWeights*>(all_w())))) {
-    addArray(fW_data, l_w->getDataArray());
+  FFitWeights* lW= 0;
+  TIter allW(collist);
+  while ((lW= (reinterpret_cast<FFitWeights*>(allW())))) {
+    addArray(fW_data, lW->getDataArray());
     nmerged++;
   }
   return nmerged;
@@ -104,7 +153,8 @@ Long64_t FFitWeights::Merge(TCollection* collist)
 void FFitWeights::addArray(TObjArray* targ, TObjArray* sour)
 {
   if (!sour) {
-    printf("Source array does not exist!\n");
+    // printf("Source array does not exist!\n");
+    //LOGF(info, "FFitWeights source array does not exist!");
     return;
   }
   for (int i = 0; i < sour->GetEntries(); i++) {
@@ -120,6 +170,34 @@ void FFitWeights::addArray(TObjArray* targ, TObjArray* sour)
   }
 };
 
+void FFitWeights::mptSel()
+{
+  TObjArray* tar{nullptr};
+  tar = fW_data;
+  if (!tar)
+    return;
+
+  TH2D* th2 = reinterpret_cast<TH2D*>(tar->FindObject("hPtWeight"));
+  if (!th2) {
+    return;
+  }
+
+  TH1D* tmp{nullptr};
+  TGraph* tmpgr{nullptr};
+  for (int iSP{0}; iSP < NumberSp; iSP++) {
+    tmp = th2->ProjectionY(Form("mpt_%i_%i", iSP, iSP + 1), iSP + 1, iSP + 1);
+    std::vector<double> xq(nResolution);
+    std::vector<double> yq(nResolution);
+    for (int i{0}; i < nResolution; i++)
+      xq[i] = static_cast<double>(i + 1) / static_cast<double>(nResolution);
+    tmp->GetQuantiles(nResolution, yq.data(), xq.data());
+    tmpgr = new TGraph(nResolution, yq.data(), xq.data());
+    tmpgr->SetName(Form("sp_mpt_%i", iSP));
+    fW_data->Add(tmpgr);
+  }
+
+}
+
 void FFitWeights::qSelection(const std::vector<int>& nhv, const std::vector<std::string>& stv) /* only execute OFFLINE */
 {
   TObjArray* tar{nullptr};
@@ -132,14 +210,15 @@ void FFitWeights::qSelection(const std::vector<int>& nhv, const std::vector<std:
     for (const auto& nh : nhv) {
       TH2D* th2{reinterpret_cast<TH2D*>(tar->FindObject(this->getQName(nh, pf.c_str())))};
       if (!th2) {
-        printf("qh not found!\n");
+        //printf("qh not found!\n");
+        //LOGF(info, "FFitWeights qh not found!");
         return;
       }
 
       TH1D* tmp{nullptr};
       TGraph* tmpgr{nullptr};
       // TSpline3* spline = nullptr;
-      for (int iSP{0}; iSP < 90; iSP++) {
+      for (int iSP{0}; iSP < NumberSp; iSP++) {
         tmp = th2->ProjectionY(Form("q%i_%i_%i", nh, iSP, iSP + 1), iSP + 1, iSP + 1);
         std::vector<double> xq(nResolution);
         std::vector<double> yq(nResolution);
@@ -161,11 +240,12 @@ float FFitWeights::eval(float centr, const float& dqn, const int nh, const char*
   TObjArray* tar{nullptr};
 
   tar = fW_data;
-  if (!tar)
+  if (!tar) {
     return -1;
+  }
 
   int isp{static_cast<int>(centr)};
-  if (isp < 0 || isp > 90) {
+  if (isp < 0 || isp > NumberSp) {
     return -1;
   }
 
@@ -175,10 +255,36 @@ float FFitWeights::eval(float centr, const float& dqn, const int nh, const char*
     return -1;
   }
 
-  float qn_val{static_cast<float>(100. * spline->Eval(dqn))};
-  if (qn_val < 0 || qn_val > 100.05) {
+  float qnVal{static_cast<float>(100. * spline->Eval(dqn))};
+  if (qnVal < 0 || qnVal > MaxTol) {
     return -1;
   }
 
-  return qn_val;
+  return qnVal;
 };
+
+float FFitWeights::evalPt(float centr, const float& mpt)
+{
+  TObjArray* tar{nullptr};
+  tar = fW_data;
+  if (!tar) {
+    return -1;
+  }
+
+  int isp{static_cast<int>(centr)};
+  if (isp < 0 || isp > NumberSp) {
+    return -1;
+  }
+
+  TGraph* spline{nullptr};
+  spline = reinterpret_cast<TGraph*>(tar->FindObject(Form("sp_mpt_%i", isp)));
+  if (!spline) {
+    return -1;
+  }
+  float ptVal{static_cast<float>(100. * spline->Eval(mpt))};
+  if (ptVal < 0 || ptVal > MaxTol) {
+    return -1;
+  }
+  return ptVal;
+};
+
