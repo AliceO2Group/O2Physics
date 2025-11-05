21830788ac25a6611136338416fceb9c51e5d56d
abmodak <67369858+abmodak@users.noreply.github.com>
2025-11-05 18:09:12 +0100

    Modifications in header

--- PWGCF/TwoParticleCorrelations/Tasks/longrangecorrDerived.cxx
+++ PWGCF/TwoParticleCorrelations/Tasks/longrangecorrDerived.cxx
@@ -7,23 +7,13 @@
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdicstion.
-// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
-//
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 ///
 /// \file longrangecorrDerived.cxx
 ///
-/// \brief task for long range correlation analysis
+/// \brief task for long range correlation analysis based on derived table
 /// \author Abhi Modak (abhi.modak@cern.ch)
-/// \since November 02, 2025
+/// \since November 05, 2025
 
 #include "PWGCF/Core/CorrelationContainer.h"
 #include "PWGCF/Core/PairCuts.h"
