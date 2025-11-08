547767367bfeecd45ba8fd7c8b529d725a73e356
ALICE Builder <alibuild@users.noreply.github.com>
2025-11-08 17:50:16 +0100

    Please consider the following formatting changes (#495)

--- Common/Tools/Multiplicity/macros/saveCorrelation.C
+++ Common/Tools/Multiplicity/macros/saveCorrelation.C
@@ -11,45 +11,46 @@
 //
 // This file provides a simple macro to convert TGlauberMC output tuples
 // into the 2D correlation histogram necessary for the ALICE machinery
-// that performs Glauber + NBD fits. 
+// that performs Glauber + NBD fits.
 
-void saveCorrelation(TString filename = "gmc-PbPb-snn68.21-md0.40-nd-1.0-rc1-smax99.0.root", TString outputFile = "basehistos.root"){
-  TFile *fin = new TFile(filename.Data(),"READ");
-  TNtuple *ntup = (TNtuple*) fin->Get("nt_Pb_Pb");
-  
+void saveCorrelation(TString filename = "gmc-PbPb-snn68.21-md0.40-nd-1.0-rc1-smax99.0.root", TString outputFile = "basehistos.root")
+{
+  TFile* fin = new TFile(filename.Data(), "READ");
+  TNtuple* ntup = (TNtuple*)fin->Get("nt_Pb_Pb");
+
   // try other Pb nuclear profiles in case "Pb" - "Pb" not found
-  if(!ntup){
-    ntup = (TNtuple*) fin->Get("nt_Pbpn_Pbpn");
+  if (!ntup) {
+    ntup = (TNtuple*)fin->Get("nt_Pbpn_Pbpn");
   }
-  if(!ntup){
-    ntup = (TNtuple*) fin->Get("nt_PbpnVar1_PbpnVar1");
+  if (!ntup) {
+    ntup = (TNtuple*)fin->Get("nt_PbpnVar1_PbpnVar1");
   }
-  if(!ntup){
-    ntup = (TNtuple*) fin->Get("nt_PbpnVar2_PbpnVar2");
+  if (!ntup) {
+    ntup = (TNtuple*)fin->Get("nt_PbpnVar2_PbpnVar2");
   }
-  if(!ntup){
-    ntup = (TNtuple*) fin->Get("nt_PbpnVar3_PbpnVar3");
+  if (!ntup) {
+    ntup = (TNtuple*)fin->Get("nt_PbpnVar3_PbpnVar3");
   }
-  if(!ntup){
-    ntup = (TNtuple*) fin->Get("nt_PbpnVar4_PbpnVar4");
+  if (!ntup) {
+    ntup = (TNtuple*)fin->Get("nt_PbpnVar4_PbpnVar4");
   }
-  
-  if(!ntup){
-    cout<<"No tree found!"<<endl;
+
+  if (!ntup) {
+    cout << "No tree found!" << endl;
     return;
   }
-  
-  cout<<"Glauber tree entries: "<<ntup->GetEntries()<<endl;
-  
-  TFile *fout = new TFile(outputFile.Data(),"RECREATE");
-  
+
+  cout << "Glauber tree entries: " << ntup->GetEntries() << endl;
+
+  TFile* fout = new TFile(outputFile.Data(), "RECREATE");
+
   // 2D correlation plot necessary for Glauber + NBD fitting
   // The provided range should be enough for Pb-Pb
-  TH2D *hNpNc = new TH2D("hNpNc","",500,-0.5,499.5,2500,-0.5,2499.5);
-  
+  TH2D* hNpNc = new TH2D("hNpNc", "", 500, -0.5, 499.5, 2500, -0.5, 2499.5);
+
   // let's draw this on screen for inspection
-  TCanvas *c1 = new TCanvas("c1", "", 800, 600);
-  c1->SetTicks(1,1);
+  TCanvas* c1 = new TCanvas("c1", "", 800, 600);
+  c1->SetTicks(1, 1);
   c1->SetLogz();
   ntup->Draw("Ncoll:Npart>>hNpNc", "", "colz");
   fout->Write();
