9a5788859163c7477a22256aae8395ba91d7d36e
Robert Forynski <forynski@gmail.com>
2025-11-13 20:46:47 +0000

    fix: Add double quotes around variable expansions in run.sh (shellcheck compliance)

--- Tools/PIDFeatureExtractor/run.sh
+++ Tools/PIDFeatureExtractor/run.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
+# Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+# See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+# All rights not expressly granted are reserved.
+#
+# This software is distributed under the terms of the GNU General Public
+# License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+#
+# In applying this license CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
 
-OPTION="-b --configuration json://myConfigExtractor.json"
+OPTION="$1"
+EXECUTABLE="o2-analysis-pid-feature-extractor"
 
-# Specify the executable binary directly (adjust the path as needed)
-EXECUTABLE=~/alice/sw/BUILD/O2Physics-latest/O2Physics/stage/bin/o2-analysistutorial-mm-my-example-task-pid-feature-extractor
-CONFIG_PATH="$(pwd)/myConfigExtractor.json"
-OPTION="-b --configuration json://$CONFIG_PATH"
-
-
-echo "Starting O2Physics PID Feature Extraction Workflow..."
-echo "Using configuration: myConfigExtractor.json"
-
-o2-analysis-timestamp ${OPTION} | \
-o2-analysis-event-selection ${OPTION} | \
-o2-analysis-tracks-extra-v002-converter ${OPTION} | \
-o2-analysis-track-propagation ${OPTION} | \
-o2-analysis-pid-tpc-base ${OPTION} | \
-o2-analysis-pid-tpc ${OPTION} | \
-o2-analysis-pid-tof-base ${OPTION} | \
-o2-analysis-pid-tof ${OPTION} | \
-o2-analysis-pid-tof-beta ${OPTION} | \
-o2-analysis-multiplicity-table ${OPTION} | \
-o2-analysis-mccollision-converter ${OPTION} | \
-${EXECUTABLE} ${OPTION}
-
-echo "Check output files (e.g. pid_features.csv, pid_features.root)"
+o2-analysis-timestamp "${OPTION}" | \
+  o2-analysis-event-selection "${OPTION}" | \
+  o2-analysis-tracks-extra-v002-converter "${OPTION}" | \
+  o2-analysis-track-propagation "${OPTION}" | \
+  o2-analysis-pid-tpc-base "${OPTION}" | \
+  o2-analysis-pid-tpc "${OPTION}" | \
+  o2-analysis-pid-tof-base "${OPTION}" | \
+  o2-analysis-pid-tof "${OPTION}" | \
+  o2-analysis-pid-tof-beta "${OPTION}" | \
+  o2-analysis-multiplicity-table "${OPTION}" | \
+  o2-analysis-mccollision-converter "${OPTION}" | \
+  "${EXECUTABLE}" "${OPTION}"
