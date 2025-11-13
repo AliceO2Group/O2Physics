7e484fb38f3b8ca261f90392d56adecd7bf3655f
cnkoster <noor.koster@cern.ch>
2025-11-13 07:43:48 +0100

    remove unused variables

--- Tutorials/PWGCF/EventPlane/src/spectatorPlaneTutorial.cxx
+++ Tutorials/PWGCF/EventPlane/src/spectatorPlaneTutorial.cxx
@@ -102,19 +102,12 @@ struct SpectatorPlaneTutorial {
     AxisSpec axisPhi = {60, 0, constants::math::TwoPI, "#varphi"};
     AxisSpec axisEta = {64, -1.6, 1.6, "#eta"};
     AxisSpec axisEtaVn = {8, -.8, .8, "#eta"};
-    AxisSpec axisVx = {40, -0.01, 0.01, "v_{x}"};
-    AxisSpec axisVy = {40, -0.01, 0.01, "v_{y}"};
-    AxisSpec axisVz = {40, -10, 10, "v_{z}"};
     AxisSpec axisCent = {90, 0, 90, "Centrality(%)"};
     AxisSpec axisPhiPlane = {100, -constants::math::PI, constants::math::PI, "#Psi"};
-    AxisSpec axisQx = {100, -0.2, 0.2, "Q_{X}"};
-    AxisSpec axisQy = {100, -0.2, 0.2, "Q_{Y}"};
     AxisSpec axisQQ = {100, -0.2, 0.2, "#LT Q_{X}^{A}Q_{Y}^{C} #GT"};
 
     std::vector<double> ptbinning = {0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.2, 2.4, 2.6, 2.8, 3, 3.5, 4, 5, 6, 8, 10};
     AxisSpec axisPt = {ptbinning, "#it{p}_{T} GeV/#it{c}"};
-
-    int ptbins = ptbinning.size() - 1;
 
     rctChecker.init(rctFlags.cfgEvtRCTFlagCheckerLabel, rctFlags.cfgEvtRCTFlagCheckerZDCCheck, rctFlags.cfgEvtRCTFlagCheckerLimitAcceptAsBad);
 
