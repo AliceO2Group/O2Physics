5d9a2fea6432b232e30d80dbd5116fd2bfec93ae
Ravindra Singh <56298081+singhra1994@users.noreply.github.com>
2025-11-14 15:31:32 +0100

    added PID histograms and fix mass window bug for V0

--- PWGHF/HFC/TableProducer/correlatorLcScHadrons.cxx
+++ PWGHF/HFC/TableProducer/correlatorLcScHadrons.cxx
@@ -623,7 +623,7 @@ registry.fill(HIST("hTPCnSigmaPrPiKRej"),posTrackV0.pt(), posTrackV0.tpcNSigmaPr
         }
       }
       if (isSelectedV0Daughter(negTrackV0, kProton) && isSelectedV0Daughter(posTrackV0, kPiPlus)) {
-        if (std::abs(massLambda - v0.mAntiLambda()) < cfgV0.cfgHypMassWindow) {
+        if (std::abs(o2::constants::physics::MassLambda - v0.mAntiLambda()) > cfgV0.cfgHypMassWindow) {
           registry.fill(HIST("hV0Lambda"), v0.mAntiLambda(), v0.pt(), negTrackV0.pt());
           registry.fill(HIST("hV0LambdaRefl"), v0.mLambda(), v0.pt(), posTrackV0.pt());
 
