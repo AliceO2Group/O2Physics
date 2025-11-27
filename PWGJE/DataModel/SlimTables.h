608e0ffc5455c28df189de2f40f947113a69aa55
louisemillot <millot.louise1@orange.fr>
2025-11-27 16:58:26 +0100

    resolve linter issues

--- PWGJE/DataModel/SlimTables.h
+++ PWGJE/DataModel/SlimTables.h
@@ -71,7 +71,7 @@ DECLARE_SOA_TABLE(SlimMcCollisions, "AOD", "SlimMcCollisions",
 
 namespace slimtracks
 {
-DECLARE_SOA_INDEX_COLUMN(SlimCollision, collision); // o2-linter: disable=name/o2-column
+DECLARE_SOA_INDEX_COLUMN(SlimCollision, collision);
 DECLARE_SOA_INDEX_COLUMN(Track, track);
 DECLARE_SOA_COLUMN(Pt, pt, float);
 DECLARE_SOA_COLUMN(Eta, eta, float);
@@ -100,7 +100,7 @@ DECLARE_SOA_TABLE(SlimTracks, "AOD", "SlimTracks",
 
 namespace slimparticles
 {
-DECLARE_SOA_INDEX_COLUMN(SlimMcCollision, mcCollision); // o2-linter: disable=name/o2-column
+DECLARE_SOA_INDEX_COLUMN(SlimMcCollision, mcCollision);
 DECLARE_SOA_INDEX_COLUMN(McParticle, mcParticle);
 DECLARE_SOA_COLUMN(Pt, pt, float);
 DECLARE_SOA_COLUMN(Eta, eta, float);
