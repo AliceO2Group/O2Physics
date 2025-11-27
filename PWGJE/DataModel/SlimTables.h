d703aa73aac7689fbaeb6dfdddfe4a9d83b1edfa
louisemillot <millot.louise1@orange.fr>
2025-11-27 14:52:53 +0100

    correct copyright notice

--- PWGJE/DataModel/SlimTables.h
+++ PWGJE/DataModel/SlimTables.h
@@ -1,12 +1,25 @@
-// Copyright 2019-2025 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details.
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   SlimTables.h
+/// \author Millot Louise <louise.millot@cern.ch>
+/// \since  2024-11-27
+/// \brief  Header for the SlimTables task for the analysis of the reduced tables.
+///
 
 #ifndef PWGJE_DATAMODEL_SLIMTABLES_H_
 #define PWGJE_DATAMODEL_SLIMTABLES_H_
-#include "Common/DataModel/TrackSelectionTables.h"
+
 #include "PWGJE/Core/JetDerivedDataUtilities.h"
-
-
 #include <Framework/ASoA.h>
 #include <Framework/AnalysisDataModel.h>
 
--- PWGJE/TableProducer/SlimTablesProducer.cxx
+++ PWGJE/TableProducer/SlimTablesProducer.cxx
@@ -1,3 +1,19 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file SlimTablesProducer.cxx
+/// \brief Task to produce a reduced version of Tables for tracks, collisions, mcparticles and mccollisions.
+/// \author Millot Louise <louise.millot@cern.ch>
+
+
 #include "PWGJE/DataModel/SlimTables.h"
 #include "Framework/AnalysisDataModel.h"
 #include "Framework/AnalysisTask.h"
