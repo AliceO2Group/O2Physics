d02d64a320f651ecd27e790bc62a2113a86231df
Valerio Di Bella <vdibella@sbgpc51.in2p3.fr>
2025-12-03 11:16:57 +0100

    Introducing a reduced workflow for D+ pairs

---
+++ PWGHF/HFC/TableProducer/correlatorDplusDplusReduced.cxx
@@ -0,0 +1,354 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file correlatorDplusDplusReduced.cxx
+/// \brief Writer of D+ → π+ K- π+ candidates in the form of flat tables to be stored in TTrees.
+///        Intended for debug, local optimization of analysis on small samples or ML training.
+///        In this file are defined and filled the output tables
+///
+/// \author Valerio DI BELLA <valerio.di.bella@cern.ch>, IPHC Strasbourg
+/// Based on the code of Alexandre Bigot <alexandre.bigot@cern.ch>, IPHC Strasbourg
+
+#include "PWGHF/Core/DecayChannels.h"
+#include "PWGHF/Core/HfHelper.h"
+#include "PWGHF/DataModel/CandidateSelectionTables.h"
+#include "PWGHF/HFC/DataModel/ReducedDMesonPairsTables.h"
+
+#include <Framework/runDataProcessing.h>
+
+#include <vector>
+
+using namespace o2;
+using namespace o2::framework;
+using namespace o2::framework::expressions;
+using namespace o2::hf_centrality;
+
+/// Writes the full information in an output TTree
+struct HfCorrelatorDplusDplusReduced {
+  Produces<o2::aod::HfCandDpFulls> rowCandidateFull;
+  Produces<o2::aod::HfCandDpLites> rowCandidateLite;
+  Produces<o2::aod::HfCandDpFullEvs> rowCandidateFullEvents;
+  Produces<o2::aod::HfCandDpMls> rowCandidateMl;
+
+  Produces<o2::aod::HfCandDpMcPs> rowCandidateMcParticles;
+  Produces<o2::aod::HfCandDpMcEvs> rowCandidateMcCollisions;
+
+  Configurable<int> selectionFlagDplus{"selectionFlagDplus", 1, "Selection Flag for Dplus"};
+  Configurable<bool> fillCandidateLiteTable{"fillCandidateLiteTable", false, "Switch to fill lite table with candidate properties"};
+  // parameters for production of training samples
+  Configurable<bool> fillCorrBkgs{"fillCorrBkgs", false, "Flag to fill derived tables with correlated background candidates"};
+  Configurable<std::vector<int>> classMlIndexes{"classMlIndexes", {0, 2}, "Indexes of ML bkg and non-prompt scores."};
+  Configurable<int> centEstimator{"centEstimator", 0, "Centrality estimation (None: 0, FT0C: 2, FT0M: 3)"};
+  Configurable<bool> cfgSkimmedProcessing{"cfgSkimmedProcessing", true, "Enables processing of skimmed datasets"};
+  Configurable<bool> skipSingleD{"skipSingleD", true, "Skip collisions with one or less D candidates"};
+
+  HfHelper hfHelper;
+
+  Service<o2::ccdb::BasicCCDBManager> ccdb;
+
+  using SelectedCandidates = soa::Filtered<soa::Join<aod::HfCand3ProngWPidPiKa, aod::HfSelDplusToPiKPi>>;
+  using SelectedCandidatesMc = soa::Filtered<soa::Join<aod::HfCand3ProngWPidPiKa, aod::HfCand3ProngMcRec, aod::HfSelDplusToPiKPi>>;
+  using MatchedGenCandidatesMc = soa::Filtered<soa::Join<aod::McParticles, aod::HfCand3ProngMcGen>>;
+  using SelectedCandidatesMcWithMl = soa::Filtered<soa::Join<aod::HfCand3ProngWPidPiKa, aod::HfCand3ProngMcRec, aod::HfSelDplusToPiKPi, aod::HfMlDplusToPiKPi>>;
+  using CollisionsCent = soa::Join<aod::Collisions, aod::CentFT0Cs, aod::CentFT0Ms>;
+
+  Filter filterSelectCandidates = aod::hf_sel_candidate_dplus::isSelDplusToPiKPi >= selectionFlagDplus;
+  Filter filterMcGenMatching = (nabs(o2::aod::hf_cand_3prong::flagMcMatchGen) == static_cast<int8_t>(hf_decay::hf_cand_3prong::DecayChannelMain::DplusToPiKPi)) || (fillCorrBkgs && (nabs(o2::aod::hf_cand_3prong::flagMcMatchGen) != 0));
+
+  Preslice<SelectedCandidates> tracksPerCollision = o2::aod::track::collisionId;
+  Preslice<aod::McParticles> mcParticlesPerMcCollision = o2::aod::mcparticle::mcCollisionId;
+
+  Partition<SelectedCandidatesMc> reconstructedCandSig = (nabs(aod::hf_cand_3prong::flagMcMatchRec) == static_cast<int8_t>(hf_decay::hf_cand_3prong::DecayChannelMain::DplusToPiKPi)) || (fillCorrBkgs && (nabs(o2::aod::hf_cand_3prong::flagMcMatchRec) != 0));
+  Partition<SelectedCandidatesMc> reconstructedCandBkg = nabs(aod::hf_cand_3prong::flagMcMatchRec) != static_cast<int8_t>(hf_decay::hf_cand_3prong::DecayChannelMain::DplusToPiKPi);
+  Partition<SelectedCandidatesMcWithMl> reconstructedCandSigMl = (nabs(aod::hf_cand_3prong::flagMcMatchRec) == static_cast<int8_t>(hf_decay::hf_cand_3prong::DecayChannelMain::DplusToPiKPi)) || (fillCorrBkgs && (nabs(o2::aod::hf_cand_3prong::flagMcMatchRec) != 0));
+
+  HistogramRegistry registry{"registry"};
+  Zorro zorro;
+  OutputObj<ZorroSummary> zorroSummary{"zorroSummary"};
+
+  void init(InitContext const&)
+  {
+    ccdb->setURL("http://alice-ccdb.cern.ch");
+    ccdb->setCaching(true);
+    ccdb->setLocalObjectValidityChecking();
+
+    if (cfgSkimmedProcessing) {
+      zorroSummary.setObject(zorro.getZorroSummary());
+    }
+  }
+
+  template <typename T>
+  void fillEvent(const T& collision)
+  {
+    rowCandidateFullEvents(
+      collision.numContrib(),
+      collision.posX(),
+      collision.posY(),
+      collision.posZ());
+  }
+
+  template <typename Coll, bool doMc = false, bool doMl = false, typename T>
+  void fillCandidateTable(const T& candidate, int localEvIdx = -1)
+  {
+    int8_t flagMc = 0;
+    int8_t originMc = 0;
+    int8_t channelMc = 0;
+    if constexpr (doMc) {
+      flagMc = candidate.flagMcMatchRec();
+      originMc = candidate.originMcRec();
+      channelMc = candidate.flagMcDecayChanRec();
+    }
+
+    std::vector<float> outputMl = {-999., -999.};
+    if constexpr (doMl) {
+      for (unsigned int iclass = 0; iclass < classMlIndexes->size(); iclass++) {
+        outputMl[iclass] = candidate.mlProbDplusToPiKPi()[classMlIndexes->at(iclass)];
+      }
+      rowCandidateMl(
+        outputMl[0],
+        outputMl[1]);
+    }
+
+    float cent{-1.};
+    auto coll = candidate.template collision_as<Coll>();
+    if (std::is_same_v<Coll, CollisionsCent> && centEstimator != CentralityEstimator::None) {
+      cent = getCentralityColl(coll, centEstimator);
+    }
+
+    if (fillCandidateLiteTable) {
+      rowCandidateLite(
+        candidate.chi2PCA(),
+        candidate.decayLength(),
+        candidate.decayLengthXY(),
+        candidate.decayLengthNormalised(),
+        candidate.decayLengthXYNormalised(),
+        candidate.ptProng0(),
+        candidate.ptProng1(),
+        candidate.ptProng2(),
+        candidate.impactParameter0(),
+        candidate.impactParameter1(),
+        candidate.impactParameter2(),
+        candidate.impactParameterZ0(),
+        candidate.impactParameterZ1(),
+        candidate.impactParameterZ2(),
+        candidate.nSigTpcPi0(),
+        candidate.nSigTpcKa0(),
+        candidate.nSigTofPi0(),
+        candidate.nSigTofKa0(),
+        candidate.tpcTofNSigmaPi0(),
+        candidate.tpcTofNSigmaKa0(),
+        candidate.nSigTpcPi1(),
+        candidate.nSigTpcKa1(),
+        candidate.nSigTofPi1(),
+        candidate.nSigTofKa1(),
+        candidate.tpcTofNSigmaPi1(),
+        candidate.tpcTofNSigmaKa1(),
+        candidate.nSigTpcPi2(),
+        candidate.nSigTpcKa2(),
+        candidate.nSigTofPi2(),
+        candidate.nSigTofKa2(),
+        candidate.tpcTofNSigmaPi2(),
+        candidate.tpcTofNSigmaKa2(),
+        candidate.isSelDplusToPiKPi(),
+        hfHelper.invMassDplusToPiKPi(candidate),
+        candidate.pt(),
+        candidate.cpa(),
+        candidate.cpaXY(),
+        candidate.maxNormalisedDeltaIP(),
+        candidate.eta(),
+        candidate.phi(),
+        hfHelper.yDplus(candidate),
+        cent,
+        coll.numContrib(),
+        localEvIdx,
+        flagMc,
+        originMc,
+        channelMc);
+    } else {
+      rowCandidateFull(
+        candidate.xSecondaryVertex(),
+        candidate.ySecondaryVertex(),
+        candidate.zSecondaryVertex(),
+        candidate.errorDecayLength(),
+        candidate.errorDecayLengthXY(),
+        candidate.chi2PCA(),
+        candidate.rSecondaryVertex(),
+        candidate.decayLength(),
+        candidate.decayLengthXY(),
+        candidate.decayLengthNormalised(),
+        candidate.decayLengthXYNormalised(),
+        candidate.impactParameterNormalised0(),
+        candidate.ptProng0(),
+        RecoDecay::p(candidate.pxProng0(), candidate.pyProng0(), candidate.pzProng0()),
+        candidate.impactParameterNormalised1(),
+        candidate.ptProng1(),
+        RecoDecay::p(candidate.pxProng1(), candidate.pyProng1(), candidate.pzProng1()),
+        candidate.impactParameterNormalised2(),
+        candidate.ptProng2(),
+        RecoDecay::p(candidate.pxProng2(), candidate.pyProng2(), candidate.pzProng2()),
+        candidate.pxProng0(),
+        candidate.pyProng0(),
+        candidate.pzProng0(),
+        candidate.pxProng1(),
+        candidate.pyProng1(),
+        candidate.pzProng1(),
+        candidate.pxProng2(),
+        candidate.pyProng2(),
+        candidate.pzProng2(),
+        candidate.impactParameter0(),
+        candidate.impactParameter1(),
+        candidate.impactParameter2(),
+        candidate.errorImpactParameter0(),
+        candidate.errorImpactParameter1(),
+        candidate.errorImpactParameter2(),
+        candidate.impactParameterZ0(),
+        candidate.impactParameterZ1(),
+        candidate.impactParameterZ2(),
+        candidate.errorImpactParameterZ0(),
+        candidate.errorImpactParameterZ1(),
+        candidate.errorImpactParameterZ2(),
+        candidate.nSigTpcPi0(),
+        candidate.nSigTpcKa0(),
+        candidate.nSigTofPi0(),
+        candidate.nSigTofKa0(),
+        candidate.tpcTofNSigmaPi0(),
+        candidate.tpcTofNSigmaKa0(),
+        candidate.nSigTpcPi1(),
+        candidate.nSigTpcKa1(),
+        candidate.nSigTofPi1(),
+        candidate.nSigTofKa1(),
+        candidate.tpcTofNSigmaPi1(),
+        candidate.tpcTofNSigmaKa1(),
+        candidate.nSigTpcPi2(),
+        candidate.nSigTpcKa2(),
+        candidate.nSigTofPi2(),
+        candidate.nSigTofKa2(),
+        candidate.tpcTofNSigmaPi2(),
+        candidate.tpcTofNSigmaKa2(),
+        candidate.isSelDplusToPiKPi(),
+        hfHelper.invMassDplusToPiKPi(candidate),
+        candidate.pt(),
+        candidate.p(),
+        candidate.cpa(),
+        candidate.cpaXY(),
+        candidate.maxNormalisedDeltaIP(),
+        hfHelper.ctDplus(candidate),
+        candidate.eta(),
+        candidate.phi(),
+        hfHelper.yDplus(candidate),
+        hfHelper.eDplus(candidate),
+        cent,
+        localEvIdx,
+        flagMc,
+        originMc,
+        channelMc);
+    }
+  }
+
+  void processData(aod::Collisions const& collisions, SelectedCandidates const& candidates)
+  {
+    static int lastRunNumber = -1;
+    // reserve memory
+    rowCandidateFullEvents.reserve(collisions.size());
+    if (fillCandidateLiteTable) {
+      rowCandidateLite.reserve(candidates.size());
+    } else {
+      rowCandidateFull.reserve(candidates.size());
+    }
+
+    for (const auto& collision : collisions) {
+      if (cfgSkimmedProcessing) {
+        auto bc = collision.bc_as<aod::BCsWithTimestamps>();
+        int runNumber = bc.runNumber();
+        if (lastRunNumber != runNumber) {
+          lastRunNumber = runNumber;
+          LOGF(info, "Initializing Zorro for run %d", runNumber);
+          uint64_t currentTimestamp = bc.timestamp();
+          zorro.initCCDB(ccdb.service, runNumber, currentTimestamp, "fHfDoubleCharm3P");
+          zorro.populateHistRegistry(registry, runNumber);
+        }
+        zorro.isSelected(bc.globalBC());
+      }
+
+      const auto colId = collision.globalIndex();
+      auto candidatesInThisCollision = candidates.sliceBy(tracksPerCollision, colId);
+      if (skipSingleD)
+        if (candidatesInThisCollision.size() < 2) // o2-linter: disable=magic-number (number of candidate must be larger than 1)
+          continue;
+      fillEvent(collision);
+      for (const auto& candidate : candidatesInThisCollision) {
+        fillCandidateTable<aod::Collisions>(candidate, rowCandidateFullEvents.lastIndex());
+      }
+    }
+  }
+  PROCESS_SWITCH(HfCorrelatorDplusDplusReduced, processData, "Process data per collision", false);
+
+  void processMcRec(aod::Collisions const& collisions,
+                    SelectedCandidatesMc const& candidates)
+  {
+    // reserve memory
+    rowCandidateFullEvents.reserve(collisions.size());
+    if (fillCandidateLiteTable) {
+      rowCandidateLite.reserve(candidates.size());
+    } else {
+      rowCandidateFull.reserve(candidates.size());
+    }
+
+    for (const auto& collision : collisions) { // No skimming for MC data. No Zorro !
+      const auto colId = collision.globalIndex();
+      auto candidatesInThisCollision = candidates.sliceBy(tracksPerCollision, colId);
+      if (skipSingleD)
+        if (candidatesInThisCollision.size() < 2) // o2-linter: disable=magic-number (number of candidate must be larger than 1)
+          continue;
+      fillEvent(collision);
+      for (const auto& candidate : candidatesInThisCollision) {
+        fillCandidateTable<aod::Collisions, true>(candidate, rowCandidateFullEvents.lastIndex());
+      }
+    }
+  }
+  PROCESS_SWITCH(HfCorrelatorDplusDplusReduced, processMcRec, "Process data per collision", false);
+
+  void processMcGen(aod::McCollisions const& mccollisions, MatchedGenCandidatesMc const& mcparticles)
+  {
+    // reserve memory
+    rowCandidateMcCollisions.reserve(mccollisions.size());
+    rowCandidateMcParticles.reserve(mcparticles.size());
+
+    for (const auto& mccollision : mccollisions) { // No skimming for MC data. No Zorro !
+      const auto colId = mccollision.globalIndex();
+      const auto particlesInThisCollision = mcparticles.sliceBy(mcParticlesPerMcCollision, colId);
+      if (skipSingleD)
+        if (particlesInThisCollision.size() < 2) // o2-linter: disable=magic-number (number of candidate must be larger than 1)
+          continue;
+      rowCandidateMcCollisions(
+        mccollision.posX(),
+        mccollision.posY(),
+        mccollision.posZ());
+      for (const auto& particle : particlesInThisCollision) {
+        rowCandidateMcParticles(
+          particle.pt(),
+          particle.eta(),
+          particle.phi(),
+          particle.y(),
+          rowCandidateMcCollisions.lastIndex(),
+          particle.flagMcMatchGen(),
+          particle.flagMcDecayChanGen(),
+          particle.originMcGen());
+      }
+    }
+  }
+  PROCESS_SWITCH(HfCorrelatorDplusDplusReduced, processMcGen, "Process MC data at the generator level", false);
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{adaptAnalysisTask<HfCorrelatorDplusDplusReduced>(cfgc)};
+}
---
+++ PWGHF/HFC/Tasks/taskCorrelationDplusDplusReduced.cxx
@@ -0,0 +1,100 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file taskCorrelationDplusDplusReduced.cxx
+/// \brief Writer of pairs of D mesons candidates in the form of flat tables to be stored in TTrees.
+///        Intended for debug, local optimization of analysis on small samples or ML training.
+///        In this file are defined and filled the output tables
+///
+/// \author Valerio DI BELLA <valerio.di.bella@cern.ch>, IPHC Strasbourg
+/// Based on the code of Alexandre Bigot <alexandre.bigot@cern.ch>, IPHC Strasbourg
+
+#include "PWGHF/Core/DecayChannels.h"
+#include "PWGHF/HFC/DataModel/ReducedDMesonPairsTables.h"
+
+#include "Framework/runDataProcessing.h"
+
+using namespace o2;
+using namespace o2::framework;
+using namespace o2::framework::expressions;
+
+struct HfTaskCorrelationDplusDplusReduced {
+  Configurable<int> selectionFlagDplus{"selectionFlagDplus", 1, "Selection Flag for Dplus"};
+
+  using SelectedCandidates = soa::Filtered<o2::aod::HfCandDpFulls>;
+  using SelectedMcParticles = o2::aod::HfCandDpMcPs;
+
+  Filter filterSelectCandidates = aod::full::candidateSelFlag >= selectionFlagDplus;
+
+  HistogramConfigSpec hTH1NCand{HistType::kTH1F, {{7, -0.5, 6.5}}};
+  HistogramConfigSpec hTH1NMcRec{HistType::kTH1F, {{7, -0.5, 6.5}}};
+  HistogramConfigSpec hTH1NMcGen{HistType::kTH1F, {{7, -0.5, 6.5}}};
+  HistogramRegistry registry{
+    "registry",
+    {{"hNCand", "Number of D candidates per event;N", hTH1NCand},
+     {"hNMcRec", "Number of reconstructed Mc D mesons per event;N", hTH1NMcRec},
+     {"hNMcGen", "Number of generated Mc D mesons per event;N", hTH1NMcGen}}};
+  void init(InitContext const&)
+  {
+    registry.add("hMassDplus", "D+ candidates;inv. mass (#pi#pi K) (GeV/#it{c}^{2}))", {HistType::kTH1F, {{120, 1.5848, 2.1848}}});
+    registry.add("hMassDplusMatched", "D+ matched candidates;inv. mass (#pi#pi K) (GeV/#it{c}^{2}))", {HistType::kTH1F, {{120, 1.5848, 2.1848}}});
+    registry.add("hMassDMesonPair", "D Meson pair candidates;inv. mass (#pi K) (GeV/#it{c}^{2});inv. mass (#pi K) (GeV/#it{c}^{2})", {HistType::kTH2F, {{120, 1.5848, 2.1848}, {120, 1.5848, 2.1848}}});
+    registry.add("hDltPhiMcGen", "Azimuthal correlation for D mesons; #Delta#phi", {HistType::kTH1F, {{100, -3.141593, 3.141593}}});
+  }
+
+  void processLocalData(o2::aod::HfCandDpFullEvs::iterator const&,
+                        SelectedCandidates const& localCandidates)
+  {
+    registry.fill(HIST("hNCand"), localCandidates.size());
+
+    for (const auto& cand1 : localCandidates) {
+      auto mass1 = cand1.m();
+      for (auto cand2 = cand1 + 1; cand2 != localCandidates.end(); ++cand2) {
+        auto mass2 = cand2.m();
+        registry.fill(HIST("hMassDMesonPair"), mass2, mass1);
+      }
+    }
+  }
+  PROCESS_SWITCH(HfTaskCorrelationDplusDplusReduced, processLocalData, "Process local data", true);
+
+  void processLocalDataMcRec(o2::aod::HfCandDpFullEvs::iterator const&,
+                             SelectedCandidates const& localCandidates)
+  {
+    registry.fill(HIST("hNMcRec"), localCandidates.size());
+
+    for (const auto& cand1 : localCandidates) {
+      auto mass1 = cand1.m();
+      registry.fill(HIST("hMassDplus"), mass1);
+      if (std::abs(cand1.flagMcMatchRec()) == hf_decay::hf_cand_3prong::DecayChannelMain::DplusToPiKPi)
+        registry.fill(HIST("hMassDplusMatched"), mass1);
+    }
+  }
+  PROCESS_SWITCH(HfTaskCorrelationDplusDplusReduced, processLocalDataMcRec, "Process local MC data", false);
+
+  void processLocalDataMcGen(o2::aod::HfCandDpMcEvs::iterator const&,
+                             SelectedMcParticles const& localMcParticles)
+  {
+    registry.fill(HIST("hNMcGen"), localMcParticles.size());
+
+    for (const auto& part1 : localMcParticles) {
+      for (auto part2 = part1 + 1; part2 != localMcParticles.end(); ++part2) {
+        registry.fill(HIST("hDltPhiMcGen"), part2.phi() - part1.phi());
+      }
+    }
+  }
+  PROCESS_SWITCH(HfTaskCorrelationDplusDplusReduced, processLocalDataMcGen, "Process local MC data at the gen level", false);
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<HfTaskCorrelationDplusDplusReduced>(cfgc)};
+}
