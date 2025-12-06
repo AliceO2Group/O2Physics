4643900536eb8603e6df45ac4cc2e4af43593233
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2025-12-06 19:07:17 +0100

    Fix typo

--- PWGHF/D2H/Utils/utilsFlow.h
+++ PWGHF/D2H/Utils/utilsFlow.h
@@ -9,7 +9,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file utilsFLow.h
+/// \file utilsFlow.h
 /// \brief Utilities for flow analyses
 /// \author Fabrizio Grosa <fabrizio.grosa@cern.ch>, CERN
 
