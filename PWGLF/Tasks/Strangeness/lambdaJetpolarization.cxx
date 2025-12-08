9120f93a1113b47cb5ca3acac9c1c893fadd9333
suyoupeng <109774812+15071832337@users.noreply.github.com>
2025-12-08 17:04:11 +0800

    Add files via upload

--- PWGLF/Tasks/Strangeness/lambdaJetpolarization.cxx
+++ PWGLF/Tasks/Strangeness/lambdaJetpolarization.cxx
@@ -1,4 +1,3 @@
-// o2-linter: disable=name/workflow-file
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
@@ -10,6 +9,8 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 ///
+
+// o2-linter: disable=name/workflow-file
 
 /// \author Youpeng Su (yousu@cern.ch)
 #include "PWGLF/DataModel/lambdaJetpolarization.h"
