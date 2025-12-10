9a85316ed1677f06294750c3578a8d2edbf5eaf6
Nicolò Jacazio <njacazio@users.noreply.github.com>
2025-12-10 12:56:13 +0100

    Add cleanup option for downloaded files

--- ALICE3/Core/DelphesO2TrackSmearer.h
+++ ALICE3/Core/DelphesO2TrackSmearer.h
@@ -250,6 +250,7 @@ class TrackSmearer
   }
   void setdNdEta(float val) { mdNdEta = val; }                                 //;
   void setCcdbManager(o2::ccdb::BasicCCDBManager* mgr) { mCcdbManager = mgr; } //;
+  void setCleanupDownloadedFile(bool val) { mCleanupDownloadedFile = val; }  //;
 
  protected:
   static constexpr unsigned int nLUTs = 9; // Number of LUT available
@@ -263,6 +264,7 @@ class TrackSmearer
 
  private:
   o2::ccdb::BasicCCDBManager* mCcdbManager = nullptr;
+  bool mCleanupDownloadedFile = true;
 };
 
 } // namespace delphes
