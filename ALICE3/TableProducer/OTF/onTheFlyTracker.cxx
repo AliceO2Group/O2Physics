c0e68a2bda3faea51cbd678470212fbc6b8b9f5d
Nicolò Jacazio <njacazio@users.noreply.github.com>
2025-12-24 11:05:10 +0100

    Fix formatting in onTheFlyTracker.cxx macros

--- ALICE3/TableProducer/OTF/onTheFlyTracker.cxx
+++ ALICE3/TableProducer/OTF/onTheFlyTracker.cxx
@@ -68,8 +68,8 @@ using namespace o2::framework;
 using std::array;
 #define getHist(type, name) std::get<std::shared_ptr<type>>(histPointers[name])
 #define fillHist(type, name, ...) \
-  std::get<std::shared_ptr<type>>(histPointers[name])->Fill(__VA_ARGS__)
-#define insertHist(name, ...) histPointers[(name).c_str()] = histos.add((name).c_str(), __VA_ARGS__)
+  std::get<std::shared_ptr<type>>(histPointers[name])->Fill(__VA_ARGS__);
+#define insertHist(name, ...) histPointers[(name).c_str()] = histos.add((name).c_str(), __VA_ARGS__);
 
 struct OnTheFlyTracker {
   Produces<aod::Collisions> tableCollisions;
