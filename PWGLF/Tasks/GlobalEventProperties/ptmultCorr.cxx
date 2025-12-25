2027709778adaff1cb42466c72ba7b2daf4f2b76
abmodak <67369858+abmodak@users.noreply.github.com>
2025-12-25 22:48:57 +0100

    Fix o2 linter error

--- PWGLF/Tasks/GlobalEventProperties/ptmultCorr.cxx
+++ PWGLF/Tasks/GlobalEventProperties/ptmultCorr.cxx
@@ -9,7 +9,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 ///
-/// \file chargedparticleRaa.cxx
+/// \file ptmultCorr.cxx
 ///
 /// \brief task for analysis of charged-particle RAA at midrapidity in light-ion collisions
 /// \author Abhi Modak (abhi.modak@cern.ch)
