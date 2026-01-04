4cbdde3b2f9f6028fcf56490938022fdb55af646
BiaoZhang (张彪) <52267892+zhangbiao-phy@users.noreply.github.com>
2026-01-04 18:03:22 +0100

    [PWGHF] Add D0 reflection tag into the charm femto producer (#14382)
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

--- PWGHF/HFC/TableProducer/producerCharmHadronsTrackFemtoDream.cxx
+++ PWGHF/HFC/TableProducer/producerCharmHadronsTrackFemtoDream.cxx
@@ -740,6 +740,8 @@ struct HfProducerCharmHadronsTrackFemtoDream {
         }
         fillTable(0, candidate.isSelD0(), outputMlD0.at(0), outputMlD0.at(1), outputMlD0.at(2));
         fillTable(1, candidate.isSelD0bar(), outputMlD0bar.at(0), outputMlD0bar.at(1), outputMlD0bar.at(2));
+        if (candidate.isSelD0() && candidate.isSelD0bar())
+          fillTable(2, candidate.isSelD0(), outputMlD0.at(0), outputMlD0.at(1), outputMlD0.at(2)); // tag reflection
 
       } else if constexpr (Channel == DecayChannel::DstarToD0Pi) {
         if constexpr (UseCharmMl) {
