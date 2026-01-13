e9a8f7f5917d35ca2e3e79c724c2edd5827ff773
Nicolò Jacazio <njacazio@users.noreply.github.com>
2026-01-13 09:24:19 +0100

    Add histogram registry and update process method

--- ALICE3/TableProducer/OTF/onTheFlyDetectorGeometryProvider.cxx
+++ ALICE3/TableProducer/OTF/onTheFlyDetectorGeometryProvider.cxx
@@ -20,6 +20,7 @@
 
 #include <CCDB/BasicCCDBManager.h>
 #include <Framework/AnalysisTask.h>
+#include <Framework/HistogramRegistry.h>
 #include <Framework/runDataProcessing.h>
 
 #include <map>
@@ -27,7 +28,7 @@
 #include <vector>
 
 struct OnTheFlyDetectorGeometryProvider {
-
+  o2::framework::HistogramRegistry histos{"Histos", {}, o2::framework::OutputObjHandlingPolicy::AnalysisObject};
   o2::framework::Configurable<std::vector<std::string>> detectorConfiguration{"detectorConfiguration",
                                                                               std::vector<std::string>{"$O2PHYSICS_ROOT/share/alice3/a3geometry_v3.ini"},
                                                                               "Paths of the detector geometry configuration files"};
@@ -45,6 +46,7 @@ struct OnTheFlyDetectorGeometryProvider {
     int idx = 0;
     for (auto& configFile : detectorConfiguration.value) {
       LOG(info) << "Loading detector geometry from configuration file: " << configFile;
+      histos.add<TH1>(Form("GeometryConfigFile_%d", idx), configFile.c_str(), o2::framework::HistType::kTH1D, {{1, 0, 1}})->Fill(0.5);
       // If the filename starts with ccdb: then take the file from the ccdb
       if (configFile.rfind("ccdb:", 0) == 0) {
         std::string ccdbPath = configFile.substr(5); // remove "ccdb:" prefix
@@ -75,10 +77,12 @@ struct OnTheFlyDetectorGeometryProvider {
         LOG(fatal) << "Inconsistent magnetic field values between configurations 0 and " << icfg << ": " << mMagneticField << " vs " << cfgBfield;
       }
     }
+    LOG(info) << "Initialization completed";
   }
 
-  void process(o2::aod::McCollisions const&)
+  void process(o2::aod::McCollisions const& mcCollisions, o2::aod::McParticles const& mcParticles)
   {
+    LOG(info) << "On-the-fly detector geometry provider processing " << mcCollisions.size() << " collisions and " << mcParticles.size() << " particles.";
   }
 };
 
