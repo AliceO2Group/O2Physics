9e6e474e26a5d1219cb9ebdef871eb1864a595f8
baasingh <balwansinghrana11@gmail.com>
2026-01-14 16:24:13 +0530

    fixingcopyrighterror

--- PWGCF/EbyEFluctuations/Tasks/FactorialMomentsTask.cxx
+++ PWGCF/EbyEFluctuations/Tasks/FactorialMomentsTask.cxx
@@ -5,7 +5,7 @@
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 // In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an intergovernmental Organization
+// granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 ///
 
