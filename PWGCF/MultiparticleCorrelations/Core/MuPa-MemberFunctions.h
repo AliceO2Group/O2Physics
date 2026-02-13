e0bea1719bd79f790488c30d4269d8cb248eae11
abilandz <Ante.Bilandzic@cern.ch>
2026-02-13 11:32:55 +0100

    utility function to print selected environment variables

--- PWGCF/MultiparticleCorrelations/Core/MuPa-MemberFunctions.h
+++ PWGCF/MultiparticleCorrelations/Core/MuPa-MemberFunctions.h
@@ -17765,6 +17765,22 @@ void ExitFunction(const char* functionName)
 
 //============================================================
 
+void printEnvironment()
+{
+  // Print the content of selected environment variables.
+
+  LOGF(info, "");
+  LOGF(info, "Environment variables:");
+  LOGF(info, " O2_ROOT = %s", std::getenv("O2_ROOT"));
+  LOGF(info, " O2PHYSICS_ROOT = %s", std::getenv("O2PHYSICS_ROOT"));
+  LOGF(info, " ROOT_RELEASE = %s", std::getenv("ROOT_RELEASE"));
+  LOGF(info, " ROOTSYS = %s", std::getenv("ROOTSYS"));
+  LOGF(info, "");
+
+} // void printEnvironment()
+
+//============================================================
+
 void BailOut(bool finalBailout = false)
 {
   // Use only locally - bail out if maximum number of events was reached, and dump all results by that point in a local ROOT file.
@@ -19264,6 +19280,11 @@ void Steer(T1 const& collision, T2 const& bcs, T3 const& tracks)
 
   // memStatus: ~50K (without differential q-vectors and eta separations)
 
+  // *) Print environment:
+  if (tc.fVerbose) {
+    printEnvironment();
+  }
+
   // *) Dry run:
   if (tc.fDryRun) {
     EventCounterForDryRun(eFill);
@@ -19399,6 +19420,11 @@ void Steer(T1 const& collision, T2 const& bcs, T3 const& tracks)
   if (tc.fUseStopwatch) {
     LOGF(info, "\033[1;32m=> Global timer: Steer ends ... %.6f\033[0m\n", tc.fTimer[eGlobal]->RealTime());
     tc.fTimer[eGlobal]->Continue(); // yes
+  }
+
+  // *) Print environment:
+  if (tc.fVerbose) {
+    printEnvironment();
   }
 
   if (tc.fVerbose) {
