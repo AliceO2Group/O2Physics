51c64f6632cd9f1e898028fc5c388be566a95322
Stanislas LAMBERT <lambert@nanpc519.local>
2026-02-13 15:15:30 +0100

    add new 2 columns to dimuonAll to flag if a track is associated to the correct event, for non MC tasks (and dqEfficiency) it is set true by default

--- PWGDQ/DataModel/ReducedInfoTables.h
+++ PWGDQ/DataModel/ReducedInfoTables.h
@@ -665,6 +665,8 @@ DECLARE_SOA_COLUMN(Vt2, vt2, float); //! Production vertex time
 
 DECLARE_SOA_COLUMN(IsAmbig1, isAmbig1, int); //!
 DECLARE_SOA_COLUMN(IsAmbig2, isAmbig2, int); //!
+DECLARE_SOA_COLUMN(IsCorrectAssoc1, isCorrectAssoc1, bool); //!
+DECLARE_SOA_COLUMN(IsCorrectAssoc2, IsCorrectAssoc2, bool); //!
 
 DECLARE_SOA_COLUMN(FwdDcaX1, fwdDcaX1, float);               //! X component of forward DCA
 DECLARE_SOA_COLUMN(FwdDcaY1, fwdDcaY1, float);               //! Y component of forward DCA
@@ -912,6 +914,7 @@ DECLARE_SOA_TABLE(DimuonsAll, "AOD", "RTDIMUONALL", //!
                   dilepton_track_index::Vx1, dilepton_track_index::Vy1, dilepton_track_index::Vz1, dilepton_track_index::Vt1,
                   dilepton_track_index::Vx2, dilepton_track_index::Vy2, dilepton_track_index::Vz2, dilepton_track_index::Vt2,
                   dilepton_track_index::IsAmbig1, dilepton_track_index::IsAmbig2,
+                  dilepton_track_index::IsCorrectAssoc1, dilepton_track_index::IsCorrectAssoc2,
                   reducedpair::U2Q2,
                   reducedpair::U3Q3,
                   reducedpair::R2EP_AB,
