984e87f0f831f867201ff96fb77bc8c329b7fc67
Md Samsul Islam <56156956+mislam17@users.noreply.github.com>
2026-02-25 14:16:57 +0530

    Fixed include format
    Fixed include format

--- PWGHF/HFL/Tasks/taskSingleMuonMultMc.cxx
+++ PWGHF/HFL/Tasks/taskSingleMuonMultMc.cxx
@@ -17,17 +17,17 @@
 #include "Common/DataModel/EventSelection.h"
 #include "Common/DataModel/TrackSelectionTables.h"
 
-#include "Framework/ASoA.h"
-#include "Framework/AnalysisDataModel.h"
-#include "Framework/AnalysisHelpers.h"
-#include "Framework/AnalysisTask.h"
-#include "Framework/Configurable.h"
-#include "Framework/HistogramRegistry.h"
-#include "Framework/HistogramSpec.h"
-#include "Framework/InitContext.h"
-#include "Framework/O2DatabasePDGPlugin.h"
-#include "Framework/OutputObjHeader.h"
-#include "Framework/runDataProcessing.h"
+#include <Framework/ASoA.h>
+#include <Framework/AnalysisDataModel.h>
+#include <Framework/AnalysisHelpers.h>
+#include <Framework/AnalysisTask.h>
+#include <Framework/Configurable.h>
+#include <Framework/HistogramRegistry.h>
+#include <Framework/HistogramSpec.h>
+#include <Framework/InitContext.h>
+#include <Framework/O2DatabasePDGPlugin.h>
+#include <Framework/OutputObjHeader.h>
+#include <Framework/runDataProcessing.h>
 
 #include <TPDGCode.h>
 #include <TString.h>
