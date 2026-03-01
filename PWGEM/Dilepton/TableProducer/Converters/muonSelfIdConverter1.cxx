05e44f49955fb986d3fda25345ceb59c57a52000
Daiki Sekihata <daiki.sekihata@cern.ch>
2026-03-01 13:12:12 +0100

    PWGEM/Dilepton: update muon analyses

---
+++ PWGEM/Dilepton/TableProducer/Converters/muonSelfIdConverter1.cxx
@@ -0,0 +1,43 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+//
+// ========================
+//
+// This code produces muon table 001 from 000.
+//    Please write to: daiki.sekihata@cern.ch
+
+#include "PWGEM/Dilepton/DataModel/dileptonTables.h"
+
+#include "Framework/ASoAHelpers.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/runDataProcessing.h"
+
+using namespace o2;
+using namespace o2::aod;
+using namespace o2::framework;
+using namespace o2::framework::expressions;
+using namespace o2::soa;
+
+struct muonSelfIdConverter1 {
+  Produces<aod::EMGlobalMuonSelfIds_001> muon_001;
+
+  void process(aod::EMGlobalMuonSelfIds_000 const& muons)
+  {
+    for (const auto& muon : muons) {
+      muon_001(std::vector<int>{}, muon.globalMuonsWithSameMFTIds());
+    } // end of muon loop
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{adaptAnalysisTask<muonSelfIdConverter1>(cfgc, TaskName{"muon-selfif-converter1"})};
+}
