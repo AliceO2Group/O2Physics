7da00937896e723e75cd818ff4d4c1e1619e1f1e
Martijn Laarhoven <94106121+MartijnLaarhoven@users.noreply.github.com>
2026-03-17 09:21:03 +0100

    Update loadGain function to accept collision object

--- PWGCF/TwoParticleCorrelations/Tasks/flowDecorrelation.cxx
+++ PWGCF/TwoParticleCorrelations/Tasks/flowDecorrelation.cxx
@@ -1274,7 +1274,7 @@ struct FlowDecorrelation {
       auto bc = collision1.bc_as<aod::BCsWithTimestamps>();
       loadAlignParam(bc.timestamp());
       loadCorrection(bc.timestamp());
-      loadGain(bc.timestamp());
+      loadGain(bc);
       float eventWeight = 1.0f;
       if (cfgUseEventWeights) {
         eventWeight = 1.0f / it.currentWindowNeighbours();
