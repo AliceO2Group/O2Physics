f4912b9b2a1cd0f891cde2d4e62ffa9f08d4ab5d
murioni <marta.urioni@cern.ch>
2026-03-20 16:58:33 +0100

    adding FT0M multiplicity at generated level, check on split events

--- PWGLF/Tasks/GlobalEventProperties/heavyionMultiplicity.cxx
+++ PWGLF/Tasks/GlobalEventProperties/heavyionMultiplicity.cxx
@@ -343,7 +343,11 @@ struct HeavyionMultiplicity {
       histos.add("hMultEta05vsCentrRec", "multiplicity in eta<0.5 of selected MC events vs centrality", kTH2F, {axisCent, multAxis});
       histos.add("hgendndetaVsMultEta05BeforeEvtSel", "hgendndetaBeforeEvtSel vs multiplicity in eta<0.5", kTH2F, {axisEta, multAxis});
       histos.add("hgendndetaVsMultEta05AfterEvtSel", "hgendndetaAfterEvtSel vs multiplicity in eta<0.5", kTH2F, {axisEta, multAxis});
-
+      histos.add("hImpactParameterSplit", "Impact parameter of selected and split MC events", kTH1F,{impactParAxis});
+      histos.add("hMultEta05Split"      , "multiplicity in eta<0.5 of selected and split MC events", kTH1F,{multAxis});
+      histos.add("hMultSplit"           , "multiplicity of selected and split MC events", kTH1F,{axisFt0cMult});
+      histos.add("hMultvsCentrSplit"    , "multiplicity of selected and split MC events vs centrality ", kTH2F,{axisCent, axisFt0cMult});
+     
       histos.add("hMultGen", "multiplicity of generated MC events", kTH1F, {axisFt0cMult});
       histos.add("hMultRec", "multiplicity of selected MC events", kTH1F, {axisFt0cMult});
       histos.add("hMultvsCentrRec", "multiplicity of selected MC events vs centrality", kTH2F, {axisCent, axisFt0cMult});
@@ -463,7 +467,9 @@ struct HeavyionMultiplicity {
     if (isApplyCentFT0C) {
       cent = col.multMCFT0C();
     }
-    if (isApplyCentFV0A) {
+    else if(isApplyCentFT0M) {
+      cent = (col.multMCFT0C() + col.multMCFT0A())/2.;
+    } else if (isApplyCentFV0A) {
       cent = col.multMCFV0A();
     }
     return cent;
@@ -990,6 +996,12 @@ struct HeavyionMultiplicity {
       if (std::abs(RecCol.posZ()) >= vtxRange) {
         continue;
       }
+      
+      histos.fill(HIST("hImpactParameterSplit"), mcCollision.impactParameter());
+      histos.fill(HIST("hMultEta05Split"), mcCollision.multMCNParticlesEta05());
+      histos.fill(HIST("hMultSplit"), selColMultMC(mcCollision));
+      histos.fill(HIST("hMultvsCentrSplit"), selColCent(RecCol), selColMultMC(mcCollision));
+
       if (RecCol.numContrib() <= numcontributors) {
         continue;
       } else {
