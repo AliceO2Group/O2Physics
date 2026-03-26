c8218b66b9734905e43cfd8dfa72f6e13f96814e
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2026-03-26 10:43:15 +0100

    Fix includes and using statements

--- DPG/Tasks/AOTTrack/PID/TOF/qaPIDTOFBeta.cxx
+++ DPG/Tasks/AOTTrack/PID/TOF/qaPIDTOFBeta.cxx
@@ -16,15 +16,24 @@
 ///
 
 #include "Common/DataModel/EventSelection.h"
-#include "Common/DataModel/FT0Corrected.h"
 #include "Common/DataModel/PIDResponseTOF.h"
 #include "Common/DataModel/TrackSelectionTables.h"
-#include "Common/TableProducer/PID/pidTOFBase.h"
 
-#include "Framework/AnalysisTask.h"
-#include "Framework/HistogramRegistry.h"
-#include "Framework/StaticFor.h"
-#include "Framework/runDataProcessing.h"
+#include <Framework/ASoA.h>
+#include <Framework/AnalysisDataModel.h>
+#include <Framework/AnalysisTask.h>
+#include <Framework/Configurable.h>
+#include <Framework/Expressions.h>
+#include <Framework/HistogramRegistry.h>
+#include <Framework/HistogramSpec.h>
+#include <Framework/InitContext.h>
+#include <Framework/OutputObjHeader.h>
+#include <Framework/runDataProcessing.h>
+
+#include <TH1.h>
+#include <TString.h>
+
+#include <cstdlib>
 
 using namespace o2;
 using namespace o2::framework;
--- DPG/Tasks/AOTTrack/PID/TOF/qaPIDTOFEvTime.cxx
+++ DPG/Tasks/AOTTrack/PID/TOF/qaPIDTOFEvTime.cxx
@@ -21,15 +21,25 @@
 #include "Common/DataModel/TrackSelectionTables.h"
 #include "Common/TableProducer/PID/pidTOFBase.h"
 
-#include "CommonConstants/LHCConstants.h"
-#include "DataFormatsFT0/Digit.h"
-#include "Framework/AnalysisTask.h"
-#include "Framework/HistogramRegistry.h"
-#include "Framework/StaticFor.h"
-#include "Framework/runDataProcessing.h"
+#include <CommonConstants/LHCConstants.h>
+#include <DataFormatsFT0/Digit.h>
+#include <Framework/ASoA.h>
+#include <Framework/AnalysisDataModel.h>
+#include <Framework/AnalysisHelpers.h>
+#include <Framework/AnalysisTask.h>
+#include <Framework/Configurable.h>
+#include <Framework/HistogramRegistry.h>
+#include <Framework/HistogramSpec.h>
+#include <Framework/InitContext.h>
+#include <Framework/OutputObjHeader.h>
+#include <Framework/runDataProcessing.h>
+#include <PID/PIDTOF.h>
 
-#include "TEfficiency.h"
-#include "THashList.h"
+#include <TEfficiency.h>
+#include <THashList.h>
+#include <TString.h>
+
+#include <bitset>
 
 using namespace o2;
 using namespace o2::framework;
