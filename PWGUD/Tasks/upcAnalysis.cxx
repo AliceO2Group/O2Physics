7e219fc5f6b028755f2933060d5cc88dd901ed34
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2026-03-28 16:51:30 +0100

    [PWGUD] Fix includes and using statements (#15390)

--- PWGUD/Tasks/upcAnalysis.cxx
+++ PWGUD/Tasks/upcAnalysis.cxx
@@ -9,16 +9,22 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include "Framework/runDataProcessing.h"
-#include "Framework/AnalysisTask.h"
-#include "Framework/AnalysisDataModel.h"
-
-#include "Common/DataModel/TrackSelectionTables.h"
+#include "Common/CCDB/EventSelectionParams.h"
+#include "Common/CCDB/TriggerAliases.h"
 #include "Common/DataModel/EventSelection.h"
+#include "Common/DataModel/TrackSelectionTables.h"
 
-#include <TH1D.h>
-#include <TH2D.h>
-#include "TLorentzVector.h"
+#include <Framework/AnalysisDataModel.h>
+#include <Framework/AnalysisHelpers.h>
+#include <Framework/AnalysisTask.h>
+#include <Framework/runDataProcessing.h>
+
+#include <TH1.h>
+#include <TH2.h>
+#include <TLorentzVector.h>
+
+#include <Rtypes.h>
+#include <RtypesCore.h>
 
 using namespace o2;
 using namespace o2::aod::evsel;
