ad748e7db77e52b35811d4b243cb4cb2c38e2d96
Rashi gupta <167059733+rashigupt@users.noreply.github.com>
2026-04-13 15:12:43 +0530

    Remove build error

--- PWGHF/HFC/DataModel/CorrelationTables.h
+++ PWGHF/HFC/DataModel/CorrelationTables.h
@@ -519,6 +519,29 @@ DECLARE_SOA_TABLE(HfElectron, "AOD", "HFELECTRON", //! Hf Electron properties
                   aod::hf_electron::GIndexCol,
                   aod::hf_electron::TimeStamp);
 // Note: definition of columns and tables for Electron Hadron correlation pairs for Data
+namespace hf_electron
+{
+DECLARE_SOA_COLUMN(PhiElectron, phiElectron, float); //! Phi of electron
+DECLARE_SOA_COLUMN(EtaElectron, etaElectron, float); //! Eta of electron
+DECLARE_SOA_COLUMN(PtElectron, ptElectron, float);   //! Transverse momentum of electron
+DECLARE_SOA_COLUMN(NElectronsLS, nElectronsLS, int); //! number of like-sign
+DECLARE_SOA_COLUMN(NElectronsUS, nElectronsUS, int); //! number of Unlike-sign
+DECLARE_SOA_COLUMN(PoolBin, poolBin, int);           //! Pool Bin of event defined using zvtx and multiplicit
+DECLARE_SOA_COLUMN(GIndexCol, gIndexCol, int);       //! Global index for the collision
+DECLARE_SOA_COLUMN(TimeStamp, timeStamp, int64_t);   //! Timestamp for the collision
+
+} // namespace hf_electron
+
+DECLARE_SOA_TABLE(HfElectron, "AOD", "HFELECTRON", //! Hf Electron properties
+                  aod::hf_electron::PhiElectron,
+                  aod::hf_electron::EtaElectron,
+                  aod::hf_electron::PtElectron,
+                  aod::hf_electron::NElectronsLS,
+                  aod::hf_electron::NElectronsUS,
+                  aod::hf_electron::PoolBin,
+                  aod::hf_electron::GIndexCol,
+                  aod::hf_electron::TimeStamp);
+// Note: definition of columns and tables for Electron Hadron correlation pairs for Data
 namespace hf_correlation_electron_hadron
 {
 DECLARE_SOA_COLUMN(DeltaPhi, deltaPhi, float);         //! DeltaPhi between Electron and Hadrons
@@ -528,16 +551,16 @@ DECLARE_SOA_COLUMN(PtHadron, ptHadron, float);         //! Transverse momentum o
 DECLARE_SOA_COLUMN(EopElectron, eopElectron, float);   //! enery momentum ratio for Electron
 DECLARE_SOA_COLUMN(M02Electron, m02Electron, float);   //! M02 of Electron
 DECLARE_SOA_COLUMN(TpcNSigmaEle, tpcNSigmaEle, float); //! TPCn Sigma of Electron
-DECLARE_SOA_COLUMN(TofnSigmaEle, tofNSigmaEle, float); //! TOFn Sigma of Electron
+DECLARE_SOA_COLUMN(TofNSigmaEle, tofNSigmaEle, float); //! TOFn Sigma of Electron
 
-DECLARE_SOA_COLUMN(TpcNClsCrRowsEle, tpcNClsCrossedRowsEle, float); //! tpcNClsCrossedRows for Electron
+DECLARE_SOA_COLUMN(TpcNClsCrRowsEle, tpcNClsCrRowsEle, float); //! tpcNClsCrossedRows for Electron
 DECLARE_SOA_COLUMN(TpcCrRowsRatioEle, tpcCrRowsRatioEle, float);    //! Ratio crossed rows over findable clusters electron track
 DECLARE_SOA_COLUMN(ItsChi2NClEle, itsChi2NClEle, float);            //! itsChi2NCl of Electron
 DECLARE_SOA_COLUMN(TpcChi2NClEle, tpcChi2NClEle, float);            //! TPC Chi2NCl of electron;
 DECLARE_SOA_COLUMN(DcaXYEle, dcaXYEle, float);                      //! DCAXY  of Electron
 DECLARE_SOA_COLUMN(DcaZEle, dcaZEle, float);                        //! DCAZ of Electron
 
-DECLARE_SOA_COLUMN(TpcNClsCrRowsHad, tpcNClsCrossedRowsHad, float); //! tpcNClsCrossedRows for Hadron
+DECLARE_SOA_COLUMN(TpcNClsCrRowsHad, tpcNClsCrRowsHad, float); //! tpcNClsCrossedRows for Hadron
 DECLARE_SOA_COLUMN(TpcCrRowsRatioHad, tpcCrRowsRatioHad, float);    //! Ratio crossed rows over findable clusters Hadron track
 DECLARE_SOA_COLUMN(ItsChi2NClHad, itsChi2NClHad, float);            //! itsChi2NCl of Hadron
 DECLARE_SOA_COLUMN(TpcChi2NClHad, tpcChi2NClHad, float);            //! TPC Chi2NCl of Hadron;
@@ -555,8 +578,8 @@ DECLARE_SOA_TABLE(HfEHadronPair, "AOD", "HFEHADRONPAIR", //! Hfe-Hadrons pairs I
                   hf_correlation_electron_hadron::PtHadron,
                   hf_correlation_electron_hadron::EopElectron,
                   hf_correlation_electron_hadron::M02Electron,
-                  hf_correlation_electron_hadron::TpcnSigmaEle,
-                  hf_correlation_electron_hadron::TofnSigmaEle,
+                  hf_correlation_electron_hadron::TpcNSigmaEle,
+                  hf_correlation_electron_hadron::TofNSigmaEle,
                   hf_correlation_electron_hadron::TpcNClsCrRowsEle,
                   hf_correlation_electron_hadron::TpcCrRowsRatioEle,
                   hf_correlation_electron_hadron::ItsChi2NClEle,
@@ -572,7 +595,6 @@ DECLARE_SOA_TABLE(HfEHadronPair, "AOD", "HFEHADRONPAIR", //! Hfe-Hadrons pairs I
                   hf_correlation_electron_hadron::PoolBin,
                   hf_correlation_electron_hadron::NPairsLS,
                   hf_correlation_electron_hadron::NPairsUS);
-
 // Note: definition of columns and tables for Electron Hadron correlation pairs for MC Gen
 namespace hf_correlation_mcgenelectron_hadron
 {
