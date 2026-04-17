73588114dcf9875f7557a2295e0390c69819ce42
Daiki Sekihata <daiki.sekihata@cern.ch>
2026-04-17 11:47:02 +0200

    [PWGEM/Dilepton] fix axis title in skimmerPrimaryMuon.cxx (#15829)

--- PWGEM/Dilepton/TableProducer/skimmerPrimaryMuon.cxx
+++ PWGEM/Dilepton/TableProducer/skimmerPrimaryMuon.cxx
@@ -206,8 +206,8 @@ struct skimmerPrimaryMuon {
     fRegistry.add("MFTMCHMID/hDCAy_PosZ", "DCAy vs. posZ;Z_{vtx} (cm);DCA_{y} (cm)", kTH2F, {{200, -10, +10}, {400, -0.2, +0.2}}, false);
     fRegistry.add("MFTMCHMID/hDCAx_Phi", "DCAx vs. #varphi;#varphi (rad.);DCA_{x} (cm)", kTH2F, {{90, 0, 2 * M_PI}, {400, -0.2, +0.2}}, false);
     fRegistry.add("MFTMCHMID/hDCAy_Phi", "DCAy vs. #varphi;#varphi (rad.);DCA_{y} (cm)", kTH2F, {{90, 0, 2 * M_PI}, {400, -0.2, +0.2}}, false);
-    fRegistry.add("MFTMCHMID/hMeanDCAx", "<DCAy>;X_{IU} (cm);Y_IU} (cm);<DCA_{x}> (cm)", kTProfile2D, {{240, -12, +12}, {240, -12, +12}}, false);
-    fRegistry.add("MFTMCHMID/hMeanDCAy", "<DCAy>;X_{IU} (cm);Y_IU} (cm);<DCA_{y}> (cm)", kTProfile2D, {{240, -12, +12}, {240, -12, +12}}, false);
+    fRegistry.add("MFTMCHMID/hMeanDCAx", "<DCAy>;X_{IU} (cm);Y_{IU} (cm);<DCA_{x}> (cm)", kTProfile2D, {{240, -12, +12}, {240, -12, +12}}, false);
+    fRegistry.add("MFTMCHMID/hMeanDCAy", "<DCAy>;X_{IU} (cm);Y_{IU} (cm);<DCA_{y}> (cm)", kTProfile2D, {{240, -12, +12}, {240, -12, +12}}, false);
     fRegistry.add("MFTMCHMID/hNmu", "#mu multiplicity;N_{#mu} per collision", kTH1F, {{21, -0.5, 20.5}}, false);
 
     fRegistry.addClone("MFTMCHMID/", "MCHMID/");
