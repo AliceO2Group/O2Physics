d80c5de40ebdb16314d3ba693ba69a17c750cab1
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2026-04-30 10:43:03 +0200

    Ignore warnings for C-style VLAs

--- dependencies/O2PhysicsCompileFlags.cmake
+++ dependencies/O2PhysicsCompileFlags.cmake
@@ -17,7 +17,7 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra")
 set(O2PHYSICS_WARNINGS_COMMON_NO_ERROR "")
 
 # Enabled warnings supported by Clang only, not treated as errors
-set(O2PHYSICS_WARNINGS_CLANG_NO_ERROR "")
+set(O2PHYSICS_WARNINGS_CLANG_NO_ERROR "vla-cxx-extension")
 
 # Enabled warnings supported by GCC only, not treated as errors
 set(O2PHYSICS_WARNINGS_GCC_NO_ERROR "")
