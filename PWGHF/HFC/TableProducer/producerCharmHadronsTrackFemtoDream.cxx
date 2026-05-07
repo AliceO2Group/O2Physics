13d61ef4678966625b342c4399eb6f3aa4ec6235
BiaoZhang (张彪) <52267892+zhangbiao-phy@users.noreply.github.com>
2026-05-07 14:13:17 +0200

    Fix nSigmaCombPiMax configuration and condition

--- PWGHF/HFC/TableProducer/producerCharmHadronsTrackFemtoDream.cxx
+++ PWGHF/HFC/TableProducer/producerCharmHadronsTrackFemtoDream.cxx
@@ -154,7 +154,7 @@ struct HfProducerCharmHadronsTrackFemtoDream {
     Configurable<float> nSigmaTpcElMin{"nSigmaTpcElMin", 3.f, "Kaon PID Method1: require |nSigmaTpcEl| > this (electron)"};
     Configurable<float> nSigmaCombKaMax{"nSigmaCombKaMax", 3.f, "Kaon PID Method2: require |nSigmaCombKa| < this"};
     Configurable<float> nSigmaCombKaTightMax{"nSigmaCombKaTightMax", 2.f, "Kaon PID Method2: for p > pTrackTightMin require |nSigmaCombKa| < this"};
-    Configurable<float> nSigmaCombPiMax{"nSigmaCombPiMax", 6.f, "Kaon PID Method2: for p > pTrackTightMin require |nSigmaCombPi| < this"};
+    Configurable<float> {"", 6.f, "Kaon PID Method2: for p > pTrackTightMin require |nSigmaCombPi| < this"};
   } kaonPidSel;
 
   Configurable<bool> isDebug{"isDebug", true, "Enable Debug tables"};
@@ -391,7 +391,7 @@ struct HfProducerCharmHadronsTrackFemtoDream {
       if (std::abs(nSigmaCombKa) >= kaonPidSel.nSigmaCombKaTightMax) {
         isTrackKaonPidMethod2 = false;
       }
-      if (std::abs(nSigmaCombPi) >= kaonPidSel.nSigmaCombPiMax) {
+      if (std::abs(nSigmaCombPi) <= kaonPidSel.nSigmaCombPiMax) {
         isTrackKaonPidMethod2 = false;
       }
     }
