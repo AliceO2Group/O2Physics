2c875faf74075b9c2b75d11e810aa787db7330d0
Rashi gupta <167059733+rashigupt@users.noreply.github.com>
2026-05-07 10:01:53 +0530

    remove offline event mixing

--- PWGHF/HFC/Tasks/taskCorrelationHfeHadrons.cxx
+++ PWGHF/HFC/Tasks/taskCorrelationHfeHadrons.cxx
@@ -37,7 +37,6 @@ struct HfTaskCorrelationHfeHadrons {
   ConfigurableAxis binsDeltaEta{"binsDeltaEta", {30, -1.8, 1.8}, "#it{#Delta#eta}"};
   ConfigurableAxis binsDeltaPhi{"binsDeltaPhi", {32, -o2::constants::math::PIHalf, 3. * o2::constants::math::PIHalf}, "#it{#Delta#varphi}"};
   ConfigurableAxis binsPt{"binsPt", {50, 0.0, 50}, "#it{p_{T}}(GeV/#it{c})"};
-  ConfigurableAxis binsPhi{"binsPhi", {100, 0.0, 7}, "#it{p_{T}}(GeV/#it{c})"};
 
   HistogramRegistry registry{
     "registry",
@@ -48,22 +47,11 @@ struct HfTaskCorrelationHfeHadrons {
     AxisSpec axisDeltaEta = {binsDeltaEta, "#Delta #eta = #eta_{Electron}- #eta_{Hadron}"};
     AxisSpec axisDeltaPhi = {binsDeltaPhi, "#Delta #varphi = #varphi_{Electron}- #varphi_{Hadron}"};
     AxisSpec axisPt = {binsPt, "#it{p_{T}}(GeV/#it{c})"};
-    AxisSpec axisPhi = {binsPhi, "#it{#varphi}"};
+
     registry.add("hInclusiveEHCorrel", "Sparse for Delta phi and Delta eta Inclusive Electron with Hadron;p_{T}^{e} (GeV#it{/c});p_{T}^{h} (GeV#it{/c});#Delta#varphi;#Delta#eta;", {HistType::kTHnSparseF, {{axisPt}, {axisPt}, {axisDeltaPhi}, {axisDeltaEta}}});
     registry.add("hLikeSignEHCorrel", "Sparse for Delta phi and Delta eta Like sign Electron pair  with Hadron;p_{T}^{e} (GeV#it{/c});p_{T}^{h} (GeV#it{/c});#Delta#varphi;#Delta#eta;", {HistType::kTHnSparseF, {{axisPt}, {axisPt}, {axisDeltaPhi}, {axisDeltaEta}}});
     registry.add("hUnLikeSignEHCorrel", "Sparse for Delta phi and Delta eta  UnLike sign Electron pair with Hadron;p_{T}^{e} (GeV#it{/c});p_{T}^{h} (GeV#it{/c});#Delta#varphi;#Delta#eta;", {HistType::kTHnSparseF, {{axisPt}, {axisPt}, {axisDeltaPhi}, {axisDeltaEta}}});
-
-    registry.add("hInclusiveMixEHCorrel", "Sparse for Delta phi and Delta eta Inclusive Electron with Hadron;p_{T}^{e} (GeV#it{/c});p_{T}^{h} (GeV#it{/c});#Delta#varphi;#Delta#eta;", {HistType::kTHnSparseD, {{axisPt}, {axisPt}, {axisDeltaPhi}, {axisDeltaEta}}});
-    registry.add("hLikeSignMixEHCorrel", "Sparse for Delta phi and Delta eta Like sign Electron pair  with Hadron;p_{T}^{e} (GeV#it{/c});p_{T}^{h} (GeV#it{/c});#Delta#varphi;#Delta#eta;", {HistType::kTHnSparseD, {{axisPt}, {axisPt}, {axisDeltaPhi}, {axisDeltaEta}}});
-    registry.add("hUnlikeSignMixEHCorrel", "Sparse for Delta phi and Delta eta  UnLike sign Electron pair with Hadron;p_{T}^{e} (GeV#it{/c});p_{T}^{h} (GeV#it{/c});#Delta#varphi;#Delta#eta;", {HistType::kTHnSparseD, {{axisPt}, {axisPt}, {axisDeltaPhi}, {axisDeltaEta}}});
-    registry.add("hPhi", "phi distribution for electron", {HistType::kTH1D, {{axisPhi}}});
-    registry.add("hPhiLS", "phi distribution for Like sign electron", {HistType::kTH1D, {{axisPhi}}});
-    registry.add("hPhiUS", "phi distribution for Unlike sign electron", {HistType::kTH1D, {{axisPhi}}});
-
-    registry.add("hadronPhi", "phi distribution for hadron", {HistType::kTH1D, {{axisPhi}}});
-
     registry.add("hMcGenInclusiveEHCorrel", "Sparse for Delta phi and Delta eta for McGen Inclusive Electron  with Hadron;p_{T}^{e} (GeV#it{/c});p_{T}^{h} (GeV#it{/c});#Delta#varphi;#Delta#eta;", {HistType::kTHnSparseF, {{axisPt}, {axisPt}, {axisDeltaPhi}, {axisDeltaEta}}});
-
     registry.add("hMcGenNonHfEHCorrel", "Sparse for Delta phi and Delta eta  for McGen  NonHeavy flavour Electron pair  with Hadron;p_{T}^{e} (GeV#it{/c});p_{T}^{h} (GeV#it{/c});#Delta#varphi;#Delta#eta;", {HistType::kTHnSparseF, {{axisPt}, {axisPt}, {axisDeltaPhi}, {axisDeltaEta}}});
   }
 
@@ -100,57 +88,6 @@ struct HfTaskCorrelationHfeHadrons {
 
   PROCESS_SWITCH(HfTaskCorrelationHfeHadrons, process, "Process ", false);
 
-  // correlation  for electron hadron
-  void processDataMixedEvent(aod::HfElectron const& electrons, aod::Hadron const& hadrons)
-  {
-
-    for (const auto& electron : electrons) {
-
-      double deltaPhiMix = -999;
-      double deltaEtaMix = -999;
-      registry.fill(HIST("hPhi"), electron.phiElectron());
-
-      if (electron.nElectronsUS() > 0) {
-
-        registry.fill(HIST("hPhiUS"), electron.phiElectron());
-      }
-
-      if (electron.nElectronsLS() > 0) {
-
-        registry.fill(HIST("hPhiLS"), electron.phiElectron());
-      }
-      if (electron.poolBin() < 0)
-        continue;
-
-      for (const auto& hadron : hadrons) {
-        if (electron.timeStamp() == hadron.timeStamp()) {
-
-          continue; // or continue; if you're inside a loop
-        }
-
-        if (hadron.poolBin() == electron.poolBin()) {
-          continue;
-        }
-        if (hadron.gIndexCol() == electron.gIndexCol()) {
-          continue;
-        }
-
-        deltaPhiMix = RecoDecay::constrainAngle(electron.phiElectron() - hadron.phi(), -o2::constants::math::PIHalf);
-        deltaEtaMix = electron.etaElectron() - hadron.eta();
-
-        registry.fill(HIST("hadronPhi"), hadron.phi());
-        registry.fill(HIST("hInclusiveMixEHCorrel"), electron.ptElectron(), hadron.ptH(), deltaPhiMix, deltaEtaMix);
-
-        if (electron.nElectronsLS() > 0) {
-          registry.fill(HIST("hLikeSignMixEHCorrel"), electron.ptElectron(), hadron.ptH(), deltaPhiMix, deltaEtaMix);
-        }
-        if (electron.nElectronsUS() > 0) {
-          registry.fill(HIST("hUnlikeSignMixEHCorrel"), electron.ptElectron(), hadron.ptH(), deltaPhiMix, deltaEtaMix);
-        }
-      }
-    }
-  }
-  PROCESS_SWITCH(HfTaskCorrelationHfeHadrons, processDataMixedEvent, "Process offlineevent mix ", true);
   void processMcGen(aod::HfEHadronMcPair const& mcGenpairEntries)
   {
     double deltaPhi = -999;
@@ -172,7 +109,7 @@ struct HfTaskCorrelationHfeHadrons {
       }
     }
   }
-  PROCESS_SWITCH(HfTaskCorrelationHfeHadrons, processMcGen, "Process for Mc Gen ", false);
+  PROCESS_SWITCH(HfTaskCorrelationHfeHadrons, processMcGen, "Process for Mc Gen ", true);
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
