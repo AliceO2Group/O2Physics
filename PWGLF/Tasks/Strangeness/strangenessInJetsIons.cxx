c2c115f66aa4eba8fc7eeacb36e5e59e45241ec0
lorber98 <95907752+lorber98@users.noreply.github.com>
2026-05-20 10:46:39 +0200

    Add rapidity cut for data in MB events

--- PWGLF/Tasks/Strangeness/strangenessInJetsIons.cxx
+++ PWGLF/Tasks/Strangeness/strangenessInJetsIons.cxx
@@ -287,7 +287,7 @@ struct StrangenessInJetsIons {
       registryData.add("number_of_events_vsmultiplicity", "number of events in data vs multiplicity", HistType::kTH1D, {{101, 0, 101, "Multiplicity percentile"}});
 
       // For MB
-      registryData.add("number_of_events_vsmultiplicity_MB", "number of events in data vs multiplicity (MB)", HistType::kTH1D, {{101, 0, 101, "Multiplicity percentile"}});
+      // registryData.add("number_of_events_vsmultiplicity_MB", "number of events in data vs multiplicity (MB)", HistType::kTH1D, {{101, 0, 101, "Multiplicity percentile"}});
 
       registryData.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(1, "All collisions");
       registryData.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(2, "Zorro selection");
@@ -555,17 +555,17 @@ struct StrangenessInJetsIons {
     if (doprocessDataMB) {
 
       // Event counters
-      registryDataMB.add("number_of_events_data", "number of events in data", HistType::kTH1D, {{20, 0, 20, "Event Cuts"}});
-      registryDataMB.add("number_of_events_vsmultiplicity", "number of events in data vs multiplicity", HistType::kTH1D, {{101, 0, 101, "Multiplicity percentile"}});
+      registryDataMB.add("number_of_events_data_MB", "number of events in data (MB)", HistType::kTH1D, {{20, 0, 20, "Event Cuts"}});
+      registryDataMB.add("number_of_events_vsmultiplicity_MB", "number of events in data vs multiplicity (MB)", HistType::kTH1D, {{101, 0, 101, "Multiplicity percentile"}});
 
-      registryDataMB.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(1, "All collisions");
-      registryDataMB.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(2, "Zorro selection");
-      registryDataMB.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(3, "sel8");
-      registryDataMB.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(4, "posZ cut");
-      registryDataMB.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(5, "kNoSameBunchPileup");
-      registryDataMB.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(6, "kIsGoodZvtxFT0vsPV");
-      registryDataMB.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(7, "No empty events");
-      registryDataMB.get<TH1>(HIST("number_of_events_data"))->GetXaxis()->SetBinLabel(8, "At least one jet");
+      registryDataMB.get<TH1>(HIST("number_of_events_data_MB"))->GetXaxis()->SetBinLabel(1, "All collisions");
+      registryDataMB.get<TH1>(HIST("number_of_events_data_MB"))->GetXaxis()->SetBinLabel(2, "Zorro selection");
+      registryDataMB.get<TH1>(HIST("number_of_events_data_MB"))->GetXaxis()->SetBinLabel(3, "sel8");
+      registryDataMB.get<TH1>(HIST("number_of_events_data_MB"))->GetXaxis()->SetBinLabel(4, "posZ cut");
+      registryDataMB.get<TH1>(HIST("number_of_events_data_MB"))->GetXaxis()->SetBinLabel(5, "kNoSameBunchPileup");
+      registryDataMB.get<TH1>(HIST("number_of_events_data_MB"))->GetXaxis()->SetBinLabel(6, "kIsGoodZvtxFT0vsPV");
+      registryDataMB.get<TH1>(HIST("number_of_events_data_MB"))->GetXaxis()->SetBinLabel(7, "No empty events");
+      registryDataMB.get<TH1>(HIST("number_of_events_data_MB"))->GetXaxis()->SetBinLabel(8, "At least one jet");
 
       // Histograms for analysis of strange hadrons
       if (particleOfInterestDict[ParticleOfInterest::kV0Particles]) {
@@ -1188,6 +1188,64 @@ struct StrangenessInJetsIons {
       return false;
     return true;
   }
+
+  // --- MB Selections ------------------------------------
+  // The rapidty cut is added, which is not required in jet and UE
+  // K0s selections (MB)
+  template <typename K0short, typename TrackPos, typename TrackNeg>
+  bool passedK0ShortSelectionMB(const K0short& v0, const TrackPos& ptrack, const TrackNeg& ntrack, const TVector3& vtxPos)
+  {
+    bool passedSel = passedK0ShortSelection(v0, ptrack, ntrack, vtxPos);
+
+    if (!passedRapidityCut(v0.yK0Short(), configV0.rapidityMax))
+      return false;
+    return passedSel;
+  }
+
+  // Lambda selections (MB)
+  template <typename Lambda, typename TrackPos, typename TrackNeg>
+  bool passedLambdaSelectionMB(const Lambda& v0, const TrackPos& ptrack, const TrackNeg& ntrack, const TVector3& vtxPos)
+  {
+    bool passedSel = passedLambdaSelection(v0, ptrack, ntrack, vtxPos);
+
+    if (!passedRapidityCut(v0.yLambda(), configV0.rapidityMax))
+      return false;
+    return passedSel;
+  }
+
+  // AntiLambda selections (MB)
+  template <typename AntiLambda, typename TrackPos, typename TrackNeg>
+  bool passedAntiLambdaSelectionMB(const AntiLambda& v0, const TrackPos& ptrack, const TrackNeg& ntrack, const TVector3& vtxPos)
+  {
+    bool passedSel = passedAntiLambdaSelection(v0, ptrack, ntrack, vtxPos);
+
+    if (!passedRapidityCut(v0.yLambda(), configV0.rapidityMax))
+      return false;
+    return passedSel;
+  }
+
+  // Xi Selections (MB)
+  template <typename Xi, typename TrackPos, typename TrackNeg, typename TrackBac, typename Coll>
+  bool passedXiSelectionMB(const Xi& casc, const TrackPos& ptrack, const TrackNeg& ntrack, const TrackBac& btrack, const Coll& coll)
+  {
+    bool passedSel = passedXiSelection(casc, ptrack, ntrack, btrack, coll);
+
+    if (!passedRapidityCut(casc.yXi(), configV0.rapidityMax))
+      return false;
+    return passedSel;
+  }
+
+  // Omega selections (MB)
+  template <typename Omega, typename TrackPos, typename TrackNeg, typename TrackBac, typename Coll>
+  bool passedOmegaSelectionMB(const Omega& casc, const TrackPos& ptrack, const TrackNeg& ntrack, const TrackBac& btrack, const Coll& coll)
+  {
+    bool passedSel = passedOmegaSelection(casc, ptrack, ntrack, btrack, coll);
+
+    if (!passedRapidityCut(casc.yOmega(), configV0.rapidityMax))
+      return false;
+    return passedSel;
+  }
+  // ------------------------------------------------------
 
   // Event selection for MC Reco collision
   template <typename coll>
@@ -1917,7 +1975,7 @@ struct StrangenessInJetsIons {
     } else {
       centrality = collision.centFT0M();
     }
-    registryData.fill(HIST("number_of_events_vsmultiplicity_MB"), centrality);
+    // registryData.fill(HIST("number_of_events_vsmultiplicity_MB"), centrality);
 
     // Loop over reconstructed tracks
     std::vector<fastjet::PseudoJet> fjParticles;
@@ -3232,7 +3290,7 @@ struct StrangenessInJetsIons {
     TVector3 vtxPos(collision.posX(), collision.posY(), collision.posZ());
 
     // Fill event counter before event selection
-    registryDataMB.fill(HIST("number_of_events_data"), 0.5);
+    registryDataMB.fill(HIST("number_of_events_data_MB"), 0.5);
 
     // Get the bunch crossing (BC) information associated with the collision
     auto bc = collision.template bc_as<aod::BCsWithTimestamps>();
@@ -3246,35 +3304,35 @@ struct StrangenessInJetsIons {
     }
 
     // Fill event counter after zorro selection
-    registryDataMB.fill(HIST("number_of_events_data"), 1.5);
+    registryDataMB.fill(HIST("number_of_events_data_MB"), 1.5);
 
     // Event selection
     if (!collision.sel8())
       return;
 
     // Fill event counter after sel8 selection
-    registryDataMB.fill(HIST("number_of_events_data"), 2.5);
+    registryDataMB.fill(HIST("number_of_events_data_MB"), 2.5);
 
     // Require vertex position within the allowed z range
     if (std::fabs(collision.posZ()) > zVtx)
       return;
 
     // Fill event counter after z vertex selection
-    registryDataMB.fill(HIST("number_of_events_data"), 3.5);
+    registryDataMB.fill(HIST("number_of_events_data_MB"), 3.5);
 
     // Reject collisions associated to the same found BC
     if (requireNoSameBunchPileup && !collision.selection_bit(o2::aod::evsel::kNoSameBunchPileup))
       return;
 
     // Fill event counter after selection kNoSameBunchPileup
-    registryDataMB.fill(HIST("number_of_events_data"), 4.5);
+    registryDataMB.fill(HIST("number_of_events_data_MB"), 4.5);
 
     // Compatible z_vtx from FT0 and from PV
     if (requireGoodZvtxFT0vsPV && !collision.selection_bit(o2::aod::evsel::kIsGoodZvtxFT0vsPV))
       return;
 
     // Fill event counter after selection kIsGoodZvtxFT0vsPV
-    registryDataMB.fill(HIST("number_of_events_data"), 5.5);
+    registryDataMB.fill(HIST("number_of_events_data_MB"), 5.5);
 
     // Event multiplicity
     float centrality;
@@ -3283,7 +3341,7 @@ struct StrangenessInJetsIons {
     } else {
       centrality = collision.centFT0M();
     }
-    registryDataMB.fill(HIST("number_of_events_vsmultiplicity"), centrality);
+    registryDataMB.fill(HIST("number_of_events_vsmultiplicity_MB"), centrality);
 
     if (particleOfInterestDict[ParticleOfInterest::kV0Particles]) { // V0s
       for (const auto& v0 : fullV0s) {
@@ -3294,15 +3352,15 @@ struct StrangenessInJetsIons {
         TVector3 v0dir(v0.px(), v0.py(), v0.pz());
 
         // K0s
-        if (passedK0ShortSelection(v0, pos, neg, vtxPos)) {
+        if (passedK0ShortSelectionMB(v0, pos, neg, vtxPos)) {
           registryDataMB.fill(HIST("K0s_in_MB"), centrality, v0.pt(), v0.mK0Short());
         }
         // Lambda
-        if (passedLambdaSelection(v0, pos, neg, vtxPos)) {
+        if (passedLambdaSelectionMB(v0, pos, neg, vtxPos)) {
           registryDataMB.fill(HIST("Lambda_in_MB"), centrality, v0.pt(), v0.mLambda());
         }
         // AntiLambda
-        if (passedAntiLambdaSelection(v0, pos, neg, vtxPos)) {
+        if (passedAntiLambdaSelectionMB(v0, pos, neg, vtxPos)) {
           registryDataMB.fill(HIST("AntiLambda_in_MB"), centrality, v0.pt(), v0.mAntiLambda());
         }
       }
@@ -3317,19 +3375,19 @@ struct StrangenessInJetsIons {
         TVector3 cascadeDir(casc.px(), casc.py(), casc.pz());
 
         // Xi+
-        if (passedXiSelection(casc, pos, neg, bach, collision) && bach.sign() > 0) {
+        if (passedXiSelectionMB(casc, pos, neg, bach, collision) && bach.sign() > 0) {
           registryDataMB.fill(HIST("XiPos_in_MB"), centrality, casc.pt(), casc.mXi());
         }
         // Xi-
-        if (passedXiSelection(casc, pos, neg, bach, collision) && bach.sign() < 0) {
+        if (passedXiSelectionMB(casc, pos, neg, bach, collision) && bach.sign() < 0) {
           registryDataMB.fill(HIST("XiNeg_in_MB"), centrality, casc.pt(), casc.mXi());
         }
         // Omega+
-        if (passedOmegaSelection(casc, pos, neg, bach, collision) && bach.sign() > 0) {
+        if (passedOmegaSelectionMB(casc, pos, neg, bach, collision) && bach.sign() > 0) {
           registryDataMB.fill(HIST("OmegaPos_in_MB"), centrality, casc.pt(), casc.mOmega());
         }
         // Omega-
-        if (passedOmegaSelection(casc, pos, neg, bach, collision) && bach.sign() < 0) {
+        if (passedOmegaSelectionMB(casc, pos, neg, bach, collision) && bach.sign() < 0) {
           registryDataMB.fill(HIST("OmegaNeg_in_MB"), centrality, casc.pt(), casc.mOmega());
         }
       }
