34518f6942df39eb5178d5eaf0f053b3cc914f7b
ariffero <andrea.giovanni.riffero@cern.ch>
2026-05-21 15:48:04 +0200

    Fix o2linter warnings

--- PWGUD/Tasks/FwdMuonsUPC.cxx
+++ PWGUD/Tasks/FwdMuonsUPC.cxx
@@ -772,7 +772,7 @@ struct FwdMuonsUPC {
     for (const auto& item : tracksPerCand) {
       if (item.second.size() != k2Tracks) {
         LOGF(debug, "mc parts = %d", item.second.size());
-        for (const auto id : item.second) {
+        for (const auto& id : item.second) {
           auto p = McParts.iteratorAt(id);
           LOGF(debug,
                "  part %d: pdg=%d status=%d has_mothers=%d has_daughters=%d",
