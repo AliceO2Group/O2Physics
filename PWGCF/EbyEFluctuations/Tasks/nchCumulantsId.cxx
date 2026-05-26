9a23fa80febcf2fda51369c36a7bfd78ff55933a
Pravata Panigrahi <158029188+pravatp98@users.noreply.github.com>
2026-05-26 18:16:32 +0530

    Fix include order for ASoAHelpers in nchCumulantsId.cxx

--- PWGCF/EbyEFluctuations/Tasks/nchCumulantsId.cxx
+++ PWGCF/EbyEFluctuations/Tasks/nchCumulantsId.cxx
@@ -23,10 +23,10 @@
 
 #include <CCDB/BasicCCDBManager.h>
 #include <CommonConstants/PhysicsConstants.h>
+#include <Framework/ASoAHelpers.h>
 #include <Framework/AnalysisDataModel.h>
 #include <Framework/AnalysisHelpers.h>
 #include <Framework/AnalysisTask.h>
-#include <Framework/ASoAHelpers.h>
 #include <Framework/Configurable.h>
 #include <Framework/HistogramRegistry.h>
 #include <Framework/HistogramSpec.h>
