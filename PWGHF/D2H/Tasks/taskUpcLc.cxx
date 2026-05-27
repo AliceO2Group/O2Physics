426c1979a04e6491d64d6ff17678004de1ad5b13
Ran Tu <ratufdu@163.com>
2026-05-27 11:38:48 +0200

    Simplify UPC Lc TTree output

--- PWGHF/D2H/Tasks/taskUpcLc.cxx
+++ PWGHF/D2H/Tasks/taskUpcLc.cxx
@@ -71,18 +71,15 @@ namespace full
 DECLARE_SOA_COLUMN(M, m, float);
 DECLARE_SOA_COLUMN(Pt, pt, float);
 DECLARE_SOA_COLUMN(BkgScore, bkgScore, float);
-DECLARE_SOA_COLUMN(PromptScore, promptScore, float);
-DECLARE_SOA_COLUMN(FdScore, fdScore, float);
 DECLARE_SOA_COLUMN(PtProng0, ptProng0, float);
 DECLARE_SOA_COLUMN(PtProng1, ptProng1, float);
 DECLARE_SOA_COLUMN(PtProng2, ptProng2, float);
 DECLARE_SOA_COLUMN(Chi2PCA, chi2PCA, float);
 DECLARE_SOA_COLUMN(DecayLength, decayLength, float);
 DECLARE_SOA_COLUMN(Cpa, cpa, float);
-DECLARE_SOA_COLUMN(PvContributors, pvContributors, float);
-DECLARE_SOA_COLUMN(Multiplicity, multiplicity, float);
+DECLARE_SOA_COLUMN(PvContributors, pvContributors, int);
+DECLARE_SOA_COLUMN(Multiplicity, multiplicity, int);
 DECLARE_SOA_COLUMN(Vtz, vtz, float);
-DECLARE_SOA_COLUMN(AmpFV0A, ampFV0A, float);
 DECLARE_SOA_COLUMN(AmpFT0A, ampFT0A, float);
 DECLARE_SOA_COLUMN(AmpFT0C, ampFT0C, float);
 DECLARE_SOA_COLUMN(ZdcTimeZNA, zdcTimeZNA, float);
@@ -92,7 +89,6 @@ DECLARE_SOA_TABLE(HfUpcQa, "AOD", "HFUPCQA",
                   full::PvContributors,
                   full::Multiplicity,
                   full::Vtz,
-                  full::AmpFV0A,
                   full::AmpFT0A,
                   full::AmpFT0C,
                   full::ZdcTimeZNA,
@@ -102,9 +98,6 @@ DECLARE_SOA_TABLE(HfUpcLcBdtInfos, "AOD", "HFUPCLCBDTINFOS",
                   full::M,
                   full::Pt,
                   full::BkgScore,
-                  full::PromptScore,
-                  full::FdScore,
-                  full::AmpFV0A,
                   full::AmpFT0A,
                   full::AmpFT0C,
                   full::ZdcTimeZNA,
@@ -119,7 +112,6 @@ DECLARE_SOA_TABLE(HfUpcLcInfos, "AOD", "HFUPCLCINFOS",
                   full::Chi2PCA,
                   full::DecayLength,
                   full::Cpa,
-                  full::AmpFV0A,
                   full::AmpFT0A,
                   full::AmpFT0C,
                   full::ZdcTimeZNA,
@@ -256,9 +248,9 @@ struct HfTaskUpcLc {
       if (gap == o2::aod::sgselector::TrueGap::SingleGapA || gap == o2::aod::sgselector::TrueGap::SingleGapC) {
         registry.fill(HIST("Data/hUpcMulti"), collision.multNTracksPV());
         registry.fill(HIST("Data/hUpcVtz"), collision.posZ());
-      }
-      if (fillTreeUpcQa) {
-        rowUpcQa(numPvContributors, collision.multNTracksPV(), collision.posZ(), fitInfo.ampFV0A, fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
+        if (fillTreeUpcQa) {
+          rowUpcQa(numPvContributors, collision.multNTracksPV(), collision.posZ(), fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
+        }
       }
 
       for (const auto& candidate : groupedLcCandidates) {
@@ -291,19 +283,19 @@ struct HfTaskUpcLc {
             /// Fill the ML outputScores and variables of candidate
             if (fillTreeOnlySingleGap) {
               if (gap == o2::aod::sgselector::TrueGap::SingleGapA || gap == o2::aod::sgselector::TrueGap::SingleGapC) {
-                rowCandUpcBdt(massLc, pt, outputBkg, outputPrompt, outputFD, fitInfo.ampFV0A, fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
+                rowCandUpcBdt(massLc, pt, outputBkg, fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
               }
             } else {
-              rowCandUpcBdt(massLc, pt, outputBkg, outputPrompt, outputFD, fitInfo.ampFV0A, fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
+              rowCandUpcBdt(massLc, pt, outputBkg, fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
             }
 
           } else {
             if (fillTreeOnlySingleGap) {
               if (gap == o2::aod::sgselector::TrueGap::SingleGapA || gap == o2::aod::sgselector::TrueGap::SingleGapC) {
-                rowCandUpc(massLc, pt, ptProng0, ptProng1, ptProng2, chi2PCA, decayLength, cpa, fitInfo.ampFV0A, fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
+                rowCandUpc(massLc, pt, ptProng0, ptProng1, ptProng2, chi2PCA, decayLength, cpa, fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
               }
             } else {
-              rowCandUpc(massLc, pt, ptProng0, ptProng1, ptProng2, chi2PCA, decayLength, cpa, fitInfo.ampFV0A, fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
+              rowCandUpc(massLc, pt, ptProng0, ptProng1, ptProng2, chi2PCA, decayLength, cpa, fitInfo.ampFT0A, fitInfo.ampFT0C, zdcTimeZNA, zdcTimeZNC);
             }
           }
         };
