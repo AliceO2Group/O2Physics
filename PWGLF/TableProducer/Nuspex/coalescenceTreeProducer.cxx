3de3ea2d6a89570e966514b01f4d0ea999124e0c
Alberto Caliva <alberto.caliva@cern.ch>
2026-05-28 13:51:02 +0200

    fix clang format

--- PWGLF/TableProducer/Nuspex/coalescenceTreeProducer.cxx
+++ PWGLF/TableProducer/Nuspex/coalescenceTreeProducer.cxx
@@ -71,10 +71,7 @@ struct CoalescenceTreeProducer {
 
   Preslice<aod::McParticles> mcParticlesPerMcCollision = aod::mcparticle::mcCollisionId;
 
-  OutputObj<TTree> treeBoundState{
-    "treeBoundState",
-    OutputObjHandlingPolicy::AnalysisObject
-  };
+  OutputObj<TTree> treeBoundState{"treeBoundState", OutputObjHandlingPolicy::AnalysisObject};
 
   int64_t eventID = 0;                  // Event ID
   int64_t idB1 = 0, idB2 = 0, idB3 = 0; // MC particle IDs
