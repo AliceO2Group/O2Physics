e9d9c68c4af1e1f15223099517d6755987423b8a
ariffero <andrea.giovanni.riffero@cern.ch>
2026-05-29 12:45:38 +0200

    Change V0A veto: veto only sel BC
    Re-implmenting commit 14410d74bc8c58e5c2cc267941d00be133b6dbe1, that was cancelled by commit 189d42375fe8a6afd39e190f34473ecd8638e93c

--- PWGUD/Tasks/fwdMuonsUpc.cxx
+++ PWGUD/Tasks/fwdMuonsUpc.cxx
@@ -414,7 +414,7 @@ struct FwdMuonsUpc {
     const auto& ampsV0A = cand.amplitudesV0A();
     const auto& ampsRelBCsV0A = cand.ampRelBCsV0A();
     for (unsigned int i = 0; i < ampsV0A.size(); ++i) {
-      if (std::abs(ampsRelBCsV0A[i]) <= 1) {
+      if (std::abs(ampsRelBCsV0A[i]) <= 0) {
         if (ampsV0A[i] > maxAmpV0A)
           return;
       }
@@ -615,7 +615,7 @@ struct FwdMuonsUpc {
     const auto& ampsV0A = cand.amplitudesV0A();
     const auto& ampsRelBCsV0A = cand.ampRelBCsV0A();
     for (unsigned int i = 0; i < ampsV0A.size(); ++i) {
-      if (std::abs(ampsRelBCsV0A[i]) <= 1) {
+      if (std::abs(ampsRelBCsV0A[i]) <= 0) {
         if (ampsV0A[i] > maxAmpV0A)
           return;
       }
