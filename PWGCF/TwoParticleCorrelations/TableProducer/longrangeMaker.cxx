eb4edaac88a9d66b89a7226d54a1c69004c2351a
abmodak <67369858+abmodak@users.noreply.github.com>
2026-06-01 21:35:54 +0200

    Update in event selection criteria

--- PWGCF/TwoParticleCorrelations/TableProducer/longrangeMaker.cxx
+++ PWGCF/TwoParticleCorrelations/TableProducer/longrangeMaker.cxx
@@ -83,7 +83,7 @@ using namespace o2::constants::math;
 auto static constexpr CintZero = 0;
 auto static constexpr KminFt0cCell = 96;
 auto static constexpr TotFt0Channels = 208;
-AxisSpec axisEvent{15, 0.5, 15.5, "#Event", "EventAxis"};
+AxisSpec axisEvent{20, 0.5, 20.5, "#Event", "EventAxis"};
 AxisSpec axisTrackSel{10, 0.5, 10.5, "#Track", "TrackAxis"};
 auto static constexpr KminCharge = 3.0f;
 static constexpr std::string_view species[] = {"Pi", "Ka", "Pr"};
@@ -252,11 +252,13 @@ struct LongrangeMaker {
     x->SetBinLabel(6, "ApplyGoodZvtxFT0vsPV");
     x->SetBinLabel(7, "ApplyGoodITSLayersAll");
     x->SetBinLabel(8, "ApplyExtraCorrCut");
-    x->SetBinLabel(9, "ApplyNoCollInTimeRangeStandard");
-    x->SetBinLabel(10, "ApplyNoCollInRofStandard");
-    x->SetBinLabel(11, "ApplyNoHighMultCollInPrevRof");
-    x->SetBinLabel(12, "ApplyOccupancySelection");
-    x->SetBinLabel(13, "reject flange event");
+    x->SetBinLabel(9, "ApplyNoCollInRofStandard");
+    x->SetBinLabel(10, "ApplyNoCollInRofStrict");
+    x->SetBinLabel(11, "ApplyNoCollInTimeRangeStandard");
+    x->SetBinLabel(12, "ApplyNoCollInTimeRangeStrict");
+    x->SetBinLabel(13, "ApplyNoHighMultCollInPrevRof");
+    x->SetBinLabel(14, "ApplyOccupancySelection");
+    x->SetBinLabel(15, "reject flange event");
     histos.add("hSelectionResult", "hSelectionResult", kTH1I, {{5, -0.5, 4.5}});
 
     histos.add("hMftTrkSel", "hMftTrkSel", kTH1D, {axisTrackSel}, false);
@@ -425,7 +427,7 @@ struct LongrangeMaker {
           return;
         }
       }
-      histos.fill(HIST("EventHist"), 13);
+      histos.fill(HIST("EventHist"), 15);
       for (std::size_t iCh = 0; iCh < ft0.channelA().size(); iCh++) {
         auto chanelid = ft0.channelA()[iCh];
         float ampl = ft0.amplitudeA()[iCh];
@@ -1088,22 +1090,30 @@ struct LongrangeMaker {
       return false;
     }
     histos.fill(HIST("EventHist"), 8);
-    if (cfgevtsel.isApplyNoCollInTimeRangeStandard && !col.selection_bit(o2::aod::evsel::kNoCollInTimeRangeStandard)) {
-      return false;
-    }
-    histos.fill(HIST("EventHist"), 9);
     if (cfgevtsel.isApplyNoCollInRofStandard && !col.selection_bit(o2::aod::evsel::kNoCollInRofStandard)) {
       return false;
     }
+    histos.fill(HIST("EventHist"), 9);
+    if (cfgevtsel.isApplyNoCollInRofStrict && !col.selection_bit(o2::aod::evsel::kNoCollInRofStrict)) {
+      return false;
+    }
     histos.fill(HIST("EventHist"), 10);
-    if (cfgevtsel.isApplyNoHighMultCollInPrevRof && !col.selection_bit(o2::aod::evsel::kNoHighMultCollInPrevRof)) {
+    if (cfgevtsel.isApplyNoCollInTimeRangeStandard && !col.selection_bit(o2::aod::evsel::kNoCollInTimeRangeStandard)) {
       return false;
     }
     histos.fill(HIST("EventHist"), 11);
-    if (cfgevtsel.isApplyOccuSelection && (col.trackOccupancyInTimeRange() > cfgevtsel.cfgOccuCut)) {
+    if (cfgevtsel.isApplyNoCollInTimeRangeStrict && !col.selection_bit(o2::aod::evsel::kNoCollInTimeRangeStrict)) {
       return false;
     }
     histos.fill(HIST("EventHist"), 12);
+    if (cfgevtsel.isApplyNoHighMultCollInPrevRof && !col.selection_bit(o2::aod::evsel::kNoHighMultCollInPrevRof)) {
+      return false;
+    }
+    histos.fill(HIST("EventHist"), 13);
+    if (cfgevtsel.isApplyOccuSelection && (col.trackOccupancyInTimeRange() > cfgevtsel.cfgOccuCut)) {
+      return false;
+    }
+    histos.fill(HIST("EventHist"), 14);
     return true;
   }
 
