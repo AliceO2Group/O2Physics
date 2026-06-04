eff41dce344f131ff9f9bc3a44027156f1977650
Ravindra Singh <56298081+singhra1994@users.noreply.github.com>
2026-06-04 17:10:29 +0200

    revert back to the original

--- PWGHF/HFC/Utils/utilsCorrelations.h
+++ PWGHF/HFC/Utils/utilsCorrelations.h
@@ -96,7 +96,7 @@ bool passPIDSelection(Atrack const& track, SpeciesContainer const mPIDspecies,
     auto const& pid = mPIDspecies->at(speciesIndex);
     auto nSigmaTPC = o2::aod::pidutils::tpcNSigma(pid, track);
 
-    if ((track.pt() > ptThreshold) && !track.hasTOF()) {
+    if (tofForced && !track.hasTOF()) {
       return false;
     }
 
