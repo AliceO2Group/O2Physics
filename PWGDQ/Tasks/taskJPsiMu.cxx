28f5ca6411cdc73bf126d7da0742ddb148f186da
Kaare Endrup <kaare.endrup@gmail.com>
2026-06-09 14:19:54 +0200

    Reverse deltaEta/Phi

--- PWGDQ/Tasks/taskJPsiMu.cxx
+++ PWGDQ/Tasks/taskJPsiMu.cxx
@@ -202,8 +202,8 @@ struct DqJPsiMuonCorrelations {
           }
 
           // Compute deltaEta and deltaPhi between the dilepton and the associated muon
-          float deltaEta = track.eta() - dilepton.eta();
-          float deltaPhi = track.phi() - dilepton.phi();
+          float deltaEta = dilepton.eta() - track.eta();
+          float deltaPhi = dilepton.phi() - track.phi();
           if (deltaPhi < -constants::math::PI/2.0f) {
             deltaPhi += 2.0f * constants::math::PI;
           } else if (deltaPhi > constants::math::PI*3.0f/2.0f) {
