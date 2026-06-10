6cdceb398329bf1ad9cfae234a67583a10321860
AlexianL <lejeune.alexian@hotmail.com>
2026-06-10 21:00:24 +0200

    solve issue with abs(), add std::abs()

--- PWGHF/HFC/Tasks/taskFlow.cxx
+++ PWGHF/HFC/Tasks/taskFlow.cxx
@@ -3809,7 +3809,7 @@ struct HfTaskFlow {
                         aod::McParticles const& mcParticles,
                         SmallGroupMcCollisions const& collisions)
   {
-    if (!(nabs(mcCollision.posZ()) <= configCollision.zVertexMax)) {
+    if (!(std::abs(mcCollision.posZ()) <= configCollision.zVertexMax)) {
       return;
     }
 
