415b4efeca23b5b1eb1a53251807d4b3ea8cbedf
rolavick <roman.lavicka@cern.ch>
2026-06-24 10:51:07 +0200

    Modifying includes as suggested by clang-tidy ran locally
    Following these instructions https://aliceo2group.github.io/analysis-framework/docs/tools/#cleaning-include-statements-and-using-statements

--- PWGUD/Tasks/upcVmRof.cxx
+++ PWGUD/Tasks/upcVmRof.cxx
@@ -20,25 +20,32 @@
 #include "Common/DataModel/TrackSelectionTables.h"
 
 #include <CCDB/BasicCCDBManager.h>
-#include <CCDB/CcdbApi.h>
 #include <CommonConstants/LHCConstants.h>
 #include <CommonDataFormat/BunchFilling.h>
-#include <DataFormatsFDD/Digit.h>
-#include <DataFormatsFIT/Triggers.h>
+#include <CommonDataFormat/TimeStamp.h>
 #include <DataFormatsParameters/AggregatedRunInfo.h>
 #include <DataFormatsParameters/GRPLHCIFData.h>
-#include <DataFormatsParameters/GRPMagField.h>
-#include <DataFormatsParameters/GRPObject.h>
 #include <Framework/ASoA.h>
-#include <Framework/ASoAHelpers.h>
 #include <Framework/AnalysisDataModel.h>
+#include <Framework/AnalysisHelpers.h>
 #include <Framework/AnalysisTask.h>
+#include <Framework/Configurable.h>
 #include <Framework/HistogramRegistry.h>
+#include <Framework/HistogramSpec.h>
+#include <Framework/InitContext.h>
 #include <Framework/runDataProcessing.h>
 #include <ReconstructionDataFormats/Vertex.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TString.h>
 
+#include <cstdint>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
 #include <map>
 #include <memory>
+#include <numeric>
 #include <string>
 #include <vector>
 
