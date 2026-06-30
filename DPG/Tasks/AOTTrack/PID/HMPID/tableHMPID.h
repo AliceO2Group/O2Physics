140c25e76d5ab31e74a97b3e1245624ecbc6c558
Rocco Liotino <rocco.liotino@cern.ch>
2026-06-30 10:56:46 +0200

    adding_mc_table_hmpid

--- DPG/Tasks/AOTTrack/PID/HMPID/tableHMPID.h
+++ DPG/Tasks/AOTTrack/PID/HMPID/tableHMPID.h
@@ -1,19 +1,8 @@
-// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
-//
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
-
 #ifndef DPG_TASKS_AOTTRACK_PID_HMPID_TABLEHMPID_H_
 #define DPG_TASKS_AOTTRACK_PID_HMPID_TABLEHMPID_H_
 
-#include <Framework/ASoA.h>
-#include <Framework/AnalysisDataModel.h>
+#include "Framework/ASoA.h"
+#include "Framework/AnalysisDataModel.h"
 
 namespace o2::aod
 {
@@ -34,22 +23,18 @@ DECLARE_SOA_COLUMN(ChargeMip, chargeMip, float);
 DECLARE_SOA_COLUMN(ClusterSize, clusterSize, float);
 DECLARE_SOA_COLUMN(Chamber, chamber, float);
 DECLARE_SOA_COLUMN(PhotonsCharge, photonsCharge, float[kDimPhotonsCharge]);
-
 DECLARE_SOA_COLUMN(EtaTrack, etaTrack, float);
 DECLARE_SOA_COLUMN(PhiTrack, phiTrack, float);
 DECLARE_SOA_COLUMN(Px, px, float);
 DECLARE_SOA_COLUMN(Py, py, float);
 DECLARE_SOA_COLUMN(Pz, pz, float);
-
 DECLARE_SOA_COLUMN(ItsNCluster, itsNCluster, float);
 DECLARE_SOA_COLUMN(TpcNCluster, tpcNCluster, float);
 DECLARE_SOA_COLUMN(TpcNClsCrossedRows, tpcNClsCrossedRows, float);
 DECLARE_SOA_COLUMN(TpcChi2, tpcChi2, float);
 DECLARE_SOA_COLUMN(ItsChi2, itsChi2, float);
-
 DECLARE_SOA_COLUMN(DcaXY, dcaXY, float);
 DECLARE_SOA_COLUMN(DcaZ, dcaZ, float);
-
 DECLARE_SOA_COLUMN(TpcNSigmaPi, tpcNSigmaPi, float);
 DECLARE_SOA_COLUMN(TofNSigmaPi, tofNSigmaPi, float);
 DECLARE_SOA_COLUMN(TpcNSigmaKa, tpcNSigmaKa, float);
@@ -59,7 +44,6 @@ DECLARE_SOA_COLUMN(TofNSigmaPr, tofNSigmaPr, float);
 DECLARE_SOA_COLUMN(TpcNSigmaDe, tpcNSigmaDe, float);
 DECLARE_SOA_COLUMN(TofNSigmaDe, tofNSigmaDe, float);
 DECLARE_SOA_COLUMN(Centrality, centrality, float);
-
 } // namespace variables_table
 
 DECLARE_SOA_TABLE(HmpidAnalysis, "AOD", "HMPIDANALYSIS",
@@ -96,6 +80,28 @@ DECLARE_SOA_TABLE(HmpidAnalysis, "AOD", "HMPIDANALYSIS",
                   variables_table::TpcNSigmaDe,
                   variables_table::TofNSigmaDe,
                   variables_table::Centrality);
+
+// -----------------------------------------------------------------------
+// MC truth table
+// -----------------------------------------------------------------------
+namespace hmpid_mc
+{
+DECLARE_SOA_COLUMN(PdgCode, pdgCode, int);
+DECLARE_SOA_COLUMN(McVx, mcVx, float);
+DECLARE_SOA_COLUMN(McVy, mcVy, float);
+DECLARE_SOA_COLUMN(McVz, mcVz, float);
+DECLARE_SOA_COLUMN(IsPhysPrimary, isPhysPrimary, bool);
+DECLARE_SOA_COLUMN(ProcessCode, processCode, int);
+
+} // namespace hmpid_mc
+
+DECLARE_SOA_TABLE(HmpidAnalysisMC, "AOD", "HMPIDANALYSISMC",
+                  hmpid_mc::PdgCode,
+                  hmpid_mc::McVx,
+                  hmpid_mc::McVy,
+                  hmpid_mc::McVz,
+                  hmpid_mc::IsPhysPrimary,
+                  hmpid_mc::ProcessCode);
 
 } // namespace o2::aod
 
