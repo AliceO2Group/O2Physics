86c6415714a94f21ccf85f4a0e99be4a21e2708c
Nicolas Wirth <74405269+wirthni@users.noreply.github.com>
2026-07-02 17:10:46 +0200

    Add include for Configurable in diffWakeTreeProducer

--- PWGJE/TableProducer/diffWakeTreeProducer.cxx
+++ PWGJE/TableProducer/diffWakeTreeProducer.cxx
@@ -25,6 +25,7 @@
 #include <Framework/AnalysisDataModel.h>
 #include <Framework/AnalysisHelpers.h>
 #include <Framework/AnalysisTask.h>
+#include <Framework/Configurable.h>
 #include <Framework/HistogramRegistry.h>
 #include <Framework/HistogramSpec.h>
 #include <Framework/InitContext.h>
