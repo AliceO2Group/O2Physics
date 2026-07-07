7d79e88f15b48c24b90c54ffde8f5944be50bf58
jesgum <jesper.gumprecht@gmail.com>
2026-07-07 11:22:01 +0200

    Fix fetching of centrality configuration

--- Common/Tasks/centralityStudy.cxx
+++ Common/Tasks/centralityStudy.cxx
@@ -450,45 +450,6 @@ struct centralityStudy {
       reportSuccess(hCentralityNTPV, "NTPV");
       reportSuccess(hCentralityNGlo, "NGlobals");
       reportSuccess(hCentralityMFT, "MFT");
-
-      LOGF(info, "Centrality calibration loading done.");
-      auto getCent = [](TH1* hist, float mult) -> float {
-        return hist ? hist->GetBinContent(mult) : 0.0;
-      };
-
-      centFV0A = getCent(hCentralityFV0A, collision.multFV0A());
-      centFT0A = getCent(hCentralityFT0A, collision.multFT0A());
-      centFT0C = getCent(hCentralityFT0C, collision.multFT0C());
-      centFT0M = getCent(hCentralityFT0M, collision.multFT0A() + collision.multFT0C());
-      centFDDM = getCent(hCentralityFDDM, collision.multFDDA() + collision.multFDDC());
-      centNTPV = getCent(hCentralityNTPV, collision.multNTracksPV());
-      centNGlo = getCent(hCentralityNGlo, collision.multNTracksGlobal());
-      centMFT = getCent(hCentralityMFT, collision.mftNtracks());
-    } else if (doprocessCollisionsWithCentrality) {
-      if constexpr (requires { collision.centFV0A(); }) {
-        centFV0A = collision.centFV0A();
-      }
-      if constexpr (requires { collision.centFT0A(); }) {
-        centFT0A = collision.centFT0A();
-      }
-      if constexpr (requires { collision.centFT0C(); }) {
-        centFT0C = collision.centFT0C();
-      }
-      if constexpr (requires { collision.centFT0M(); }) {
-        centFT0M = collision.centFT0M();
-      }
-      if constexpr (requires { collision.centFDDM(); }) {
-        centFDDM = collision.centFDDM();
-      }
-      if constexpr (requires { collision.centNTPV(); }) {
-        centNTPV = collision.centNTPV();
-      }
-      if constexpr (requires { collision.centNGlobal(); }) {
-        centNGlo = collision.centNGlobal();
-      }
-      if constexpr (requires { collision.centMFT(); }) {
-        centMFT = collision.centMFT();
-      }
     }
 
     if (!studies.doRunByRunHistograms) {
@@ -591,6 +552,52 @@ struct centralityStudy {
       histPointers.insert({histPath + "hPVzProfileCoVsTime", histos.add((histPath + "hPVzProfileCoVsTime").c_str(), "hPVzProfileCoVsTime", {kTProfile, {{axisDeltaTimestamp}}})});
       histPointers.insert({histPath + "hPVzProfileBcVsTime", histos.add((histPath + "hPVzProfileBcVsTime").c_str(), "hPVzProfileBcVsTime", {kTProfile, {{axisDeltaTimestamp}}})});
       histPointers.insert({histPath + "hIRProfileVsTime", histos.add((histPath + "hIRProfileVsTime").c_str(), "hIRProfileVsTime", {kTProfile, {{axisDeltaTimestamp}}})});
+    }
+  }
+
+  template <typename TCollision>
+  void configureCentrality(const TCollision& collision)
+  {
+    if (ccdbSettings.fetchCentralityCalibration) {
+      LOGF(info, "Centrality calibration loading done.");
+      auto getCent = [](TH1* hist, float mult) -> float {
+        return hist ? hist->GetBinContent(mult) : 0.0;
+      };
+
+      centFV0A = getCent(hCentralityFV0A, collision.multFV0A());
+      centFT0A = getCent(hCentralityFT0A, collision.multFT0A());
+      centFT0C = getCent(hCentralityFT0C, collision.multFT0C());
+      centFT0M = getCent(hCentralityFT0M, collision.multFT0A() + collision.multFT0C());
+      centFDDM = getCent(hCentralityFDDM, collision.multFDDA() + collision.multFDDC());
+      centNTPV = getCent(hCentralityNTPV, collision.multNTracksPV());
+      centNGlo = getCent(hCentralityNGlo, collision.multNTracksGlobal());
+      centMFT = getCent(hCentralityMFT, collision.mftNtracks());
+    } else if (doprocessCollisionsWithCentrality) {
+      if constexpr (requires { collision.centFV0A(); }) {
+        centFV0A = collision.centFV0A();
+      }
+      if constexpr (requires { collision.centFT0A(); }) {
+        centFT0A = collision.centFT0A();
+      }
+      if constexpr (requires { collision.centFT0C(); }) {
+        centFT0C = collision.centFT0C();
+      }
+      if constexpr (requires { collision.centFT0M(); }) {
+        centFT0M = collision.centFT0M();
+      }
+      if constexpr (requires { collision.centFDDM(); }) {
+        centFDDM = collision.centFDDM();
+      }
+      if constexpr (requires { collision.centNTPV(); }) {
+        centNTPV = collision.centNTPV();
+      }
+      if constexpr (requires { collision.centNGlobal(); }) {
+        centNGlo = collision.centNGlobal();
+      }
+      if constexpr (requires { collision.centMFT(); }) {
+        centMFT = collision.centMFT();
+      }
+    }
     }
   }
 
