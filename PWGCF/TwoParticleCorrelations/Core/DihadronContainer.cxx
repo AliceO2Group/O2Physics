2315f8e5a61cf90ab963048bc829606a5a880883
Luzhiyong <71517277+Luzhiyongg@users.noreply.github.com>
2026-07-09 16:05:59 +0800

    create a minimum encapsulated container for di-hadron correlations

---
+++ PWGCF/TwoParticleCorrelations/Core/DihadronContainer.cxx
@@ -0,0 +1,210 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file DihadronContainer.h
+/// \brief minimum encapsulated container for di-hadron correlations
+/// \author Zhiyong Lu (zhiyong.lu@cern.ch)
+/// \since  July/2026
+
+#include "PWGCF/TwoParticleCorrelations/Core/DihadronContainer.h"
+
+#include <CommonConstants/MathConstants.h>
+#include <Framework/HistogramSpec.h>
+#include <Framework/Logger.h>
+#include <Framework/StepTHn.h>
+
+#include <TCanvas.h>
+#include <TCollection.h>
+#include <TF1.h>
+#include <THn.h>
+#include <TIterator.h>
+#include <TList.h>
+#include <TMath.h>
+#include <TMathBase.h>
+#include <TNamed.h>
+#include <TObject.h>
+#include <TString.h>
+
+#include <Rtypes.h>
+#include <RtypesCore.h>
+
+#include <cstring>
+#include <vector>
+
+using namespace o2;
+using namespace o2::framework;
+using namespace o2::constants::math;
+
+ClassImp(DihadronContainer);
+
+DihadronContainer::DihadronContainer() : TNamed(),
+                                         mCorrHist(nullptr)
+{
+    // Default constructor
+}
+
+
+DihadronContainer::DihadronContainer(const char* name, const char* objTitle, 
+                                     const std::vector<o2::framework::AxisSpec>& correlationAxis,
+                                     const uint8_t nStep) : TNamed(name, objTitle),
+                                                            mCorrHist(nullptr),
+                                                            nCorrStep(nStep)
+{
+
+  if (strlen(name) == 0) {
+    return;
+  }
+
+  std::vector<o2::framework::AxisSpec> pairAxis(correlationAxis);
+  long bins = 1;
+  LOGF(info, "Creating DihadronContainer:");
+  for (uint iAxis = 0; iAxis < pairAxis.size(); iAxis++) {
+    LOGF(info, "pairAxis[%d].getNbins() = %d", iAxis, pairAxis[iAxis].getNbins());
+    bins *= pairAxis[iAxis].getNbins();
+  }
+  LOGF(info, "DihadronContainer with %ld bins in the correlation histogram (approx. %ld-%ld MB of memory)", bins, bins * 4 / 1024 / 1024, bins * 8 / 1024 / 1024);
+
+  mCorrHist = HistFactory::createHist<StepTHnF>({"mCorrHist", "d^{2}N_{ch}/d#varphid#eta", {HistType::kStepTHnF, pairAxis, nStep}}).release();
+
+}
+
+//_____________________________________________________________________________
+DihadronContainer::DihadronContainer(const DihadronContainer& c) : TNamed(c),
+                                                                   mCorrHist(nullptr),
+                                                                   nCorrStep(1)
+{
+  // DihadronContainer copy constructor
+  ((DihadronContainer&)c).Copy(*this);
+}
+
+//____________________________________________________________________
+DihadronContainer::~DihadronContainer()
+{
+  // Destructor
+  if (mCorrHist) {
+    delete mCorrHist;
+    mCorrHist = nullptr;
+  }
+}
+
+//____________________________________________________________________
+DihadronContainer& DihadronContainer::operator=(const DihadronContainer& c)
+{
+  // assigment operator
+  if (this != &c) {
+    ((DihadronContainer&)c).Copy(*this);
+  }
+  return *this;
+}
+
+//____________________________________________________________________
+void DihadronContainer::Copy(TObject& c) const
+{
+  // copy function
+  DihadronContainer& target = (DihadronContainer&)c;
+
+  if (mCorrHist) {
+    target.mCorrHist = dynamic_cast<StepTHn*>(mCorrHist->Clone());
+  }
+
+  target.nCorrStep = nCorrStep;
+}
+
+//____________________________________________________________________
+void DihadronContainer::deepCopy(DihadronContainer* from)
+{
+  // copies the entries of this object's members from the object <from> to this object
+  // fills using the fill function and thus allows that the objects have different binning
+
+  for (Int_t step = 0; step < mCorrHist->getNSteps(); step++) {
+    LOGF(info, "Copying step %d", step);
+    THnBase* target = mCorrHist->getTHn(step);
+    THnBase* source = from->mCorrHist->getTHn(step);
+
+    target->Reset();
+    target->RebinnedAdd(source);
+  }
+}
+
+
+//____________________________________________________________________
+Long64_t DihadronContainer::Merge(TCollection* list)
+{
+  // Merge a list of DihadronContainer objects with this
+  // Returns the number of merged objects (including this).
+
+  if (!list) {
+    return 0;
+  }
+
+  if (list->IsEmpty()) {
+    return 1;
+  }
+
+  TIterator* iter = list->MakeIterator();
+  TObject* obj = nullptr;
+
+  // collections of objects
+  const UInt_t kMaxLists = 1;
+  TList** lists = new TList*[kMaxLists];
+
+  for (UInt_t i = 0; i < kMaxLists; i++) {
+    lists[i] = new TList;
+  }
+
+  Int_t count = 0;
+  while ((obj = iter->Next())) {
+
+    DihadronContainer* entry = dynamic_cast<DihadronContainer*>(obj);
+    if (entry == nullptr) {
+      continue;
+    }
+
+    if (entry->mCorrHist) {
+      lists[0]->Add(entry->mCorrHist);
+    }
+
+    count++;
+  }
+
+  if (mCorrHist) {
+    mCorrHist->Merge(lists[0]);
+  }
+
+  for (UInt_t i = 0; i < kMaxLists; i++) {
+    delete lists[i];
+  }
+
+  delete[] lists;
+
+  return count + 1;
+}
+
+//____________________________________________________________________
+void DihadronContainer::Reset()
+{
+  // resets all contained histograms
+
+  for (Int_t step = 0; step < mCorrHist->getNSteps(); step++) {
+    mCorrHist->getTHn(step)->Reset();
+  }
+
+}
+
+THnBase* DihadronContainer::changeToThn(THnBase* sparse)
+{
+  // change the object to THn for faster processing
+
+  return THn::CreateHn(Form("%s_thn", sparse->GetName()), sparse->GetTitle(), sparse);
+}
+
+
+ClassImp(DihadronContainer);
---
+++ PWGCF/TwoParticleCorrelations/Core/DihadronContainer.h
@@ -0,0 +1,71 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef PWGCF_TWOPARTICLECORRELATIONS_CORE_DIHADRONCONTAINER_H_
+#define PWGCF_TWOPARTICLECORRELATIONS_CORE_DIHADRONCONTAINER_H_
+
+/// \file DihadronContainer.h
+/// \brief minimum encapsulated container for di-hadron correlations
+/// \author Zhiyong Lu (zhiyong.lu@cern.ch)
+/// \since  July/2026
+
+#include <Framework/HistogramSpec.h>
+
+#include <TNamed.h>
+
+#include <Rtypes.h>
+#include <RtypesCore.h>
+
+#include <vector>
+
+class TH1;
+class TH1F;
+class TH3;
+class TH3F;
+class TH2F;
+class TH1D;
+class TH2;
+class TH2D;
+class TCollection;
+class THnSparse;
+class THnBase;
+class StepTHn;
+
+class DihadronContainer : public TNamed
+{
+    public:
+        DihadronContainer();
+        DihadronContainer(const char* name, const char* objTitle, const std::vector<o2::framework::AxisSpec>& corrAxis, const uint8_t nStep = 1);
+        virtual ~DihadronContainer();
+
+        StepTHn* getCorrHist() { return mCorrHist; }
+        void setCorrHist(StepTHn* hist) { mCorrHist = hist; }
+        void printCorrHist();
+
+        DihadronContainer(const DihadronContainer& c);
+        DihadronContainer& operator=(const DihadronContainer& corr);
+        virtual void Copy(TObject& c) const; // NOLINT: Making this override breaks compilation for unknown reason
+        void deepCopy(DihadronContainer* from);
+
+        virtual Long64_t Merge(TCollection* list);
+        void Reset();
+        THnBase* changeToThn(THnBase* sparse);
+
+    protected:
+        StepTHn* mCorrHist;            // container for n-dimension correlations
+        uint8_t nCorrStep;
+
+    ClassDef(DihadronContainer, 2) // underlying event histogram container
+};
+
+
+
+#endif // PWGCF_TWOPARTICLECORRELATIONS_CORE_DIHADRONCONTAINER_H_
