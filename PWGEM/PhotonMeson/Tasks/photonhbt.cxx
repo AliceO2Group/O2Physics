3af5d343fea72a16abe0b1af6828f6088341ecdb
Stefanie Mrozinski <stefanie.mrozinski@stud.uni-frankfurt.de>
2026-07-13 13:33:43 +0200

    Fix clang-format

--- PWGEM/PhotonMeson/Tasks/photonhbt.cxx
+++ PWGEM/PhotonMeson/Tasks/photonhbt.cxx
@@ -2512,8 +2512,8 @@ struct Photonhbt {
             poolBin.pop_front();
         }
       } // end cfgDoTruthMix
-    }   // end collision loop
-  }     // end runTruthEfficiency
+    } // end collision loop
+  } // end runTruthEfficiency
 
   /*************************************************/
   // PROCESS FUNCTIONS
