17defcebd2a5cc82f5b2a5cebb2f627b98f2b3a9
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2026-07-17 07:56:23 +0200

    [PWGLF] Compile headers (#16996)

---
+++ PWGLF/Utils/decay3bodyBuilderHelper.cxx
@@ -0,0 +1,18 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file decay3bodyBuilderHelper.cxx
+/// \brief Helper file to provide compilation command for headers.
+///
+/// \author Vít Kučera <vit.kucera@cern.ch>, Inha University
+
+// o2-linter: disable=name/workflow-file (not a workflow file)
+#include "decay3bodyBuilderHelper.h" // IWYU pragma: keep
