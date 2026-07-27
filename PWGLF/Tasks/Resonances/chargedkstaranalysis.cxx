f0fcc0be4b1c917bb48f14aceebd5d02df995bcf
Navneet <navneet.kumar@cern.ch>
2026-07-27 23:00:41 +0530

    To fix the ccdb initializer

--- PWGLF/Tasks/Resonances/chargedkstaranalysis.cxx
+++ PWGLF/Tasks/Resonances/chargedkstaranalysis.cxx
@@ -158,7 +158,7 @@ struct Chargedkstaranalysis {
 
   Service<o2::ccdb::BasicCCDBManager> ccdb{};
   Service<o2::framework::O2DatabasePDG> pdg{};
-  o2::ccdb::CcdbApi ccdbApi{};
+  o2::ccdb::CcdbApi ccdbApi;
 
   Configurable<std::string> cfgURL{"cfgURL", "http://alice-ccdb.cern.ch", "Address of the CCDB to browse"};
   // Configurable<int64_t> nolaterthan{"ccdb-no-later-than", std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(), "Latest acceptable timestamp of creation for the object"};
