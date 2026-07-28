ebfff6d6d23c43b060475e3c0d9b0fa4ad0e562f
Jifeng Deng <jifeng.deng@cern.ch>
2026-07-28 15:59:56 +0800

    Fix the reserve size problem in asymmetric pairing of tableReader_withAssoc
    In the original code, ditrack table reserves based on the number of tracks rather than on the number of pairs, which may cause the crash of the program.
    
    Adapt the similar strategy as what has been done in same event pairing to fix the problem.

--- PWGDQ/Tasks/tableReader_withAssoc.cxx
+++ PWGDQ/Tasks/tableReader_withAssoc.cxx
@@ -3485,8 +3485,23 @@ struct AnalysisAsymmetricPairing {
 
     int sign1 = 0;
     int sign2 = 0;
-    ditrackList.reserve(assocs.size());
-    ditrackExtraList.reserve(assocs.size());
+
+    //Reserve capacity for the output tables
+    int64_t reserveSize = 0;
+    for (auto const& event : events) {
+      if (!event.isEventSelected_bit(0)) {
+        continue;
+      }
+      if (fConfigRemoveCollSplittingCandidates.value && event.isEventSelected_bit(2)) {
+        continue; 
+      }
+      auto groupedLegAAssocs = legACandidateAssocs.sliceBy(preslice, event.globalIndex());
+      auto groupedLegBAssocs = legBCandidateAssocs.sliceBy(preslice, event.globalIndex());
+      reserveSize += static_cast<int64_t>(groupedLegAAssocs.size()) *
+                    static_cast<int64_t>(groupedLegBAssocs.size());
+    }
+    ditrackList.reserve(reserveSize);
+    ditrackExtraList.reserve(reserveSize);
 
     constexpr bool trackHasCov = ((TTrackFillMap & VarManager::ObjTypes::TrackCov) > 0 || (TTrackFillMap & VarManager::ObjTypes::ReducedTrackBarrelCov) > 0);
 
