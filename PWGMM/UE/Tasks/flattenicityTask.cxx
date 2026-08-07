33bec9e73094d7aa8ea87239619d651e9b5bc553
PaolaVT <paola.vargas.torres@cern.ch>
2026-08-07 00:42:15 -0600

    Fix copyright for flattenicityTask

--- PWGMM/UE/Tasks/flattenicityTask.cxx
+++ PWGMM/UE/Tasks/flattenicityTask.cxx
@@ -1,8 +1,3 @@
-/// \file flattenicityTask.cxx
-/// \brief Flattenicity analysis task for UE studies
-/// \author Eisha Rani
-/// \since August 2026
-
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
@@ -13,6 +8,11 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
+
+/// \file flattenicityTask.cxx
+/// \brief Flattenicity analysis task for UE studies
+/// \author Eisha Rani
+/// \since August 2026
 
 #include "Common/Core/TrackSelection.h"
 #include "Common/Core/TrackSelectionDefaults.h"
