b4f16fd3ff1b3464c93a8a4ec930ba351d0ad0ce
Fabrizio Chinu <91954233+fchinu@users.noreply.github.com>
2026-08-12 17:09:58 +0200

    [PWGHF] Fix unused headers in HF tree creators (#17430)

--- PWGHF/TableProducer/treeCreatorD0ToKPi.cxx
+++ PWGHF/TableProducer/treeCreatorD0ToKPi.cxx
@@ -21,7 +21,6 @@
 #include "PWGHF/Core/HfHelper.h"
 #include "PWGHF/DataModel/CandidateReconstructionTables.h"
 #include "PWGHF/DataModel/CandidateSelectionTables.h"
-#include "PWGHF/Utils/utilsAnalysis.h"
 
 #include "Common/Core/RecoDecay.h"
 
