c4b488316935b1fd205f834797f8fcf59fa2a8e7
Alexander Tiekötter <alexander.tiekoetter@cern.ch>
2026-08-20 11:27:04 +0200

    Update ALICE3 centrality task

--- ALICE3/TableProducer/alice3Centrality.cxx
+++ ALICE3/TableProducer/alice3Centrality.cxx
@@ -10,6 +10,8 @@
 // or submit itself to any jurisdiction.
 /// \author Nicolo' Jacazio <nicolo.jacazio@cern.ch>, CERN
 /// \author David Dobrigkeit Chinellato <david.dobrigkeit.chinellato@cern.ch>, UNICAMP/CERN
+/// \brief Centrality producer for ALICE3
+/// \file alice3Centrality.cxx
 
 #include "Common/DataModel/Centrality.h"
 #include "Common/DataModel/TrackSelectionTables.h"
@@ -25,6 +27,7 @@
 #include <Framework/InitContext.h>
 #include <Framework/OutputObjHeader.h>
 #include <Framework/runDataProcessing.h>
+#include <Framework/Expressions.h>
 
 #include <TH1.h>
 #include <TString.h>
@@ -36,63 +39,60 @@ using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
-struct ALICE3Centrality {
+constexpr float DefaultMult = 10000.0f;
+
+struct Alice3Centrality {
   Produces<aod::CentRun2V0Ms> cent;
   Service<o2::ccdb::BasicCCDBManager> ccdb;
 
   HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
-  Configurable<float> MinEta{"MinEta", -4.0f, "Minimum eta in range"};
-  Configurable<float> MaxEta{"MaxEta", 4.0f, "Maximum eta in range"};
-  Configurable<float> MaxMult{"MaxMult", 10000.f, "Maximum multiplicity in range"};
-  Configurable<float> MaxDCA{"MaxDCA", 0.0025f, "Max DCAxy and DCAz for counted tracks"};
-  Configurable<std::string> url{"ccdb-url", "http://alice-ccdb.cern.ch", "url of the ccdb repository"};
+  Configurable<int64_t> ccdbNoLaterThan{"ccdbNoLaterThan", 1, "latest acceptable timestamp of creation for the object"};
+  Configurable<float> minEta{"minEta", -4.0f, "Minimum eta in range"};
+  Configurable<float> maxEta{"maxEta", 4.0f, "Maximum eta in range"};
+  Configurable<float> maxMult{"maxMult", 10000.0f, "Maximum multiplicity in range"};
+  Configurable<float> maxDCA{"maxDCA", 0.0025f, "Max DCAxy and DCAz for counted tracks"};
+  Configurable<float> vtxZ{"vtxZ", 10.0f, "Max event vertex z position allowed"};
+  Configurable<int> minNumContrib{"minNumContrib", 1, "Minimum required number of primary vertex contributors"};
+  Configurable<std::string> ccdbUrl{"ccdbUrl", "http://alice-ccdb.cern.ch", "url of the ccdb repository"};
+
+  Filter trackFilter = (aod::track::eta >= minEta) && (aod::track::eta <= maxEta) && (nabs(aod::track::dcaXY) <= maxDCA) && (nabs(aod::track::dcaZ) <= maxDCA);
 
   bool centralityLoaded = false;
   TH1D* hCumMultALICE3 = nullptr;
 
   void init(InitContext&)
   {
-    const AxisSpec axisMult{MaxMult.value > 10000.f ? 10000 : (int)MaxMult, 0, MaxMult, "Reconstructed tracks"};
+    const AxisSpec axisMult{maxMult.value > DefaultMult ? (int)DefaultMult : (int)maxMult, 0, maxMult, "Reconstructed tracks"};
     const AxisSpec axisCent{150, 0, 150, "Percentile"};
-    TString tit = Form("%.3f < #it{#eta} < %.3f", MinEta.value, MaxEta.value);
+    TString tit = Form("%.3f < #it{#eta} < %.3f", minEta.value, maxEta.value);
     histos.add("centrality/numberOfTracks", tit, kTH1D, {axisMult});
     histos.add("centrality/centralityDistribution", "Centrality test", kTH1D, {axisCent});
 
-    ccdb->setURL(url.value);
+    ccdb->setURL(ccdbUrl.value);
     ccdb->setCaching(true);
     ccdb->setLocalObjectValidityChecking();
   }
 
-  int nevs = 0;
-  void process(const o2::aod::Collision& collision, const soa::Join<aod::Tracks, aod::TracksDCA>& tracks)
+  void process(const o2::aod::Collision& collision, const soa::Filtered<soa::Join<aod::Tracks, aod::TracksDCA>>& tracks)
   {
     if (!centralityLoaded) {
-      hCumMultALICE3 = ccdb->getForTimeStamp<TH1D>("Analysis/ALICE3/Centrality", 1);
+      hCumMultALICE3 = ccdb->getForTimeStamp<TH1D>("Analysis/ALICE3/Centrality", ccdbNoLaterThan.value);
       centralityLoaded = true;
       LOGF(info, "ALICE 3 centrality calibration loaded!");
     }
 
-    int nTracks = 0;
-    if (collision.numContrib() < 1) {
+    if (collision.numContrib() < minNumContrib.value) {
       histos.fill(HIST("centrality/centralityDistribution"), 101);
       cent(101);
       return;
     }
-    if (fabs(collision.posZ()) > 10) {
+    if (std::fabs(collision.posZ()) > vtxZ.value) {
       histos.fill(HIST("centrality/centralityDistribution"), 102);
       cent(102);
       return;
     }
-    for (const auto& track : tracks) {
-      if (track.eta() < MinEta || track.eta() > MaxEta) {
-        continue;
-      }
-      if (abs(track.dcaXY()) > MaxDCA || abs(track.dcaZ()) > MaxDCA) {
-        continue;
-      }
-      nTracks++;
-    }
-    LOG(info) << nevs++ << ") Event " << collision.globalIndex() << " has " << nTracks << " tracks";
+
+    const auto nTracks = tracks.size();
     histos.fill(HIST("centrality/numberOfTracks"), nTracks);
 
     float centALICE3 = hCumMultALICE3->GetBinContent(hCumMultALICE3->FindBin(nTracks));
@@ -103,5 +103,5 @@ struct ALICE3Centrality {
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
-  return WorkflowSpec{adaptAnalysisTask<ALICE3Centrality>(cfgc, TaskName{"alice3-centrality"})};
+  return WorkflowSpec{adaptAnalysisTask<Alice3Centrality>(cfgc)};
 }
