a2475915f5587775c7272f4236d80c6a5e2f3230
Alberto Caliva <alberto.caliva@cern.ch>
2026-08-21 22:10:32 +0200

    removed unused libraries

--- PWGLF/Tasks/Nuspex/antinucleiInJetsCDF.cxx
+++ PWGLF/Tasks/Nuspex/antinucleiInJetsCDF.cxx
@@ -28,25 +28,17 @@
 #include <CommonConstants/PhysicsConstants.h>
 #include <Framework/ASoA.h>
 #include <Framework/AnalysisDataModel.h>
-#include <Framework/AnalysisHelpers.h>
 #include <Framework/AnalysisTask.h>
 #include <Framework/Configurable.h>
 #include <Framework/HistogramRegistry.h>
 #include <Framework/HistogramSpec.h>
 #include <Framework/InitContext.h>
-#include <Framework/Logger.h>
 #include <Framework/OutputObjHeader.h>
 #include <Framework/runDataProcessing.h>
-#include <ReconstructionDataFormats/PID.h>
 
-#include <Math/GenVector/Boost.h>
 #include <Math/Vector4D.h> // IWYU pragma: keep (do not replace with Math/Vector4Dfwd.h)
 #include <Math/Vector4Dfwd.h>
-#include <TH1.h>
-#include <TList.h>
 #include <TPDGCode.h>
-#include <TRandom3.h>
-#include <TString.h>
 #include <TVector3.h>
 
 #include <cmath>
