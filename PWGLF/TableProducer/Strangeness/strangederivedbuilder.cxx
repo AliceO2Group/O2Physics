84756971f495526af04b87625b7a9e70bbd2b5b0
SCHOTTER Romain <47983209+romainschotter@users.noreply.github.com>
2026-08-25 18:01:13 +0200

    Adapt strangederivedbuilder to new StraMCMults model

--- PWGLF/TableProducer/Strangeness/strangederivedbuilder.cxx
+++ PWGLF/TableProducer/Strangeness/strangederivedbuilder.cxx
@@ -854,6 +854,7 @@ struct strangederivedbuilder {
       products.strangeMCColl(mccollision.posX(), mccollision.posY(), mccollision.posZ(),
                              mccollision.impactParameter(), mccollision.eventPlaneAngle(), mccollision.generatorsID());
       products.strangeMCMults(mccollision.multMCFT0A(), mccollision.multMCFT0C(),
+                              mccollision.multMCFV0A(), mccollision.multMCFDDA(), mccollision.multMCFDDC(),
                               mccollision.multMCNParticlesEta05(),
                               mccollision.multMCNParticlesEta08(),
                               mccollision.multMCNParticlesEta10(),
