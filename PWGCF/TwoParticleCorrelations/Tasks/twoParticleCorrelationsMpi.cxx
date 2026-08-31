95174f2c3e7cd7255bdfba8570c41829658c26ec
Emil Gorm Nielsen <emilgorm31@gmail.com>
2026-08-31 11:47:47 +0200

    fix lambda capture

--- PWGCF/TwoParticleCorrelations/Tasks/twoParticleCorrelationsMpi.cxx
+++ PWGCF/TwoParticleCorrelations/Tasks/twoParticleCorrelationsMpi.cxx
@@ -336,7 +336,7 @@ struct TwoParticleCorrelationsMpi {
         const float threshold = 1.e-6;
         if (configuredEdges.size() != expectedEdges.size() ||
             !std::equal(configuredEdges.begin(), configuredEdges.end(), expectedEdges.begin(),
-                        [](double lhs, double rhs) { return std::abs(lhs - rhs) < threshold; })) {
+                        [&threshold](double lhs, double rhs) { return std::abs(lhs - rhs) < threshold; })) {
           LOGF(fatal, "Percentile-class Seed axis requires axisUser edges {-1.5,-0.5,0.5,1.5,2.5}");
         }
       }
