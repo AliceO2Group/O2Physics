e123f66f4cdc011eef9eaf634aa5fd9656eabe99
Sushanta Tripathy <sushanta.tripathy@cern.ch>
2026-09-18 09:24:22 +0200

    Update copyright notice and file description

--- PWGLF/Tasks/Resonances/f0phiproxy.cxx
+++ PWGLF/Tasks/Resonances/f0phiproxy.cxx
@@ -1,6 +1,6 @@
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright
-// holders. All rights not expressly granted are reserved.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
@@ -10,9 +10,8 @@
 // or submit itself to any jurisdiction.
 
 /// \file f0phiproxy.cxx
+/// \brief task for dipion f0 candidates with a half-momentum kaon proxy and bachelor kaon.
 /// \author Sushanta Tripathy
-/// \brief Dipion f0 candidates with a half-momentum kaon proxy and bachelor
-/// kaon.
 
 #include "Common/DataModel/EventSelection.h"
 #include "Common/DataModel/Multiplicity.h"
