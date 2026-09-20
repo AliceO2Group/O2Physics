46d391ec079529960c918597282d7dd6f47fdbf1
ayankrmanna <ayan.kumar.manna@cern.ch>
2026-09-21 03:59:44 +0530

    Remove unused AnalysisHelpers include

--- PWGHF/HFC/Tasks/taskCorrelationD0Hadrons.cxx
+++ PWGHF/HFC/Tasks/taskCorrelationD0Hadrons.cxx
@@ -38,7 +38,6 @@
 #include <CommonConstants/PhysicsConstants.h>
 #include <Framework/ASoA.h>
 #include <Framework/AnalysisDataModel.h>
-#include <Framework/AnalysisHelpers.h>
 #include <Framework/AnalysisTask.h>
 #include <Framework/Configurable.h>
 #include <Framework/HistogramRegistry.h>
